5c7de88fb17102327b14f59752fd3552d0c61c14
Wes Hardaker <hardaker@users.sourceforge.net>
1994-10-24 22:00:25 +0000

    Added the extensible mib section. The config file is by default /etc/snmpProcWatch.conf Takes both 'exec' arguments and 'shell' arguments:   exec:   calls execv() to handle the request 	  (a bit faster?, No shell required?)   shell:  calls system() instead.  Allows usage of * chars, and shell           type stuff.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@15 06827809-a52a-0410-b366-d66718629ded

--- agent/extensible/extensible.c
+++ agent/extensible/extensible.c
@@ -10,7 +10,8 @@
 #include "wes.h"
 
 static struct myproc *procwatch;
-int numprocs;
+static struct exstensible *extens;
+int numprocs, numextens;
 
 unsigned char *var_wes_proc(vp, name, length, exact, var_len, write_method)
     register struct variable *vp;
@@ -34,74 +35,51 @@ unsigned char *var_wes_proc(vp, name, length, exact, var_len, write_method)
   long long_ret;
   char errmsg[300];
 
-  fprintf(stderr,"Wes stuff\t");
 
   if (*length == 8) {
-#ifdef DEBUG
-    printf("here: -\n");
-#endif
     bcopy((char *) vp->name, (char *)newname,
           (int)vp->namelen * sizeof (oid));
     newname[8] = 1;
     *length = vp->namelen+1;
   }
   else if (*length != 9) {
-#ifdef DEBUG
-    printf("Too long...  ret\n");
-#endif
     *var_len = NULL;
     return NULL;
   }
   else {
-#ifdef DEBUG
-    printf("here: %d\n",name[8]);
-#endif
     bcopy((char *) vp->name, (char *)newname, (int)vp->namelen * sizeof (oid));
     for(i=0,rtest=0; i < *length-1; i++) {
-#ifdef DEBUG
-      printf(".%d",name[i]);
-#endif
       if (name[i] != vp->name[i]) {
         rtest = 1;
       }
     }
-#ifdef DEBUG
-    printf(".%d: len=%d,%d\n",name[i],*length,vp->namelen);
-#endif
     if (rtest || name[8] >= numprocs) {
-#ifdef DEBUG
-      printf("test1:  %d\n",rtest);
-#endif
       return NULL;
     }
 
     if (!exact) newname[8] = name[8] + 1;
-#ifdef DEBUG
-    printf("in:%d\ttest: %d\texact:%d\tnumproc:%d\n",name[8],rtest,exact,
-           numprocs);
-#endif
   }  
   bcopy((char *)newname, (char *)name, (*length) * sizeof(oid));
   *write_method = 0;
   *var_len = sizeof(long);   /* default */
   if (proc = get_proc_instance(procwatch,newname[8])) {
     switch (vp->magic) {
-      case WESINDEX:
+      case PROCINDEX:
         long_ret = newname[8];
         return((u_char *) (&long_ret));
-      case WESNAMES:
+      case PROCNAMES:
         *var_len = strlen(proc->name);
         return((u_char *) (proc->name));
-      case WESMIN:
+      case PROCMIN:
         long_ret = proc->min;
         return((u_char *) (&long_ret));
-      case WESMAX:
+      case PROCMAX:
         long_ret = proc->max;
         return ((u_char *) (&long_ret));
-      case WESCOUNT:
+      case PROCCOUNT:
         long_ret = sh_count_procs(proc->name);
         return ((u_char *) (& long_ret));
-      case WESERROR:
+      case PROCERROR:
         long_ret = sh_count_procs(proc->name);
         if ((proc->min && long_ret < proc->min) || 
             (proc->max && long_ret > proc->max) ||
@@ -112,7 +90,7 @@ unsigned char *var_wes_proc(vp, name, length, exact, var_len, write_method)
           long_ret = 0;
         }
         return ((u_char *) (& long_ret));
-      case WESERRORMSG:
+      case PROCERRORMSG:
         long_ret = sh_count_procs(proc->name);
         if (proc->min && long_ret < proc->min) {
           sprintf(errmsg,"Too few copies of %s running (# = %d)",
@@ -136,7 +114,87 @@ unsigned char *var_wes_proc(vp, name, length, exact, var_len, write_method)
   return NULL;
 }
 
+unsigned char *var_wes_shell(vp, name, length, exact, var_len, write_method)
+    register struct variable *vp;
+/* IN - pointer to variable entry that points here */
+    register oid	*name;
+/* IN/OUT - input name requested, output name found */
+    register int	*length;
+/* IN/OUT - length of input and output oid's */
+    int			exact;
+/* IN - TRUE if an exact match was requested. */
+    int			*var_len;
+/* OUT - length of variable or 0 if function returned. */
+    int			(**write_method)();
+/* OUT - pointer to function to set variable, otherwise 0 */
+{
+
+  oid newname[30];
+  int count, result,i, rtest=0;
+  register int interface;
+  struct extensible *exten;
+  long long_ret;
+  char errmsg[300];
+
+  if (*length == 8) {
+    bcopy((char *) vp->name, (char *)newname,
+          (int)vp->namelen * sizeof (oid));
+    newname[8] = 1;
+    *length = vp->namelen+1;
+  }
+  else if (*length != 9) {
+    *var_len = NULL;
+    return NULL;
+  }
+  else {
+    bcopy((char *) vp->name, (char *)newname, (int)vp->namelen * sizeof (oid));
+    for(i=0,rtest=0; i < 7; i++) {
+/*      printf(".%d",name[i]); */
+      if (name[i] != vp->name[i]) {
+        rtest = 1;
+      }
+    }
+/*    printf("\nrtest:  %d, name[8]:  %d, num:  %d, exact:  %d\n",rtest,name[8],numextens,exact); */
+    if (!exact) newname[8] = name[8] + 1;
+    if (rtest || newname[8] > numextens)
+      return NULL;
+
+  }  
+  bcopy((char *)newname, (char *)name, (*length) * sizeof(oid));
+  *write_method = 0;
+  *var_len = sizeof(long);   /* default */
+  if (exten = get_exten_instance(extens,newname[8])) {
+    switch (vp->magic) {
+      case SHELLINDEX:
+        long_ret = newname[8];
+        return((u_char *) (&long_ret));
+      case SHELLNAMES:
+        *var_len = strlen(exten->name);
+        return((u_char *) (exten->name));
+      case SHELLCOMMAND:
+        *var_len = strlen(exten->command);
+        return((u_char *) (exten->command));
+      case SHELLRESULT:
+        if (exten->type == EXECPROC)
+          exec_command(exten);
+        else
+          shell_command(exten);
+        return((u_char *) (&exten->result));
+        return((u_char *) (&long_ret));
+      case SHELLOUTPUT:
+        if (exten->type == EXECPROC)
+          exec_command(exten);
+        else
+          shell_command(exten);
+        *var_len = strlen(exten->output);
+        return((u_char *) (exten->output));
+    }
+    return NULL;
+  }
+  return NULL;
+}
+
 
 init_wes() {
-  numprocs = read_config (DEFPROCFILE,&procwatch);
+  read_config (DEFPROCFILE,&procwatch,&numprocs,&extens,&numextens);
 }
