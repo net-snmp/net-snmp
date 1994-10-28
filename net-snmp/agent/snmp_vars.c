28fc67cf6a10b721b7821fa7c0aa7c99bfa8d989
Wes Hardaker <hardaker@users.sourceforge.net>
1994-10-28 20:35:24 +0000

    hp-ux machines now return the correct mib location for their arch type. Added hp-ux to the ifdefs for Route_Scan_Reload() function for correct    kernal reading.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@23 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/snmp_vars.c
+++ net-snmp/agent/snmp_vars.c
@@ -932,8 +932,11 @@ char sysContact[128] = "support@ece.ucdavis.edu";
 char sysName[128] = "Unknown";
 char sysLocation[128] = "UCDavis Electrical Engineering Department";
 
-
+#ifdef hpux
+oid version_id[] = {1, 3, 6, 1, 4, 1, 11, 2, 3, 2, 5};
+#else
 oid version_id[] = {1, 3, 6, 1, 4, 1, 3, 1, 1};
+#endif
 
 u_long
 sysUpTime(){
