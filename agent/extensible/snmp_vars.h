c692ed82a4fd37e00328f7a85af4810dd645376a
Wes Hardaker <hardaker@users.sourceforge.net>
1994-11-19 00:02:04 +0000

    Added some hp-mibs Added more memory/swap variables changed shell output mibs from 4/5 to 6/7 to equal the proc output mibs changed default input file to /etc/ece-snmpd.conf bug fix: namelength >= max was checked with name instead of newname
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@31 06827809-a52a-0410-b366-d66718629ded

--- agent/extensible/snmp_vars.h
+++ agent/extensible/snmp_vars.h
@@ -1,4 +1,5 @@
 u_char *var_wes_shell();
+u_char *var_wes_hp();
 
 /* the variable that stores the process watching mib info */
 struct variable2 wes_proc_variables[] = {
@@ -16,12 +17,30 @@ struct variable2 wes_extensible_variables[] = {
   {SHELLINDEX, INTEGER, RONLY, var_wes_shell, 1, {1 }},
   {SHELLNAMES, STRING, RONLY, var_wes_shell, 1, {2 }}, 
     {SHELLCOMMAND, STRING, RONLY, var_wes_shell, 1, {3 }}, 
-    {SHELLRESULT, INTEGER, RONLY, var_wes_shell, 1, {4 }},
-    {SHELLOUTPUT, STRING, RONLY, var_wes_shell, 1, {5 }}
+    {SHELLRESULT, INTEGER, RONLY, var_wes_shell, 1, {6 }},
+    {SHELLOUTPUT, STRING, RONLY, var_wes_shell, 1, {7 }}
 };
 
 struct variable2 wes_mem_variables[] = {
   {MEMTOTALSWAP, INTEGER, RONLY, var_wes_mem, 1, {1 }},
-  {MEMUSEDSWAP, INTEGER, RONLY, var_wes_mem, 1, {2 }}
+  {MEMUSEDSWAP, INTEGER, RONLY, var_wes_mem, 1, {2 }},
+  {MEMTOTALREAL, INTEGER, RONLY, var_wes_mem, 1, {3 }},
+  {MEMUSEDREAL, INTEGER, RONLY, var_wes_mem, 1, {4 }},
+  {MEMTOTALSWAPTXT, INTEGER, RONLY, var_wes_mem, 1, {5 }},
+  {MEMUSEDSWAPTXT, INTEGER, RONLY, var_wes_mem, 1, {6 }},
+  {MEMTOTALREALTXT, INTEGER, RONLY, var_wes_mem, 1, {7 }},
+  {MEMUSEDREALTXT, INTEGER, RONLY, var_wes_mem, 1, {8 }},
+  {MEMTOTALFREE, INTEGER, RONLY, var_wes_mem, 1, {9 }}
 };
 
+struct variable2 wes_hp_variables[] = {
+  {HPCONF, INTEGER, RWRITE, var_wes_hp, 1, {1 }},
+  {HPRECONFIG, INTEGER, RWRITE, var_wes_hp, 1, {2 }},
+  {HPFLAG, INTEGER, RWRITE, var_wes_hp, 1, {3 }},
+  {HPLOGMASK, INTEGER, RWRITE, var_wes_hp, 1, {4 }},
+  {HPSTATUS, INTEGER, RWRITE, var_wes_hp, 1, {6 }}
+};
+
+struct variable2 wes_hptrap_variables[] = {
+  {HPTRAP, IPADDRESS, RWRITE, var_wes_hp, 1, {1 }},
+};
