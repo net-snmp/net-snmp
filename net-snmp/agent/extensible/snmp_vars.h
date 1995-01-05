3ca2dea48cc6adf4cd0f3d1f1fcd3655f5e709e4
Wes Hardaker <hardaker@users.sourceforge.net>
1995-01-05 00:26:49 +0000

    Feature:   Added the disk watching facilities to report disk space     -- new config option 'disk DISK MINAVAIL'   Added support for a second config file as CONFIGFILETWO     -- defaults to /etc/ece-snmpd.local.conf     -- intent:  one is rdisted and one is machine specific     -- TODO:  Needs to erase previous definitions rather than adding        two wit the same name
    Change:
      MIB definition change:
        -- All mibs with error flags now use ERRORFLAG/ERRORMSG instead.
        -- These default to 100 and 101, and are set by only two defines
      Shortened error message for process errors to fit on the screen.
      Swap mib code cleanup
    
    TODO:
      Added beginnings of kernal locking tests.  Need to find a machine
      with a broken rpc.lockd to test the code though.
    
    Bugs:
      My nlist array didn't have a NULL ending.
    
    
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@45 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/extensible/snmp_vars.h
+++ net-snmp/agent/extensible/snmp_vars.h
@@ -1,46 +1,70 @@
 u_char *var_wes_shell();
+u_char *var_wes_disk();
 u_char *var_wes_hp();
+u_char *var_wes_lockd_test();
 
 /* the variable that stores the process watching mib info */
 struct variable2 wes_proc_variables[] = {
-  {PROCINDEX, INTEGER, RONLY, var_wes_proc, 1, {1 }},
-  {PROCNAMES, STRING, RONLY, var_wes_proc, 1, {2 }}, 
-    {PROCMIN, INTEGER, RONLY, var_wes_proc, 1, {3 }}, 
-    {PROCMAX, INTEGER, RONLY, var_wes_proc, 1, {4 }},
-    {PROCCOUNT, INTEGER, RONLY, var_wes_proc, 1, {5 }},
-    {PROCERROR, INTEGER, RONLY, var_wes_proc, 1, {6 }},
-    {PROCERRORMSG, STRING, RONLY, var_wes_proc, 1, {7 }}
+  {PROCINDEX, INTEGER, RONLY, var_wes_proc, 1, {PROCINDEX}},
+  {PROCNAMES, STRING, RONLY, var_wes_proc, 1, {PROCNAMES}}, 
+    {PROCMIN, INTEGER, RONLY, var_wes_proc, 1, {PROCMIN}}, 
+    {PROCMAX, INTEGER, RONLY, var_wes_proc, 1, {PROCMAX}},
+    {PROCCOUNT, INTEGER, RONLY, var_wes_proc, 1, {PROCCOUNT}},
+    {ERRORFLAG, INTEGER, RONLY, var_wes_proc, 1, {ERRORFLAG}},
+    {ERRORMSG, STRING, RONLY, var_wes_proc, 1, {ERRORMSG}}
 };
 
 /* the extensible commands variables */
 struct variable2 wes_extensible_variables[] = {
-  {SHELLINDEX, INTEGER, RONLY, var_wes_shell, 1, {1 }},
-  {SHELLNAMES, STRING, RONLY, var_wes_shell, 1, {2 }}, 
-    {SHELLCOMMAND, STRING, RONLY, var_wes_shell, 1, {3 }}, 
-    {SHELLRESULT, INTEGER, RONLY, var_wes_shell, 1, {6 }},
-    {SHELLOUTPUT, STRING, RONLY, var_wes_shell, 1, {7 }}
+  {SHELLINDEX, INTEGER, RONLY, var_wes_shell, 1, {SHELLINDEX}},
+  {SHELLNAMES, STRING, RONLY, var_wes_shell, 1, {SHELLNAMES}}, 
+    {SHELLCOMMAND, STRING, RONLY, var_wes_shell, 1, {SHELLCOMMAND}}, 
+    {ERRORFLAG, INTEGER, RONLY, var_wes_shell, 1, {ERRORFLAG}},
+    {ERRORMSG, STRING, RONLY, var_wes_shell, 1, {ERRORMSG}}
+};
+
+/* the lockd test variables */
+struct variable2 wes_lockd_variables[] = {
+  {LOCKDINDEX, INTEGER, RONLY, var_wes_lockd_test, 1, {LOCKDINDEX}},
+    {ERRORFLAG, INTEGER, RONLY, var_wes_lockd_test, 1, {ERRORFLAG}},
+    {ERRORMSG, STRING, RONLY, var_wes_lockd_test, 1, {ERRORMSG}}
 };
 
 struct variable2 wes_mem_variables[] = {
-  {MEMTOTALSWAP, INTEGER, RONLY, var_wes_mem, 1, {1 }},
-  {MEMUSEDSWAP, INTEGER, RONLY, var_wes_mem, 1, {2 }},
-  {MEMTOTALREAL, INTEGER, RONLY, var_wes_mem, 1, {3 }},
-  {MEMUSEDREAL, INTEGER, RONLY, var_wes_mem, 1, {4 }},
-  {MEMTOTALSWAPTXT, INTEGER, RONLY, var_wes_mem, 1, {5 }},
-  {MEMUSEDSWAPTXT, INTEGER, RONLY, var_wes_mem, 1, {6 }},
-  {MEMTOTALREALTXT, INTEGER, RONLY, var_wes_mem, 1, {7 }},
-  {MEMUSEDREALTXT, INTEGER, RONLY, var_wes_mem, 1, {8 }},
-  {MEMTOTALFREE, INTEGER, RONLY, var_wes_mem, 1, {9 }}
+  {MEMTOTALSWAP, INTEGER, RONLY, var_wes_mem, 1, {MEMTOTALSWAP}},
+  {MEMUSEDSWAP, INTEGER, RONLY, var_wes_mem, 1, {MEMUSEDSWAP}},
+  {MEMTOTALREAL, INTEGER, RONLY, var_wes_mem, 1, {MEMTOTALREAL}},
+  {MEMUSEDREAL, INTEGER, RONLY, var_wes_mem, 1, {MEMUSEDREAL}},
+  {MEMTOTALSWAPTXT, INTEGER, RONLY, var_wes_mem, 1, {MEMTOTALSWAPTXT}},
+  {MEMUSEDSWAPTXT, INTEGER, RONLY, var_wes_mem, 1, {MEMUSEDSWAPTXT}},
+  {MEMTOTALREALTXT, INTEGER, RONLY, var_wes_mem, 1, {MEMTOTALREALTXT}},
+  {MEMUSEDREALTXT, INTEGER, RONLY, var_wes_mem, 1, {MEMUSEDREALTXT}},
+  {MEMTOTALFREE, INTEGER, RONLY, var_wes_mem, 1, {MEMTOTALFREE}},
+  {ERRORFLAG, INTEGER, RONLY, var_wes_mem, 1, {ERRORFLAG }},
+  {ERRORMSG, STRING, RONLY, var_wes_mem, 1, {ERRORMSG }}
+};
+
+struct variable2 wes_disk_variables[] = {
+  {DISKINDEX, INTEGER, RONLY, var_wes_disk, 1, {DISKINDEX}},
+  {DISKPATH, STRING, RONLY, var_wes_disk, 1, {DISKPATH}},
+  {DISKDEVICE, STRING, RONLY, var_wes_disk, 1, {DISKDEVICE}},
+  {DISKMINIMUM, INTEGER, RONLY, var_wes_disk, 1, {DISKMINIMUM}},
+  {DISKTOTAL, INTEGER, RONLY, var_wes_disk, 1, {DISKTOTAL}},
+  {DISKAVAIL, INTEGER, RONLY, var_wes_disk, 1, {DISKAVAIL}},
+  {DISKUSED, INTEGER, RONLY, var_wes_disk, 1, {DISKUSED}},
+  {DISKPERCENT, INTEGER, RONLY, var_wes_disk, 1, {DISKPERCENT}},
+  {ERRORFLAG, INTEGER, RONLY, var_wes_disk, 1, {ERRORFLAG }},
+  {ERRORMSG, STRING, RONLY, var_wes_disk, 1, {ERRORMSG }}
 };
 
 struct variable2 wes_hp_variables[] = {
-  {HPCONF, INTEGER, RWRITE, var_wes_hp, 1, {1 }},
-  {HPRECONFIG, INTEGER, RWRITE, var_wes_hp, 1, {2 }},
-  {HPFLAG, INTEGER, RWRITE, var_wes_hp, 1, {3 }},
-  {HPLOGMASK, INTEGER, RWRITE, var_wes_hp, 1, {4 }},
-  {HPSTATUS, INTEGER, RWRITE, var_wes_hp, 1, {6 }}
+  {HPCONF, INTEGER, RWRITE, var_wes_hp, 1, {HPCONF}},
+  {HPRECONFIG, INTEGER, RWRITE, var_wes_hp, 1, {HPRECONFIG}},
+  {HPFLAG, INTEGER, RWRITE, var_wes_hp, 1, {HPFLAG}},
+  {HPLOGMASK, INTEGER, RWRITE, var_wes_hp, 1, {ERRORFLAG}},
+  {HPSTATUS, INTEGER, RWRITE, var_wes_hp, 1, {ERRORMSG}}
 };
 
 struct variable2 wes_hptrap_variables[] = {
-  {HPTRAP, IPADDRESS, RWRITE, var_wes_hp, 1, {1 }},
+  {HPTRAP, IPADDRESS, RWRITE, var_wes_hp, 1, {HPTRAP }},
 };
