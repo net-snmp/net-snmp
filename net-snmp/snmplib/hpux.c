b724f6c619861c872afdcb6ad475e7932b13fc51
Wes Hardaker <hardaker@users.sourceforge.net>
1995-03-14 00:20:58 +0000

    This commit was manufactured by cvs2svn to create tag 'HP-port-1-0'.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/tags/HP-port-1-0@117 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/hpux.c
+++ net-snmp/snmplib/hpux.c
@@ -13,3 +13,18 @@ void srandom(seed)
 }
 
 #endif
+/* hp-port:  needed random defs and htonl and htons defs */
+
+#ifdef hpux
+int random()
+{
+  return(rand());
+}
+
+void srandom(seed)
+  unsigned int seed;
+{
+  srand(seed);
+}
+
+#endif
