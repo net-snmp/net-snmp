fc9a4befa14db3477f2f635c868fb7b3d53474e9
Wes Hardaker <hardaker@users.sourceforge.net>
1995-06-02 22:27:06 +0000

    Reorganized config.h in more or less order of importance Added definitions for SNMPV2ONLY type authorization.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@175 06827809-a52a-0410-b366-d66718629ded

--- acconfig.h
+++ acconfig.h
@@ -1,5 +1,28 @@
 /* config.h:  a general config file */
 
+/* configuration files.  I rdist the first one and leave machine
+   specific stuff in the second one */
+
+#define CONFIGFILE "/etc/ece-snmpd.conf"          /* default config file */
+#define CONFIGFILETWO "/etc/ece-snmpd.local.conf" /* optional second file */
+
+#define SNMPV1      0xAAAA       /* don't change these values! */
+#define SNMPV2ANY   0x2
+#define SNMPV2AUTH  0x4
+
+/* If GLOBALSECURITY is defined, it sets the default SNMP access type
+   for the extensible mibs to the setting type described. */
+
+#define GLOBALSECURITY SNMPV2AUTH    /* only authenticated snmpv2 requests
+                                        permited */
+
+#define SECURITYEXCEPTIONS {100,SNMPV1,-1} /* the ErrorFlag is V1 accessable
+                                              because HP Openview does not
+                                              support V2 */
+/* additional note:  if SECURITYEXCEPTIONS is defined, you must use an
+                     ANSI compiler (gcc) for agent/extensible/extensible.c */
+
+
 /* Mib-2 tree Info */
 
 #if defined(hpux)
@@ -14,19 +37,18 @@
 #define SYS_NAME    "unknown"
 #define SYS_LOC     "UCDavis Electrical Engineering Departement"
 
-/* Logfile.  If defined it closes stdout/err and opens this in their place */
+/* LOGFILE:  If defined it closes stdout/err/in and opens this in out/err's
+   place.  (stdin is closed so that sh scripts won't wait for it) */
 
 #define LOGFILE "/usr/adm/ece-snmpd.log"
 
-/* mib pointer to my tree */
+/* mib pointer to the top of the extensible tree */
 
 #define EXTENSIBLEMIB 1,3,6,1,4,10 /* location of the extensible mib tree */
-#define EXTENSIBLENUM 6            /* count the above */
-
-/* Command to generate ps output, the final column must be the process
-   name withOUT arguments */
+#define EXTENSIBLENUM 6            /* count the above numbers */
 
 /* comment out to turn off functionality for any of these: */
+/* (See README for details) */
 
 #define PROCMIBNUM 1              /*   proc PROCESSNAME [MAX] [MIN] */
 #define SHELLMIBNUM 3             /*   exec/shell NAME COMMAND      */
@@ -36,8 +58,13 @@
 
 #define VERSIONMIBNUM 100  /* which version are you using?
                               This mibloc will tell you */
-#define ERRORMIBNUM 101
+
+#define ERRORMIBNUM 101     /* Reports errors the agent runs into
+                               (typically its "can't fork, no mem" problems) */
 #define ERRORTIMELENGTH 600 /* how long to wait for error querys */
+
+/* Command to generate ps output, the final column must be the process
+   name withOUT arguments */
 
 #if defined(hpux) || defined(SYSV)
 #define PSCMD "/bin/ps -e"
@@ -63,18 +90,17 @@
 #define CACHEFILE "/tmp/.snmp-exec-cache"
 #define MAXCACHESIZE (200*80)   /* roughly 200 lines max */
 
-#define CONFIGFILE "/etc/ece-snmpd.conf"  /* default config file */
-#define CONFIGFILETWO "/etc/ece-snmpd.local.conf" /* optional second file */
-
 #define MAXDISKS 10                      /* can't scan more than this number */
-#define DEFDISKMINIMUMSPACE 100000       /* 100 meg minimum disk space */
+#define DEFDISKMINIMUMSPACE 100000       /* default of 100 meg minimum
+                                            if the minimum size is not
+                                            specified in the config file */
 
 #define DEFMAXLOADAVE 12.0      /* default maximum load average before error */
 
-#define LASTFIELD -1
-
 #define MAXREADCOUNT 20   /* max times to loop reading output from
-                             execs because of sleep(1)s this will also
-                             be time in seconds */
+                             execs.  Because of sleep(1)s, this will also
+                             be time to wait (in seconds) for exec to finish */
+
+#define LASTFIELD -1      /* internal define */
 
 #include "agent/extensible/struct.h"
