f458865365d50b0368e3f506c10ea233dea5ba1c
Wes Hardaker <hardaker@users.sourceforge.net>
1997-10-21 01:01:46 +0000

    - (config*, agent/mibgroup/v2party.h, ucd_snmp.h, smux_gated.h):   - created new wrappers for the various modules to make them easy to     add and remove as needed.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@963 06827809-a52a-0410-b366-d66718629ded

---
+++ net-snmp/agent/mibgroup/v2party.h
@@ -0,0 +1,7 @@
+/* module to include the modules relavent to the party mib(s) */
+config_require(acl_vars)
+config_require(party_vars)
+config_require(view_vars)
+config_require(context_vars)
+config_require(alarm)
+config_require(event)
