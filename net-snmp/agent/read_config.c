28bb3f42975397da6174a074ea0ca561cdb2f167
Wes Hardaker <hardaker@users.sourceforge.net>
1998-01-30 18:41:53 +0000

    - (read_config.c, snmp_vars.c, snmp_vars.h, var_struct.h):   - merge Dave's code with mine.  Still a few bugs to work through.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@1193 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/read_config.c
+++ net-snmp/agent/read_config.c
@@ -121,7 +121,6 @@ free_config __P((void))
 RETSIGTYPE update_config(a)
 int a;
 {
-  extern struct subtree *subtrees;
   int i;
   char configfile[300];
   struct extensible **etmp, *ptmp;
@@ -168,16 +167,6 @@ int a;
   signal(SIGHUP,update_config);
 }
 
-int pass_compare(a, b)
-  void *a, *b;
-{
-  struct extensible **ap, **bp;
-  ap = (struct extensible **) a;
-  bp = (struct extensible **) b;
-
-  return compare((*ap)->miboid,(*ap)->miblen,(*bp)->miboid,(*bp)->miblen);
-}
-
 void config_perror(string)
   char *string;
 {
@@ -222,4 +211,164 @@ int tree_compare(a, b)
   bp = (struct subtree *) b;
 
   return compare(ap->name,ap->namelen,bp->name,bp->namelen);
+}
+
+int is_parent(name1, len1, name2)
+    register oid	    *name1, *name2;
+    register int	    len1;
+{
+    register int len = len1;
+
+    if ( name2 == NULL )
+	return 0;	/* Null child - doesn't count */
+	
+		/*
+		 * Is name1 a strict prefix of name2 ?
+		 */
+    while(len-- > 0){
+	if (*name2++ != *name1++)
+	    return 0;	/* No */
+    }
+    return 1;		/* Yes */
+}
+
+
+struct subtree*
+insert_in_children_list( eldest, new_tree )
+    struct subtree *eldest;	/* The eldest child in the list of potential
+					siblings (or ancestors) */
+    struct subtree *new_tree;	/* The new subtree to install */
+{
+    struct subtree *sibling = eldest;
+    struct subtree *previous = NULL;
+
+		/* Search for earlier subtrees (including ancestors) */
+
+    while ( (sibling!=NULL) &&
+	    tree_compare(sibling, new_tree) <0 ) {
+	if ( is_parent(sibling->name, sibling->namelen, new_tree->name) ) {
+
+		/* Found an ancestor of the new subtree, so recurse */
+	    struct subtree *result;
+
+	    result = insert_in_children_list( sibling->children, new_tree );
+	    if ( result != NULL )
+		sibling->children = result;	/* New eldest child */
+	    return NULL;
+	}
+		/* Skip over earlier sibling (or 'uncle') subtrees */
+	previous = sibling;
+	sibling = sibling->next;
+    }
+
+
+		/* Run out of earlier subtrees */
+
+    if (( sibling == NULL ) ||
+	    tree_compare(sibling, new_tree) >0 ) {
+	if (( sibling != NULL ) &&
+	      is_parent(new_tree->name, new_tree->namelen, sibling->name) ) {
+		/* The new subtree is the parent of existing subtrees,
+			which need to be cut out of the current sibling list,
+			and inserted as children of the new subtree */
+
+	    struct subtree *prev_kid = sibling;
+	    struct subtree *next_kid = sibling->next;
+
+	    new_tree->children = sibling;
+	
+	    while (( next_kid != NULL ) &&
+		     is_parent(new_tree->name, new_tree->namelen, next_kid->name) ) {
+		prev_kid = next_kid;
+		next_kid = next_kid->next;
+	    }
+	    prev_kid = NULL;
+	    sibling = next_kid;		/* I.e. the next sibling of the new subtree */
+	}
+		
+		/* Found the correct place to insert the new tree */
+	new_tree->next = sibling;
+	if ( previous != NULL ) {
+	    previous->next = new_tree;
+	    return NULL;
+	}
+	else {
+	    return new_tree;	/* Tell the parent this is a new eldest child */
+	}
+    }
+
+		/*
+		   This point is only reached if the new subtree
+			corresponds exactly to an existing subtree.
+		   There are three possibilities:
+			- Two module implementations are covering
+			    exactly the same data  (Not A Good Idea)
+			- The same module implemntation has been
+			    loaded twice  (so it's safe to ignore it)
+			- Two module implementations are cooperating
+			    to cover different portions of the subtree.
+
+		  This third is legitmate, so ought to be handled.
+		  The probable implementation should move the 'mount_point'
+		    sufficiently to differentiate the two, possibly splitting
+		    one or both into a collection of smaller subtrees.
+
+		  This idea will be quietly shelved for now, at least until
+		    the simple case has been implemented, weighed in the
+		    balance and found wanting.....
+		*/
+   return NULL;
+}
+
+
+void
+load_subtree ( new_subtree )
+    struct subtree *new_subtree;
+{
+    extern struct subtree *subtrees;
+    insert_in_children_list( subtrees, new_subtree );
+}
+
+static struct subtree root_subtrees[] = {
+   { { 0 }, 1 },	/* ccitt */
+   { { 1 }, 1 },	/*  iso  */
+   { { 2 }, 1 }		/* joint-ccitt-iso */
+};
+
+
+void setup_tree __P((void))
+{
+  extern struct subtree *subtrees,subtrees_old[];
+  struct subtree *sb;
+  int old_treesz;
+  int i;
+#if defined(USING_EXTENSIBLE_MODULE) || defined(USING_PASS_MODULE)
+  static struct subtree *mysubtree;
+  struct extensible *exten;
+#ifdef USING_EXTENSIBLE_MODULE
+  extern int numrelocs;
+  extern struct extensible *relocs;
+  extern struct variable2 extensible_relocatable_variables[];
+#endif
+#ifdef USING_PASS_MODULE
+  extern int numpassthrus;
+  extern struct extensible *passthrus;
+  extern struct variable2 extensible_passthru_variables[];
+#endif
+#endif
+    
+  if ( subtrees == NULL ) {
+	subtrees =             &(root_subtrees[0]);
+	subtrees->next =       &(root_subtrees[1]);
+	subtrees->next->next = &(root_subtrees[2]);
+  }
+
+  /* Go through the 'static' subtrees (subtrees_old),
+	and link them into the global subtree structure */
+
+  for ( i=0 ; i < subtree_old_size(); i++ )
+	load_subtree( &(subtrees_old[i]) );
+
+  /* No longer necessary to sort the mib tree - this is inherent in
+     the construction of the subtree structure */
 }
