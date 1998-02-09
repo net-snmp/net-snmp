1b0f0cacce311aafca790b6d753789f742d7ae52
Wes Hardaker <hardaker@users.sourceforge.net>
1998-02-09 19:04:31 +0000

    - (bsd.h, bsdi.h, freebsd.h, freebsd3.h, generic.h, hpux.h, netbsd.h,   solaris.h):   - define symbols for auto_nlist to use.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@1212 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/s/hpux.h
+++ net-snmp/s/hpux.h
@@ -5,3 +5,9 @@
 
 /* hpux specific */
 #define MIB_IPCOUNTER_SYMBOL "MIB_ipcounter"
+#define MIB_TCPCOUNTER_SYMBOL "MIB_tcpcounter"
+#define MIB_UDPCOUNTER_SYMBOL "MIB_udpcounter"
+#undef ARPTAB_SYMBOL
+#define ARPTAB_SYMBOL "arptab_nb"
+#undef ARPTAB_SIZE_SYMBOL
+#define ARPTAB_SIZE_SYMBOL "arphd"
--- net-snmp/s/solaris.h
+++ net-snmp/s/solaris.h
@@ -2,3 +2,7 @@
 
 #undef bsdlike
 #define IP_FORWARDING_SYMBOL "ip_forwarding"
+#undef ARPTAB_SYMBOL
+#define ARPTAB_SYMBOL "arptab_nb"
+#undef ARPTAB_SIZE_SYMBOL
+#define ARPTAB_SIZE_SYMBOL "arphd"
