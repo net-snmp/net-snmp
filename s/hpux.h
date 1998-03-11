4d30eb676cbbb5c590465a3f075e2330fac82ccc
Wes Hardaker <hardaker@users.sourceforge.net>
1998-03-11 23:45:22 +0000

    - (hpux.h): switch incorrect arptab symbols.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@1268 06827809-a52a-0410-b366-d66718629ded

--- s/hpux.h
+++ s/hpux.h
@@ -8,9 +8,9 @@
 #define MIB_TCPCOUNTER_SYMBOL "MIB_tcpcounter"
 #define MIB_UDPCOUNTER_SYMBOL "MIB_udpcounter"
 #undef ARPTAB_SYMBOL
-#define ARPTAB_SYMBOL "arptab_nb"
+#define ARPTAB_SYMBOL "arphd"
 #undef ARPTAB_SIZE_SYMBOL
-#define ARPTAB_SIZE_SYMBOL "arphd"
+#define ARPTAB_SIZE_SYMBOL "arptab_nb"
 
 /* ARP_Scan_Next needs a 4th ifIndex argument */
 #define ARP_SCAN_FOUR_ARGUMENTS
