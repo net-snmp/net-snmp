b9b0c8772baa2b864e51d1ee10300e31747c9945
Wes Hardaker <hardaker@users.sourceforge.net>
1998-03-25 18:39:13 +0000

    - (read_config.c, read_config.h, snmp_vars.c, registry.c, registry.h):   - fix the registry mib to use the new module structure.   - create a working find_subtree() and find_subtree_next().
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@1280 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/registry.c
+++ agent/mibgroup/registry.c
@@ -16,47 +16,12 @@
 #include "registry.h"
 #include "../../snmplib/system.h"
 
-
-/*  
- *  header_wombat routines are called to implement the final part of
- *  the oid search.  The parent snmpd routines search the subtree
- *  structure, composed of the various entries in the wombat.h file,
- *  to identify the routine likely responsible for the given oid.  vp
- *  points to the subtree element that contained pointers to the
- *  var_wombat routine, and name points to the actual request.  The
- *  var_wombat routine is called with this info, and it calls
- *  header_wombat to either verify that the request is valid (in the
- *  case of a Get [exact == 1]), or turn the request into a valid
- *  request, if possible (in the case of Get Next [exact == 0]).  When
- *  a valid request is found or generated, a pointer to the routine
- *  responsible for handling Set requests is filled in, in case that's
- *  what's really caused our invocation.
- *
- *  The subtree structure only identifies Types.  In the case of Get,
- *  we just check to see if the length is right and the request
- *  matches something we can answer to.  If the oid is a table, we
- *  validate the index.  This routine could be modified to deal with a
- *  single routine handling a sequence or other data structures, but
- *  you're probably reinventing the wheel if you do (the subtree
- *  structure should be used to reduce those cases down to a scalar or
- *  a table).
- *
- *  In the case of Get Next, we have to deal with the fact that the
- *  incoming request is probably not going to match anything --- it
- *  can be too short or too long, or the index of a table might not
- *  match anything actually in the table.
- *
- *  If the incoming request is too short, convert it to the first valid
- *  oid.  If it's too long, match as far as possible, and then convert
- *  it to the next valid oid.  */
-
 	/*********************
 	 *
 	 *  Kernel & interface information,
 	 *   and internal forward declarations
 	 *
 	 *********************/
-
 
 extern int subtree_size;
 extern struct subtree *subtrees;
@@ -81,45 +46,27 @@ header_registry(vp, name, length, exact, var_len, write_method)
     
     if (snmp_get_do_debugging()) {
       sprint_objid (c_oid, name, *length);
-      DEBUGP ("var_registry: %s %d %d\n", c_oid, exact, *length);
+      DEBUGP ("var_registry: %s\n", c_oid);
     }
-    if (*length < REGISTRY_NAME_LENGTH)
+    if (*length < REGISTRY_NAME_LENGTH ||
+        compare(name, *length, vp->name, vp->namelen) < 1)
       mine = subtrees;
     else
       mine = find_subtree_next(&(name[REGISTRY_NAME_LENGTH]),
                                *length-REGISTRY_NAME_LENGTH,
                                subtrees);
-    
-    memcpy( (char *)newname,(char *)vp->name, (int)vp->namelen * sizeof(oid));
-    memcpy( (char *)name,(char *)newname, ((int)vp->namelen + 1) * sizeof(oid));
+
     if (mine != NULL) {
+      memcpy( (char *)newname,(char *)vp->name, (int)vp->namelen * sizeof(oid));
+      memcpy( (char *)name,(char *)newname,
+              ((int)vp->namelen + 1) * sizeof(oid));
       memcpy((char *)(name+vp->namelen), (char *)mine->name,
             ((int)mine->namelen) * sizeof(oid));
       *length = vp->namelen + mine->namelen;
     }
 
     sprint_objid (c_oid, name, *length);
-    DEBUGP ("var_registry return: %s %d\n", c_oid, *length);
-
     return mine;
-    
-    for(i=0; i < subtree_size; i++) {
-      newname[REGISTRY_NAME_LENGTH] = i+1;
-      result = compare(name, *length, newname, (int)vp->namelen + 1);
-      if (!((exact && (result != 0)) || (!exact && (result >= 0))))
-        break;
-    }
-    if (i >= subtree_size) {
-      DEBUGP ("... index out of range: %d > %d\n",i,subtree_size);
-      return(NULL);
-    }
-    DEBUGP ("... doing %d\n", i);
-    memcpy( (char *)name,(char *)newname, ((int)vp->namelen + 1) * sizeof(oid));
-    *length = vp->namelen + 1;
-
-    *write_method = 0;
-    *var_len = sizeof(long);	/* default to 'long' results */
-    return(NULL);
 }
 
 
@@ -156,11 +103,3 @@ var_registry(vp, name, length, exact, var_len, write_method)
     }
     return NULL;
 }
-
-
-	/*********************
-	 *
-	 *  Internal implementation functions
-	 *
-	 *********************/
-
