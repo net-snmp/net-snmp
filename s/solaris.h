01fa7d8d042eec9848cd8b42cfb2f84f596922aa
Wes Hardaker <hardaker@users.sourceforge.net>
1998-04-20 23:06:21 +0000

    - (solaris.h): Patch from Niels Baggesen:   - undefine a bunch of nlist symbols.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@1321 06827809-a52a-0410-b366-d66718629ded

--- s/solaris.h
+++ s/solaris.h
@@ -2,8 +2,18 @@
 
 #undef bsdlike
 #undef IP_FORWARDING_SYMBOL
-#define IP_FORWARDING_SYMBOL "ip_forwarding"
 #undef ARPTAB_SYMBOL
 #define ARPTAB_SYMBOL "arptab_nb"
 #undef ARPTAB_SIZE_SYMBOL
 #define ARPTAB_SIZE_SYMBOL "arphd"
+#undef ICMPSTAT_SYMBOL
+#undef TCPSTAT_SYMBOL
+#undef TCP_SYMBOL
+#undef UDPSTAT_SYMBOL
+#undef UDB_SYMBOL
+#undef RTTABLES_SYMBOL
+#undef RTHASHSIZE_SYMBOL
+#undef RTHOST_SYMBOL
+#undef RTNET_SYMBOL
+#undef IPSTAT_SYMBOL
+#undef TCP_TTL_SYMBOL
