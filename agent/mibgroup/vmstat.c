d092edc4d9473da0dc9c10577ff94b23a9c2e56a
Wes Hardaker <hardaker@users.sourceforge.net>
1998-05-12 22:14:48 +0000

    - (disk.c, example.c, interfaces.c, loadave.c, memory.c, pass.c,   proc.c, util_funcs.h, vmstat.c):   - include time.h properly instead of relying on util_funcs.h to do it.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@1384 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/vmstat.c
+++ agent/mibgroup/vmstat.c
@@ -85,6 +85,16 @@
 #else
 #include <strings.h>
 #endif
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 
 #include "mibincl.h"
 #include "mibdefs.h"
