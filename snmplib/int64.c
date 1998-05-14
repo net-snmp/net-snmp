75063b64d9c849a12357129a5faa76ea8b332795
Wes Hardaker <hardaker@users.sourceforge.net>
1998-05-14 04:21:22 +0000

    - (int64.c, int64.h, mib.c): make int64.c use counter64's structure.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@1387 06827809-a52a-0410-b366-d66718629ded

--- snmplib/int64.c
+++ snmplib/int64.c
@@ -6,11 +6,12 @@
 */
 
 #include <config.h>
-
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include "asn1.h"
 #include "int64.h"
 
 #define TRUE 1
@@ -37,30 +38,30 @@ divBy10(u64, pu64Q, puR)
 
 
     /* top 16 bits */
-    ulT = (u64.ulHi>>16) & 0x0ffff;
+    ulT = (u64.high>>16) & 0x0ffff;
     ulQ = ulT/10;
     ulR = ulT%10;
-    pu64Q->ulHi = ulQ<<16;
+    pu64Q->high = ulQ<<16;
 
     /* next 16 */
-    ulT = (u64.ulHi & 0x0ffff);
+    ulT = (u64.high & 0x0ffff);
 	ulT += (ulR<<16);
     ulQ = ulT/10;
     ulR = ulT%10;
-    pu64Q->ulHi = pu64Q->ulHi | ulQ;
+    pu64Q->high = pu64Q->high | ulQ;
 
     /* next 16 */
-    ulT = ((u64.ulLo>>16) & 0x0ffff) + (ulR<<16);
+    ulT = ((u64.low>>16) & 0x0ffff) + (ulR<<16);
     ulQ = ulT/10;
     ulR = ulT%10;
-    pu64Q->ulLo = ulQ<<16;
+    pu64Q->low = ulQ<<16;
 
     /* final 16 */
-    ulT = (u64.ulLo & 0x0ffff);
+    ulT = (u64.low & 0x0ffff);
 	ulT += (ulR<<16);
     ulQ = ulT/10;
     ulR = ulT%10;
-    pu64Q->ulLo = pu64Q->ulLo | ulQ;
+    pu64Q->low = pu64Q->low | ulQ;
 
     *puR = (unsigned int)(ulR);
 
@@ -86,28 +87,28 @@ multBy10(u64, pu64P)
 
 
     /* lower 16 bits */
-    ulT = u64.ulLo & 0x0ffff;
+    ulT = u64.low & 0x0ffff;
     ulP = ulT * 10;
     ulK = ulP>>16;
-    pu64P->ulLo = ulP & 0x0ffff;
+    pu64P->low = ulP & 0x0ffff;
 
     /* next 16 */
-    ulT = (u64.ulLo>>16) & 0x0ffff;
+    ulT = (u64.low>>16) & 0x0ffff;
     ulP = (ulT * 10) + ulK;
     ulK = ulP>>16;
-    pu64P->ulLo = (ulP & 0x0ffff)<<16 | pu64P->ulLo;
+    pu64P->low = (ulP & 0x0ffff)<<16 | pu64P->low;
 
     /* next 16 bits */
-    ulT = u64.ulHi & 0x0ffff;
+    ulT = u64.high & 0x0ffff;
     ulP = (ulT * 10) + ulK;
     ulK = ulP>>16;
-    pu64P->ulHi = ulP & 0x0ffff;
+    pu64P->high = ulP & 0x0ffff;
 
     /* final 16 */
-    ulT = (u64.ulHi>>16) & 0x0ffff;
+    ulT = (u64.high>>16) & 0x0ffff;
     ulP = (ulT * 10) + ulK;
     ulK = ulP>>16;
-    pu64P->ulHi = (ulP & 0x0ffff)<<16 | pu64P->ulHi;
+    pu64P->high = (ulP & 0x0ffff)<<16 | pu64P->high;
 
 
 } /* multBy10 */
@@ -132,12 +133,12 @@ incrByU16(pu64, u16)
 
 
     /* lower 16 bits */
-    ulT1 = pu64->ulLo;
+    ulT1 = pu64->low;
     ulT2 = ulT1 & 0x0ffff;
     ulR = ulT2 + u16;
     ulK = ulR>>16;
     if (ulK == 0) {
-        pu64->ulLo = ulT1 + u16;
+        pu64->low = ulT1 + u16;
         return;
     }
 
@@ -146,13 +147,13 @@ incrByU16(pu64, u16)
     ulR = ulT2+1;
     ulK = ulR>>16;
     if (ulK == 0) {
-        pu64->ulLo = ulT1 + u16;
+        pu64->low = ulT1 + u16;
         return;
     }
    
     /* next 32 - ignore any overflow */
-    pu64->ulLo = (ulT1 + u16) & 0x0FFFFFFFFL;
-    pu64->ulHi++;
+    pu64->low = (ulT1 + u16) & 0x0FFFFFFFFL;
+    pu64->high++;
 
 } /* incrByV16 */
 
@@ -162,10 +163,10 @@ incrByU32(pu64, u32)
   unsigned int u32;
 {
   unsigned int tmp;
-  tmp = pu64->ulLo;
-  pu64->ulLo += u32;
-  if (pu64->ulLo < tmp)
-    pu64->ulHi++;
+  tmp = pu64->low;
+  pu64->low += u32;
+  if (pu64->low < tmp)
+    pu64->high++;
 }
 
 /** zeroU64 - set an unsigned 64-bit number to zero
@@ -179,8 +180,8 @@ zeroU64(pu64)
   U64 *pu64;
 {
 
-    pu64->ulLo = 0;
-    pu64->ulHi = 0;
+    pu64->low = 0;
+    pu64->high = 0;
 
 } /* zeroU64 */
 
@@ -196,7 +197,7 @@ isZeroU64(pu64)
   U64 *pu64;
 {
 
-    if ((pu64->ulLo == 0) && (pu64->ulHi == 0))
+    if ((pu64->low == 0) && (pu64->high == 0))
         return(TRUE);
     else
         return(FALSE);
@@ -215,15 +216,15 @@ printU64(pu64)
   unsigned int u;
   int j;
 
-  u64a.ulHi = pu64->ulHi;
-  u64a.ulLo = pu64->ulLo;
+  u64a.high = pu64->high;
+  u64a.low = pu64->low;
   
   aRes[I64CHARSZ] = 0;
   for (j = 0; j < I64CHARSZ; j++) {
     divBy10(u64a, &u64b, &u);
     aRes[(I64CHARSZ-1)-j] = (char)('0' + u);
-    u64a.ulHi = u64b.ulHi;
-    u64a.ulLo = u64b.ulLo;
+    u64a.high = u64b.high;
+    u64a.low = u64b.low;
     if (isZeroU64(&u64a))
       break;
   }
@@ -268,7 +269,7 @@ main(int argc, char *argv[])
         }
 
         printf("number \"%s\" in hex is '%08x%08x'h\n",
-                argv[i], u64a.ulHi, u64a.ulLo);
+                argv[i], u64a.high, u64a.low);
 
         printf("number is \"%s\"\n", printU64(&u64a));
         for (j = 0; j < MXSZ; j++) {
