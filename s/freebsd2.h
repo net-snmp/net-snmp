340a7793f39a4ab2ee9a6faf1c8cca71f675f4b4
Wes Hardaker <hardaker@users.sourceforge.net>
1998-06-03 20:52:28 +0000

    - (config.h.in, configure, configure.in, freebsd2.h):   - move configurable defines to configure rather than in freebsd2.h.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@1447 06827809-a52a-0410-b366-d66718629ded

--- s/freebsd2.h
+++ s/freebsd2.h
@@ -3,9 +3,6 @@
 /* this is not good enough before freebsd3! */
 #undef HAVE_NET_IF_MIB_H
 #define UTMP_FILE "/var/run/utmp"
-#define HAVE_GETFSSTAT 1
-#define HAVE_KVM_GETPROCS 1
-#define HAVE_SYS_DISKLABEL_H 1
 #undef PROC_SYMBOL
 #undef NPROC_SYMBOL
 #undef LOADAVE_SYMBOL
