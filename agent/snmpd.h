a3ecf4ab4260bd415294558c9a42e1544502cef1
Wes Hardaker <hardaker@users.sourceforge.net>
1998-06-17 15:08:23 +0000

    - (snmp2p.c, snmpd.c, snmpd.h): Patch from Niels Baggesen:   - misc type fixes.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@1486 06827809-a52a-0410-b366-d66718629ded

--- agent/snmpd.h
+++ agent/snmpd.h
@@ -4,6 +4,7 @@ extern int verbose;
 
 extern int snmp_agent_parse __P((u_char *, int, u_char *, int *, u_long));
 extern void init_snmp __P((void));
+extern char *reverse_bytes __P((char *, int));
 extern void send_easy_trap __P((int));
 extern u_char *getStatPtr __P((oid *, int *, u_char *, int *, u_short *, int, int (**write) __P((int, u_char *, u_char, int, u_char *, oid *, int)), struct packet_info *, int * ));
 extern char *reverse_bytes __P((char *, int));
