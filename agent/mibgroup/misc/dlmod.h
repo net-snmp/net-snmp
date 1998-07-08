c82d78b29cecf0761fa521cedbbbc53f315db857
Wes Hardaker <hardaker@users.sourceforge.net>
1998-07-08 21:04:22 +0000

    - (Makefile.in, Makefile.top, configure, configure.in, Makefile.in,   .cvsignore, Makefile.in, dlmod_mib.c, dlmod_mib.h, example.c,   example.h, dlmod.c, dlmod.h, Makefile.in, Makefile.in,   DLMOD-MIB.txt, Makefile.in, Makefile.in): Patch from Eugene   Polovnikov:   - new dlmod set.   - fix configure script.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@1537 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/misc/dlmod.h
+++ agent/mibgroup/misc/dlmod.h
@@ -5,11 +5,39 @@
 #define _MIBGROUP_DLMOD_H
 
 
-/* config file parsing routines */
-void dlmod_free_config __P((void));
-void dlmod_parse_config __P((char *, char *));
-config_parse_dot_conf("dlmod", dlmod_parse_config, dlmod_free_config)
 
 #include "mibdefs.h"
 
+#ifndef DLMOD_PATH
+#define DLMOD_DEFAULT_PATH "/usr/local/lib/snmp/dlmod"
+#endif
+
+struct dlmod {
+	struct dlmod *next;
+	int	index;
+	char name[64];
+	char path[255];
+	char error[255];
+	void *handle;
+	int status;
+};
+
+#define DLMOD_LOADED 	1
+#define DLMOD_UNLOADED	2
+#define DLMOD_ERROR		3
+#define DLMOD_LOAD		4
+#define	DLMOD_UNLOAD	5
+#define DLMOD_CREATE	6
+#define DLMOD_DELETE	7
+
+void dlmod_load_module __P((struct dlmod *));
+void dlmod_unload_module __P((struct dlmod *));
+struct dlmod *dlmod_create_module __P((void));
+void dlmod_delete_module __P((struct dlmod*));
+struct dlmod *dlmod_get_by_index __P((int));
+
+void dlmod_init __P((void));
+void dlmod_deinit __P((void));
+
+extern int dlmod_next_index;
 #endif /* _MIBGROUP_DLMOD_H */
