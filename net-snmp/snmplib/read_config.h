42bb770a1625c44b076e28ff67493a854d56cf61
Wes Hardaker <hardaker@users.sourceforge.net>
1998-09-03 01:47:15 +0000

    - (mib.c, mib.h, read_config.c, read_config.h):   - pre/post mib loading config files.   - create init_snmp() to wrap other fuctions.   - new snmp.conf directives: mibs, mibdirs, mibfile.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@1611 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/read_config.h
+++ net-snmp/snmplib/read_config.h
@@ -7,6 +7,10 @@
 
 #define STRINGMAX 1024
 
+#define NORMAL_CONFIG 0
+#define PREMIB_CONFIG 1
+#define EITHER_CONFIG 2
+
 struct config_files {
    char *fileHeader;
    struct config_line *start;
@@ -18,10 +22,13 @@ struct config_line {
    void (*parse_line) __P((char *, char *));
    void (*free_func) __P((void));
    struct config_line *next;
+   char config_time;
 };
 
-void read_config __P((char *, struct config_line *));
+void read_config __P((char *, struct config_line *, int));
 void read_configs __P((void));
+void read_premib_configs __P((void));
+void read_config_files __P((int));
 void free_config __P((void));
 void config_perror __P((char *));
 void config_pwarn __P((char *));
@@ -29,9 +36,12 @@ char *skip_white __P((char *));
 char *skip_not_white __P((char *));
 void copy_word __P((char *, char *));
 void read_config_with_type __P((char *, char *));
-void register_config_handler __P((char *, char *,
-                                  void (*parser)(char *, char *),
-                                  void (*releaser) (void)));
+struct config_line *register_config_handler __P((char *, char *,
+                                                 void (*parser)(char *, char *),
+                                                 void (*releaser) (void)));
+struct config_line *register_premib_handler __P((char *, char *,
+                                                 void (*parser)(char *, char *),
+                                                 void (*releaser) (void)));
 void unregister_config_handler __P((char *, char *));
 
 #endif /* READ_CONFIG_H */
