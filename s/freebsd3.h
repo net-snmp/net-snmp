e73202c578ec6c21fa8c0315fd3485f75257bdc0
Wes Hardaker <hardaker@users.sourceforge.net>
1998-10-14 16:50:26 +0000

    - (freebsd3.h): define freebsd2 and MOUNT_NFS.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@1840 06827809-a52a-0410-b366-d66718629ded

--- s/freebsd3.h
+++ s/freebsd3.h
@@ -1,4 +1,12 @@
 #include "freebsd.h"
 
+#define freebsd2      /* freebsd3 is a superset of freebsd2 */
+
+#if HAVE_GETFSSTAT
+#if defined(MFSNAMELEN)
+#define MOUNT_NFS              /* needed for HR mib */
+#endif
+#endif
+
 #undef IFADDR_SYMBOL
 #define IFADDR_SYMBOL "in_ifaddrhead"
