f9c84103c2c9aa4f5aaa63f3170062bc033207da
Wes Hardaker <hardaker@users.sourceforge.net>
1998-10-16 15:38:44 +0000

    - (at.c, icmp.c, interfaces.c, aix.h): aix header file fixes.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@1869 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII/at.c
+++ agent/mibgroup/mibII/at.c
@@ -16,6 +16,15 @@
 #endif
 #include <sys/types.h>
 #include <sys/socket.h>
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+#endif
 
 #if HAVE_NETINET_IN_H
 #include <netinet/in.h>
