db688f40d150f0ee741fbd9c22d3b1b8bbb71ffd
Wes Hardaker <hardaker@users.sourceforge.net>
1999-01-20 23:17:41 +0000

    - (linux.h): define LINUX_INTERFACE_SCAN_LINE.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@2184 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/s/linux.h
+++ net-snmp/s/linux.h
@@ -8,3 +8,8 @@
 #ifndef MNTTYPE_PROC
 #define MNTTYPE_PROC "proc"
 #endif
+
+/* for 2.0 kernels */
+#define LINUX_INTERFACE_SCAN_LINE "%[^:]: %d %d %*d %*d %*d %d %d %*d %*d %d"
+/* for 2.1 kernels */
+/* #define LINUX_INTERFACE_SCAN_LINE "%[^:]: %*d %d %d %*d %*d %*d %*d %*d %*d %d %d %*d %*d %*d %*d %d" */
