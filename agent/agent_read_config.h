c6ccf0f593d05d50b58ac91c6ee77bf87d545dc0
Wes Hardaker <hardaker@users.sourceforge.net>
1999-02-08 23:11:08 +0000

    - (agent_read_config.c, agent_read_config.h): Patch from Michael Slifcak:   - make tree_compare() be a real typedef'ed function again now that     qsorting is no longer needed.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@2247 06827809-a52a-0410-b366-d66718629ded

--- agent/agent_read_config.h
+++ agent/agent_read_config.h
@@ -2,12 +2,13 @@
  *  read_config: reads configuration files for extensible sections.
  *
  */
-#ifndef _MIBGROUP_READ_CONFIG_H
-#define _MIBGROUP_READ_CONFIG_H
+#ifndef _AGENT_READ_CONFIG_H
+#define _AGENT_READ_CONFIG_H
 
 void init_agent_read_config (void);
 RETSIGTYPE update_config (int);
-int tree_compare (const void *, const void *);
+struct subtree;
+int tree_compare (const struct subtree *, const struct subtree *);
 void setup_tree (void);
 void load_subtree (struct subtree *);
 int is_parent (oid *, int, oid *);
@@ -18,4 +19,4 @@ void snmpd_register_config_handler (char *,
 void snmpd_unregister_config_handler (char *);
 void snmpd_store_config (char *);
 
-#endif /* _MIBGROUP_READ_CONFIG_H */
+#endif /* _AGENT_READ_CONFIG_H */
