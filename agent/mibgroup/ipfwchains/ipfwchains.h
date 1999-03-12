3267e8576f9e298c90a340466c8c7c63ab357bff
Wes Hardaker <hardaker@users.sourceforge.net>
1999-03-12 23:33:26 +0000

    - (IPFWCHAINS-MIB.txt, ipfwchains.h): move to a sub-oid in the   experimental tree.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@2429 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/ipfwchains/ipfwchains.h
+++ agent/mibgroup/ipfwchains/ipfwchains.h
@@ -47,30 +47,30 @@ u_char *var_ipfwrules(struct variable *, oid *, int *, int, int *, WriteMethod *
 #ifdef IN_SNMP_VARS_C
 
 struct variable4 ipfwchains_variables[] = {
-    { IPFWCCHAININDEX,		ASN_INTEGER, 	RONLY, var_ipfwchains, 3, {65, 1, 1}},
-    { IPFWCCHAINLABEL,       	ASN_OCTET_STR, 	RONLY, var_ipfwchains, 3, {65, 1, 2}},
-    { IPFWCPOLICY,          	ASN_OCTET_STR,  RONLY, var_ipfwchains, 3, {65, 1, 3}},
-    { IPFWCREFCNT,          	ASN_INTEGER,  	RONLY, var_ipfwchains, 3, {65, 1, 4}},
-    { IPFWCPKTS,          	ASN_OCTET_STR,  RONLY, var_ipfwchains, 3, {65, 1, 5}},
-    { IPFWCBYTES,          	ASN_OCTET_STR,  RONLY, var_ipfwchains, 3, {65, 1, 6}},
-    { IPFWRRULEINDEX,          	ASN_INTEGER,  	RONLY, var_ipfwrules,  3, {66, 1, 1}},
-    { IPFWRCHAIN,             	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 2}},
-    { IPFWRPKTS,                ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 3}},
-    { IPFWRBYTES,               ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 4}},
-    { IPFWRTARGET,              ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 5}},
-    { IPFWRPROT,               	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 6}},
-    { IPFWRSOURCE,              ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 7}},
-    { IPFWRDESTINATION,    	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 8}},
-    { IPFWRPORTS,               ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 9}},
-    { IPFWROPT,         	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 10}},
-    { IPFWRIFNAME,              ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 11}},
-    { IPFWRTOSA,               	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 12}},
-    { IPFWRTOSX,               	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 13}},
-    { IPFWRMARK,              	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 14}},
-    { IPFWROUTSIZE,             ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {66, 1, 15}}
+    { IPFWCCHAININDEX,		ASN_INTEGER, 	RONLY, var_ipfwchains, 3, {1, 1, 1}},
+    { IPFWCCHAINLABEL,       	ASN_OCTET_STR, 	RONLY, var_ipfwchains, 3, {1, 1, 2}},
+    { IPFWCPOLICY,          	ASN_OCTET_STR,  RONLY, var_ipfwchains, 3, {1, 1, 3}},
+    { IPFWCREFCNT,          	ASN_INTEGER,  	RONLY, var_ipfwchains, 3, {1, 1, 4}},
+    { IPFWCPKTS,          	ASN_OCTET_STR,  RONLY, var_ipfwchains, 3, {1, 1, 5}},
+    { IPFWCBYTES,          	ASN_OCTET_STR,  RONLY, var_ipfwchains, 3, {1, 1, 6}},
+    { IPFWRRULEINDEX,          	ASN_INTEGER,  	RONLY, var_ipfwrules,  3, {2, 1, 1}},
+    { IPFWRCHAIN,             	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 2}},
+    { IPFWRPKTS,                ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 3}},
+    { IPFWRBYTES,               ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 4}},
+    { IPFWRTARGET,              ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 5}},
+    { IPFWRPROT,               	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 6}},
+    { IPFWRSOURCE,              ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 7}},
+    { IPFWRDESTINATION,    	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 8}},
+    { IPFWRPORTS,               ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 9}},
+    { IPFWROPT,         	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 10}},
+    { IPFWRIFNAME,              ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 11}},
+    { IPFWRTOSA,               	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 12}},
+    { IPFWRTOSX,               	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 13}},
+    { IPFWRMARK,              	ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 14}},
+    { IPFWROUTSIZE,             ASN_OCTET_STR,  RONLY, var_ipfwrules,  3, {2, 1, 15}}
 };
 
-config_load_mib(1.3.6.1.4.1.2021.13, 8, ipfwchains_variables)
+config_load_mib(1.3.6.1.4.1.2021.13.3, 9, ipfwchains_variables)
   /* arguments:
      .1.3.6.1.2.1.2021.13:  MIB oid to put the tables at.
      8:                     Length of the mib oid above.
