7f14ad4e3c9198b7dcd8851e69551d1bd19d5f85
Michael Slifcak <slif@users.sourceforge.net>
1999-03-12 22:35:38 +0000

    - (target.h, target/*.h) require util_funcs once.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@2424 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/target/snmpTargetParamsEntry.h
+++ net-snmp/agent/mibgroup/target/snmpTargetParamsEntry.h
@@ -8,10 +8,6 @@
 #ifndef _MIBGROUP_SNMPTARGETPARAMSENTRY_H
 #define _MIBGROUP_SNMPTARGETPARAMSENTRY_H
 
-/* we use header_generic and checkmib from the util_funcs module */
-
-config_require(util_funcs)
-
 /* Magic number definitions: */
 
 #define   SNMPTARGETPARAMSMPMODEL        1
