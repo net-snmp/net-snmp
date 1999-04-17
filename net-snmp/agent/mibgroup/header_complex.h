0e7410b25db52b3594c497fbe8f3a01c8ab60956
Wes Hardaker <hardaker@users.sourceforge.net>
1999-04-17 04:12:58 +0000

    - (header_complex.h): prototypes.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@2533 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/header_complex.h
+++ net-snmp/agent/mibgroup/header_complex.h
@@ -15,5 +15,18 @@ void *header_complex(struct header_complex_index *datalist, struct variable *vp,
                      oid *name, int *length, int exact, int *var_len,
                      WriteMethod **write_method);
 
+int header_complex_generate_varoid(struct variable_list *var);
+int header_complex_parse_oid(oid *oidIndex, int oidLen,
+                             struct header_complex_index *data);
+void header_complex_generate_oid(oid *name, int *length, oid *prefix,
+                                 int prefix_len,
+                                 struct header_complex_index *data);
+int header_complex_var_compare(struct variable_list *varl,
+                               struct variable_list *varr);
+struct header_complex_index *
+  header_complex_add_data(struct header_complex_index **thedata,
+                          struct variable_list *var, void *data);
+
+
 #endif /* _MIBGROUP_HEADER_COMPLEX_H */
 
