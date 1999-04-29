7f39461f5eee0875643f8a246e1520cadc272fd0
Wes Hardaker <hardaker@users.sourceforge.net>
1999-04-29 14:32:40 +0000

    - (Makefile.in, callback.c, callback.h): callback API.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@2575 06827809-a52a-0410-b366-d66718629ded

---
+++ snmplib/callback.h
@@ -0,0 +1,27 @@
+/* callback.c: A generic callback mechanism */
+
+#ifndef CALLBACK_H
+#define CALLBACK_H
+
+#define MAX_CALLBACK_IDS    1
+#define MAX_CALLBACK_SUBIDS 2
+
+/* Callback Major Types */
+#define SNMP_CALLBACK_LIBRARY     0
+#define SNMP_CALLBACK_APPLICATION 1
+
+/* SNMP_CALLBACK_LIBRARY minor types */
+#define SNMP_CALLBACK_POST_READ_CONFIG	0
+#define SNMP_CALLBACK_STORE_DATA	1
+#define SNMP_CALLBACK_SHUTDOWN		2
+
+typedef int (SNMPCallback)(int majorID, int minorID, void *serverarg,
+                           void *clientarg);
+
+struct snmp_callback {
+   SNMPCallback         *sc_callback;
+   void                 *sc_client_arg;
+   struct snmp_callback *next;
+};
+
+#endif /* CALLBACK_H */
