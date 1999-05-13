614177e54161528981946dbd687e6112d1a8180e
Wes Hardaker <hardaker@users.sourceforge.net>
1999-05-13 16:09:18 +0000

    - (vacm_vars.c): Set pointers to NULL after freeing them.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V3-line@2614 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII/vacm_vars.c
+++ agent/mibgroup/mibII/vacm_vars.c
@@ -247,6 +247,7 @@ void vacm_parse_group (char *token,
     gp->storageType = SNMP_STORAGE_PERMANENT;
     gp->status = SNMP_ROW_ACTIVE;
     free (gp->reserved);
+    gp->reserved = NULL;
 }
 
 void vacm_free_group (void)
@@ -327,6 +328,7 @@ void vacm_parse_access (char *token, char *param)
     ap->storageType = SNMP_STORAGE_PERMANENT;
     ap->status = SNMP_ROW_ACTIVE;
     free (ap->reserved);
+    ap->reserved = NULL;
 }
 
 void vacm_free_access (void)
@@ -400,6 +402,7 @@ void vacm_parse_view (char *token,
     vp->viewStorageType = SNMP_STORAGE_PERMANENT;
     vp->viewStatus = SNMP_ROW_ACTIVE;
     free (vp->reserved);
+    vp->reserved = NULL;
 }
 
 void vacm_free_view (void)
