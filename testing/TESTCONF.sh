d50da20b3a61bea02ba192162805300ce165adfe
Wes Hardaker <hardaker@users.sourceforge.net>
1999-06-15 23:26:17 +0000

    - (TESTCONF.sh): save packet dump.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@2748 06827809-a52a-0410-b366-d66718629ded

--- testing/TESTCONF.sh
+++ testing/TESTCONF.sh
@@ -47,7 +47,7 @@ SNMP_PERSISTENT_FILE="$SNMP_TMPDIR/persistent-store.conf"
 SNMP_CONFIG_FILE="$SNMP_TMPDIR/snmpd.conf"
 SNMP_SNMPD_PID_FILE="$SNMP_TMPDIR/snmpd.pid"
 SNMP_SNMPD_LOG_FILE="$SNMP_TMPDIR/snmpd.log"
-SNMP_SNMPD_PORT="-p 8765"
+SNMP_SNMPD_PORT="-d -p 8765"
 
 # Make sure the agent doesn't parse any config file but what we give it.  
 # this is mainly to protect against a broken agent that doesn't
