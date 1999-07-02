2d38dd7e53191f166bc3d1e61207d988ca884d8e
Dave Shield <dts12@users.sourceforge.net>
1999-07-02 12:15:56 +0000

    - (var_struct.h agent_registry.c):   - map between MIB registrations and the supporting (subagent) session
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@2809 06827809-a52a-0410-b366-d66718629ded

--- agent/var_struct.h
+++ agent/var_struct.h
@@ -16,7 +16,7 @@ struct subtree {
     int			variables_len;	/* number of entries in above array */
     int			variables_width; /* sizeof each variable entry */
     char                label[256];     /* calling module's label */
-    int			session;			/* or struct snmp_session* */
+    struct snmp_session *session;
     u_char		flags;
     u_char		priority;
     struct subtree      *next;		/* List of 'sibling' subtrees */
