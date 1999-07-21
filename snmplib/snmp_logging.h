030a2cd81f0120b6bcffa49aaab3ce6c22c9d7ab
Wes Hardaker <hardaker@users.sourceforge.net>
1999-07-21 00:07:46 +0000

    - (snmp_logging.c, snmp_logging.h): -Wall fixes (ANSI).
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@2852 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_logging.h
+++ snmplib/snmp_logging.h
@@ -7,17 +7,17 @@
 #include <varargs.h>
 #endif
 
-extern disable_syslog();
-extern disable_filelog();
-extern disable_stderrlog();
-extern disable_log();
-extern enable_syslog();
-extern enable_filelog(const char *logfilename, int dont_zero_log);
-extern enable_stderrlog();
-extern log_syslog(int priority, const char *format, ...);
-extern log_filelog(int priority, const char *format, ...);
-extern log_stderrlog(int priority, const char *format, ...);
-extern snmp_log(int priority, const char *format, ...);
-extern vlog(int priority, const char *format, va_list ap);
-extern log_perror(const char *s);
+void disable_syslog(void);
+void disable_filelog(void);
+void disable_stderrlog(void);
+void disable_log(void);
+void enable_syslog(void);
+void enable_filelog(const char *logfilename, int dont_zero_log);
+void enable_stderrlog(void);
+void log_syslog(int priority, const char *format, ...);
+void log_filelog(int priority, const char *format, ...);
+void log_stderrlog(int priority, const char *format, ...);
+void snmp_log(int priority, const char *format, ...);
+void vlog(int priority, const char *format, va_list ap);
+void log_perror(const char *s);
 #endif
