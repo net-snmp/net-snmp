98ac917345320d491b882fd89f8571add6532c9c
Wes Hardaker <hardaker@users.sourceforge.net>
1999-08-09 21:28:35 +0000

    - (snmpusm.c): more error checking in the MAKE_ENTRY macro.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@2986 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/snmpusm.c
+++ net-snmp/snmplib/snmpusm.c
@@ -94,6 +94,11 @@ usm_check_secLevel_vs_protocols(int level,
  */
 #define MAKE_ENTRY( type, item, len, field, field_len )			\
 {									\
+        if (len == 0 || item == NULL) {					\
+		ref->field = NULL;					\
+		ref->field_len = 0;					\
+		return 0;						\
+	}					 			\
 	if (ref == NULL)						\
 		return -1;						\
 	if (ref->field != NULL)	{					\
