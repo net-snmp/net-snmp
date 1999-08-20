95fac5aede56ecdd809a361fd6fa00b16180ceee
Wes Hardaker <hardaker@users.sourceforge.net>
1999-08-20 20:43:58 +0000

    - (default_store.h, snmp_api.c, snmp_logging.c, snmp_logging.h):   - make logging of timestamps optional by a snmp.conf directive.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@3047 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_api.c
+++ snmplib/snmp_api.c
@@ -523,6 +523,7 @@ init_snmp(const char *type)
 
   snmp_debug_init(); /* should be done first, to turn on debugging ASAP */
   init_callbacks();
+  init_snmp_logging();
   snmp_init_statistics();
   register_mib_handlers();
   register_default_handlers();
