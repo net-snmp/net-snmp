a438cfe36c3f2fce8085196895b29db0e7b08dee
Dave Shield <dts12@users.sourceforge.net>
1999-10-04 17:17:53 +0000

    - (agent_callbacks.h  mibgroup/mibII/sysORTable.[ch]    mibgroup/agentx/master_admin.c mibgroup/agentx/subagent.c):    - Complete subagent handling of Agent Capabilities / sysORTable
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@3214 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/mibII/sysORTable.h
+++ net-snmp/agent/mibgroup/mibII/sysORTable.h
@@ -12,7 +12,14 @@ struct sysORTable {
    oid  *OR_oid;
    size_t  OR_oidlen;
    struct timeval OR_uptime;
+   struct snmp_session *OR_sess;
    struct sysORTable *next;
+};
+
+struct register_sysOR_parameters {
+   oid  *name;
+   int   namelen;
+   char *descr;
 };
 
 extern void     init_sysORTable (void);
@@ -20,10 +27,19 @@ extern FindVarMethod var_sysORTable;
 extern FindVarMethod var_sysORLastChange;
 extern int      register_sysORTable (oid *, size_t, const char *);
 extern int    unregister_sysORTable (oid *, size_t);
+extern int      register_sysORTable_sess (oid *, size_t, const char *, struct snmp_session *);
+extern int    unregister_sysORTable_sess (oid *, size_t, struct snmp_session *);
+extern int    unregister_sysORTable_by_session (struct snmp_session *);
 
 #define	SYSORTABLEINDEX		        1
 #define	SYSORTABLEID		        2
 #define	SYSORTABLEDESCR		        3
 #define	SYSORTABLEUPTIME	        4
+
+#define SYS_ORTABLE_REGISTERED_OK              0
+#define SYS_ORTABLE_REGISTRATION_FAILED       -1
+#define SYS_ORTABLE_UNREGISTERED_OK            0
+#define SYS_ORTABLE_NO_SUCH_REGISTRATION      -1
+
 
 #endif /* _MIBGROUP_SYSORTABLE_H */
