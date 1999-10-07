6ba63918d9ec6be210e3b585f8374b45d4a155a1
Dave Shield <dts12@users.sourceforge.net>
1999-10-07 14:26:20 +0000

    - (agent_trap.c agent_trap.h agent_read_config.c snmpd.h):   -  Re-write of agent trap handling: 	Provision for v1 traps with varbinds 	Consolidate v1 and v2 trap sink lists 	Preliminary INFORM support
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@3229 06827809-a52a-0410-b366-d66718629ded

--- agent/agent_trap.h
+++ agent/agent_trap.h
@@ -1,12 +1,15 @@
 #ifndef AGENT_TRAP_H
 #define AGENT_TRAP_H
 
-void send_easy_trap (int trap, int specific);
-void send_trap_pdu(struct snmp_pdu *pdu);
+void send_easy_trap (int, int);
+void send_trap_pdu(struct snmp_pdu *);
+void send_v2trap ( struct variable_list *);
+void send_trap_vars (int, int, struct variable_list *);
 
 void snmpd_parse_config_authtrap (char *, char *);
 void snmpd_parse_config_trapsink (char *, char *);
 void snmpd_parse_config_trap2sink (char *, char *);
+void snmpd_parse_config_informsink (char *, char *);
 void snmpd_free_trapsinks (void);
 void snmpd_parse_config_trapcommunity (char *, char *);
 void snmpd_free_trapcommunity (void);
