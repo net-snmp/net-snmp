f4ae6f6b1143490b468b729f5e790063fc390b0c
Wes Hardaker <hardaker@users.sourceforge.net>
1999-10-27 21:34:55 +0000

    - (Makefile.in, ucd-snmp-agent-includes.h): added a header file to   wrap around the various agent include files.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@3277 06827809-a52a-0410-b366-d66718629ded

---
+++ net-snmp/agent/ucd-snmp-agent-includes.h
@@ -0,0 +1,12 @@
+/* A simple helpful wrapper to include lots of agent specific include
+ * files for people wanting to embed and snmp agent into an external
+ * application
+ */
+#ifndef UCD_SNMP_AGENT_INCLUDES_H
+#define UCD_SNMP_AGENT_INCLUDES_H
+
+#include <ucd-snmp/snmp_agent.h>
+#include <ucd-snmp/snmp_vars.h>
+#include <ucd-snmp/ds_agent.h>
+
+#endif /* UCD_SNMP_AGENT_INCLUDES_H */
