b52d2adcbbfd1d0e1502e5c3b0ab96ed095c24d7
Michael Slifcak <slif@users.sourceforge.net>
1999-10-28 12:23:05 +0000

    u_int8/16/32_t types no longer needed for WIN32 builds.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@3287 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/win32/config.h
+++ net-snmp/win32/config.h
@@ -1064,12 +1064,6 @@
 
 /* #undef u_int32_t */
 
-typedef unsigned char  u_int8_t;
-
-typedef unsigned short u_int16_t;
-
-typedef unsigned int   u_int32_t;
-
 
 
 /* mib pointer to the top of the extensible tree.  This has been
