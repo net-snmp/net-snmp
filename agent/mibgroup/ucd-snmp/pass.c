51e86aed1ffecb8926a7f5cc6579d8662c8da2f6
Niels Baggesen <nba@users.sourceforge.net>
1999-11-14 10:01:37 +0000

    - pass.c, snmpd.conf.5.def: add an UNSIGNED return type
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@3350 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/ucd-snmp/pass.c
+++ agent/mibgroup/ucd-snmp/pass.c
@@ -252,8 +252,13 @@ u_char *var_extensible_pass(struct variable *vp,
           return((unsigned char *) buf2);
         } else if (!strncasecmp(buf,"integer",7)) {
           *var_len = sizeof(long_ret);
-          long_ret = atoi(buf2);
+          long_ret = strtol(buf2, NULL, 10);
           vp->type = ASN_INTEGER;
+          return((unsigned char *) &long_ret);
+        } else if (!strncasecmp(buf,"unsigned",7)) {
+          *var_len = sizeof(long_ret);
+          long_ret = strtoul(buf2, NULL, 10);
+          vp->type = ASN_UNSIGNED;
           return((unsigned char *) &long_ret);
         } else if (!strncasecmp(buf,"counter",7)) {
           *var_len = sizeof(long_ret);
