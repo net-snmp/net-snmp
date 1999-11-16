d41ddb90a5b90f8086c7f6cb75f50a820e57c04b
Michael Slifcak <slif@users.sourceforge.net>
1999-11-16 00:43:25 +0000

    Don't add NULL varbind when ENDOFMIBVIEW is reached.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@3373 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/snmp_agent.c
+++ net-snmp/agent/snmp_agent.c
@@ -300,13 +300,15 @@ handle_snmp_packet(int operation, struct snmp_session *session, int reqid,
 		  var_ptr = var_ptr->next_variable ) {
 				/* XXX: we don't know the size of the next
 					OID, so assume the maximum length */
+		if ( var_ptr->type != SNMP_ENDOFMIBVIEW )
+		{
 		var_ptr2 = snmp_add_null_var(asp->pdu, var_ptr->name, MAX_OID_LEN);
 		for ( i=var_ptr->name_length ; i<MAX_OID_LEN ; i++)
-		    var_ptr2->name[i] = '\0';
+		    var_ptr2->name[i] = 0;
 		var_ptr2->name_length = var_ptr->name_length;
 
-		if ( var_ptr->type != SNMP_ENDOFMIBVIEW )
 		    allDone = FALSE;
+		}
 	    }
 	    if ( allDone )
 		break;
