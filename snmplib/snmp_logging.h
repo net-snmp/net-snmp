89f08defa4d62ed0068d700d810aeda77513e995
Michael Slifcak <slif@users.sourceforge.net>
1999-12-15 17:42:44 +0000

    Remove snmp_log_syslog, snmp_log_filelog, snmp_log_stderrlog; Simplify snmp_log_string.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@3549 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_logging.h
+++ snmplib/snmp_logging.h
@@ -34,9 +34,6 @@ void snmp_disable_log(void);
 void snmp_enable_syslog(void);
 void snmp_enable_filelog(const char *logfilename, int dont_zero_log);
 void snmp_enable_stderrlog(void);
-void snmp_log_syslog(int priority, const char *string);
-void snmp_log_filelog(int priority, const char *string);
-void snmp_log_stderrlog(int priority, const char *string);
 
 int snmp_log(int priority, const char *format, ...);
 int snmp_vlog(int priority, const char *format, va_list ap);
