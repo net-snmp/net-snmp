06d1b83d58e0a95ab3c015a4fd7458ea17bebbdc
Dave Shield <dts12@users.sourceforge.net>
2000-01-24 15:21:34 +0000

    - (examples/{example,wombat}.[ch]):   - Consolidated example modules into one, including config and SET support.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@3713 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/examples/example.c
+++ agent/mibgroup/examples/example.c
@@ -5,6 +5,14 @@
 
 /* include important headers */
 #include <config.h>
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#if HAVE_STRING_H
+#include <string.h>
+#else
+#include <strings.h>
+#endif
 
 /* needed by util_funcs.h */
 #if TIME_WITH_SYS_TIME
@@ -18,6 +26,7 @@
 # endif
 #endif
 
+
 /* mibincl.h contains all the snmp specific headers to define the
    return types and various defines and structures. */
 #include "mibincl.h"
@@ -25,8 +34,28 @@
 /* header_generic() comes from here */
 #include "util_funcs.h"
 
+#include "agent_read_config.h"
+
 /* include our .h file */
 #include "example.h"
+
+
+   /*
+    *  Certain objects can be set via configuration file directives.
+    *  These variables hold the values for such objects, as they need to
+    *   be accessible to both the config handlers, and the callback routine.
+    */
+#define EXAMPLE_STR_LEN	300
+#define EXAMPLE_STR_DEFAULT	"life the universe and everything"
+int	example_int = 42;
+char	example_str[ EXAMPLE_STR_LEN ];
+
+	/* Forward declarations for the config handlers */
+void example_parse_config_exampleint( const char *token, char *cptr );
+void example_parse_config_examplestr( const char *token, char *cptr );
+void example_free_config_exampleint( void );
+void example_free_config_examplestr( void );
+
 
 	/*********************
 	 *
@@ -34,41 +63,48 @@
 	 *
 	 *********************/
 
-
-/* this is an optional function called at the time the agent starts up
-   to do any initilizations you might require.  You don't have to
-   create it, as it is optional. */
-
-/* IMPORTANT: If you add or remove this function, you *must* re-run
-   the configure script as it checks for its existance. */
-
-void init_example(void)
-{
-/* Define a 'variable' structure that is a representation of our mib. */
-
-/* first, we have to pick the variable type.  They are all defined in
-   the var_struct.h file in the agent subdirectory.  I'm picking the
-   variable2 structure since the longest sub-component of the oid I
-   want to load is .2.1 and .2.2 so I need at most 2 spaces in the
-   last entry. */
-
-  struct variable2 example_variables[] = {
-    { EXAMPLESTRING,  ASN_OCTET_STR, RONLY, var_example, 1, {1}},
-    /* Load the first table entry.  arguments:
-       1: EXAMPLESTRING: magic number to pass back to us and used to
-       check against the incoming mib oid requested
-       later.
-       2: STRING: type of value returned.
-       3: RONLY: Its read-only.  We can't use 'snmpset' to set it.
-       4: var_example: The return callback function, defined in example.c
-       5: 1: The length of the next miboid this will be located at
-
-       6: {1}: The sub-oid this table entry is for.  It's appended to
-       the table entry defined later.
-    */
-
-    /* Now do the rest.  The next two are the sub-table, at .2.1 and .2.2: */
-    { EXAMPLEINTEGER,   ASN_INTEGER,   RONLY, var_example, 2, {2,1}},
+    /*
+     * This array structure defines a representation of the
+     *  MIB being implemented.
+     *
+     * The type of the array is 'struct variableN', where N is
+     *  large enough to contain the longest OID sub-component
+     *  being loaded.  This will normally be the maximum value
+     *  of the fifth field in each line.  In this case, the second
+     *  and third entries are both of size 2, so we're using
+     *  'struct variable2'
+     *
+     * The supported values for N are listed in <agent/var_struct.h>
+     *  If the value you need is not listed there, simply use the
+     *  next largest that is.
+     *
+     * The format of each line is as follows
+     *  (using the first entry as an example):
+     *	    1: EXAMPLESTRING:
+     *		The magic number defined in the example header file.
+     *		This is passed to the callback routine and is used
+     *		  to determine which object is being queried.
+     *	    2: ASN_OCTET_STR:
+     *		The type of the object.
+     *		Valid types are listed in <snmp_impl.h>
+     *	    3: RONLY (or RWRITE):
+     *		Whether this object can be SET or not.
+     *	    4: var_example:
+     *		The callback routine, used when the object is queried.
+     *		This will usually be the same for all objects in a module
+     *		  and is typically defined later in this file.
+     *	    5: 1:
+     *		The length of the OID sub-component (the next field)
+     *	    6: {1}:
+     *		The OID sub-components of this entry.
+     *		In other words, the bits of the full OID that differ
+     *		  between the various entries of this array.
+     *		This value is appended to the common prefix (defined later)
+     *		  to obtain the full OID of each entry.
+     */
+struct variable2 example_variables[] = {
+    { EXAMPLESTRING,    ASN_OCTET_STR, RONLY, var_example, 1, {1}},
+    { EXAMPLEINTEGER,   ASN_INTEGER,   RWRITE, var_example, 2, {2,1}},
     { EXAMPLEOBJECTID,  ASN_OBJECT_ID, RONLY, var_example, 2, {2,2}},
     { EXAMPLETIMETICKS, ASN_TIMETICKS, RONLY, var_example, 1, {3}},
     { EXAMPLEIPADDRESS, ASN_IPADDRESS, RONLY, var_example, 1, {4}},
@@ -76,48 +112,144 @@ void init_example(void)
     { EXAMPLEGAUGE,     ASN_GAUGE,     RONLY, var_example, 1, {6}}
   };
 
-  /* Define the OID pointer to the top of the mib tree that we're
-   registering underneath. */
+    /*
+     * This array defines the OID of the top of the mib tree that we're
+     *  registering underneath.
+     * Note that this needs to be the correct size for the OID being 
+     *  registered, so that the length of the OID can be calculated.
+     *  The format given here is the simplest way to achieve this.
+     */
   oid example_variables_oid[] = { 1,3,6,1,4,1,2021,254 };
 
-  /* register ourselves with the agent to handle our mib tree
+   
 
-   This is a macro defined in ../../snmp_vars.h.  The arguments are:
-
-   descr:   A short description of the mib group being loaded.
-   var:     The variable structure to load.
-   vartype: The variable structure used to define it (variable2, variable4, ...)
-   theoid:  A *initialized* *exact length* oid pointer.
-            (sizeof(theoid) *must* return the number of elements!)  
-  */
+    /*
+     * This function is called at the time the agent starts up
+     *  to do any initializations that might be required.
+     *
+     * In theory it is optional and can be omitted if no
+     *  initialization is needed.  In practise, every module
+     *  will need to register itself (or the objects being
+     *  implemented will not appear in the MIB tree), and this
+     *  registration is typically done here.
+     *
+     * If this function is added or removed, you must re-run
+     *  the configure script, to detect this change.
+     */
+void init_example(void)
+{
+    /*
+     * Register ourselves with the agent to handle our mib tree.
+     * The arguments are:
+     *    descr:   A short description of the mib group being loaded.
+     *    var:     The variable structure to load.
+     *			(the name of the variable structure defined above)
+     *    vartype: The type of this variable structure
+     *    theoid:  The OID pointer this MIB is being registered underneath.
+     */
   REGISTER_MIB("example", example_variables, variable2, example_variables_oid);
 
-  /* call auto_nlist to load the nlist symbols.  We
-     actually don't need it, so its commented out. */
-  /* auto_nlist( "example_symbol" ); */
+
+    /*
+     *  Register config handlers for the two objects that can be set
+     *   via configuration file directive.
+     *  Also set a default value for the string object.  Note that the
+     *   example integer variable was initialised above.
+     */
+  strcpy( example_str, EXAMPLE_STR_DEFAULT );
+
+  snmpd_register_config_handler( "exampleint",
+			example_parse_config_exampleint,
+			example_free_config_exampleint,
+			"exampleint value" );
+  snmpd_register_config_handler( "examplestr",
+			example_parse_config_examplestr,
+			example_free_config_examplestr,
+			"examplestr value" );
+  snmpd_register_config_handler( "examplestring",
+			example_parse_config_examplestr,
+			example_free_config_examplestr,
+			"examplestring value" );
+
+    /*
+     * One common requirement is to read values from the kernel.
+     * This is usually initialised here, to speed up access when the
+     *  information is read in, as a response to an incoming request.
+     *
+     * This module doesn't actually use this mechanism,
+     * so this call is commented out here.
+     */
+  /* auto_nlist( "example_symbol", 0, 0 ); */
 }
 
-#if USING_DLMOD_MODULE
+	/*********************
+	 *
+	 *  Configuration file handling functions
+	 *
+	 *********************/
 
-static struct subtree example_subtree = {
-	{1,3,6,1,4,1,2021,254}, 8, example_variables, 
-	sizeof(example_variables)/sizeof(*example_variables), 
-	sizeof(*example_variables), "example"
-};
-
-int example_init(void) {
-  load_subtree(&example_subtree);
-  return 0;
+void
+example_parse_config_exampleint( const char *token, char *cptr )
+{
+    example_int = atoi( cptr );
 }
-#endif
+
+void
+example_parse_config_examplestr( const char *token, char *cptr )
+{
+		/*
+		 * Make sure the string fits in the space allocated for it.
+		 */
+    if ( strlen( cptr ) < EXAMPLE_STR_LEN )
+	strcpy( example_str, cptr );
+    else {
+		/*
+		 * Truncate the string if necessary.
+		 * An alternative approach would be to log an error,
+		 *  and discard this value altogether.
+		 */
+	strncpy( example_str, cptr, EXAMPLE_STR_LEN-3 );
+	example_str[ EXAMPLE_STR_LEN-3 ] = 0;
+	strcat(  example_str, "..." );
+    }
+}
+
+	/*  We don't need to do anything special when closing down */
+void
+example_free_config_exampleint( void ) {}
+
+void
+example_free_config_examplestr( void ) {}
+
 	/*********************
 	 *
 	 *  System specific implementation functions
 	 *
 	 *********************/
 
-/* define the callback function used in the example_variables
-   structure in example.h */
+    /*
+     * Define the callback function used in the example_variables structure.
+     * This is called whenever an incoming request refers to an object
+     *  within this sub-tree.
+     *
+     * Four of the parameters are used to pass information in.
+     * These are:
+     *	  vp	  The entry from the 'example_variables' array for the
+     *		   object being queried.
+     *	  name	  The OID from the request.
+     *	  length  The length of this OID.
+     *	  exact	  A flag to indicate whether this is an 'exact' request
+     *		   (GET/SET) or an 'inexact' one (GETNEXT/GETBULK).
+     *
+     * Four of the parameters are used to pass information back out.
+     * These are:
+     *	  name	   The OID being returned.
+     *	  length   The length of this OID.
+     *	  var_len  The length of the answer being returned.
+     *    write_method   A pointer to the SET function for this object.
+     *
+     * Note that name & length serve a dual purpose in both roles.
+     */
 
 u_char	*
 var_example(struct variable *vp,
@@ -127,34 +259,74 @@ var_example(struct variable *vp,
 	    size_t *var_len,
 	    WriteMethod **write_method)
 {
-  /* define any variables we might return as static! */
-  static long long_ret;
-  static char string[300];
-  static oid oid_ret[8];
+    /*
+     *  The result returned from this function needs to be a pointer to
+     *    static data (so that it can be accessed from outside).
+     *  Define suitable variables for any type of data we may return.
+     */
+  static char string[EXAMPLE_STR_LEN];	/* for EXAMPLESTRING   */
+  static oid  oid_ret[8];		/* for EXAMPLEOBJECTID */
+  static long long_ret;			/* for everything else */
   
-  /* header_generic is a simple function for finding out if we're in
-     the right place.  This only works on scalar objects.  Use
-     header_simple_table for simple tables, and write your own for anything
-     else. */
-  if (header_generic(vp, name, length, exact, var_len, write_method))
+    /*
+     * Before returning an answer, we need to check that the request
+     *  refers to a valid instance of this object.  The utility routine
+     *  'header_generic' can be used to do this for scalar objects.
+     *
+     * This routine 'header_simple_table' does the same thing for "simple"
+     *  tables. (See the AGENT.txt file for the definition of a simple table).
+     *
+     * Both these utility routines also set up default values for the
+     *  return arguments (assuming the check succeeded).
+     * The name and length are set suitably for the current object,
+     *  var_len assumes that the result is an integer of some form,
+     *  and write_method assumes that the object cannot be set.
+     *
+     * If these assumptions are correct, this callback routine simply
+     * needs to return a pointer to the appropriate value (using 'long_ret').
+     * Otherwise, 'var_len' and/or 'write_method' should be set suitably.
+     */
+  if (header_generic(vp, name, length, exact, var_len, write_method) == MATCH_FAILED)
     return NULL;
 
-  /* We can now simply test on vp's magic number, defined in example.h */
+
+    /*
+     * Many object will need to obtain data from the operating system in
+     *  order to return the appropriate value.  Typically, this is done
+     *  here - immediately following the 'header' call, and before the
+     *  switch statement. This is particularly appropriate if a single 
+     *  interface call can return data for all the objects supported.
+     *
+     * This example module does not rely on external data, so no such
+     *  calls are needed in this case.  
+     */
+
+    /*
+     * Now use the magic number from the variable pointer 'vp' to
+     *  select the particular object being queried.
+     * In each case, one of the static objects is set up with the
+     *  appropriate information, and returned mapped to a 'u_char *'
+     */
   switch (vp->magic){
     case EXAMPLESTRING:
-      /* set up return information */
-      sprintf(string,"life the universe and everything");
-      
-      /* set the length of the returned data */
+      sprintf(string, example_str);
+		/*
+		 * Note that the assumption that the answer will be an
+		 *  integer does not hold true in this case, so the length
+		 *  of the answer needs to be set explicitly.		
+		 */
       *var_len = strlen(string);
-      
-      /* return everything as mapped to a u_char * */
       return (u_char *) string;
 
     case EXAMPLEINTEGER:
-      long_ret = 42;
+		/*
+		 * Here the length assumption is correct, but the
+		 *  object is writeable, so we need to set the
+		 *  write_method pointer as well as the current value.
+		 */
+      long_ret = example_int;
+      *write_method = write_exampleint;
       return (u_char *) &long_ret;
-      /* note: var_len defaults to the length of a long */
       
     case EXAMPLEOBJECTID:
       oid_ret[0] = 1;
@@ -163,11 +335,18 @@ var_example(struct variable *vp,
       oid_ret[3] = 1;
       oid_ret[4] = 4;
       oid_ret[5] = oid_ret[6] = oid_ret[7] = 42;
+		/*
+		 * Again, the assumption regarding the answer length is wrong.
+		 */
       *var_len = 8*sizeof(oid);
       return (u_char *) oid_ret;
       
     case EXAMPLETIMETICKS:
-      long_ret = 363136200;
+		/*
+		 * Here both assumptions are correct,
+		 *  so we just need to set up the answer.
+		 */
+      long_ret = 363136200;	/* 42 days, 42 minutes and 42.0 seconds */
       return (u_char *) &long_ret;
       
     case EXAMPLEIPADDRESS:
@@ -181,12 +360,116 @@ var_example(struct variable *vp,
       return (u_char *) &long_ret;
       
     case EXAMPLEGAUGE:
-      long_ret = 42;
+      long_ret = 42;	/* Do we detect a theme running through these answers? */
       return (u_char *) &long_ret;
 
     default:
+		/*
+		 *  This will only be triggered if there's a problem with
+		 *   the coding of the module.  SNMP requests that reference
+		 *   a non-existant OID will be directed elsewhere.
+		 *  If this branch is reached, log an error, so that
+		 *   the problem can be investigated.
+		 */
       DEBUGMSGTL(("snmpd", "unknown sub-id %d in examples/var_example\n", vp->magic));
   }
-  /* if we fall to here, fail by returning NULL */
+  /*
+   * If we fall through to here, fail by returning NULL.
+   * This is essentially a continuation of the 'default' case above.
+   */
   return NULL;
 }
+
+	/*********************
+	 *
+	 *  Writeable object SET handling routines
+	 *
+	 *********************/
+int
+write_exampleint(int	action,
+		 u_char	*var_val,
+		 u_char	var_val_type,
+		 size_t	var_val_len,
+		 u_char	*statP,
+		 oid	*name,
+		 size_t	name_len)
+{
+		/* Define an arbitrary maximum permissible value */
+#define MAX_EXAMPLE_INT	100
+    static long intval;
+    static long old_intval;
+
+    switch ( action ) {
+	case RESERVE1:
+			/*
+			 *  Check that the value being set is acceptable
+			 */
+	    if (var_val_type != ASN_INTEGER ) {
+		DEBUGMSGTL(("example", "%x not integer type", var_val_type));
+		return SNMP_ERR_WRONGTYPE;
+	    }
+	    if (var_val_len > sizeof(long)) {
+		DEBUGMSGTL(("example", "wrong length %x", var_val_len));
+		return SNMP_ERR_WRONGLENGTH;
+	    }
+
+	    intval = *((long *) var_val);
+	    if (intval > MAX_EXAMPLE_INT) {
+		DEBUGMSGTL(("example", "wrong value %x", intval));
+		return SNMP_ERR_WRONGVALUE;
+	    }
+	    break;
+
+	case RESERVE2:
+			/*
+			 *  This is conventially where any necesary
+			 *   resources are allocated (e.g. calls to malloc)
+			 *  Here, we are using static variables
+			 *   so don't need to worry about this.
+			 */
+	    break;
+
+	case FREE:
+			/*
+			 *  This is where any of the above resources
+			 *   are freed again (because one of the other
+			 *   values being SET failed for some reason).
+			 *  Again, since we are using static variables
+			 *   we don't need to worry about this either.
+			 */
+	    break;
+
+	case ACTION:
+			/*
+			 *  Set the variable as requested.
+			 *   Note that this may need to be reversed,
+			 *   so save any information needed to do this.
+			 */
+	    old_intval	= example_int;
+	    example_int	= intval;
+	    break;
+
+	case UNDO:
+			/*
+			 *  Something failed, so re-set the
+			 *   variable to its previous value
+			 *  (and free any allocated resources).
+			 */
+	    example_int	= old_intval;
+	    break;
+
+	case COMMIT:
+			/*
+			 *  Everything worked, so we can discard any
+			 *   saved information, and make the change
+			 *   permanent (e.g. write to the config file).
+			 *  We also free any allocated resources.
+			 *
+			 *  In this case, there's nothing to do.
+			 */
+	    break;
+
+    }
+    return SNMP_ERR_NOERROR;
+}
+
--- agent/mibgroup/examples/example.h
+++ agent/mibgroup/examples/example.h
@@ -1,40 +1,50 @@
 /*
  *  Template MIB group interface - example.h
  *
- *  This file is essentially a copy of wombat.h and then modified.
- *
- *  This mib is essentially duplicating the passtest script in the
- *  local directory, used by the EXAMPLE.conf file in the top level
- *  source directory.
- *
  */
 
 /* Don't include ourselves twice */
 #ifndef _MIBGROUP_EXAMPLE_H
 #define _MIBGROUP_EXAMPLE_H
 
+    /*
+     * We use 'header_generic' from the util_funcs module,
+     *  so make sure this module is included in the agent.
+     */
 config_require(util_funcs)
 
-/* Define all our functions using prototyping for ANSI compilers */
-/* These functions are then defined in the example.c file */
 
-void	init_example(void);
-FindVarMethod var_example;
+    /*
+     * Declare our publically-visible functions.
+     * Typically, these will include the initialization and shutdown functions,
+     *  the main request callback routine and any writeable object methods.
+     *
+     * Function prototypes are provided for the callback routine ('FindVarMethod')
+     *  and writeable object methods ('WriteMethod').
+     */
+extern void		init_example(void);
+extern FindVarMethod	var_example;
+extern WriteMethod	write_exampleint;
 
 
-/* Magic number definitions.  These numbers are the last oid index
-   numbers to the table that you are going to define.  For example,
-   lets say (since we are) creating a mib table at the location
-   .1.3.6.1.4.1.2021.254.  The following magic numbers would be the
-   next numbers on that oid for the var_example function to use, ie:
-   .1.3.6.1.4.1.2021.254.1 (and .2 and .3 ...) */
+    /*
+     * Magic number definitions.
+     * These must be unique for each object implemented within a
+     *  single mib module callback routine.
+     *
+     * Typically, these will be the last OID sub-component for
+     *  each entry, or integers incrementing from 1.
+     *  (which may well result in the same values anyway).
+     *
+     * Here, the second and third objects are form a 'sub-table' and
+     *   the magic numbers are chosen to match these OID sub-components.
+     * This is purely for programmer convenience.
+     * All that really matters is that the numbers are unique.
+     */
 
 #define	EXAMPLESTRING		1
-/* These two are going to be a sub-table at ...2021.254.1.2.X */
-/* they must be unique, and don't have to map exactly to the mib oids */
 #define EXAMPLEINTEGER		21  
 #define	EXAMPLEOBJECTID         22
-/* Back to the normal table */
 #define EXAMPLETIMETICKS	3
 #define	EXAMPLEIPADDRESS        4
 #define EXAMPLECOUNTER		5  
