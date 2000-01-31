8fa9b8c47d8b1239286670ef60680908245150cf
Dave Shield <dts12@users.sourceforge.net>
2000-01-31 10:40:02 +0000

    - (snmplib/snmp_api.[ch]): Restore traditional socket field names.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@3768 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_api.h
+++ snmplib/snmp_api.h
@@ -60,18 +60,15 @@ struct timeval;
 		(sizeof( unsigned short ) + _UCD_SS_PAD1SIZE + _UCD_SS_ALIGNSIZE ))
 
 typedef struct {
-/* NOTE: Underscore is inserted into member names
- *       to avoid namespace pollution (IRIX 6.x)
- */
 #ifdef STRUCT_SOCKADDR_HAS_SA_LEN
-    unsigned char	sa__len;
-    unsigned char	sa__family;
+    unsigned char	sa_len;
+    unsigned char	sa_family;
 #else
-    unsigned short	sa__family;
+    unsigned short	sa_family;
 #endif
-    char		sa__data[ _UCD_SS_PAD1SIZE ];
-    long		sa__align;
-    char		sa__pad2[ _UCD_SS_PAD2SIZE ];
+    char		sa_data[ _UCD_SS_PAD1SIZE ];
+    long		sa_align;
+    char		sa_pad2[ _UCD_SS_PAD2SIZE ];
 } snmp_ipaddr;
 
 #define USM_AUTH_KU_LEN     32
