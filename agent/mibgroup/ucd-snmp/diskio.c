278caea75c9ae1224f5d8163c58c9c3fbd48d00a
Wes Hardaker <hardaker@users.sourceforge.net>
2000-02-01 16:34:24 +0000

    - (diskio.c): move to ucdExperimental.15.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@3786 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/ucd-snmp/diskio.c
+++ agent/mibgroup/ucd-snmp/diskio.c
@@ -68,7 +68,7 @@ void init_diskio(void)
 
   /* Define the OID pointer to the top of the mib tree that we're
    registering underneath. */
-  oid diskio_variables_oid[] = { 1,3,6,1,4,1,4579,1,1 };
+  oid diskio_variables_oid[] = { 1,3,6,1,4,1,2021,13,15 };
 
   /* register ourselves with the agent to handle our mib tree
 
