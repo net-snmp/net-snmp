11d45cbe59fcd8b37631d4d5bd8ed05a850a3fb1
Michael Slifcak <slif@users.sourceforge.net>
2000-02-16 16:18:50 +0000

    include time.h once
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@3906 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII/interfaces.c
+++ agent/mibgroup/mibII/interfaces.c
@@ -108,16 +108,6 @@
 #if HAVE_IOCTLS_H
 #include <ioctls.h>
 #endif
-#if TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
-#endif
 
 #if HAVE_DMALLOC_H
 #include <dmalloc.h>
