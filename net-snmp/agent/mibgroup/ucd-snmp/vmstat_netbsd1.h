6c6bd981c50c6eff30f42f5d709947cbb3e270b3
Niels Baggesen <nba@users.sourceforge.net>
2000-03-04 19:15:48 +0000

    - ucd_snmp.h, {memory,vmstat}_netbsd1.[ch]: support for NetBSD
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@3947 06827809-a52a-0410-b366-d66718629ded

---
+++ net-snmp/agent/mibgroup/ucd-snmp/vmstat_netbsd1.h
@@ -0,0 +1,24 @@
+/*
+ *  vmstat mib groups
+ *
+ */
+#ifndef _MIBGROUP_VMSTAT_H
+#define _MIBGROUP_VMSTAT_H
+
+#include "mibdefs.h"
+
+extern FindVarMethod var_extensible_vmstat;
+extern void init_vmstat_netbsd1(void);
+
+#define SWAPIN 3
+#define SWAPOUT 4
+#define IOSENT 5
+#define IORECEIVE 6
+#define SYSINTERRUPTS 7
+#define SYSCONTEXT 8
+#define CPUUSER 9
+#define CPUSYSTEM 10
+#define CPUIDLE 11
+#define CPUERROR 16
+
+#endif /* _MIBGROUP_VMSTAT_H */
