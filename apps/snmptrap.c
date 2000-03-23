cc8b0895aba7180699b89ea50deb68170f37e97a
Niels Baggesen <nba@users.sourceforge.net>
2000-03-23 11:02:15 +0000

    - snmptrap.1, snmpcmd.1, snmpinform.1, snmptrap.c, Makefile.in: 	create and document snmpinform.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-1-1-patches@4002 06827809-a52a-0410-b366-d66718629ded

--- apps/snmptrap.c
+++ apps/snmptrap.c
@@ -84,10 +84,11 @@ oid objid_enterprise[] = {1, 3, 6, 1, 4, 1, 3, 1, 1};
 oid objid_sysdescr[]   = {1, 3, 6, 1, 2, 1, 1, 1, 0};
 oid objid_sysuptime[]  = {1, 3, 6, 1, 2, 1, 1, 3, 0};
 oid objid_snmptrap[]   = {1, 3, 6, 1, 6, 3, 1, 1, 4, 1, 0};
+int inform = 0;
 
 void usage(void)
 {
-    fprintf(stderr,"Usage: snmptrap ");
+    fprintf(stderr,"Usage: %s ", inform ? "snmpinform" : "snmptrap");
     snmp_parse_args_usage(stderr);
     fprintf(stderr," [<trap parameters> ...]\n\n");
     snmp_parse_args_descriptions(stderr);
@@ -132,12 +133,15 @@ int main(int argc, char *argv[])
     oid name[MAX_OID_LEN];
     size_t name_length;
     int	arg;
-    int status, inform = 0;
+    int status;
     char *trap = NULL, *specific = NULL, *description = NULL, *agent = NULL;
+    char *prognam;
 
-    /*
-     * usage: snmptrap gateway-name trap-type specific-type device-description [ -a agent-addr ]
-     */
+    prognam = strrchr(argv[0], '/');
+    if (prognam) prognam++;
+    else prognam = argv[0];
+
+    if (strcmp(prognam, "snmpinform") == 0) inform = 1;
     arg = snmp_parse_args(argc, argv, &session, NULL, NULL);
 
     SOCK_STARTUP;
@@ -155,6 +159,10 @@ int main(int argc, char *argv[])
     }
 
     if (session.version == SNMP_VERSION_1) {
+	if (inform) {
+	    fprintf(stderr, "Cannot send INFORM as SNMPv1 PDU\n");
+	    exit(1);
+	}
 	pdu = snmp_pdu_create(SNMP_MSG_TRAP);
 	pduIp = (struct sockaddr_in *)&pdu->agent_addr;
 	if (arg == argc) {
@@ -223,13 +231,7 @@ int main(int argc, char *argv[])
     else {
 	long sysuptime;
 	char csysuptime [20];
-	char *prognam;
 
-	prognam = strrchr(argv[0], '/');
-	if (prognam) prognam++;
-	else prognam = argv[0];
-
-	if (strcmp(prognam, "snmpinform") == 0) inform = 1;
 	pdu = snmp_pdu_create(inform ? SNMP_MSG_INFORM : SNMP_MSG_TRAP2);
 	if (arg == argc) {
 	    fprintf(stderr, "Missing up-time parameter\n");
