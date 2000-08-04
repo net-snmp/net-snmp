c34943815bf3216d61f598bd03c072ff323cfd8e
Dave Shield <dts12@users.sourceforge.net>
2000-08-04 12:26:28 +0000

    - (mibgroup/mibJJ.h mibgroup/mibJJ/*): Experimental re-write of mibII
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@4402 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/mibgroup/mibJJ.h
@@ -0,0 +1,11 @@
+/* module to include the modules relavent to the mib-JJ mib(s) */
+
+config_require(mibJJ/system_mib)
+config_require(mibJJ/sysORTable)
+config_require(mibJJ/interfaces)
+config_require(mibJJ/snmp_mib)
+config_require(mibJJ/tcp)
+config_require(mibJJ/icmp)
+config_require(mibJJ/ip)
+config_require(mibJJ/udp)
+config_require(mibJJ/vacm_vars)
