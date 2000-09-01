ff3c3ffa21286122676350472b4edf50a025efec
Wes Hardaker <hardaker@users.sourceforge.net>
2000-09-01 22:14:24 +0000

    - (vmstat_solaris2.c, vmstat_solaris2.h): Patch from Jochen Kmietsch:   - updated code.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@4476 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/ucd-snmp/vmstat_solaris2.h
+++ net-snmp/agent/mibgroup/ucd-snmp/vmstat_solaris2.h
@@ -1,12 +1,8 @@
 /*
  *  vmstat_solaris2.h
  *  Header file for vmstat_solaris2 module for UCD-SNMP
- *  Jochen Kmietsch <jochen.kmietsch@gmx.de>
- *  see vmstat_solaris2.c for more comments
- *  Version 0.1 initial release (Dec 1999)
- *  Version 0.2 added support for multiprocessor machines (Jan 2000)
- *  Version 0.3 some reliability enhancements and compile time fixes (Feb 2000)
- *  Version 0.4 portability issue and raw cpu value support (Jun 2000)
+ *  Jochen Kmietsch <kmietsch@jochen.de>
+ *  see vmstat_solaris2.c for revision history and additional comments
  *
  */
 
@@ -14,8 +10,12 @@
 #ifndef _MIBGROUP_VMSTAT_H
 #define _MIBGROUP_VMSTAT_H
 
-/* dependency requirements */
+/* The following statements are used by the configure script */
+/* Dependency requirements */
 config_arch_require(solaris2, kernel_sunos5)
+
+/* Directive to include utility module */
+config_require(util_funcs)
 
 /* Defines for vp magic numbers */
 #define SWAPIN 3
@@ -36,16 +36,18 @@ config_arch_require(solaris2, kernel_sunos5)
 /* MIB wants CPU_SYSTEM which is sysinfo CPU_KERNEL + CPU_WAIT */
 #define CPU_SYSTEM 4 
 
-/* Directive to include utility module */
-config_require(util_funcs)
+/* Time interval to gather system data */
+/* Lower value -> more accurate data, higher value -> less CPU usage */
+/* Value is in seconds */
+#define POLL_INTERVAL 60
+
+/* How many snapshots of system data to keep.  Values returned are over */
+/* time difference between first and last snapshot */
+/* Using POLL_INTERVAL 60 and POLL_VALUES 5 we get the values */
+/* over five minutes, which is a reasonable figure */
+#define POLL_VALUES    5
 
 /* Declared in vmstat_solaris2.c, from prototype */
 extern void init_vmstat_solaris2(void);
-
-/* Declared in snmp_vars.h */
-extern FindVarMethod var_extensible_vmstat; 
-
-/* Missing in Solaris header files prior to 2.6, no harm done on 2.6 and up. */
-extern int getpagesize(void);
 
 #endif /* _MIBGROUP_VMSTAT_H */
