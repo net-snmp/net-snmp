54ef1bbf1ca0ee98927616a2923c08856578602f
Wes Hardaker <hardaker@users.sourceforge.net>
2000-09-18 21:59:57 +0000

    - (snmp_vars.h, snmpd.h): Patch from Harrie Hazewinkel:   - move getStatPtr definition to proper .h file.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@4497 06827809-a52a-0410-b366-d66718629ded

--- agent/snmpd.h
+++ agent/snmpd.h
@@ -12,8 +12,6 @@ extern int (*sd_handlers[])(int);
 extern int smux_listen_sd;
 
 extern int snmp_read_packet (int);
-extern u_char *getStatPtr (oid *, size_t *, u_char *, size_t *,
-	u_short *, int, WriteMethod **write_method, struct snmp_pdu *, int *);
 
 /* config file parsing routines */
 void agentBoots_conf (char *, char *);
