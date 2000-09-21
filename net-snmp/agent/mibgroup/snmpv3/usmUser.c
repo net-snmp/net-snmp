29ad815d874c7edca973931d499eb9ab6f5bf492
Wes Hardaker <hardaker@users.sourceforge.net>
2000-09-21 00:27:33 +0000

    - (vacm_vars.c, usmUser.c, snmpv3.c): NULL out help messages for .conf   tokens that are either internal or no longer the standard way of   doing things.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@4516 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/snmpv3/usmUser.c
+++ net-snmp/agent/mibgroup/snmpv3/usmUser.c
@@ -57,8 +57,7 @@ void
 init_usmUser(void)
 {
   snmpd_register_config_handler("usmUser",
-                                usm_parse_config_usmUser, NULL,
-                                "internal use only");
+                                usm_parse_config_usmUser, NULL, NULL);
   snmpd_register_config_handler("createUser",
                                 usm_parse_create_usmUser, NULL,
                                 "username (MD5|SHA) passphrase [DES] [passphrase]");
