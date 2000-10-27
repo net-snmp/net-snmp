31a44258dae083ee176749458b72d75c526928d1
Wes Hardaker <hardaker@users.sourceforge.net>
2000-10-27 00:51:08 +0000

    - (eval_tools.sh): make SKIPIFNOT check the mib module list too.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@4659 06827809-a52a-0410-b366-d66718629ded

--- testing/eval_tools.sh
+++ testing/eval_tools.sh
@@ -87,7 +87,7 @@ GRONIK
 #------------------------------------ -o-
 #
 SKIPIFNOT() {
-	grep "define $1" $SNMP_UPDIR/config.h > /dev/null
+	grep "define $1" $SNMP_UPDIR/config.h $SNMP_UPDIR/mib_module_config.h > /dev/null
 	if [ $? != 0 ]; then
 	    REMOVETESTDATA
 	    echo "SKIPPED"
