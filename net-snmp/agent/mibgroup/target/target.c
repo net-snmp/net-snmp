4a3d0c69ef4f9d3ac8f02b91ad14cd71fd639c7a
Wes Hardaker <hardaker@users.sourceforge.net>
2000-11-07 16:33:54 +0000

    - (target.c): a better sanity check for session deletion.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@4705 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/target/target.c
+++ net-snmp/agent/mibgroup/target/target.c
@@ -87,7 +87,7 @@ get_target_sessions(char *taglist, TargetFilterFunction *filterfunct,
                         }
 
                         if (targaddrs->storageType != ST_PERMANENT &&
-                            param->updateTime &&
+                            targaddrs->sess &&
                             param->updateTime >=
                             targaddrs->sessionCreationTime) {
                             /* parameters have changed, nuke the old session */
