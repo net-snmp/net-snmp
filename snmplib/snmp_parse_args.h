f905edd6b9683451c79cec2698345c71e10a5a29
Niels Baggesen <nba@users.sourceforge.net>
2000-11-30 22:06:03 +0000

    - getopt.h, snmp_parse_args.h: extern "C" bracketing
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@4802 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_parse_args.h
+++ snmplib/snmp_parse_args.h
@@ -1,6 +1,12 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int snmp_parse_args (int, char * const *, struct snmp_session *, const char *, void (*)(int, char *const *, int));
 void snmp_parse_args_descriptions (FILE *);
 void snmp_parse_args_usage (FILE *);
 oid *snmp_parse_oid (const char *,oid *,size_t *);
 
+#ifdef __cplusplus
+}
+#endif
