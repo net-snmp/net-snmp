d5374b3d39557e1e49cc6e4a9548baa7bfa93d1e
Michael Slifcak <slif@users.sourceforge.net>
2001-03-14 18:00:20 +0000

    Avoid warnings of struct timeval decl in system.h by including *time.h*.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-2-patches@5044 06827809-a52a-0410-b366-d66718629ded

--- snmplib/parse.c
+++ snmplib/parse.c
@@ -70,6 +70,20 @@ SOFTWARE.
 #  include <ndir.h>
 # endif
 #endif
+#if TIME_WITH_SYS_TIME
+# ifdef WIN32
+#  include <sys/timeb.h>
+# else
+#  include <sys/time.h>
+# endif
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 #if HAVE_WINSOCK_H
 #include <winsock.h>
 #endif
