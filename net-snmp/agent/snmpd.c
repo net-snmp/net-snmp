0578367624011e04ee4fa21e29490128fa9cdb38
John Naylon <jbpn@users.sourceforge.net>
2001-04-04 13:46:57 +0000

    - call external signal handlers before calling snmp_select_info(),    rather than after calling snmp_select_info() but before select().    This fixes a potential problem that could arise if a signal handler    closed a session, which would cause select() to return -1 with    EBADF, and thus terminate the agent.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@5165 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/snmpd.c
+++ net-snmp/agent/snmpd.c
@@ -696,6 +696,14 @@ receive(void)
 	    snmp_log(LOG_INFO, "Reconfiguring daemon\n");
 	    update_config();
         }
+
+	for (i = 0; i < NUM_EXTERNAL_SIGS; i++) {
+	    if (external_signal_scheduled[i]) {
+		external_signal_scheduled[i]--;
+		external_signal_handler[i](i);
+	    }
+	}
+
 	tvp =  &timeout;
 	tvp->tv_sec = 0;
 	tvp->tv_usec = TIMETICK;
@@ -720,13 +728,6 @@ receive(void)
 	}
 #endif	/* USING_SMUX_MODULE */
 
-	for (i = 0; i < NUM_EXTERNAL_SIGS; i++) {
-	    if (external_signal_scheduled[i]) {
-		external_signal_scheduled[i]--;
-		external_signal_handler[i](i);
-	    }
-	}
-	
 	for (i = 0; i < external_readfdlen; i++) {
 	    FD_SET(external_readfd[i], &readfds);
 	    if (external_readfd[i] >= numfds)
