eb04c1ba78d4856300a6b5ade7c4cb03f56b547f
Michael Slifcak <slif@users.sourceforge.net>
2001-05-09 19:36:13 +0000

    Include Alex Rozin's Rmon.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@5258 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/mibgroup/Rmon/rows.h
@@ -0,0 +1,38 @@
+/**************************************************************
+ * Copyright (C) 2001 Alex Rozin, Optical Access 
+ *
+ *                     All Rights Reserved
+ *
+ * Permission to use, copy, modify and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation.
+ * 
+ * ALEX ROZIN DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ * ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+ * ALEX ROZIN BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ ******************************************************************/
+
+/*
+ * $Log$
+ * Revision 1.1  2001/05/09 19:36:13  slif
+ * Include Alex Rozin's Rmon.
+ *
+ * Revision 1.1.2.1  2001/04/16 14:47:31  alex
+ * Rmon1 : first edition (only history group and rows api);
+ * ethernet statistics are emulated by random values.
+ *
+ */
+
+#ifndef _rows_h_included__
+#define _rows_h_included__
+
+void init_rows(void);
+
+#endif /* _rows_h_included__ */
+
