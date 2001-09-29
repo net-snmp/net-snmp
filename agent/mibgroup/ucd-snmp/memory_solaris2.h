2a4a5b8ba4074bd999078b9383b6632856bbb619
Niels Baggesen <nba@users.sourceforge.net>
2001-09-29 13:05:24 +0000

    Fix a bad #ifdef parantheses
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-2-patches@5643 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/ucd-snmp/memory_solaris2.h
+++ agent/mibgroup/ucd-snmp/memory_solaris2.h
@@ -2,11 +2,11 @@
  *  memory quantity mib groups
  *
  */
-#ifndef _MIBGROUP_MEMORY_H
-#define _MIBGROUP_MEMORY_H
+#ifndef _MIBGROUP_MEMORY_SOLARIS2_H
+#define _MIBGROUP_MEMORY_SOLARIS2_H
 
 #include "mibdefs.h"
 
 void init_memory_solaris2(void);
 
-#endif /* _MIBGROUP_MEMORY_H */
+#endif /* _MIBGROUP_MEMORY_SOLARIS2_H */
