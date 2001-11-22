e897a5ba22bba3af639df981c69085c242fea890
Wes Hardaker <hardaker@users.sourceforge.net>
2001-11-22 08:20:45 +0000

    mork wore on the notification log mib.  It now supports variable storage, but segfaults on access due to pdu cloning problems I'm not going to work out right now.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@5907 06827809-a52a-0410-b366-d66718629ded

--- apps/notification_log.h
+++ apps/notification_log.h
@@ -7,6 +7,7 @@
  */
 void init_notification_log(void);
 NodeHandler     nlmLogTable_handler;
+NodeHandler     nlmLogVariableTable_handler;
 
 /*
  * column number definitions for table nlmLogTable 
@@ -20,5 +21,21 @@ NodeHandler     nlmLogTable_handler;
 #define COLUMN_NLMLOGCONTEXTENGINEID		7
 #define COLUMN_NLMLOGCONTEXTNAME		8
 #define COLUMN_NLMLOGNOTIFICATIONID		9
+
+/*
+ * column number definitions for table nlmLogVariableTable 
+ */
+#define COLUMN_NLMLOGVARIABLEINDEX		1
+#define COLUMN_NLMLOGVARIABLEID		2
+#define COLUMN_NLMLOGVARIABLEVALUETYPE		3
+#define COLUMN_NLMLOGVARIABLECOUNTER32VAL		4
+#define COLUMN_NLMLOGVARIABLEUNSIGNED32VAL		5
+#define COLUMN_NLMLOGVARIABLETIMETICKSVAL		6
+#define COLUMN_NLMLOGVARIABLEINTEGER32VAL		7
+#define COLUMN_NLMLOGVARIABLEOCTETSTRINGVAL		8
+#define COLUMN_NLMLOGVARIABLEIPADDRESSVAL		9
+#define COLUMN_NLMLOGVARIABLEOIDVAL		10
+#define COLUMN_NLMLOGVARIABLECOUNTER64VAL		11
+#define COLUMN_NLMLOGVARIABLEOPAQUEVAL		12
 
 #endif /* NOTIFICATION_LOG_H */
