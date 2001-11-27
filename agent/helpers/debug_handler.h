c2a99fcdaf26ccfd4d9320626e4e506ba67e5937
Wes Hardaker <hardaker@users.sourceforge.net>
2001-11-27 15:48:32 +0000

    debug handler created for inserting debugging output into the calling chain.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@5939 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/helpers/debug_handler.h
@@ -0,0 +1,9 @@
+#ifndef DEBUG_HANDLER_H
+#define DEBUG_HANDLER_H
+
+mib_handler *get_debug_handler(void);
+void init_debug_helper(void);
+   
+NodeHandler debug_helper;
+
+#endif /* DEBUG_HANDLER_H */
