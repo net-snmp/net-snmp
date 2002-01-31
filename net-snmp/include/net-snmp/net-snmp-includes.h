a7a40133f41b4b4d4487c37c116202ba14e012e5
Wes Hardaker <hardaker@users.sourceforge.net>
2002-01-31 22:58:01 +0000

    s/snmplib\///;
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@6216 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/net-snmp-includes.h
+++ net-snmp/include/net-snmp/net-snmp-includes.h
@@ -7,17 +7,17 @@
 /* uncomment if you don't have in_addr_t in netinet/in.h */
 /* typedef u_int in_addr_t; */
 
-#include <net-snmp/snmplib/asn1.h>
-#include <net-snmp/snmplib/snmp_api.h>
-#include <net-snmp/snmplib/snmp_impl.h>
-#include <net-snmp/snmplib/snmp_client.h>
-#include <net-snmp/snmplib/mib.h>
-#include <net-snmp/snmplib/snmp.h>
-#include <net-snmp/snmplib/snmp-tc.h>
+#include <net-snmp/asn1.h>
+#include <net-snmp/snmp_api.h>
+#include <net-snmp/snmp_impl.h>
+#include <net-snmp/snmp_client.h>
+#include <net-snmp/mib.h>
+#include <net-snmp/snmp.h>
+#include <net-snmp/snmp-tc.h>
 
-#include <net-snmp/snmplib/default_store.h>
-#include <net-snmp/snmplib/snmp_logging.h>
-#include <net-snmp/snmplib/snmp_debug.h>
-#include <net-snmp/snmplib/read_config.h>
-#include <net-snmp/snmplib/tools.h>
-#include <net-snmp/snmplib/system.h>
+#include <net-snmp/default_store.h>
+#include <net-snmp/snmp_logging.h>
+#include <net-snmp/snmp_debug.h>
+#include <net-snmp/read_config.h>
+#include <net-snmp/tools.h>
+#include <net-snmp/system.h>
