ce6bd04ddd55f019ae05316c42c4419006b76024
Dave Shield <dts12@users.sourceforge.net>
2002-02-06 13:55:32 +0000

    Framework for new library header file organisation, with eight main groups of API routines.  Currently these still refer to the UCD-style style header files for the actual function declarations.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@6233 06827809-a52a-0410-b366-d66718629ded

---
+++ include/net-snmp/logging_api.h
@@ -0,0 +1,26 @@
+#ifndef NET_SNMP_LOGGING_API_H
+#define NET_SNMP_LOGGING_API_H
+
+    /**
+     *  Library API routines concerned with logging and message output
+     *    (including error handling and debugging).
+     */
+
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/types.h>
+
+    /*
+     *  For the initial release, this will just refer to the
+     *  relevant UCD header files.
+     *    In due course, the routines relevant to this area of the
+     *  API will be identified, and listed here directly.
+     *
+     *  But for the time being, this header file is a placeholder,
+     *  to allow application writers to adopt the new header file names.
+     */
+
+#include <net-snmp/snmp_client.h>
+#include <net-snmp/snmp_debug.h>
+#include <net-snmp/snmp_logging.h>
+
+#endif /* NET_SNMP_LOGGING_API_H */
---
+++ include/net-snmp/types.h
@@ -0,0 +1,36 @@
+#ifndef NET_SNMP_TYPES_H
+#define NET_SNMP_TYPES_H
+
+    /**
+     *  Definitions of data structures, used within the library API.
+     */
+
+#include <net-snmp/net-snmp-config.h>
+#include <sys/types.h>
+
+#ifndef MAX_SUBID	/* temporary - duplicate definition protection */
+#ifndef EIGHTBIT_SUBIDS
+typedef u_long	oid;
+#define MAX_SUBID   0xFFFFFFFF
+#else
+typedef u_char	oid;
+#define MAX_SUBID   0xFF
+#endif
+#endif
+
+
+    /*
+     *  For the initial release, this will just refer to the
+     *  relevant UCD header files.
+     *    In due course, the types and structures relevant to the
+     *  Net-SNMP API will be identified, and defined here directly.
+     *
+     *  But for the time being, this header file is primarily a placeholder,
+     *  to allow application writers to adopt the new header file names.
+     */
+
+#include <net-snmp/definitions.h>
+#include <net-snmp/snmp_api.h>
+/* #include <net-snmp/libsnmp.h> */
+
+#endif /* NET_SNMP_TYPES_H */
