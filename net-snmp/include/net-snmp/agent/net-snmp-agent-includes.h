419b7065341ebb6a3bfa6818dff5d0e8862b74c6
Wes Hardaker <hardaker@users.sourceforge.net>
2002-02-13 00:38:47 +0000

    add agent/agent_handler.h
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@6309 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/agent/net-snmp-agent-includes.h
+++ net-snmp/include/net-snmp/agent/net-snmp-agent-includes.h
@@ -12,6 +12,7 @@
 #include <net-snmp/agent/var_struct.h>
 #include <net-snmp/agent/agent_read_config.h>
 #include <net-snmp/agent/agent_trap.h>
+#include <net-snmp/agent/agent_handler.h>
 
 #include <net-snmp/agent/mib_module_config.h>
 
