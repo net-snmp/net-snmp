da2f5fe49841435fe120a4a9e31727745cc52f72
Wes Hardaker <hardaker@users.sourceforge.net>
2002-02-16 00:28:01 +0000

    add back in netinet/in.h and sys/time.h (in proper protected form) which was deleted from 4.2.3 for some reason we never figured out.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-2-patches@6365 06827809-a52a-0410-b366-d66718629ded

--- snmplib/ucd-snmp-includes.h
+++ snmplib/ucd-snmp-includes.h
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+#if TIME_WITH_SYS_TIME
+# ifdef WIN32
+#  include <sys/timeb.h>
+# else
+#  include <sys/time.h>
+# endif
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
 /* uncomment if you don't have in_addr_t in netinet/in.h */
 /* typedef u_int in_addr_t; */
 
