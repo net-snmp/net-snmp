482d99bd986ba06dd54031b56a4eb033bd3c8313
Niels Baggesen <nba@users.sourceforge.net>
2002-02-25 16:57:26 +0000

    FreeBSD4 has new disk names
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-2-patches@6441 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/host/hr_disk.c
+++ agent/mibgroup/host/hr_disk.c
@@ -185,6 +185,9 @@ void init_hr_disk(void)
 #elif defined(solaris2)
     Add_HR_Disk_entry ( "/dev/rdsk/c%dt%dd0s%d", 0, 1, 0, 15, "/dev/rdsk/c%dt%dd0s0", 0, 7 );
     Add_HR_Disk_entry ( "/dev/rdsk/c%dd%ds%d",   0, 1, 0, 15, "/dev/rdsk/c%dd%ds0", 0, 7 );
+#elif defined(freebsd4)
+    Add_HR_Disk_entry ( "/dev/ad%ds%d%c", 0, 1, 1, 4, "/dev/ad%ds%d", 'a', 'h');
+    Add_HR_Disk_entry ( "/dev/da%ds%d%c", 0, 1, 1, 4, "/dev/da%ds%d", 'a', 'h');
 #elif defined(freebsd3)
     Add_HR_Disk_entry ( "/dev/wd%ds%d%c", 0, 1, 1, 4, "/dev/wd%ds%d", 'a', 'h');
     Add_HR_Disk_entry ( "/dev/sd%ds%d%c", 0, 1, 1, 4, "/dev/sd%ds%d", 'a', 'h');
