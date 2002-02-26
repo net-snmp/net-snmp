5b2e7b443be875ebb7a2284647bb18b3005c8cf1
John Naylon <jbpn@users.sourceforge.net>
2002-02-26 14:40:35 +0000

    - include net-snmp-config.h
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@6454 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/ucd-snmp/vmstat_solaris2.c
+++ net-snmp/agent/mibgroup/ucd-snmp/vmstat_solaris2.c
@@ -33,6 +33,7 @@
 #include <kstat.h>
 #include <sys/sysinfo.h>
 
+#include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 
