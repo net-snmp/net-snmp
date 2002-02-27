401236c53d673d62b154a10f2821b56d16a7438c
John Naylon <jbpn@users.sourceforge.net>
2002-02-27 10:08:29 +0000

    - don't use sprint_variable, no, I MEAN it this time
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@6465 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/snmp_agent.c
+++ net-snmp/agent/snmp_agent.c
@@ -1725,13 +1725,24 @@ handle_getnext_loop(struct agent_snmp_session  *asp) {
           break; */
         
         DEBUGIF("results") {
-            DEBUGMSGTL(("results","getnext results, before next pass: \n"));
-            for(var_ptr = asp->pdu->variables; var_ptr;
-                var_ptr = var_ptr->next_variable) {
-                char buf[SPRINT_MAX_LEN];
-                sprint_variable(buf, var_ptr->name, var_ptr->name_length, var_ptr);
-                DEBUGMSGTL(("results","  %s\n", buf));
+	    u_char *buf = NULL;
+	    size_t buf_len = 0, out_len = 0;
+            DEBUGMSGTL(("results", "getnext results, before next pass:\n"));
+            for (var_ptr = asp->pdu->variables; var_ptr;
+		 var_ptr = var_ptr->next_variable) {
+		out_len = 0;
+		if (sprint_realloc_variable(&buf, &buf_len, &out_len, 1,
+			      var_ptr->name, var_ptr->name_length, var_ptr)) {
+		    DEBUGMSGTL(("results", " %s\n", buf));
+		} else {
+		    if (buf != NULL) {
+			DEBUGMSGTL(("results", " %s [TRUNCATED]\n", buf));
+		    }
+		}
             }
+	    if (buf != NULL) {
+		free(buf);
+	    }
         }
 
         reassign_requests(asp);
