bcd322c0b2813b0a3d06d3a1fdc54b1fe2514b4c
Wes Hardaker <hardaker@users.sourceforge.net>
2002-03-05 18:44:12 +0000

    removed unused files
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@6533 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/agentx/master_request.h
+++
@@ -1,9 +0,0 @@
-#ifndef _AGENTX_MASTER_REQUEST_H
-#define _AGENTX_MASTER_REQUEST_H
-
-int agentx_add_inclusive(struct agent_snmp_session *asp,
-			 struct variable_list *vbp);
-int agentx_add_exclusive(struct agent_snmp_session *asp,
-			 struct variable_list *vbp);
-
-#endif /* _AGENTX_MASTER_REQUEST_H */
