1e7850d8f40ccac34776c85b51de8e8af3a4fc34
Dave Shield <dts12@users.sourceforge.net>
2002-04-18 09:52:29 +0000

    Bring integer and string output formats into line with the other types by printing a type token before the value (unless supressed by 'quick print') Thanks to Erik for the patch
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@6878 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/mib.c
+++ net-snmp/snmplib/mib.c
@@ -328,6 +328,13 @@ sprint_realloc_octet_string(u_char **buf, size_t *buf_len, size_t *out_len,
     }
   }
 
+  if (!ds_get_boolean(DS_LIBRARY_ID, DS_LIB_QUICK_PRINT)) {
+     if (!snmp_strcat(buf, buf_len, out_len, allow_realloc,
+         (const u_char*)"STRING: ")) {
+        return 0;
+     }
+  }
+
   if (hint) {
     int repeat, width = 1;
     long value;
@@ -896,6 +903,13 @@ sprint_realloc_integer(u_char **buf, size_t *buf_len, size_t *out_len,
       enum_string = enums->label;
       break;
     }
+  }
+
+  if (!ds_get_boolean(DS_LIBRARY_ID, DS_LIB_QUICK_PRINT)) {
+     if (!snmp_strcat(buf, buf_len, out_len, allow_realloc,
+         (const u_char*)"INTEGER: ")) {
+        return 0;
+     }
   }
 
   if (enum_string == NULL ||
