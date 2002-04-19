11a2d3a022dcedb4d751a1944f48e313fd1c817b
Wes Hardaker <hardaker@users.sourceforge.net>
2002-04-19 18:34:19 +0000

    Add some new snmpd.conf tokens for controlling perl better. Fix embedded perl support so it actually works.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@6894 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/snmp_perl.c
+++ net-snmp/agent/snmp_perl.c
@@ -8,7 +8,7 @@
 static PerlInterpreter *my_perl;
 
 void boot_DynaLoader (CV* cv);
- 
+
 void
 xs_init(void)
 {
@@ -18,10 +18,41 @@ xs_init(void)
     newXS(modulename, boot_DynaLoader, myfile);
 }
 
+void
+maybe_source_perl_startup(void) 
+{
+    const char *embedargs[] = { "", "" };
+    const char *perl_init_file = ds_get_string(DS_APPLICATION_ID,
+                                               DS_AGENT_PERL_INIT_FILE);
+    char init_file[SNMP_MAXBUF];
+
+    static int have_done_init = 0;
+
+    if (have_done_init)
+        return;
+    have_done_init = 1;
+    
+    if (!perl_init_file) {
+        snprintf(init_file, sizeof(init_file)-1,
+                 "%s/%s", SNMPSHAREPATH, "snmp_perl.pl");
+        perl_init_file = init_file;
+    }
+    embedargs[1] = perl_init_file;
+
+    DEBUGMSGTL(("perl", "initializing perl (%s)\n", embedargs[1]));
+    my_perl = perl_alloc();
+    perl_construct(my_perl);
+    perl_parse(my_perl, xs_init, 2, (char **) embedargs, NULL);
+    perl_run(my_perl);
+    DEBUGMSGTL(("perl", "done initializing perl\n"));
+}
 
 void
 do_something_perlish(char *something)
 {
+    if (ds_get_boolean(DS_APPLICATION_ID, DS_AGENT_DISABLE_PERL))
+        return;
+    maybe_source_perl_startup();
     DEBUGMSGTL(("perl", "calling perl\n"));
     eval_pv(something, TRUE);
     DEBUGMSGTL(("perl", "finished calling perl\n"));
@@ -35,23 +66,30 @@ void perl_config_handler(const char *token, char *line)
 void
 init_perl(void) 
 {
-    const char *embedargs[] = { "", "snmp_perl.pl" };
+    const char *appid = ds_get_string(DS_LIBRARY_ID, DS_LIB_APPTYPE);
+    const char *defaultid = "snmpd";
 
-    DEBUGMSGTL(("perl", "initializing perl\n"));
-    my_perl = perl_alloc();
-    perl_construct(my_perl);
-    perl_parse(my_perl, xs_init, 2, (char **) embedargs, NULL);
-    perl_run(my_perl);
-    DEBUGMSGTL(("perl", "done initializing perl\n"));
-
+    if (!appid)
+        appid = defaultid;
+    
     /* register config handlers */
     snmpd_register_config_handler("perl", perl_config_handler, NULL,
                                   "PERLCODE");
+
+    /* define the perlInitFile token to point to an init file */
+    ds_register_config(ASN_OCTET_STR, appid, "perlInitFile",
+                       DS_APPLICATION_ID, DS_AGENT_PERL_INIT_FILE);
+
+    /* define the perlInitFile token to point to an init file */
+    ds_register_config(ASN_BOOLEAN, appid, "disablePerl",
+                       DS_APPLICATION_ID, DS_AGENT_DISABLE_PERL);
 }
 
 void
 shutdown_perl(void) 
 {
+    if (ds_get_boolean(DS_APPLICATION_ID, DS_AGENT_DISABLE_PERL))
+        return;
     DEBUGMSGTL(("perl", "shutting down perl\n"));
     perl_destruct(my_perl);
     perl_free(my_perl);
--- net-snmp/agent/snmp_perl.pl
+++ net-snmp/agent/snmp_perl.pl
@@ -2,13 +2,7 @@
 ## SNMPD perl initialization file.
 ##
 
-use lib ("../perl/agent/blib/arch");
-use lib ("../perl/agent/blib/lib");
-use lib ("../perl/default_store/blib/arch");
-use lib ("../perl/default_store/blib/lib");
-
 use NetSNMP::agent;
 $agent = new NetSNMP::agent('dont_init_agent' => 1,
 			    'dont_init_lib' => 1);
 
-print "***in perl " . ref($agent) . "\n";
