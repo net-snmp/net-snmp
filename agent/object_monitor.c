922798b95dce15f4b58ca0cffe4db5944642543d
Robert Story <rstory@users.sourceforge.net>
2002-04-27 15:07:43 +0000

    ifdef gymnastics to make sure asserts are only compiled in only if explicity requested
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@6954 06827809-a52a-0410-b366-d66718629ded

--- agent/object_monitor.c
+++ agent/object_monitor.c
@@ -15,7 +15,15 @@
  * WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!
  */
 
+#if ! defined(NDEBUG) && ! defined(NETSNMP_USE_ASSERT)
+# define NETSNMP_TMP_NDEBUG
+# define NDEBUG
+#endif
 #include <assert.h>
+#if defined(NETSNMP_TMP_NDEBUG)
+# undef NDEBUG
+# undef NETSNMP_TMP_NDEBUG
+#endif
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
