cf72bb703ecc52c979ce688b61c99d55e7956888
Robert Story <rstory@users.sourceforge.net>
2002-04-27 15:08:22 +0000

    add missing prototype to fix compiler warning
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@6955 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/agentx/subagent.h
+++ net-snmp/agent/mibgroup/agentx/subagent.h
@@ -5,6 +5,7 @@ config_require(agentx/protocol)
 config_require(agentx/client)
 config_require(agentx/agentx_config)
 
+    void             init_subagent(void);
      int             subagent_pre_init(void);
      int             handle_agentx_packet(int, netsnmp_session *, int,
                                           netsnmp_pdu *, void *);
