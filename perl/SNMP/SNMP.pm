aa2882833c06ee78f7f5b216d134d6af8c7e93ef
Wes Hardaker <hardaker@users.sourceforge.net>
2002-05-07 00:11:31 +0000

    - (version.h sedscript.in): version tag ( 4.2.5 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-2-patches@7000 06827809-a52a-0410-b366-d66718629ded

--- perl/SNMP/SNMP.pm
+++ perl/SNMP/SNMP.pm
@@ -7,7 +7,7 @@
 #     modify it under the same terms as Perl itself.
 
 package SNMP;
-$VERSION = '4.2.4';   # current release version number
+$VERSION = '4.2.5';   # current release version number
 
 require Exporter;
 require DynaLoader;
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-static const char *VersionInfo="4.2.4";
+static const char *VersionInfo="4.2.5";
