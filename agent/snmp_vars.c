ba9a9e6127dd57bc25fa9c756295af5095fd2027
Wes Hardaker <hardaker@users.sourceforge.net>
2002-05-08 16:58:22 +0000

    fix bug mentioned in -users list where agentx_add_request isn't available.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-2-patches@7018 06827809-a52a-0410-b366-d66718629ded

--- agent/snmp_vars.c
+++ agent/snmp_vars.c
@@ -370,8 +370,11 @@ search_subtree_vars(struct subtree *tp,
                     /* check for permission to view this part of the OID tree */
 		    if ((access != NULL || (*write_method != NULL && exact)) &&
                         in_a_view(name, namelen, pdu, cvp->type)) {
-			if ( access && !exact &&
-                             (access!=agentx_add_request) ) {
+			if ( access && !exact
+#ifdef USING_AGENTX_MASTER_MODULE
+                             && (access!=agentx_add_request)
+#endif
+                            ) {
 				/*
 				 * We've got an answer, but shouldn't use it.
 				 * But we *might* be able to use a later
