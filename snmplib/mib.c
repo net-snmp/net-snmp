9ac04b5f705fc319a446e9d8ed9f0d88119f9502
Wes Hardaker <hardaker@users.sourceforge.net>
2002-05-09 22:30:47 +0000

    create a token for the -Ov equivelant
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7024 06827809-a52a-0410-b366-d66718629ded

--- snmplib/mib.c
+++ snmplib/mib.c
@@ -1819,6 +1819,8 @@ register_mib_handlers(void)
                        DS_LIBRARY_ID, DS_LIB_EXTENDED_INDEX);
     ds_register_premib(ASN_BOOLEAN, "snmp", "printHexText",
                        DS_LIBRARY_ID, DS_LIB_PRINT_HEX_TEXT);
+    ds_register_premib(ASN_BOOLEAN, "snmp", "printValueOnly",
+                       DS_LIBRARY_ID, DS_LIB_PRINT_BARE_VALUE);
 
 }
 
