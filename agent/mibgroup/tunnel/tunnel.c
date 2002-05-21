2f8faaa7f4ca86673e6d9f73cb3e7c7728379559
Robert Story <rstory@users.sourceforge.net>
2002-05-21 15:14:13 +0000

    apply patch [ 558129 ] Make the tunnel mib compile in 5.0.1, submitted by Lorenzo Colitti (lcolitti), which adds missing net-snmp-config.h
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7071 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/tunnel/tunnel.c
+++ agent/mibgroup/tunnel/tunnel.c
@@ -59,6 +59,7 @@
 #include <linux/if_tunnel.h>
 #include <linux/if_arp.h>
 
+#include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 
