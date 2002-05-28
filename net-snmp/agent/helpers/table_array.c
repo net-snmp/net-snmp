6c8f5c0afd78683e6fd10a78360706bd48a18304
Dave Shield <dts12@users.sourceforge.net>
2002-05-28 13:23:18 +0000

    Protect "#include <search.h>" with the HAVE_SEARCH_H token.  (Suggested by Harrie)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7086 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/helpers/table_array.c
+++ net-snmp/agent/helpers/table_array.c
@@ -21,7 +21,9 @@
 # undef NETSNMP_TMP_NDEBUG
 #endif
 
+#ifdef HAVE_SEARCH_H
 #include <search.h>
+#endif
 
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
