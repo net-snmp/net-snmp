69de44b1ee53c74c208623c9ca0ac06c18e71c1f
John Naylon <jbpn@users.sourceforge.net>
2002-08-20 12:54:05 +0000

    - add #define NO_KMEM_USAGE 1 since net-snmp-config.h.in no longer     pays attention to the DONT_USE_NLIST token.  Omission of this     causes the agent to exit with the message "init_kmem: kvm_open     failed: error 0".
        NOTE: this may need applying to other members of the Solaris 2
        family, but I don't have machines to hand to test this.
    
    
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@7503 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/system/solaris2.7.h
+++ include/net-snmp/system/solaris2.7.h
@@ -1,3 +1,6 @@
 #include "solaris.h"
 #define _SLASH_PROC_METHOD_ 1
 #define DONT_USE_NLIST 1
+/*  NO_KMEM_USAGE #defined by jbpn since net-snmp-config.h.in no
+    longer pays attention to the DONT_USE_NLIST token above.  */
+#define NO_KMEM_USAGE 1
