0daf423094f16cde26a78ac531437a559fef3034
Wes Hardaker <hardaker@users.sourceforge.net>
2002-09-11 23:24:50 +0000

    use snmp_log instead of printf for shutdown message.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7585 06827809-a52a-0410-b366-d66718629ded

--- apps/snmptrapd.c
+++ apps/snmptrapd.c
@@ -1442,10 +1442,10 @@ main(int argc, char *argv[])
         time_t          timer;
         time(&timer);
         tm = localtime(&timer);
-        printf
-            ("%.4d-%.2d-%.2d %.2d:%.2d:%.2d NET-SNMP version %s Stopped.\n",
-             tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour,
-             tm->tm_min, tm->tm_sec, netsnmp_get_version());
+        snmp_log(LOG_INFO,
+                 "%.4d-%.2d-%.2d %.2d:%.2d:%.2d NET-SNMP version %s Stopped.\n",
+                 tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour,
+                 tm->tm_min, tm->tm_sec, netsnmp_get_version());
     }
     if (Syslog) {
         snmp_log(LOG_INFO, "Stopping snmptrapd");
