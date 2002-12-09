aacf806b2eb0f3820eefdfcf9b529e1668969285
Dave Shield <dts12@users.sourceforge.net>
2002-12-09 11:10:49 +0000

    Remove some warnings, and try to cope with 'sysctl' failures. Patches supplied by Chris Johns.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7807 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII/interfaces.c
+++ agent/mibgroup/mibII/interfaces.c
@@ -306,7 +306,9 @@ typedef struct _conf_if_list {
 } conf_if_list;
 
 static conf_if_list *conf_list;
+#ifdef linux
 static struct ifnet *ifnetaddr_list;
+#endif
 
 static void
 parse_interface_config(const char *token, char *cptr)
@@ -569,15 +571,17 @@ Interface_Scan_Get_Count(void)
 {
     u_char         *cp;
     struct if_msghdr *ifp;
-    long            n;
+    long            n = 0;
 
     Interface_Scan_Init();
 
-    for (cp = if_list, n = 0; cp < if_list_end; cp += ifp->ifm_msglen) {
-        ifp = (struct if_msghdr *) cp;
-
-        if (ifp->ifm_type == RTM_IFINFO) {
-            ++n;
+    if (if_list_size) {
+        for (cp = if_list, n = 0; cp < if_list_end; cp += ifp->ifm_msglen) {
+            ifp = (struct if_msghdr *) cp;
+    
+            if (ifp->ifm_type == RTM_IFINFO) {
+                ++n;
+            }
         }
     }
     return n;
@@ -595,11 +599,13 @@ Interface_Scan_Init(void)
         if (if_list == 0 || if_list_size < size) {
             if (if_list != 0) {
                 free(if_list);
-                if_list = 0;
             }
-            if ((if_list = malloc(size)) == 0) {
+            if_list      = NULL;
+            if_list_size = 0;
+            if_list_end  = 0;
+            if ((if_list = malloc(size)) == NULL) {
                 snmp_log(LOG_ERR,
-                         "out of memory allocating route table\n");
+                         "out of memory allocating route table (size = %d)\n", size);
                 return;
             }
             if_list_size = size;
