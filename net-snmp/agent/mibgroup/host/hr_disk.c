0a6e74f05fc6272b336fa5ae3b22e9bd8c2421b1
Niels Baggesen <nba@users.sourceforge.net>
2002-12-11 21:52:24 +0000

    Patch for FreeBSD. Thanks to Jun Kuriyama
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7843 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/host/hr_disk.c
+++ net-snmp/agent/mibgroup/host/hr_disk.c
@@ -185,6 +185,9 @@ init_hr_disk(void)
                       "/dev/rdsk/c%dt%dd0s0", 0, 7);
     Add_HR_Disk_entry("/dev/rdsk/c%dd%ds%d", 0, 1, 0, 15,
                       "/dev/rdsk/c%dd%ds0", 0, 7);
+#elif defined(freebsd4) || defined(freebsd5)
+    Add_HR_Disk_entry("/dev/ad%ds%d%c", 0, 1, 1, 4, "/dev/ad%ds%d", 'a', 'h');
+    Add_HR_Disk_entry("/dev/da%ds%d%c", 0, 1, 1, 4, "/dev/da%ds%d", 'a', 'h');
 #elif defined(freebsd3)
     Add_HR_Disk_entry("/dev/wd%ds%d%c", 0, 1, 1, 4, "/dev/wd%ds%d", 'a',
                       'h');
