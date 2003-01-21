67b2949b52180742d570a4c3f16d8aafea05e359
Robert Story <rstory@users.sourceforge.net>
2003-01-21 03:11:52 +0000

    add string test case for binary array testing
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7975 06827809-a52a-0410-b366-d66718629ded

--- snmplib/test_binary_array.c
+++ snmplib/test_binary_array.c
@@ -25,15 +25,16 @@
 #include <net-snmp/library/tools.h>
 #include <net-snmp/library/snmp_assert.h>
 
+#define TEST_SIZE 7
+
 void
-print_obj(netsnmp_index *i, void *v)
+print_int(netsnmp_index *i, void *v)
 {
     printf("item %p = %ld\n", i, i->oids[0]);
 }
 
-#define TEST_SIZE 7
 int
-main(int argc, char**argv)
+test_int(void)
 {
     oid o1 = 1;
     oid o2 = 2;
@@ -68,18 +69,22 @@ main(int argc, char**argv)
     CONTAINER_INSERT(c,&i5);
 
     printf("For each...\n");
-    CONTAINER_FOR_EACH(c, print_obj, NULL);
+    CONTAINER_FOR_EACH(c, print_int, NULL);
     printf("Done.\n");
 
+    printf("\n");
     ip = CONTAINER_FIRST(c);
     printf("Find first = %p %ld\n",ip, ip->oids[0]);
     while( ip ) {
         ip = CONTAINER_NEXT(c,ip);
         if(ip)
             printf("Find next = %p %ld\n",ip, ip->oids[0]);
+        else
+            printf("Find next = %s\n",ip);
     }
 
     for( i=0; i < TEST_SIZE; ++i) {
+        printf("\n");
         ip = CONTAINER_FIND(c,a[i]);
         printf("Find %ld = %p %ld\n", a[i]->oids[0], ip, ip ? ip->oids[0] : 0);
         ip = CONTAINER_NEXT(c,a[i]);
@@ -89,4 +94,78 @@ main(int argc, char**argv)
     printf("Done.\n");
 
     return 0;
+}
+
+void
+print_string(char *i, void *v)
+{
+    printf("item %s\n", i);
+}
+
+int
+my_strcmp(char *lhs, char *rhs)
+{
+    int rc = strcmp(lhs,rhs);
+/*      printf("%s %d %s\n",lhs, rc, rhs); */
+    return rc;
+}
+
+int
+test_string()
+{
+    const char *o1 = "zebra";
+    const char *o2 = "b-two";
+    const char *o3 = "b";
+    const char *o4 = "cedar";
+    const char *o5 = "alpha";
+    const char *ox = "dev";
+    const char *oy = "aa";
+    const char * ip;
+    
+    const char *a[TEST_SIZE] = { o1, o2, o3, ox, o4, o5, oy };
+    netsnmp_container *c = netsnmp_container_get_binary_array();
+    int i;
+
+    c->compare = my_strcmp;
+    
+    printf("Creating container...\n");
+
+    printf("Inserting data...\n");
+    CONTAINER_INSERT(c,o4);
+    CONTAINER_INSERT(c,o2);
+    CONTAINER_INSERT(c,o3);
+    CONTAINER_INSERT(c,o1);
+    CONTAINER_INSERT(c,o5);
+    printf("\n");
+    printf("For each...\n");
+    CONTAINER_FOR_EACH(c, print_string, NULL);
+    printf("Done.\n");
+
+    printf("\n");
+    ip = CONTAINER_FIRST(c);
+    printf("Find first = %s\n",ip);
+    while( ip ) {
+        ip = CONTAINER_NEXT(c,ip);
+        printf("Find next = %s\n",ip);
+    }
+
+    for( i=0; i < TEST_SIZE; ++i) {
+        printf("\n");
+        ip = CONTAINER_FIND(c,a[i]);
+        printf("Find %s = %s\n", a[i], ip);
+        ip = CONTAINER_NEXT(c,a[i]);
+        printf("Next %s = %s\n", a[i], ip);
+    }
+
+    printf("Done.\n");
+
+    return 0;
+}
+
+int
+main(int argc, char** argv)
+{
+
+    test_int();
+    test_string();
 }
