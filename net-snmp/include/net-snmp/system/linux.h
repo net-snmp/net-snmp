7479ca5feef5d9736a6a3d3331a900fc00842762
Wes Hardaker <hardaker@users.sourceforge.net>
2003-02-07 05:17:42 +0000

    For linux, use /proc/X/status instead.  It has the name of processes when /proc/cmdline does not.  A define (commented out) was put in system/linux.h to revert to previous behavior if desired.  I'm not sure at which kernel release the status file was added.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@8009 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/system/linux.h
+++ net-snmp/include/net-snmp/system/linux.h
@@ -8,6 +8,9 @@
 
 #define ARP_SCAN_FOUR_ARGUMENTS
 
+/* uncomment this to read process names from /proc/X/cmdline (like <= 5.0) */
+/* #define USE_PROC_CMDLINE */
+
 /*
  * red hat >= 5.0 doesn't have this 
  */
