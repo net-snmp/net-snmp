19c88ad22ea1dda7516a4eb0c4d1c8a68ae8895a
Dave Shield <dts12@users.sourceforge.net>
2003-02-25 13:17:48 +0000

    List "fixed type" storage entries first, so that there isn't an arbitrary limit of 100 disk-based storage entries.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@8084 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/host/hr_storage.h
+++ agent/mibgroup/host/hr_storage.h
@@ -8,14 +8,10 @@
 extern void     init_hr_storage(void);
 extern FindVarMethod var_hrstore;
 
-#define	HRS_TYPE_FS_MAX		100     /* Maximum # of filesystems supported */
 
-#define	HRS_TYPE_MEM		101     /* incrementally from FS_MAX */
-#define	HRS_TYPE_SWAP		102
-#define	HRS_TYPE_MBUF		103
-                                        /*
-                                         * etc, etc, etc 
-                                         */
-#define	HRS_TYPE_MAX		104     /* one greater than largest type */
+#define	HRS_TYPE_MBUF		1
+#define	HRS_TYPE_MEM		2
+#define	HRS_TYPE_SWAP		3
+#define	HRS_TYPE_FIXED_MAX	3     /* the largest fixed type */
 
 #endif                          /* _MIBGROUP_HRSTORAGE_H */
