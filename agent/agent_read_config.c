d5144cb18c0a7654a0f0c356561de23882c5aec4
Dave Shield <dts12@users.sourceforge.net>
2003-05-06 15:09:24 +0000

    Don't strdup arg to ds_set_string (v5->v4 patch merge)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-2-patches@8331 06827809-a52a-0410-b366-d66718629ded

--- agent/agent_read_config.c
+++ agent/agent_read_config.c
@@ -174,7 +174,7 @@ void snmpd_set_agent_address(const char *token, char *cptr)
     buf[sizeof(buf)-1] = '\0';
 
     DEBUGMSGTL(("snmpd_ports","port spec: %s\n", buf));
-    ds_set_string(DS_APPLICATION_ID, DS_AGENT_PORTS, strdup(buf));
+    ds_set_string(DS_APPLICATION_ID, DS_AGENT_PORTS, buf);
 
 }
 
