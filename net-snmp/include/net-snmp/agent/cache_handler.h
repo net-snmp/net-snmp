1a418a9ebc156ebf9769a4d3f55cb9148d2fd79b
Dave Shield <dts12@users.sourceforge.net>
2003-06-04 12:56:49 +0000

    First pass at a data-caching helper - independent of individual MIB modules. (i.e. it's up to each MIB module to provide "load data" and "free data"  routines, but the handling of when to load these caches is done here).
    The intention is that this functionality will be SNMP-manageable (including
    configuration of validity timeouts, invalidation of cached values, etc).
    But for now, this is just the basic handler.
    
    
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@8417 06827809-a52a-0410-b366-d66718629ded

---
+++ net-snmp/include/net-snmp/agent/cache_handler.h
@@ -0,0 +1,54 @@
+#ifndef CACHE_H
+#define CACHE_H
+
+/*
+ * This caching helper provides a generalised (SNMP-manageable) caching
+ * mechanism.  Individual SNMP table and scalar/scalar group MIB
+ * implementations can use data caching in a consistent manner, without
+ * needing to handle the generic caching details themselves.
+ */
+
+#include <net-snmp/library/tools.h>
+
+#ifdef __cplusplus
+extern          "C" {
+#endif
+
+    typedef struct netsnmp_cache_s netsnmp_cache;
+
+    typedef int  (NetsnmpCacheLoad)(netsnmp_cache*);
+    typedef void (NetsnmpCacheFree)(void);
+
+    struct netsnmp_cache_s {
+        /*
+	 * For operation of the data caches
+	 */
+        int      status;	/* RowStatus-style status */
+        int      timeout;	/* Length of time the cache is valid (in s) */
+        marker_t timestamp;	/* When the cache was last loaded */
+
+        NetsnmpCacheLoad *load_cache;
+        NetsnmpCacheFree *free_cache;
+
+        /*
+	 * For SNMP-management of the data caches
+	 */
+	netsnmp_cache *next, *prev;
+        oid *rootoid;
+        int  rootoid_len;
+    };
+
+
+    netsnmp_mib_handler *netsnmp_get_cache_handler(int, NetsnmpCacheLoad *,
+                                                        NetsnmpCacheFree *,
+                                                        oid*, int);
+    int   netsnmp_register_cache_handler(netsnmp_handler_registration *reginfo,
+                                         int, NetsnmpCacheLoad *,
+                                              NetsnmpCacheFree *);
+
+    Netsnmp_Node_Handler netsnmp_cache_helper_handler;
+
+#ifdef __cplusplus
+};
+#endif
+#endif
