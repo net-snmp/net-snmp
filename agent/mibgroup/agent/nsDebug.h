e0ba54802adfac8519fe031dc58e233c99a5a6bb
Dave Shield <dts12@users.sourceforge.net>
2003-06-09 10:56:21 +0000

    Rework nsDebugTokenTable to be indexed by the debug token string values.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@8422 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/agent/nsDebug.h
+++ agent/mibgroup/agent/nsDebug.h
@@ -5,12 +5,19 @@
  * function declarations 
  */
 void            init_nsDebug(void);
+
+/*
+ * Handlers for the scalar objects
+ */
 Netsnmp_Node_Handler handle_nsDebugEnabled;
 Netsnmp_Node_Handler handle_nsDebugOutputAll;
 Netsnmp_Node_Handler handle_nsDebugDumpPdu;
 
-FindVarMethod        var_dbgtokens;
-WriteMethod          write_dbgPrefix;
-WriteMethod          write_dbgEnabled;
+/*
+ * Handler and iterators for the debug table
+ */
+Netsnmp_Node_Handler handle_nsDebugTable;
+Netsnmp_First_Data_Point  get_first_debug_entry;
+Netsnmp_Next_Data_Point   get_next_debug_entry;
 
 #endif /* NSDEBUG_H */
