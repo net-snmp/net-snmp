95370d6b4c4a8d8e66ebd2678ab64de6dca9b63a
Dave Shield <dts12@users.sourceforge.net>
2003-06-30 12:05:21 +0000

    Include recent helper modules in the helper library and meta-include file.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@8508 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/agent/all_helpers.h
+++ include/net-snmp/agent/all_helpers.h
@@ -6,9 +6,12 @@ extern          "C" {
 #endif
 
 #include <net-snmp/agent/instance.h>
+#include <net-snmp/agent/scalar.h>
+#include <net-snmp/agent/scalar_group.h>
 #include <net-snmp/agent/multiplexer.h>
 #include <net-snmp/agent/null.h>
 #include <net-snmp/agent/debug_handler.h>
+#include <net-snmp/agent/cache_handler.h>
 #include <net-snmp/agent/old_api.h>
 #include <net-snmp/agent/read_only.h>
 #include <net-snmp/agent/serialize.h>
