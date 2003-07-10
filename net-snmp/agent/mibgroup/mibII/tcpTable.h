c0377b773f692887f834b030d572052d57bfe7a4
Dave Shield <dts12@users.sourceforge.net>
2003-07-10 12:57:02 +0000

    New-API implementation of UDP and TCP tables, using cache and iterator helpers. Works on Linux and HP-UX 10, compiles (but untested) on Solaris & Windows.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@8565 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/mibII/tcpTable.h
+++ net-snmp/agent/mibgroup/mibII/tcpTable.h
@@ -1,42 +1,24 @@
 /*
- *  TCP MIB group interface - tcp.h
+ *  Template MIB group interface - tcp.h
  *
  */
 #ifndef _MIBGROUP_TCPTABLE_H
 #define _MIBGROUP_TCPTABLE_H
 
-#ifdef linux
-struct inpcb {
-    struct inpcb   *inp_next;   /* pointers to other pcb's */
-    struct in_addr  inp_faddr;  /* foreign host table entry */
-    u_short         inp_fport;  /* foreign port */
-    struct in_addr  inp_laddr;  /* local host table entry */
-    u_short         inp_lport;  /* local port */
-    int             inp_state;
-    int             uid;        /* owner of the connection */
-};
-#endif
-
-#ifdef hpux11
-#include <sys/mib.h>
-#endif
-
-#ifndef solaris2
-#if !defined(linux) && !defined(hpux11)
-extern int      TCP_Count_Connections(void);
-#endif
-extern void     TCP_Scan_Init(void);
-#ifdef hpux11
-extern int      TCP_Scan_Next(mib_tcpConnEnt *);
-#else
-struct inpcb;
-extern int      TCP_Scan_Next(int *, struct inpcb *);
-#endif
-#endif
-
 config_arch_require(solaris2, kernel_sunos5)
-config_require(mibII/tcp util_funcs)
+config_require(mibII/ip util_funcs)
 
-     extern FindVarMethod var_tcpEntry;
+extern void                init_tcpTable(void);
+extern Netsnmp_Node_Handler     tcpTable_handler;
+extern NetsnmpCacheLoad         tcpTable_load;
+extern NetsnmpCacheFree         tcpTable_free;
+extern Netsnmp_First_Data_Point tcpTable_first_entry;
+extern Netsnmp_Next_Data_Point  tcpTable_next_entry;
+
+#define TCPCONNSTATE	     1
+#define TCPCONNLOCALADDRESS  2
+#define TCPCONNLOCALPORT     3
+#define TCPCONNREMOTEADDRESS 4
+#define TCPCONNREMOTEPORT    5
 
 #endif                          /* _MIBGROUP_TCPTABLE_H */
