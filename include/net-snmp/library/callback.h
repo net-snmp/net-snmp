cefa30d2cfc1eedfb763aae1ead9e0b753b1bc43
Robert Story <rstory@users.sourceforge.net>
2003-07-13 18:19:23 +0000

    add priority to callback so user can determine callback order
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@8570 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/library/callback.h
+++ include/net-snmp/library/callback.h
@@ -34,6 +34,7 @@ extern          "C" {
     struct snmp_gen_callback {
         SNMPCallback   *sc_callback;
         void           *sc_client_arg;
+        int             priority;
         struct snmp_gen_callback *next;
     };
 
@@ -41,6 +42,10 @@ extern          "C" {
      * function prototypes 
      */
     void            init_callbacks(void);
+
+    int             netsnmp_register_callback(int major, int minor,
+                                              SNMPCallback * new_callback,
+                                              void *arg, int priority);
     int             snmp_register_callback(int major, int minor,
                                            SNMPCallback * new_callback,
                                            void *arg);
