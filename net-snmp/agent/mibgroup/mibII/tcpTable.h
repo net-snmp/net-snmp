9af20c5231bd93fffbb19c1b215eca24a5a0795c
Robert Story <rstory@users.sourceforge.net>
2003-07-13 16:44:43 +0000

    restore inpcb struct for linux (until it shows up somewhere else in cvs)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@8569 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/mibII/tcpTable.h
+++ net-snmp/agent/mibgroup/mibII/tcpTable.h
@@ -8,6 +8,18 @@
 config_arch_require(solaris2, kernel_sunos5)
 config_require(mibII/ip util_funcs)
 
+#ifdef linux
+struct inpcb {
+    struct inpcb   *inp_next;   /* pointers to other pcb's */
+    struct in_addr  inp_faddr;  /* foreign host table entry */
+    u_short         inp_fport;  /* foreign port */
+    struct in_addr  inp_laddr;  /* local host table entry */
+    u_short         inp_lport;  /* local port */
+    int             inp_state;
+    int             uid;        /* owner of the connection */
+};
+#endif
+
 extern void                init_tcpTable(void);
 extern Netsnmp_Node_Handler     tcpTable_handler;
 extern NetsnmpCacheLoad         tcpTable_load;
