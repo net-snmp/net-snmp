93434896f1b8f4adc93debe723fa81facaad6b21
John Naylon <jbpn@users.sourceforge.net>
2003-07-22 16:07:18 +0000

    - zero out sensitive command-line arguments (community string, user name,     authentication passphrase, privacy passphrase).
        NOTE: this does not work for all plaforms -- certainly it doesn't
              work for Solaris.  A different, more gross method is needed
              for these platforms (see sendmail code for details --  need
              I say more?).
    
    
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@8592 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/library/snmp_parse_args.h
+++ include/net-snmp/library/snmp_parse_args.h
@@ -1,15 +1,18 @@
 #ifndef SNMP_PARSE_ARGS_H
 #define SNMP_PARSE_ARGS_H
 #ifdef __cplusplus
-extern          "C" {
+extern "C" {
 #endif
 
-    int             snmp_parse_args(int, char *const *, netsnmp_session *,
-                                    const char *, void (*)(int,
-                                                           char *const *,
-                                                           int));
-    void            snmp_parse_args_descriptions(FILE *);
-    void            snmp_parse_args_usage(FILE *);
+int
+snmp_parse_args(int argc, char **argv, netsnmp_session *session,
+		const char *localOpts, void (*proc)(int, char *const *, int));
+
+void
+snmp_parse_args_descriptions(FILE *);
+
+void
+snmp_parse_args_usage(FILE *);
 
 #ifdef __cplusplus
 }
