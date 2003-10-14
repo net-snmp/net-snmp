46ebd2aee568a082f25ae66e26d82669a79fa2ba
Robert Story <rstory@users.sourceforge.net>
2003-10-14 16:49:23 +0000

    don't require DEBUG dfine for asserts; new logic: use assert if NETSNMP_USE_ASSERT defines. otherwise, failed asserts will use snmp_log to log a message (unless SNMP_NO_DEBUGGING is defined).
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@8892 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/library/snmp_assert.h
+++ net-snmp/include/net-snmp/library/snmp_assert.h
@@ -12,36 +12,38 @@
  * MACROs don't need extern "C"
  */
 
-#ifdef DEBUG
-
+/*
+ * always use assert if requested
+ */
 #ifdef NETSNMP_USE_ASSERT
 /*   void netsnmp_assert( int );*/
 #   define netsnmp_assert(x)  assert( x )
 #else
-#   ifdef  HAVE_CPP_UNDERBAR_FUNCTION_DEFINED
-#      define netsnmp_assert(x)  do { \
+/*
+ *  if asserts weren't requested, just log, unless SNMP_NO_DEBUGGING specified
+ */
+#   ifndef SNMP_NO_DEBUGGING
+#      ifdef  HAVE_CPP_UNDERBAR_FUNCTION_DEFINED
+#         define netsnmp_assert(x)  do { \
                  if ( x ) \
                     ; \
                  else \
                     snmp_log(LOG_ERR,"netsnmp_assert %s failed %s:%d %s()\n", \
                              __STRING(x),__FILE__,__LINE__,__FUNCTION__); \
               }while(0)
-#   else
-#      define netsnmp_assert(x)  do { \
+#      else
+#         define netsnmp_assert(x)  do { \
                  if( x )\
                     ; \
                  else \
                     snmp_log(LOG_ERR,"netsnmp_assert %s failed %s:%d\n", \
                              __STRING(x),__FILE__,__LINE__); \
               }while(0)
-#   endif
-#endif
-
-#else
-
-#define netsnmp_assert(x)
-
-#endif
+#      endif
+#   else /* NO DEBUGGING */
+#      define netsnmp_assert(x) void
+#   endif /* NO DEBUGGING */
+#endif /* not NETSNMP_USE_ASSERT */
 
 
 #endif /* SNMP_ASSERT_H */
