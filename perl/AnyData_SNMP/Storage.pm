a973d703747a5703041862b08af788418bf0190b
Wes Hardaker <hardaker@users.sourceforge.net>
2003-10-17 23:16:03 +0000

    - (perl/AnyData_SNMP/Storage.pm): version tag ( 5.1.pre1 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@8946 06827809-a52a-0410-b366-d66718629ded

--- perl/AnyData_SNMP/Storage.pm
+++ perl/AnyData_SNMP/Storage.pm
@@ -6,7 +6,7 @@ package AnyData::Storage::SNMP;
 ##   scalar sets?
 ##   multi-hosts
 
-$AnyData::Storage::VERSION = '0.01';
+$AnyData::Storage::VERSION = '5.1.pre1';
 use strict;
 
 use vars qw(@basecols);
