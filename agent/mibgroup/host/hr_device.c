074c58001de8e8c327dcda0d5bf2ee1e8f754945
Dave Shield <dts12@users.sourceforge.net>
2003-11-24 12:00:18 +0000

    Handle GETNEXT requests on the hrDeviceTable with a "too large" index. Patch #811717 by Bob Rowlands, fixing bug #807315
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9163 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/host/hr_device.c
+++ agent/mibgroup/host/hr_device.c
@@ -2,6 +2,16 @@
  *  Host Resources MIB - Device group implementation - hr_device.c
  *
  */
+/* Portions of this file are subject to the following copyright(s).  See
+ * the Net-SNMP's COPYING file for more details and other copyrights
+ * that may apply:
+ */
+/*
+ * Portions of this file are copyrighted by:
+ * Copyright © 2003 Sun Microsystems, Inc. All rights reserved.
+ * Use is subject to license terms specified in the COPYING file
+ * distributed with the Net-SNMP package.
+ */
 
 #include <net-snmp/net-snmp-config.h>
 #if HAVE_STRING_H
@@ -296,7 +306,9 @@ Init_Device(void)
            init_device[current_type] == NULL)
         if (++current_type >= HRDEV_TYPE_MAX)
             return;
-    (*init_device[current_type]) ();
+    /* Check current_type, if >= MAX first time into loop, would fail below */
+    if (current_type < HRDEV_TYPE_MAX)
+        (*init_device[current_type]) ();
 }
 
 int
