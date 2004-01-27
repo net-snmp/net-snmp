743358376f743ff5f3f75df2088de1e8f43b315d
Michael Slifcak <slif@users.sourceforge.net>
2004-01-27 17:24:27 +0000

    Refer to the netsnmp library version string via netsnmp_get_version; Keep direct string reference iff UCD_COMPATIBLE is defined.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9292 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/snmp_version.c
+++ net-snmp/snmplib/snmp_version.c
@@ -1,5 +1,9 @@
 #include <net-snmp/version.h>
 
+/* expose ONLY for benefit of grandfathered code */
+#ifndef UCD_COMPATIBLE
+static
+#endif
 const char     *NetSnmpVersionInfo = "5.1";
 
 const char     *
