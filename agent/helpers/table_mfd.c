4c098efc5d4c73bd2a26666e709bbd9bb76dd8f1
Michael Slifcak <slif@users.sourceforge.net>
2004-01-28 05:49:13 +0000

    provide function hints in lieu of __FUNCTION__
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@9306 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/table_mfd.c
+++ agent/helpers/table_mfd.c
@@ -89,7 +89,7 @@ netsnmp_mfd_register_table(netsnmp_mfd_registration *mfdr, const char *name,
     netsnmp_handler_registration *reginfo;
     u_long mfd_modes = 0;
         
-    DEBUGMSGT(("mfd",">%s\n",__FUNCTION__));
+    DEBUGMSGT(("mfd",">%s\n","register_table"));
 
     if (!mfdr) {
         snmp_log(LOG_ERR, "table_mfd registration with no callbacks\n" );
@@ -395,7 +395,7 @@ _mfd_data_lookup(netsnmp_mib_handler *handler,
     netsnmp_table_request_info *tblreq_info;
     netsnmp_index index;
     
-    DEBUGMSGT(("mfd",">%s\n",__FUNCTION__));
+    DEBUGMSGT(("mfd",">%s\n","data_lookup"));
 
     /** mull test in previous function */
     netsnmp_assert(handler && handler->myvoid);
@@ -426,7 +426,7 @@ _mfd_data_find(netsnmp_mib_handler *handler,
     netsnmp_mfd_registration *mfdr;
     netsnmp_table_request_info *tblreq_info;
     
-    DEBUGMSGT(("mfd",">%s\n",__FUNCTION__));
+    DEBUGMSGT(("mfd",">%s\n","data_find"));
 
     /** mull test in previous function */
     netsnmp_assert(handler && handler->myvoid);
@@ -454,7 +454,7 @@ _mfd_group(netsnmp_handler_registration *reginfo, netsnmp_index *row,
     netsnmp_table_request_info *tblreq_info;
     netsnmp_request_group *rgroup;
     
-    DEBUGMSGT(("mfd",">%s\n",__FUNCTION__));
+    DEBUGMSGT(("mfd",">%s\n","_mfd_group"));
 
     if(NULL == row)
         return NULL;
