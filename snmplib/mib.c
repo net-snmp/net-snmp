a29edbcbf13cad34e1122e6a55373989aef2b91f
Niels Baggesen <nba@users.sourceforge.net>
2004-02-03 10:09:39 +0000

    Fix bug returning only four characters for uptime string
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9409 06827809-a52a-0410-b366-d66718629ded

--- snmplib/mib.c
+++ snmplib/mib.c
@@ -5556,7 +5556,7 @@ uptime_string(u_long timeticks, char *buf)
     if (cp)
         *cp = '\0';
 #endif
-    strlcpy(buf, tbuf, sizeof(buf));
+    strlcpy(buf, tbuf, sizeof(tbuf));
     return buf;
 }
 
