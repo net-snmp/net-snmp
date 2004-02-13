fec906e506ced71e9d6433352b06b01e02023e7f
Wes Hardaker <hardaker@users.sourceforge.net>
2004-02-13 00:53:28 +0000

    patch [ 811278 ] make agentx udp/tcp transports configurable from Bob Rowlands
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-0-patches@9545 06827809-a52a-0410-b366-d66718629ded

--- acconfig.h
+++ acconfig.h
@@ -1,3 +1,14 @@
+/* Portions of this file are subject to the following copyright(s).  See
+ * the Net-SNMP's COPYING file for more details and other copyrights
+ * that may apply:
+ */
+/*
+ * Portions of this file are copyrighted by:
+ * Copyright © 2003 Sun Microsystems, Inc. All rights reserved.
+ * Use is subject to license terms specified in the COPYING file
+ * distributed with the Net-SNMP package.
+ */
+
 #ifndef NET_SNMP_CONFIG_H
 #define NET_SNMP_CONFIG_H
 
@@ -613,6 +624,9 @@
 #endif
 
 #undef INET6
+
+/* define if agentx transport is to use domain sockets only */
+#undef AGENTX_DOM_SOCK_ONLY
 
 #ifndef NETSNMP_INLINE
 #   define NETSNMP_NO_INLINE
