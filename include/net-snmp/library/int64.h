6d6e712a1342a78dc7c814f4b0894606a179bfd3
Wes Hardaker <hardaker@users.sourceforge.net>
2004-02-13 18:05:30 +0000

    [ 849158 ] 5.1: /const/ fixes for the API from Stephen J. Friedl
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9551 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/library/int64.h
+++ include/net-snmp/library/int64.h
@@ -14,11 +14,11 @@ extern          "C" {
     void            incrByU16(U64 *, unsigned int);
     void            incrByU32(U64 *, unsigned int);
     void            zeroU64(U64 *);
-    int             isZeroU64(U64 *);
-    void            printU64(char *, U64 *);
-    void            printI64(char *, U64 *);
+    int             isZeroU64(const U64 *);
+    void            printU64(char *, const U64 *);
+    void            printI64(char *, const U64 *);
     int             read64(U64 *, const char *);
-    void            u64Subtract(U64 * pu64one, U64 * pu64two,
+    void            u64Subtract(const U64 * pu64one, const U64 * pu64two,
                                 U64 * pu64out);
 
 #ifdef __cplusplus
