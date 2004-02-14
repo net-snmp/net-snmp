b342c1148fb6ff564049047a42f5ee5837d56d10
Michael Slifcak <slif@users.sourceforge.net>
2004-02-14 00:02:42 +0000

    better explanatory messages, and improved test results collected.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@9570 06827809-a52a-0410-b366-d66718629ded

--- win32/nmakeperl.bat
+++ win32/nmakeperl.bat
@@ -18,11 +18,13 @@ perl Makefile.PL CAPI=TRUE -NET-SNMP-PATH=%INSTALL_BASE%
 REM Or, if the libraries have been built, look back in the build directory.
 REM perl Makefile.PL CAPI=TRUE -NET-SNMP-IN-SOURCE=TRUE
 
-echo Make the Perl SNMP modules.  Errors should not be allowed.
+echo Make the Perl SNMP modules.
 nmake /nologo > nmake.out
+echo If errors are seen stop here and review nmake.out.
 pause
 
-echo Test the Perl SNMP modules. Errors can be forgiven.
-nmake /nologo test > nmaketest.out
+echo Test the Perl SNMP modules.
+nmake /nologo test > nmaketest.out 2>&1
+echo If no errors are seen, review test results in nmaketest.out.
 
 cd ..
