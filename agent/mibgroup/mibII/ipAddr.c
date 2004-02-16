8a4ee284f156c667c29260cc9e81138ef30907fc
Wes Hardaker <hardaker@users.sourceforge.net>
2004-02-16 14:40:24 +0000

    Fix a missing variable from the previous patch.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-1-patches@9579 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII/ipAddr.c
+++ agent/mibgroup/mibII/ipAddr.c
@@ -627,6 +627,7 @@ var_ipAddrEntry(struct variable * vp,
     static mib2_ipAddrEntry_t Lowentry;
     int             Found = 0;
     req_e           req_type;
+    unsigned int    ipaddr_return;
 
     /*
      * fill in object part of name for current (less sizeof instance part) 
