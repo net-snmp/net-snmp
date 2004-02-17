8744508afd2b83bf099ad55b76c7e8e9172afd88
Michael Slifcak <slif@users.sourceforge.net>
2004-02-17 13:55:10 +0000

    fix error after change for Fix [801928]
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-0-patches@9603 06827809-a52a-0410-b366-d66718629ded

--- snmplib/mib.c
+++ snmplib/mib.c
@@ -5237,9 +5237,11 @@ uptime_string(u_long timeticks, char *buf)
 {
     uptimeString(timeticks, buf);
 #ifdef CMU_COMPATIBLE
+    {
     char *cp = strrchr(buf, '.');
     if (cp)
         *cp = '\0';
+    }
 #endif
     return buf;
 }
