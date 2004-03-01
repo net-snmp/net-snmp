499c6065b45de5f3cf960a90880f1119eaa04cd9
Robert Story <rstory@users.sourceforge.net>
2004-03-01 05:12:44 +0000

    add table_container; remove table_mfd
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@9742 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/agent/all_helpers.h
+++ include/net-snmp/agent/all_helpers.h
@@ -26,7 +26,7 @@ extern          "C" {
 #include <net-snmp/agent/table_data.h>
 #include <net-snmp/agent/table_dataset.h>
 #include <net-snmp/agent/table_iterator.h>
-#include <net-snmp/agent/table_mfd.h>
+#include <net-snmp/agent/table_container.h>
 /*
  * #include <net-snmp/agent/table_array.h> 
  */
