542df1f215a4450c5b56fd25b0001c2a0b394ee8
Robert Story <rstory@users.sourceforge.net>
2004-03-02 05:00:33 +0000

    new baby_steps access multiplexer handler; make actual helper routines static; rename functions, defines for backwards compatability
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@9755 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/baby_steps.c
+++ agent/helpers/baby_steps.c
@@ -37,6 +37,17 @@ static u_short set_mode_map[SNMP_MSG_INTERNAL_SET_MAX][BABY_STEPS_PER_MODE_MAX] 
     { MODE_BSTEP_UNDO_COMMIT, MODE_BSTEP_UNDO_SET, MODE_BSTEP_UNDO_CLEANUP,
       MODE_BSTEP_POST_REQUEST}
 };
+
+static int
+_baby_steps_helper(netsnmp_mib_handler *handler,
+                   netsnmp_handler_registration *reginfo,
+                   netsnmp_agent_request_info *reqinfo,
+                   netsnmp_request_info *requests);
+static int
+_baby_steps_access_multiplexer(netsnmp_mib_handler *handler,
+                               netsnmp_handler_registration *reginfo,
+                               netsnmp_agent_request_info *reqinfo,
+                               netsnmp_request_info *requests);
     
 /** @defgroup baby_steps baby_steps: calls your handler in baby_steps for set processing.
  *  @ingroup handler
@@ -47,22 +58,27 @@ static u_short set_mode_map[SNMP_MSG_INTERNAL_SET_MAX][BABY_STEPS_PER_MODE_MAX] 
  *  handler chain.
  */
 netsnmp_mib_handler *
-netsnmp_get_baby_steps_handler(u_long modes)
+netsnmp_baby_steps_handler_get(u_long modes)
 {
     netsnmp_mib_handler *mh;
 
-    mh = netsnmp_create_handler("baby_steps", netsnmp_baby_steps_helper);
+    mh = netsnmp_create_handler("baby_steps", _baby_steps_helper);
     if(!mh)
         return NULL;
 
     mh->myvoid = (void*)modes;
+
+    /*
+     * don't set MIB_HANDLER_AUTO_NEXT, since we need to call lower
+     * handlers with a munged mode.
+     */
     
     return mh;
 }
 
 /** @internal Implements the baby_steps handler */
-int
-netsnmp_baby_steps_helper(netsnmp_mib_handler *handler,
+static int
+_baby_steps_helper(netsnmp_mib_handler *handler,
                          netsnmp_handler_registration *reginfo,
                          netsnmp_agent_request_info *reqinfo,
                          netsnmp_request_info *requests)
@@ -188,6 +204,7 @@ netsnmp_baby_steps_helper(netsnmp_mib_handler *handler,
      * restore original mode
      */
     reqinfo->mode = save_mode;
+
     
     return rc;
 }
@@ -197,8 +214,155 @@ netsnmp_baby_steps_helper(netsnmp_mib_handler *handler,
  *  use.
  */
 void
-netsnmp_init_baby_steps_helper(void)
+netsnmp_baby_steps_handler_init(void)
 {
     netsnmp_register_handler_by_name("baby_steps",
-                                     netsnmp_get_baby_steps_handler(BABY_STEP_ALL));
+                                     netsnmp_baby_steps_handler_get(BABY_STEP_ALL));
+}
+
+/** @} */
+
+/** @defgroup baby_steps baby_steps_access_multiplexer: calls individual access methods based on baby_step mode.
+ *  @ingroup handler
+ *  @{
+ */
+
+/** returns a baby_steps handler that can be injected into a given
+ *  handler chain.
+ */
+netsnmp_mib_handler *
+netsnmp_baby_steps_access_multiplexer_get(netsnmp_baby_steps_access_methods *am)
+{
+    netsnmp_mib_handler *mh;
+
+    mh = netsnmp_create_handler("baby_steps_access_multiplexer",
+                                _baby_steps_access_multiplexer);
+    if(!mh)
+        return NULL;
+
+    mh->myvoid = am;
+    mh->flags |= MIB_HANDLER_AUTO_NEXT;
+    
+    return mh;
+}
+
+/** @internal Implements the baby_steps handler */
+static int
+_baby_steps_access_multiplexer(netsnmp_mib_handler *handler,
+                               netsnmp_handler_registration *reginfo,
+                               netsnmp_agent_request_info *reqinfo,
+                               netsnmp_request_info *requests)
+{
+    void *temp_void;
+    Netsnmp_Node_Handler *method = NULL;
+    netsnmp_baby_steps_access_methods *access_methods;
+    int rc = SNMP_ERR_NOERROR;
+
+    /** call handlers should enforce these */
+    netsnmp_assert((handler!=NULL) && (reginfo!=NULL) && (reqinfo!=NULL) &&
+                   (requests!=NULL));
+
+    DEBUGMSGT(("helper:baby_steps_access_multiplexer",
+               "baby_steps_access_multiplexer; mode %d\n", reqinfo->mode));
+
+    access_methods = (netsnmp_baby_steps_access_methods *)handler->myvoid;
+    if(!access_methods) {
+        snmp_log(LOG_ERR,"baby_steps_access_multiplexer has no methods\n");
+        return SNMPERR_GENERR;
+    }
+
+    switch(reqinfo->mode) {
+        
+    case MODE_BSTEP_PRE_REQUEST:
+        if( access_methods->pre_request )
+            method = access_methods->pre_request;
+        break;
+        
+    case MODE_BSTEP_OBJECT_LOOKUP:
+        if( access_methods->object_lookup )
+            method = access_methods->object_lookup;
+        break;
+
+    case SNMP_MSG_GET:
+    case SNMP_MSG_GETNEXT:
+        if( access_methods->get_values )
+            method = access_methods->get_values;
+        break;
+        
+    case MODE_BSTEP_CHECK_VALUE:
+        if( access_methods->object_syntax_checks )
+            method = access_methods->object_syntax_checks;
+        break;
+
+    case MODE_BSTEP_ROW_CREATE:
+        if( access_methods->row_creation )
+            method = access_methods->row_creation;
+        break;
+
+    case MODE_BSTEP_UNDO_SETUP:
+        if( access_methods->undo_setup )
+            method = access_methods->undo_setup;
+        break;
+
+    case MODE_BSTEP_SET_VALUE:
+        if( access_methods->set_values )
+            method = access_methods->set_values;
+        break;
+
+    case MODE_BSTEP_CHECK_CONSISTENCY:
+        if( access_methods->consistency_checks )
+            method = access_methods->consistency_checks;
+        break;
+
+    case MODE_BSTEP_UNDO_SET:
+        if( access_methods->undo_sets )
+            method = access_methods->undo_sets;
+        break;
+
+    case MODE_BSTEP_COMMIT:
+        if( access_methods->commit )
+            method = access_methods->commit;
+        break;
+
+    case MODE_BSTEP_UNDO_COMMIT:
+        if( access_methods->undo_commit )
+            method = access_methods->undo_commit;
+        break;
+
+    case MODE_BSTEP_IRREVERSIBLE_COMMIT:
+        if( access_methods->irreversible_commit )
+            method = access_methods->irreversible_commit;
+        break;
+
+    case MODE_BSTEP_UNDO_CLEANUP:
+        if( access_methods->undo_cleanup )
+            method = access_methods->undo_cleanup;
+        break;
+        
+    case MODE_BSTEP_POST_REQUEST:
+        if( access_methods->post_request )
+            method = access_methods->post_request;
+        break;
+
+    default:
+        snmp_log(LOG_ERR,"unknown mode %d\n", reqinfo->mode);
+        return SNMP_ERR_GENERR;
+    }
+
+    /*
+     * if method exists, set up handler void and call method.
+     */
+    if(NULL != method) {
+        temp_void = handler->myvoid;
+        handler->myvoid = access_methods->my_access_void;
+        rc = (*method)(handler, reginfo, reqinfo, requests);
+        handler->myvoid = temp_void;
+    }
+
+    /*
+     * don't call any lower handlers, it will be done for us 
+     * since we set MIB_HANDLER_AUTO_NEXT
+     */
+
+    return rc;
 }
