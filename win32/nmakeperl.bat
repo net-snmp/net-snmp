d3d5c4ac2908638ea8d87ac7431fc4b0f4728bc4
Michael Slifcak <slif@users.sourceforge.net>
2004-03-03 15:47:02 +0000

    per AlexB - add "-help" descriptions. create BASE/temp subdir.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@9764 06827809-a52a-0410-b366-d66718629ded

--- win32/nmakeperl.bat
+++ win32/nmakeperl.bat
@@ -3,12 +3,38 @@ REM
 REM build Net-SNMP Perl module using nmake
 REM
 
-echo Remember to run this script from the base of the source directory.
-
 REM INSTALL_BASE must point to the directory ABOVE the library files.
 REM Generally follows what is the install-net-snmp.bat setting.
 
 set INSTALL_BASE="c:\Program Files\Net-SNMP"
+
+if "%1" == "-?" goto help
+if "%1" == "/?" goto help
+if "%1" == "-h" goto help
+if "%1" == "/h" goto help
+if "%1" == "-help" goto help
+if "%1" == "/help" goto help
+goto start
+
+:help
+echo .
+echo This script will compile the Net-SNMP Perl modules.  Net-SNMP must 
+echo already be installed.
+echo .
+echo The current install base is %INSTALL_BASE%.  
+echo This must match the directory that Net-SNMP has been installed in.
+echo .
+echo To change the installation directory, modify the INSTALL_BASE variable
+echo inside this script.
+echo .
+echo Run this script from the base of the source directory, NOT the win32 
+echo directory.
+echo .
+goto end
+
+:start
+
+echo Remember to run this script from the base of the source directory.
 
 cd perl
 
@@ -28,3 +54,6 @@ nmake /nologo test > nmaketest.out 2>&1
 echo If no errors are seen, review test results in perl\nmaketest.out.
 
 cd ..
+
+:end
+
