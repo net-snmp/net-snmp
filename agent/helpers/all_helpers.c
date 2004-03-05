12a83db446b4890e4181eef5a7af2d1344088bab
Wes Hardaker <hardaker@users.sourceforge.net>
2004-03-05 06:28:10 +0000

    initialize the table_dataset helper (again...  I *know* it was initialized in the past.  What happened?)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-0-patches@9786 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/all_helpers.c
+++ agent/helpers/all_helpers.c
@@ -34,6 +34,7 @@ netsnmp_init_helpers(void)
     netsnmp_init_serialize();
     netsnmp_init_read_only_helper();
     netsnmp_init_bulk_to_next_helper();
+    netsnmp_init_table_dataset();
 }
 
 /** @} */
