d10b1af83fa41d92b0a10645326e04d37f7c6d27
Michael Slifcak <slif@users.sourceforge.net>
2004-03-12 21:07:26 +0000

    doc change for patch 912447
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9861 06827809-a52a-0410-b366-d66718629ded

--- win32/build.bat
+++ win32/build.bat
@@ -2,6 +2,8 @@
 
 REM play with SNMPCONFPATH, to pass the perl conf.t test
 REM this play is harmless otherwise
+REM See the Win32 section in perl/SNMP/README for details on
+REM why this is needed. 
 
 set saveXXSNMPCONFPATH=%SNMPCONFPATH%
 set SNMPCONFPATH=t;%SNMPCONFPATH%
