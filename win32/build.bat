54a02a91c5fc97792d7cd7c6b409fe93b5c76278
Michael Slifcak <slif@users.sourceforge.net>
2004-03-16 15:49:59 +0000

    synch with working version, delta install base, of course.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@9904 06827809-a52a-0410-b366-d66718629ded

--- win32/build.bat
+++ win32/build.bat
@@ -1,17 +1,5 @@
 @echo off
 
-REM play with SNMPCONFPATH, to pass the perl conf.t test
-REM this play is harmless otherwise
-REM See the Win32 section in perl/SNMP/README for details on
-REM why this is needed. 
-
-set saveXXSNMPCONFPATH=%SNMPCONFPATH%
-set SNMPCONFPATH=t;%SNMPCONFPATH%
-
 cd win32 > NUL: 2>&1
-
 perl build.pl
-
-set SNMPCONFPATH=saveXXSNMPCONFPATH
-set saveXXSNMPCONFPATH=
 
