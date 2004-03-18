6fda6964ff3dc64a347632476f696e7110d68dd5
Michael Slifcak <slif@users.sourceforge.net>
2004-03-18 15:06:13 +0000

    patch [917649] find uint32_t in win32-gcc build environments
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9907 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/system/cygwin.h
+++ include/net-snmp/system/cygwin.h
@@ -1,5 +1,9 @@
 #include "generic.h"
 
+#ifdef HAVE_STDINT_H
+#include <stdint.h>	/* uint32_t */
+#endif
+
 #undef HAVE_WINSOCK_H
 #undef bsdlike
 #undef MBSTAT_SYMBOL
--- include/net-snmp/system/mingw32.h
+++ include/net-snmp/system/mingw32.h
@@ -1,5 +1,9 @@
 #include "generic.h"
 
+#ifdef HAVE_STDINT_H
+#include <stdint.h>	/* uint32_t */
+#endif
+
 #undef bsdlike
 #undef MBSTAT_SYMBOL
 #undef TOTAL_MEMORY_SYMBOL
