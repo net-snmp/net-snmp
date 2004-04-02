133ebc917ca48cf04dea33b27c6684f9c4a1cd45
Robert Story <rstory@users.sourceforge.net>
2004-04-02 02:00:42 +0000

    move config_require stuff back into mibgroup, since that's the only place configure will look for them
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@9984 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/data_access/route.h
+++ include/net-snmp/data_access/route.h
@@ -17,46 +17,6 @@
 #endif
 NETSNMP_CPP_WRAP_START  /* no semi-colon */
 
-
-/**---------------------------------------------------------------------*/
-/*
- * configure required files
- *
- * Notes:
- *
- * 1) prefer functionality over platform, where possible. If a method
- *    is available for multiple platforms, test that first. That way
- *    when a new platform is ported, it won't need a new test here.
- *
- * 2) don't do detail requirements here. If, for example,
- *    HPUX11 had different reuirements than other HPUX, that should
- *    be handled in the *_hpux.h header file.
- */
-config_require(ip-forward-mib/data_access/route_common)
-#if defined( linux )
-config_require(ip-forward-mib/data_access/route_linux);
-#elif defined ( solaris )
-config_require(ip-forward-mib/data_access/route_solaris);
-#elif defined ( hpux )
-config_require(ip-forward-mib/data_access/route_hpux);
-#elif defined ( bsd )
-config_require(ip-forward-mib/data_access/route_bsd);
-#else
-/*
- * couldn't determine the correct file!
- * require a bogus file to generate an error.
- */
-configure_require(ip-forward-mib/data_access/route_unknown-arch);
-#endif
-
-/** need interface for ifIndex */
-config_require(if-mib/data_access/interface);
-
-
-/*
- * define flags to indicate the availability of certain data
- */
-
 /**---------------------------------------------------------------------*/
 /*
  * structure definitions
