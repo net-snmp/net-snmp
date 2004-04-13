d4cd4e6e26358611f9d20263db75b21df8cad704
Robert Story <rstory@users.sourceforge.net>
2004-04-13 19:32:48 +0000

    new files
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@10080 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/mibgroup/ip-mib/data_access/ipaddress_ioctl.h
@@ -0,0 +1,7 @@
+/*
+ * internal header, not for distribution
+ */
+
+int
+_netsnmp_access_ipaddress_container_ioctl_load_v4(netsnmp_container *container,
+                                                  int idx_offset);
