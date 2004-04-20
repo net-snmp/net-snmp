53da72a7a39b7fb489b5f3c0db427d7e6b2c6029
Michael Slifcak <slif@users.sourceforge.net>
2004-04-20 04:52:43 +0000

    conditionally compile for platforms with static object lists [no configure].
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@10139 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmpUDPIPv6Domain.c
+++ snmplib/snmpUDPIPv6Domain.c
@@ -1,4 +1,5 @@
 #include <net-snmp/net-snmp-config.h>
+#ifdef SNMP_TRANSPORT_UDPIPV6_DOMAIN
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -1312,3 +1313,6 @@ netsnmp_udp6_ctor(void)
 
     netsnmp_tdomain_register(&udp6Domain);
 }
+
+#endif /* SNMP_TRANSPORT_UDPIPV6_DOMAIN */
+
