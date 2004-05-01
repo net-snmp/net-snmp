4e02e5884b1dfe06392de8a242c7a722b54631d1
Wes Hardaker <hardaker@users.sourceforge.net>
2004-05-01 00:00:36 +0000

    Many misc changes and improvements.  - Now takes command line arguments for changing MIBDIRs and output dirs.  - accepts a nodemap to map MIBs to other nodes.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@10226 06827809-a52a-0410-b366-d66718629ded

--- mibs/makehtml.pl
+++ mibs/makehtml.pl
@@ -2,23 +2,55 @@
 
 use SNMP;
 
+use Getopt::Std;
+
+%opts = ( M => ".",
+	  D => "html");
+
+getopts("M:D:WH:", \%opts) || die "usage: makehtml.pl [-M MIBDIR] [-D OUTDIR]";
+
 $SNMP::save_descriptions = 1;
 
-$ENV{'MIBDIRS'} = '.';
+$ENV{'MIBDIRS'} = $opts{'M'};
 $ENV{'SNMPCONFPATH'} = 'bogus';
 
 if (-f "rfclist") {
     open(I,"rfclist");
     while (<I>) {
-	$mibs{$2} = $1 if (/^(\d+)\s+([-\w]+)\s*$/);
+	if (/^(\d+)\s+([-:\w]+)\s*$/) {
+	    my $mib = $2;
+	    my $rfc = $1;
+	    my @mibs = split(/:/,$mib);
+	    foreach my $i (@mibs) {
+		$mibs{$i} = $rfc; 
+	    }
+	}
     }
     close(I);
 }
 
-print "<head><title>Net-SNMP Distributed MIBs</title></head>\n";
-print "<body bgcolor=\"#ffffff\">\n";
-print "<h1>Net-SNMP Distributed MIBs</h1>\n";
-print "<p>The following are the MIB files distributed with Net-SNMP.  Note that because they are distributed with Net-SNMP does not mean the agent implements them all.  Another good place for finding other MIB definitions can be found <a href=\"http://www.mibdepot.com/\">at the MIB depot</a>.</p>\n";
+if (-f "nodemap") {
+    open(I,"nodemap");
+    while (<I>) {
+	if (/^([-\w]+)\s+(\w+)\s*$/) {
+	    $nodemap{$1} = $2;
+	}
+    }
+    close(I);
+}
+
+if ($opts{'H'}) {
+    open(I,"$opts{'H'}");
+    while (<I>) {
+	print;
+    }
+    close(I);
+} else {
+    print "<head><title>Net-SNMP Distributed MIBs</title></head>\n";
+    print "<body bgcolor=\"#ffffff\">\n";
+    print "<h1>Net-SNMP Distributed MIBs</h1>\n";
+    print "<p>The following are the MIB files distributed with Net-SNMP.  Note that because they are distributed with Net-SNMP does not mean the agent implements them all.  Another good place for finding other MIB definitions can be found <a href=\"http://www.mibdepot.com/\">at the MIB depot</a>.</p>\n";
+}
 print "<table border=2 bgcolor=\"#dddddd\">\n";
 print "<tr><th>MIB</th><th>RFC</th><th>Description</th>\n";
 
@@ -27,7 +59,7 @@ foreach my $mibf (@ARGV) {
     my $mib = $mibf;
     $mib =~ s/.txt//;
 
-    open(I, $mibf);
+    open(I, "$opts{M}/$mibf");
     while (<I>) {
 	if (/(\w+)\s+MODULE-IDENTITY/) {
 	    $node = $1;
@@ -44,6 +76,11 @@ foreach my $mibf (@ARGV) {
 
     $desc = $SNMP::MIB{$node}{'description'};
 
+    # get a different tree than the module identity though.
+    if (exists($nodemap{$mib})) {
+	$node = $nodemap{$mib};
+    }
+
     $desc =~ s/\t/        /g;
     my ($s) = ($desc =~ /\n(\s+)/);
     $desc =~ s/^$s//gm;
@@ -54,22 +91,14 @@ foreach my $mibf (@ARGV) {
     print "<td>&nbsp</td>\n" if (!$mibs{$mib});
     print "<td><pre>$desc</pre></td></tr>\n";
 
-    system("(cd html && env MIBDIRS=.. MIBS=$mib mib2c -c ../../local/mib2c.genhtml.conf $node)");
-    open(O,">>html/$node.html");
-    print O "<!--#include virtual=\"/sfbutton.html\" -->\n";
-    close(O);
+    system("MIBS=$mib mib2c -c mib2c.genhtml.conf $node");
+    system("mv $node.html $opts{D}");
+    if ($opts{'W'}) {
+	open(O,">>$opts{D}/$node.html");
+	print O "<!--#include virtual=\"/sfbutton.html\" -->\n";
+	close(O);
+    }
 }
 
 print "</table>";
-print "<!--#include virtual=\"/sfbutton.html\" -->\n";
-
-# 	@for i in $(MIBS) ; do \
-# 		echo $$i ; \
-# 		if grep MODULE-IDENTITY $$i > /dev/null ; then \
-# 			m=`echo $$i | sed 's/.txt//'` ; \
-# 			a=`egrep '[a-zA-Z]+ +MODULE-IDENTITY' $$i | awk '{print $$1}'` ; \
-# 			(cd html && env MIBDIRS=.. MIBS=$$m mib2c -c $(top_srcdir)/../local/mib2c.genhtml.conf $$a ) ; \
-# 			perl makehtml.pl $$m $$a >> html/index.html ; \
-# 		fi \
-# 	done
-# 	echo "</table>" >> html/index.html
+print "<!--#include virtual=\"/sfbutton.html\" -->\n" if ($opts{'W'});
