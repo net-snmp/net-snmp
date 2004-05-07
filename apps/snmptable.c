998301069797dbd7870c876047fecadc48866d18
Niels Baggesen <nba@users.sourceforge.net>
2004-05-07 21:16:55 +0000

    Fix to allow specifying numeric oid for table
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-0-patches@10245 06827809-a52a-0410-b366-d66718629ded

--- apps/snmptable.c
+++ apps/snmptable.c
@@ -114,7 +114,7 @@ static int      use_getbulk = 1;
 static int      max_getbulk = 25;
 
 void            usage(void);
-void            get_field_names(char *);
+void            get_field_names(void);
 void            get_table_entries(netsnmp_session * ss);
 void            getbulk_table_entries(netsnmp_session * ss);
 void            print_table(void);
@@ -217,7 +217,6 @@ int
 main(int argc, char *argv[])
 {
     netsnmp_session session, *ss;
-    char           *tblname;
 
     setvbuf(stdout, NULL, _IOLBF, 1024);
     netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, 
@@ -255,15 +254,8 @@ main(int argc, char *argv[])
     }
     localdebug = netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID, 
                                         NETSNMP_DS_LIB_DUMP_PACKET);
-    tblname = strrchr(argv[optind], '.');
-    if (!tblname)
-        tblname = strrchr(argv[optind], ':');
-    if (tblname)
-        ++tblname;
-    else
-        tblname = argv[optind];
 
-    get_field_names(tblname);
+    get_field_names();
     reverse_fields();
 
     /*
@@ -366,14 +358,14 @@ print_table(void)
 }
 
 void
-get_field_names(char *tblname)
+get_field_names(void)
 {
     u_char         *buf = NULL, *name_p = NULL;
     size_t          buf_len = 0, out_len = 0;
     struct tree    *tbl = NULL;
     int             going = 1;
 
-    tbl = find_tree_node(tblname, -1);
+    tbl = get_tree(root, rootlen, get_tree_head());
     if (tbl) {
         tbl = tbl->child_list;
         if (tbl) {
