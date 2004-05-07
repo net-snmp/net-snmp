1bf49afc6ac10e34595020186f8ba0ec72171ffd
Michael Slifcak <slif@users.sourceforge.net>
2004-05-07 03:15:17 +0000

    Per Alex Burger, add names to acknowledge the individual contributors.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-1-patches@10243 06827809-a52a-0410-b366-d66718629ded

--- perl/SNMP/SNMP.pm
+++ perl/SNMP/SNMP.pm
@@ -2085,7 +2085,13 @@ feedback.
  Wayne Marquette
  Scott Schumate
  Michael Slifcak
+ Srivathsan Srinivasagopalan
+ Bill Fenner
+ Jef Peeraer
+ Daniel Hagerty
+ Karl "Rat" Schilke and Electric Lightwave, Inc.
  Perl5 Porters
+ Alex Burger
 
 Apologies to any/all who's patch/feature/request was not mentioned or
 included - most likely it was lost when paying work intruded on my
