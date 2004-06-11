2518c5ebde039f35dc3eeadcbff90ba0597b4e2c
Robert Story <rstory@users.sourceforge.net>
2004-06-11 03:47:00 +0000

    - new ioctl interface access
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@10414 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/mibgroup/if-mib/data_access/interface_ioctl.h
@@ -0,0 +1,28 @@
+/*
+ * ioctl interface data access header
+ *
+ * $Id$
+ */
+#ifndef NETSNMP_ACCESS_INTERFACE_IOCTL_H
+#define NETSNMP_ACCESS_INTERFACE_IOCTL_H
+
+#ifdef __cplusplus
+extern          "C" {
+#endif
+
+/**---------------------------------------------------------------------*/
+/**/
+
+int netsnmp_access_interface_ioctl_physaddr_get(int fd,
+                                                netsnmp_interface_entry *ifentry);
+
+int netsnmp_access_interface_ioctl_flags_get(int fd,
+                                             netsnmp_interface_entry *ifentry);
+
+/**---------------------------------------------------------------------*/
+
+# ifdef __cplusplus
+};
+#endif
+
+#endif /* NETSNMP_ACCESS_INTERFACE_IOCTL_H */
