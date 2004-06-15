ec3e849a7b0f01bfc93232777211f093a043de9d
Dave Shield <dts12@users.sourceforge.net>
2004-06-15 12:55:19 +0000

    Define the example notification as a notification (using an RFC2576-safe OID), rather than a MIB object, and define example compulsory and optional payload objects. Tweak the example code to use these new definitions.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@10435 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/examples/notification.c
+++ net-snmp/agent/mibgroup/examples/notification.c
@@ -77,10 +77,10 @@ send_example_notification(unsigned int clientreg, void *clientarg)
 {
     /*
      * define the OID for the notification we're going to send
-     * NET-SNMP-EXAMPLES-MIB::netSnmpExampleNotification 
+     * NET-SNMP-EXAMPLES-MIB::netSnmpExampleHeartbeatNotification 
      */
     oid             notification_oid[] =
-        { 1, 3, 6, 1, 4, 1, 8072, 2, 3, 1 };
+        { 1, 3, 6, 1, 4, 1, 8072, 2, 3, 0, 1 };
     size_t          notification_oid_len = OID_LENGTH(notification_oid);
 
     /*
@@ -91,9 +91,26 @@ send_example_notification(unsigned int clientreg, void *clientarg)
     size_t          objid_snmptrap_len = OID_LENGTH(objid_snmptrap);
 
     /*
+     * define the OIDs for the varbinds we're going to include
+     *  with the notification -
+     * NET-SNMP-EXAMPLES-MIB::netSnmpExampleHeartbeatRate  and
+     * NET-SNMP-EXAMPLES-MIB::netSnmpExampleHeartbeatName 
+     */
+    oid      hbeat_rate_oid[]   = { 1, 3, 6, 1, 4, 1, 8072, 2, 3, 2, 1, 0 };
+    size_t   hbeat_rate_oid_len = OID_LENGTH(hbeat_rate_oid);
+    oid      hbeat_name_oid[]   = { 1, 3, 6, 1, 4, 1, 8072, 2, 3, 2, 2, 0 };
+    size_t   hbeat_name_oid_len = OID_LENGTH(hbeat_name_oid);
+
+    /*
      * here is where we store the variables to be sent in the trap 
      */
     netsnmp_variable_list *notification_vars = NULL;
+    char *heartbeat_name = "A girl named Maria";
+#ifdef  RANDOM_HEARTBEAT
+    int  heartbeat_rate = rand() % 60;
+#else
+    int  heartbeat_rate = 30;
+#endif
 
     DEBUGMSGTL(("example_notification", "defining the trap\n"));
 
@@ -119,8 +136,24 @@ send_example_notification(unsigned int clientreg, void *clientarg)
                               notification_oid_len * sizeof(oid));
 
     /*
-     * if we wanted to insert additional objects, we'd do it here 
+     * add in the additional objects defined as part of the trap
      */
+
+    snmp_varlist_add_variable(&notification_vars,
+                               hbeat_rate_oid, hbeat_rate_oid_len,
+                               ASN_INTEGER,
+                              (u_char *)&heartbeat_rate,
+                                  sizeof(heartbeat_rate));
+
+    /*
+     * if we want to insert additional objects, we do it here 
+     */
+    if (heartbeat_rate < 30 ) {
+        snmp_varlist_add_variable(&notification_vars,
+                               hbeat_name_oid, hbeat_name_oid_len,
+                               ASN_OCTET_STR,
+                               heartbeat_name, strlen(heartbeat_name));
+    }
 
     /*
      * send the trap out.  This will send it to all registered
