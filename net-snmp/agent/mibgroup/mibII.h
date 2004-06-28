822f23016dab6d813fbe7c0bb963000174059caa
Robert Story <rstory@users.sourceforge.net>
2004-06-28 23:55:38 +0000

    - new configure flage: --enable-mfd-rewrites will turn on rewritten modules.   - currently has the side effect of disabling deprecated tables
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@10571 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/mibII.h
+++ net-snmp/agent/mibgroup/mibII.h
@@ -4,12 +4,18 @@
 
 config_require(mibII/system_mib)
 config_require(mibII/sysORTable)
+#if defined(NETSNMP_ENABLE_MFD_REWRITES)
+config_require(ip-mib)
+config_require(if-mib)
+config_require(ip-forward-mib)
+#else
 config_require(mibII/at)
 config_require(mibII/interfaces)
+config_require(mibII/ip)
+#endif
 config_require(mibII/snmp_mib)
 config_require(mibII/tcp)
 config_require(mibII/icmp)
-config_require(mibII/ip)
 config_require(mibII/udp)
 config_require(mibII/vacm_vars)
 config_require(mibII/setSerialNo)
