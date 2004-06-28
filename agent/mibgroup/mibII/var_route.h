d7bc289497a40b3c390adf70c8b07582fac57706
Robert Story <rstory@users.sourceforge.net>
2004-06-28 23:55:38 +0000

    - new configure flage: --enable-mfd-rewrites will turn on rewritten modules.   - currently has the side effect of disabling deprecated tables
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@10571 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII/var_route.h
+++ agent/mibgroup/mibII/var_route.h
@@ -5,7 +5,10 @@
 #ifndef _MIBGROUP_VAR_ROUTE_H
 #define _MIBGROUP_VAR_ROUTE_H
 
-config_require(mibII/ip util_funcs)
+#if !defined(NETSNMP_ENABLE_MFD_REWRITES)
+config_require(mibII/ip)
+#endif
+config_require(util_funcs)
     config_arch_require(solaris2, kernel_sunos5)
 
      void            init_var_route(void);
