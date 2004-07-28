712eb8c5a3ed6d6fe34539cfe0e80f762545bb5c
Wes Hardaker <hardaker@users.sourceforge.net>
2004-07-28 22:30:49 +0000

    - (snmplib/snmp_version.c): version tag ( 5.1.2.rc2 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-1-patches@10782 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_version.c
+++ snmplib/snmp_version.c
@@ -4,7 +4,7 @@
 #ifndef UCD_COMPATIBLE
 static
 #endif
-const char     *NetSnmpVersionInfo = "5.1.2.rc1";
+const char     *NetSnmpVersionInfo = "5.1.2.rc2";
 
 const char     *
 netsnmp_get_version()
