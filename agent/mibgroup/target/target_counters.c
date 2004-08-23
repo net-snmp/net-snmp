2648cfd0bfd87cfbf58907e0fda817241d0c5fff
Dave Shield <dts12@users.sourceforge.net>
2004-08-23 08:59:32 +0000

    Implement context error counters as proper scalar objects (rather than simply instances)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@10893 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/target/target_counters.c
+++ agent/mibgroup/target/target_counters.c
@@ -12,9 +12,8 @@
 #include "target_counters.h"
 #include <net-snmp/agent/instance.h>
 
-static oid      unavailable_context_oid[] =
-    { 1, 3, 6, 1, 6, 3, 12, 1, 4, 0 };
-static oid      unknown_context_oid[] = { 1, 3, 6, 1, 6, 3, 12, 1, 5, 0 };
+static oid   unavailable_context_oid[] = { 1, 3, 6, 1, 6, 3, 12, 1, 4 };
+static oid   unknown_context_oid[]     = { 1, 3, 6, 1, 6, 3, 12, 1, 5 };
 
 void
 init_target_counters(void)
@@ -25,23 +24,23 @@ init_target_counters(void)
      * unknown contexts
      */
 
-    netsnmp_register_read_only_instance(netsnmp_create_handler_registration
-                                        ("myInstance",
+    netsnmp_register_scalar(
+        netsnmp_create_handler_registration("snmpUnknownContexts",
                                          get_unknown_context_count,
                                          unknown_context_oid,
-                                         sizeof(unknown_context_oid) /
-                                         sizeof(oid), HANDLER_CAN_RONLY));
+                                         OID_LENGTH(unknown_context_oid),
+                                         HANDLER_CAN_RONLY));
 
     /*
-     * unavailable available
+     * unavailable contexts
      */
 
-    netsnmp_register_read_only_instance(netsnmp_create_handler_registration
-                                        ("myInstance",
+    netsnmp_register_scalar(
+        netsnmp_create_handler_registration("snmpUnavailableContexts",
                                          get_unavailable_context_count,
                                          unavailable_context_oid,
-                                         sizeof(unavailable_context_oid) /
-                                         sizeof(oid), HANDLER_CAN_RONLY));
+                                         OID_LENGTH(unavailable_context_oid),
+                                         HANDLER_CAN_RONLY));
 
 }
 
