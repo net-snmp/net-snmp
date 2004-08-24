72d269ffae89299eb244b8eb19b930f9fa268cae
Dave Shield <dts12@users.sourceforge.net>
2004-08-24 15:15:03 +0000

    Preliminary implementation of the DisMan Remote Operation MIB groups. Note that these are not yet ready for production use.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@10899 06827809-a52a-0410-b366-d66718629ded

---
+++ net-snmp/agent/mibgroup/disman.h
@@ -0,0 +1,8 @@
+/*
+ * Wrapper for the full DisMan implementation
+ */
+config_require(disman/event-mib)
+config_require(disman/expression-mib)
+config_require(disman/nslookup-mib)
+config_require(disman/ping-mib)
+config_require(disman/traceroute-mib)
