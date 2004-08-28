22708b5d707cab1ab0cd43526dc08bd9400e2b22
Robert Story <rstory@users.sourceforge.net>
2004-08-28 06:27:08 +0000

    - fix 1/2 of bug 702435: memory leak [...] for agentx subagents   - Patch supplied by Franz G. Fischer
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@10927 06827809-a52a-0410-b366-d66718629ded

--- agent/snmp_agent.c
+++ agent/snmp_agent.c
@@ -233,6 +233,7 @@ typedef struct agent_set_cache_s {
 
     int             vbcount;
     netsnmp_request_info *requests;
+    netsnmp_variable_list *saved_vars;
     netsnmp_data_list *agent_data;
 
     /*
@@ -258,8 +259,8 @@ save_set_cache(netsnmp_agent_session *asp)
     /*
      * Save the important information 
      */
-    DEBUGMSGTL(("verbose:asp", "asp %p reqinfo %p saved in cache\n",
-                asp, asp->reqinfo));
+    DEBUGMSGTL(("verbose:asp", "asp %p reqinfo %p saved in cache (mode %d)\n",
+                asp, asp->reqinfo, asp->pdu->command));
     ptr->transID = asp->pdu->transid;
     ptr->sess = asp->session;
     ptr->treecache = asp->treecache;
@@ -267,6 +268,7 @@ save_set_cache(netsnmp_agent_session *asp)
     ptr->treecache_num = asp->treecache_num;
     ptr->agent_data = asp->reqinfo->agent_data;
     ptr->requests = asp->requests;
+    ptr->saved_vars = asp->pdu->variables; /* requests contains pointers to variables */
     ptr->vbcount = asp->vbcount;
 
     /*
@@ -274,17 +276,7 @@ save_set_cache(netsnmp_agent_session *asp)
      */
     asp->treecache = NULL;
     asp->reqinfo->agent_data = NULL;
-    //
-    // xxx-rks: THINK ABOUT THIS BEFORE RELEASING 5.2
-    //
-    // this NULL was checked in version 1.190 to fix a double
-    // free. However, that introduced a memory leak. So I'm
-    // taking it out to find the source of the double free.
-    // If it hasn't been found by release time, we may want to
-    // consider restoring it, as a leak is better than a crash!
-    //
-    //asp->pdu->variables = NULL;
-    //
+    asp->pdu->variables = NULL;
     asp->requests = NULL;
 
     ptr->next = Sets;
@@ -314,8 +306,43 @@ get_set_cache(netsnmp_agent_session *asp)
             asp->treecache = ptr->treecache;
             asp->treecache_len = ptr->treecache_len;
             asp->treecache_num = ptr->treecache_num;
+
+            /*
+             * Free previously allocated requests before overwriting by
+             * cached ones, otherwise memory leaks!
+             */
+	    if (asp->requests) {
+                /*
+                 * I don't think this case should ever happen. Please email
+                 * the net-snmp-coders@lists.sourceforge.net if you have
+                 * a test case that hits this assert. -- rstory
+                 */
+		int i;
+                netsnmp_assert(NULL == asp->requests); /* see note above */
+		for (i = 0; i < asp->vbcount; i++) {
+		    netsnmp_free_request_data_sets(&asp->requests[i]);
+		}
+		free(asp->requests);
+	    }
+	    /*
+	     * If we replace asp->requests with the info from the set cache,
+	     * we should replace asp->pdu->variables also with the cached
+	     * info, as asp->requests contains pointers to them.  And we
+	     * should also free the current asp->pdu->variables list...
+	     */
+	    if (ptr->saved_vars) {
+		if (asp->pdu->variables)
+		    snmp_free_varbind(asp->pdu->variables);
+		asp->pdu->variables = ptr->saved_vars;
+                asp->vbcount = ptr->vbcount;
+	    } else {
+                /*
+                 * when would we not have saved variables? someone
+                 * let me know if they hit this assert. -- rstory
+                 */
+                netsnmp_assert(NULL != ptr->saved_vars);
+            }
             asp->requests = ptr->requests;
-            asp->vbcount = ptr->vbcount;
 
             netsnmp_assert(NULL != asp->reqinfo);
             asp->reqinfo->asp = asp;
@@ -1199,8 +1226,6 @@ free_agent_snmp_session(netsnmp_agent_session *asp)
         for (i = 0; i < asp->vbcount; i++) {
             netsnmp_free_request_data_sets(&asp->requests[i]);
         }
-    }
-    if (asp->requests) {
         SNMP_FREE(asp->requests);
     }
     if (asp->cache_store) {
