506f565a8689e1f101f6537cfddd5de388b3e868
Robert Story <rstory@users.sourceforge.net>
2004-09-02 00:06:46 +0000

    ifdef out arch specific functions if arch not supported
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@10980 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/if-mib/data_access/interface.h
+++ agent/mibgroup/if-mib/data_access/interface.h
@@ -24,23 +24,8 @@ config_require(if-mib/data_access/interface_common)
 #if defined( linux )
 config_require(if-mib/data_access/interface_linux);
 config_require(if-mib/data_access/interface_ioctl);
-/*
- * these haven't been written yet... move them out of
- * this comment block once they are.
- *
-#elif defined ( solaris )
-config_require(if-mib/data_access/interface_solaris);
-#elif defined ( hpux )
-config_require(if-mib/data_access/interface_hpux);
-#elif defined ( bsd )
-config_require(if-mib/data_access/interface_bsd);
-*/
 #else
-/*
- * couldn't determine the correct file!
- * require a bogus file to generate an error.
- */
-config_require(if-mib/data_access/interface-unknown-arch);
+#   define NETSNMP_ACCESS_INTERFACE_NOARCH 1
 #endif
 
 /*
