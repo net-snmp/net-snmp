dbb5dd9e52ea430ce19e265e49b629b9bac4f676
Wes Hardaker <hardaker@users.sourceforge.net>
2004-09-15 16:55:23 +0000

    Make the usm module handle its own reprots
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@11086 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmpusm.c
+++ snmplib/snmpusm.c
@@ -2657,6 +2657,52 @@ usm_process_in_msg(int msgProcModel,    /* (UNUSED) */
 }                               /* end usm_process_in_msg() */
 
 void
+usm_handle_report(void *sessp,
+                  netsnmp_transport *transport, netsnmp_session *session,
+                  int result, netsnmp_pdu *pdu)
+{
+    /*
+     * handle reportable errors 
+     */
+    switch (result) {
+    case SNMPERR_USM_AUTHENTICATIONFAILURE:
+    {
+        int res = session->s_snmp_errno;
+        session->s_snmp_errno = result;
+        if (session->callback) {
+            session->callback(NETSNMP_CALLBACK_OP_RECEIVED_MESSAGE,
+                              session, pdu->reqid, pdu,
+                              session->callback_magic);
+        }
+        session->s_snmp_errno = res;
+    }  
+    case SNMPERR_USM_UNKNOWNENGINEID:
+    case SNMPERR_USM_UNKNOWNSECURITYNAME:
+    case SNMPERR_USM_UNSUPPORTEDSECURITYLEVEL:
+    case SNMPERR_USM_NOTINTIMEWINDOW:
+    case SNMPERR_USM_DECRYPTIONERROR:
+
+        if (SNMP_CMD_CONFIRMED(pdu->command) ||
+            (pdu->command == 0
+             && (pdu->flags & SNMP_MSG_FLAG_RPRT_BIT))) {
+            netsnmp_pdu    *pdu2;
+            int             flags = pdu->flags;
+
+            pdu->flags |= UCD_MSG_FLAG_FORCE_PDU_COPY;
+            pdu2 = snmp_clone_pdu(pdu);
+            pdu->flags = pdu2->flags = flags;
+            snmpv3_make_report(pdu2, result);
+            if (0 == snmp_sess_send(sessp, pdu2)) {
+                snmp_free_pdu(pdu2);
+                /*
+                 * TODO: indicate error 
+                 */
+            }
+        }
+        break;
+    }       
+}
+void
 init_usm(void)
 {
     struct snmp_secmod_def *def;
@@ -2677,6 +2723,7 @@ init_usm(void)
     def->encode_forward = usm_secmod_generate_out_msg;
     def->decode = usm_secmod_process_in_msg;
     def->pdu_free_state_ref = usm_free_usmStateReference;
+    def->handle_report = usm_handle_report;
     register_sec_mod(USM_SEC_MODEL_NUMBER, "usm", def);
 
     snmp_register_callback(SNMP_CALLBACK_LIBRARY,
