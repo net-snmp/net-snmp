c014e0035a3b4d4fbc9f4d695eb4cb66624d2b3b
Robert Story <rstory@users.sourceforge.net>
2004-10-26 16:39:56 +0000

    backport fix for inet address display
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-1-patches@11476 06827809-a52a-0410-b366-d66718629ded

--- snmplib/mib.c
+++ snmplib/mib.c
@@ -167,6 +167,13 @@ PrefixList      mib_prefixes[] = {
     {NULL, 0}                   /* end of list */
 };
 
+enum inet_address_type {
+    IPV4 = 1,
+    IPV6 = 2,
+    IPV4Z = 3,
+    IPV6Z = 4,
+    DNS = 16
+};
 
 /**
  * @internal
@@ -3736,6 +3743,82 @@ parse_one_oid_index(oid ** oidStart, size_t * oidLen,
     return SNMPERR_SUCCESS;
 }
 
+/*
+ * dump_realloc_oid_to_inetaddress:
+ *   return 0 for failure,
+ *   return 1 for success,
+ *   return 2 for not handled
+ */
+
+int
+dump_realloc_oid_to_inetaddress(const int addr_type, const oid * objid, size_t objidlen,
+                                u_char ** buf, size_t * buf_len,
+                                size_t * out_len, int allow_realloc,
+                                char quotechar)
+{
+    if (buf) {
+        int             i, len;
+        char            intbuf[64], * p;
+        int             zone;
+
+        memset(intbuf, 0, 64);
+
+        p = intbuf;
+        *p = quotechar;
+        p++;
+        switch (addr_type) {
+            case IPV4:
+            case IPV4Z:
+                if ((addr_type == IPV4  && objidlen != 4) ||
+                    (addr_type == IPV4Z && objidlen != 8))
+                    return 2;
+
+                len = sprintf(p, "%lu.%lu.%lu.%lu", objid[0], objid[1], objid[2], objid[3]);
+                p += len;
+                if (addr_type == IPV4Z) {
+                    zone = ntohl(*((int *) &(objid[4])));
+                    len = sprintf(p, "%%%lu", zone);
+                    p += len;
+                }
+
+                break;
+
+            case IPV6:
+            case IPV6Z:
+                if ((addr_type == IPV6 && objidlen != 16) ||
+                    (addr_type == IPV6Z && objidlen != 20))
+                    return 2;
+
+                len = 0;
+                for (i = 0; i < 16; i ++) {
+                    len = snprintf(p, 4, "%02x:", objid[i]);
+                    p += len;
+                }
+                p-- ; /* do not include the last ':' */
+
+                if (addr_type == IPV6Z) {
+                    zone = ntohl(*((int *) &(objid[16])));
+                    len = sprintf(p, "%%%lu", zone);
+                    p += len;
+                }
+
+                break;
+
+            case DNS:
+            default:
+                /* DNS can just be handled by dump_realloc_oid_to_string() */
+                return 2;
+        }
+
+        *p = quotechar;
+
+        return snmp_strcat(buf, buf_len, out_len, allow_realloc,
+                                               (const u_char *) intbuf);
+    }
+    return 1;
+}
+
+
 int
 dump_realloc_oid_to_string(const oid * objid, size_t objidlen,
                            u_char ** buf, size_t * buf_len,
@@ -3985,7 +4068,7 @@ _get_realloc_symbol(const oid * objid, size_t objidlen,
                 numids = objidlen;
                 if (numids > objidlen)
                     goto finish_it;
-
+                
                 if (!*buf_overflow) {
                     if (!dump_realloc_oid_to_string
                         (objid, numids, buf, buf_len, out_len,
@@ -4046,7 +4129,36 @@ _get_realloc_symbol(const oid * objid, size_t objidlen,
                     }
                 } else {
                     if (!*buf_overflow) {
-                        if (!dump_realloc_oid_to_string
+                        struct tree * next_peer;
+                        int normal_handling = 1;
+                        
+                        if (tp->next_peer) {
+                            next_peer = tp->next_peer;
+                            
+                        }
+                        /* Try handling the InetAddress in the OID, in case of failure,
+                         * use the normal_handling.
+                         */
+                        if (tp->next_peer &&
+                            strcmp(get_tc_descriptor(tp->tc_index), "InetAddress") == 0 &&
+                            strcmp(get_tc_descriptor(next_peer->tc_index),
+                                   "InetAddressType") == 0 ) {
+                            
+                            int ret;
+                            int addr_type = *(objid - 1);
+                            
+                            ret = dump_realloc_oid_to_inetaddress(addr_type, objid + 1,
+                                                                  numids - 1, buf, buf_len,
+                                                                  out_len, allow_realloc, '"');
+                        if (ret != 2) {
+                            normal_handling = 0;
+                            if (ret == 0) {
+                                *buf_overflow = 1;
+                            }
+                            
+                        }
+                        }
+                        if (normal_handling && !dump_realloc_oid_to_string
                             (objid + 1, numids - 1, buf, buf_len, out_len,
                              allow_realloc, '"')) {
                             *buf_overflow = 1;
