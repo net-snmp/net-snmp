9bcc734079d918221d4406e6cff0d6d6947dd27e
Robert Story <rstory@users.sourceforge.net>
2005-03-04 00:11:55 +0000

    - keep saved status data between mode calls (in reqinfo), so we can.. - build all temporary request lists before calling next handler, so we can.. - track number of requests and number of rows, so we can.. - provide api for lower handler to know if current call is for first/last row - log a warning if handler for row returns error, but DONT exit loop   - if lower handler returns err, pass it to parent - add more debugging
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@11982 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/agent/row_merge.h
+++ net-snmp/include/net-snmp/agent/row_merge.h
@@ -11,9 +11,24 @@
 extern          "C" {
 #endif
 
+    typedef struct netsnmp_row_merge_status_x {
+       char                  count;    /* number of requests */
+       char                  rows;     /* number of rows (unique indexes) */
+       char                  current;  /* current row number */
+       char                  reserved; /* reserver for future use */
+
+       netsnmp_request_info  **saved_requests; /* internal use */
+       char                  *saved_status;    /* internal use */
+    } netsnmp_row_merge_status;
+
     netsnmp_mib_handler *netsnmp_get_row_merge_handler(int);
     int   netsnmp_register_row_merge(netsnmp_handler_registration *reginfo);
     void  netsnmp_init_row_merge(void);
+
+    int netsnmp_row_merge_status_first(netsnmp_handler_registration *reginfo,
+                                       netsnmp_agent_request_info *reqinfo);
+    int netsnmp_row_merge_status_last(netsnmp_handler_registration *reginfo,
+                                      netsnmp_agent_request_info *reqinfo);
 
     Netsnmp_Node_Handler netsnmp_row_merge_helper_handler;
 
