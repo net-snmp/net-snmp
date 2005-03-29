4e7b2685ebbed7f421bb16a5e2a9b18742d578cc
Niels Baggesen <nba@users.sourceforge.net>
2005-03-29 21:05:26 +0000

    Fix bad format parameters to snmp_log
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@12038 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/if-mib/ifTable/ifTable_interface.c
+++ agent/mibgroup/if-mib/ifTable/ifTable_interface.c
@@ -337,7 +337,7 @@ ifTable_index_to_oid(netsnmp_index * oid_idx, ifTable_mib_index * mib_idx)
     err = build_oid_noalloc(oid_idx->oids, oid_idx->len, &oid_idx->len,
                             NULL, 0, &var_ifIndex);
     if (err)
-        snmp_log(LOG_ERR, "error %d converting index to oid\n");
+        snmp_log(LOG_ERR, "error %d converting index to oid\n", err);
 
     /*
      * parsing may have allocated memory. free it.
