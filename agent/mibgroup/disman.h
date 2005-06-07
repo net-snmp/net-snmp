8f8211f473f4711bc52a4332ecd9ba3727101e79
Dave Shield <dts12@users.sourceforge.net>
2005-06-07 14:17:28 +0000

    Preliminary (and incomplete) implementation of the DisMan Schedule MIB.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12272 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/disman.h
+++ agent/mibgroup/disman.h
@@ -3,6 +3,7 @@
  */
 config_require(disman/event-mib)
 config_require(disman/expression-mib)
-config_require(disman/nslookup-mib)
-config_require(disman/ping-mib)
-config_require(disman/traceroute-mib)
+config_require(disman/schedule)
+/* config_require(disman/nslookup-mib)   */
+/* config_require(disman/ping-mib)       */
+/* config_require(disman/traceroute-mib) */
---
+++ agent/mibgroup/disman/schedule.h
@@ -0,0 +1,3 @@
+config_require(disman/schedCore)
+config_require(disman/schedConf)
+config_require(disman/schedTable)
