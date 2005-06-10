49789d83711b7e696ec33c2687d1710a483030f6
Dave Shield <dts12@users.sourceforge.net>
2005-06-10 11:32:06 +0000

    Tweak the specifically agent-internal query routines to use the SNMP library versions.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12289 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/utilities/iquery.h
+++ agent/mibgroup/utilities/iquery.h
@@ -3,28 +3,16 @@
 
 void init_iquery(void);
 
-netsnmp_session *iquery_user_session(      char* secName);
-netsnmp_session *iquery_community_session( char* community, int version );
-netsnmp_session *iquery_pdu_session(       netsnmp_pdu* pdu);
-netsnmp_session *iquery_session( char* secName,   int   mpModel,
-                                 int   secModel,  int   secLevel,
-                               u_char* engineID, size_t engIDLen);
+netsnmp_session *netsnmp_iquery_user_session(      char* secName);
+netsnmp_session *netsnmp_iquery_community_session( char* community, int version );
+netsnmp_session *netsnmp_iquery_pdu_session(netsnmp_pdu* pdu);
+netsnmp_session *netsnmp_iquery_session( char* secName,   int   mpModel,
+                                        int   secModel,  int   secLevel,
+                                      u_char* engineID, size_t engIDLen);
 
-
-int iquery_get(        netsnmp_variable_list *list);
-int iquery_get_session(netsnmp_variable_list *list,
-                       netsnmp_session       *session);
-
-int iquery_getnext(        netsnmp_variable_list *list);
-int iquery_getnext_session(netsnmp_variable_list *list,
-                           netsnmp_session       *session);
-
-int iquery_walk(        netsnmp_variable_list *list);
-int iquery_walk_session(netsnmp_variable_list *list,
-                        netsnmp_session       *session);
-
-int iquery_set(        netsnmp_variable_list *list);
-int iquery_set_session(netsnmp_variable_list *list,
-                       netsnmp_session       *session);
+int netsnmp_iquery_get(     netsnmp_variable_list *list);
+int netsnmp_iquery_getnext( netsnmp_variable_list *list);
+int netsnmp_iquery_walk(    netsnmp_variable_list *list);
+int netsnmp_iquery_set(     netsnmp_variable_list *list);
 
 #endif                          /* IQUERY_H */
