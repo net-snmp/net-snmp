ff61c3d7f888ce8a76bc46fd9dcfb90a337f83d1
Dave Shield <dts12@users.sourceforge.net>
2005-06-30 08:35:49 +0000

    Standard (system-independent) mechanism for tracking historical CPU statistics, in order to calculate rolling averages.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12480 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/hardware/cpu/cpu.c
+++ net-snmp/agent/mibgroup/hardware/cpu/cpu.c
@@ -4,12 +4,35 @@
 #include <net-snmp/agent/hardware/cpu.h>
 
 extern NetsnmpCacheLoad netsnmp_cpu_arch_load;
+static void _cpu_update_stats( unsigned int, void* );
+
+int _cpuAutoUpdate =  5;
+int _cpuHistoryLen;
 
 netsnmp_cpu_info *_cpu_head  = NULL;
 netsnmp_cpu_info *_cpu_tail  = NULL;
 netsnmp_cache    *_cpu_cache = NULL;
 
 void init_cpu( void ) {
+    /*
+     * If we're sampling the CPU statistics automatically,
+     *   then arrange for this to be triggered regularly,
+     *   keeping sufficient samples to cover the last minute.
+     * If the system-specific code has already initialised
+     *   the list of CPU entries, then retrieve the first set
+     *   of stats immediately.
+     * Otherwise, wait until the regular sampling kicks in.
+     *
+     * If we're not sampling these statistics regularly,
+     *   create a suitable cache handler instead.
+     */
+    if ( _cpuAutoUpdate ) {
+         
+        _cpuHistoryLen = 60/_cpuAutoUpdate;
+        snmp_alarm_register(_cpuAutoUpdate, SA_REPEAT, _cpu_update_stats, NULL);
+        if ( _cpu_head )
+            _cpu_update_stats( 0, NULL );
+    } else
     _cpu_cache = netsnmp_cache_create( 5, netsnmp_cpu_arch_load, NULL, NULL, 0);
 }
 
@@ -106,5 +129,60 @@ netsnmp_cache *netsnmp_cpu_get_cache( void ) {
 }
 
 int netsnmp_cpu_load( void ) {
-     return netsnmp_cache_check_and_reload( _cpu_cache );
+        /*
+         * If we're automatically updating the stats regularly,
+         * then don't invoke the cache handling.
+         */
+    return ( _cpuAutoUpdate ? 1
+                            : netsnmp_cache_check_and_reload( _cpu_cache ));
+}
+
+    /*
+     * Call the system-specific load routine regularly,
+     * keeping track of the relevant earlier results.
+     */
+static void
+_cpu_update_stats( unsigned int reg, void* magic ) {
+    netsnmp_cpu_info *cpu;
+    int i;
+
+    for ( cpu=_cpu_head; cpu; cpu=cpu->next ) {
+        if ( !cpu->history ) {
+            /*
+             * First time through, we need to create buffers
+             * for the historical stats
+             */
+            cpu->history  = calloc( _cpuHistoryLen, sizeof(struct netsnmp_cpu_history));
+        } else {
+            /*
+             * Otherwise, rotate these values - in descending order
+             *   with the earliest (relevant) statistics in entry 0.
+             * This means that the code to calculate the rolling averages
+             *   is independent of the number of historical samples saved.
+             */
+            for (i=0; i<_cpuHistoryLen-2; i++) {
+                cpu->history[i] = cpu->history[i+1];
+            }
+            cpu->history[i].user_hist  = cpu->user_ticks;
+            cpu->history[i].sys_hist   = cpu->sys_ticks;
+            cpu->history[i].idle_hist  = cpu->idle_ticks;
+            cpu->history[i].nice_hist  = cpu->nice_ticks;
+            cpu->history[i].total_hist = cpu->user_ticks +
+                                         cpu->nice_ticks +
+                                         cpu->sys_ticks +
+                                         cpu->idle_ticks +
+                                         cpu->wait_ticks +
+                                         cpu->kern_ticks +
+                                         cpu->intrpt_ticks +
+                                         cpu->sirq_ticks;
+            cpu->history[i].ctx_hist   = cpu->nCtxSwitches;
+            cpu->history[i].intr_hist  = cpu->nInterrupts;
+        }
+    }
+
+    /*
+     * Now call the system-specific load routine, to
+     * retrieve the latest set of data.
+     */
+    netsnmp_cpu_arch_load( NULL, NULL );
 }
