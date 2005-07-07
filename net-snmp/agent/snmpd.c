34dd66e1b955ba59e97dc506ee9cfa30be77f659
Robert Story <rstory@users.sourceforge.net>
2005-07-07 16:22:55 +0000

    close all non-standard fds inherited from shell immediately
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@12518 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/snmpd.c
+++ net-snmp/agent/snmpd.c
@@ -465,6 +465,18 @@ main(int argc, char *argv[])
     FILE           *PID;
 #endif
 
+    /*
+     * close all non-standard file descriptors we may have
+     * inherited from the shell.
+     */
+    for (i = getdtablesize() - 1; i > 2; --i) {
+        (void) close(i);
+    }
+    
+    /*
+     * register signals ASAP to prevent default action (usually core)
+     * for signals during startup...
+     */
 #ifdef SIGTERM
     DEBUGMSGTL(("signal", "registering SIGTERM signal handler\n"));
     signal(SIGTERM, SnmpdShutDown);
