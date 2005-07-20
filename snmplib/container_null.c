c67e9ecacfb46ada99c9aff29d5ed8f10446e399
Robert Story <rstory@users.sourceforge.net>
2005-07-20 22:14:56 +0000

    fix include of own header (cut-n-paste error)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-1-patches@12558 06827809-a52a-0410-b366-d66718629ded

--- snmplib/container_null.c
+++ snmplib/container_null.c
@@ -29,7 +29,7 @@
 #include <net-snmp/types.h>
 #include <net-snmp/library/snmp_api.h>
 #include <net-snmp/library/container.h>
-#include <net-snmp/library/container_binary_array.h>
+#include <net-snmp/library/container_null.h>
 #include <net-snmp/library/tools.h>
 #include <net-snmp/library/snmp_assert.h>
 
