8fca005dd3d0c06b2e1cd7b07281a032a09bf72b
Robert Story <rstory@users.sourceforge.net>
2005-08-30 00:09:43 +0000

    move find_varbind_int_list to snmp_client.c
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12691 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_client.c
+++ snmplib/snmp_client.c
@@ -731,6 +731,17 @@ find_varbind_of_type(netsnmp_variable_list * var_ptr, u_char type)
     return var_ptr;
 }
 
+netsnmp_variable_list*
+find_varbind_in_list( netsnmp_variable_list *vblist,
+                      oid *name, size_t len)
+{
+    for (; vblist != NULL; vblist = vblist->next_variable)
+        if (!snmp_oid_compare(vblist->name, vblist->name_length, name, len))
+            return vblist;
+
+    return NULL;
+}
+
 /*
  * Add some value to SNMP variable.
  * If the value is large, additional memory is allocated.
