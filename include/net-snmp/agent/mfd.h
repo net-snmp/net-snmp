428b6fd669116c9ae07888e62968bffcaa01ff9b
Robert Story <rstory@users.sourceforge.net>
2005-08-30 00:47:11 +0000

    - fix comments on user flags - add user flag mask - add mfd row mask - add MFD_ROW_DIRTY flag
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12707 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/agent/mfd.h
+++ include/net-snmp/agent/mfd.h
@@ -60,22 +60,25 @@
  * 8 flags resevered for the user
  */
 #define MFD_ROW_FLAG_USER_1            0x00000001 /* user flag 1 */
-#define MFD_ROW_FLAG_USER_2            0x00000002 /* user flag 1 */
-#define MFD_ROW_FLAG_USER_3            0x00000004 /* user flag 1 */
-#define MFD_ROW_FLAG_USER_4            0x00000008 /* user flag 1 */
-#define MFD_ROW_FLAG_USER_5            0x00000010 /* user flag 1 */
-#define MFD_ROW_FLAG_USER_6            0x00000020 /* user flag 1 */
-#define MFD_ROW_FLAG_USER_7            0x00000040 /* user flag 1 */
-#define MFD_ROW_FLAG_USER_8            0x00000080 /* user flag 1 */
+#define MFD_ROW_FLAG_USER_2            0x00000002 /* user flag 2 */
+#define MFD_ROW_FLAG_USER_3            0x00000004 /* user flag 3 */
+#define MFD_ROW_FLAG_USER_4            0x00000008 /* user flag 4 */
+#define MFD_ROW_FLAG_USER_5            0x00000010 /* user flag 5 */
+#define MFD_ROW_FLAG_USER_6            0x00000020 /* user flag 6 */
+#define MFD_ROW_FLAG_USER_7            0x00000040 /* user flag 7 */
+#define MFD_ROW_FLAG_USER_8            0x00000080 /* user flag 8 */
+#define MFD_ROW_FLAG_USER_MASK         0x000000ff /* user flag mask */
 
 /*----------------------------------------------------------------------
  * MFD flags
  *
  * grow left to right, in case we want to add more user flags later
  */
+#define MFD_ROW_MASK                   0xffffff00 /* mask to clear user flags */
 #define MFD_ROW_CREATED                0x80000000 /* newly created row */
 #define MFD_ROW_DATA_FROM_USER         0x40000000 /* we didn't allocate data */
 #define MFD_ROW_DELETED                0x20000000 /* deleted row */
+#define MFD_ROW_DIRTY                  0x10000000 /* changed row */
 
 
 #endif                          /* NETSNMP_MFD_H */
