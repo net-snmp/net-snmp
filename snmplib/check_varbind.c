cae8e0fcc358a633e3c11dd23a06a55d40ed37d3
Dave Shield <dts12@users.sourceforge.net>
2005-09-12 14:46:39 +0000

    Additional SET validation utility routines (for OID and general INTEGER assignments)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12754 06827809-a52a-0410-b366-d66718629ded

--- snmplib/check_varbind.c
+++ snmplib/check_varbind.c
@@ -116,6 +116,31 @@ netsnmp_check_vb_type_and_max_size(const netsnmp_variable_list *var,
 }
 
 NETSNMP_INLINE int
+netsnmp_check_vb_oid(const netsnmp_variable_list *var)
+{
+    register int rc = SNMP_ERR_NOERROR;
+
+    if (NULL == var)
+        return SNMP_ERR_GENERR;
+    
+    if ((rc = netsnmp_check_vb_type(var,ASN_OBJECT_ID)))
+        ;
+    else
+        rc = netsnmp_check_vb_max_size(var, MAX_OID_LEN*sizeof(oid));
+
+    return rc;
+}
+
+NETSNMP_INLINE int
+netsnmp_check_vb_int(const netsnmp_variable_list *var)
+{
+    if (NULL == var)
+        return SNMP_ERR_GENERR;
+    
+    return netsnmp_check_vb_type_and_size(var, ASN_INTEGER, sizeof(int));
+}
+
+NETSNMP_INLINE int
 netsnmp_check_vb_int_range(const netsnmp_variable_list *var, int low, int high)
 {
     register int rc = SNMP_ERR_NOERROR;
