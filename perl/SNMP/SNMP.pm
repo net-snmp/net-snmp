6f5dafa69145dd3b0efbcbdace21397d0fe24907
Wes Hardaker <hardaker@users.sourceforge.net>
2005-09-19 21:59:44 +0000

    - (perl/SNMP/SNMP.pm): version tag ( 5.2.2.pre1 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@12815 06827809-a52a-0410-b366-d66718629ded

--- perl/SNMP/SNMP.pm
+++ perl/SNMP/SNMP.pm
@@ -7,7 +7,7 @@
 #     modify it under the same terms as Perl itself.
 
 package SNMP;
-$VERSION = '5.2.1';   # current release version number
+$VERSION = '5.2.2.pre1';   # current release version number
 
 require Exporter;
 require DynaLoader;
