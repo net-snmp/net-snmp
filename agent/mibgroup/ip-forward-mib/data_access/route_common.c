a4b53fcb9978b4ca82f045bc13f29fe7bcc1be5d
Robert Story <rstory@users.sourceforge.net>
2005-09-27 15:39:44 +0000

    - don't free rt_policy if static flag set - new function to set route changes - new function to copy route_entries
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12849 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/ip-forward-mib/data_access/route_common.c
+++ agent/mibgroup/ip-forward-mib/data_access/route_common.c
@@ -25,6 +25,10 @@ static void _access_route_entry_release(netsnmp_route_entry * entry, void *unuse
  */
 extern int netsnmp_access_route_container_arch_load(netsnmp_container* container,
                                                     u_int load_flags);
+extern int
+netsnmp_arch_route_create(netsnmp_route_entry *entry);
+extern int
+netsnmp_arch_route_delete(netsnmp_route_entry *entry);
 
 
 /**---------------------------------------------------------------------*/
@@ -124,7 +128,8 @@ netsnmp_access_route_entry_free(netsnmp_route_entry * entry)
         return;
 
 #ifdef USING_IP_FORWARD_MIB_INETCIDRROUTETABLE_INETCIDRROUTETABLE_MODULE
-    if ((NULL != entry->rt_policy) && !(entry->flags & NETSNMP_ROUTE_ENTRY_POLICY_STATIC))
+    if ((NULL != entry->rt_policy) &&
+        !(entry->flags & NETSNMP_ACCESS_ROUTE_POLICY_STATIC))
         free(entry->rt_policy);
 #endif
 #ifdef USING_IP_FORWARD_MIB_IPCIDRROUTETABLE_IPCIDRROUTETABLE_MODULE
@@ -134,6 +139,129 @@ netsnmp_access_route_entry_free(netsnmp_route_entry * entry)
 
     free(entry);
 }
+
+
+/**
+ * update underlying data store (kernel) for entry
+ *
+ * @retval  0 : success
+ * @retval -1 : error
+ */
+int
+netsnmp_access_route_entry_set(netsnmp_route_entry * entry)
+{
+    int rc = SNMP_ERR_NOERROR;
+
+    if (NULL == entry) {
+        netsnmp_assert(NULL != entry);
+        return -1;
+    }
+    
+    /*
+     *
+     */
+    if (entry->flags & NETSNMP_ACCESS_ROUTE_CREATE) {
+        rc = netsnmp_arch_route_create(entry);
+    }
+    else if (entry->flags & NETSNMP_ACCESS_ROUTE_CHANGE) {
+        #warning "route change not implemented"
+        snmp_log(LOG_ERR,"netsnmp_access_route_entry_set change not supported yet\n");
+        rc = -1;
+    }
+    else if (entry->flags & NETSNMP_ACCESS_ROUTE_DELETE) {
+        rc = netsnmp_arch_route_delete(entry);
+    }
+    else {
+        snmp_log(LOG_ERR,"netsnmp_access_route_entry_set with no mode\n");
+        netsnmp_assert("route_entry_set" == "unknown mode"); 
+        rc = -1;
+    }
+    
+    return rc;
+}
+
+/**
+ * copy an  route_entry
+ *
+ * @retval -1  : error
+ * @retval 0   : no error
+ */
+int
+netsnmp_access_route_entry_copy(netsnmp_route_entry *lhs,
+                                netsnmp_route_entry *rhs)
+{
+#if 0 /* no arch stuff in route (yet) */
+    int rc;
+
+    /*
+     * copy arch stuff. we don't care if it changed
+     */
+    rc = netsnmp_arch_route_entry_copy(lhs,rhs);
+    if (0 != rc) {
+        snmp_log(LOG_ERR,"arch route copy failed\n");
+        return -1;
+    }
+#endif
+
+    lhs->if_index = rhs->if_index;
+
+    lhs->rt_dest_len = rhs->rt_dest_len;
+    memcpy(lhs->rt_dest, rhs->rt_dest, rhs->rt_dest_len);
+    lhs->rt_dest_type = rhs->rt_dest_type;
+
+    lhs->rt_nexthop_len = rhs->rt_nexthop_len;
+    memcpy(lhs->rt_nexthop, rhs->rt_nexthop, rhs->rt_nexthop_len);
+    lhs->rt_nexthop_type = rhs->rt_nexthop_type;
+
+#ifdef USING_IP_FORWARD_MIB_INETCIDRROUTETABLE_INETCIDRROUTETABLE_MODULE
+    if (NULL != lhs->rt_policy)
+        if (NETSNMP_ACCESS_ROUTE_POLICY_STATIC & lhs->flags) {
+            SNMP_FREE(lhs->rt_policy);
+        }
+        else
+            lhs->rt_policy = NULL;
+    if (NULL != rhs->rt_policy)
+        if ((NETSNMP_ACCESS_ROUTE_POLICY_STATIC & rhs->flags) &&
+            ! (NETSNMP_ACCESS_ROUTE_POLICY_DEEP_COPY & rhs->flags)) {
+            lhs->rt_policy = rhs->rt_policy;
+        }
+        else {
+            snmp_clone_mem((void **) &lhs->rt_policy, rhs->rt_policy,
+                           rhs->rt_policy_len * sizeof(oid));
+        }
+    lhs->rt_policy_len = rhs->rt_policy_len;
+#endif
+
+    lhs->rt_pfx_len = rhs->rt_pfx_len;
+    lhs->rt_type = rhs->rt_type;
+    lhs->rt_proto = rhs->rt_proto;
+
+#ifdef USING_IP_FORWARD_MIB_IPCIDRROUTETABLE_IPCIDRROUTETABLE_MODULE
+    if (NULL != lhs->rt_info)
+        SNMP_FREE(lhs->rt_info);
+    if (NULL != rhs->rt_info)
+        snmp_clone_mem((void **) &lhs->rt_info, rhs->rt_info,
+                       rhs->rt_info_len * sizeof(oid));
+    lhs->rt_info_len = rhs->rt_info_len;
+
+    lhs->rt_mask = rhs->rt_mask;
+    lhs->rt_tos = rhs->rt_tos;
+#endif
+
+    lhs->rt_age = rhs->rt_age;
+    lhs->rt_nexthop_as = rhs->rt_nexthop_as;
+
+    lhs->rt_metric1 = rhs->rt_metric1;
+    lhs->rt_metric2 = rhs->rt_metric2;
+    lhs->rt_metric3 = rhs->rt_metric3;
+    lhs->rt_metric4 = rhs->rt_metric4;
+    lhs->rt_metric5 = rhs->rt_metric5;
+
+    lhs->flags = rhs->flags;
+   
+    return 0;
+}
+
 
 /**---------------------------------------------------------------------*/
 /*
