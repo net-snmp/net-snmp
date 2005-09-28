1ce6eaf5932f992d6fa5505ec221d4799a30fefd
Dave Shield <dts12@users.sourceforge.net>
2005-09-28 15:36:38 +0000

    New implementation of the DisMan Event MIB.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12900 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/mibgroup/disman/event.h
@@ -0,0 +1,18 @@
+/*
+ * wrapper for the disman event mib code files 
+ */
+config_require(disman/event/mteTrigger)
+config_require(disman/event/mteTriggerTable)
+config_require(disman/event/mteTriggerDeltaTable)
+config_require(disman/event/mteTriggerExistenceTable)
+config_require(disman/event/mteTriggerBooleanTable)
+config_require(disman/event/mteTriggerThresholdTable)
+config_require(disman/event/mteTriggerConf)
+config_require(disman/event/mteEvent)
+config_require(disman/event/mteEventTable)
+config_require(disman/event/mteEventSetTable)
+config_require(disman/event/mteEventNotificationTable)
+config_require(disman/event/mteEventConf)
+config_require(disman/event/mteObjects)
+config_require(disman/event/mteObjectsTable)
+config_require(disman/event/mteObjectsConf)
