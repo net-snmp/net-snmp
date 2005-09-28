a48c3b0ddc2d50980060c1585a838c42ea6a7c1e
Dave Shield <dts12@users.sourceforge.net>
2005-09-28 10:07:27 +0000

    Use the default query session from the library.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@12896 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/utilities/iquery.h
+++ agent/mibgroup/utilities/iquery.h
@@ -12,9 +12,4 @@ netsnmp_session *netsnmp_iquery_session( char* secName,  int   version,
                                         int   secModel,  int   secLevel,
                                       u_char* engineID, size_t engIDLen);
 
-int netsnmp_iquery_get(     netsnmp_variable_list *list);
-int netsnmp_iquery_getnext( netsnmp_variable_list *list);
-int netsnmp_iquery_walk(    netsnmp_variable_list *list);
-int netsnmp_iquery_set(     netsnmp_variable_list *list);
-
 #endif                          /* IQUERY_H */
