11b280155d792bdefb9b986ca7f2e9cc3808c08b
Wes Hardaker <hardaker@users.sourceforge.net>
2005-10-11 04:45:55 +0000

    - (perl/OID/OID.pm): version tag ( 5.2.2.pre4 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@13017 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/perl/OID/OID.pm
+++ net-snmp/perl/OID/OID.pm
@@ -91,7 +91,7 @@ use vars qw(@ISA %EXPORT_TAGS @EXPORT_OK @EXPORT $VERSION $AUTOLOAD);
 	snmp_oid_compare
         compare
 );
-$VERSION = '5.2.2.pre3';
+$VERSION = '5.2.2.pre4';
 
 sub new {
     my $type = shift;
