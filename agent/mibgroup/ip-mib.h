90d36f2cb5752d1dec939864a56c94cbf4332e00
Robert Story <rstory@users.sourceforge.net>
2005-10-22 00:27:22 +0000

    add new IP-MIB scalars
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@13150 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/ip-mib.h
+++ agent/mibgroup/ip-mib.h
@@ -5,4 +5,5 @@
 config_require(ip-mib/ipAddressTable);
 config_require(ip-mib/inetNetToMediaTable);
 config_require(ip-mib/ipSystemStatsTable);
+config_require(ip-mib/ip_scalars);
 config_add_mib(IP-MIB)
