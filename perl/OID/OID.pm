6d335da257e88955cfc3d7d79677dc79fa730047
Robert Story <rstory@users.sourceforge.net>
2005-10-31 09:45:47 +0000

    - (perl/OID/OID.pm): version tag ( 5.3.pre1 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@13356 06827809-a52a-0410-b366-d66718629ded

--- perl/OID/OID.pm
+++ perl/OID/OID.pm
@@ -91,7 +91,7 @@ use vars qw(@ISA %EXPORT_TAGS @EXPORT_OK @EXPORT $VERSION $AUTOLOAD);
 	snmp_oid_compare
         compare
 );
-$VERSION = '5.3.dev';
+$VERSION = '5.3.pre1';
 
 sub new {
     my $type = shift;
