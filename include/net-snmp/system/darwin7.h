87d3b082f84569fdf8302b934ffd8c6af0410cab
Robert Story <rstory@users.sourceforge.net>
2005-10-31 09:03:24 +0000

    patches from apple
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@13339 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/system/darwin7.h
+++ include/net-snmp/system/darwin7.h
@@ -6,3 +6,9 @@
  * At least, as of 5.3.
  */
 #undef HAVE_FSTAB_H
+
+#define MBSTAT_SYMBOL "mbstat"
+#undef TOTAL_MEMORY_SYMBOL
+
+#define SWAPFILE_DIR "/private/var/vm"
+#define SWAPFILE_PREFIX "swapfile"
