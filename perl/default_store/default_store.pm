ce735527a9a5d780bcd0361dbfcb7625b1333067
Wes Hardaker <hardaker@users.sourceforge.net>
2005-11-05 01:47:46 +0000

    - (perl/default_store/default_store.pm): version tag ( 5.2.2.rc4 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@13401 06827809-a52a-0410-b366-d66718629ded

--- perl/default_store/default_store.pm
+++ perl/default_store/default_store.pm
@@ -145,7 +145,7 @@ use vars qw(@ISA %EXPORT_TAGS @EXPORT_OK @EXPORT $VERSION $AUTOLOAD);
 				   NETSNMP_DS_LIB_SECMODEL
 				   NETSNMP_DS_LIB_MIBDIRS
 );
-$VERSION = '5.2.2.rc3';
+$VERSION = '5.2.2.rc4';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
