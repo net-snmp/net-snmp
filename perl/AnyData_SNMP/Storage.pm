a477d26ef7272a561b29c604a7e0741919f18aae
Robert Story <rstory@users.sourceforge.net>
2005-11-07 06:57:07 +0000

    - (perl/AnyData_SNMP/Storage.pm): version tag ( 5.3.pre2 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@13441 06827809-a52a-0410-b366-d66718629ded

--- perl/AnyData_SNMP/Storage.pm
+++ perl/AnyData_SNMP/Storage.pm
@@ -6,7 +6,7 @@ package AnyData::Storage::SNMP;
 ##   scalar sets?
 ##   multi-hosts
 
-$AnyData::Storage::VERSION = '5.3.pre1';
+$AnyData::Storage::VERSION = '5.3.pre2';
 use strict;
 
 use vars qw(@basecols);
