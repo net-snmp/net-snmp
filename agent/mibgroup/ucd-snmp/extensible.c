16d591c16b8330c58b9e2ae19d62ab2cab5a4553
Dave Shield <dts12@users.sourceforge.net>
2005-11-22 10:43:00 +0000

    Beef up the "relocatable exec" warning message.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@13705 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/ucd-snmp/extensible.c
+++ agent/mibgroup/ucd-snmp/extensible.c
@@ -220,7 +220,7 @@ extensible_parse_config(const char *token, char *cptr)
         /*
          * its a relocatable extensible mib 
          */
-        config_perror("Warning: relocatable 'exec' format will change in a future release - See 'NET-SNMP-EXTEND-MIB' for an alternative" );
+        config_perror("WARNING: This output format is being deprecated - Please use the 'extend' directive instead" );
         for (pp = &relocs, numrelocs++; *pp; pp = &((*pp)->next));
         (*pp) = ptmp;
         pp = &relocs; scount = numrelocs;
