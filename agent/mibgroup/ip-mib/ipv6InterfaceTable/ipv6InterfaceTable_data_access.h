f03e63b8a715b0199d744dd554e8c353d48ef898
Robert Story <rstory@users.sourceforge.net>
2005-12-01 20:54:19 +0000

    regenerate mfd code
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@13770 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/ip-mib/ipv6InterfaceTable/ipv6InterfaceTable_data_access.h
+++ agent/mibgroup/ip-mib/ipv6InterfaceTable/ipv6InterfaceTable_data_access.h
@@ -35,22 +35,52 @@ extern          "C" {
      */
 
 
-    int            
+    int
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         ipv6InterfaceTable_init_data(ipv6InterfaceTable_registration *
                                      ipv6InterfaceTable_reg);
 
 
-    void            ipv6InterfaceTable_container_init(netsnmp_container **
-                                                      container_ptr_ptr);
+    void            ipv6InterfaceTable_container_init(netsnmp_container
+                                                      **container_ptr_ptr);
     void            ipv6InterfaceTable_container_shutdown(netsnmp_container
-                                                          * container_ptr);
+                                                          *container_ptr);
 
-    int             ipv6InterfaceTable_container_load(netsnmp_container *
-                                                      container);
-    void            ipv6InterfaceTable_container_free(netsnmp_container *
-                                                      container);
+    int             ipv6InterfaceTable_container_load(netsnmp_container
+                                                      *container);
+    void            ipv6InterfaceTable_container_free(netsnmp_container
+                                                      *container);
 
-    int            
+    int
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         ipv6InterfaceTable_row_prep(ipv6InterfaceTable_rowreq_ctx *
                                     rowreq_ctx);
 
