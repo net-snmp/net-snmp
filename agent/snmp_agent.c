84c0b6b5abcdcccf51f6a7d8b3526325f2087efc
Robert Story <rstory@users.sourceforge.net>
2005-12-06 17:11:10 +0000

    - fix 1356769: agent consumes CPU, doesnt respond to SIGTERM   - app local running -> agent lib global netsnmp_running   - while(1) -> while(netsnmp_running)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-1-patches@13870 06827809-a52a-0410-b366-d66718629ded

--- agent/snmp_agent.c
+++ agent/snmp_agent.c
@@ -136,7 +136,7 @@ struct addrCache {
 static struct addrCache addrCache[SNMP_ADDRCACHE_SIZE];
 int             log_addresses = 0;
 
-
+int             netsnmp_running = 1;
 
 typedef struct _agent_nsap {
     int             handle;
@@ -2744,7 +2744,7 @@ handle_getnext_loop(netsnmp_agent_session *asp)
     /*
      * loop 
      */
-    while (1) {
+    while (netsnmp_running) {
 
         /*
          * bail for now if anything is delegated. 
