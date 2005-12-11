2ba569ac63a5b56e5fd920e5f3760151707da156
Robert Story <rstory@users.sourceforge.net>
2005-12-11 20:14:17 +0000

    - (perl/SNMP/SNMP.pm): version tag ( 5.3.rc1 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@13933 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/perl/SNMP/SNMP.pm
+++ net-snmp/perl/SNMP/SNMP.pm
@@ -7,7 +7,7 @@
 #     modify it under the same terms as Perl itself.
 
 package SNMP;
-$VERSION = '5.3.pre5';   # current release version number
+$VERSION = '5.3.rc1';   # current release version number
 
 require Exporter;
 require DynaLoader;
