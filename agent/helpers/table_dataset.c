57c68f91db2560c606d202fab9cbe3f4ff0cd895
Dave Shield <dts12@users.sourceforge.net>
2006-01-11 14:55:17 +0000

    Clarify the behaviour of netsnmp_table_dataset_remove_row()
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-1-patches@14101 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/table_dataset.c
+++ agent/helpers/table_dataset.c
@@ -1103,7 +1103,7 @@ netsnmp_table_dataset_delete_row(netsnmp_table_row *row)
     netsnmp_table_dataset_delete_all_data(data);
 }
 
-/** removes a row from the table, but doesn't delete/free anything */
+/** removes a row from the table, but doesn't delete/free the column values */
 NETSNMP_INLINE void
 netsnmp_table_dataset_remove_row(netsnmp_table_data_set *table,
                                  netsnmp_table_row *row)
@@ -1114,7 +1114,7 @@ netsnmp_table_dataset_remove_row(netsnmp_table_data_set *table,
     netsnmp_table_data_remove_and_delete_row(table->table, row);
 }
 
-/** removes a row from the table and then deletes it (and all it's data) */
+/** removes a row from the table and then deletes it (and all its data) */
 NETSNMP_INLINE void
 netsnmp_table_dataset_remove_and_delete_row(netsnmp_table_data_set *table,
                                             netsnmp_table_row *row)
