81b41459b1e6e0cc226822c3d25eada32219fc6f
Robert Story <rstory@users.sourceforge.net>
2006-02-10 23:47:28 +0000

    - a tangled web of fixes for override   - back out fix for 711465: override directive ignored with snmpbulkget   - apply new fixes for 711465 (see bug report for gory details)   - also fix override of table instance, reported on coders - and, quoting Dave:
    I'm not at all sure that this is necessarily the correct fix, but
    it wasn't very clear as to exactly what is being handled here.
    This change certainly addresses the above bug report, without
    seeming to break anything else.
    
    Let's give this patch time to settle into the main development code,
    and then consider back-porting it to earlier lines.....
    
    
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@14243 06827809-a52a-0410-b366-d66718629ded

--- agent/snmp_agent.c
+++ agent/snmp_agent.c
@@ -2789,9 +2789,7 @@ check_getnext_results(netsnmp_agent_session *asp)
             /*
              * out of range? 
              */
-            if (request->requestvb->type != ASN_NULL &&
-                request->requestvb->type != ASN_PRIV_RETRY &&
-                snmp_oid_compare(request->requestvb->name,
+            if (snmp_oid_compare(request->requestvb->name,
                                  request->requestvb->name_length,
                                  request->range_end,
                                  request->range_end_len) >= 0) {
@@ -2804,7 +2802,18 @@ check_getnext_results(netsnmp_agent_session *asp)
                 DEBUGMSGTL(("check_getnext_results",
                             "request response %d out of range\n",
                             request->index));
-                request->inclusive = 1;
+                /*
+                 * I'm not sure why inclusive is set unconditionally here (see
+                 * comments for revision 1.161), but it causes a problem for
+                 * GETBULK over an overridden variable. The bulk-to-next
+                 * handler re-uses the same request for multiple varbinds,
+                 * and once inclusive was set, it was never cleared. So, a
+                 * hack. Instead of setting it to 1, set it to 2, so bulk-to
+                 * next can clear it later. As of the time of this hack, all
+                 * checks of this var are boolean checks (not == 1), so this
+                 * should be safe. Cross your fingers.
+                 */
+                request->inclusive = 2;
                 /*
                  * XXX: should set this to the original OID? 
                  */
@@ -2821,6 +2830,7 @@ check_getnext_results(netsnmp_agent_session *asp)
             if (request->requestvb->type == SNMP_ENDOFMIBVIEW) {
                 /*
                  * illegal response from a subagent.  Change it back to NULL 
+                 *  xxx-rks: err, how do we know this is a subagent?
                  */
                 request->requestvb->type = ASN_NULL;
                 request->inclusive = 1;
