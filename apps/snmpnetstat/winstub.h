b6a80f52251b3779e685b2854e1ff504983bec65
Dave Shield <dts12@users.sourceforge.net>
2006-03-24 16:39:36 +0000

    Copy/port (redistributable) snmpnetstat rewrite
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-2-patches@14450 06827809-a52a-0410-b366-d66718629ded

--- apps/snmpnetstat/winstub.h
+++ apps/snmpnetstat/winstub.h
@@ -4,34 +4,45 @@
 
 #if (defined(WIN32) || defined(cygwin))
 
-/* database access functions for host, services, protocols, networks */
+/*
+ * database access functions for host, services, protocols, networks 
+ */
 
-/* sets can open. ends must close. */
-void sethostent (int stay_open);
-void setservent (int stay_open);
-void setprotoent (int stay_open);
-void setnetent (int stay_open);
-void endhostent (void);
-void endservent (void);
-void endprotoent (void);
-void endnetent (void);
+/*
+ * sets can open. ends must close. 
+ */
+void            sethostent(int stay_open);
+void            setservent(int stay_open);
+void            setprotoent(int stay_open);
+void            setnetent(int stay_open);
+void            endhostent(void);
+void            endservent(void);
+void            endprotoent(void);
+void            endnetent(void);
 
-/* get next entry from data base file, or from NIS if possible. */
-/* returns 0 if there are no more entries to read. */
-struct hostent *gethostent (void);
-struct servent *getservent (void);
-struct protoent *getprotoent (void);
-struct netent *getnetent (void);
+/*
+ * get next entry from data base file, or from NIS if possible. 
+ */
+/*
+ * returns 0 if there are no more entries to read. 
+ */
+struct hostent *gethostent(void);
+struct servent *getservent(void);
+struct protoent *getprotoent(void);
+struct netent  *getnetent(void);
 
-struct netent * getnetbyaddr (long net, int type);
+struct netent  *getnetbyaddr(long net, int type);
 
-/* Return the network number from an internet address */
-u_long inet_netof(struct in_addr in);
+/*
+ * Return the network number from an internet address 
+ */
+u_long          inet_netof(struct in_addr in);
 
-/* Return the host number from an internet address */
-u_long inet_lnaof(struct in_addr in);
+/*
+ * Return the host number from an internet address 
+ */
+u_long          inet_lnaof(struct in_addr in);
 
-#endif /* WIN32 or cygwin */
+#endif                          /* WIN32 or cygwin */
 
 #endif /*_WINSTUB_H_ */
-
