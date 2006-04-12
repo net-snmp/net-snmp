3179e4db480a0671f69016e114011c3357ba0b6e
Wes Hardaker <hardaker@users.sourceforge.net>
2006-04-12 17:09:58 +0000

    fix a bug reported in 1118814 where init length wasn't getting initialized properly
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@14468 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/vacm.c
+++ net-snmp/snmplib/vacm.c
@@ -137,6 +137,7 @@ vacm_parse_config_view(const char *token, char *line)
     line = skip_token(line);
     view.viewType = atoi(line);
     line = skip_token(line);
+    len = sizeof(view.viewName);
     line =
         read_config_read_octet_string(line, (u_char **) & viewName, &len);
     view.viewSubtreeLen = MAX_OID_LEN;
@@ -222,8 +223,10 @@ vacm_parse_config_access(const char *token, char *line)
     line = skip_token(line);
     access.contextMatch = atoi(line);
     line = skip_token(line);
+    len = sizeof(access.groupName);
     line =
         read_config_read_octet_string(line, (u_char **) & groupName, &len);
+    len = sizeof(access.contextPrefix);
     line =
         read_config_read_octet_string(line, (u_char **) & contextPrefix,
                                       &len);
@@ -240,12 +243,15 @@ vacm_parse_config_access(const char *token, char *line)
     aptr->securityLevel = access.securityLevel;
     aptr->contextMatch = access.contextMatch;
     readView = (char *) aptr->readView;
+    len = sizeof(aptr->readView);
     line =
         read_config_read_octet_string(line, (u_char **) & readView, &len);
     writeView = (char *) aptr->writeView;
+    len = sizeof(aptr->writeView);
     line =
         read_config_read_octet_string(line, (u_char **) & writeView, &len);
     notifyView = (char *) aptr->notifyView;
+    len = sizeof(aptr->notifyView);
     line =
         read_config_read_octet_string(line, (u_char **) & notifyView,
                                       &len);
@@ -294,6 +300,7 @@ vacm_parse_config_group(const char *token, char *line)
     line = skip_token(line);
     group.securityModel = atoi(line);
     line = skip_token(line);
+    len = sizeof(group.securityName);
     line =
         read_config_read_octet_string(line, (u_char **) & securityName,
                                       &len);
@@ -305,6 +312,7 @@ vacm_parse_config_group(const char *token, char *line)
     gptr->status = group.status;
     gptr->storageType = group.storageType;
     groupName = (char *) gptr->groupName;
+    len = sizeof(group.groupName);
     line =
         read_config_read_octet_string(line, (u_char **) & groupName, &len);
 }
