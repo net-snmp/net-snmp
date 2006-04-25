4e458fb083f1fa523b5be469307459fc31900989
Wes Hardaker <hardaker@users.sourceforge.net>
2006-04-25 17:26:23 +0000

    modified version of [ 1466451 ] ipAddress size with 64bit to use sizeof() for the datatype in question
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-3-patches@14504 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/mibII/ipAddr.c
+++ net-snmp/agent/mibgroup/mibII/ipAddr.c
@@ -297,6 +297,7 @@ var_ipAddrEntry(struct variable *vp,
         long_return = lowin_ifaddr.Addr;
         return (u_char *) & long_return;
 #elif defined(linux) || defined(sunV3)
+        *var_len = sizeof(((struct sockaddr_in *) &lowin_ifnet.if_addr)->sin_addr.s_addr);
         return (u_char *) & ((struct sockaddr_in *) &lowin_ifnet.if_addr)->
             sin_addr.s_addr;
 #else
@@ -311,6 +312,7 @@ var_ipAddrEntry(struct variable *vp,
         long_return = lowin_ifaddr.NetMask;
         return (u_char *) & long_return;
 #elif defined(linux)
+        *var_len = sizeof(((struct sockaddr_in *) &lowin_ifnet.ia_subnetmask)->sin_addr.s_addr);
         return (u_char *) & ((struct sockaddr_in *) &lowin_ifnet.
                              ia_subnetmask)->sin_addr.s_addr;
 #elif !defined(sunV3)
@@ -321,6 +323,7 @@ var_ipAddrEntry(struct variable *vp,
 #ifdef hpux11
         long_return = lowin_ifaddr.BcastAddr;
 #elif defined(linux) || defined(sunV3)
+        *var_len = sizeof(long_return);
         long_return =
             ntohl(((struct sockaddr_in *) &lowin_ifnet.ifu_broadaddr)->
                   sin_addr.s_addr) & 1;
