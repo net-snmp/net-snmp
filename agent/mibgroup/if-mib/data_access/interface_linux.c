ebbf42bab8d540070f67b4aa11313884173fca95
Wes Hardaker <hardaker@users.sourceforge.net>
2006-04-25 17:39:54 +0000

    [ 1466450 ] fix compilation with linux/ethtool.  In other words: ugh nasty but needed patch
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@14505 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/if-mib/data_access/interface_linux.c
+++ agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -23,12 +23,11 @@
 #include <sys/types.h>
 
 #ifdef HAVE_LINUX_ETHTOOL_H
-#   ifdef HAVE_ASM_TYPES_H
-#      include <asm/types.h>
-#   endif /* HAVE_ASM_TYPES_H */
-#   ifdef HAVE_PCI_PCI_H
-#      include <pci/pci.h> /* for u32 typedef in linux/ethtool */
-#   endif /* HAVE_PCI_PCI_H */
+#include <sys/types.h>
+typedef unsigned long long u64;         /* hack, so we may include kernel's ethtool.h */
+typedef __uint32_t u32;         /* ditto */
+typedef __uint16_t u16;         /* ditto */
+typedef __uint8_t u8;           /* ditto */
 #include <linux/ethtool.h>
 #endif /* HAVE_LINUX_ETHTOOL_H */
 #include <linux/sockios.h>
