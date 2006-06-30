0dea6b253e4a9152fdcf724153070db689b7b8a0
Wes Hardaker <hardaker@users.sourceforge.net>
2006-06-30 22:30:20 +0000

    new perl versioning support
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@14832 06827809-a52a-0410-b366-d66718629ded

--- perl/AnyData_SNMP/Storage.pm
+++ perl/AnyData_SNMP/Storage.pm
@@ -6,7 +6,7 @@ package AnyData::Storage::SNMP;
 ##   scalar sets?
 ##   multi-hosts
 
-$AnyData::Storage::VERSION = '5.2.3.pre3';
+$AnyData::Storage::VERSION = '5.0203';
 use strict;
 
 use vars qw(@basecols);
