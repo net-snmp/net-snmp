a4ed8d6dea3cc68a363b6e9494e68cde54c79829
Thomas Anders <tanders@users.sourceforge.net>
2006-08-14 23:29:22 +0000

    simplify/aggregate FreeBSD headers
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15000 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/system/freebsd6.h
+++ include/net-snmp/system/freebsd6.h
@@ -1,25 +1,3 @@
-#include "freebsd.h"
-
-/*
- * freebsd6 is a superset of all since freebsd2
- */
-#define freebsd2 1
-#define freebsd3 1
-#define freebsd4 1
+/* freebsd6 is a superset of freebsd5 */
+#include "freebsd5.h"
 #define freebsd5 1
-
-#undef IFADDR_SYMBOL
-#define IFADDR_SYMBOL "in_ifaddrhead"
-
-#undef PROC_SYMBOL
-#define PROC_SYMBOL "allproc"
-
-#undef NPROC_SYMBOL
-#define NPROC_SYMBOL "nprocs"
-
-#undef TOTAL_MEMORY_SYMBOL
-
-#undef MBSTAT_SYMBOL
-
-#undef INP_NEXT_SYMBOL
-#define INP_NEXT_SYMBOL inp_next
