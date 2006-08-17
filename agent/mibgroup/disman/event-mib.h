e0b6667788d681cbb3585e6e170362d3c3b532aa
Dave Shield <dts12@users.sourceforge.net>
2006-08-17 15:41:13 +0000

    Drop DisMan Event MIB re-write warning.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@15017 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/disman/event-mib.h
+++ agent/mibgroup/disman/event-mib.h
@@ -1,3 +1,2 @@
-config_warning(New version of the Event MIB which may be subtly different from the original implementation - configure with 'disman/old-event-mib' for the previous version)
 config_require(disman/event)
 
