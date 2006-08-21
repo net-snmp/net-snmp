9720999d4199273a9efacb9c816c84e9307c437d
Dave Shield <dts12@users.sourceforge.net>
2006-08-21 09:27:09 +0000

    Active HAL CPU module for *BSD systems.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@15032 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/hardware/cpu.h
+++ agent/mibgroup/hardware/cpu.h
@@ -1,2 +1,16 @@
 config_require(hardware/cpu/cpu)
 config_arch_require(linux, hardware/cpu/cpu_linux)
+
+config_arch_require(freebsd2, hardware/cpu/cpu_nlist)
+config_arch_require(freebsd3, hardware/cpu/cpu_nlist)
+config_arch_require(freebsd4, hardware/cpu/cpu_nlist)
+config_arch_require(freebsd5, hardware/cpu/cpu_nlist)
+config_arch_require(freebsd6, hardware/cpu/cpu_nlist)
+
+config_arch_require(netbsd,     hardware/cpu/cpu_sysctl)
+config_arch_require(netbsd1,    hardware/cpu/cpu_sysctl)
+config_arch_require(netbsdelf,  hardware/cpu/cpu_sysctl)
+config_arch_require(netbsdelf2, hardware/cpu/cpu_sysctl)
+config_arch_require(netbsdelf3, hardware/cpu/cpu_sysctl)
+config_arch_require(openbsd2,   hardware/cpu/cpu_sysctl)
+config_arch_require(openbsd3,   hardware/cpu/cpu_sysctl)
