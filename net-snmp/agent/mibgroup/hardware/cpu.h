e8422259bad975b78bedfa10c9d6338014b3f625
Thomas Anders <tanders@users.sourceforge.net>
2006-08-22 21:48:45 +0000

    activate mem/cpu HAL code for AIX
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15050 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/hardware/cpu.h
+++ net-snmp/agent/mibgroup/hardware/cpu.h
@@ -14,3 +14,5 @@ config_arch_require(netbsdelf2, hardware/cpu/cpu_sysctl)
 config_arch_require(netbsdelf3, hardware/cpu/cpu_sysctl)
 config_arch_require(openbsd2,   hardware/cpu/cpu_sysctl)
 config_arch_require(openbsd3,   hardware/cpu/cpu_sysctl)
+
+config_arch_require(aix5, hardware/cpu/cpu_perfstat)
