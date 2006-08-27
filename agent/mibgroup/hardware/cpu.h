fa9cbc85e6eca4d64d04b890296d75fae5c558b3
Dave Shield <dts12@users.sourceforge.net>
2006-08-27 17:04:22 +0000

    Ensure that some form of HAL CPU module is included for all systems.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15099 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/hardware/cpu.h
+++ agent/mibgroup/hardware/cpu.h
@@ -1,19 +1,17 @@
 config_require(hardware/cpu/cpu)
-config_arch_require(linux, hardware/cpu/cpu_linux)
 
-config_arch_require(freebsd2, hardware/cpu/cpu_nlist)
-config_arch_require(freebsd3, hardware/cpu/cpu_nlist)
-config_arch_require(freebsd4, hardware/cpu/cpu_nlist)
-config_arch_require(freebsd5, hardware/cpu/cpu_nlist)
-config_arch_require(freebsd6, hardware/cpu/cpu_nlist)
+#if defined(linux)
+config_require(hardware/cpu/cpu_linux)
 
-config_arch_require(netbsd,     hardware/cpu/cpu_sysctl)
-config_arch_require(netbsd1,    hardware/cpu/cpu_sysctl)
-config_arch_require(netbsdelf,  hardware/cpu/cpu_sysctl)
-config_arch_require(netbsdelf2, hardware/cpu/cpu_sysctl)
-config_arch_require(netbsdelf3, hardware/cpu/cpu_sysctl)
-config_arch_require(openbsd2,   hardware/cpu/cpu_sysctl)
-config_arch_require(openbsd3,   hardware/cpu/cpu_sysctl)
+#elif (defined(freebsd2) || defined(freebsd3) || defined(freebsd4)  || defined(freebsd5)|| defined(freebsd6))
+config_require(hardware/cpu/cpu_nlist)
 
-config_arch_require(aix4, hardware/cpu/cpu_perfstat)
-config_arch_require(aix5, hardware/cpu/cpu_perfstat)
+#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3))
+config_require(hardware/cpu/cpu_sysctl)
+
+#elif (defined(aix4) || defined(aix5))
+config_require(hardware/cpu/cpu_perfstat)
+
+#else
+config_require(hardware/cpu/cpu_null)
+#endif
