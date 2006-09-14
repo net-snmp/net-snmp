a8ab354f4aa917b70d16976d6448c2769433dd51
Robert Story <rstory@users.sourceforge.net>
2006-09-14 17:34:16 +0000

    use NETSNMP_INCLUDE_IFTABLE_REWRITES to decide about if-mib support
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15209 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII.h
+++ agent/mibgroup/mibII.h
@@ -13,10 +13,15 @@ config_require(mibII/icmp)
 config_require(mibII/udp)
 config_require(mibII/vacm_vars)
 config_require(mibII/setSerialNo)
+
+#ifdef NETSNMP_INCLUDE_IFTABLE_REWRITES
+config_require(if-mib)
+#endif
+
 /*
  * these new module re-rewrites have only been implemented for
  * linux.
  */
 #if defined( linux )
-config_require(ip-mib if-mib ip-forward-mib tcp-mib udp-mib)
+config_require(ip-mib ip-forward-mib tcp-mib udp-mib)
 #endif
