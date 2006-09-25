3d7c8607d51ca618f91a1f1d70c854d9e4fa706c
Thomas Anders <tanders@users.sourceforge.net>
2006-09-25 15:48:30 +0000

    NetSNMP::agent tests neither need config nor MIBs
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15313 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/perl/agent/test.pl
+++ net-snmp/perl/agent/test.pl
@@ -6,7 +6,11 @@
 # Change 1..1 below to 1..last_test_to_print .
 # (It may become useful if the test is moved to ./t subdirectory.)
 
-BEGIN { $| = 1; print "1..5\n"; }
+BEGIN { $| = 1; 
+        $ENV{'SNMPCONFPATH'} = 'nopath';
+        $ENV{'MIBS'} = '';
+        print "1..5\n";
+      }
 END {print "not ok 1\n" unless $loaded;}
 use NetSNMP::agent (':all');
 use NetSNMP::default_store (':all');
