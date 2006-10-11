18865e60379abb400dc71c937b61fde88740dcca
Robert Story <rstory@users.sourceforge.net>
2006-10-11 15:02:50 +0000

    include if-mib rewrites if mfd rewrites is defined
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15390 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/system/solaris.h
+++ include/net-snmp/system/solaris.h
@@ -29,6 +29,9 @@
 
 /* define the extra mib modules that are supported */
 #define NETSNMP_INCLUDE_HOST_RESOURCES
+#ifdef NETSNMP_ENABLE_MFD_REWRITES
+#define NETSNMP_INCLUDE_IFTABLE_REWRITES
+#endif
 
 /* Solaris 2.6+ */
 #define _SLASH_PROC_METHOD_ 1
