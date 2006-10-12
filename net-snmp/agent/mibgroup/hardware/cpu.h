6313c49401b01908bca09624b6d97c142cd4e064
Dave Shield <dts12@users.sourceforge.net>
2006-10-12 09:17:04 +0000

    Include the appropriate HAL modules for Darwin (Darwin9) systems. Supplied by Chris Jalbert.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15393 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/hardware/cpu.h
+++ net-snmp/agent/mibgroup/hardware/cpu.h
@@ -6,7 +6,7 @@ config_require(hardware/cpu/cpu_linux)
 #elif (defined(freebsd2) || defined(freebsd3) || defined(freebsd4)  || defined(freebsd5)|| defined(freebsd6))
 config_require(hardware/cpu/cpu_nlist)
 
-#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3))
+#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3) || defined(darwin))
 config_require(hardware/cpu/cpu_sysctl)
 
 #elif (defined(aix4) || defined(aix5))
