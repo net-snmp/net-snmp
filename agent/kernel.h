84cbfa2f92dd2eeb821e85aa7c2f76799707eced
Thomas Anders <tanders@users.sourceforge.net>
2006-10-15 17:03:42 +0000

    klookup takes three arguments, not one
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15419 06827809-a52a-0410-b366-d66718629ded

--- agent/kernel.h
+++ agent/kernel.h
@@ -2,7 +2,7 @@
 #ifdef NETSNMP_CAN_USE_NLIST
 extern void     init_kmem(const char *);
 extern int      klookup(unsigned long, char *, int);
-#define NETSNMP_KLOOKUP(x) klookup((unsigned long) x )
+#define NETSNMP_KLOOKUP(x,y,z) klookup((unsigned long) x,y,z)
 #endif
 
 #if HAVE_KVM_H
