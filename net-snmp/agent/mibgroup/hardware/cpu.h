9c16dc5fff82ed46215df1d92e3c791da38f9f86
Thomas Anders <tanders@users.sourceforge.net>
2006-10-20 15:13:11 +0000

    fix syntax error; make sure we get cpu_null for darwin6/7, cpu_sysctl for darwin8+
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15471 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/hardware/cpu.h
+++ net-snmp/agent/mibgroup/hardware/cpu.h
@@ -3,13 +3,14 @@ config_require(hardware/cpu/cpu)
 #if defined(linux)
 config_require(hardware/cpu/cpu_linux)
 
-#if (defined(darwin7) || defined(darwin6))
+#elif (defined(darwin6) || defined(darwin7))
+/* XXX: we need to come up with working code for OSX 10.2/10.3 */
 config_require(hardware/cpu/cpu_null)
 
 #elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3) || defined(openbsd4) || defined(darwin))
 config_require(hardware/cpu/cpu_sysctl)
 
-#elif (defined(freebsd2) || defined(freebsd3) || defined(freebsd4)  || defined(freebsd5)|| defined(freebsd6) || defined(darwin))
+#elif (defined(freebsd2) || defined(freebsd3) || defined(freebsd4)  || defined(freebsd5)|| defined(freebsd6))
 config_require(hardware/cpu/cpu_nlist)
 
 #elif (defined(aix4) || defined(aix5))
