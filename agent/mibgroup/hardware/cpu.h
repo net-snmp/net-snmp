03415d53409a6230672c7e9c7dbda22e7f96e4f7
Thomas Anders <tanders@users.sourceforge.net>
2006-10-20 08:44:13 +0000

    openbsd4 and darwin should use cpu_sysctl
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15469 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/hardware/cpu.h
+++ agent/mibgroup/hardware/cpu.h
@@ -3,11 +3,11 @@ config_require(hardware/cpu/cpu)
 #if defined(linux)
 config_require(hardware/cpu/cpu_linux)
 
-#elif (defined(freebsd2) || defined(freebsd3) || defined(freebsd4)  || defined(freebsd5)|| defined(freebsd6))
-config_require(hardware/cpu/cpu_nlist)
-
-#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3) || defined(darwin))
+#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3) || defined(openbsd4) || defined(darwin))
 config_require(hardware/cpu/cpu_sysctl)
+
+#elif (defined(freebsd2) || defined(freebsd3) || defined(freebsd4)  || defined(freebsd5)|| defined(freebsd6) || defined(darwin))
+config_require(hardware/cpu/cpu_nlist)
 
 #elif (defined(aix4) || defined(aix5))
 config_require(hardware/cpu/cpu_perfstat)
