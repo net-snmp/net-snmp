87b85a827c9dc33c618b080f5dc645a3a60c296f
Thomas Anders <tanders@users.sourceforge.net>
2006-12-07 15:54:47 +0000

    NEWS: MacOSX: PATCH: 1600522: from "Bill Fenner": CPU HAL implementation for mach/darwin
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@15644 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/hardware/cpu.h
+++ net-snmp/agent/mibgroup/hardware/cpu.h
@@ -4,11 +4,9 @@ config_require(hardware/cpu/cpu)
 config_require(hardware/cpu/cpu_linux)
 
 #elif defined(darwin)
-/* XXX: note this is a duplicate case (see darwin in net/open bsd below */
-/* XXX: we need to come up with working code for OSX */
-config_require(hardware/cpu/cpu_null)
+config_require(hardware/cpu/cpu_mach)
 
-#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3) || defined(openbsd4) || defined(darwin))
+#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3) || defined(openbsd4))
 config_require(hardware/cpu/cpu_sysctl)
 
 #elif (defined(freebsd2) || defined(freebsd3) || defined(freebsd4)  || defined(freebsd5)|| defined(freebsd6))
