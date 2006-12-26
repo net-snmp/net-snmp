7db4ac9505f6850402051ecf33add4aadb6ef77f
Alex Burger <alex_b@users.sourceforge.net>
2006-12-26 16:52:37 +0000

    CHANGES: Win32: PATCH: 1622080: from Grueninger A: pass Win32 handles remain open.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@15680 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/struct.h
+++ net-snmp/agent/mibgroup/struct.h
@@ -19,6 +19,7 @@ struct extensible {
     unsigned long   miboid[MIBMAX];
     size_t          miblen;
     int             pid;
+    int             tid;                /* WIN32 thread */
 };
 
 struct myproc {
