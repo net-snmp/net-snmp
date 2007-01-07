dd53d6b4b6a7be651cd08b01b311966ec443dc18
Thomas Anders <tanders@users.sourceforge.net>
2007-01-07 19:47:39 +0000

    add missing declaration for netsnmp_register_service_handlers (from magfr)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@15738 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/library/snmp_service.h
+++ include/net-snmp/library/snmp_service.h
@@ -26,6 +26,9 @@ netsnmp_lookup_default_target(const char* application, const char* domain);
 extern void
 netsnmp_clear_default_target(void);
 
+extern void
+netsnmp_register_service_handlers(void);
+
 #ifdef __cplusplus
 }
 #endif
