a67733e1ec7c127a830190dfa276e5c5bd091270
Dave Shield <dts12@users.sourceforge.net>
2007-01-22 16:42:40 +0000

    CHANGES: snmpd: PATCH: 1641332: Include missing agent config definitions.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@15771 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/agent_trap.c
+++ net-snmp/agent/agent_trap.c
@@ -62,6 +62,7 @@
 #include <net-snmp/agent/snmp_agent.h>
 #include <net-snmp/agent/agent_callbacks.h>
 
+#include <net-snmp/agent/agent_module_config.h>
 #include <net-snmp/agent/mib_module_config.h>
 
 #ifdef USING_AGENTX_PROTOCOL_MODULE
