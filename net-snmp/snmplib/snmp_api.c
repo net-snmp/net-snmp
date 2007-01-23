3307b5e5b84df41c87d5984a91f9c6d99b44d205
Dave Shield <dts12@users.sourceforge.net>
2007-01-23 15:46:23 +0000

    CHANGES: snmplib: PATCH: 1634620: Fix leaks from create_user_from_session failure. Patch provided by Michael Kirkham.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@15776 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/snmp_api.c
+++ net-snmp/snmplib/snmp_api.c
@@ -1384,7 +1384,7 @@ snmpv3_engineID_probe(struct session_list *slp,
         if (create_user_from_session(slp->session) != SNMPERR_SUCCESS) {
             in_session->s_snmp_errno = SNMPERR_UNKNOWN_USER_NAME;       /* XX?? */
             DEBUGMSGTL(("snmp_api",
-                        "snmp_sess_open(): failed(2) to create a new user from session\n"));
+                        "snmpv3_engine_probe(): failed(2) to create a new user from session\n"));
             return 0;
         }
     }
@@ -1458,13 +1458,14 @@ _sess_open(netsnmp_session * in_session)
 
     if (!snmpv3_engineID_probe(slp, in_session)) {
         snmp_sess_close(slp);
-        return 0;
+        return NULL;
     }
     if (create_user_from_session(slp->session) != SNMPERR_SUCCESS) {
         in_session->s_snmp_errno = SNMPERR_UNKNOWN_USER_NAME;       /* XX?? */
         DEBUGMSGTL(("snmp_api",
                     "_sess_open(): failed(2) to create a new user from session\n"));
-        return 0;
+        snmp_sess_close(slp);
+        return NULL;
     }
     
     session->flags &= ~SNMP_FLAGS_DONT_PROBE;
@@ -1600,10 +1601,11 @@ snmp_sess_add_ex(netsnmp_session * in_session,
             return NULL;
         }
         if (create_user_from_session(slp->session) != SNMPERR_SUCCESS) {
-            slp->session->s_snmp_errno = SNMPERR_UNKNOWN_USER_NAME;
+            in_session->s_snmp_errno = SNMPERR_UNKNOWN_USER_NAME;
             DEBUGMSGTL(("snmp_api",
-                        "_sess_open(): failed(2) to create a new user from session\n"));
-            return 0;
+                        "snmp_sess_add(): failed(2) to create a new user from session\n"));
+            snmp_sess_close(slp);
+            return NULL;
         }
     }
 
@@ -4768,7 +4770,7 @@ _sess_async_send(void *sessp,
     if (create_user_from_session(session) != SNMPERR_SUCCESS) {
         session->s_snmp_errno = SNMPERR_UNKNOWN_USER_NAME;  /* XX?? */
         DEBUGMSGTL(("snmp_api",
-                    "snmp_sess_open(): failed(2) to create a new user from session\n"));
+                    "snmp_send(): failed(2) to create a new user from session\n"));
         return 0;
     }
 
