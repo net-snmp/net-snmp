2f0ade63feb0952d12dcf531a60226ecd3ddd119
Dave Shield <dts12@users.sourceforge.net>
2007-03-12 19:40:42 +0000

    CHANGES: snmpd: PATCHES: 1644280: Use the correct engineID when sending v3 traps.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@15940 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/agent_trap.c
+++ net-snmp/agent/agent_trap.c
@@ -889,6 +889,9 @@ send_trap_to_sess(netsnmp_session * sess, netsnmp_pdu *template_pdu)
 {
     netsnmp_pdu    *pdu;
     int            result;
+    char           tmp[SPRINT_MAX_LEN];
+    int            len;
+
 
     if (!sess || !template_pdu)
         return;
@@ -917,6 +920,14 @@ send_trap_to_sess(netsnmp_session * sess, netsnmp_pdu *template_pdu)
             snmp_async_send(sess, pdu, &handle_inform_response, NULL);
         
     } else {
+        if ((sess->version == SNMP_VERSION_3) &&
+                (pdu->command == SNMP_MSG_TRAP2) &&
+                (pdu->securityEngineIDLen == 0)) {
+            len = snmpv3_get_engineID(tmp, sizeof(tmp));
+            memdup(&pdu->securityEngineID, tmp, len);
+            pdu->securityEngineIDLen = len;
+        }
+
         result = snmp_send(sess, pdu);
     }
 
