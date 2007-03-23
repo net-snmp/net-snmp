e84ef47387365ab8eaa8e915a4d278abae9dcd88
Dave Shield <dts12@users.sourceforge.net>
2007-03-23 09:01:39 +0000

    CHANGES: snmplib: PATCH: 1653670: Consistent SNMPv3 behaviour when opening sessions.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@15985 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_api.c
+++ snmplib/snmp_api.c
@@ -1582,6 +1582,8 @@ snmp_sess_add_ex(netsnmp_session * in_session,
         }
     }
 
+    slp->session->flags &= ~SNMP_FLAGS_DONT_PROBE;
+
     return (void *) slp;
 }                               /*  end snmp_sess_add_ex()  */
 
