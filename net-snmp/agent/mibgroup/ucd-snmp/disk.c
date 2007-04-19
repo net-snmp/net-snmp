618e2e039469fa71febfcb9e9ad9ad898d08930e
Dave Shield <dts12@users.sourceforge.net>
2007-04-19 12:25:10 +0000

    CHANGES: snmpd: BUG: 1602286: Don't let table holes block later rows.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@16300 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/ucd-snmp/disk.c
+++ net-snmp/agent/mibgroup/ucd-snmp/disk.c
@@ -618,6 +618,7 @@ var_extensible_disk(struct variable *vp,
 #endif
 #endif
 
+tryAgain:
     if (header_simple_table
         (vp, name, length, exact, var_len, write_method, numdisks))
         return (NULL);
@@ -649,6 +650,8 @@ var_extensible_disk(struct variable *vp,
         snmp_log(LOG_ERR, "Couldn't open device %s\n",
                  disks[disknum].device);
         setPerrorstatus("statvfs dev/disk");
+        if (!exact)
+            goto tryAgain;
         return NULL;
     }
 #ifdef STAT_STATFS_FS_DATA
@@ -736,6 +739,8 @@ var_extensible_disk(struct variable *vp,
         snmp_log(LOG_ERR, "Couldn't open device %s\n",
                  disks[disknum].device);
         setPerrorstatus("open dev/disk");
+        if (!exact)
+            goto tryAgain;
         return (NULL);
     }
     lseek(file, (long) (SBLOCK * DEV_BSIZE), 0);
@@ -744,6 +749,8 @@ var_extensible_disk(struct variable *vp,
         snmp_log(LOG_ERR, "Error reading device %s\n",
                  disks[disknum].device);
         close(file);
+        if (!exact)
+            goto tryAgain;
         return (NULL);
     }
     close(file);
