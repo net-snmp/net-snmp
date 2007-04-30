7673452d121f0f38ccc11c9f168cb6d409397ab7
Dave Shield <dts12@users.sourceforge.net>
2007-04-30 13:06:45 +0000

    CHANGES: snmpd: PATCH: 1700732: Fix debug output statements.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@16324 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/cache_handler.c
+++ agent/helpers/cache_handler.c
@@ -536,8 +536,7 @@ _cache_load( netsnmp_cache *cache )
     if ( cache->load_cache)
         ret = cache->load_cache(cache, cache->magic);
     if (ret < 0) {
-        DEBUGMSG(("helper:cache_handler", " load failed (%d)\n",
-                  ret));
+        DEBUGMSGT(("helper:cache_handler", " load failed (%d)\n", ret));
         cache->valid = 0;
         return ret;
     }
@@ -558,8 +557,7 @@ _cache_load( netsnmp_cache *cache )
         atime_setMarker(cache->timestamp);
     else
         cache->timestamp = atime_newMarker();
-    DEBUGMSG(("helper:cache_handler", " loaded (%d)\n",
-              cache->timeout));
+    DEBUGMSGT(("helper:cache_handler", " loaded (%d)\n", cache->timeout));
 
     return ret;
 }
