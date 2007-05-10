4dc74dfe7d3787241bd41b42b644ff53214ec843
Thomas Anders <tanders@users.sourceforge.net>
2007-05-10 21:52:01 +0000

    NEWS: python: PATCH: 1716114: Let python build in source tree Patch provided by Jochen Friedrich (Debian patch #38).
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16352 06827809-a52a-0410-b366-d66718629ded

--- python/setup.py
+++ python/setup.py
@@ -3,10 +3,29 @@ from setuptools import setup, Extension, find_packages
 import os
 import re
 import string
+import sys
 
-netsnmp_libs = os.popen('net-snmp-config --libs').read()
-libdirs = re.findall(r"-L(\S+)", netsnmp_libs)
-libs = re.findall(r"-l(\S+)", netsnmp_libs)
+intree=0
+
+args = sys.argv[:]
+for arg in args:
+    if string.find(arg,'--basedir=') == 0:
+        basedir = string.split(arg,'=')[1]
+	sys.argv.remove(arg)
+        intree=1
+
+if intree:
+    netsnmp_libs = os.popen(basedir+'/net-snmp-config --libs').read()
+    libdir = os.popen(basedir+'/net-snmp-config --build-lib-dirs '+basedir).read()
+    incdir = os.popen(basedir+'/net-snmp-config --build-includes '+basedir).read()
+    libs = re.findall(r"-l(\S+)", netsnmp_libs)
+    libdirs = re.findall(r"-L(\S+)", libdir)
+    incdirs = re.findall(r"-I(\S+)", incdir)
+else:
+    netsnmp_libs = os.popen('net-snmp-config --libs').read()
+    libdirs = re.findall(r"-L(\S+)", netsnmp_libs)
+    incdirs = []
+    libs = re.findall(r"-l(\S+)", netsnmp_libs)
 
 setup(
     name="netsnmp-python", version="1.0a1",
@@ -21,6 +40,7 @@ setup(
     ext_modules = [
        Extension("netsnmp.client_intf", ["netsnmp/client_intf.c"],
                  library_dirs=libdirs,
+                 include_dirs=incdirs,
                  libraries=libs )
        ]
     )
