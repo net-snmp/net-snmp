13e128d5eb9106e0ccaf8a1c943cd913b471f13e
Thomas Anders <tanders@users.sourceforge.net>
2007-05-16 23:53:15 +0000

    CHANGES: HP-UX: HP-UX needs _REENTRANT defined to pick up strtok_r. Otherwise, at least for 64-bit code, strtok_r will not work and will make net-snmp segfault.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@16372 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/system/hpux.h
+++ net-snmp/include/net-snmp/system/hpux.h
@@ -4,6 +4,13 @@
 #define DONT_USE_NLIST 1
 #endif
 
+/* 
+ * HP-UX needs _REENTRANT defined to pick up strtok_r.
+ * Otherwise, at least for 64-bit code, strtok_r will not work 
+ * and will make net-snmp segfault.
+ */
+#define _REENTRANT 1
+
 #undef TCP_TTL_SYMBOL
 #ifndef hpux11
 #define TCP_TTL_SYMBOL "ipDefaultTTL"
