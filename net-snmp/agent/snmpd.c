d5849f027cd4b0650bd4948d8d3ee22ebcb94373
Wes Hardaker <hardaker@users.sourceforge.net>
2007-05-17 13:53:50 +0000

    CHANGES: snmpd: patch: 1225440: Apply intent of patch: to delay sighup till it is safe. CHANGES: snmptrapd: patch: 1225440: Apply intent of patch: to delay sighup till it is safe.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@16375 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/snmpd.c
+++ net-snmp/agent/snmpd.c
@@ -489,8 +489,7 @@ main(int argc, char *argv[])
     signal(SIGINT, SnmpdShutDown);
 #endif
 #ifdef SIGHUP
-    DEBUGMSGTL(("signal", "registering SIGHUP signal handler\n"));
-    signal(SIGHUP, SnmpdReconfig);
+    signal(SIGHUP, SIG_IGN);   /* do not terminate on early SIGHUP */
 #endif
 #ifdef SIGUSR1
     DEBUGMSGTL(("signal", "registering SIGUSR1 signal handler\n"));
@@ -919,6 +918,11 @@ main(int argc, char *argv[])
      * Store persistent data immediately in case we crash later.  
      */
     snmp_store("snmpd");
+
+#ifdef SIGHUP
+    DEBUGMSGTL(("signal", "registering SIGHUP signal handler\n"));
+    signal(SIGHUP, SnmpdReconfig);
+#endif
 
     /*
      * Send coldstart trap if possible.  
