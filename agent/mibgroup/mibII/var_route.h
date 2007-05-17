00d1dadce942202dad06e58753e357c271d030c3
Wes Hardaker <hardaker@users.sourceforge.net>
2007-05-17 17:46:45 +0000

    NEWS: snmpd: patch: 1708243: from nhorman: implement the ipDefaultRouteTable on linux
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16379 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII/var_route.h
+++ agent/mibgroup/mibII/var_route.h
@@ -19,6 +19,10 @@ config_arch_require(solaris2, kernel_sunos5)
 
      extern FindVarMethod var_ipRouteEntry;
 
+#ifdef linux
+     extern FindVarMethod var_ipDflRtTblEntry;
+#endif
+
 #if !defined(hpux11) && !defined(solaris2)
      RTENTRY **netsnmp_get_routes(size_t *out_numroutes);
 #endif
