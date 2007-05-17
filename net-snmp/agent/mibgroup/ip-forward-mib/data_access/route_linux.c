4d38a41b1a6ca4c34f6bfcb785a0d801e95ff5eb
Wes Hardaker <hardaker@users.sourceforge.net>
2007-05-17 21:53:28 +0000

    CHANGES: snmpd: patch: 1710632: from mavarley: fix memory leak on broken container insertions for the ipAddressTable
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@16381 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/ip-forward-mib/data_access/route_linux.c
+++ net-snmp/agent/mibgroup/ip-forward-mib/data_access/route_linux.c
@@ -92,6 +92,8 @@ _load_ipv4(netsnmp_container* container, u_long *index )
             snmp_log(LOG_ERR,
                      "/proc/net/route data format error (%d!=8), line ==|%s|",
                      rc, line);
+            
+            netsnmp_access_route_entry_free(entry);        
             continue;
         }
 
@@ -175,7 +177,12 @@ _load_ipv4(netsnmp_container* container, u_long *index )
         /*
          * insert into container
          */
-        CONTAINER_INSERT(container, entry);
+        if (CONTAINER_INSERT(container, entry) < 0)
+        {
+            DEBUGMSGTL(("access:route:container", "error with route_entry: insert into container failed.\n"));
+            netsnmp_access_route_entry_free(entry);
+            continue;
+        }
     }
 
     fclose(in);
