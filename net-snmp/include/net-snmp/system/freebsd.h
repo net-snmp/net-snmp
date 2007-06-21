e5c1584e4a03a9d65692ac1cfa76823b4b3a3285
Robert Story <rstory@users.sourceforge.net>
2007-06-21 14:01:08 +0000

    default to using ifTable rewrites
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16553 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/system/freebsd.h
+++ net-snmp/include/net-snmp/system/freebsd.h
@@ -24,6 +24,4 @@
 
 /* define the extra mib modules that are supported */
 #define NETSNMP_INCLUDE_HOST_RESOURCES
-#ifdef NETSNMP_ENABLE_MFD_REWRITES
 #define NETSNMP_INCLUDE_IFTABLE_REWRITES
-#endif
