0507c91bdf4107f84e9e2928c30a13786330e9d9
Wes Hardaker <hardaker@users.sourceforge.net>
2007-07-16 23:59:44 +0000

    Apply (correct) patch from Marcus Rueckert to remove asserts using sort-o-illegal C string compares
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-3-patches@16612 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/mibII/udp.c
+++ net-snmp/agent/mibgroup/mibII/udp.c
@@ -185,7 +185,7 @@ udp_handler(netsnmp_mib_handler          *handler,
      */
 #ifndef hpux11
     if (!netsnmp_cache_is_valid(reqinfo, reginfo->handlerName)) {
-        netsnmp_assert("cache" == "valid"); /* always false */
+        netsnmp_assert(!"cache == valid"); /* always false */
         udp_load( NULL, NULL );	/* XXX - check for failure */
     }
 #endif
