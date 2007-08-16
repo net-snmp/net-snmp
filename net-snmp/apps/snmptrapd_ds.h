58a743409f11ef0c013d40f6138b25b9a63be92b
Dave Shield <dts12@users.sourceforge.net>
2007-08-16 15:24:47 +0000

    CHANGES: snmptrapd: PATCH: 1746992: Improve snmptrapd access-control error messages.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@16661 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/apps/snmptrapd_ds.h
+++ net-snmp/apps/snmptrapd_ds.h
@@ -5,4 +5,10 @@
 #define NETSNMP_DS_APP_NUMERIC_IP       16
 #define NETSNMP_DS_APP_NO_AUTHORIZATION 17
 
+/*
+ * NB: The NETSNMP_DS_APP_NO_AUTHORIZATION definition is repeated
+ *     in the code file agent/mibgroup/mibII/vacm_conf.c
+ *     If this definition is changed, it should be updated there too.
+ */
+
 #endif /* SNMPTRAPD_DS_H */
