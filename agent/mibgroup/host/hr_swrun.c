2b751e9b6eb8131824b16ab2cfaac4f46d549dea
Dave Shield <dts12@users.sourceforge.net>
2007-08-16 12:03:44 +0000

    CHANGES: snmpd: PATCH: 1774612: More resilient process status handling
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@16655 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/host/hr_swrun.c
+++ agent/mibgroup/host/hr_swrun.c
@@ -724,8 +724,12 @@ var_hrswrun(struct variable * vp,
             sprintf(string, "/proc/%d/status", pid);
             if ((fp = fopen(string, "r")) == NULL)
                 return NULL;
-            fgets(buf, sizeof(buf), fp);        /* Name: process name */
+            cp = fgets(buf, sizeof(buf), fp);        /* Name: process name */
+            if ( cp == NULL )
+                return NULL;    /* the process probably died */
             cp = strchr(buf, ':');
+            if ( cp == NULL )
+                return NULL;    /* the process file is malformed */
             ++cp;
             while (isspace(*cp))
                 ++cp;
