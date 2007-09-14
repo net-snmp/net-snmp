9c1f901e202d369de0f4bc724cf56ecdfbf3030f
Robert Story <rstory@users.sourceforge.net>
2007-09-14 13:13:50 +0000

    Apple patches - config_require for new rewrites, if enabled
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16695 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/host.h
+++ agent/mibgroup/host.h
@@ -2,6 +2,16 @@
  *  Host Resources 'wrapper' interface
  *	calls the per-group interfaces from 'hr_*.h'
  */
+/* Portions of this file are subject to the following copyright(s).  See
+ * the Net-SNMP's COPYING file for more details and other copyrights
+ * that may apply:
+ */
+/*
+ * Portions of this file are copyrighted by:
+ * Copyright (C) 2007 Apple, Inc. All rights reserved.
+ * Use is subject to license terms specified in the COPYING file
+ * distributed with the Net-SNMP package.
+ */
 
 	config_require(host/hr_system)
 	config_require(host/hr_storage)
@@ -13,8 +23,17 @@
 	config_require(host/hr_disk)
 	config_require(host/hr_partition)
 	config_require(host/hr_filesys)
-	config_require(host/hr_swrun)
+#ifndef NETSNMP_INCLUDE_HRSWINST_REWRITES
 	config_require(host/hr_swinst)
+#else
+	config_require(host/hrSWInstalledTable)
+#endif
+#ifndef NETSNMP_INCLUDE_HRSWRUN_REWRITES
+	config_require(host/hr_swrun)
+#else
+	config_require(host/hrSWRunTable)
+	config_require(host/hrSWRunPerfTable)
+#endif
 
 /* add the host resources mib to the default mibs to load */
 config_add_mib(HOST-RESOURCES-MIB)
