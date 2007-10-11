fdb730b2ed637ba61b8e54370b80db1928c47d13
Magnus Fromreide <magfr@users.sourceforge.net>
2007-10-11 20:46:30 +0000

    CHANGES: snmplib: Do not leak memory whenever an udp address is formatted.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@16716 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmpUDPDomain.c
+++ snmplib/snmpUDPDomain.c
@@ -105,11 +105,11 @@ netsnmp_udp_fmtaddr(netsnmp_transport *t, void *data, int len)
         to = (struct sockaddr_in *) &(addr_pair->remote_addr);
         if (to == NULL) {
             sprintf(tmp, "UDP: [%s]->unknown",
-                strdup(inet_ntoa(addr_pair->local_addr)));
+                    inet_ntoa(addr_pair->local_addr));
         } else {
-            sprintf(tmp, "UDP: [%s]->[%s]:%hd",
-                strdup(inet_ntoa(addr_pair->local_addr)),
-                inet_ntoa(to->sin_addr), ntohs(to->sin_port));
+            sprintf(tmp, "UDP: [%s]->", inet_ntoa(addr_pair->local_addr));
+            sprintf(tmp + strlen(tmp), "[%s]:%hd",
+                    inet_ntoa(to->sin_addr), ntohs(to->sin_port));
         }
         return strdup(tmp);
     }
