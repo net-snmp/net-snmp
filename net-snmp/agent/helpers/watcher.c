8ac5bb389678884eb21e8e075259450c2c6eb4ef
Magnus Fromreide <magfr@users.sourceforge.net>
2007-10-16 07:17:51 +0000

    CHANGES: misc: avoid calling debug macros with too many arguments.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16728 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/helpers/watcher.c
+++ net-snmp/agent/helpers/watcher.c
@@ -87,7 +87,7 @@ netsnmp_watcher_helper_handler(netsnmp_mib_handler *handler,
                            requests->requestvb->name_length,
                            reginfo->rootoid, reginfo->rootoid_len);
 
-    DEBUGMSGTL(( "helper:watcher", "  oid:", cmp));
+    DEBUGMSGTL(( "helper:watcher", "  oid:"));
     DEBUGMSGOID(("helper:watcher", requests->requestvb->name,
                                    requests->requestvb->name_length));
     DEBUGMSG((   "helper:watcher", "\n"));
@@ -225,7 +225,7 @@ netsnmp_watched_timestamp_handler(netsnmp_mib_handler *handler,
                            requests->requestvb->name_length,
                            reginfo->rootoid, reginfo->rootoid_len);
 
-    DEBUGMSGTL(( "helper:watcher:timestamp", "  oid:", cmp));
+    DEBUGMSGTL(( "helper:watcher:timestamp", "  oid:"));
     DEBUGMSGOID(("helper:watcher:timestamp", requests->requestvb->name,
                                    requests->requestvb->name_length));
     DEBUGMSG((   "helper:watcher:timestamp", "\n"));
@@ -313,7 +313,7 @@ netsnmp_watched_spinlock_handler(netsnmp_mib_handler *handler,
                            requests->requestvb->name_length,
                            reginfo->rootoid, reginfo->rootoid_len);
 
-    DEBUGMSGTL(( "helper:watcher:spinlock", "  oid:", cmp));
+    DEBUGMSGTL(( "helper:watcher:spinlock", "  oid:"));
     DEBUGMSGOID(("helper:watcher:spinlock", requests->requestvb->name,
                                    requests->requestvb->name_length));
     DEBUGMSG((   "helper:watcher:spinlock", "\n"));
