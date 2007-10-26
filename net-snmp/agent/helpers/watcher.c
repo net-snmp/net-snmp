0a6ad2661105401ca0264da134de686d73ae0b4f
Magnus Fromreide <magfr@users.sourceforge.net>
2007-10-26 21:22:54 +0000

    NEWS: snmpd: Added support for a user provided length variable and C string values to the watcher helper.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16735 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/helpers/watcher.c
+++ net-snmp/agent/helpers/watcher.c
@@ -8,6 +8,8 @@
 #include <net-snmp/agent/instance.h>
 #include <net-snmp/agent/scalar.h>
 
+#include <string.h>
+
 /** @defgroup watcher watcher
  *  Watch a specified variable and process it as an instance or scalar object
  *  @ingroup leaf
@@ -27,21 +29,30 @@ netsnmp_get_watcher_handler(void)
 }
 
 netsnmp_watcher_info *
-netsnmp_create_watcher_info(void *data, size_t size, u_char type, int flags)
+netsnmp_create_watcher_info6(void *data, size_t size, u_char type,
+                             int flags, size_t max_size, size_t* size_p)
 {
     netsnmp_watcher_info *winfo = SNMP_MALLOC_TYPEDEF(netsnmp_watcher_info);
 
     if (winfo != NULL) {
         winfo->data = data;
         winfo->data_size = size;
-        winfo->max_size = size; /* Probably wrong for non-fixed size data */
+        winfo->max_size = max_size;
         winfo->type = type;
-        if (flags)
-            winfo->flags = flags;
-        else
-            winfo->flags = WATCHER_FIXED_SIZE;
+        winfo->flags = flags;
+        winfo->data_size_p = size_p;
     }
     return winfo;
+}
+
+netsnmp_watcher_info *
+netsnmp_create_watcher_info(void *data, size_t size, u_char type, int flags)
+{
+    return netsnmp_create_watcher_info6(
+        data, size,
+        type, (flags ? flags : WATCHER_FIXED_SIZE),
+        size,  /* Probably wrong for non-fixed size data */
+        NULL);
 }
 
 int
@@ -70,7 +81,49 @@ netsnmp_register_watched_scalar(netsnmp_handler_registration *reginfo,
     return netsnmp_register_scalar(reginfo);
 }
 
+/** @cond */
 
+NETSNMP_STATIC_INLINE size_t
+get_data_size(const netsnmp_watcher_info* winfo)
+{
+    if (winfo->flags & WATCHER_SIZE_STRLEN)
+        return strlen((const char*)winfo->data);
+    else if (winfo->flags & WATCHER_SIZE_IS_PTR)
+        return *winfo->data_size_p;
+    else
+        return winfo->data_size;
+}
+
+NETSNMP_STATIC_INLINE void
+set_data(netsnmp_watcher_info* winfo, void* data, size_t size)
+{
+    memcpy(winfo->data, data, size);
+    if (winfo->flags & WATCHER_SIZE_STRLEN)
+        ((char*)winfo->data)[size] = '\0';
+    else if (winfo->flags & WATCHER_SIZE_IS_PTR)
+        *winfo->data_size_p = size;
+    else
+        winfo->data_size = size;
+}
+
+typedef struct {
+    size_t size;
+    char data[1];
+} netsnmp_watcher_cache;
+
+NETSNMP_STATIC_INLINE netsnmp_watcher_cache*
+netsnmp_watcher_cache_create(const void* data, size_t size)
+{
+    netsnmp_watcher_cache *res =
+        malloc(sizeof(netsnmp_watcher_cache) + size - 1);
+    if (res) {
+        res->size = size;
+        memcpy(res->data, data, size);
+    }
+    return res;
+}
+
+/** @endcond */
 
 int
 netsnmp_watcher_helper_handler(netsnmp_mib_handler *handler,
@@ -78,9 +131,9 @@ netsnmp_watcher_helper_handler(netsnmp_mib_handler *handler,
                                netsnmp_agent_request_info *reqinfo,
                                netsnmp_request_info *requests)
 {
-    netsnmp_watcher_info *winfo = (netsnmp_watcher_info *) handler->myvoid;
-    u_char              *old_data;
-    int                  cmp;
+    netsnmp_watcher_info  *winfo = (netsnmp_watcher_info *) handler->myvoid;
+    netsnmp_watcher_cache *old_data;
+    int                    cmp;
 
     DEBUGMSGTL(("helper:watcher", "Got request:  %d\n", reqinfo->mode));
     cmp = snmp_oid_compare(requests->requestvb->name,
@@ -102,7 +155,7 @@ netsnmp_watcher_helper_handler(netsnmp_mib_handler *handler,
         snmp_set_var_typed_value(requests->requestvb,
                                  winfo->type,
                                  winfo->data,
-                                 winfo->data_size);
+                                 get_data_size(winfo));
         break;
 
         /*
@@ -110,30 +163,31 @@ netsnmp_watcher_helper_handler(netsnmp_mib_handler *handler,
          */
     case MODE_SET_RESERVE1:
         if (requests->requestvb->type != winfo->type)
-            netsnmp_set_request_error(reqinfo, requests,
-                                      SNMP_ERR_WRONGTYPE);
-
-        if (((winfo->flags & WATCHER_MAX_SIZE) &&
-               requests->requestvb->val_len >  winfo->max_size) ||
+            netsnmp_set_request_error(reqinfo, requests, SNMP_ERR_WRONGTYPE);
+        else if (((winfo->flags & WATCHER_MAX_SIZE) &&
+                     requests->requestvb->val_len > winfo->max_size) ||
             ((winfo->flags & WATCHER_FIXED_SIZE) &&
-               requests->requestvb->val_len != winfo->data_size))
-             netsnmp_set_request_error(reqinfo, requests,
-                                      SNMP_ERR_WRONGLENGTH);
+                requests->requestvb->val_len != get_data_size(winfo)))
+            netsnmp_set_request_error(reqinfo, requests, SNMP_ERR_WRONGLENGTH);
+        else if ((winfo->flags & WATCHER_SIZE_STRLEN) &&
+            (memchr(requests->requestvb->val.string, '\0',
+                requests->requestvb->val_len) != NULL))
+            netsnmp_set_request_error(reqinfo, requests, SNMP_ERR_WRONGVALUE);
         break;
 
     case MODE_SET_RESERVE2:
         /*
          * store old info for undo later 
          */
-        memdup(&old_data, (u_char *) winfo->data, winfo->data_size);
-        if (old_data == NULL) {
+        old_data =
+            netsnmp_watcher_cache_create(winfo->data, get_data_size(winfo));
+        if (old_data == NULL)
             netsnmp_set_request_error(reqinfo, requests,
                                       SNMP_ERR_RESOURCEUNAVAILABLE);
-            return SNMP_ERR_NOERROR;
-        }
-        netsnmp_request_add_list_data(requests,
-                                      netsnmp_create_data_list
-                                      ("watcher", old_data, free));
+        else
+            netsnmp_request_add_list_data(requests,
+                                          netsnmp_create_data_list
+                                          ("watcher", old_data, free));
         break;
 
     case MODE_SET_FREE:
@@ -146,18 +200,16 @@ netsnmp_watcher_helper_handler(netsnmp_mib_handler *handler,
         /*
          * update current 
          */
-        memcpy(winfo->data, (void *)requests->requestvb->val.string,
-                                    requests->requestvb->val_len);
+        set_data(winfo, (void *)requests->requestvb->val.string,
+                                requests->requestvb->val_len);
         break;
 
     case MODE_SET_UNDO:
-        memcpy(winfo->data,
-               netsnmp_request_get_list_data(requests, "watcher"),
-               winfo->data_size);
+        old_data = netsnmp_request_get_list_data(requests, "watcher");
+        set_data(winfo, old_data->data, old_data->size);
         break;
 
     case MODE_SET_COMMIT:
-        winfo->data_size = requests->requestvb->val_len;
         break;
 
     }
--- net-snmp/include/net-snmp/agent/watcher.h
+++ net-snmp/include/net-snmp/agent/watcher.h
@@ -8,6 +8,10 @@
 extern "C" {
 #endif
 
+/** @ingroup watcher
+ *  @{
+ */
+
 /*
  * if handler flag has this bit set, the timestamp will be
  * treated as a pointer to the timestamp. If this bit is
@@ -16,8 +20,29 @@ extern "C" {
  */
 #define NETSNMP_WATCHER_DIRECT MIB_HANDLER_CUSTOM1
 
+/** The size of the watched object is constant.
+ *  @hideinitializer
+ */
 #define WATCHER_FIXED_SIZE    0x01
+/** The maximum size of the watched object is stored in max_size.
+ *  If WATCHER_SIZE_STRLEN is set then it is supposed that max_size + 1
+ *  bytes could be stored in the buffer.
+ *  @hideinitializer
+ */
 #define WATCHER_MAX_SIZE      0x02
+/** If set then the variable data_size_p points to is supposed to hold the
+ *  current size of the watched object and will be updated on writes.
+ *  @hideinitializer
+ *  @since Net-SNMP 5.5
+ */
+#define WATCHER_SIZE_IS_PTR   0x04
+/** If set then data is suppposed to be a zero-terminated character array
+ *  and both data_size and data_size_p are ignored. Additionally \\0 is a
+ *  forbidden character in the data set.
+ *  @hideinitializer
+ *  @since Net-SNMP 5.5
+ */
+#define WATCHER_SIZE_STRLEN   0x08
 
 typedef struct netsnmp_watcher_info_s {
     void     *data;
@@ -25,7 +50,10 @@ typedef struct netsnmp_watcher_info_s {
     size_t    max_size;
     u_char    type;
     int       flags;
+    size_t   *data_size_p;
 } netsnmp_watcher_info;
+
+/** @} */
 
 int netsnmp_register_watched_instance( netsnmp_handler_registration *reginfo,
                                        netsnmp_watcher_info         *winfo);
@@ -76,6 +104,10 @@ int netsnmp_register_read_only_counter32_scalar(const char *name,
 
 netsnmp_mib_handler  *netsnmp_get_watcher_handler(void);
 netsnmp_watcher_info *netsnmp_create_watcher_info(void *, size_t, u_char, int);
+
+netsnmp_watcher_info *
+netsnmp_create_watcher_info6(void *, size_t, u_char, int, size_t, size_t*);
+
 Netsnmp_Node_Handler  netsnmp_watcher_helper_handler;
 
 netsnmp_mib_handler  *netsnmp_get_watched_timestamp_handler(void);
