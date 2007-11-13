acaa6c245237d04f6cbb7a13ff84df3252071606
Dave Shield <dts12@users.sourceforge.net>
2007-11-13 13:59:01 +0000

    Preliminary re-working of the LM-SENSORS-MIB, to separate out the sensor hardware processing from the MIB tables themselves.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@16737 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/mibgroup/hardware/sensors.h
@@ -0,0 +1,13 @@
+config_require(hardware/sensors/hw_sensors)
+
+#if defined(solaris)
+# if defined(HAVE_PICL_H)
+config_require(hardware/sensors/picld_sensors)
+# else
+config_require(hardware/sensors/kstat_sensors)
+# endif
+#else
+config_require(hardware/sensors/lmsensors_v2)
+#endif
+
+//config_require(hardware/sensors/dummy_sensors)
