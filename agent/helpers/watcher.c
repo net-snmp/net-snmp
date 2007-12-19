76b9dab9a74d51c3f0dcc5dd22f29b97cc122822
Magnus Fromreide <magfr@users.sourceforge.net>
2007-12-19 20:46:20 +0000

    CHANGES: BUG: 1851047: Do not try to do more handling of a value once it is found to be corrupt by the watcher helper, thus avoiding code that counts on getting the right type. This bug was found by Charlie Miller, Independent Security Evaluators.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-3-patches@16754 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/watcher.c
+++ agent/helpers/watcher.c
@@ -114,16 +114,20 @@ netsnmp_watcher_helper_handler(netsnmp_mib_handler *handler,
          * SET requests.  Should only get here if registered RWRITE 
          */
     case MODE_SET_RESERVE1:
-        if (requests->requestvb->type != winfo->type)
+        if (requests->requestvb->type != winfo->type) {
             netsnmp_set_request_error(reqinfo, requests,
                                       SNMP_ERR_WRONGTYPE);
+            handler->flags |= MIB_HANDLER_AUTO_NEXT_OVERRIDE_ONCE;
+        }
 
         if (((winfo->flags & WATCHER_MAX_SIZE) &&
                requests->requestvb->val_len >  winfo->max_size) ||
             ((winfo->flags & WATCHER_FIXED_SIZE) &&
-               requests->requestvb->val_len != winfo->data_size))
+               requests->requestvb->val_len != winfo->data_size)) {
              netsnmp_set_request_error(reqinfo, requests,
                                       SNMP_ERR_WRONGLENGTH);
+             handler->flags |= MIB_HANDLER_AUTO_NEXT_OVERRIDE_ONCE;
+        }
         break;
 
     case MODE_SET_RESERVE2:
@@ -134,6 +138,7 @@ netsnmp_watcher_helper_handler(netsnmp_mib_handler *handler,
         if (old_data == NULL) {
             netsnmp_set_request_error(reqinfo, requests,
                                       SNMP_ERR_RESOURCEUNAVAILABLE);
+            handler->flags |= MIB_HANDLER_AUTO_NEXT_OVERRIDE_ONCE;
             return SNMP_ERR_NOERROR;
         }
         netsnmp_request_add_list_data(requests,
@@ -184,7 +189,7 @@ netsnmp_get_watched_timestamp_handler(void)
 {
     netsnmp_mib_handler *ret = NULL;
     
-    ret = netsnmp_create_handler("watcher",
+    ret = netsnmp_create_handler("watcher-timestamp",
                                  netsnmp_watched_timestamp_handler);
     if (ret) {
         ret->flags |= MIB_HANDLER_AUTO_NEXT;
@@ -259,6 +264,7 @@ netsnmp_watched_timestamp_handler(netsnmp_mib_handler *handler,
     case MODE_SET_RESERVE1:
         netsnmp_set_request_error(reqinfo, requests,
                                   SNMP_ERR_NOTWRITABLE);
+        handler->flags |= MIB_HANDLER_AUTO_NEXT_OVERRIDE_ONCE;
         return SNMP_ERR_NOTWRITABLE;
     }
 
@@ -278,7 +284,7 @@ netsnmp_get_watched_spinlock_handler(void)
 {
     netsnmp_mib_handler *ret = NULL;
     
-    ret = netsnmp_create_handler("watcher",
+    ret = netsnmp_create_handler("watcher-spinlock",
                                  netsnmp_watched_spinlock_handler);
     if (ret) {
         ret->flags |= MIB_HANDLER_AUTO_NEXT;
@@ -333,6 +339,7 @@ netsnmp_watched_spinlock_handler(netsnmp_mib_handler *handler,
         for (request=requests; request; request=request->next) {
             if (*request->requestvb->val.integer != *spinlock) {
                 netsnmp_set_request_error(reqinfo, requests, SNMP_ERR_WRONGVALUE);
+                handler->flags |= MIB_HANDLER_AUTO_NEXT_OVERRIDE_ONCE;
                 return SNMP_ERR_WRONGVALUE;
 
             }
