9aecc3e98521f5deff6c3187a0c69d1cc21da5e7
Magnus Fromreide <magfr@users.sourceforge.net>
2008-01-06 13:01:49 +0000

    Make error_string private and readonly
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@16771 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/snmp_client.c
+++ net-snmp/snmplib/snmp_client.c
@@ -1101,31 +1101,31 @@ snmp_sess_synch_response(void *sessp,
 }
 
 
-const char     *error_string[19] = {
-    "(noError) No Error",
-    "(tooBig) Response message would have been too large.",
-    "(noSuchName) There is no such variable name in this MIB.",
-    "(badValue) The value given has the wrong type or length.",
-    "(readOnly) The two parties used do not have access to use the specified SNMP PDU.",
-    "(genError) A general failure occured",
-    "noAccess",
-    "wrongType (The set datatype does not match the data type the agent expects)",
-    "wrongLength (The set value has an illegal length from what the agent expects)",
-    "wrongEncoding",
-    "wrongValue (The set value is illegal or unsupported in some way)",
-    "noCreation (That table does not support row creation or that object can not ever be created)",
-    "inconsistentValue (The set value is illegal or unsupported in some way)",
-    "resourceUnavailable (This is likely a out-of-memory failure within the agent)",
-    "commitFailed",
-    "undoFailed",
-    "authorizationError (access denied to that object)",
-    "notWritable (That object does not support modification)",
-    "inconsistentName (That object can not currently be created)"
-};
-
 const char     *
 snmp_errstring(int errstat)
 {
+    const char * const error_string[19] = {
+        "(noError) No Error",
+        "(tooBig) Response message would have been too large.",
+        "(noSuchName) There is no such variable name in this MIB.",
+        "(badValue) The value given has the wrong type or length.",
+        "(readOnly) The two parties used do not have access to use the specified SNMP PDU.",
+        "(genError) A general failure occured",
+        "noAccess",
+        "wrongType (The set datatype does not match the data type the agent expects)",
+        "wrongLength (The set value has an illegal length from what the agent expects)",
+        "wrongEncoding",
+        "wrongValue (The set value is illegal or unsupported in some way)",
+        "noCreation (That table does not support row creation or that object can not ever be created)",
+        "inconsistentValue (The set value is illegal or unsupported in some way)",
+        "resourceUnavailable (This is likely a out-of-memory failure within the agent)",
+        "commitFailed",
+        "undoFailed",
+        "authorizationError (access denied to that object)",
+        "notWritable (That object does not support modification)",
+        "inconsistentName (That object can not currently be created)"
+    };
+
     if (errstat <= MAX_SNMP_ERR && errstat >= SNMP_ERR_NOERROR) {
         return error_string[errstat];
     } else {
