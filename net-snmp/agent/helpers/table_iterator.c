9c10d842eda8dfc0481b5254929d9b4a20b341e6
Wes Hardaker <hardaker@users.sourceforge.net>
2008-03-21 16:35:59 +0000

    NEWS: snmpd: PATCH: 1909813: fix table_iterator hint to get_first when SORTED flag is set
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16852 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/helpers/table_iterator.c
+++ net-snmp/agent/helpers/table_iterator.c
@@ -395,6 +395,7 @@ netsnmp_table_iterator_helper_handler(netsnmp_mib_handler *handler,
     int             oldmode = 0;
     netsnmp_iterator_info *iinfo;
     int notdone;
+    int hintok = 0;
     netsnmp_request_info *request, *reqtmp = NULL;
     netsnmp_variable_list *index_search = NULL;
     netsnmp_variable_list *free_this_index_search = NULL;
@@ -514,6 +515,7 @@ netsnmp_table_iterator_helper_handler(netsnmp_mib_handler *handler,
           if (!request->processed)
             request_count++;
         notdone = 1;
+        hintok = 1;
         while(notdone) {
             notdone = 0;
 
@@ -555,7 +557,7 @@ netsnmp_table_iterator_helper_handler(netsnmp_mib_handler *handler,
             }
 
             /* if sorted, pass in a hint */
-            if (iinfo->flags & NETSNMP_ITERATOR_FLAG_SORTED) {
+            if (hintok && (iinfo->flags & NETSNMP_ITERATOR_FLAG_SORTED)) {
                 callback_loop_context = table_info;
             }
             index_search =
@@ -772,7 +774,8 @@ netsnmp_table_iterator_helper_handler(netsnmp_mib_handler *handler,
                             break;
                         } else {
                           table_info->colnum = nc;
-                            notdone = 1;
+                          hintok = 0;
+                          notdone = 1;
                         }
                     }
                 }
