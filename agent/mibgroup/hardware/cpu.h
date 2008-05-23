b32581a8c69af560564b7817ccab27aab8f69d7a
Dave Shield <dts12@users.sourceforge.net>
2008-05-23 20:27:43 +0000

    CHANGES: snmpd: PATCH: Fix for CPU stats on FreeBSD Provided by Maxim Sobolev
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@16959 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/hardware/cpu.h
+++ agent/mibgroup/hardware/cpu.h
@@ -12,10 +12,10 @@ config_require(hardware/cpu/cpu_pcp)
 #elif defined(irix6)
 config_require(hardware/cpu/cpu_sysinfo)
 
-#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2)|| defined(openbsd3) || defined(openbsd4))
+#elif (defined(netbsd) || defined(netbsd1) || defined(netbsdelf) || defined(netbsdelf2)|| defined(netbsdelf3) || defined(openbsd2) || defined(openbsd3) || defined(openbsd4) || defined(freebsd4)  || defined(freebsd5) || defined(freebsd6))
 config_require(hardware/cpu/cpu_sysctl)
 
-#elif (defined(freebsd2) || defined(freebsd3) || defined(freebsd4)  || defined(freebsd5)|| defined(freebsd6))
+#elif (defined(freebsd2) || defined(freebsd3))
 config_require(hardware/cpu/cpu_nlist)
 
 #elif (defined(aix4) || defined(aix5))
