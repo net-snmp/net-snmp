f8a329d0a0a8c5b53a618c1132b096ba45a5906d
Dave Shield <dts12@users.sourceforge.net>
2008-05-27 21:22:12 +0000

    CHANGES: snmplib: BUG: 1958041: Ensure IDs are not truncated.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@16971 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_api.c
+++ snmplib/snmp_api.c
@@ -419,7 +419,7 @@ snmp_get_next_reqid(void)
     if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_16BIT_IDS))
         return (retVal & 0x7fff);	/* mask to 15 bits */
     else
-        return retVal;
+        return (retVal & 0x7fffffff);	/* mask to 31 bits */
 }
 
 long
@@ -435,7 +435,7 @@ snmp_get_next_msgid(void)
     if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_16BIT_IDS))
         return (retVal & 0x7fff);	/* mask to 15 bits */
     else
-        return retVal;
+        return (retVal & 0x7fffffff);	/* mask to 31 bits */
 }
 
 long
@@ -451,7 +451,7 @@ snmp_get_next_sessid(void)
     if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_16BIT_IDS))
         return (retVal & 0x7fff);	/* mask to 15 bits */
     else
-        return retVal;
+        return (retVal & 0x7fffffff);	/* mask to 31 bits */
 }
 
 long
@@ -467,7 +467,7 @@ snmp_get_next_transid(void)
     if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_16BIT_IDS))
         return (retVal & 0x7fff);	/* mask to 15 bits */
     else
-        return retVal;
+        return (retVal & 0x7fffffff);	/* mask to 31 bits */
 }
 
 void
