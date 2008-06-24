6bd0dea74eac60681c93e981f40e8eb77efb8fa4
Wes Hardaker <hardaker@users.sourceforge.net>
2008-06-24 14:32:13 +0000

    Version number update
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@17090 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/snmp_version.c
+++ net-snmp/snmplib/snmp_version.c
@@ -5,7 +5,7 @@
 #ifndef UCD_COMPATIBLE
 static
 #endif
-const char     *NetSnmpVersionInfo = "5.2.5.rc1";
+const char     *NetSnmpVersionInfo = "5.2.5.rc2";
 
 const char     *
 netsnmp_get_version()
