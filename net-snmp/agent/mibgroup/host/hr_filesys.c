5e8cd65414a1afe02fb9e5e9ed6bf92f03b50ab2
Dave Shield <dts12@users.sourceforge.net>
2008-07-30 09:58:30 +0000

    CHANGES: agent: BUG: 1557372: Realign hrFSStorageIndex with hrStorageTable
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@17138 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/host/hr_filesys.c
+++ net-snmp/agent/mibgroup/host/hr_filesys.c
@@ -4,6 +4,9 @@
  */
 
 #include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/agent/hardware/memory.h>
 #include "host_res.h"
 #include "hr_filesys.h"
 #include "hr_storage.h"
@@ -541,7 +544,7 @@ var_hrfilesys(struct variable *vp,
             long_return = 2;    /* others probably aren't */
         return (u_char *) & long_return;
     case HRFSYS_STOREIDX:
-        long_return = fsys_idx + HRS_TYPE_FIXED_MAX;
+        long_return = fsys_idx + NETSNMP_MEM_TYPE_MAX;
         return (u_char *) & long_return;
     case HRFSYS_FULLDUMP:
         return when_dumped(HRFS_entry->HRFS_name, FULL_DUMP, var_len);
