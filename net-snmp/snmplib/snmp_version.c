bf707ab960e3fcc7f0e6f4ada24fdb26a455283c
Dave Shield <dts12@users.sourceforge.net>
2008-07-31 16:48:10 +0000

    - version tag ( 5.4.2.rc1 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@17145 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/snmp_version.c
+++ net-snmp/snmplib/snmp_version.c
@@ -5,7 +5,7 @@
 #ifndef UCD_COMPATIBLE
 static
 #endif
-const char     *NetSnmpVersionInfo = "5.4.2.pre2";
+const char     *NetSnmpVersionInfo = "5.4.2.rc1";
 
 const char     *
 netsnmp_get_version()
