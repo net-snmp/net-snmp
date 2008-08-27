57243faaa2615e19345c15efb165ad1086316f0b
Jan Safranek <jsafranek@users.sourceforge.net>
2008-08-27 13:39:38 +0000

    S/390 uses slightly different format of the /proc/cpuinfo file: processor 0: version = FF,  identification = 060AFE,  machine = 2094
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-3-patches@17191 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/hardware/cpu/cpu_linux.c
+++ net-snmp/agent/mibgroup/hardware/cpu/cpu_linux.c
@@ -45,6 +45,16 @@ void init_cpu_linux( void ) {
             strcat( cpu->descr, "An S/390 CPU" );
 #endif
         }
+#if defined(__s390__) || defined(__s390x__)
+        /* s390 may have different format of CPU_FILE */
+        else { 
+            if (sscanf( buf, "processor %d:", &i ) == 1)  {
+                cpu = netsnmp_cpu_get_byIdx( i, 1 );
+                sprintf( cpu->name, "cpu%d", i );
+                strcat( cpu->descr, "An S/390 CPU" );
+            }
+        }
+#endif
 
 #ifdef DESCR2_FIELD
         if (!strncmp( buf, DESCR_FIELD, strlen(DESCR_FIELD))) {
