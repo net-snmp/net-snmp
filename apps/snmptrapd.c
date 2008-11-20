3bf0bb38bf6c275d1aa7ef45d116d9e2ff02fd94
Magnus Fromreide <magfr@users.sourceforge.net>
2008-11-20 20:46:54 +0000

    CHANGES: Do not use %m in printf-formatted strings since %m in printf is a glibc extension.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-3-patches@17305 06827809-a52a-0410-b366-d66718629ded

--- apps/snmptrapd.c
+++ apps/snmptrapd.c
@@ -1202,7 +1202,7 @@ main(int argc, char *argv[])
                  */
                 snmptrapd_close_sessions(sess_list);
                 netsnmp_transport_free(transport);
-                snmp_log(LOG_ERR, "couldn't open snmp - %m");
+                snmp_log(LOG_ERR, "couldn't open snmp - %s", strerror(errno));
                 SOCK_CLEANUP;
                 exit(1);
             } else {
