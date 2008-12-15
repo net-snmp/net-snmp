bca86b7e166f242daa73ff947d31d4e1e1159644
Wes Hardaker <hardaker@users.sourceforge.net>
2008-12-15 23:25:34 +0000

    Patch to change a typedef name from link to nodelink to avoid naming conflict issues.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-2-patches@17322 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/disman/expression/expValueTable.c
+++ net-snmp/agent/mibgroup/disman/expression/expValueTable.c
@@ -63,9 +63,9 @@ struct s_node {
     unsigned        data;
     struct s_node  *next;
 };
-typedef struct s_node link;
-link           *operater = NULL;
-link           *operand = NULL;
+typedef struct s_node nodelink;
+nodelink           *operater = NULL;
+nodelink           *operand = NULL;
 
 /*
  * variable2 expObjectTable_variables:
@@ -731,10 +731,10 @@ var_expValueTable(struct variable *vp,
 
 
 void
-push(link ** stack, unsigned long value)
+push(nodelink ** stack, unsigned long value)
 {
-    link           *newnode;
-    newnode = (link *) malloc(sizeof(link));
+    nodelink           *newnode;
+    newnode = (nodelink *) malloc(sizeof(nodelink));
     if (!newnode) {
         printf("\nMemory allocation failure!");
         return;
@@ -745,10 +745,10 @@ push(link ** stack, unsigned long value)
 }
 
 unsigned long
-pop(link ** stack)
+pop(nodelink ** stack)
 {
     unsigned long   value;
-    link           *top;
+    nodelink           *top;
     top = *stack;
     *stack = (*stack)->next;
     value = top->data;
