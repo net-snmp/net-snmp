01d8725fb0d49393124c85b285bb81d1c589ed38
Wes Hardaker <hardaker@users.sourceforge.net>
2008-12-31 18:01:26 +0000

    an openssl initialization wrapper
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@17328 06827809-a52a-0410-b366-d66718629ded

---
+++ include/net-snmp/library/snmp_openssl.h
@@ -0,0 +1,17 @@
+/*
+ * Header file for the OpenSSL Wrapper
+ */
+
+#ifndef SNMP_OPENSSL_H
+#define SNMP_OPENSSL_H
+
+#ifdef __cplusplus
+extern          "C" {
+#endif
+
+    void netsnmp_init_openssl(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif                          /* SNMP_OPENSSL_H */
