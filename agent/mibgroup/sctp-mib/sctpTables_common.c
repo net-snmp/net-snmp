955f06a859afda78b8744ebd7aedeb3f9bbe9605
Jan Safranek <jsafranek@users.sourceforge.net>
2009-02-18 14:41:29 +0000

    Configure script does not like net-snmp-config.h being included from header files, which are added by --with-mib-modules=, let's remove it from there.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@17390 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/sctp-mib/sctpTables_common.c
+++ agent/mibgroup/sctp-mib/sctpTables_common.c
@@ -1,3 +1,7 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+
 #include "sctpTables_common.h"
 #include "sctpAssocTable.h"
 #include "sctpAssocRemAddrTable.h"
