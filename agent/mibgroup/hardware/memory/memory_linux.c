fd987e6538a8b944d50a8ecda2431cef64cd9b2d
Dave Shield <dts12@users.sourceforge.net>
2009-03-18 10:31:05 +0000

    memory_linux: avoid reading past end of buffer. Patch provided by Stephen Hemminger
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk/net-snmp@17420 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/hardware/memory/memory_linux.c
+++ agent/mibgroup/hardware/memory/memory_linux.c
@@ -40,14 +40,14 @@ int netsnmp_mem_arch_load( netsnmp_cache *cache, void *magic ) {
     }
     if (bsize == 0) {
         bsize = MEMINFO_INIT_SIZE;
-        buff = malloc(bsize);
+        buff = malloc(bsize+1);
         if (NULL == buff) {
             snmp_log(LOG_ERR, "malloc failed\n");
             return -1;
         }
     }
     while ((bytes_read = read(statfd, buff, bsize)) == bsize) {
-        b = realloc(buff, bsize + MEMINFO_STEP_SIZE);
+        b = realloc(buff, bsize + MEMINFO_STEP_SIZE + 1);
         if (NULL == b) {
             snmp_log(LOG_ERR, "malloc failed\n");
             return -1;
@@ -65,6 +65,8 @@ int netsnmp_mem_arch_load( netsnmp_cache *cache, void *magic ) {
     close(statfd);
     if (bytes_read <= 0) {
         snmp_log_perror(MEMINFO_FILE);
+    else {
+        buff[bytes_read] = '\0';
     }
 
     /*
