2853abc1783b64a252136e57826529b65a7b04be
Magnus Fromreide <magfr@users.sourceforge.net>
2009-04-06 18:45:41 +0000

    snmpd,perl: If my_perl is NULL when passed to perl_destruct then that function crashes for some perl versions so check that it is non-null before destroying it.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@17462 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/snmp_perl.c
+++ net-snmp/agent/snmp_perl.c
@@ -141,7 +141,8 @@ void
 shutdown_perl(void)
 {
     if (netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID, 
-			       NETSNMP_DS_AGENT_DISABLE_PERL)) {
+			       NETSNMP_DS_AGENT_DISABLE_PERL) ||
+        my_perl == NULL) {
         return;
     }
     DEBUGMSGTL(("perl", "shutting down perl\n"));
