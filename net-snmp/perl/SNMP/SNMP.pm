d47811df70a2f878c550f12b59891ef8629bf1a5
Dave Shield <dts12@users.sourceforge.net>
2009-10-29 16:05:23 +0000

    - version tag ( 5.4.3.pre1 )
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@17806 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/perl/SNMP/SNMP.pm
+++ net-snmp/perl/SNMP/SNMP.pm
@@ -7,7 +7,7 @@
 #     modify it under the same terms as Perl itself.
 
 package SNMP;
-$VERSION = '5.0402';   # current release version number
+$VERSION = '5.0403';   # current release version number
 
 use strict;
 use warnings;
