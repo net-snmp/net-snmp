630f515c76bcfcf62fea5c1ed2d677b421141982
Bart Van Assche <bvassche@users.sourceforge.net>
2009-12-08 15:47:51 +0000

    Builds again on Unix systems (fixed a typo introduced in r17881).
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@17883 06827809-a52a-0410-b366-d66718629ded

--- apps/snmptable.c
+++ apps/snmptable.c
@@ -255,7 +255,7 @@ main(int argc, char *argv[])
     netsnmp_session session, *ss;
     int            total_entries = 0;
 
-    netsnmp_enable_line_buffering(stdout);
+    netsnmp_set_line_buffering(stdout);
 
     netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, 
                            NETSNMP_DS_LIB_QUICK_PRINT, 1);
