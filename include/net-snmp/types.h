d3db3ccb3802f092d8c19a38022aac8aeae18484
Bart Van Assche <bvassche@users.sourceforge.net>
2010-01-23 16:28:16 +0000

    MinGW: made gettimeofday() declaration available for the compiler.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18025 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/types.h
+++ include/net-snmp/types.h
@@ -14,12 +14,11 @@
                         /*
                          * For 'timeval' 
                          */
+#ifdef HAVE_SYS_TIMEB_H
+# include <sys/timeb.h>
+#endif
 #if TIME_WITH_SYS_TIME
-# ifdef WIN32
-#  include <sys/timeb.h>
-# else
-#  include <sys/time.h>
-# endif
+# include <sys/time.h>
 # include <time.h>
 #else
 # if HAVE_SYS_TIME_H
