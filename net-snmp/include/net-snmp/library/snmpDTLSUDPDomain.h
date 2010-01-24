627b8cffd029df5837294ff39be6a9104dc5ed7f
Magnus Fromreide <magfr@users.sourceforge.net>
2010-01-24 06:08:43 +0000

    CHANGES: snmplib: Remove declarations of functions that don't exist or are declared elsewhere.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18037 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/library/snmpDTLSUDPDomain.h
+++ net-snmp/include/net-snmp/library/snmpDTLSUDPDomain.h
@@ -18,17 +18,8 @@ extern          "C" {
 #define TRANSPORT_DOMAIN_DTLS_UDP_IP	1,3,6,1,4,1,8072,3,3,8
 NETSNMP_IMPORT oid netsnmpDTLSUDPDomain[];
 
-netsnmp_transport *netsnmp_dtlsudp_transport(struct sockaddr_in *addr, int local);
-
-
-/*
- * Convert a "traditional" peername into a sockaddr_in structure which is
- * written to *addr.  Returns 1 if the conversion was successful, or 0 if it
- * failed.  
- */
-
-int             netsnmp_sockaddr_in(struct sockaddr_in *addr,
-                                    const char *peername, int remote_port);
+netsnmp_transport *netsnmp_dtlsudp_transport(struct sockaddr_in *addr,
+                                             int local);
 
 
 /*
@@ -36,18 +27,6 @@ int             netsnmp_sockaddr_in(struct sockaddr_in *addr,
  */
 
 void            netsnmp_dtlsudp_agent_config_tokens_register(void);
-
-void            netsnmp_dtlsudp_parse_security(const char *token, char *param);
-
-int             netsnmp_dtlsudp_getSecName(void *opaque, int olength,
-                                       const char *community,
-                                       size_t community_len,
-                                       char **secname,
-                                       char **contextName);
-
-int             netsnmp_sock_buffer_set(int s, int optname, int local,
-                                        int size);
-
 
 /*
  * "Constructor" for transport domain object.  
