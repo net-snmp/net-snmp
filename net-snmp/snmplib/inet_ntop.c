ff15904994f65e6f1847d7704824ff4c88a714bc
Bart Van Assche <bvassche@users.sourceforge.net>
2010-01-25 14:13:13 +0000

    Added missing #include <string.h>.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18061 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/inet_ntop.c
+++ net-snmp/snmplib/inet_ntop.c
@@ -25,6 +25,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <net-snmp/types.h>
 
--- net-snmp/snmplib/inet_pton.c
+++ net-snmp/snmplib/inet_pton.c
@@ -27,6 +27,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <net-snmp/types.h>
 #include "inet_pton.h"
