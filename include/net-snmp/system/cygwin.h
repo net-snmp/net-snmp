efeb3ff1d9f98454c0c703b70bd5869fa4e6ad27
Bart Van Assche <bvassche@users.sourceforge.net>
2010-01-26 11:43:11 +0000

    NEWS: Cygwin: BUG: 2939168: byte order of udpLocalPort is now correct.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18074 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/system/cygwin.h
+++ include/net-snmp/system/cygwin.h
@@ -7,10 +7,6 @@
 #include <stdint.h>	/* uint32_t */
 #endif
 
-#undef HAVE_WINSOCK_H
 #undef bsdlike
 #undef MBSTAT_SYMBOL
 #undef TOTAL_MEMORY_SYMBOL
-
-#define UDP_ADDRESSES_IN_HOST_ORDER 1
-#define UDP_PORTS_IN_HOST_ORDER 1
