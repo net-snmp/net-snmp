b510a541dc829031501e557511ff28677572b26a
Wes Hardaker <hardaker@users.sourceforge.net>
2010-02-09 23:59:00 +0000

    move TCP common functions to a TCPBase domain implementation
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18130 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/snmpTCPIPv6Domain.c
+++ net-snmp/snmplib/snmpTCPIPv6Domain.c
@@ -72,54 +72,14 @@ static int
 netsnmp_tcp6_recv(netsnmp_transport *t, void *buf, int size,
 		  void **opaque, int *olength)
 {
-    int rc = -1;
-
-    if (t != NULL && t->sock >= 0) {
-	while (rc < 0) {
-	    rc = recvfrom(t->sock, buf, size, 0, NULL, NULL);
-	    if (rc < 0 && errno != EINTR) {
-		DEBUGMSGTL(("netsnmp_tcp6", "recv fd %d err %d (\"%s\")\n",
-			    t->sock, errno, strerror(errno)));
-		return -1;
-	    }
-	}
-        DEBUGMSGTL(("netsnmp_tcp6", "recv fd %d got %d bytes\n", t->sock, rc));
-    } else {
-        return -1;
-    }
-
-    if (opaque != NULL && olength != NULL) {
-        if (t->data_length > 0) {
-            if ((*opaque = malloc(t->data_length)) != NULL) {
-                memcpy(*opaque, t->data, t->data_length);
-                *olength = t->data_length;
-            } else {
-                *olength = 0;
-            }
-        } else {
-            *opaque = NULL;
-            *olength = 0;
-        }
-    }
-
-    return rc;
+    return netsnmp_tcpbase_recv(t, buf, size, opaque, olength);
 }
 
 static int
 netsnmp_tcp6_send(netsnmp_transport *t, void *buf, int size,
 		  void **opaque, int *olength)
 {
-    int rc = -1;
-
-    if (t != NULL && t->sock >= 0) {
-	while (rc < 0) {
-	    rc = sendto(t->sock, buf, size, 0, NULL, 0);
-	    if (rc < 0 && errno != EINTR) {
-		break;
-	    }
-	}
-    }
-    return rc;
+    return netsnmp_tcpbase_send(t, buf, size, opaque, olength);
 }
 
 static int
