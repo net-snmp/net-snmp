8c88b815914d4f34e36f6098a79d7f982bee8a8a
Wes Hardaker <hardaker@users.sourceforge.net>
2010-02-10 23:21:29 +0000

    moved another error logging function
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18142 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/library/snmpTLSBaseDomain.h
+++ net-snmp/include/net-snmp/library/snmpTLSBaseDomain.h
@@ -14,6 +14,7 @@ extern          "C" {
 
     void netsnmp_init_tlsbase(void);
     const char * _x509_get_error(int x509failvalue, const char *location);
+    void _openssl_log_error(int rc, SSL *con, const char *location);
 
     /* will likely go away */
     SSL_CTX *get_client_ctx(void);
