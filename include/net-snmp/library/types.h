94e56d5881dff45dd477878dd6233337240caf2e
Bart Van Assche <bvassche@users.sourceforge.net>
2010-02-18 09:45:35 +0000

    Source code cleanup: - Moved documentation of netsnmp_large_fd_set members to the comment   block just above the definition of this type. - Removed superfluous 'extern "C"' specifier: this linkage specifier   only has an effect on declarations of variables of functions and not   on typedefs or struct definitions. - Ran this header file through indent.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-5-patches@18155 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/library/types.h
+++ include/net-snmp/library/types.h
@@ -8,48 +8,42 @@
 
 #include <net-snmp/types.h>
 
-#ifdef __cplusplus
-extern          "C" {
-#endif
 
-    typedef struct netsnmp_index_s {
-       size_t      len;
-       oid         *oids;
-    } netsnmp_index;
+typedef struct netsnmp_index_s {
+    size_t          len;
+    oid            *oids;
+} netsnmp_index;
 
+typedef struct netsnmp_void_array_s {
+    size_t          size;
+    void          **array;
+} netsnmp_void_array;
 
-    typedef struct netsnmp_void_array_s {
-       size_t  size;
-       void * *array;
-    } netsnmp_void_array;
+/*
+ * references to various types
+ */
+typedef struct netsnmp_ref_void {
+    void           *val;
+} netsnmp_ref_void;
 
-    /*
-     * references to various types
-     */
-    typedef struct netsnmp_ref_void {
-       void * val;
-    } netsnmp_ref_void;
+typedef union {
+    u_long          ul;
+    u_int           ui;
+    u_short         us;
+    u_char          uc;
+    long            sl;
+    int             si;
+    short           ss;
+    char            sc;
+    char           *cp;
+    void           *vp;
+} netsnmp_cvalue;
 
-    typedef union {
-        u_long  ul;
-        u_int   ui;
-        u_short us;
-        u_char  uc;
-        long    sl;
-        int     si;
-        short   ss;
-        char    sc;
-        char *  cp;
-        void *  vp;
-    } netsnmp_cvalue;
+typedef struct netsnmp_ref_size_t_s {
+    size_t          val;
+}              *netsnmp_ref_size_t;
 
-    typedef struct netsnmp_ref_size_t_s {
-       size_t val;
-    } * netsnmp_ref_size_t;
-
-
-
-/**
+/*
  * Structure for holding a set of file descriptors, similar to fd_set.
  *
  * This structure however can hold so-called large file descriptors
@@ -61,20 +55,16 @@ extern          "C" {
  * This structure must be initialized by calling netsnmp_large_fd_set_init()
  * and must be cleaned up via netsnmp_large_fd_set_cleanup(). If this last
  * function is not called this may result in a memory leak.
+ *
+ * The members of this structure are:
+ * lfs_setsize: maximum set size.
+ * lsf_setptr:  points to lfs_set if lfs_setsize <= FD_SETSIZE, and otherwise
+ *              to dynamically allocated memory.
+ * lfs_set:     file descriptor / socket set data if lfs_setsize <= FD_SETSIZE.
  */
 typedef struct netsnmp_large_fd_set_s {
-    /** Maximum set size. */
     unsigned        lfs_setsize;
-    /**
-     * Points to lfs_set if lfs_setsize <= FD_SETSIZE, and otherwise
-     * to dynamically allocated memory.
-     */
     fd_set         *lfs_setptr;
-    /** File descriptor / socket set data if lfs_setsize <= FD_SETSIZE. */
     fd_set          lfs_set;
 } netsnmp_large_fd_set;
-#ifdef __cplusplus
-}
-#endif
-
 #endif                          /* NET_SNMP_LIBRARY_TYPES_H */
