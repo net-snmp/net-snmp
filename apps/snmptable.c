net-snmp/net-snmp

commit 009f849af1d4d355219417c4d7d85f28eb4ecb71

Cleaned up the code for setting line buffering: - Instead of having calls to setvbuf() in multiple files, there is now one call   to setvbuf() from the new function netsnmp_set_filelog. These source files   are: snmplib/snmp_logging.c, agent/snmpd.c, apps/snmptrapd.c, apps/snmptable.c   and agent\mibgroup\disman\traceroute\traceRouteCtlTable.c. - Using -Lo / -Le does now enable line buffering for stdout / stderr respectively. - Removed code in snmpd.c and snmptrapd.c for setting line buffering on stdout   since setting line buffering on stdout is now triggered by the command-line   option -Lo. Bugs fixed: - Line buffering mode is now also set for stderr when the command-line option   -Le has been specified. - Line buffering mode is now set correctly for stdout on Windows systems   (command-line option -Lo).
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@17881 06827809-a52a-0410-b366-d66718629ded

apps/snmptable.c
@@ -255,7 +255,8 @@ main(int argc, char *argv[])
     netsnmp_session session, *ss;
     int            total_entries = 0;
 
-    setvbuf(stdout, NULL, _IOLBF, 1024);
+    netsnmp_enable_line_buffering(stdout);
+
     netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, 
                            NETSNMP_DS_LIB_QUICK_PRINT, 1);
 

agent/mibgroup/disman/traceroute/traceRouteCtlTable.c
@@ -4171,11 +4171,7 @@ run_traceRoute(unsigned int clientreg, void *clientarg)
         freehostinfo(hi);
 
 
-#ifdef HAVE_SETLINEBUF
-        setlinebuf(stdout);
-#else
-        setvbuf(stdout, NULL, _IOLBF, 0);
-#endif
+        netsnmp_set_line_buffering(stdout);
 
         outip = (struct ip *) malloc(packlen);
         if (outip == NULL) {

agent/snmpd.c
@@ -906,10 +906,6 @@ main(int argc, char *argv[])
     *cptr = 0;
     *argvptr = NULL;
 
-#ifdef BUFSIZ
-    setvbuf(stdout, NULL, _IOLBF, BUFSIZ);
-#endif
-
     SOCK_STARTUP;
     init_agent(app_name);        /* do what we need to do first. */
     init_mib_modules();

apps/snmptrapd.c
@@ -661,12 +661,6 @@ main(int argc, char *argv[])
     register_config_handler("snmptrapd", "outputOption",
                             parse_config_outputOption, NULL, "string");
 
-#ifdef WIN32
-    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-#else
-    setvbuf(stdout, NULL, _IOLBF, BUFSIZ);
-#endif
-
     /*
      * Add some options if they are available.  
      */

include/net-snmp/library/snmp_logging.h
@@ -80,6 +80,7 @@ extern          "C" {
 #define NETSNMP_LOGHANDLER_CALLBACK	5
 #define NETSNMP_LOGHANDLER_NONE		6
 
+    void netsnmp_set_line_buffering(FILE *stream);
     int snmp_log_options(char *optarg, int argc, char *const *argv);
     void snmp_log_options_usage(const char *lead, FILE *outf);
     char *snmp_log_syslogname(const char *syslogname);

snmplib/snmp_logging.c
@@ -170,6 +170,26 @@ shutdown_snmp_logging(void)
 #define LOG_USER	0
 #endif
 
+/* Set line buffering mode for a stream. */
+void
+netsnmp_set_line_buffering(FILE *stream)
+{
+#if defined(WIN32)
+    /*
+     * According to MSDN, the Microsoft Visual Studio C runtime library does
+     * not support line buffering, so turn off buffering completely.
+     * See also http://msdn.microsoft.com/en-us/library/86cebhfs(VS.71).aspx.
+     */
+    setvbuf(stream, NULL, _IONBF, BUFSIZ);
+#elif defined(HAVE_SETLINEBUF)
+    /* setlinefunction() is a function from the BSD Unix API. */
+    setlinebuf(stream);
+#else
+    /* See also the C89 or C99 standard for more information about setvbuf(). */
+    setvbuf(stream, NULL, _IOLBF, BUFSIZ);
+#endif
+}
+
 /*
  * Decodes log priority.
  * @param optarg - IN - priority to decode, "0" or "0-7"
@@ -343,6 +363,7 @@ snmp_log_options(char *optarg, int argc, char *const *argv)
     case 'e':
         logh = netsnmp_register_loghandler(NETSNMP_LOGHANDLER_STDERR, priority);
         if (logh) {
+            netsnmp_set_line_buffering(stderr);
             logh->pri_max = pri_max;
             logh->token   = strdup("stderr");
 	}
@@ -360,6 +381,7 @@ snmp_log_options(char *optarg, int argc, char *const *argv)
     case 'o':
         logh = netsnmp_register_loghandler(NETSNMP_LOGHANDLER_STDERR, priority);
         if (logh) {
+            netsnmp_set_line_buffering(stdout);
             logh->pri_max = pri_max;
             logh->token   = strdup("stdout");
             logh->imagic  = 1;	    /* stdout, not stderr */
@@ -720,16 +742,7 @@ netsnmp_enable_filelog(netsnmp_log_handler *logh, int dont_zero_log)
         if (!logfile)
             return;
         logh->magic = (void*)logfile;
-#ifdef WIN32
-        /*
-         * Apparently, "line buffering" under Windows is
-         *  actually implemented as "full buffering".
-         *  Let's try turning off buffering completely.
-         */
-        setvbuf(logfile, NULL, _IONBF, BUFSIZ);
-#else
-        setvbuf(logfile, NULL, _IOLBF, BUFSIZ);
-#endif
+        netsnmp_set_line_buffering(logfile);
     }
     logh->enabled = 1;
 }

commit 7af0a15e680d0564a527da7c780f3d2dfc07f55d

Backported trunk r17883, which fixes a typo introduced in r17881.
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-5-patches@18174 06827809-a52a-0410-b366-d66718629ded

apps/snmptable.c
@@ -255,7 +255,7 @@ main(int argc, char *argv[])
     netsnmp_session session, *ss;
     int            total_entries = 0;
 
-    netsnmp_enable_line_buffering(stdout);
+    netsnmp_set_line_buffering(stdout);
 
     netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, 
                            NETSNMP_DS_LIB_QUICK_PRINT, 1);