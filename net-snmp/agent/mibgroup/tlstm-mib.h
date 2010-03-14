86d4d3429b40256b1e18a4d45fa4265dca5e5514
Robert Story <rstory@users.sourceforge.net>
2010-03-14 19:08:54 +0000

    import initial code for dtls mibs
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18295 06827809-a52a-0410-b366-d66718629ded

---
+++ net-snmp/agent/mibgroup/tlstm-mib.h
@@ -0,0 +1,6 @@
+/*
+ * module to include the modules
+ */
+config_require(tlstm-mib/snmpTlstmSession)
+config_require(tlstm-mib/tlstmAddrTable)
+config_add_mib(TLSTM-MIB)
---
+++ net-snmp/agent/mibgroup/tlstm-mib/snmpTlstmSession/snmpTlstmSession.h
@@ -0,0 +1,8 @@
+#ifndef SNMPTLSTMSESSION_H
+#define SNMPTLSTMSESSION_H
+
+/** Initializes the snmpTlstmSession module */
+void init_snmpTlstmSession(void);
+
+#endif /* SNMPTLSTMSESSION_H */
+
