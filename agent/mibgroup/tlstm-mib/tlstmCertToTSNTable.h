c24a9cd3142af153ede69481eaa3849205b3d1e4
Robert Story <rstory@users.sourceforge.net>
2010-04-10 22:25:25 +0000

    initial cut at tlstmCertToTSNTable
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18490 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/mibgroup/tlstm-mib/tlstmCertToTSNTable.h
@@ -0,0 +1,6 @@
+#ifndef TLSTMCERTTOTSNTABLE_H
+#define TLSTMCERTTOTSNTABLE_H
+
+config_require(tlstm-mib/tlstmCertToTSNTable/tlstmCertToTSNTable)
+
+#endif                          /* TLSTMCERTTOTSNTABLE_H */
