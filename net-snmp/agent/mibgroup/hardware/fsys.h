79b189cf2f0064ca6315fd7a22504f0939cb0093
Dave Shield <dts12@users.sourceforge.net>
2010-05-19 12:05:28 +0000

    First-draft support for AIX filesystem HAL module (untested)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18799 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/hardware/fsys.h
+++ net-snmp/agent/mibgroup/hardware/fsys.h
@@ -1,5 +1,7 @@
 config_require(hardware/fsys/hw_fsys)
-#if defined(HAVE_GETVFSSTAT) || defined(HAVE_GETFSSTAT)
+#if defined(aix4) || defined(aix5) || defined(aix6)
+config_require(hardware/fsys/fsys_mntctl)
+#elif defined(HAVE_GETVFSSTAT) || defined(HAVE_GETFSSTAT)
 config_require(hardware/fsys/fsys_getfsstat)
 #else
 config_require(hardware/fsys/fsys_mntent)
