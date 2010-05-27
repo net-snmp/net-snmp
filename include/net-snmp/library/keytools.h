e0f9083d0a68420b48f080e32da4181d2a74b5cf
Wes Hardaker <hardaker@users.sourceforge.net>
2010-05-27 05:16:49 +0000

    more const correctness
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@18881 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/library/keytools.h
+++ include/net-snmp/library/keytools.h
@@ -26,13 +26,13 @@ extern          "C" {
      */
     NETSNMP_IMPORT
     int             generate_Ku(const oid * hashtype, u_int hashtype_len,
-                                u_char * P, size_t pplen,
+                                const u_char * P, size_t pplen,
                                 u_char * Ku, size_t * kulen);
 
     NETSNMP_IMPORT
     int             generate_kul(const oid * hashtype, u_int hashtype_len,
-                                 u_char * engineID, size_t engineID_len,
-                                 u_char * Ku, size_t ku_len,
+                                 const u_char * engineID, size_t engineID_len,
+                                 const u_char * Ku, size_t ku_len,
                                  u_char * Kul, size_t * kul_len);
 
     NETSNMP_IMPORT
