88066614fbc8e008f936f0ed5102391a5161a6ad
Robert Story <rstory@users.sourceforge.net>
2010-06-19 16:40:10 +0000

    fix missing specifier in debug
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19051 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/transports/snmpTLSBaseDomain.c
+++ net-snmp/snmplib/transports/snmpTLSBaseDomain.c
@@ -373,7 +373,7 @@ sslctx_client_setup(const SSL_METHOD *method, _netsnmpTLSBaseData *tlsbase) {
         id_cert = netsnmp_cert_find(NS_CERT_IDENTITY, NS_CERTKEY_MULTIPLE,
                                     tlsbase->our_identity);
     } else {
-        DEBUGMSGTL(("sslctx_client", "looking for default local id\n", tlsbase->our_identity));
+        DEBUGMSGTL(("sslctx_client", "looking for default local id: %s\n", tlsbase->our_identity));
         id_cert = netsnmp_cert_find(NS_CERT_IDENTITY, NS_CERTKEY_DEFAULT, NULL);
     }
 
