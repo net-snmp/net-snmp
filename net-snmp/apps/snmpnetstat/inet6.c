net-snmp/net-snmp

commit 67774aedcb2671447207aa1589b144d0d328a631

Eliminate "signed and unsigned comparison" from app code [-Wsign-compare]
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19014 06827809-a52a-0410-b366-d66718629ded

net-snmp/apps/snmpnetstat/inet6.c
@@ -71,7 +71,7 @@ static char sccsid[] = "@(#)inet.c	8.4 (Berkeley) 4/20/94";
 #include "netstat.h"
 
 struct stat_table {
-    int             entry;      /* entry number in table */
+    unsigned int entry;      /* entry number in table */
     /*
      * format string to printf(description, value) 
      * warning: the %d must be before the %s 
@@ -224,7 +224,7 @@ _dump_v6stats( const char *name, oid *oid_buf, size_t buf_len,
     netsnmp_variable_list *var, *vp;
     struct stat_table     *sp;
     oid   *stats, stat;
-    int    max_stat = 0;
+    unsigned int max_stat = 0;
     int    active   = 0;
 
     var = NULL;
@@ -413,7 +413,7 @@ inet6print(unsigned char *in6, int port, const char *proto, int local)
 
 	struct servent *sp = NULL;
 	char line[80], *cp;
-	int width;
+	size_t width;
 	int len = sizeof line;
 
 	width = Aflag ? 12 : 16;

net-snmp/apps/agentxtrap.c
@@ -90,9 +90,9 @@ static size_t       contextLen; /**<< Length of eventual context */
 static int          result = 1; /**<< Program return value */
 static netsnmp_pdu *pdu = NULL; /**<< The trap pdu that is to be sent */
 /** The reference number of the next packet */
-static uint32_t     packetid = 0;
+static long         packetid = 0;
 /** The session id of the session to the master */
-static uint32_t     session;
+static long         session;
 static void        *sessp = NULL; /**<< The current communication session */
 
 #define STATE_CALL(method)                                              \

net-snmp/apps/snmpnetstat/if.c
@@ -72,7 +72,7 @@ static void timerPause(void);
         char            ip[128], route[128];
         int             mtu;
         int             drops;
-        int             ifindex;
+        unsigned int    ifindex;
                         /*
                          * Save "expandable" fields as string values
                          *  rather than integer statistics
@@ -131,7 +131,7 @@ _set_address( struct _if_info *cur_if )
      */
     for (vp=addr_if_var, vp2=addr_mask_var;  vp;
          vp=vp->next_variable, vp2=vp2->next_variable) {
-        if ( vp->val.integer && *vp->val.integer == cur_if->ifindex )
+        if ( vp->val.integer && *vp->val.integer == (int)cur_if->ifindex )
             break;
     }
     if (vp2) {
@@ -519,7 +519,7 @@ sidewaysintpr(unsigned int interval)
     struct iftot *sum = NULL, *total  = NULL;    /* overall summary    */
     int    line;
     int    first;
-    int    i;
+    size_t i;
 
     var = NULL;
     if ( intrface ) {

net-snmp/apps/snmpnetstat/inet.c
@@ -67,7 +67,7 @@ static const char *rcsid = "$OpenBSD: inet.c,v 1.92 2005/02/10 14:25:08 itojun E
 #include "netstat.h"
 
 struct stat_table {
-    int             entry;      /* entry number in table */
+    unsigned int  entry;      /* entry number in table */
     /*
      * format string to printf(description, value) 
      * warning: the %d must be before the %s 

net-snmp/apps/snmptable.c
@@ -662,7 +662,7 @@ get_table_entries(netsnmp_session * ss)
                     col++;
                     name[rootlen] = column[col].subid;
                     if ((vars->name_length < name_length) ||
-                        ((int) vars->name[rootlen] != column[col].subid) ||
+                        (vars->name[rootlen] != column[col].subid) ||
                         memcmp(name, vars->name,
                                name_length * sizeof(oid)) != 0
                         || vars->type == SNMP_ENDOFMIBVIEW) {

net-snmp/apps/snmptrapd_log.c
@@ -104,7 +104,7 @@ SOFTWARE.
 typedef struct {
     char            cmd;        /* the format command itself */
     size_t          width;      /* the field's minimum width */
-    size_t          precision;  /* the field's precision */
+    int             precision;  /* the field's precision */
     int             left_justify;       /* if true, left justify this field */
     int             alt_format; /* if true, display in alternate format */
     int             leading_zeroes;     /* if true, display with leading zeroes */
@@ -368,7 +368,7 @@ realloc_output_temp_bfr(u_char ** buf, size_t * buf_len, size_t * out_len,
     temp_to_write = temp_len;
 
     if (options->precision != UNDEF_PRECISION &&
-        temp_to_write > options->precision) {
+        temp_to_write > (size_t)options->precision) {
         temp_to_write = options->precision;
     }
 
@@ -538,7 +538,7 @@ realloc_handle_time_fmt(u_char ** buf, size_t * buf_len, size_t * out_len,
             sprintf(safe_bfr, "%d", parsed_time->tm_year + 1900);
             if (options->precision != UNDEF_PRECISION) {
                 year_len = (size_t) strlen(safe_bfr);
-                if (year_len > options->precision)
+                if (year_len > (size_t)options->precision)
                     offset = year_len - options->precision;
             }
             break;
@@ -1000,7 +1000,7 @@ realloc_handle_auth_fmt(u_char ** buf, size_t * buf_len, size_t * out_len,
     char            fmt_cmd = options->cmd;     /* what we're outputting */
     u_char         *temp_buf = NULL;
     size_t          tbuf_len = 64, tout_len = 0;
-    int             i;
+    unsigned int    i;
 
     if ((temp_buf = (u_char*)calloc(tbuf_len, 1)) == NULL) {
         return 0;
@@ -1764,8 +1764,9 @@ realloc_format_trap(u_char ** buf, size_t * buf_len, size_t * out_len,
                 }
             } else if (is_fmt_cmd(next_chr)) {
                 options.cmd = next_chr;
-                if (options.width < options.precision) {
-                    options.width = options.precision;
+                if ((options.precision != UNDEF_PRECISION) &&
+                    (options.width < (size_t)options.precision)) {
+                    options.width = (size_t)options.precision;
                 }
                 if (!realloc_dispatch_format_cmd
                     (buf, buf_len, out_len, allow_realloc, &options, pdu,

net-snmp/apps/snmpusm.c
@@ -214,7 +214,7 @@ get_USM_DH_key(netsnmp_variable_list *vars, netsnmp_variable_list *dhvar,
         return SNMPERR_GENERR;
     }
             
-    if (vars->val_len != BN_num_bytes(dh->pub_key)) {
+    if (vars->val_len != (unsigned int)BN_num_bytes(dh->pub_key)) {
         SNMP_FREE(dhkeychange);
         fprintf(stderr,"incorrect diffie-helman lengths (%lu != %d)\n",
                 (unsigned long)vars->val_len, BN_num_bytes(dh->pub_key));
@@ -247,7 +247,7 @@ get_USM_DH_key(netsnmp_variable_list *vars, netsnmp_variable_list *dhvar,
 
         printf("new %s key: 0x", keyname);
         for(kp = key + key_len - outkey_len;
-            kp - key < key_len;  kp++) {
+            kp - key < (int)key_len;  kp++) {
             printf("%02x", (unsigned char) *kp);
         }
         printf("\n");

commit 5104bd0d3202d83e274bc069071a15ea8460c4e6

The width argument for %.*d must have type int, not size_t. This bug was introduced in r19014.
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19074 06827809-a52a-0410-b366-d66718629ded

net-snmp/apps/snmpnetstat/inet6.c
@@ -413,7 +413,7 @@ inet6print(unsigned char *in6, int port, const char *proto, int local)
 
 	struct servent *sp = NULL;
 	char line[80], *cp;
-	size_t width;
+	unsigned width;
 	int len = sizeof line;
 
 	width = Aflag ? 12 : 16;