d97e9b8fb7f5cf2659e6e03534ed8539bbe8c4ce
Giovanni Marzot <marz@users.sourceforge.net>
2010-06-26 17:46:55 +0000

    better interactive, ?=help, <tab> compl, more help, SAN loop format and termination fix, tolerate spaces in fields and filenames, more complete error output, embryonic import from url
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19111 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/local/certgen-test.pl
+++ net-snmp/local/certgen-test.pl
@@ -1,46 +1,55 @@
 #!/usr/bin/perl
 
-system("rm -rf ~/.snmp/tls");
-system("rm -rf /tmp/.snmp");
+system("rm -rf /tmp/.snmp1");
+system("rm -rf /tmp/.snmp2");
 
-system("cp ~/Projects/SNMP-DTLS/trunk/net-snmp/local/net-snmp-cert /home/gmarzot/bin");
+system("cp net-snmp-cert ~/bin");
 
-$str = "\ngenca (in -C /tmp/.snmp) : ca-snmp\n\n";
+$str = "\ngenca (in -C /tmp/.snmp1) : ca-snmp\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert genca -I -C /tmp/.snmp --cn ca-snmp --email ca\@ca.com --host host.a.b.com  --san DNS:ca.a.b.com --san EMAIL:ca\@ca.com");
+die("$str\n") if system("net-snmp-cert genca -I -C /tmp/.snmp1 --cn ca-snmp --email ca\@ca.com --host host.a.b.com  --san DNS:ca.a.b.com --san EMAIL:ca\@ca.com");
 
+print "\nusing -C /tmp/.snmp2 for all following tests\n";
 $str = "\ngenca: ca-snmp\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert genca -I --cn ca-snmp --email ca\@ca.com --host host.a.b.com  --san DNS:ca.a.b.com --san EMAIL:ca\@ca.com");
+die("$str\n") if system("net-snmp-cert genca -I -C /tmp/.snmp2 --cn ca-snmp --email ca\@ca.com --host host.a.b.com  --san DNS:ca.a.b.com --san EMAIL:ca\@ca.com");
 
 $str = "\ngenca: ca-snmp-2 (signed w/ ca-snmp)\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert genca -I --with-ca ca-snmp --cn ca-snmp-2 --email ca2\@ca.com --host host2.a.b.com  --san DNS:ca2.a.b.com --san EMAIL:ca2\@ca.com");
+die("$str\n") if system("net-snmp-cert genca -I -C /tmp/.snmp2 --with-ca ca-snmp --cn ca-snmp-2 --email ca2\@ca.com --host host2.a.b.com  --san DNS:ca2.a.b.com --san EMAIL:ca2\@ca.com");
 
 $str = "\ngencsr: snmpapp\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert gencsr -I -t snmpapp --cn 'admin' --email admin@net-snmp.org --host admin-host.net-snmp.org  --san EMAIL:a\@b.com --san IP:1.2.3.4 --san DNS:admin.a.b.org");
+die("$str\n") if system("net-snmp-cert gencsr -I -C /tmp/.snmp2 -t snmpapp --cn 'admin' --email admin@net-snmp.org --host admin-host.net-snmp.org  --san EMAIL:a\@b.com --san IP:1.2.3.4 --san DNS:admin.a.b.org");
 
 $str = "\nsigncsr: snmpapp w/ca-snmp\n\n";
 print("$str");
-die("died: $str\n") if system("net-snmp-cert signcsr -I --with-ca ca-snmp --csr snmpapp  --install");
+die("died: $str\n") if system("net-snmp-cert signcsr -I -C /tmp/.snmp2 --with-ca ca-snmp --csr snmpapp  --install");
 
 $str = "\nsigncsr: snmpapp w/ca-snmp-2\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert signcsr -I --with-ca ca-snmp-2 --csr snmpapp --san EMAIL:noinstall\@b.com --san IP:5.6.7.8");
+die("$str\n") if system("net-snmp-cert signcsr -I -C /tmp/.snmp2 --with-ca ca-snmp-2 --csr snmpapp --san EMAIL:noinstall\@b.com --san IP:5.6.7.8");
 
 $str = "\ngencert: snmptrapd (self-signed)\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert gencert -I -t snmptrapd --cn 'NOC' --email 'noc\@net-snmp.org' --host noc-host.net-snmp.org  --san DNS:noc.a.b.org --san 'EMAIL:noc\@net-snmp.org'");
+die("$str\n") if system("net-snmp-cert gencert -I -C /tmp/.snmp2 -t snmptrapd --cn 'NOC' --email 'noc\@net-snmp.org' --host noc-host.net-snmp.org  --san DNS:noc.a.b.org --san 'EMAIL:noc\@net-snmp.org'");
 
 $str = "\ngencert: snmpd (signed w/ ca-snmp-2)\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert gencert -I -t snmpd --with-ca ca-snmp-2 --email snmpd\@net-snmp.org --host snmpd-host.net-snmp.org  --san DNS:snmpd.a.b.org --san EMAIL:snmpd\@net-snmp.org");
+die("$str\n") if system("net-snmp-cert gencert -I -C /tmp/.snmp2 -t snmpd --with-ca ca-snmp-2 --email snmpd\@net-snmp.org --host snmpd-host.net-snmp.org  --san DNS:snmpd.a.b.org --san EMAIL:snmpd\@net-snmp.org");
 
 $str = "\nshow CAs\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert showca --issuer --subject");
+die("$str\n") if system("net-snmp-cert -C /tmp/.snmp2 showca --issuer --subject");
 
 $str = "show Certs\n\n";
 print("$str");
-die("$str\n") if system("net-snmp-cert showcert --issuer --subject");
+die("$str\n") if system("net-snmp-cert -C /tmp/.snmp2 showcert --issuer --subject");
+
+$str = "show CAs fingerprint\n\n";
+print("$str");
+die("$str\n") if system("net-snmp-cert -C /tmp/.snmp2 showca --fingerprint --brief");
+
+$str = "\nshow Certs fingerprint\n\n";
+print("$str");
+die("$str\n") if system("net-snmp-cert -C /tmp/.snmp2 showcert --fingerprint --brief");
