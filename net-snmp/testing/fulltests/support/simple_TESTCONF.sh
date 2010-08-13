6d09cbeb1db83c4fb46077cf340da4e04f289da9
Robert Story <rstory@users.sourceforge.net>
2010-08-13 19:00:20 +0000

    use 127.0.0.1 instead of localhost as default SNMP_TEST_DEST so TLS tests work
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19321 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/testing/fulltests/support/simple_TESTCONF.sh
+++ net-snmp/testing/fulltests/support/simple_TESTCONF.sh
@@ -206,7 +206,7 @@ if [ "x$SNMP_TRANSPORT_SPEC" = "x" ];then
 	SNMP_TRANSPORT_SPEC="udp"
 fi
 if [ "x$SNMP_TEST_DEST" = "x" -a $SNMP_TRANSPORT_SPEC != "unix" ];then
-	SNMP_TEST_DEST="localhost:"
+	SNMP_TEST_DEST="127.0.0.1:"
 fi
 export SNMP_FLAGS SNMP_SNMPD_PORT SNMP_SNMPTRAPD_PORT
 
