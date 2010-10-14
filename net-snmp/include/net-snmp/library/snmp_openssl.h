aa8a88a8ac2b21c19842c587cc9441cdf0de53a2
Robert Story <rstory@users.sourceforge.net>
2010-10-14 03:26:08 +0000

    add netsnmp_openssl_null_checks
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19434 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/library/snmp_openssl.h
+++ net-snmp/include/net-snmp/library/snmp_openssl.h
@@ -38,6 +38,7 @@ extern          "C" {
      * misc
      */
     void netsnmp_openssl_err_log(const char *prefix);
+    void netsnmp_openssl_null_checks(SSL *ssl, int *nullAuth, int *nullCipher);
 
 #ifdef __cplusplus
 }
