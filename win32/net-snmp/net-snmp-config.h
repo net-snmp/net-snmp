f3a7d1b09609dad649f58bbdb5b7e10d39b398c1
Bart Van Assche <bvassche@users.sourceforge.net>
2010-10-17 11:04:06 +0000

    Follow-up for r19476: fixed a typo.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-6-patches@19478 06827809-a52a-0410-b366-d66718629ded

--- win32/net-snmp/net-snmp-config.h
+++ win32/net-snmp/net-snmp-config.h
@@ -1592,7 +1592,7 @@ typedef unsigned short   uint16_t;
 #ifndef __cplusplus
 enum {
     netsnmp_compile_time_uintptr_t_size_check
-    = sizeof(struct { int:-!(sizeof(netsnmp_uintptr_t) != sizeof(void*)); })
+    = sizeof(struct { int:-!(sizeof(uintptr_t) != sizeof(void*)); })
 };
 #endif
 
