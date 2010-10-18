8d5b8bfd29da49e54b4bed0cf1041be8ca6e13f3
Magnus Fromreide <magfr@users.sourceforge.net>
2010-10-18 06:15:00 +0000

    CHANGES: snmpd: Use the same autoconf symbol to decide both if strcasecmp should be declared and if it should be defined.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19488 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/library/system.h
+++ net-snmp/include/net-snmp/library/system.h
@@ -97,12 +97,12 @@ SOFTWARE.
 
 /*
  * Note: when compiling Net-SNMP with dmalloc enabled on a system without
- * strcasecmp() or strncasecmp(), the macros HAVE_STRCASECMP and HAVE_STRNCASECMP
- * are not defined but strcasecmp() and strncasecmp() are defined as macros in
+ * strcasecmp() or strncasecmp(), the macro HAVE_STRNCASECMP is
+ * not defined but strcasecmp() and strncasecmp() are defined as macros in
  * <dmalloc.h>. In order to prevent a compilation error, do not declare
  * strcasecmp() or strncasecmp() when the <dmalloc.h> header has been included.
  */
-#if !defined(HAVE_STRCASECMP) && !defined(strcasecmp)
+#if !defined(HAVE_STRNCASECMP) && !defined(strcasecmp)
     NETSNMP_IMPORT
     int             strcasecmp(const char *s1, const char *s2);
 #endif
