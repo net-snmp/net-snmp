0fa77dbca6cb48cfa0e50cc0dc8ae3a2cb13a66d
Magnus Fromreide <magfr@users.sourceforge.net>
2010-10-18 06:15:00 +0000

    CHANGES: snmpd: Use the same autoconf symbol to decide both if strcasecmp should be declared and if it should be defined.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@19488 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/library/system.h
+++ include/net-snmp/library/system.h
@@ -84,10 +84,8 @@ SOFTWARE.
 #ifndef HAVE_GETTIMEOFDAY
     int             gettimeofday(struct timeval *, struct timezone *tz);
 #endif
-#ifndef HAVE_STRCASECMP
-    int             strcasecmp(const char *s1, const char *s2);
-#endif
 #ifndef HAVE_STRNCASECMP
+    int             strcasecmp(const char *s1, const char *s2);
     int             strncasecmp(const char *s1, const char *s2, size_t n);
 #endif
 
