b63892149d8532936d18caea5cb5abd8073e7f89
Niels Baggesen <nba@users.sourceforge.net>
2010-10-19 20:55:16 +0000

    PATCH: 3085295 from Bill Fenner: recognize LISTEN state again
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19505 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibII/tcpTable.c
+++ agent/mibgroup/mibII/tcpTable.c
@@ -688,7 +688,7 @@ tcpTable_load(netsnmp_cache *cache, void *vmagic)
         int             state, uid;
 
         if (6 != sscanf(line,
-                        "%*d: %x:%x %x:%x %d %*X:%*X %*X:%*X %*X %d",
+                        "%*d: %x:%x %x:%x %x %*X:%*X %*X:%*X %*X %d",
                         &pcb.inp_laddr.s_addr, &lp,
                         &pcb.inp_faddr.s_addr, &fp, &state, &uid))
             continue;
