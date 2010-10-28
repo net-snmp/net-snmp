8b230a3685a75542f626258a6cc33b9b37d3fde1
Bart Van Assche <bvassche@users.sourceforge.net>
2010-10-28 09:47:04 +0000

    Fixed yet another crash in netsnmp_cache_remove().
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-6-patches@19563 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/cache_handler.c
+++ agent/helpers/cache_handler.c
@@ -248,7 +248,8 @@ netsnmp_cache_remove(netsnmp_cache *cache)
     for (; cur; prev = cur, cur = cur->next) {
         if (cache == cur) {
             prev->next = cur->next;
-            cur->next->prev = cur->prev;
+            if (cur->next)
+                cur->next->prev = cur->prev;
             return 0;
         }
     }
