45de48f272b5c8717d6cfdac11ad3bd11379a0ca
Bart Van Assche <bvassche@users.sourceforge.net>
2010-10-28 15:13:49 +0000

    CHANGES: agent: Added netsnmp_register_watched_instance2(), netsnmp_register_watched_scalar2(), netsnmp_clone_watcher_info() and netsnmp_owns_watcher_info(). See also the Doxygen comments for more info.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19574 06827809-a52a-0410-b366-d66718629ded

--- include/net-snmp/agent/watcher.h
+++ include/net-snmp/agent/watcher.h
@@ -64,7 +64,11 @@ typedef struct netsnmp_watcher_info_s {
 
 int netsnmp_register_watched_instance( netsnmp_handler_registration *reginfo,
                                        netsnmp_watcher_info         *winfo);
+int netsnmp_register_watched_instance2(netsnmp_handler_registration *reginfo,
+                                       netsnmp_watcher_info         *winfo);
 int netsnmp_register_watched_scalar(   netsnmp_handler_registration *reginfo,
+                                       netsnmp_watcher_info         *winfo);
+int netsnmp_register_watched_scalar2(  netsnmp_handler_registration *reginfo,
                                        netsnmp_watcher_info         *winfo);
 int netsnmp_register_watched_timestamp(netsnmp_handler_registration *reginfo,
                                        marker_t timestamp);
@@ -123,6 +127,11 @@ netsnmp_create_watcher_info(void *, size_t, u_char, int);
 
 netsnmp_watcher_info *
 netsnmp_create_watcher_info6(void *, size_t, u_char, int, size_t, size_t*);
+
+netsnmp_watcher_info *
+netsnmp_clone_watcher_info(netsnmp_watcher_info *winfo);
+void
+netsnmp_owns_watcher_info(netsnmp_mib_handler *handler);
 
 Netsnmp_Node_Handler  netsnmp_watcher_helper_handler;
 
