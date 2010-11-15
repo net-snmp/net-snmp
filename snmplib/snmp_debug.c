be210056b0fef569b3e33996c15d59f9a49a017d
Bart Van Assche <bvassche@users.sourceforge.net>
2010-11-15 12:33:28 +0000

    CHANGES: snmplib: Changed the argument type of debug_register_tokens() from char * into const char *.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19665 06827809-a52a-0410-b366-d66718629ded

--- snmplib/snmp_debug.c
+++ snmplib/snmp_debug.c
@@ -92,7 +92,7 @@ debug_config_turn_on_debugging(const char *configtoken, char *line)
 }
 
 void
-debug_register_tokens(char *tokens)
+debug_register_tokens(const char *tokens)
 {
     char           *newp, *cp;
     char           *st = NULL;
