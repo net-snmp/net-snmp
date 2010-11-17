d3eace1c1a5aa74d67aac99e6459dbf819095877
Niels Baggesen <nba@users.sourceforge.net>
2010-11-17 19:39:24 +0000

    Test getfsstat/getvfsstat in time for module selection
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19684 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/hardware/fsys.h
+++ agent/mibgroup/hardware/fsys.h
@@ -2,7 +2,7 @@ config_require(hardware/fsys/hw_fsys)
 #if defined(aix4) || defined(aix5) || defined(aix6) || defined(aix7)
 config_require(hardware/fsys/fsys_mntctl)
 #elif defined(HAVE_GETVFSSTAT) || defined(HAVE_GETFSSTAT)
-config_require(hardware/fsys/fsys_getfsstat)
+config_require(hardware/fsys/fsys_getfsstats)
 #else
 config_require(hardware/fsys/fsys_mntent)
 #endif
