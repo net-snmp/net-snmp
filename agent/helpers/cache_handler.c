c929a2643906fa8bc91304bc008ea95dbd049d2c
Robert Story <rstory@users.sourceforge.net>
2010-11-29 22:14:11 +0000

    CHANGES: snmpd: use cache-unique id instead of handler name for shared cache reload protection
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-5-patches@19692 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/cache_handler.c
+++ agent/helpers/cache_handler.c
@@ -466,6 +466,8 @@ netsnmp_cache_helper_handler(netsnmp_mib_handler * handler,
                              netsnmp_agent_request_info * reqinfo,
                              netsnmp_request_info * requests)
 {
+    char addrstr[32];
+
     netsnmp_cache  *cache = NULL;
     netsnmp_handler_args cache_hint;
 
@@ -485,6 +487,10 @@ netsnmp_cache_helper_handler(netsnmp_mib_handler * handler,
                    "cache not found, disabled or had no load method\n"));
         return SNMP_ERR_NOERROR;
     }
+    snprintf(addrstr,sizeof(addrstr), "%ld", cache);
+    DEBUGMSGTL(("helper:cache_handler", "using cache %s: ", addrstr));
+    DEBUGMSGOID(("helper:cache_handler", cache->rootoid, cache->rootoid_len));
+    DEBUGMSG(("helper:cache_handler", "\n"));
 
     /*
      * Make the handler-chain parameters available to
@@ -511,7 +517,7 @@ netsnmp_cache_helper_handler(netsnmp_mib_handler * handler,
          * a previous (delegated) request is still using the cache.
          * maybe use a reference counter?
          */
-        if (netsnmp_cache_is_valid(reqinfo, reginfo->handlerName))
+        if (netsnmp_cache_is_valid(reqinfo, addrstr))
             break;
 
         /*
@@ -519,7 +525,7 @@ netsnmp_cache_helper_handler(netsnmp_mib_handler * handler,
          * If it's not already there, add to reqinfo
          */
         netsnmp_cache_check_and_reload(cache);
-        netsnmp_cache_reqinfo_insert(cache, reqinfo, reginfo->handlerName);
+        netsnmp_cache_reqinfo_insert(cache, reqinfo, addrstr);
         /** next handler called automatically - 'AUTO_NEXT' */
         break;
 
@@ -527,7 +533,7 @@ netsnmp_cache_helper_handler(netsnmp_mib_handler * handler,
     case MODE_SET_FREE:
     case MODE_SET_ACTION:
     case MODE_SET_UNDO:
-        netsnmp_assert(netsnmp_cache_is_valid(reqinfo, reginfo->handlerName));
+        netsnmp_assert(netsnmp_cache_is_valid(reqinfo, addrstr));
         /** next handler called automatically - 'AUTO_NEXT' */
         break;
 
