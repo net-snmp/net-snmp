15d56730bb9b856208e513eeeadd63bcd7c366ee
Robert Story <rstory@users.sourceforge.net>
2011-01-02 05:47:24 +0000

    fix cut-n-paste error in debug token
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19775 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/transports/snmpIPv6BaseDomain.c
+++ net-snmp/snmplib/transports/snmpIPv6BaseDomain.c
@@ -60,7 +60,7 @@ netsnmp_ipv6_fmtaddr(const char *prefix, netsnmp_transport *t,
     char addr[INET6_ADDRSTRLEN];
     char tmp[INET6_ADDRSTRLEN + 18];
 
-    DEBUGMSGTL(("netsnmp_udp6", "fmtaddr: t = %p, data = %p, len = %d\n", t,
+    DEBUGMSGTL(("netsnmp_ipv6", "fmtaddr: t = %p, data = %p, len = %d\n", t,
                 data, len));
     if (data != NULL && len == sizeof(struct sockaddr_in6)) {
         to = (struct sockaddr_in6 *) data;
