51f0c04d849413ae14a46711fbb89490cb1e69ca
Dave Shield <dts12@users.sourceforge.net>
2011-01-07 14:09:15 +0000

    Remove duplicate include directives [From FreeBSD patches archive]
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19817 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/sctp-mib/sctpLookupRemIPAddrTable.c
+++ net-snmp/agent/mibgroup/sctp-mib/sctpLookupRemIPAddrTable.c
@@ -6,7 +6,6 @@
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
-#include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/agent/table_container.h>
 #include "sctpLookupRemIPAddrTable.h"
 
