65ebaba3483652a39ab9c4e815721dec25259be6
Dave Shield <dts12@users.sourceforge.net>
2011-01-07 14:38:37 +0000

    Update retrieval of cache usage statistics on FreeBSD [From FreeBSD patch archive]
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-5-patches@19821 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/hardware/memory/memory_freebsd.c
+++ agent/mibgroup/hardware/memory/memory_freebsd.c
@@ -43,10 +43,15 @@ int netsnmp_mem_arch_load( netsnmp_cache *cache, void *magic ) {
 
     u_long         phys_mem;
     u_long         user_mem;
+    unsigned int   cache_count;
+    unsigned int   cache_max;
     unsigned int   bufspace;
     unsigned int   maxbufspace;
-    size_t         mem_size  = sizeof(phys_mem);
-    size_t         buf_size  = sizeof(bufspace);
+    unsigned int   inact_count;
+    size_t         mem_size   = sizeof(phys_mem);
+    size_t         cache_size = sizeof(cache_count);
+    size_t         buf_size   = sizeof(bufspace);
+    size_t         inact_size = sizeof(inact_count);
     int            phys_mem_mib[] = { CTL_HW, HW_PHYSMEM };
     int            user_mem_mib[] = { CTL_HW, HW_USERMEM };
 
@@ -56,9 +61,11 @@ int netsnmp_mem_arch_load( netsnmp_cache *cache, void *magic ) {
     sysctl(total_mib,    2, &total,    &total_size,    NULL, 0);
     sysctl(phys_mem_mib, 2, &phys_mem, &mem_size,      NULL, 0);
     sysctl(user_mem_mib, 2, &user_mem, &mem_size,      NULL, 0);
+    sysctlbyname("vm.stats.vm.v_cache_count",    &cache_count, &cache_size, NULL, 0);
+    sysctlbyname("vm.stats.vm.v_cache_max",      &cache_max,   &cache_size, NULL, 0);
+    sysctlbyname("vm.stats.vm.v_inactive_count", &inact_count, &inact_size, NULL, 0);
     sysctlbyname("vfs.bufspace",    &bufspace,    &buf_size, NULL, 0);
     sysctlbyname("vfs.maxbufspace", &maxbufspace, &buf_size, NULL, 0);
-    auto_nlist(SUM_SYMBOL,      (char *) &vmem,     sizeof(vmem));
 #ifndef freebsd4
     pagesize = 1024;
 #else
@@ -75,7 +82,7 @@ int netsnmp_mem_arch_load( netsnmp_cache *cache, void *magic ) {
         if (!mem->descr)
              mem->descr = strdup("Physical memory");
         mem->units = pagesize;
-        mem->size  = user_mem/pagesize;
+        mem->size  = phys_mem/pagesize;
         mem->free  = total.t_free;
     }
 
@@ -129,9 +136,9 @@ int netsnmp_mem_arch_load( netsnmp_cache *cache, void *magic ) {
     } else {
         if (!mem->descr)
              mem->descr = strdup("Cached memory");
-        mem->units = vmem.v_page_size;
-        mem->size  = vmem.v_cache_max;
-        mem->free  = vmem.v_cache_max - vmem.v_cache_count;
+        mem->units = pagesize;
+        mem->size  = cache_max + inact_count;
+        mem->free  = cache_max - cache_count;
     }
 
     nswap = swapmode(pagesize);
