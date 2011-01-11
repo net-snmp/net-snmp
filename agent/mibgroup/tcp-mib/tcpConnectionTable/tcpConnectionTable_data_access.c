net-snmp/net-snmp

commit 26ce5fc0d0b3f13453b60d24028218912a3846a0

Fix memory leak in UDP- and TCP-MIB. The data from raw_data are copied to cache, so it should be freed.
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@17719 06827809-a52a-0410-b366-d66718629ded

agent/mibgroup/tcp-mib/tcpConnectionTable/tcpConnectionTable_data_access.c
@@ -256,8 +256,7 @@ tcpConnectionTable_container_load(netsnmp_container *container)
      * free the container. we've either claimed each entry, or released it,
      * so the dal function doesn't need to clear the container.
      */
-    netsnmp_access_tcpconn_container_free(raw_data,
-                                          NETSNMP_ACCESS_TCPCONN_FREE_DONT_CLEAR);
+    netsnmp_access_tcpconn_container_free(raw_data, 0);
 
     DEBUGMSGT(("verbose:tcpConnectionTable:tcpConnectionTable_cache_load",
                "%d records\n", (int)CONTAINER_SIZE(container)));

agent/mibgroup/tcp-mib/tcpListenerTable/tcpListenerTable_data_access.c
@@ -254,8 +254,7 @@ tcpListenerTable_container_load(netsnmp_container *container)
      * free the container. we've either claimed each entry, or released it,
      * so the dal function doesn't need to clear the container.
      */
-    netsnmp_access_tcpconn_container_free(raw_data,
-                                          NETSNMP_ACCESS_TCPCONN_FREE_DONT_CLEAR);
+    netsnmp_access_tcpconn_container_free(raw_data, 0);
 
     DEBUGMSGT(("verbose:tcpListenerTable:tcpListenerTable_cache_load",
                "%d records\n", (int)CONTAINER_SIZE(container)));

agent/mibgroup/udp-mib/udpEndpointTable/udpEndpointTable_data_access.c
@@ -290,8 +290,7 @@ udpEndpointTable_container_load(netsnmp_container *container)
 
     ITERATOR_RELEASE(ep_it);
 
-    netsnmp_access_udp_endpoint_container_free(ep_c,
-                                               NETSNMP_ACCESS_UDP_ENDPOINT_FREE_DONT_CLEAR);
+    netsnmp_access_udp_endpoint_container_free(ep_c, 0);
 
 
     DEBUGMSGT(("verbose:udpEndpointTable:udpEndpointTable_container_load",

commit 0bdf125cd42d8dfe27e0005f577d18b155da8eb8

revert PATCH 3053436: tcpConnectionTable_data_access invalid memory access
  the bug this is fixing was introduced in r17719, and fixed in r17861.


git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-5-patches@19847 06827809-a52a-0410-b366-d66718629ded

agent/mibgroup/tcp-mib/tcpConnectionTable/tcpConnectionTable_data_access.c
@@ -168,23 +168,15 @@ tcpConnectionTable_container_shutdown(netsnmp_container *container_ptr)
 static void
 _add_connection(netsnmp_tcpconn_entry *entry, netsnmp_container *container)
 {
-    netsnmp_tcpconn_entry *new_entry;
     tcpConnectionTable_rowreq_ctx *rowreq_ctx;
 
     DEBUGMSGTL(("tcpConnectionTable:access", "creating new entry\n"));
-
-    /* Allocate a new entry, becuase the old entry will be destroyed by
-     * the function which called this one.
-     */
-    new_entry = netsnmp_access_tcpconn_entry_create();
-    (void)netsnmp_access_tcpconn_entry_update(new_entry, entry);
-
 
     /*
      * allocate an row context and set the index(es), then add it to
      * the container
      */
-    rowreq_ctx = tcpConnectionTable_allocate_rowreq_ctx(new_entry, NULL);
+    rowreq_ctx = tcpConnectionTable_allocate_rowreq_ctx(entry, NULL);
     if ((NULL != rowreq_ctx) &&
         (MFD_SUCCESS == tcpConnectionTable_indexes_set(rowreq_ctx,
                                                        entry->loc_addr_len,
@@ -209,7 +201,7 @@ _add_connection(netsnmp_tcpconn_entry *entry, netsnmp_container *container)
         } else {
             snmp_log(LOG_ERR, "memory allocation failed while loading "
                      "tcpConnectionTable cache.\n");
-            netsnmp_access_tcpconn_entry_free(new_entry);
+            netsnmp_access_tcpconn_entry_free(entry);
         }
     }
 }

agent/mibgroup/tcp-mib/tcpListenerTable/tcpListenerTable_data_access.c
@@ -169,22 +169,15 @@ tcpListenerTable_container_shutdown(netsnmp_container *container_ptr)
 static void
 _add_connection(netsnmp_tcpconn_entry *entry, netsnmp_container *container)
 {
-    netsnmp_tcpconn_entry *new_entry;
     tcpListenerTable_rowreq_ctx *rowreq_ctx;
 
     DEBUGMSGTL(("tcpListenerTable:access", "creating new entry\n"));
-
-    /* Allocate a new entry, becuase the old entry will be destroyed by
-     * the function which called this one.
-     */
-    new_entry = netsnmp_access_tcpconn_entry_create();
-    (void)netsnmp_access_tcpconn_entry_update(new_entry, entry);
 
     /*
      * allocate an row context and set the index(es), then add it to
      * the container
      */
-    rowreq_ctx = tcpListenerTable_allocate_rowreq_ctx(new_entry, NULL);
+    rowreq_ctx = tcpListenerTable_allocate_rowreq_ctx(entry, NULL);
     if ((NULL != rowreq_ctx) &&
         (MFD_SUCCESS == tcpListenerTable_indexes_set(rowreq_ctx,
                                                      entry->loc_addr_len,
@@ -200,7 +193,7 @@ _add_connection(netsnmp_tcpconn_entry *entry, netsnmp_container *container)
         } else {
             snmp_log(LOG_ERR, "memory allocation failed while loading "
                      "tcpListenerTable cache.\n");
-            netsnmp_access_tcpconn_entry_free(new_entry);
+            netsnmp_access_tcpconn_entry_free(entry);
         }
     }
 }
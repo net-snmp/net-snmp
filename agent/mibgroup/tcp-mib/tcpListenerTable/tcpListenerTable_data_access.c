0bdf125cd42d8dfe27e0005f577d18b155da8eb8
Robert Story <rstory@users.sourceforge.net>
2011-01-11 14:14:07 +0000

    revert PATCH 3053436: tcpConnectionTable_data_access invalid memory access
      the bug this is fixing was introduced in r17719, and fixed in r17861.
    
    
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-5-patches@19847 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/tcp-mib/tcpListenerTable/tcpListenerTable_data_access.c
+++ agent/mibgroup/tcp-mib/tcpListenerTable/tcpListenerTable_data_access.c
@@ -169,22 +169,15 @@ tcpListenerTable_container_shutdown(netsnmp_container *container_ptr)
 static void
 _add_connection(netsnmp_tcpconn_entry *entry, netsnmp_container *container)
 {
-    netsnmp_tcpconn_entry *new_entry;
     tcpListenerTable_rowreq_ctx *rowreq_ctx;
 
     DEBUGMSGTL(("tcpListenerTable:access", "creating new entry\n"));
-
-    /* Allocate a new entry, becuase the old entry will be destroyed by
-     * the function which called this one.
-     */
-    new_entry = netsnmp_access_tcpconn_entry_create();
-    (void)netsnmp_access_tcpconn_entry_update(new_entry, entry);
 
     /*
      * allocate an row context and set the index(es), then add it to
      * the container
      */
-    rowreq_ctx = tcpListenerTable_allocate_rowreq_ctx(new_entry, NULL);
+    rowreq_ctx = tcpListenerTable_allocate_rowreq_ctx(entry, NULL);
     if ((NULL != rowreq_ctx) &&
         (MFD_SUCCESS == tcpListenerTable_indexes_set(rowreq_ctx,
                                                      entry->loc_addr_len,
@@ -200,7 +193,7 @@ _add_connection(netsnmp_tcpconn_entry *entry, netsnmp_container *container)
         } else {
             snmp_log(LOG_ERR, "memory allocation failed while loading "
                      "tcpListenerTable cache.\n");
-            netsnmp_access_tcpconn_entry_free(new_entry);
+            netsnmp_access_tcpconn_entry_free(entry);
         }
     }
 }
