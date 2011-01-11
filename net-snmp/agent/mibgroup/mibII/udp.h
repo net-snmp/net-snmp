4227389f883cb4605eeffaf05dac9659cd0947df
Dave Shield <dts12@users.sourceforge.net>
2011-01-11 11:27:06 +0000

    Pull in kernel_netbsd module when required [Based on fix from NetBSD patch archive]
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19840 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/mibII/udp.h
+++ net-snmp/agent/mibgroup/mibII/udp.h
@@ -7,8 +7,11 @@
 
 
 config_require(mibII/udpTable)
-config_arch_require(solaris2, kernel_sunos5)
-config_arch_require(linux, mibII/kernel_linux)
+
+config_arch_require(solaris2,        kernel_sunos5)
+config_arch_require(linux,     mibII/kernel_linux)
+config_arch_require(netbsd,    mibII/kernel_netbsd)
+config_arch_require(netbsdelf, mibII/kernel_netbsd)
 
 extern void     init_udp(void);
 extern Netsnmp_Node_Handler udp_handler;
