777c365c692f07b97e117bbb17534e3364e60409
Bart Van Assche <bvassche@users.sourceforge.net>
2011-01-12 19:04:39 +0000

    Fixed a compiler warning (format specification mismatch) that was introduced in r19808 ("snmpd: PATCHES: 2975254: Avoid callback re-use of closed sessions").
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-5-patches@19856 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/agentx/subagent.c
+++ agent/mibgroup/agentx/subagent.c
@@ -117,7 +117,7 @@ subagent_init_callback_session(void)
         agentx_callback_sess = netsnmp_callback_open(callback_master_num,
                                                      handle_subagent_response,
                                                      NULL, NULL);
-        DEBUGMSGTL(("agentx/subagent", "subagent_init sess %08x\n",
+        DEBUGMSGTL(("agentx/subagent", "subagent_init sess %p\n",
                     agentx_callback_sess));
     }
 }
