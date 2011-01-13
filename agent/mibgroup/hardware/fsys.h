cab053bc3692d7ea18647cc78dd9d184765f18e4
Dave Shield <dts12@users.sourceforge.net>
2011-01-13 15:54:57 +0000

    Fix structure of /tags SVN release (part 2)
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/tags/Ext-5-5-1-pre2@19874 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/hardware/fsys.h
+++
@@ -1,8 +0,0 @@
-config_require(hardware/fsys/hw_fsys)
-#if defined(aix4) || defined(aix5) || defined(aix6)
-config_require(hardware/fsys/fsys_mntctl)
-#elif defined(HAVE_GETVFSSTAT) || defined(HAVE_GETFSSTAT)
-config_require(hardware/fsys/fsys_getfsstat)
-#else
-config_require(hardware/fsys/fsys_mntent)
-#endif
--- agent/mibgroup/target/target_counters_5_5.h
+++
@@ -1,4 +0,0 @@
-config_require(utilities/snmp_get_statistic)
-config_exclude(target/target_counters)
-
-void            init_target_counters_5_5(void);
