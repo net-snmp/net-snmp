eae188fcb38c788ee86e441812040111101e466e
Wes Hardaker <hardaker@users.sourceforge.net>
2011-02-08 17:37:01 +0000

    CHANGES: snmpd: Fix a strlen calculation as pointed out by Konstantin Baydarov
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19915 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/agent/mibgroup/if-mib/data_access/interface_linux.c
+++ net-snmp/agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -449,7 +449,7 @@ _parse_stats(netsnmp_interface_entry *entry, char *stats, int expected)
 
     if ((*stats == 'N') &&
         (0 == strncmp(stats, "No statistics available",
-                      sizeof("No statistics available"))))
+                      strlen("No statistics available"))))
         return -1;
 
     /*
