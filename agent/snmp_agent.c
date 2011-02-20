net-snmp/net-snmp

commit 081a7355a2ea1c53520109c1bf72f36081bb374b

CHANGES: snmplib: Added snmp_sess_session_lookup(). CHANGES: agent: Call shutdown_master_agent() and shutdown_agent() during shutdown. CHANGES: agent: The function clean_subtree() does no longer trigger double frees and invalid memory reads. Note: this function was already wrong at the time it was introduced (patch #724925 / r8531, July 2003). CHANGES: agent: Removed a comment in netsnmp_handler_free() that is now obsolete because of the clean_subtree() changes. CHANGES: agent: Modified netsnmp_deregister_agent_nsap() such that it never examines sessions that already have been closed. It is for this change that it was necessary to introduce the new function snmp_sess_session_lookup().
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-6-patches@19511 06827809-a52a-0410-b366-d66718629ded

agent/snmp_agent.c
@@ -1182,13 +1182,15 @@ netsnmp_deregister_agent_nsap(int handle)
 
     if (a != NULL && a->handle == handle) {
         *prevNext = a->next;
-        if (main_session == snmp_sess_session(a->s)) {
-            main_session_deregistered = 1;
+	if (snmp_sess_session_lookup(a)) {
+            if (main_session == snmp_sess_session(a->s)) {
+                main_session_deregistered = 1;
+            }
+            snmp_close(snmp_sess_session(a->s));
+            /*
+             * The above free()s the transport and session pointers.  
+             */
         }
-        snmp_close(snmp_sess_session(a->s));
-        /*
-         * The above free()s the transport and session pointers.  
-         */
         SNMP_FREE(a);
     }
 

agent/agent_handler.c
@@ -680,10 +680,6 @@ netsnmp_handler_free(netsnmp_mib_handler *handler)
             netsnmp_handler_free(handler->next);
             handler->next = NULL;
         }
-        /** XXX : segv here at shutdown if SHUTDOWN_AGENT_CLEANLY
-         *  defined. About 30 functions down the stack, starting
-         *  in clear_context() -> clear_subtree()
-         */
         if ((handler->myvoid != NULL) && (handler->data_free != NULL))
         {
             handler->data_free(handler->myvoid);

agent/agent_registry.c
@@ -939,17 +939,14 @@ netsnmp_subtree_load(netsnmp_subtree *new_sub, const char *context_name)
 void
 clear_subtree (netsnmp_subtree *sub) {
 
-    netsnmp_subtree *nxt;
+    netsnmp_subtree *c;
     
     if (sub == NULL)
 	return;
 
-    for(nxt = sub; nxt;) {
-        if (nxt->children != NULL) {
-            clear_subtree(nxt->children);
-        }
-        sub = nxt;
-        nxt = nxt->next;
+    for(c = sub; c;) {
+        sub = c;
+        c = c->children;
         netsnmp_subtree_free(sub);
     }
 

agent/snmpd.c
@@ -1064,11 +1064,8 @@ main(int argc, char *argv[])
     SnmpTrapNodeDown();
     DEBUGMSGTL(("snmpd/main", "Bye...\n"));
     snmp_shutdown(app_name);
-#ifdef SHUTDOWN_AGENT_CLEANLY /* broken code */
-    /* these attempt to free all known memory, but result in double frees */
     shutdown_master_agent();
     shutdown_agent();
-#endif
 
     if (!netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID, 
 				NETSNMP_DS_AGENT_LEAVE_PIDFILE) &&

include/net-snmp/session_api.h
@@ -221,6 +221,8 @@ extern          "C" {
     void           *snmp_sess_pointer(netsnmp_session *);
     NETSNMP_IMPORT
     netsnmp_session *snmp_sess_session(void *);
+    NETSNMP_IMPORT
+    netsnmp_session *snmp_sess_session_lookup(void *);
 
 
     /*

snmplib/snmp_api.c
@@ -7546,6 +7546,30 @@ snmp_sess_session(void *sessp)
     return (slp->session);
 }
 
+/**
+ * Look up a session that already may have been closed.
+ *
+ * @param sessp Opaque pointer, returned by snmp_sess_open.
+ *
+ * @return Pointer to session upon success or NULL upon failure.
+ *
+ * @see snmp_sess_session()
+ */
+netsnmp_session *
+snmp_sess_session_lookup(void *sessp)
+{
+    struct session_list *slp;
+
+    snmp_res_lock(MT_LIBRARY_ID, MT_LIB_SESSION);
+    for (slp = Sessions; slp; slp = slp->next) {
+        if (slp == sessp) {
+            break;
+        }
+    }
+    snmp_res_unlock(MT_LIBRARY_ID, MT_LIB_SESSION);
+
+    return (netsnmp_session *)slp;
+}
 
 
 /*

commit 71b615e38a5af0c07ab98994d94058ded1ef006d

CHANGES: snmpd: BUG: 3185528: shutdown_master_agent doesn't close UDP socket (Fixes a bug introduced in r19511.)
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@19942 06827809-a52a-0410-b366-d66718629ded

agent/snmp_agent.c
@@ -1102,7 +1102,7 @@ netsnmp_deregister_agent_nsap(int handle)
 
     if (a != NULL && a->handle == handle) {
         *prevNext = a->next;
-	if (snmp_sess_session_lookup(a)) {
+	if (snmp_sess_session_lookup(a->s)) {
             if (main_session == snmp_sess_session(a->s)) {
                 main_session_deregistered = 1;
             }