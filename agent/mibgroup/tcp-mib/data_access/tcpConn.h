d928d48fcd2e12b0e386d606dea23755faeccbbd
Niels Baggesen <nba@users.sourceforge.net>
2011-02-21 22:20:04 +0000

    tcp-mib/udp-mib support for the *bsds
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19960 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/tcp-mib/data_access/tcpConn.h
+++ agent/mibgroup/tcp-mib/data_access/tcpConn.h
@@ -23,6 +23,12 @@ config_require(tcp-mib/data_access/tcpConn_linux)
 config_require(util_funcs/get_pid_from_inode)
 #elif defined( solaris2 )
 config_require(tcp-mib/data_access/tcpConn_solaris2)
+#elif defined(freebsd4) || defined(dragonfly)
+config_require(tcp-mib/data_access/tcpConn_freebsd4)
+#elif defined(openbsd4)
+config_require(tcp-mib/data_access/tcpConn_openbsd)
+#elif defined(netbsd1)
+config_require(tcp-mib/data_access/tcpConn_netbsd)
 #else
 #   define NETSNMP_TCPCONN_COMMON_ONLY
 #endif
