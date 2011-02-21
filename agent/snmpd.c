a951c20a6865920949f4504cb076906c23cab485
Bart Van Assche <bvassche@users.sourceforge.net>
2011-02-21 07:13:33 +0000

    Fixed a MinGW-specific compiler warning (backported r19922 from trunk).
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-6-patches@19943 06827809-a52a-0410-b366-d66718629ded

--- agent/snmpd.c
+++ agent/snmpd.c
@@ -1098,14 +1098,14 @@ static unsigned __stdcall wait_for_stdin(void* arg)
     return 0;
 }
 
-static void create_stdin_waiter_thread()
+static void create_stdin_waiter_thread(void)
 {
     netsnmp_assert(s_thread_handle == 0);
     s_thread_handle = (HANDLE)_beginthreadex(0, 0, wait_for_stdin, 0, 0, &s_threadid);
     netsnmp_assert(s_thread_handle != 0);
 }
 
-static void join_stdin_waiter_thread()
+static void join_stdin_waiter_thread(void)
 {
     int result;
 
