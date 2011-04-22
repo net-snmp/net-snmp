274e2c22b8df04d393182e85bf9643fd587b4280
Wes Hardaker <hardaker@users.sourceforge.net>
2011-04-22 16:25:39 +0000

    added a function to calculate the time until the next run
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@20200 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/deliver/deliverByNotify.h
+++ agent/mibgroup/deliver/deliverByNotify.h
@@ -1,11 +1,34 @@
 #ifndef DELIVERBYNOTIFY_H
 #define DELIVERBYNOTIFY_H 1
 
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
 void init_mteTrigger(void);
 
 void parse_deliver_config(const char *, char *);
 void free_deliver_config(void);
 
 SNMPAlarmCallback deliver_execute;
+
+
+/* implementation details */
+typedef struct deliver_by_notify_s {
+   int     frequency;
+   int     last_run;
+   oid    *target;
+   size_t  target_size;
+   int     max_packet_size;
+} deliver_by_notify;
+
+int calculate_time_until_next_run(deliver_by_notify *it, time_t *now);
 
 #endif /* deliverByNotify_h */
