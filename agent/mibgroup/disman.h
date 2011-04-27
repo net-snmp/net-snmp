2cd9c40d01cd85ab709cea96eb8294459f385db2
Wes Hardaker <hardaker@users.sourceforge.net>
2011-04-27 21:28:36 +0000

    the disman schedule mib is only useful when SETs are turned on
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@20247 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/disman.h
+++ agent/mibgroup/disman.h
@@ -3,7 +3,10 @@
  */
 config_require(disman/event-mib)
 config_require(disman/expression-mib)
+#ifndef NETSNMP_NO_WRITE_SUPPORT
+/* the schedule mib is all about writing (SETs) */ 
 config_require(disman/schedule)
+#endif /* !NETSNMP_NO_WRITE_SUPPORT */
 /* config_require(disman/nslookup-mib)   */
 /* config_require(disman/ping-mib)       */
 /* config_require(disman/traceroute-mib) */
