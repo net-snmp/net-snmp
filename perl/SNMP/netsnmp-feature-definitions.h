5987eaf26029682cd415bd0363b7a9bd759c5336
Wes Hardaker <hardaker@users.sourceforge.net>
2011-05-04 14:39:34 +0000

    Added perl feature requirements
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@20312 06827809-a52a-0410-b366-d66718629ded

--- perl/SNMP/netsnmp-feature-definitions.h
+++ perl/SNMP/netsnmp-feature-definitions.h
@@ -2,3 +2,7 @@
 #include <net-snmp/net-snmp-features.h>
 
 netsnmp_feature_require(snprint_objid)
+netsnmp_feature_require(snprint_value)
+netsnmp_feature_require(find_module)
+netsnmp_feature_require(get_tc_description)
+
