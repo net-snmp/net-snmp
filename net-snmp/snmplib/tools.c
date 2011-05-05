043683bb58382454507684a6139ced33f5052d70
Bart Van Assche <bvassche@users.sourceforge.net>
2011-05-05 06:07:51 +0000

    CHANGES: MinGW: Made regression test output independent of the Net-SNMP variables defined in the Windows registry. Some tests that failed previously after the Net-SNMP binaries (MSVC build) had been installed do now pass.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-4-patches@20329 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/tools.c
+++ net-snmp/snmplib/tools.c
@@ -953,6 +953,9 @@ char *netsnmp_getenv(const char *name)
   /* Next try HKCU */
   if (temp == NULL)
   {
+    if (getenv("SNMP_IGNORE_WINDOWS_REGISTRY"))
+      return NULL;
+
     if (RegOpenKeyExA(
           HKEY_CURRENT_USER, 
           "SOFTWARE\\Net-SNMP", 
