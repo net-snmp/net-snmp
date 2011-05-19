40a97e7d5cc38f5641f5a31b4baeb0fb33d76ce4
Dave Shield <dts12@users.sourceforge.net>
2011-05-19 08:26:57 +0000

    CHANGES: snmplib: BUG: 3184697: Don't reference internal MD5 when it's explicitly disabled
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@20437 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/snmplib/scapi.c
+++ net-snmp/snmplib/scapi.c
@@ -379,9 +379,12 @@ sc_generate_keyed_hash(const oid * authtype, size_t authtypelen,
 #elif NETSNMP_USE_INTERNAL_CRYPTO
     if (*maclen > properlength)
         *maclen = properlength;
+#ifndef NETSNMP_DISABLE_MD5
     if (ISTRANSFORM(authtype, HMACMD5Auth))
         rval = MD5_hmac(message, msglen, MAC, *maclen, key, keylen);
-    else if (ISTRANSFORM(authtype, HMACSHA1Auth))
+    else
+#endif
+         if (ISTRANSFORM(authtype, HMACSHA1Auth))
         rval = SHA1_hmac(message, msglen, MAC, *maclen, key, keylen);
     else {
         QUITFUN(SNMPERR_GENERR, sc_generate_keyed_hash_quit);
