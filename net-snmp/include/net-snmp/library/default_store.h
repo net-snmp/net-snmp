9a4a55c107931098ab3f306ff8578e553e66784e
Robert Story <rstory@users.sourceforge.net>
2011-06-07 16:48:09 +0000

    add a max define for each default store type
      not actually used, just a helpful reminder for those adding
      new ids who might not think to check NETSNMP_DS_MAX_SUBIDS.
      (the bool list is rapidly approaching the current limit.)
    
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@20497 06827809-a52a-0410-b366-d66718629ded

--- net-snmp/include/net-snmp/library/default_store.h
+++ net-snmp/include/net-snmp/library/default_store.h
@@ -92,6 +92,7 @@ extern          "C" {
 #define NETSNMP_DS_LIB_NO_DISCOVERY        38 /* don't support RFC5343 contextEngineID discovery */
 #define NETSNMP_DS_LIB_TSM_USE_PREFIX      39 /* TSM's simple security name mapping */
 #define NETSNMP_DS_LIB_DONT_LOAD_HOST_FILES 40 /* don't read host.conf files */
+#define NETSNMP_DS_LIB_MAX_BOOL_ID          48 /* match NETSNMP_DS_MAX_SUBIDS */
 
     /*
      * library integers 
@@ -114,6 +115,7 @@ extern          "C" {
 #define NETSNMP_DS_SSHDOMAIN_SOCK_GROUP    13
 #define NETSNMP_DS_LIB_TIMEOUT             14
 #define NETSNMP_DS_LIB_RETRIES             15
+#define NETSNMP_DS_LIB_MAX_INT_ID          48 /* match NETSNMP_DS_MAX_SUBIDS */
     
     /*
      * special meanings for the default SNMP version slot (NETSNMP_DS_LIB_SNMPVERSION) 
@@ -165,6 +167,7 @@ extern          "C" {
 #define NETSNMP_DS_LIB_SSH_USERNAME      32
 #define NETSNMP_DS_LIB_SSH_PUBKEY        33
 #define NETSNMP_DS_LIB_SSH_PRIVKEY       34
+#define NETSNMP_DS_LIB_MAX_STR_ID        48 /* match NETSNMP_DS_MAX_SUBIDS */
 
     /*
      * end storage definitions 
