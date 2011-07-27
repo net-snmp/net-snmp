13c3a502ff82229c939d269e94ff439626c86fcf
Bart Van Assche <bvanassche@acm.org>
2011-07-27 16:14:01 +0200

    CHANGES: snmpd: Make TCP-MIB::tcpCurrEstab work again. Was broken in 5.7 by the introduction of the "features" feature.

--- agent/mibgroup/mibII/tcp.c
+++ agent/mibgroup/mibII/tcp.c
@@ -92,7 +92,7 @@ perfstat_id_t ps_name;
 int  hz = 1000;
 #endif
 
-#ifdef NETSNMP_FEATURE_REMOVE_TCP_COUNT_CONNECTIONS
+#ifndef NETSNMP_FEATURE_REMOVE_TCP_COUNT_CONNECTIONS
 extern int TCP_Count_Connections( void );
 #endif /* NETSNMP_FEATURE_REMOVE_TCP_COUNT_CONNECTIONS */
         /*********************
@@ -373,7 +373,7 @@ tcp_handler(netsnmp_mib_handler          *handler,
 #ifdef NETSNMP_FEATURE_CHECKING
         netsnmp_feature_want(tcp_count_connections)
 #endif
-#ifdef NETSNMP_FEATURE_REMOVE_TCP_COUNT_CONNECTIONS
+#ifndef NETSNMP_FEATURE_REMOVE_TCP_COUNT_CONNECTIONS
         ret_value = TCP_Count_Connections();
 #else
         ret_value = 0;
