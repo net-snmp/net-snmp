net-snmp/net-snmp

commit 612247cdf14c1e745c384cefe1f3fde3e445b347

CHANGES: building: build fix for systems lacking MSG_DONTWAIT

apps/sshtosnmp.c
@@ -151,7 +151,7 @@ main(int argc, char **argv) {
         m.msg_flags             = 0;
         
         DEBUG("sending to sock");
-        rc = sendmsg(sock, &m, MSG_NOSIGNAL|MSG_DONTWAIT);
+        rc = sendmsg(sock, &m, NETSNMP_NOSIGNAL|NETSNMP_DONTWAIT);
         if (rc < 0) {
             fprintf(stderr, "failed to send startup message\n");
             DEBUG("failed to send startup message\n");

include/net-snmp/library/snmpUDPBaseDomain.h
@@ -10,6 +10,21 @@ extern          "C" {
 #endif
 
 /*
+ * Definitions
+ */
+#ifdef  MSG_DONTWAIT
+#define NETSNMP_DONTWAIT MSG_DONTWAIT
+#else
+#define NETSNMP_DONTWAIT 0
+#endif
+
+#ifdef  MSG_NOSIGNAL
+#define NETSNMP_NOSIGNAL MSG_NOSIGNAL
+#else
+#define NETSNMP_NOSIGNAL 0
+#endif
+
+/*
  * Prototypes
  */
     void _netsnmp_udp_sockopt_set(int fd, int local);

include/net-snmp/library/snmpUDPIPv4BaseDomain.h
@@ -21,15 +21,6 @@ extern          "C" {
 #endif
 
 /*
- * Definitions
- */
-#ifdef  MSG_DONTWAIT
-#define NETSNMP_DONTWAIT MSG_DONTWAIT
-#else
-#define NETSNMP_DONTWAIT 0
-#endif
-
-/*
  * Prototypes
  */
 

snmplib/transports/snmpUDPBaseDomain.c
@@ -295,11 +295,7 @@ int netsnmp_udpbase_sendto(int fd, struct in_addr *srcip, int if_index,
     DEBUGMSGTL(("udpbase:sendto", "sending from %s iface %d\n",
                 (srcip ? inet_ntoa(*srcip) : "NULL"), if_index));
     errno = 0;
-#ifdef MSG_NOSIGNAL
-    ret = sendmsg(fd, &m, MSG_NOSIGNAL|MSG_DONTWAIT);
-#else
-    ret = sendmsg(fd, &m, MSG_DONTWAIT);
-#endif
+    ret = sendmsg(fd, &m, NETSNMP_NOSIGNAL|NETSNMP_DONTWAIT);
     if (ret < 0 && errno == EINVAL && srcip) {
         /* The error might be caused by broadcast srcip (i.e. we're responding
          * to broadcast request) - sendmsg does not like it. Try to resend it
@@ -308,11 +304,7 @@ int netsnmp_udpbase_sendto(int fd, struct in_addr *srcip, int if_index,
         cmsg.ipi.ipi_ifindex = if_index;
         cmsg.ipi.ipi_spec_dst.s_addr = INADDR_ANY;
         DEBUGMSGTL(("udpbase:sendto", "re-sending the message\n"));
-#ifdef MSG_NOSIGNAL
-        ret = sendmsg(fd, &m, MSG_NOSIGNAL|MSG_DONTWAIT);
-#else
-        ret = sendmsg(fd, &m, MSG_DONTWAIT);
-#endif
+        ret = sendmsg(fd, &m, NETSNMP_NOSIGNAL|NETSNMP_DONTWAIT);
     }
     return ret;
 }
@@ -341,11 +333,7 @@ int netsnmp_udpbase_sendto(int fd, struct in_addr *srcip, int if_index,
 
     memcpy((struct in_addr *)CMSG_DATA(cm), srcip, sizeof(struct in_addr));
 
-#ifdef MSG_NOSIGNAL
-    return sendmsg(fd, &m, MSG_NOSIGNAL|MSG_DONTWAIT);
-#else
-    return sendmsg(fd, &m, MSG_DONTWAIT);
-#endif
+    return sendmsg(fd, &m, NETSNMP_NOSIGNAL|NETSNMP_DONTWAIT);
 }
 #endif
 #endif /* (linux && IP_PKTINFO) || IP_RECVDSTADDR */

commit 674e064a28144eb7833378a4efb4e18b29e51100

Make apps/sshtosnmp.c build again (was broken by commit 612247cdf14c1e745c384cefe1f3fde3e445b347 that changed MSG_NOSIGNAL into NETSNMP_NOSIGNAL)

apps/sshtosnmp.c
@@ -151,7 +151,7 @@ main(int argc, char **argv) {
         m.msg_flags             = 0;
         
         DEBUG("sending to sock");
-        rc = sendmsg(sock, &m, NETSNMP_NOSIGNAL|NETSNMP_DONTWAIT);
+        rc = sendmsg(sock, &m, MSG_NOSIGNAL|MSG_DONTWAIT);
         if (rc < 0) {
             fprintf(stderr, "failed to send startup message\n");
             DEBUG("failed to send startup message\n");