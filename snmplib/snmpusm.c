60dd9b42322e7e1599736ebec3f3d9de1e80acf1
Sachin Surendran <sachinsurendran@users.sourceforge.net>
2011-08-04 08:12:09 -0700

    CHANGES: snmptrapd: PATCH 3383441: from sachinsurendran: Fix crash on 5.4.4 due to access of freed memory
    Signed-off-by: Wes Hardaker <hardaker@users.sourceforge.net>

--- snmplib/snmpusm.c
+++ snmplib/snmpusm.c
@@ -293,7 +293,34 @@ usm_set_usmStateReference_sec_level(struct usmStateReference *ref,
     return 0;
 }
 
+int
+usm_clone_usmStateReference(struct usmStateReference *from, struct usmStateReference **to)
+{
+    int ret;
+    struct usmStateReference *cloned_usmStateRef;
 
+    if (from == NULL || to == NULL)
+        return -1;
+
+    *to = usm_malloc_usmStateReference();
+    cloned_usmStateRef = *to;
+
+    if (usm_set_usmStateReference_name(cloned_usmStateRef, from->usr_name, from->usr_name_length) ||
+        usm_set_usmStateReference_engine_id(cloned_usmStateRef, from->usr_engine_id, from->usr_engine_id_length) ||
+        usm_set_usmStateReference_auth_protocol(cloned_usmStateRef, from->usr_auth_protocol, from->usr_auth_protocol_length) ||
+        usm_set_usmStateReference_auth_key(cloned_usmStateRef, from->usr_auth_key, from->usr_auth_key_length) ||
+        usm_set_usmStateReference_priv_protocol(cloned_usmStateRef, from->usr_priv_protocol, from->usr_priv_protocol_length) ||
+        usm_set_usmStateReference_priv_key(cloned_usmStateRef, from->usr_priv_key, from->usr_priv_key_length) ||
+        usm_set_usmStateReference_sec_level(cloned_usmStateRef, from->usr_sec_level))
+    {
+        usm_free_usmStateReference(*to);
+        *to = NULL;
+        return -1;
+    }
+
+    return 0;
+
+}
 
 #ifdef NETSNMP_ENABLE_TESTING_CODE
 /*******************************************************************-o-******
