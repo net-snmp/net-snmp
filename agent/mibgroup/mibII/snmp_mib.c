net-snmp/net-snmp

commit bf7178b0b22587d43d1a80c269cd216bd5930214

agent: change type of snmp_enableauthentraps to long
Type of snmp_enableauthentraps variable is changed from int to long.
This fixes the bug with writing to snmpEnableAuthenTraps if server is
compiled for 64bit (snmpset reports wrong length). This bug was
introduced a long time ago (by faeecd0 commit).

Actually this is a 'less effort' type of fix but I wonder why we need a
long to store an INTEGER in the first place?

Signed-off-by: Ilya Yanok <yanok@emcraft.com>
Signed-off-by: Wes Hardaker <hardaker@users.sourceforge.net>

agent/mibgroup/mibII/snmp_mib.c
@@ -33,7 +33,7 @@
 	 *
 	 *********************/
 
-extern int      snmp_enableauthentraps;
+extern long     snmp_enableauthentraps;
 extern int      snmp_enableauthentrapsset;
 int             old_snmp_enableauthentraps;
 

agent/agent_trap.c
@@ -116,7 +116,7 @@ char           *snmp_trapcommunity = NULL;
 #define SNMP_AUTHENTICATED_TRAPS_ENABLED	1
 #define SNMP_AUTHENTICATED_TRAPS_DISABLED	2
 
-int             snmp_enableauthentraps = SNMP_AUTHENTICATED_TRAPS_DISABLED;
+long            snmp_enableauthentraps = SNMP_AUTHENTICATED_TRAPS_DISABLED;
 int             snmp_enableauthentrapsset = 0;
 
 /*

commit 9b7ff4abfc9529bb9e18a3b874201520bc0e7141

Unbreak the V5-4-patches branch - was broken by commit bf7178b0b22587d43d1a80c269cd216bd5930214 (agent: change type of snmp_enableauthentraps to long)

agent/mibgroup/mibII/snmp_mib.c
@@ -99,7 +99,7 @@ snmp_enableauthentraps_store(int a, int b, void *c, void *d)
     char            line[SNMP_MAXBUF_SMALL];
 
     if (snmp_enableauthentrapsset > 0) {
-        snprintf(line, SNMP_MAXBUF_SMALL, "pauthtrapenable %d",
+        snprintf(line, SNMP_MAXBUF_SMALL, "pauthtrapenable %ld",
                  snmp_enableauthentraps);
         snmpd_store_config(line);
     }

agent/mibgroup/mibII/snmp_mib.h
@@ -39,7 +39,7 @@ extern int      snmp_outgetnexts;       /* 26 - obsolete */
 extern int      snmp_outsetrequests;    /* 27 - obsolete */
 extern int      snmp_outgetresponses;   /* 28 - obsolete */
 extern int      snmp_outtraps;  /* 29 - obsolete */
-extern int      snmp_enableauthentraps; /* 30 - current */
+extern long     snmp_enableauthentraps; /* 30 - current */
 extern int      snmp_silentdrops;       /* 31 - current */
 extern int      snmp_proxydrops;        /* 32 - current */
 