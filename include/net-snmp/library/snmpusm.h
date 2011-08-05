net-snmp/net-snmp

commit 0e6842fe080cec853b08cd2323015ababc381ce1

CHANGES: snmptrapd: PATCH 3383441: from sachinsurendran: Fix crash on 5.4.4 due to access of freed memory
Signed-off-by: Wes Hardaker <hardaker@users.sourceforge.net>

include/net-snmp/library/snmpusm.h
@@ -136,6 +136,9 @@ extern          "C" {
                                                         usmStateReference
                                                         *ref,
                                                         int sec_level);
+    int             usm_clone_usmStateReference(struct usmStateReference *from,
+                                                    struct usmStateReference **to);
+
 
 #ifdef NETSNMP_ENABLE_TESTING_CODE
     void            emergency_print(u_char * field, u_int length);

snmplib/snmp_client.c
@@ -338,6 +338,7 @@ _clone_pdu_header(netsnmp_pdu *pdu)
 {
     netsnmp_pdu    *newpdu;
     struct snmp_secmod_def *sptr;
+    int ret;
 
     newpdu = (netsnmp_pdu *) malloc(sizeof(netsnmp_pdu));
     if (!newpdu)
@@ -376,6 +377,19 @@ _clone_pdu_header(netsnmp_pdu *pdu)
                           pdu->transport_data_length)) {
         snmp_free_pdu(newpdu);
         return 0;
+    }
+
+    if (pdu != NULL && pdu->securityStateRef &&
+        pdu->command == SNMP_MSG_TRAP2) {
+
+        ret = usm_clone_usmStateReference((struct usmStateReference *) pdu->securityStateRef,
+                (struct usmStateReference **) &newpdu->securityStateRef );
+
+        if (ret)
+        {
+            snmp_free_pdu(newpdu);
+            return 0;
+        }
     }
     if ((sptr = find_sec_mod(newpdu->securityModel)) != NULL &&
         sptr->pdu_clone != NULL) {

snmplib/snmpusm.c
@@ -269,7 +269,34 @@ usm_set_usmStateReference_sec_level(struct usmStateReference *ref,
     return 0;
 }
 
+int
+usm_clone_usmStateReference(struct usmStateReference *from, struct usmStateReference **to)
+{
+    int ret;
+    struct usmStateReference *cloned_usmStateRef;
 
+    if (from == NULL || to == NULL)
+        return -1;
+
+    *to = usm_malloc_usmStateReference();
+    cloned_usmStateRef = *to;
+
+    if (usm_set_usmStateReference_name(cloned_usmStateRef, from->usr_name, from->usr_name_length) ||
+        usm_set_usmStateReference_engine_id(cloned_usmStateRef, from->usr_engine_id, from->usr_engine_id_length) ||
+        usm_set_usmStateReference_auth_protocol(cloned_usmStateRef, from->usr_auth_protocol, from->usr_auth_protocol_length) ||
+        usm_set_usmStateReference_auth_key(cloned_usmStateRef, from->usr_auth_key, from->usr_auth_key_length) ||
+        usm_set_usmStateReference_priv_protocol(cloned_usmStateRef, from->usr_priv_protocol, from->usr_priv_protocol_length) ||
+        usm_set_usmStateReference_priv_key(cloned_usmStateRef, from->usr_priv_key, from->usr_priv_key_length) ||
+        usm_set_usmStateReference_sec_level(cloned_usmStateRef, from->usr_sec_level))
+    {
+        usm_free_usmStateReference(*to);
+        *to = NULL;
+        return -1;
+    }
+
+    return 0;
+
+}
 
 #ifdef NETSNMP_ENABLE_TESTING_CODE
 /*******************************************************************-o-******

commit 03049e1036c2c06cbd6e5ed5fe084999d5468a5f

Unbreak the V5-4-patches branch - was broken by commit 0e6842fe080cec853b08cd2323015ababc381ce1 (CHANGES: snmptrapd: PATCH 3383441: from sachinsurendran: Fix crash on 5.4.4 due to access of freed memory).

include/net-snmp/library/snmpusm.h
@@ -7,6 +7,8 @@
 #ifndef SNMPUSM_H
 #define SNMPUSM_H
 
+#include <net-snmp/library/callback.h>
+
 #ifdef __cplusplus
 extern          "C" {
 #endif