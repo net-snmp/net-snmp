net-snmp/net-snmp

commit 31fa07cd9ffde46d41d2b5838c3fc4d01548bfb5

CHANGES: snmpd: purge zombies of pass_persist child processes.
If pass_persist process ends, we should process its sigchild signal.

agent/mibgroup/ucd-snmp/pass_persist.c
@@ -55,6 +55,7 @@ static void     close_persist_pipe(int iindex);
 static int      open_persist_pipe(int iindex, char *command);
 static void     destruct_persist_pipes(void);
 static int      write_persist_pipe(int iindex, const char *data);
+static void     sigchld_handler(int sig);
 
 /*
  * These are defined in pass.c 
@@ -82,6 +83,13 @@ init_pass_persist(void)
                                   pass_persist_parse_config,
                                   pass_persist_free_config,
                                   "miboid program");
+    register_signal(SIGCHLD, sigchld_handler);
+}
+
+void
+deinit_pass_persist(void)
+{
+    unregister_signal(SIGCHLD);
 }
 
 void
@@ -806,3 +814,24 @@ close_persist_pipe(int iindex)
     }
 
 }
+
+static void sigchld_handler(int sig) {
+    int i;
+ 
+    if (!persist_pipes)
+        return;
+
+#if HAVE_SYS_WAIT_H
+    for (i = 1; i <= numpersistpassthrus; i++) {
+        if (persist_pipes[i].pid > 0) {
+            if (waitpid(persist_pipes[i].pid, 0, WNOHANG)) {
+                DEBUGMSGTL(("ucd-snmp/pass_persist",
+                            "sigchld_handler: closing persist_pipe %d\n",
+                            i));
+                close_persist_pipe(i);
+            }
+        }
+    }
+#endif
+}
+

agent/mibgroup/ucd-snmp/pass_persist.h
@@ -7,6 +7,7 @@
 config_require(util_funcs)
 
 void            init_pass_persist(void);
+void            deinit_pass_persist(void);
 extern FindVarMethod var_extensible_pass_persist;
 extern WriteMethod setPassPersist;
 

commit 04b83bc48df495fcd2f5db1f6cbbad7f68db8548

CHANGES: snmpd: pass_persist:  Fixes a race condition introduced in commit 31fa07cd9ffde46d41d2b5838c3fc4d01548bfb5.
The following race condition could happen:
- A system call fails and errno is set accordingly by the C library.
- SIGCHLD handler invokes a system call that fails and hence overwrites errno.
- SIGCHLD handler leaves.
- original context checks errno and finds the value from the system call invoked
  by the SIGCHLD handler instead of the system call that failed originally.

Other fixes included in this patch:
- Renamed deinit_pass_persist() into shutdown_pass_persist() such that this
  function gets called during agent shutdown.
- Made sure that shutdown_pass_persist() is called by the Windows snmpd.exe too.
- Windows: avoid calling CloseHandle(INVALID_HANDLE_VALUE).
- Windows: avoid that closing a pass_persist pipe triggers a crash.
- Windows: pass_persist: close handles of finished processes.

agent/mibgroup/ucd-snmp/pass_persist.c
@@ -51,14 +51,13 @@ struct persist_pipe_type {
     int             fdIn, fdOut;
     netsnmp_pid_t   pid;
 }              *persist_pipes = (struct persist_pipe_type *) NULL;
+static unsigned pipe_check_alarm_id;
 static int      init_persist_pipes(void);
 static void     close_persist_pipe(int iindex);
 static int      open_persist_pipe(int iindex, char *command);
+static void     check_persist_pipes(unsigned clientreg, void *clientarg);
 static void     destruct_persist_pipes(void);
 static int      write_persist_pipe(int iindex, const char *data);
-#ifndef WIN32
-static void     sigchld_handler(int sig);
-#endif
 
 /*
  * These are defined in pass.c 
@@ -87,17 +86,19 @@ init_pass_persist(void)
                                   pass_persist_parse_config,
                                   pass_persist_free_config,
                                   "miboid program");
-#ifndef WIN32
-    register_signal(SIGCHLD, sigchld_handler);
-#endif
+    pipe_check_alarm_id = snmp_alarm_register(10, SA_REPEAT, check_persist_pipes, NULL);
 }
 
 void
-deinit_pass_persist(void)
+shutdown_pass_persist(void)
 {
-#ifndef WIN32
-    unregister_signal(SIGCHLD);
-#endif
+    if (pipe_check_alarm_id) {
+        snmp_alarm_unregister(pipe_check_alarm_id);
+        pipe_check_alarm_id = 0;
+    }
+
+    /* Close any open pipes. */
+    destruct_persist_pipes();
 }
 
 void
@@ -209,11 +210,6 @@ void
 pass_persist_free_config(void)
 {
     struct extensible *etmp, *etmp2;
-
-    /*
-     * Close any open pipes to any programs 
-     */
-    destruct_persist_pipes();
 
     for (etmp = persistpassthrus; etmp != NULL;) {
         etmp2 = etmp;
@@ -584,6 +580,44 @@ init_persist_pipes(void)
     return persist_pipes ? 1 : 0;
 }
 
+/**
+ * Return true if and only if the process associated with the persistent
+ * pipe has stopped.
+ *
+ * @param[in] idx Persistent pipe index.
+ */
+static int process_stopped(int idx)
+{
+    if (persist_pipes[idx].pid != NETSNMP_NO_SUCH_PROCESS) {
+#if HAVE_SYS_WAIT_H
+        return waitpid(persist_pipes[idx].pid, NULL, WNOHANG) > 0;
+#endif
+#if defined(WIN32) && !defined (mingw32) && !defined(HAVE_SIGNAL)
+        return WaitForSingleObject(persist_pipes[idx].pid, 0) == WAIT_OBJECT_0;
+#endif
+    }
+    return 0;
+}
+
+/**
+ * Iterate over all persistent pipes and close those pipes of which the
+ * associated process has stopped.
+ */
+static void check_persist_pipes(unsigned clientreg, void *clientarg)
+{
+    int             i;
+
+    if (!persist_pipes)
+        return;
+
+    for (i = 0; i <= numpersistpassthrus; i++) {
+        if (process_stopped(i)) {
+            snmp_log(LOG_INFO, "pass_persist[%d]: child process stopped - closing pipe\n", i);
+            close_persist_pipe(i);
+        }
+    }
+}
+
 /*
  * Destruct our persistent pipes
  *
@@ -801,7 +835,14 @@ close_persist_pipe(int iindex)
         persist_pipes[iindex].fOut = (FILE *) 0;
     }
     if (persist_pipes[iindex].fdOut != -1) {
+#ifndef WIN32
+        /*
+         * The sequence open()/fdopen()/fclose()/close() triggers an access
+         * violation with the MSVC runtime. Hence skip the close() call when
+         * using the MSVC runtime.
+         */
         close(persist_pipes[iindex].fdOut);
+#endif
         persist_pipes[iindex].fdOut = -1;
     }
     if (persist_pipes[iindex].fIn) {
@@ -809,16 +850,17 @@ close_persist_pipe(int iindex)
         persist_pipes[iindex].fIn = (FILE *) 0;
     }
     if (persist_pipes[iindex].fdIn != -1) {
+#ifndef WIN32
+        /*
+         * The sequence open()/fdopen()/fclose()/close() triggers an access
+         * violation with the MSVC runtime. Hence skip the close() call when
+         * using the MSVC runtime.
+         */
         close(persist_pipes[iindex].fdIn);
+#endif
         persist_pipes[iindex].fdIn = -1;
     }
 
-#if defined(WIN32) && !defined (mingw32) && !defined (HAVE_SIGNAL)
-    if (!CloseHandle(persist_pipes[iindex].pid)) {
-          DEBUGMSGTL(("ucd-snmp/pass_persist","close_persist_pipe pid: close error\n"));
-        } 
-#endif
-    
 #ifdef __uClinux__
 	/*remove the pipes*/
 	unlink(fifo_in_path);
@@ -829,30 +871,12 @@ close_persist_pipe(int iindex)
 #if HAVE_SYS_WAIT_H
         waitpid(persist_pipes[iindex].pid, NULL, 0);
 #endif
+#if defined(WIN32) && !defined (mingw32) && !defined (HAVE_SIGNAL)
+        if (!CloseHandle(persist_pipes[iindex].pid)) {
+            DEBUGMSGTL(("ucd-snmp/pass_persist","close_persist_pipe pid: close error\n"));
+        }
+#endif
         persist_pipes[iindex].pid = NETSNMP_NO_SUCH_PROCESS;
     }
 
 }
-
-#ifndef WIN32
-static void sigchld_handler(int sig) {
-    int i;
- 
-    if (!persist_pipes)
-        return;
-
-#if HAVE_SYS_WAIT_H
-    for (i = 1; i <= numpersistpassthrus; i++) {
-        if (persist_pipes[i].pid > 0) {
-            if (waitpid(persist_pipes[i].pid, 0, WNOHANG)) {
-                DEBUGMSGTL(("ucd-snmp/pass_persist",
-                            "sigchld_handler: closing persist_pipe %d\n",
-                            i));
-                close_persist_pipe(i);
-            }
-        }
-    }
-#endif
-}
-#endif
-

agent/mibgroup/ucd-snmp/pass_persist.h
@@ -7,7 +7,7 @@
 config_require(util_funcs)
 
 void            init_pass_persist(void);
-void            deinit_pass_persist(void);
+void            shutdown_pass_persist(void);
 extern FindVarMethod var_extensible_pass_persist;
 extern WriteMethod setPassPersist;
 

win32/mib_module_shutdown.h
@@ -2,6 +2,10 @@
   if (should_init("proxy")) shutdown_proxy();
   if (should_init("snmpNotifyFilterTable")) shutdown_snmpNotifyFilterTable();
  
+#ifdef USING_UCD_SNMP_MODULE
+  if (should_init("pass_persist")) shutdown_pass_persist();
+#endif
+
 #ifdef USING_WINEXTDLL_MODULE
    if (should_init("winExtDLL")) shutdown_winExtDLL();
 #endif