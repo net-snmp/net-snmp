d2252d016693807326f72b66123649dea00af916
Bart Van Assche <bvanassche@acm.org>
2011-09-29 10:20:08 +0200

    CHANGES: testing: Require a semicolon after OK(), OKF() and PLAN()
    Also, let OKF() emit a single line of output such that all output it
    produces is visible in the output of "make test".

--- include/net-snmp/library/testing.h
+++ include/net-snmp/library/testing.h
@@ -7,10 +7,10 @@
 static int __test_counter = 0;
 static int __did_plan = 0;
 
-#define OK(isok, description) { printf("%s %d - %s\n", ((isok) ? "ok" : "not ok"), ++__test_counter, description); }
+#define OK(isok, description) do { printf("%s %d - %s\n", ((isok) ? "ok" : "not ok"), ++__test_counter, description); } while (0)
 
-#define OKF(isok, description) { printf("%s %d - \n", ((isok) ? "ok" : "not ok"), ++__test_counter); printf description; printf("\n"); }
+#define OKF(isok, description) do { printf("%s %d - ", ((isok) ? "ok" : "not ok"), ++__test_counter); printf description; printf("\n"); } while (0)
 
-#define PLAN(number) { printf("1..%d\n", number); __did_plan = 1;}
+#define PLAN(number) do { printf("1..%d\n", number); __did_plan = 1; } while (0)
 
 #endif /* NETSNMP_LIBRARY_TESTING_H */
