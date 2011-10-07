net-snmp/net-snmp

commit 71d8293f387a6cd66bb0dbb13c0f50174d2e678b

CHANGES: snmpd: reworked hrStorageTable to support large filesystems.
The hrStorageAllocationUnits does not report real allocation unit size,
but some calculated value so hrStorageAllocationUnits * hrStorageSize
gives real size of the filesystem.

This calculation happens only when hrStorageSize is too small (32bits)
for filesystem size, e.g. filesystems larger than 8TB with 4096 bytes
block size.

This calculation can be turned off by 'realStorageUnits' config option.

git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@19941 06827809-a52a-0410-b366-d66718629ded

agent/mibgroup/hardware/fsys/hw_fsys.c
@@ -249,7 +249,7 @@ _fsys_create_entry( void )
  *    (attempting to avoid 32-bit overflow!)
  */
 unsigned long long
-_fsys_to_K( int size, int units )
+_fsys_to_K( unsigned long long size, unsigned long long units )
 {
     int factor = 1;
 
@@ -311,3 +311,25 @@ netsnmp_fsys_avail( netsnmp_fsys_info *f) {
     return (int)v;
 }
 
+#ifndef INT32_MAX
+#define INT32_MAX 0x7fffffff
+#endif
+
+/* recalculate f->size_32, used_32, avail_32 and units_32 from f->size & comp.*/
+void
+netsnmp_fsys_calculate32( netsnmp_fsys_info *f)
+{
+    unsigned long long s = f->size;
+    unsigned long long u = f->units;
+    int factor = 0;
+    while (s > INT32_MAX) {
+        s = s >> 1;
+        u = u << 1;
+        factor++;
+    }
+
+    f->size_32 = s;
+    f->units_32 = u;
+    f->avail_32 = f->avail << factor;
+    f->used_32 = f->used << factor;
+}

agent/mibgroup/hardware/fsys/fsys_getfsstats.c
@@ -173,6 +173,6 @@ netsnmp_fsys_arch_load( void )
         if (  stats[i].NSFS_FLAGS & MNT_ROOTFS ) {
             entry->flags |= NETSNMP_FS_FLAG_BOOTABLE;
         }
-        
+        netsnmp_fsys_calculate32(entry);
     }
 }

agent/mibgroup/hardware/fsys/fsys_mntctl.c
@@ -171,6 +171,7 @@ netsnmp_fsys_arch_load( void )
         entry->avail =  stat_buf.f_bavail;
         entry->inums_total = stat_buf.f_files;
         entry->inums_avail = stat_buf.f_ffree;
+        netsnmp_fsys_calculate32(entry);
     }
     free(aixmnt);
     aixmnt  = NULL;

agent/mibgroup/hardware/fsys/fsys_mntent.c
@@ -237,6 +237,7 @@ netsnmp_fsys_arch_load( void )
         entry->avail =  stat_buf.f_bavail;
         entry->inums_total = stat_buf.f_files;
         entry->inums_avail = stat_buf.f_ffree;
+        netsnmp_fsys_calculate32(entry);
     }
     fclose( fp );
 }

agent/mibgroup/host/hrh_storage.c
@@ -130,6 +130,10 @@ init_hrh_storage(void)
 			       NETSNMP_DS_APPLICATION_ID,
 			       NETSNMP_DS_AGENT_SKIPNFSINHOSTRESOURCES);
 
+    netsnmp_ds_register_config(ASN_BOOLEAN, appname, "realStorageUnits",
+                   NETSNMP_DS_APPLICATION_ID,
+                   NETSNMP_DS_AGENT_REALSTORAGEUNITS);
+
     snmpd_register_config_handler("storageUseNFS", parse_storage_config, NULL,
 	"1 | 2\t\t(1 = enable, 2 = disable)");
 }
@@ -415,27 +419,39 @@ really_try_next:
             return (u_char *) mem->descr;
         }
     case HRSTORE_UNITS:
-        if (store_idx > NETSNMP_MEM_TYPE_MAX)
-            long_return = HRFS_entry->units;
-        else {
+        if (store_idx > NETSNMP_MEM_TYPE_MAX) {
+            if (netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID,
+                    NETSNMP_DS_AGENT_REALSTORAGEUNITS))
+                long_return = HRFS_entry->units & 0xffffffff;
+            else
+                long_return = HRFS_entry->units_32;
+        } else {
             if ( !mem || mem->units == -1 )
                 goto try_next;
             long_return = mem->units;
         }
         return (u_char *) & long_return;
     case HRSTORE_SIZE:
-        if (store_idx > NETSNMP_MEM_TYPE_MAX)
-            long_return = HRFS_entry->size;
-        else {
+        if (store_idx > NETSNMP_MEM_TYPE_MAX) {
+            if (netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID,
+                    NETSNMP_DS_AGENT_REALSTORAGEUNITS))
+                long_return = HRFS_entry->size & 0xffffffff;
+            else
+                long_return = HRFS_entry->size_32;
+        } else {
             if ( !mem || mem->size == -1 )
                 goto try_next;
             long_return = mem->size;
         }
         return (u_char *) & long_return;
     case HRSTORE_USED:
-        if (store_idx > NETSNMP_MEM_TYPE_MAX)
-            long_return = HRFS_entry->used;
-        else {
+        if (store_idx > NETSNMP_MEM_TYPE_MAX) {
+            if (netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID,
+                    NETSNMP_DS_AGENT_REALSTORAGEUNITS))
+                long_return = HRFS_entry->used & 0xffffffff;
+            else
+                long_return = HRFS_entry->used_32;
+        } else {
             if ( !mem || mem->size == -1 || mem->free == -1 )
                 goto try_next;
             long_return = mem->size - mem->free;

include/net-snmp/agent/ds_agent.h
@@ -27,6 +27,7 @@
 #define NETSNMP_DS_AGENT_DONT_LOG_TCPWRAPPERS_CONNECTS 12   /* 1 = disable logging */
 #define NETSNMP_DS_APP_DONT_LOG         NETSNMP_DS_AGENT_DONT_RETAIN_NOTIFICATIONS /* compat */
 #define NETSNMP_DS_AGENT_SKIPNFSINHOSTRESOURCES    13   /* 1 = don't store NFS entries in hrStorageTable */
+#define NETSNMP_DS_AGENT_REALSTORAGEUNITS 14    /* 1 = use real allocation units in hrStorageTable, 0 = recalculate it to fit 32bits */
 
 /* WARNING: The trap receiver uses DS flags and must not conflict with
    these!  If you use a value above 15, change the minimimum DS bool

include/net-snmp/agent/hardware/fsys.h
@@ -60,13 +60,19 @@ struct netsnmp_fsys_info_s {
      char device[SNMP_MAXPATH+1];
      int  type;
 
-     long size;
-     long used;
-     long avail;
-     long units;
+     unsigned long long size;
+     unsigned long long used;
+     unsigned long long avail;
+     unsigned long long units;
 
-     long inums_total;
-     long inums_avail;
+     /* artificially computed values, both 'size_32' and 'units_32' fit INT32 */
+     unsigned long size_32;
+     unsigned long used_32;
+     unsigned long avail_32;
+     unsigned long units_32;
+
+     unsigned long long inums_total;
+     unsigned long long inums_avail;
 
      int  minspace;
      int  minpercent;
@@ -99,3 +105,5 @@ int netsnmp_fsys_avail(netsnmp_fsys_info* );
 unsigned long long netsnmp_fsys_size_ull( netsnmp_fsys_info* );
 unsigned long long netsnmp_fsys_used_ull( netsnmp_fsys_info* );
 unsigned long long netsnmp_fsys_avail_ull(netsnmp_fsys_info* );
+
+void netsnmp_fsys_calculate32( netsnmp_fsys_info *f);

man/snmpd.conf.5.def
@@ -572,6 +572,25 @@ Historically, the Net-SNMP agent has reported such file systems
 as 'Fixed Disks', and this is still the default behaviour.
 Setting this directive to '1' reports such file systems as
 \'Network Disks', as required by the Host Resources MIB.
+.IP "realStorageUnits"
+controlls how the agent reports hrStorageAllocationUnits, hrStorageSize and
+hrStorageUsed in hrStorageTable.
+For big storage drives with small allocation units the agent re-calculates
+these values so they all fit Integer32 and
+hrStorageAllocationUnits x hrStorageSize
+gives real size of the storage.
+.RS
+.IP "Example:"
+Linux xfs 16TB filesystem with 4096 bytes large blocks will be
+reported as  hrStorageAllocationUnits = 8192 and hrStorageSize = 2147483647,
+so 8192 x 2147483647 gives real size of the filesystem (=16 TB).
+.RE
+.IP
+Setting this directive to '1' turns off
+this calculation and the agent reports real hrStorageAllocationUnits, but it
+might report wrong hrStorageSize for big drives because the value won't fit into
+Integer32. In this case, hrStorageAllocationUnits x hrStorageSize won't give
+real size of the storage.
 .SS Process Monitoring 
 The \fChrSWRun\fR group of the Host Resources MIB provides
 information about individual processes running on the local system.

commit 76ff25d9bf97579e7213102065fd5096f049a4c5

CHANGES: snmpd: HOST-RESOURCES-MIB::hrStorageTable: fix bug in handling large filesystems, where large means above 8 TB (= 2**31 * 4096 bytes).
This patch fixes a bug introduced in commit
71d8293f387a6cd66bb0dbb13c0f50174d2e678b.
For the original bug report, see also
https://sourceforge.net/tracker/?func=detail&atid=112694&aid=3419825&group_id=12694.

agent/mibgroup/hardware/fsys/hw_fsys.c
@@ -321,19 +321,23 @@ netsnmp_fsys_avail( netsnmp_fsys_info *f) {
 
 /* recalculate f->size_32, used_32, avail_32 and units_32 from f->size & comp.*/
 void
-netsnmp_fsys_calculate32( netsnmp_fsys_info *f)
+netsnmp_fsys_calculate32(netsnmp_fsys_info *f)
 {
     unsigned long long s = f->size;
-    unsigned long long u = f->units;
-    int factor = 0;
+    unsigned shift = 0;
+
     while (s > INT32_MAX) {
         s = s >> 1;
-        u = u << 1;
-        factor++;
+        shift++;
     }
 
     f->size_32 = s;
-    f->units_32 = u;
-    f->avail_32 = f->avail << factor;
-    f->used_32 = f->used << factor;
+    f->units_32 = f->units << shift;
+    f->avail_32 = f->avail >> shift;
+    f->used_32 = f->used >> shift;
+
+    DEBUGMSGTL(("fsys", "Results of 32-bit conversion: size %llu -> %lu;"
+		" units %llu -> %lu; avail %llu -> %lu; used %llu -> %lu\n",
+		f->size, f->size_32, f->units, f->units_32,
+		f->avail, f->avail_32, f->used, f->used_32));
 }