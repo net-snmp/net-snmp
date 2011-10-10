net-snmp/net-snmp

commit 76ff25d9bf97579e7213102065fd5096f049a4c5

CHANGES: snmpd: HOST-RESOURCES-MIB::hrStorageTable: fix bug in handling large filesystems, where large means above 8 TB (= 2**31 * 4096 bytes).
This patch fixes a bug introduced in commit
71d8293f387a6cd66bb0dbb13c0f50174d2e678b.
For the original bug report, see also
https://sourceforge.net/tracker/?func=detail&atid=112694&aid=3419825&group_id=12694.

agent/mibgroup/hardware/fsys/hw_fsys.c
@@ -321,19 +321,23 @@ netsnmp_fsys_avail( netsnmp_fsys_info *f) {
 
 /* recalculate f->size_32, used_32, avail_32 and units_32 from f->size & comp.*/
 void
-netsnmp_fsys_calculate32( netsnmp_fsys_info *f)
+netsnmp_fsys_calculate32(netsnmp_fsys_info *f)
 {
     unsigned long long s = f->size;
-    unsigned long long u = f->units;
-    int factor = 0;
+    unsigned shift = 0;
+
     while (s > INT32_MAX) {
         s = s >> 1;
-        u = u << 1;
-        factor++;
+        shift++;
     }
 
     f->size_32 = s;
-    f->units_32 = u;
-    f->avail_32 = f->avail << factor;
-    f->used_32 = f->used << factor;
+    f->units_32 = f->units << shift;
+    f->avail_32 = f->avail >> shift;
+    f->used_32 = f->used >> shift;
+
+    DEBUGMSGTL(("fsys", "Results of 32-bit conversion: size %llu -> %lu;"
+		" units %llu -> %lu; avail %llu -> %lu; used %llu -> %lu\n",
+		f->size, f->size_32, f->units, f->units_32,
+		f->avail, f->avail_32, f->used, f->used_32));
 }

commit 664d3ae107d754051b8c42279d459aa57a1be1c3

MinGW, hw_fsys: Use proper format specifiers in a debug statement.
This patch fixes an issue introduced in commit 76ff25d.

agent/mibgroup/hardware/fsys/hw_fsys.c
@@ -3,6 +3,9 @@
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/agent/hardware/fsys.h>
+#ifdef HAVE_INTTYPES_H
+#include <inttypes.h>
+#endif
 
 netsnmp_feature_child_of(hw_fsys_get_container, netsnmp_unused)
 
@@ -336,8 +339,9 @@ netsnmp_fsys_calculate32(netsnmp_fsys_info *f)
     f->avail_32 = f->avail >> shift;
     f->used_32 = f->used >> shift;
 
-    DEBUGMSGTL(("fsys", "Results of 32-bit conversion: size %llu -> %lu;"
-		" units %llu -> %lu; avail %llu -> %lu; used %llu -> %lu\n",
+    DEBUGMSGTL(("fsys", "Results of 32-bit conversion: size %" PRIu64 " -> %lu;"
+		" units %" PRIu64 " -> %lu; avail %" PRIu64 " -> %lu;"
+                " used %" PRIu64 " -> %lu\n",
 		f->size, f->size_32, f->units, f->units_32,
 		f->avail, f->avail_32, f->used, f->used_32));
 }