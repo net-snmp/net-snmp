4495e17c078ac6997a9c7fa1a88db0644e919491
Bart Van Assche <bvanassche@acm.org>
2011-10-26 09:32:46 +0200

    Fix compiler warnings about casting away const

--- snmplib/openssl/openssl_cfb128.c
+++ snmplib/openssl/openssl_cfb128.c
@@ -98,7 +98,7 @@ void CRYPTO_cfb128_encrypt(const unsigned char *in, unsigned char *out,
 			(*block)(ivec, ivec, key);
 			for (n=0; n<16; n+=sizeof(size_t)) {
 				*(size_t*)(out+n) =
-				*(size_t*)(ivec+n) ^= *(size_t*)(in+n);
+				*(size_t*)(ivec+n) ^= *(const size_t*)(in+n);
 			}
 			len -= 16;
 			out += 16;
@@ -142,8 +142,8 @@ void CRYPTO_cfb128_encrypt(const unsigned char *in, unsigned char *out,
 		while (len>=16) {
 			(*block)(ivec, ivec, key);
 			for (n=0; n<16; n+=sizeof(size_t)) {
-				size_t t = *(size_t*)(in+n);
-				*(size_t*)(out+n) = *(size_t*)(ivec+n) ^ t;
+				size_t t = *(const size_t*)(in+n);
+				*(size_t*)(out+n) = *(const size_t*)(ivec+n) ^ t;
 				*(size_t*)(ivec+n) = t;
 			}
 			len -= 16;
