net-snmp/net-snmp

commit 85d96c4a61b3fb5caeebbab7155c2eff35d8cdfc

more dns related tweaks
 - add netsnmp_gethostbyaddr
 - always define netsnmp_* versions; logonce if underlying
   function is not available
 - use new functions in disman mibs

git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@20427 06827809-a52a-0410-b366-d66718629ded

snmplib/system.c
@@ -788,11 +788,11 @@ netsnmp_gethostbyname_v4(const char* name, in_addr_t *addr_out)
 #endif
 }
 
-#if HAVE_GETADDRINFO
 int
 netsnmp_getaddrinfo(const char *name, const char *service,
                     const struct addrinfo *hints, struct addrinfo **res)
 {
+#if HAVE_GETADDRINFO
     struct addrinfo *addrs = NULL;
     struct addrinfo hint;
     int             err;
@@ -836,13 +836,16 @@ netsnmp_getaddrinfo(const char *name, const char *service,
                     ntohs(((struct sockaddr_in*)addrs->ai_addr)->sin_port)));
     }
     return 0;
-}
+#else
+    NETSNMP_LOGONCE((LOG_ERR, "getaddrinfo not available"));
+    return EAI_FAIL;
 #endif /* getaddrinfo */
+}
 
-#if HAVE_GETHOSTBYNAME
 struct hostent *
 netsnmp_gethostbyname(const char *name)
 {
+#if HAVE_GETHOSTBYNAME
 #ifdef DNSSEC_LOCAL_VALIDATION
     val_status_t val_status;
 #endif
@@ -880,8 +883,56 @@ netsnmp_gethostbyname(const char *name)
                     "%s resolved okay\n", name));
     }
     return hp;
-}
+#else
+    NETSNMP_LOGONCE((LOG_ERR, "gethostbyname not available"));
+    return NULL;
 #endif /* HAVE_GETHOSTBYNAME */
+}
+
+struct hostent *
+netsnmp_gethostbyaddr(const void *addr, socklen_t len, int type)
+{
+#if HAVE_GETHOSTBYADDR
+    struct hostent *hp = NULL;
+    struct sockaddr_in *saddr_in =
+        NETSNMP_REMOVE_CONST(struct sockaddr_in *,addr);
+
+    DEBUGMSGTL(("dns:gethostbyaddr", "resolving { AF_INET, %s:%hu }\n",
+                inet_ntoa(saddr_in->sin_addr), ntohs(saddr_in->sin_port)));
+
+#ifdef DNSSEC_LOCAL_VALIDATION
+    val_status_t val_status;
+    hp = val_gethostbyaddr(NULL, (const char*)&saddr_in->sin_addr,
+                           sizeof(struct in_addr), AF_INET, &val_status);
+    DEBUGMSGTL(("dns:sec:val", "val_status %d / %s; trusted: %d\n",
+                val_status, p_val_status(val_status),
+                val_istrusted(val_status)));
+    if (!val_istrusted(val_status)) {
+        snmp_log(LOG_WARNING,
+                 "The authenticity of DNS response is not trusted (%s)\n.",
+                 p_val_status(val_status));
+        hp = NULL;
+    }
+    else if (val_does_not_exist(val_status) && hp)
+        hp = NULL;
+#else
+    hp = gethostbyaddr((gethost_addrptr_t) &saddr_in->sin_addr,
+                       sizeof(struct in_addr), AF_INET);
+#endif
+    if (hp == NULL) {
+        DEBUGMSGTL(("dns:gethostbyaddr", "couldn't resolve addr\n"));
+    } else if (hp->h_addrtype != AF_INET) {
+        DEBUGMSGTL(("dns:gethostbyaddr",
+                    "warning: response for addr not AF_INET!\n"));
+    } else {
+        DEBUGMSGTL(("dns:gethostbyaddr", "addr resolved okay\n"));
+    }
+    return hp;
+#else
+    NETSNMP_LOGONCE((LOG_ERR, "gethostbyname not available"));
+    return NULL;
+#endif
+}
 
 /*******************************************************************/
 

agent/mibgroup/disman/nslookup/lookupCtlTable.c
@@ -538,7 +538,7 @@ run_lookup(struct lookupTable_data *item)
         }
 
         gettimeofday(&tpstart, NULL);
-        lookup = gethostbyaddr(&addr_in, sizeof(addr_in), AF_INET);
+        lookup = netsnmp_gethostbyaddr(&addr_in, sizeof(addr_in), AF_INET);
         gettimeofday(&tpend, NULL);
         timeuse = 1000000 * (tpend.tv_sec - tpstart.tv_sec) +
             tpend.tv_usec - tpstart.tv_usec;
@@ -588,7 +588,7 @@ run_lookup(struct lookupTable_data *item)
 #endif
 
         gettimeofday(&tpstart, NULL);
-        lookup = gethostbyname(address);
+        lookup = netsnmp_gethostbyname(address);
         gettimeofday(&tpend, NULL);
         timeuse4 = 1000000 * (tpend.tv_sec - tpstart.tv_sec) +
             tpend.tv_usec - tpstart.tv_usec;
@@ -630,7 +630,7 @@ run_lookup(struct lookupTable_data *item)
 
 #if HAVE_GETADDRINFO
         gettimeofday(&tpstart, NULL);
-        res = getaddrinfo(address, NULL, &hints, &ais);
+        res = netsnmp_getaddrinfo(address, NULL, &hints, &ais);
         gettimeofday(&tpend, NULL);
         timeuse6 = 1000000 * (tpend.tv_sec - tpstart.tv_sec) +
             tpend.tv_usec - tpstart.tv_usec;
@@ -749,7 +749,7 @@ run_lookup(struct lookupTable_data *item)
             DEBUGMSGTL(("lookupCtlTable", "error! \n"));
 
         gettimeofday(&tpstart, NULL);
-        lookup = gethostbyaddr(&addr_in6, sizeof(addr_in6), AF_INET6);
+        lookup = netsnmp_gethostbyaddr(&addr_in6, sizeof(addr_in6), AF_INET6);
         gettimeofday(&tpend, NULL);
         timeuse = 1000000 * (tpend.tv_sec - tpstart.tv_sec) +
             tpend.tv_usec - tpstart.tv_usec;

agent/mibgroup/disman/ping/pingCtlTable.c
@@ -1027,7 +1027,7 @@ host_serv(const char *host, const char *serv, int family, int socktype)
     hints.ai_family = family;   /* AF_UNSPEC, AF_INET, AF_INET6, etc. */
     hints.ai_socktype = socktype;       /* 0, SOCK_STREAM, SOCK_DGRAM, etc. */
 
-    if ((n = getaddrinfo(host, serv, &hints, &res)) != 0)
+    if ((n = netsnmp_getaddrinfo(host, serv, &hints, &res)) != 0)
         return (NULL);
 
     return (res);               /* return pointer to first on linked list */
@@ -1055,7 +1055,7 @@ Host_serv(const char *host, const char *serv, int family, int socktype)
     hints.ai_family = family;   /* 0, AF_INET, AF_INET6, etc. */
     hints.ai_socktype = socktype;       /* 0, SOCK_STREAM, SOCK_DGRAM, etc. */
 
-    if ((n = getaddrinfo(host, serv, &hints, &res)) != 0)
+    if ((n = netsnmp_getaddrinfo(host, serv, &hints, &res)) != 0)
 #if HAVE_GAI_STRERROR
         printf("host_serv error for %s, %s: %s",
                (host == NULL) ? "(no hostname)" : host,
@@ -5778,8 +5778,8 @@ pr_addr(struct in6_addr *addr, int options)
     struct hostent *hp = NULL;
 
     if (!(options & F_NUMERIC))
-        hp = gethostbyaddr((__u8 *) addr, sizeof(struct in6_addr),
-                           AF_INET6);
+        hp = netsnmp_gethostbyaddr((__u8 *) addr, sizeof(struct in6_addr),
+                                   AF_INET6);
 
     return hp ? hp->h_name : pr_addr_n(addr);
 }

agent/mibgroup/disman/traceroute/traceRouteCtlTable.c
@@ -5981,7 +5981,7 @@ gethostinfo(register char *hostname)
         return (hi);
     }
 
-    hp = gethostbyname(hostname);
+    hp = netsnmp_gethostbyname(hostname);
     if (hp == NULL) {
         Fprintf(stderr, "%s: unknown host %s\n", "traceroute", hostname);
         printf("hp=NULL\n");

apps/snmpnetstat/inet.c
@@ -616,7 +616,8 @@ inetname(struct in_addr *inp)
 				cp = np->n_name;
 		}
 		if (cp == NULL) {
-			hp = gethostbyaddr((char *)inp, sizeof (*inp), AF_INET);
+			hp = netsnmp_gethostbyaddr((char *)inp, sizeof (*inp),
+                                                   AF_INET);
 			if (hp) {
 				if ((cp = strchr(hp->h_name, '.')) &&
 				    !strcmp(cp + 1, domain))

apps/snmpnetstat/route.c
@@ -287,7 +287,7 @@ routename(in_addr_t in)
 	}
 	cp = NULL;
 	if (!nflag) {
-		hp = gethostbyaddr((char *)&in, sizeof (struct in_addr),
+		hp = netsnmp_gethostbyaddr((char *)&in, sizeof (struct in_addr),
 		    AF_INET);
 		if (hp) {
 			if ((cp = strchr(hp->h_name, '.')) &&

apps/snmptrapd_log.c
@@ -655,7 +655,7 @@ realloc_handle_ip_fmt(u_char ** buf, size_t * buf_len, size_t * out_len,
          */
         if (!netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID, 
                                     NETSNMP_DS_APP_NUMERIC_IP)) {
-            host = gethostbyaddr((char *) pdu->agent_addr, 4, AF_INET);
+            host = netsnmp_gethostbyaddr((char *) pdu->agent_addr, 4, AF_INET);
         }
         if (host != NULL) {
             if (!snmp_strcat(&temp_buf, &temp_buf_len, &temp_out_len, 1,
@@ -1373,7 +1373,7 @@ realloc_format_plain_trap(u_char ** buf, size_t * buf_len,
      */
     if (!netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID, 
                                 NETSNMP_DS_APP_NUMERIC_IP)) {
-        host = gethostbyaddr((char *) pdu->agent_addr, 4, AF_INET);
+        host = netsnmp_gethostbyaddr((char *) pdu->agent_addr, 4, AF_INET);
     }
     if (host != (struct hostent *) NULL) {
         if (!snmp_strcat

configure
@@ -21791,6 +21791,73 @@ _ACEOF
 fi
 done
 
+  { $as_echo "$as_me:${as_lineno-$LINENO}: checking for library containing gethostbyaddr" >&5
+$as_echo_n "checking for library containing gethostbyaddr... " >&6; }
+if ${ac_cv_search_gethostbyaddr+:} false; then :
+  $as_echo_n "(cached) " >&6
+else
+  ac_func_search_save_LIBS=$LIBS
+cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+/* Override any GCC internal prototype to avoid an error.
+   Use char because int might match the return type of a GCC
+   builtin and then its argument prototype would still apply.  */
+#ifdef __cplusplus
+extern "C"
+#endif
+char gethostbyaddr ();
+int
+main ()
+{
+return gethostbyaddr ();
+  ;
+  return 0;
+}
+_ACEOF
+for ac_lib in '' nsl socket; do
+  if test -z "$ac_lib"; then
+    ac_res="none required"
+  else
+    ac_res=-l$ac_lib
+    LIBS="-l$ac_lib  $ac_func_search_save_LIBS"
+  fi
+  if ac_fn_c_try_link "$LINENO"; then :
+  ac_cv_search_gethostbyaddr=$ac_res
+fi
+rm -f core conftest.err conftest.$ac_objext \
+    conftest$ac_exeext
+  if ${ac_cv_search_gethostbyaddr+:} false; then :
+  break
+fi
+done
+if ${ac_cv_search_gethostbyaddr+:} false; then :
+
+else
+  ac_cv_search_gethostbyaddr=no
+fi
+rm conftest.$ac_ext
+LIBS=$ac_func_search_save_LIBS
+fi
+{ $as_echo "$as_me:${as_lineno-$LINENO}: result: $ac_cv_search_gethostbyaddr" >&5
+$as_echo "$ac_cv_search_gethostbyaddr" >&6; }
+ac_res=$ac_cv_search_gethostbyaddr
+if test "$ac_res" != no; then :
+  test "$ac_res" = "none required" || LIBS="$ac_res $LIBS"
+
+fi
+
+  for ac_func in gethostbyaddr
+do :
+  ac_fn_c_check_func "$LINENO" "gethostbyaddr" "ac_cv_func_gethostbyaddr"
+if test "x$ac_cv_func_gethostbyaddr" = xyes; then :
+  cat >>confdefs.h <<_ACEOF
+#define HAVE_GETHOSTBYADDR 1
+_ACEOF
+
+fi
+done
+
   ;;
 esac
 
@@ -25247,6 +25314,25 @@ rm -f core conftest.err conftest.$ac_objext \
 int
 main ()
 {
+ gethostbyaddr(NULL);
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_link "$LINENO"; then :
+   ac_cv_func_gethostaddr=yes
+      $as_echo  "#define HAVE_GETHOSTBYADDR 1" >>confdefs.h
+
+fi
+rm -f core conftest.err conftest.$ac_objext \
+    conftest$ac_exeext conftest.$ac_ext
+
+    cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+#include <winsock2.h>
+int
+main ()
+{
  gethostname(NULL, 0);
   ;
   return 0;
@@ -25262,7 +25348,7 @@ rm -f core conftest.err conftest.$ac_objext \
     ;;
 
   *)
-    for ac_func in gai_strerror getaddrinfo gethostbyname gethostbyname2 gethostname
+    for ac_func in gai_strerror getaddrinfo gethostbyname gethostbyname2 gethostname gethostbyaddr
 do :
   as_ac_var=`$as_echo "ac_cv_func_$ac_func" | $as_tr_sh`
 ac_fn_c_check_func "$LINENO" "$ac_func" "$as_ac_var"

commit a0af3852b1f84bc50090c5ecbf97996927363792

CHANGES: snmplib: BUG: 3428506: Fix netsnmp_gethostbyaddr()
This patch should fix the regressions introduced by commit 85d96c4 (May 13, 2011).

snmplib/system.c
@@ -925,22 +925,33 @@ netsnmp_gethostbyname(const char *name)
 #endif /* HAVE_GETHOSTBYNAME */
 }
 
+/**
+ * Look up the host name via DNS.
+ *
+ * @param[in] addr Pointer to the address to resolve. This argument points e.g.
+ *   to a struct in_addr for AF_INET or to a struct in6_addr for AF_INET6.
+ * @param[in] len  Length in bytes of *addr.
+ * @param[in] type Address family, e.g. AF_INET or AF_INET6.
+ *
+ * @return Pointer to a hostent structure if address lookup succeeded or NULL
+ *   if the lookup failed.
+ *
+ * @see See also the gethostbyaddr() man page.
+ */
 struct hostent *
 netsnmp_gethostbyaddr(const void *addr, socklen_t len, int type)
 {
 #if HAVE_GETHOSTBYADDR
     struct hostent *hp = NULL;
-    struct sockaddr_in *saddr_in =
-        NETSNMP_REMOVE_CONST(struct sockaddr_in *,addr);
+    char buf[64];
 
-    DEBUGMSGTL(("dns:gethostbyaddr", "resolving { AF_INET, %s:%hu }\n",
-                inet_ntoa(saddr_in->sin_addr), ntohs(saddr_in->sin_port)));
+    DEBUGMSGTL(("dns:gethostbyaddr", "resolving %s\n",
+                inet_ntop(type, addr, buf, sizeof(buf))));
 
 #ifdef DNSSEC_LOCAL_VALIDATION
     val_status_t val_status;
-    hp = val_gethostbyaddr(netsnmp_validator_context(),
-                           (const void*)&saddr_in->sin_addr,
-                           sizeof(struct in_addr), AF_INET, &val_status);
+    hp = val_gethostbyaddr(netsnmp_validator_context(), addr, len, type,
+                           &val_status);
     DEBUGMSGTL(("dns:sec:val", "val_status %d / %s; trusted: %d\n",
                 val_status, p_val_status(val_status),
                 val_istrusted(val_status)));
@@ -956,8 +967,7 @@ netsnmp_gethostbyaddr(const void *addr, socklen_t len, int type)
     else if (val_does_not_exist(val_status) && hp)
         hp = NULL;
 #else
-    hp = gethostbyaddr((const void*) &saddr_in->sin_addr,
-                       sizeof(struct in_addr), AF_INET);
+    hp = gethostbyaddr(addr, len, type);
 #endif
     if (hp == NULL) {
         DEBUGMSGTL(("dns:gethostbyaddr", "couldn't resolve addr\n"));

testing/fulltests/support/clib_build
@@ -23,6 +23,9 @@ cat >>"$2.c" <<EOF
 #else
 #include <strings.h>
 #endif
+#ifdef HAVE_NETDB_H
+#include <netdb.h>
+#endif
 #ifdef HAVE_ARPA_INET_H
 #include <arpa/inet.h>
 #endif

testing/fulltests/unit-tests/T014gethostbyaddr_clib.c
@@ -1,0 +1,52 @@
+/* HEADER Testing netsnmp_gethostbyaddr() */
+
+SOCK_STARTUP;
+
+{
+    struct hostent *h;
+    struct in_addr v4loop;
+    struct sockaddr_in sin_addr;
+    int s, ran_test = 0;
+
+    v4loop.s_addr = htonl(INADDR_LOOPBACK);
+    memset(&sin_addr, 0, sizeof(sin_addr));
+    sin_addr.sin_family = AF_INET;
+    sin_addr.sin_addr = v4loop;
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s >= 0) {
+        if (bind(s, &sin_addr, sizeof(sin_addr)) >= 0) {
+            h = netsnmp_gethostbyaddr(&v4loop, sizeof(v4loop), AF_INET);
+            OKF(h && strcmp(h->h_name, "localhost") == 0,
+                ("127.0.0.1 lookup (%s)", h ? h->h_name : "(failed)"));
+            ran_test = 1;
+        }
+        close(s);
+    }
+    if (!ran_test)
+        OKF(1, ("Skipped IPv4 test"));
+}
+
+{
+    struct hostent *h;
+    static const struct in6_addr v6loop = IN6ADDR_LOOPBACK_INIT;
+    struct sockaddr_in6 sin6_addr;
+    int s, ran_test = 0;
+
+    memset(&sin6_addr, 0, sizeof(sin6_addr));
+    sin6_addr.sin6_family = AF_INET6;
+    sin6_addr.sin6_addr = v6loop;
+    s = socket(AF_INET6, SOCK_DGRAM, 0);
+    if (s >= 0) {
+        if (bind(s, &sin6_addr, sizeof(sin6_addr)) >= 0) {
+            h = netsnmp_gethostbyaddr(&v6loop, sizeof(v6loop), AF_INET6);
+            OKF(h && strcmp(h->h_name, "localhost") == 0,
+                ("::1 lookup (%s)", h ? h->h_name : "(failed)"));
+            ran_test = 1;
+        }
+        close(s);
+    }
+    if (!ran_test)
+        OKF(1, ("Skipped IPv6 test"));
+}
+
+SOCK_CLEANUP;