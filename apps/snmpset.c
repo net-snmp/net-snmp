fe58a0e5f4d4e2e2ecbcb47382b08cb1e4df352c
Dave Shield <D.T.Shield@liverpool.ac.uk>
2011-12-27 18:16:23 +0000

    CHANGES: snmplib: PATCHES: 3456770: Handle (obsolete) UInteger type properly Patch provided by Bill Fenner

--- apps/snmpset.c
+++ apps/snmpset.c
@@ -176,6 +176,7 @@ main(int argc, char *argv[])
             case '=':
             case 'i':
             case 'u':
+            case '3':
             case 't':
             case 'a':
             case 'o':
