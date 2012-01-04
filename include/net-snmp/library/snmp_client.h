24ff260ad89ce2aa361a55c7c72e916031defe35
Bart Van Assche <bvanassche@acm.org>
2012-01-04 17:11:24 +0100

    Change the type of the second argument of snmp_set_var_value() and snmp_set_var_typed_value() from u_char* to void*, like on more recent branches

--- include/net-snmp/library/snmp_client.h
+++ include/net-snmp/library/snmp_client.h
@@ -49,13 +49,13 @@ extern          "C" {
     };
 
     int             snmp_set_var_value(netsnmp_variable_list *,
-                                       const u_char *, size_t);
+                                       const void *, size_t);
     int             snmp_set_var_objid(netsnmp_variable_list * vp,
                                        const oid * objid,
                                        size_t name_length);
     int             snmp_set_var_typed_value(netsnmp_variable_list *
                                              newvar, u_char type,
-                                             const u_char * val_str,
+                                             const void * val_str,
                                              size_t val_len);
     int             snmp_set_var_typed_integer(netsnmp_variable_list * newvar,
                                                u_char type, long val);
