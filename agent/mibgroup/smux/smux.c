net-snmp/net-snmp

commit a809fe5075c9f374715f7d1a682949a0730172c1

Added check for failed register_mib.

agent/mibgroup/smux/smux.c
@@ -1138,9 +1138,17 @@ smux_rreq_process(int sd, u_char * ptr, size_t * len)
         if (nrptr->sr_priority == -1)
             nrptr->sr_priority = 0;
         smux_list_add(&ActiveRegs, nrptr);
-        register_mib("smux", (struct variable *)
-                     smux_variables, sizeof(struct variable2),
-                     1, nrptr->sr_name, nrptr->sr_name_len);
+        if (register_mib("smux", (struct variable *)
+                             smux_variables, sizeof(struct variable2),
+                             1, nrptr->sr_name, nrptr->sr_name_len)
+                     != SNMPERR_SUCCESS) {
+		DEBUGMSGTL(("smux", "[smux_rreq_process] Failed to register subtree\n"));
+		smux_list_detach(&ActiveRegs, nrptr);
+		free(nptr);
+		smux_send_rrsp(sd, -1);
+		return NULL;
+	}
+
       done:
         smux_send_rrsp(sd, nrptr->sr_priority);
         return ptr;

commit 7b7a3eb2500527eaa1699287a6ca6fcc4cc2e08a

Fix a build error caused by commit a809fe5075c9f374715f7d1a682949a0730172c1.

agent/mibgroup/smux/smux.c
@@ -1144,7 +1144,7 @@ smux_rreq_process(int sd, u_char * ptr, size_t * len)
                      != SNMPERR_SUCCESS) {
 		DEBUGMSGTL(("smux", "[smux_rreq_process] Failed to register subtree\n"));
 		smux_list_detach(&ActiveRegs, nrptr);
-		free(nptr);
+		free(nrptr);
 		smux_send_rrsp(sd, -1);
 		return NULL;
 	}