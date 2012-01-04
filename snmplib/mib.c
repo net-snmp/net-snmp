299ea9a89c3f1758a7748bb9b3411f62f2c70f88
Jan Safranek <jsafranek@users.sourceforge.net>
2012-01-04 10:54:55 +0100

    Added check of fopen() return value, just in case of some race.

--- snmplib/mib.c
+++ snmplib/mib.c
@@ -2771,6 +2771,8 @@ netsnmp_mibindex_load( void )
               get_persistent_directory(), i );
         tmpbuf[sizeof(tmpbuf)-1] = 0;
         fp = fopen( tmpbuf, "r" );
+        if (!fp)
+            continue;
         cp = fgets( tmpbuf2, sizeof(tmpbuf2), fp );
         if ( !cp ) {
             DEBUGMSGTL(("mibindex", "Empty MIB index (%d)\n", i));
