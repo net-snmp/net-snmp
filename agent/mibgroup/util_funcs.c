0efdd022da09af1b5e16171fa7a2b523c5d6ec59
Jan Safranek <jsafranek@users.sourceforge.net>
2012-01-10 15:01:53 +0100

    Fixed buffer overflow when too big temp file pattern is used.

--- agent/mibgroup/util_funcs.c
+++ agent/mibgroup/util_funcs.c
@@ -139,7 +139,8 @@ make_tempfile(void)
     int             fd = -1;
     mode_t          oldmask;
 
-    strcpy(name, get_temp_file_pattern());
+    strncpy(name, get_temp_file_pattern(), sizeof(name));
+    name[sizeof(name)-1] = '\0';
 #ifdef HAVE_MKSTEMP
     oldmask = umask(S_IRUSR | S_IWUSR);
     fd = mkstemp(name);
