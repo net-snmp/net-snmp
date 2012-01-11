net-snmp/net-snmp

commit bb8fcbb874998000f945f058d1aa6fe14e925899

Secure mkstemp() with umask() - on some implementations it does not set 0600 permissions automatically.

agent/mibgroup/util_funcs.c
@@ -137,10 +137,13 @@ make_tempfile(void)
 {
     static char     name[32];
     int             fd = -1;
+    mode_t          oldmask;
 
     strcpy(name, get_temp_file_pattern());
 #ifdef HAVE_MKSTEMP
+    oldmask = umask(S_IRUSR | S_IWUSR);
     fd = mkstemp(name);
+    umask(oldmask);
 #else
     if (mktemp(name)) {
 # ifndef WIN32        

snmplib/system.c
@@ -1103,10 +1103,13 @@ netsnmp_mktemp(void)
     static char     name[256];
 #endif
     int             fd = -1;
+    mode_t          oldmask;
 
     strcpy(name, get_temp_file_pattern());
 #ifdef HAVE_MKSTEMP
+    oldmask = umask(S_IRUSR | S_IWUSR);
     fd = mkstemp(name);
+    umask(oldmask);
 #else
     if (mktemp(name)) {
 # ifndef WIN32

commit afc396ab1618a8595a2519fbc25f7a498e174411

Unbreak netsnmp_mktemp() and make_temp().
These were broken by commit bb8fcbb874998000f945f058d1aa6fe14e925899.
Also replace a strncpy() call by a call to strlcpy() and replace the
make_tempfile() implementation by a call to netsnmp_mktemp().

agent/mibgroup/util_funcs.c
@@ -135,36 +135,7 @@ Exit(int var)
 const char *
 make_tempfile(void)
 {
-    static char     name[32];
-    int             fd = -1;
-
-    strncpy(name, get_temp_file_pattern(), sizeof(name));
-    name[sizeof(name)-1] = '\0';
-#ifdef HAVE_MKSTEMP
-    {
-        mode_t oldmask = umask(S_IRUSR | S_IWUSR);
-        fd = mkstemp(name);
-        umask(oldmask);
-    }
-#else
-    if (mktemp(name)) {
-# ifndef WIN32        
-        fd = open(name, O_CREAT | O_EXCL | O_WRONLY, S_IRUSR | S_IWUSR);
-# else
-        /*
-          Win32 needs _S_IREAD | _S_IWRITE to set permissions on file after closing
-        */
-        fd = _open(name, _O_CREAT | _O_EXCL | _O_WRONLY, _S_IREAD | _S_IWRITE);
-# endif
-    }
-#endif
-    if (fd >= 0) {
-        close(fd);
-        DEBUGMSGTL(("make_tempfile", "temp file created: %s\n", name));
-        return name;
-    }
-    snmp_log(LOG_ERR,"make_tempfile: error creating file %s\n", name);
-    return NULL;
+    return netsnmp_mktemp();
 }
 
 int

snmplib/system.c
@@ -1112,11 +1112,11 @@ netsnmp_mktemp(void)
 #endif
     int             fd = -1;
 
-    strncpy(name, get_temp_file_pattern(), sizeof(name));
-    name[sizeof(name)-1] = '\0';
+    strlcpy(name, get_temp_file_pattern(), sizeof(name));
 #ifdef HAVE_MKSTEMP
     {
-        mode_t oldmask = umask(S_IRUSR | S_IWUSR);
+        mode_t oldmask = umask(~(S_IRUSR | S_IWUSR));
+        netsnmp_assert(oldmask != -1);
         fd = mkstemp(name);
         umask(oldmask);
     }