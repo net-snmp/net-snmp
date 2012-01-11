net-snmp/net-snmp

commit 885061687d9caa38470979667d7d8ad6563cedad

Get rid of (insecure) SNMP_MAXPATH in mkdirhier()

snmplib/system.c
@@ -1022,8 +1022,12 @@ mkdirhier(const char *pathname, mode_t mode, int skiplast)
     struct stat     sbuf;
     char           *ourcopy = strdup(pathname);
     char           *entry;
-    char            buf[SNMP_MAXPATH];
+    char           *buf;
     char           *st = NULL;
+
+    buf = malloc(strlen(pathname));
+    if (!buf)
+        return SNMPERR_GENERR;
 
 #if defined (WIN32) || defined (cygwin)
     /* convert backslash to forward slash */
@@ -1069,6 +1073,7 @@ mkdirhier(const char *pathname, mode_t mode, int skiplast)
 #endif
             {
                 free(ourcopy);
+                free(buf);
                 return SNMPERR_GENERR;
             }
         } else {
@@ -1080,11 +1085,13 @@ mkdirhier(const char *pathname, mode_t mode, int skiplast)
                  * ack! can't make a directory on top of a file 
                  */
                 free(ourcopy);
+                free(buf);
                 return SNMPERR_GENERR;
             }
         }
     }
     free(ourcopy);
+    free(buf);
     return SNMPERR_SUCCESS;
 }
 

commit 210185c15d0d65f0ea98b5e3c2e0776f6222d170

Fix a crash during startup of snmpd introduced by commit 885061687d9caa38470979667d7d8ad6563cedad.
Also, make sure there is only a single copy of the cleanup code in function
mkdirhier().

snmplib/system.c
@@ -1022,12 +1022,17 @@ mkdirhier(const char *pathname, mode_t mode, int skiplast)
     struct stat     sbuf;
     char           *ourcopy = strdup(pathname);
     char           *entry;
-    char           *buf;
+    char           *buf = NULL;
     char           *st = NULL;
+    int             res;
 
-    buf = malloc(strlen(pathname));
+    res = SNMPERR_GENERR;
+    if (!ourcopy)
+        goto out;
+
+    buf = malloc(strlen(pathname) + 2);
     if (!buf)
-        return SNMPERR_GENERR;
+        goto out;
 
 #if defined (WIN32) || defined (cygwin)
     /* convert backslash to forward slash */
@@ -1071,11 +1076,7 @@ mkdirhier(const char *pathname, mode_t mode, int skiplast)
 #else
             if (mkdir(buf, mode) == -1)
 #endif
-            {
-                free(ourcopy);
-                free(buf);
-                return SNMPERR_GENERR;
-            }
+                goto out;
         } else {
             /*
              * exists, is it a file? 
@@ -1084,15 +1085,15 @@ mkdirhier(const char *pathname, mode_t mode, int skiplast)
                 /*
                  * ack! can't make a directory on top of a file 
                  */
-                free(ourcopy);
-                free(buf);
-                return SNMPERR_GENERR;
+                goto out;
             }
         }
     }
-    free(ourcopy);
+    res = SNMPERR_SUCCESS;
+out:
     free(buf);
-    return SNMPERR_SUCCESS;
+    free(ourcopy);
+    return res;
 }
 
 /**