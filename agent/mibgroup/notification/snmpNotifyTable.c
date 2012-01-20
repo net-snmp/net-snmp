net-snmp/net-snmp

commit dbf26a05a04ef24c47346e5adda48e338498e71f

Free previously allocated memory on errors.

agent/mibgroup/notification/snmpNotifyTable.c
@@ -355,8 +355,10 @@ notifyTable_register_notifications(int major, int minor,
         pptr->secModel = ss->securityModel;
         pptr->secLevel = ss->securityLevel;
         pptr->secName = (char *) malloc(ss->securityNameLen + 1);
-        if (pptr->secName == NULL)
+        if (pptr->secName == NULL) {
+            snmpTargetAddrTable_dispose(pptr);
             return 0;
+        }
         memcpy((void *) pptr->secName, (void *) ss->securityName,
                ss->securityNameLen);
         pptr->secName[ss->securityNameLen] = 0;
@@ -372,8 +374,10 @@ notifyTable_register_notifications(int major, int minor,
         pptr->secName = NULL;
         if (ss->community && (ss->community_len > 0)) {
             pptr->secName = (char *) malloc(ss->community_len + 1);
-            if (pptr->secName == NULL)
+            if (pptr->secName == NULL) {
+                snmpTargetAddrTable_dispose(pptr);
                 return 0;
+            }
             memcpy((void *) pptr->secName, (void *) ss->community,
                    ss->community_len);
             pptr->secName[ss->community_len] = 0;

commit cbf1c91baf7191be5fa27d81bcf8714331a2c12f

Use snmpTargetParamTable_dispose() to free a struct targetParamTable_struct.
Fixes a bug introduced in commit dbf26a05a04ef24c47346e5adda48e338498e71f.

agent/mibgroup/notification/snmpNotifyTable.c
@@ -356,7 +356,7 @@ notifyTable_register_notifications(int major, int minor,
         pptr->secLevel = ss->securityLevel;
         pptr->secName = (char *) malloc(ss->securityNameLen + 1);
         if (pptr->secName == NULL) {
-            snmpTargetAddrTable_dispose(pptr);
+            snmpTargetParamTable_dispose(pptr);
             return 0;
         }
         memcpy((void *) pptr->secName, (void *) ss->securityName,
@@ -375,7 +375,7 @@ notifyTable_register_notifications(int major, int minor,
         if (ss->community && (ss->community_len > 0)) {
             pptr->secName = (char *) malloc(ss->community_len + 1);
             if (pptr->secName == NULL) {
-                snmpTargetAddrTable_dispose(pptr);
+                snmpTargetParamTable_dispose(pptr);
                 return 0;
             }
             memcpy((void *) pptr->secName, (void *) ss->community,

agent/mibgroup/target/snmpTargetParamsEntry.h
@@ -47,6 +47,7 @@
                                               targetParamTable_struct
                                               *newEntry);
      struct targetParamTable_struct *snmpTargetParamTable_create(void);
+     void snmpTargetParamTable_dispose(struct targetParamTable_struct *);
 
 /*
  * function definitions 