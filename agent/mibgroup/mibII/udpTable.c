net-snmp/net-snmp

commit c76fb96f65e33ba2fd212a921e8c386155ee5c7a

Fixed unitialized values in udpTable

agent/mibgroup/mibII/udpTable.c
@@ -495,6 +495,7 @@ udpTable_load(netsnmp_cache *cache, void *vmagic)
         if (state != 7)         /* fix me:  UDP_LISTEN ??? */
             continue;
 
+        memset(&pcb, 0, sizeof(pcb));
         /* store in network byte order */
         pcb.inp_laddr.s_addr = htonl(pcb.inp_laddr.s_addr);
         pcb.inp_lport = htons((unsigned short) (lport));

commit 0dd1e54557d1ba498b55d017b0a93e8ccf9bd522

CHANGES: snmpd: UDP-MIB: Report the proper address in udpLocalAddress instead of 0.0.0.0. Makes test T160snmpnetstat pass again. Fixes a bug introduced in commit c76fb96f65e33ba2fd212a921e8c386155ee5c7a.

agent/mibgroup/mibII/udpTable.c
@@ -491,6 +491,8 @@ udpTable_load(netsnmp_cache *cache, void *vmagic)
         struct inpcb    pcb, *nnew;
         unsigned int    state, lport;
 
+        memset(&pcb, 0, sizeof(pcb));
+
         if (3 != sscanf(line, "%*d: %x:%x %*x:%*x %x",
                         &pcb.inp_laddr.s_addr, &lport, &state))
             continue;
@@ -498,7 +500,6 @@ udpTable_load(netsnmp_cache *cache, void *vmagic)
         if (state != 7)         /* fix me:  UDP_LISTEN ??? */
             continue;
 
-        memset(&pcb, 0, sizeof(pcb));
         /* store in network byte order */
         pcb.inp_laddr.s_addr = htonl(pcb.inp_laddr.s_addr);
         pcb.inp_lport = htons((unsigned short) (lport));