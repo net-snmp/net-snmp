8e69742f143e4fc11196e5b2ed66c8f7f53fd02f
Dave Shield <D.T.Shield@liverpool.ac.uk>
2012-03-06 09:24:21 +0000

    Version number update: 5.6.2.pre1

--- perl/OID/OID.pm
+++ perl/OID/OID.pm
@@ -92,7 +92,7 @@ use vars qw(@ISA %EXPORT_TAGS @EXPORT_OK @EXPORT $VERSION $AUTOLOAD);
 	snmp_oid_compare
         compare
 );
-$VERSION = '5.0601';
+$VERSION = '5.0602';
 
 sub new {
     my $type = shift;
