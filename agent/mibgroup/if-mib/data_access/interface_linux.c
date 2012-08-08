net-snmp/net-snmp

commit 028614ecf2d67398699dc3bd784029f3360f2e13

CHANGES: snmpd: fixed snmpd exiting shortly after start due to incoming signal.

agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -1017,6 +1017,8 @@ void netsnmp_prefix_process(int fd, void *data)
 
     status = recv(fd, buf, sizeof(buf), 0);
     if (status < 0) {
+        if (errno == EINTR)
+            continue;
         snmp_log(LOG_ERR,"netsnmp_prefix_listen: Receive failed.\n");
         return;
     }

commit 023f61ea783bfcc53fe90aa66545c5aea2db8061

Fix build breakage caused by commit 028614ecf2d67398699dc3bd784029f3360f2e13

agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -34,6 +34,7 @@ typedef __u8 u8;           /* ditto */
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <unistd.h>
 
 #include <linux/sockios.h>
@@ -1018,7 +1019,7 @@ void netsnmp_prefix_process(int fd, void *data)
     status = recv(fd, buf, sizeof(buf), 0);
     if (status < 0) {
         if (errno == EINTR)
-            continue;
+            return;
         snmp_log(LOG_ERR,"netsnmp_prefix_listen: Receive failed.\n");
         return;
     }