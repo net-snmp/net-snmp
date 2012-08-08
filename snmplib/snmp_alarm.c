net-snmp/net-snmp

commit 37ba7e20a521fbf0458a0c77eea2e5e36bd7ec0a

CHANGES: agent: PATCH 3481686: from vbernat: Dont ping master agent unless there is no incoming traffic
Signed-off-by: Wes Hardaker <hardaker@users.sourceforge.net>

snmplib/snmp_alarm.c
@@ -521,4 +521,35 @@ snmp_alarm_register_hr(struct timeval t, unsigned int flags,
 
     return (*s)->clientreg;
 }
+
+/**
+ * This function resets an existing alarm.
+ *
+ * @param clientreg is a unique unsigned integer representing a registered
+ *	alarm which the client wants to unregister.
+ *
+ * @return 0 on success, -1 if the alarm was not found
+ *
+ * @see snmp_alarm_register
+ * @see snmp_alarm_register_hr
+ * @see snmp_alarm_unregister
+ */
+int
+snmp_alarm_reset(unsigned int clientreg)
+{
+    struct snmp_alarm *a;
+    struct timeval  t_now;
+    if ((a = sa_find_specific(clientreg)) != NULL) {
+        gettimeofday(&t_now, NULL);
+        a->t_last.tv_sec = t_now.tv_sec;
+        a->t_last.tv_usec = t_now.tv_usec;
+        a->t_next.tv_sec = 0;
+        a->t_next.tv_usec = 0;
+        NETSNMP_TIMERADD(&t_now, &a->t, &a->t_next);
+        return 0;
+    }
+    DEBUGMSGTL(("snmp_alarm_reset", "alarm %d not found\n",
+                clientreg));
+    return -1;
+}
 /**  @} */

agent/mibgroup/agentx/subagent.c
@@ -336,6 +336,11 @@ handle_agentx_packet(int operation, netsnmp_session * session, int reqid,
     pdu->version = AGENTX_VERSION_1;
     pdu->flags |= UCD_MSG_FLAG_ALWAYS_IN_VIEW;
 
+    /* Master agent is alive, no need to ping */
+    if (session->securityModel != SNMP_DEFAULT_SECMODEL) {
+        snmp_alarm_reset(session->securityModel);
+    }
+
     if (pdu->command == AGENTX_MSG_GET
         || pdu->command == AGENTX_MSG_GETNEXT
         || pdu->command == AGENTX_MSG_GETBULK) {

commit cd820949a1491af103a8555f6bdae04851f3d285

Undo build breakage caused by commit 37ba7e20a521fbf0458a0c77eea2e5e36bd7ec0a

snmplib/snmp_alarm.c
@@ -545,7 +545,7 @@ snmp_alarm_reset(unsigned int clientreg)
         a->t_last.tv_usec = t_now.tv_usec;
         a->t_next.tv_sec = 0;
         a->t_next.tv_usec = 0;
-        NETSNMP_TIMERADD(&t_now, &a->t, &a->t_next);
+        timeradd(&t_now, &a->t, &a->t_next);
         return 0;
     }
     DEBUGMSGTL(("snmp_alarm_reset", "alarm %d not found\n",