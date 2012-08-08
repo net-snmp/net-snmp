net-snmp/net-snmp

commit 7374b84fe4c2ef8497fde3dae80a69aa89eba19c

CHANGES: freebsd: PATCH 3383970: from yaberauneya: Fix compile error on FreeBSD 9.x/POSIXify auto_nlist, etc
Signed-off-by: Wes Hardaker <hardaker@users.sourceforge.net>

agent/mibgroup/util_funcs.c
@@ -10,6 +10,7 @@
 
 #include <net-snmp/net-snmp-config.h>
 
+#include <sys/types.h>
 #if HAVE_IO_H
 #include <io.h>
 #endif
@@ -20,7 +21,6 @@
 #if HAVE_MALLOC_H
 #include <malloc.h>
 #endif
-#include <sys/types.h>
 #ifdef __alpha
 #ifndef _BSD
 #define _BSD
@@ -244,7 +244,7 @@ get_exec_output(struct extensible *ex)
 #if HAVE_EXECV
     char            cachefile[STRMAX];
     char            cache[NETSNMP_MAXCACHESIZE];
-    int             cachebytes;
+    size_t          cachebytes;
     int             cfd;
 #ifdef NETSNMP_EXCACHETIME
     long            curtime;

agent/mibgroup/utilities/execute.h
@@ -5,8 +5,8 @@ config_require(util_funcs)
 config_belongs_in(agent_module)
 
 int run_shell_command(char *command, char *input,
-                      char *output,  int  *out_len);
+                      char *output,  size_t  *out_len);
 int run_exec_command( char *command, char *input,
-                      char *output,  int  *out_len);
+                      char *output,  size_t  *out_len);
 
 #endif /* _MIBGROUP_EXECUTE_H */

agent/auto_nlist.c
@@ -68,6 +68,8 @@ auto_nlist_value(const char *string)
 #if defined(aix4) || defined(aix5) || defined(aix6)
         strcpy(it->nl[0].n_name, string);
         it->nl[0].n_name[strlen(string)+1] = '\0';
+#elif defined(freebsd9)
+        sprintf(__DECONST(char*, it->nl[0].n_name), "_%s", string);
 #else
 
         if (n_name != NULL)
@@ -86,6 +88,10 @@ auto_nlist_value(const char *string)
 #if !(defined(aix4) || defined(aix5) || defined(aix6) || defined(aix7) || \
                     defined(netbsd1) || defined(dragonfly))
         if (it->nl[0].n_type == 0) {
+#if defined(freebsd9)
+            strcpy(__DECONST(char*, it->nl[0].n_name), string);
+            __DECONST(char*, it->nl[0].n_name)[strlen(string)+1] = '\0';
+#else
             static char *n_name2 = NULL;
 
             if (n_name2 != NULL)
@@ -98,6 +104,7 @@ auto_nlist_value(const char *string)
             }
             strcpy(n_name2, string);
             it->nl[0].n_name = (const char*)n_name2;
+#endif
             init_nlist(it->nl);
         }
 #endif
@@ -109,7 +116,8 @@ auto_nlist_value(const char *string)
 	    }
             return (-1);
         } else {
-            DEBUGMSGTL(("auto_nlist:auto_nlist_value", "found symbol %s at %x.\n",
+            DEBUGMSGTL(("auto_nlist:auto_nlist_value",
+			"found symbol %s at %lx.\n",
                         it->symbol, it->nl[0].n_value));
             return (it->nl[0].n_value);
         }
@@ -118,7 +126,7 @@ auto_nlist_value(const char *string)
 }
 
 int
-auto_nlist(const char *string, char *var, int size)
+auto_nlist(const char *string, char *var, size_t size)
 {
     long            result;
     int             ret;
@@ -217,7 +225,7 @@ init_nlist(struct nlist nl[])
 }
 
 int
-KNLookup(struct nlist nl[], int nl_which, char *buf, int s)
+KNLookup(struct nlist nl[], int nl_which, char *buf, size_t s)
 {
     struct nlist   *nlp = &nl[nl_which];
 

agent/kernel.c
@@ -86,7 +86,7 @@ init_kmem(const char *file)
 
 
 int
-klookup(unsigned long off, char *target, int siz)
+klookup(unsigned long off, void *target, size_t siz)
 {
     int             result;
     if (kd == NULL)
@@ -94,7 +94,7 @@ klookup(unsigned long off, char *target, int siz)
     result = kvm_read(kd, off, target, siz);
     if (result != siz) {
 #if HAVE_KVM_OPENFILES
-        snmp_log(LOG_ERR, "kvm_read(*, %lx, %p, %d) = %d: %s\n", off,
+        snmp_log(LOG_ERR, "kvm_read(*, %lx, %p, %zx) = %d: %s\n", off,
                  target, siz, result, kvm_geterr(kd));
 #else
         snmp_log(LOG_ERR, "kvm_read(*, %lx, %p, %d) = %d: ", off, target,
@@ -179,7 +179,7 @@ klread(char *buf, int buflen)
 
 
 int
-klookup(unsigned long off, char *target, int siz)
+klookup(unsigned long off, char *target, size_t siz)
 {
     long            retsiz;
 

agent/kernel.h
@@ -1,7 +1,7 @@
 
 #ifdef NETSNMP_CAN_USE_NLIST
 extern int      init_kmem(const char *);
-extern int      klookup(unsigned long, char *, int);
+extern int      klookup(unsigned long, char *, size_t);
 #define NETSNMP_KLOOKUP(x,y,z) klookup((unsigned long) x,y,z)
 #else
 #define NETSNMP_KLOOKUP(x,y,z) (0)

include/net-snmp/agent/auto_nlist.h
@@ -12,9 +12,9 @@ extern          "C" {
 #endif
 
 #ifdef NETSNMP_CAN_USE_NLIST
-int             auto_nlist(const char *, char *, int);
+int             auto_nlist(const char *, char *, size_t);
 long            auto_nlist_value(const char *);
-int             KNLookup(struct nlist *, int, char *, int);
+int             KNLookup(struct nlist *, int, char *, size_t);
 #else
 int             auto_nlist_noop(void);
 #	define auto_nlist(x,y,z) auto_nlist_noop()

commit f143008ccc57ea10cbb88ab3554822b17b6270aa

Make get_exec_output() work on big endian systems.
Undo API breakage introduced by commit 7374b84fe4c2ef8497fde3dae80a69aa89eba19c.

agent/mibgroup/util_funcs.c
@@ -244,7 +244,7 @@ get_exec_output(struct extensible *ex)
 #if HAVE_EXECV
     char            cachefile[STRMAX];
     char            cache[NETSNMP_MAXCACHESIZE];
-    size_t          cachebytes;
+    int             cachebytes;
     int             cfd;
 #ifdef NETSNMP_EXCACHETIME
     long            curtime;

agent/mibgroup/utilities/execute.c
@@ -36,6 +36,7 @@
 #include <ucd-snmp/errormib.h>
 
 #include "util_funcs.h"
+#include "execute.h"
 
 #define setPerrorstatus(x) snmp_log_perror(x)
 

agent/mibgroup/utilities/execute.h
@@ -5,8 +5,8 @@ config_require(util_funcs)
 config_belongs_in(agent_module)
 
 int run_shell_command(char *command, char *input,
-                      char *output,  size_t  *out_len);
+                      char *output,  int  *out_len);
 int run_exec_command( char *command, char *input,
-                      char *output,  size_t  *out_len);
+                      char *output,  int  *out_len);
 
 #endif /* _MIBGROUP_EXECUTE_H */