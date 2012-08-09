net-snmp/net-snmp

commit f95df6341b1d06249443e516b59a43cdfa3d4777

CHANGES: snmpd: removed error log messages when IPv6 is disabled.

agent/mibgroup/tcp-mib/data_access/tcpConn_linux.c
@@ -256,18 +256,13 @@ _load6(netsnmp_container *container, u_int load_flags)
     int             rc = 0;
     FILE           *in;
     char            line[180];
-    static int      log_open_err = 1;
 
     netsnmp_assert(NULL != container);
 
 #undef PROCFILE
 #define PROCFILE "/proc/net/tcp6"
     if (!(in = fopen(PROCFILE, "r"))) {
-        snmp_log(LOG_ERR,"could not open " PROCFILE "\n");
-        if (1 == log_open_err) {
-            snmp_log(LOG_ERR,"could not open " PROCFILE "\n");
-            log_open_err = 0;
-        }
+        DEBUGMSGTL(("access:tcpconn:container","could not open " PROCFILE "\n"));
         return -2;
     }
     /*

agent/mibgroup/ip-forward-mib/data_access/route_linux.c
@@ -202,7 +202,6 @@ _load_ipv6(netsnmp_container* container, u_long *index )
     FILE           *in;
     char            line[256];
     netsnmp_route_entry *entry = NULL;
-    static int      log_open_err = 1;
 
     DEBUGMSGTL(("access:route:container",
                 "route_container_arch_load ipv6\n"));
@@ -213,10 +212,7 @@ _load_ipv6(netsnmp_container* container, u_long *index )
      * fetch routes from the proc file-system:
      */
     if (!(in = fopen("/proc/net/ipv6_route", "r"))) {
-        if (1 == log_open_err) {
-            snmp_log(LOG_ERR, "cannot open /proc/net/ipv6_route\n");
-            log_open_err = 0;
-        }
+        DEBUGMSGTL(("9:access:route:container", "cannot open /proc/net/ipv6_route\n");
         return -2;
     }
     /*

agent/mibgroup/ip-mib/data_access/ipaddress_linux.c
@@ -210,17 +210,13 @@ _load_v6(netsnmp_container *container, int idx_offset)
     size_t          in_len, out_len;
     netsnmp_ipaddress_entry *entry;
     _ioctl_extras           *extras;
-    static int      log_open_err = 1;
     struct address_flag_info addr_info;
     
     netsnmp_assert(NULL != container);
 
 #define PROCFILE "/proc/net/if_inet6"
     if (!(in = fopen(PROCFILE, "r"))) {
-        if (1 == log_open_err) {
-            snmp_log(LOG_ERR,"could not open " PROCFILE "\n");
-            log_open_err = 0;
-        }
+        DEBUGMSGTL(("access:ipaddress:container","could not open " PROCFILE "\n"));
         return -2;
     }
     /*

agent/mibgroup/ip-mib/data_access/ipv6scopezone_linux.c
@@ -61,16 +61,12 @@ _scopezone_v6(netsnmp_container* container, int idx_offset)
     int             if_index, pfx_len, scope, flags, rc = 0;
     int             last_if_index = -1;
     netsnmp_v6scopezone_entry *entry;
-    static int      log_open_err = 1;
     
     netsnmp_assert(NULL != container);
 
 #define PROCFILE "/proc/net/if_inet6"
     if (!(in = fopen(PROCFILE, "r"))) {
-        if (1 == log_open_err) {
-            snmp_log(LOG_ERR,"could not open " PROCFILE "\n");
-            log_open_err = 0;
-        }
+        DEBUGMSGTL(("access:scopezone:container","could not open " PROCFILE "\n"));
         return -2;
     }
     /*

agent/mibgroup/ip-mib/data_access/systemstats_linux.c
@@ -532,7 +532,6 @@ _systemstats_v6_load_systemstats(netsnmp_container* container, u_int load_flags)
     FILE *devin;
     netsnmp_systemstats_entry *entry = NULL;
     const char     *filename = "/proc/net/snmp6";
-    static int      warned_open = 0;
     int rc = 0;
     
     entry = netsnmp_access_systemstats_entry_create(2, 0);
@@ -545,12 +544,8 @@ _systemstats_v6_load_systemstats(netsnmp_container* container, u_int load_flags)
      */
     if (!(devin = fopen(filename, "r"))) {
         DEBUGMSGTL(("access:systemstats",
-                    "Failed to load Systemstats Table (linux1)\n"));
-        if(!warned_open) {
-            ++warned_open;
-            snmp_log(LOG_ERR, "cannot open %s ...\n", filename);
-        }
-        free(entry);
+                "Failed to load Systemstats Table (linux1), cannot open %s\n",
+                filename));
         return 0;
     }
     

commit 5d7edadd4761680b66d1df3bfde7ca5b16ca5a16

Fix compilation error introduced by commit f95df6341b1d06249443e516b59a43cdfa3d4777.

agent/mibgroup/tcp-mib/data_access/tcpConn_linux.c
@@ -265,13 +265,7 @@ _load6(netsnmp_container *container, u_int load_flags)
         DEBUGMSGTL(("access:tcpconn:container","could not open " PROCFILE "\n"));
         return -2;
     }
-    /*
-     * if we turned off logging of open errors, turn it back on now that
-     * we have been able to open the file.
-     */
-    if (0 == log_open_err)
-        log_open_err = 1;
-    
+
     fgets(line, sizeof(line), in); /* skip header */
 
     /*