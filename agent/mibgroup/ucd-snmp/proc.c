net-snmp/net-snmp

commit 213941dfbee6aa4a3ff9e543652d9d13fe42b359

CHANGES: agent: PATCH 3525595: from dhubler: Add support for finding processes via regex
Signed-off-by: Wes Hardaker <hardaker@users.sourceforge.net>

agent/mibgroup/ucd-snmp/proc.c
@@ -42,6 +42,9 @@
 #if HAVE_KVM_H
 #include <kvm.h>
 #endif
+#if HAVE_PCRE_H
+#include <pcre.h>
+#endif
 
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
@@ -58,6 +61,7 @@
 #endif
 #include "util_funcs.h"
 #include "kernel.h"
+#include <unistd.h>
 
 static struct myproc *get_proc_instance(struct myproc *, oid);
 struct myproc  *procwatch = NULL;
@@ -67,7 +71,6 @@ int             numprocs = 0;
 void
 init_proc(void)
 {
-
     /*
      * define the structure we're going to ask the agent to register our
      * information at 
@@ -126,6 +129,11 @@ proc_free_config(void)
     for (ptmp = procwatch; ptmp != NULL;) {
         ptmp2 = ptmp;
         ptmp = ptmp->next;
+#if HAVE_PCRE_H
+        if (ptmp->regexp) {
+            free(ptmp->regexp);
+        }
+#endif
         free(ptmp2);
     }
     procwatch = NULL;
@@ -177,6 +185,10 @@ proc_parse_config(const char *token, char *cptr)
 {
     char            tmpname[STRMAX];
     struct myproc **procp = &procwatch;
+#if HAVE_PCRE_H
+    const char *pcre_error;
+    int pcre_error_offset;
+#endif
 
     /*
      * don't allow two entries with the same name 
@@ -197,6 +209,9 @@ proc_parse_config(const char *token, char *cptr)
     if (*procp == NULL)
         return;                 /* memory alloc error */
     numprocs++;
+#if HAVE_PCRE_H
+    (*procp)->regexp = NULL;
+#endif
     /*
      * not blank and not a comment 
      */
@@ -205,9 +220,19 @@ proc_parse_config(const char *token, char *cptr)
     if ((cptr = skip_white(cptr))) {
         (*procp)->max = atoi(cptr);
         cptr = skip_not_white(cptr);
-        if ((cptr = skip_white(cptr)))
+        if ((cptr = skip_white(cptr))) {
             (*procp)->min = atoi(cptr);
-        else
+#if HAVE_PCRE_H
+            cptr = skip_not_white(cptr);
+            if ((cptr = skip_white(cptr))) {
+                DEBUGMSGTL(("ucd-snmp/regexp_proc", "Loading regex %s\n", cptr));
+                (*procp)->regexp = pcre_compile(cptr, 0,  &pcre_error, &pcre_error_offset, NULL);
+                if ((*procp)->regexp == NULL) {
+                    config_perror(pcre_error);
+                }
+            }
+#endif
+        } else
             (*procp)->min = 0;
     } else {
         /* Default to asssume that we require at least one
@@ -260,10 +285,10 @@ var_extensible_proc(struct variable *vp,
             long_ret = proc->max;
             return ((u_char *) (&long_ret));
         case PROCCOUNT:
-            long_ret = sh_count_procs(proc->name);
+            long_ret = sh_count_myprocs(proc);
             return ((u_char *) (&long_ret));
         case ERRORFLAG:
-            long_ret = sh_count_procs(proc->name);
+            long_ret = sh_count_myprocs(proc);
             if (long_ret >= 0 &&
                    /* Too few processes running */
                 ((proc->min && long_ret < proc->min) ||
@@ -277,7 +302,7 @@ var_extensible_proc(struct variable *vp,
             }
             return ((u_char *) (&long_ret));
         case ERRORMSG:
-            long_ret = sh_count_procs(proc->name);
+            long_ret = sh_count_myprocs(proc);
             if (long_ret < 0) {
                 errmsg[0] = 0;  /* catch out of mem errors return 0 count */
             } else if (proc->min && long_ret < proc->min) {
@@ -359,13 +384,37 @@ get_proc_instance(struct myproc *proc, oid inst)
     return (proc);
 }
 
+int
+sh_count_myprocs(struct myproc *proc)
+{
+    if (proc == NULL)
+        return (NULL);
+
+#if HAVE_PCRE_H
+    if (proc->regexp != NULL)
+      return sh_count_procs_by_regex(proc->name, proc->regexp);
+#endif
+
+    return sh_count_procs(proc->name);
+}
+
 #ifdef USING_HOST_DATA_ACCESS_SWRUN_MODULE
 netsnmp_feature_require(swrun_count_processes_by_name)
 int
 sh_count_procs(char *procname)
 {
-    return swrun_count_processes_by_name( procname );
+  return swrun_count_processes_by_name( procname );
 }
+
+#if HAVE_PCRE_H
+netsnmp_feature_require(swrun_count_processes_by_regex)
+int
+sh_count_procs_by_regex(char *procname, netsnmp_regex_ptr regexp)
+{
+  return swrun_count_processes_by_regex( procname, (pcre *) regexp );
+}
+#endif
+
 #else
 
 #ifdef bsdi2

agent/mibgroup/host/data_access/swrun.c
@@ -15,10 +15,15 @@
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/data_access/swrun.h>
 
+#if HAVE_PCRE_H
+#include <pcre.h>
+#endif
+
 netsnmp_feature_child_of(software_running, libnetsnmpmibs)
 
 netsnmp_feature_child_of(swrun_max_processes, software_running)
 netsnmp_feature_child_of(swrun_count_processes_by_name, software_running)
+netsnmp_feature_child_of(swrun_count_processes_by_regex, software_running)
 
 /**---------------------------------------------------------------------*/
 /*
@@ -88,6 +93,36 @@ swrun_max_processes( void )
     return _swrun_max;
 }
 #endif /* NETSNMP_FEATURE_REMOVE_SWRUN_MAX_PROCESSES */
+
+#ifndef NETSNMP_FEATURE_REMOVE_SWRUN_COUNT_PROCESSES_BY_REGEX
+int
+swrun_count_processes_by_regex( char *name, pcre *regexp )
+{
+    netsnmp_swrun_entry *entry;
+    netsnmp_iterator  *it;
+    int i = 0;
+    int found_ndx[30];
+    int found;
+    char fullCommand[64 + 128 + 128 + 3];
+
+    netsnmp_cache_check_and_reload(swrun_cache);
+    if ( !swrun_container || !name || !regexp )
+        return 0;    /* or -1 */
+
+    it = CONTAINER_ITERATOR( swrun_container );
+    while ((entry = (netsnmp_swrun_entry*)ITERATOR_NEXT( it )) != NULL) {
+        // need to assemble full command back so regexps can get full picture
+        sprintf(fullCommand, "%s %s", entry->hrSWRunPath, entry->hrSWRunParameters);
+        found = pcre_exec(regexp, NULL, fullCommand, strlen(fullCommand), 0, 0, found_ndx, 30);
+        if (found > 0) {
+            i++;
+        }
+    }
+    ITERATOR_RELEASE( it );
+
+    return i;
+}
+#endif /* NETSNMP_FEATURE_REMOVE_SWRUN_COUNT_PROCESSES_BY_REGEX */
 
 #ifndef NETSNMP_FEATURE_REMOVE_SWRUN_COUNT_PROCESSES_BY_NAME
 int

agent/mibgroup/mibII/var_route.c
@@ -45,6 +45,7 @@ PERFORMANCE OF THIS SOFTWARE.
  * (simon@switch.ch) 1997
  */
 
+
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-features.h>
 

agent/mibgroup/struct.h
@@ -25,8 +25,17 @@ struct extensible {
 #endif
 };
 
+#if HAVE_PCRE_H
+// Pointer to pcre struct. Abstract pcre native pointer so all *.c files
+// do not have to include pcre.h
+typedef netsnmp_regex_ptr (void *);
+#endif
+
 struct myproc {
     char            name[STRMAX];
+#if HAVE_PCRE_H
+    netsnmp_regex_ptr *regexp;
+#endif
     char            fixcmd[STRMAX];
     int             min;
     int             max;

agent/mibgroup/ucd-snmp/proc.h
@@ -10,7 +10,11 @@ config_require(util_funcs)
 
      extern FindVarMethod var_extensible_proc;
      extern WriteMethod fixProcError;
+     int sh_count_myprocs(struct myproc *);
      int             sh_count_procs(char *);
+#ifdef HAVE_PCRE_H
+     int sh_count_procs_by_regex(char *, netsnmp_regex_ptr);
+#endif
 
 /*
  * config file parsing routines 

configure
@@ -20026,6 +20026,119 @@ done
 
 LIBS="$netsnmp_save_LIBS"
 
+#
+#   regex in process table
+#
+ac_fn_c_check_header_mongrel "$LINENO" "pcre.h" "ac_cv_header_pcre_h" "$ac_includes_default"
+if test "x$ac_cv_header_pcre_h" = xyes; then :
+
+
+$as_echo "#define HAVE_PCRE_H 1" >>confdefs.h
+
+    pcre_h=yes
+
+else
+
+    pcre_h=no
+
+
+fi
+
+
+if test "x$pcreheader_h" = "xno" -o "x$pcre_h" = "xno" ; then
+  if test "x$with_pcre" = "xyes" ; then
+    as_fn_error $? "Could not find the pcre header file needed and was specifically asked to use pcre support" "$LINENO" 5
+  else
+    with_pcre=no
+  fi
+fi
+
+#
+# pcre libraries only needed for the ucd process agent mib software
+#
+if test "x$with_pcre" != "xno"; then
+  _pcrelibs=""
+
+ { $as_echo "$as_me:${as_lineno-$LINENO}: checking for library containing pcre_exec" >&5
+$as_echo_n "checking for library containing pcre_exec... " >&6; }
+if ${netsnmp_cv_func_pcre_exec__pcrelibs+:} false; then :
+  $as_echo_n "(cached) " >&6
+else
+  netsnmp_func_search_save_LIBS="$LIBS"
+     netsnmp_target_val="$_pcrelibs"
+          netsnmp_temp_LIBS="${netsnmp_target_val}  ${LIBS}"
+     netsnmp_result=no
+     LIBS="${netsnmp_temp_LIBS}"
+     cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+/* Override any GCC internal prototype to avoid an error.
+   Use char because int might match the return type of a GCC
+   builtin and then its argument prototype would still apply.  */
+#ifdef __cplusplus
+extern "C"
+#endif
+char pcre_exec ();
+int
+main ()
+{
+return pcre_exec ();
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_link "$LINENO"; then :
+  netsnmp_result="none required"
+else
+  for netsnmp_cur_lib in pcre ; do
+              LIBS="-l${netsnmp_cur_lib} ${netsnmp_temp_LIBS}"
+              cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+/* Override any GCC internal prototype to avoid an error.
+   Use char because int might match the return type of a GCC
+   builtin and then its argument prototype would still apply.  */
+#ifdef __cplusplus
+extern "C"
+#endif
+char pcre_exec ();
+int
+main ()
+{
+return pcre_exec ();
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_link "$LINENO"; then :
+  netsnmp_result=-l${netsnmp_cur_lib}
+                   break
+fi
+rm -f core conftest.err conftest.$ac_objext \
+    conftest$ac_exeext conftest.$ac_ext
+          done
+fi
+rm -f core conftest.err conftest.$ac_objext \
+    conftest$ac_exeext conftest.$ac_ext
+     LIBS="${netsnmp_func_search_save_LIBS}"
+     netsnmp_cv_func_pcre_exec__pcrelibs="${netsnmp_result}"
+fi
+{ $as_echo "$as_me:${as_lineno-$LINENO}: result: $netsnmp_cv_func_pcre_exec__pcrelibs" >&5
+$as_echo "$netsnmp_cv_func_pcre_exec__pcrelibs" >&6; }
+ if test "${netsnmp_cv_func_pcre_exec__pcrelibs}" != "no" ; then
+    if test "${netsnmp_cv_func_pcre_exec__pcrelibs}" != "none required" ; then
+       _pcrelibs="${netsnmp_result} ${netsnmp_target_val}"
+    fi
+
+
+ fi
+
+  if test "x$_pcrelibs" != "x"; then
+    LAGENTLIBS="$LAGENTLIBS $_pcrelibs"
+    LMIBLIBS="$LMIBLIBS $_pcrelibs"
+  fi
+fi
+
 # -*- autoconf -*-
 #
 # generate empty files

configure.d/config_os_libs1
@@ -93,3 +93,35 @@ netsnmp_save_LIBS="$LIBS"
 LIBS="$LMIBLIBS $LAGENTLIBS $LSNMPLIBS $LIBS"
 AC_CHECK_FUNCS([dlopen])
 LIBS="$netsnmp_save_LIBS"
+
+#
+#   regex in process table
+#
+AC_CHECK_HEADER([pcre.h], 
+  [
+    AC_DEFINE([HAVE_PCRE_H], [1], [Define to 1 if you have <pcre.h>.])
+    pcre_h=yes
+  ],
+  [
+    pcre_h=no
+  ]
+)
+if test "x$pcreheader_h" = "xno" -o "x$pcre_h" = "xno" ; then
+  if test "x$with_pcre" = "xyes" ; then
+    AC_MSG_ERROR([Could not find the pcre header file needed and was specifically asked to use pcre support])
+  else
+    with_pcre=no
+  fi
+fi
+
+#
+# pcre libraries only needed for the ucd process agent mib software
+#
+if test "x$with_pcre" != "xno"; then
+  _pcrelibs=""
+  NETSNMP_SEARCH_LIBS([pcre_exec], [pcre],,,, _pcrelibs)
+  if test "x$_pcrelibs" != "x"; then
+    LAGENTLIBS="$LAGENTLIBS $_pcrelibs"
+    LMIBLIBS="$LMIBLIBS $_pcrelibs"
+  fi
+fi

include/net-snmp/net-snmp-config.h.in
@@ -593,6 +593,9 @@
 /* Define to 1 if you have the <pci/pci.h> header file. */
 #undef HAVE_PCI_PCI_H
 
+/* Define to 1 if you have <pcre.h>. */
+#undef HAVE_PCRE_H
+
 /* on aix, if you have perfstat */
 #undef HAVE_PERFSTAT
 
@@ -1623,9 +1626,6 @@
 
 /* Define to `int' if <sys/types.h> does not define. */
 #undef pid_t
-
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-#undef size_t
 
 /* Define to the type of an unsigned integer type of width exactly 16 bits if
    such a type exists and the standard includes do not define it. */

commit 0a38351790d5300a1fe3fa1a3805cfaab2444774

Windows: Visual Studio build fix (was broken by commit 213941dfbee6aa4a3ff9e543652d9d13fe42b359).

agent/mibgroup/ucd-snmp/proc.c
@@ -61,7 +61,6 @@
 #endif
 #include "util_funcs.h"
 #include "kernel.h"
-#include <unistd.h>
 
 static struct myproc *get_proc_instance(struct myproc *, oid);
 struct myproc  *procwatch = NULL;