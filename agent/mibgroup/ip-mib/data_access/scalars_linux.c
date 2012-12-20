795274fc21e1ba03efbe9b6042c6a3513d89debf
Niels Baggesen <nba@users.sourceforge.net>
2012-12-20 07:24:47 +0100

    Clean up and value checking for ipDefaultTTL, ipv6IpDefaultHopLimit

--- agent/mibgroup/ip-mib/data_access/scalars_linux.c
+++ agent/mibgroup/ip-mib/data_access/scalars_linux.c
@@ -9,8 +9,12 @@
 
 #include <net-snmp/data_access/ip_scalars.h>
 
+netsnmp_feature_require(register_num_file_instance)
+
 static const char ipfw_name[] = "/proc/sys/net/ipv4/conf/all/forwarding";
+static const char ipttl_name[] = "/proc/sys/net/ipv4/ip_default_ttl";
 static const char ipfw6_name[] = "/proc/sys/net/ipv6/conf/all/forwarding";
+static const char iphop6_name[] = "/proc/sys/net/ipv6/conf/all/hop_limit";
 
 int
 netsnmp_arch_ip_scalars_ipForwarding_get(u_long *value)
@@ -74,6 +78,70 @@ netsnmp_arch_ip_scalars_ipForwarding_set(u_long value)
     if (1 != rc) {
         DEBUGMSGTL(("access:ipForwarding", "could not write %s\n",
                     ipfw_name));
+        return SNMP_ERR_GENERR;
+    }
+
+    return 0;
+}
+
+int
+netsnmp_arch_ip_scalars_ipDefaultTTL_get(u_long *value)
+{
+    FILE *filep;
+    int rc;
+
+    if (NULL == value)
+        return -1;
+
+
+    filep = fopen(ipttl_name, "r");
+    if (NULL == filep) {
+        DEBUGMSGTL(("access:ipDefaultTTL", "could not open %s\n",
+                    ipttl_name));
+        return -2;
+    }
+
+    rc = fscanf(filep, "%lu", value);
+    fclose(filep);
+    if (1 != rc) {
+        DEBUGMSGTL(("access:ipDefaultTTL", "could not read %s\n",
+                    ipttl_name));
+        return -3;
+    }
+
+    if ((0 == *value) || (255 < *value)) {
+        DEBUGMSGTL(("access:ipDefaultTTL", "unexpected value %ld in %s\n",
+                    *value, ipttl_name));
+        return -4;
+    }
+
+    return 0;
+}
+
+int
+netsnmp_arch_ip_scalars_ipDefaultTTL_set(u_long value)
+{
+    FILE *filep;
+    int rc;
+
+    if (value == 0 || value > 255) {
+        DEBUGMSGTL(("access:ipDefaultTTL", "bad value %ld for %s\n",
+                    value, ipttl_name));
+        return SNMP_ERR_WRONGVALUE;
+    }
+
+    filep = fopen(ipttl_name, "w");
+    if (NULL == filep) {
+        DEBUGMSGTL(("access:ipDefaultTTL", "could not open %s\n",
+                    ipttl_name));
+        return SNMP_ERR_RESOURCEUNAVAILABLE;
+    }
+
+    rc = fprintf(filep, "%ld", value);
+    fclose(filep);
+    if (1 != rc) {
+        DEBUGMSGTL(("access:ipDefaultTTL", "could not write %s\n",
+                    ipttl_name));
         return SNMP_ERR_GENERR;
     }
 
@@ -148,12 +216,74 @@ netsnmp_arch_ip_scalars_ipv6IpForwarding_set(u_long value)
     return 0;
 }
 
+int
+netsnmp_arch_ip_scalars_ipv6IpDefaultHopLimit_get(u_long *value)
+{
+    FILE *filep;
+    int rc;
+
+    if (NULL == value)
+        return -1;
+
+
+    filep = fopen(iphop6_name, "r");
+    if (NULL == filep) {
+        DEBUGMSGTL(("access:ipDefaultHopLimit", "could not open %s\n",
+                    iphop6_name));
+        return -2;
+    }
+
+    rc = fscanf(filep, "%lu", value);
+    fclose(filep);
+    if (1 != rc) {
+        DEBUGMSGTL(("access:ipDefaultHopLimit", "could not read %s\n",
+                    iphop6_name));
+        return -3;
+    }
+
+    if ((0 == *value) || (255 < *value)) {
+        DEBUGMSGTL(("access:ipDefaultHopLimit", "unexpected value %ld in %s\n",
+                    *value, iphop6_name));
+        return -4;
+    }
+
+    return 0;
+}
+
+int
+netsnmp_arch_ip_scalars_ipv6IpDefaultHopLimit_set(u_long value)
+{
+    FILE *filep;
+    int rc;
+
+    if (value == 0 || value > 255) {
+        DEBUGMSGTL(("access:ipDefaultHopLimit", "bad value %ld for %s\n",
+                    value, iphop6_name));
+        return SNMP_ERR_WRONGVALUE;
+    }
+
+    filep = fopen(iphop6_name, "w");
+    if (NULL == filep) {
+        DEBUGMSGTL(("access:ipDefaultHopLimit", "could not open %s\n",
+                    iphop6_name));
+        return SNMP_ERR_RESOURCEUNAVAILABLE;
+    }
+
+    rc = fprintf(filep, "%ld", value);
+    fclose(filep);
+    if (1 != rc) {
+        DEBUGMSGTL(("access:ipDefaultHopLimit", "could not write %s\n",
+                    iphop6_name));
+        return SNMP_ERR_GENERR;
+    }
+
+    return 0;
+}
+
 void
 netsnmp_arch_ip_scalars_register_handlers(void)
 {
     static oid ipReasmTimeout_oid[] = { 1, 3, 6, 1, 2, 1, 4, 13, 0 };
-    static oid ipDefaultTTL_oid[] = { 1, 3, 6, 1, 2, 1, 4, 2, 0 };
-    static oid ipv6IpDefaultHopLimit_oid[] = { 1, 3, 6, 1, 2, 1, 4, 26, 0 };
 
     netsnmp_register_num_file_instance
         ("ipReasmTimeout",
@@ -161,15 +291,4 @@ netsnmp_arch_ip_scalars_register_handlers(void)
          "/proc/sys/net/ipv4/ipfrag_time", ASN_INTEGER,
          HANDLER_CAN_RONLY, NULL, NULL);
 
-    netsnmp_register_num_file_instance
-        ("ipv6IpDefaultHopLimit",
-         ipv6IpDefaultHopLimit_oid, OID_LENGTH(ipv6IpDefaultHopLimit_oid),
-         "/proc/sys/net/ipv6/conf/default/hop_limit", ASN_INTEGER,
-         HANDLER_CAN_RWRITE, NULL, NULL);
-
-     netsnmp_register_num_file_instance
-        ("ipDefaultTTL",
-         ipDefaultTTL_oid, OID_LENGTH(ipDefaultTTL_oid),
-         "/proc/sys/net/ipv4/ip_default_ttl", ASN_INTEGER,
-         HANDLER_CAN_RWRITE, NULL, NULL);
 }
