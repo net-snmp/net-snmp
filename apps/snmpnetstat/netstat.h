39193600ec38deb51379a109648d69e7e9001702
Niels Baggesen <nba@users.sourceforge.net>
2013-03-07 21:44:59 +0100

    snmpnetstat: prototype route6pr

--- apps/snmpnetstat/netstat.h
+++ apps/snmpnetstat/netstat.h
@@ -97,6 +97,7 @@ char	*netname(in_addr_t, in_addr_t);
 char	*ns_print(struct sockaddr *);
 void	routepr(void);
 int     route4pr(int);
+int     route6pr(int);
 int     routexpr(int);
 
 void	intpr(int);
