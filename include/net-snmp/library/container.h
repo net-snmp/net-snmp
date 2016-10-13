9a8509f35c3828c6c0ba15721e81220546cbf7ef
Robert Story <rstory@freesnmp.com>
2016-10-13 13:46:31 -0400

    add VMware copyrights

--- include/net-snmp/library/container.h
+++ include/net-snmp/library/container.h
@@ -7,6 +7,11 @@
  * Copyright (C) 2007 Apple, Inc. All rights reserved.
  * Use is subject to license terms specified in the COPYING file
  * distributed with the Net-SNMP package.
+ *
+ * Portions of this file are copyrighted by:
+ * Copyright (c) 2016 VMware, Inc. All rights reserved.
+ * Use is subject to license terms specified in the COPYING file
+ * distributed with the Net-SNMP package.
  */
 #ifndef NETSNMP_CONTAINER_H
 #define NETSNMP_CONTAINER_H
