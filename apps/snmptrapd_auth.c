114d8426600f931dc70f87438a3c7b87b8cf24c9
Robert Story <rstory@freesnmp.com>
2016-10-13 21:04:24 -0400

    add VMware copyrights

--- apps/snmptrapd_auth.c
+++ apps/snmptrapd_auth.c
@@ -1,6 +1,11 @@
 /*
  * snmptrapd_auth.c - authorize notifications for further processing
  *
+ * Portions of this file are copyrighted by:
+ * Copyright (c) 2016 VMware, Inc. All rights reserved.
+ * Use is subject to license terms specified in the COPYING file
+ * distributed with the Net-SNMP package.
+ *
  */
 #include <net-snmp/net-snmp-config.h>
 
