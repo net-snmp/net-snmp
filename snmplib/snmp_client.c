05a9ee73350e9d04662c83a90bc782f7b617cb87
Robert Story <rstory@freesnmp.com>
2016-11-01 10:31:17 -0400

    fix potential deref of freed ptr
    introduced in commit 9bce2c951

--- snmplib/snmp_client.c
+++ snmplib/snmp_client.c
@@ -1046,13 +1046,11 @@ snmp_synch_response_cb(netsnmp_session * ss,
     ss->callback_magic = (void *) state;
     netsnmp_large_fd_set_init(&fdset, FD_SETSIZE);
 
-    if (snmp_send(ss, pdu) == 0) {
+    if ((state->reqid = snmp_send(ss, pdu)) == 0) {
         snmp_free_pdu(pdu);
         state->status = STAT_ERROR;
-    } else {
-        state->reqid = pdu->reqid;
+    } else
         state->waiting = 1;
-    }
 
     while (state->waiting) {
         numfds = 0;
@@ -1142,13 +1140,11 @@ snmp_sess_synch_response(void *sessp,
     ss->callback_magic = (void *) state;
     netsnmp_large_fd_set_init(&fdset, FD_SETSIZE);
 
-    if (snmp_sess_send(sessp, pdu) == 0) {
+    if ((state->reqid = snmp_sess_send(sessp, pdu)) == 0) {
         snmp_free_pdu(pdu);
         state->status = STAT_ERROR;
-    } else {
+    } else
         state->waiting = 1;
-        state->reqid = pdu->reqid;
-    }
 
     while (state->waiting) {
         numfds = 0;
