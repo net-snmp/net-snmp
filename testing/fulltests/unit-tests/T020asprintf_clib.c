4ad885bc3047f7a69bb8f12e9bc9df73768611c2
Bart Van Assche <bvanassche@acm.org>
2017-04-16 09:03:20 -0700

    snmplib: Add asprintf()
    Add an asprintf() implementation for the platforms that do not provide
    asprintf() through their C library.

---
+++ testing/fulltests/unit-tests/T020asprintf_clib.c
@@ -0,0 +1,11 @@
+/* HEADER Testing asprintf() */
+
+char *strp = NULL;
+
+OK(asprintf(&strp, "%s", "") == 0, "asprintf() (1) failed");
+OKF(strp && strcmp(strp, "") == 0, ("%s <> \"\"", strp));
+OK(asprintf(&strp, "%s", "abc") == 3, "asprintf() (1) failed");
+OKF(strp && strcmp(strp, "abc") == 0, ("%s <> \"abc\"", strp));
+OK(asprintf(&strp, "%s %s", "abc", "def") == 7, "asprintf() (2) failed");
+OKF(strp && strcmp(strp, "abc def") == 0, ("%s <> \"abc def\"", strp));
+free(strp);
