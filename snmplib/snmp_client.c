65128febb24bddb39061e2d06993c8449e37860e
Bill Fenner <fenner@gmail.com>
2017-05-30 14:13:34 -0700

    Allow clients to use reqid zero
    Some of 9bce2c9519a0551ee0c82ac4706fd9f3f875feab got lost
    when merging it to master.  Re-apply the changes to handle
    snmp_*send() returning 1 when successfully sending a PDU
    with reqid 0.

--- snmplib/snmp_client.c
+++ snmplib/snmp_client.c
@@ -1046,11 +1046,13 @@ snmp_synch_response_cb(netsnmp_session * ss,
     ss->callback_magic = (void *) state;
     netsnmp_large_fd_set_init(&fdset, FD_SETSIZE);
 
-    if ((state->reqid = snmp_send(ss, pdu)) == 0) {
+    if (snmp_send(ss, pdu) == 0) {
         snmp_free_pdu(pdu);
         state->status = STAT_ERROR;
-    } else
+    } else {
+        state->reqid = pdu->reqid;
         state->waiting = 1;
+    }
 
     while (state->waiting) {
         numfds = 0;
@@ -1138,11 +1140,13 @@ snmp_sess_synch_response(void *sessp,
     ss->callback_magic = (void *) state;
     netsnmp_large_fd_set_init(&fdset, FD_SETSIZE);
 
-    if ((state->reqid = snmp_sess_send(sessp, pdu)) == 0) {
+    if (snmp_sess_send(sessp, pdu) == 0) {
         snmp_free_pdu(pdu);
         state->status = STAT_ERROR;
-    } else
+    } else {
         state->waiting = 1;
+        state->reqid = pdu->reqid;
+    }
 
     while (state->waiting) {
         numfds = 0;
