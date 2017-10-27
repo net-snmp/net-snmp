d8e7bc0246009a9ac58784c1b67baafb44d77902
Robert Story <rstory@isi.edu>
2017-10-26 21:42:29 -0400

    Version number update: 5.4.5.rc1

--- snmplib/snmp_version.c
+++ snmplib/snmp_version.c
@@ -5,7 +5,7 @@
 #ifndef UCD_COMPATIBLE
 static
 #endif
-const char     *NetSnmpVersionInfo = "5.4.5.pre2";
+const char     *NetSnmpVersionInfo = "5.4.5.rc1";
 
 const char     *
 netsnmp_get_version()
