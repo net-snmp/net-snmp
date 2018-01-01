net-snmp/net-snmp

commit db2f552f427be8cf3886f3a213e291abb7894f14

WIP: snmppcap
snmppcap parses a pcap file, sending each packet through
snmp_parse().

It's a work in progress, because:
- snmp_parse() is static inside snmp_api
- it's very much hardcoded for ethernet/ipv4/udp/snmp
but it's useful enough to commit, and maybe others can
work on it.

apps/snmppcap.c
@@ -1,0 +1,148 @@
+/*
+ * Copyright (c) 2015, Arista Networks, inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pcap/pcap.h>
+
+typedef struct mystuff {
+    int pktnum;
+    netsnmp_session *pss;
+} mystuff_t;
+
+void
+handle_pcap(u_char *user, const struct pcap_pkthdr *h,
+                                         const u_char *bytes)
+{
+    size_t len;
+    u_char *buf;
+    int skip;
+    netsnmp_pdu pdu;
+    mystuff_t *mystuff = (mystuff_t *)user;
+    int retval;
+    netsnmp_variable_list *vars;
+
+    /*
+     * If it's not a full packet, then we can't parse it.
+     */
+    if ( h->caplen < h->len ) {
+        printf( "Skipping packet; we only have %d of %d bytes\n", h->caplen, h->len );
+        return;
+    }
+
+    /*
+     * For now, no error checking and almost no parsing.
+     * Assume that we have all Ethernet/IPv4/UDP/SNMP.
+     */
+    skip = 14 /* Ethernet */ + 20 /* IPv4 */ + 8 /* UDP */;
+    buf = bytes + skip;
+    len = h->len - skip;
+    if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,NETSNMP_DS_LIB_DUMP_PACKET)) {
+        snmp_log(LOG_DEBUG, "\nReceived %d byte packet\n",
+                 len);
+        xdump(buf, len, "");
+    }
+    /* snmp_pdu_parse( &pdu, buf, &len ); */
+    /* can we get away with NULL? */
+    memset( &pdu, '\0', sizeof( pdu ) );
+    retval = snmp_parse( NULL, mystuff->pss, &pdu, buf, len );
+    printf( "packet %d retval %d (%s)\n", mystuff->pktnum++, retval, retval ? snmp_api_errstring( retval ) : "Success" );
+    for (vars = pdu.variables; vars; vars = vars->next_variable) {
+       printf( "   " );
+       print_variable(vars->name, vars->name_length, vars );
+    }
+}
+
+void
+usage(void)
+{
+    fprintf(stderr, "USAGE: snmppcap ");
+    snmp_parse_args_usage(stderr);
+    fprintf(stderr, " FILE\n\n");
+    snmp_parse_args_descriptions(stderr);
+}
+
+int main(int argc, char **argv)
+{
+    netsnmp_session session, *ss;
+    int arg;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    char *fname;
+    pcap_t *p;
+    mystuff_t mystuff;
+
+    /*
+     * snmp_parse_args usage here is totally overkill, but trying to
+     * parse -D
+     */
+    switch (arg = snmp_parse_args(argc, argv, &session, "", NULL)) {
+    case NETSNMP_PARSE_ARGS_ERROR:
+        exit(1);
+    case NETSNMP_PARSE_ARGS_SUCCESS_EXIT:
+        exit(0);
+    case NETSNMP_PARSE_ARGS_ERROR_USAGE:
+        usage();
+        exit(1);
+    default:
+        break;
+    }
+    if (arg != argc) {
+        fprintf(stderr, "Specify exactly one file name\n");
+        usage();
+        exit(1);
+    }
+    fname = argv[ arg-1 ];
+    p = pcap_open_offline( fname, errbuf );
+    if ( p == NULL ) {
+        fprintf(stderr, "%s: %s\n", fname, errbuf );
+        return 1;
+    }
+    if ( pcap_datalink( p ) != DLT_EN10MB) {
+        fprintf(stderr, "Only Ethernet pcaps currently supported\n");
+        return 2;
+    }
+    /* todo: add the option of a filter here */
+    mystuff.pktnum = 1;
+    mystuff.pss = &session;
+    /*XXX*/
+    session.securityModel = SNMP_SEC_MODEL_USM;
+    printf("flags %x securityModel %d version %d securityNameLen %d securityEngineIDLen %d\n",
+          session.flags, session.securityModel, session.version,
+          session.securityNameLen, session.securityEngineIDLen);
+    create_user_from_session(&session);
+    pcap_loop(p, -1, handle_pcap, &mystuff);
+    return 0;
+}

apps/Makefile.in
@@ -41,6 +41,8 @@ mysubdir=apps
 @NETSNMP_BUILD_SET_PROG_FALSE@SNMPVACMINSTALLBINPROG = 
 @NETSNMP_BUILD_SET_PROG_TRUE@SNMPVACMFEATUREPROG = snmpvacm.ft
 @NETSNMP_BUILD_SET_PROG_FALSE@SNMPVACMFEATUREPROG = 
+@NETSNMP_BUILD_PCAP_PROG_TRUE@SNMPPCAPINSTALLBINPROG = snmppcap$(EXEEXT)
+@NETSNMP_BUILD_PCAP_PROG_FALSE@SNMPPCAPINSTALLBINPROG =
 
 @NETSNMP_HAVE_AGENTX_LIBS_TRUE@AGENTXTRAP = agentxtrap$(EXEEXT)
 @NETSNMP_HAVE_AGENTX_LIBS_FALSE@AGENTXTRAP =
@@ -62,7 +64,8 @@ INSTALLBINPROGS	= snmpget$(EXEEXT) 			\
 		$(AGENTXTRAP)				\
 		$(SNMPVACMINSTALLBINPROG)	        \
                 $(SSHINSTALLBINPROG) $(TLSINSTALLBINPROG) \
-		$(USMINSTALLBINPROG) $(EKCSTALLBINPROG)
+		$(USMINSTALLBINPROG) $(EKCSTALLBINPROG) \
+		$(SNMPPCAPINSTALLBINPROG)
 
 INSTALLSBINPROGS = snmptrapd$(EXEEXT)
 
@@ -210,6 +213,11 @@ snmpdf$(EXEEXT):    snmpdf.$(OSUFFIX) $(USELIBS)
 
 snmpps$(EXEEXT):    snmpps.$(OSUFFIX) $(USELIBS)
 	$(LINK) ${CFLAGS} -o $@ snmpps.$(OSUFFIX) ${LDFLAGS} @LIBCURSES@ ${LIBS}
+
+# TODO: @LIBPCAP@ and configure magic
+# (including @NETSNMP_BUILD_PCAP_PROG_{TRUE,FALSE}@)
+snmppcap$(EXEEXT):    snmppcap.$(OSUFFIX) $(USELIBS)
+	$(LINK) ${CFLAGS} -o $@ snmppcap.$(OSUFFIX) ${LDFLAGS} ${LIBS} -lpcap
 
 libnetsnmptrapd.$(LIB_EXTENSION)$(LIB_VERSION): $(LLIBTRAPD_OBJS)
 	$(LIB_LD_CMD) $@ ${LLIBTRAPD_OBJS} $(MIBLIB) $(USELIBS) $(PERLLDOPTS_FOR_LIBS) $(LIB_LD_LIBS)

commit 1cfb9326cda38afd557a785c5cc08bcdfd8c248d

apps/snmppcap: Fix three format specifiers
Fixes: commit db2f552f427b ("WIP: snmppcap").

apps/snmppcap.c
@@ -226,7 +226,7 @@ int main(int argc, char **argv)
     mystuff.pktnum = 0;
     /* todo: user, etc. parsing. */
     ss->securityModel = SNMP_SEC_MODEL_USM;
-    printf("flags %x securityModel %d version %d securityNameLen %d securityEngineIDLen %d\n",
+    printf("flags %lx securityModel %d version %ld securityNameLen %" NETSNMP_PRIz "d securityEngineIDLen %" NETSNMP_PRIz "d\n",
           ss->flags, ss->securityModel, ss->version,
           ss->securityNameLen, ss->securityEngineIDLen);
     create_user_from_session(ss);