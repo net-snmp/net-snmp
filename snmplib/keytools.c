net-snmp/net-snmp

commit 9e49de2e03b18ceb1b7cba2e715ad6180f394815

NEWS: snmplib: AES-192/AES-256 compatibility with SNMP Research / CISCO
- they use key localization algorithm from draft-reeder-snmpv3-usm-3desede-00
  instead of the Blumenthal draft

Also:
- configure option to enable larger AES key length code
- localized key extension via blumenthal and reeder method

snmplib/keytools.c
@@ -42,6 +42,8 @@
 #include <dmalloc.h>
 #endif
 
+#include <math.h>
+
 #include <net-snmp/types.h>
 #include <net-snmp/output_api.h>
 #include <net-snmp/utilities.h>
@@ -271,7 +273,8 @@ generate_Ku(const oid * hashtype, u_int hashtype_len,
 
 
 #ifdef NETSNMP_ENABLE_TESTING_CODE
-    DEBUGMSGTL(("generate_Ku", "generating Ku (from %s): ", P));
+    DEBUGMSGTL(("generate_Ku", "generating Ku (%s from %s): ",
+                usm_lookup_auth_str(auth_type), P));
     for (i = 0; i < *kulen; i++)
         DEBUGMSG(("generate_Ku", "%02x", Ku[i]));
     DEBUGMSG(("generate_Ku", "\n"));
@@ -454,6 +457,279 @@ generate_kul(const oid * hashtype, u_int hashtype_len,
 #else
 _KEYTOOLS_NOT_AVAILABLE
 #endif                          /* internal or openssl */
+
+/*******************************************************************-o-******
+ * _kul_extend_blumenthal
+ *
+ * Parameters:
+ *     *hashoid        MIB OID for the hash transform type.
+ *      hashoid_len    Length of the MIB OID hash transform type.
+ *     *origKul        original kul (localized; IN/OUT)
+ *     *origKulLen     Length of original kul in bytes (IN/OUT)
+ *      origKulSize    Size of original kul buffer
+ *      needKeyLen     Size needed for key
+ *
+ * Returns:
+ *      SNMPERR_SUCCESS      Success.
+ *      SNMPERR_GENERR       All errors.
+ */
+#ifdef NETSNMP_DRAFT_BLUMENTHAL_AES_04
+static int
+_kul_extend_blumenthal(int needKeyLen, oid *hashoid, u_int hashoid_len,
+                       u_char *origKul, size_t *origKulLen, int origKulSize)
+{
+    char newKul[USM_LENGTH_KU_HASHBLOCK];
+    size_t newKulLen;
+    int count, hashBits, hashBytes, authtype, i, saveLen;
+
+    DEBUGMSGTL(("usm:extend_kul", " blumenthal called\n"));
+
+    if (NULL == hashoid || NULL == origKul || NULL == origKulLen ||
+        needKeyLen > origKulSize)
+        return SNMPERR_GENERR;
+
+    authtype = sc_get_authtype(hashoid, hashoid_len);
+    if (authtype < 0 )
+        return SNMPERR_GENERR;
+
+    saveLen = *origKulLen;
+    needKeyLen -= *origKulLen; /* subtract bytes we already have */
+
+    /*
+     * 3.1.2.1:
+     *   1)Let Hnnn() the hash function of the authentication protocol for
+     *      the user U on the SNMP authoritative engine E. nnn being the size
+     *      of the output of the hash function (e.g. nnn=128 bits for MD5, or
+     *      nnn=160 bits for SHA1).
+     */
+    hashBytes = sc_get_proper_auth_length_bytype(authtype);
+    hashBits = 8 * hashBytes;
+
+    /* 3.1.2.1:
+     *   2)Set c = ceil ( 256 / nnn )
+     */
+    count = ceil( 256 / hashBits );
+
+    /* 3.1.2.1:
+     *   3)For i = 1, 2, ..., c
+     *        a.Set Kul = Kul || Hnnn(Kul);     Where Hnnn() is the hash
+     *          function of the authentication protocol defined for that user
+     */
+    for(i = 0; i < count; ++i) {
+        int copyLen, rc;
+
+        rc = sc_hash_type( authtype, origKul, *origKulLen, newKul, &newKulLen);
+        if (SNMPERR_SUCCESS != rc)
+            return SNMPERR_GENERR;
+
+        copyLen = SNMP_MIN(needKeyLen, newKulLen);
+        memcpy(origKul + *origKulLen, newKul, copyLen);
+        needKeyLen -= copyLen;
+        *origKulLen += copyLen;
+
+        /** not part of the draft, but stop if we already have enought bits */
+        if (needKeyLen <= 0)
+            break;
+    }
+
+    DEBUGMSGTL(("usm:extend_kul:blumenthal","orig len %d, new len %d\n",
+                saveLen, *origKulLen));
+
+    return SNMPERR_SUCCESS;
+}
+#endif /* NETSNMP_DRAFT_BLUMENTHAL_AES_04 */
+
+/*******************************************************************-o-******
+ * _kul_extend_reeder
+ *
+ * Parameters:
+ *     *hashoid        MIB OID for the hash transform type.
+ *      hashoid_len    Length of the MIB OID hash transform type.
+ *     *engineID       engineID
+ *      engineIDLen    Length of engineID
+ *     *origKul        original kul (localized; IN/OUT)
+ *     *origKulLen     Length of original kul in bytes (IN/OUT)
+ *      origKulSize    Size of original kul buffer
+ *
+ * Returns:
+ *      SNMPERR_SUCCESS      Success.
+ *      SNMPERR_GENERR       All errors.
+ */
+#if defined(NETSNMP_DRAFT_REEDER_3DES) || defined(NETSNMP_DRAFT_BLUMENTHAL_AES_04)
+static int
+_kul_extend_reeder(int needKeyLen, oid *hashoid, u_int hashoid_len,
+                   u_char *engineID, int engineIDLen,
+                   u_char *origKul, size_t *origKulLen, int origKulSize)
+{
+    char newKu[USM_LENGTH_KU_HASHBLOCK], newKul[USM_LENGTH_KU_HASHBLOCK];
+    size_t newKuLen, newKulLen, saveLen;
+    int authType, copylen;
+
+    DEBUGMSGTL(("usm:extend_kul", " reeder called\n"));
+
+    if (NULL == hashoid || NULL == engineID || NULL == origKul ||
+        NULL == origKulLen || needKeyLen > origKulSize)
+        return SNMPERR_GENERR;
+
+    authType = sc_get_authtype(hashoid, hashoid_len);
+    if (SNMPERR_GENERR == authType)
+        return SNMPERR_GENERR;
+    newKulLen = sc_get_proper_auth_length_bytype(authType);
+    saveLen = *origKulLen;
+    needKeyLen -= *origKulLen; /* subtract bytes we already have */
+    while (needKeyLen > 0) {
+
+        newKuLen = sizeof(newKu);
+        /*
+         * hash the existing key using the passphrase to Ku algorithm.
+         * [ Ku' = Ku(kul) ]
+         */
+        if (generate_Ku(hashoid, hashoid_len, origKul, *origKulLen,
+                        newKu, &newKuLen) != SNMPERR_SUCCESS)
+            return SNMPERR_GENERR;
+
+        /*
+         * localize the new key generated from current localized key
+         * and append it to the current localized key.
+         * [ kul' = kul || kul(Ku') ]
+         */
+        newKulLen = sizeof(newKul);
+        if(generate_kul(hashoid, hashoid_len, engineID, engineIDLen,
+                        newKu, newKuLen, newKul,
+                        &newKulLen) != SNMPERR_SUCCESS)
+            return SNMPERR_GENERR;
+
+        copylen = SNMP_MIN(needKeyLen, newKulLen);
+        memcpy(origKul + *origKulLen, newKul, copylen);
+        needKeyLen -= copylen;
+        *origKulLen += copylen;
+    }
+
+    DEBUGMSGTL(("usm:extend_kul:reeder","orig len %d, new len %d\n",
+                saveLen, *origKulLen));
+    return SNMPERR_SUCCESS;
+}
+#endif /* NETSNMP_DRAFT_REEDER_3DES || NETSNMP_DRAFT_BLUMENTHAL_AES_04 */
+
+/*******************************************************************-o-******
+ * netsnmp_extend_key
+ *
+ * Extend a kul buffer to the needed key size. if the passed kulBuf
+ * is not large enough, a new one will be allocated and the old one
+ * will be freed.
+ *
+ * Parameters:
+ *       neededKeyLen   The neede key length
+ *      *hashoid        MIB OID for the hash transform type.
+ *       hashoid_len    Length of the MIB OID hash transform type.
+ *       privType       Privay algorithm type
+ *       engineID       engineID
+ *       engineIDLen    Length of engineID
+ *     **kulBuf         Pointer to a buffer pointer
+ *      *kulBufLen      Length of current kul buffer
+ *       kulBufSize     Allocated size of current kul buffer
+ *
+ * OUT:
+ *     **kulBuf         New kulBuf pointer (if it needed to be expanded)
+ *      *kulBufLen      Length of new kul buffer
+ *
+ * Returns:
+ *     SNMPERR_SUCCESS  Success.
+ *     SNMPERR_GENERR   All errors.
+ */
+int
+netsnmp_extend_kul(u_int needKeyLen, oid *hashoid, u_int hashoid_len,
+                   int privType, u_char *engineID, u_int engineIDLen,
+                   u_char **kulBuf, u_int *kulBufLen, u_int kulBufSize)
+{
+    int ret;
+    u_char *newKul;
+    size_t newKulLen;
+    /* authType = sc_get_authtype(hashoid, hashoid_len); */
+
+    DEBUGMSGTL(("usm:extend_kul", " called\n"));
+
+    if (*kulBufLen >= needKeyLen) {
+        DEBUGMSGTL(("usm:extend_kul", " key already big enough\n"));
+        return SNMPERR_SUCCESS; /* already have enough key material */
+    }
+
+    switch (privType & (USM_PRIV_MASK_ALG | USM_PRIV_MASK_VARIANT)) {
+#ifdef NETSNMP_DRAFT_BLUMENTHAL_AES_04
+        case USM_CREATE_USER_PRIV_AES192:
+        case USM_CREATE_USER_PRIV_AES256:
+            break;
+#endif
+#if defined(NETSNMP_DRAFT_REEDER_3DES) || defined(NETSNMP_DRAFT_BLUMENTHAL_AES_04)
+        case USM_CREATE_USER_PRIV_3DES:
+            break;
+#endif
+         default:
+            DEBUGMSGTL(("usm:extend_kul",
+                        "no extension method defined for priv type 0x%x\n",
+                        privType));
+            return SNMPERR_SUCCESS;
+    }
+
+    DEBUGMSGTL(("usm:extend_kul", " have %d bytes; need %d\n", *kulBufLen,
+                needKeyLen));
+
+    if (kulBufSize < needKeyLen) {
+        newKul = calloc(1, needKeyLen);
+        if (NULL == newKul)
+            return SNMPERR_GENERR;
+        memcpy(newKul, *kulBuf, *kulBufLen);
+        newKulLen = *kulBufLen;
+        kulBufSize = needKeyLen;
+    }
+    else {
+        newKul = *kulBuf;
+        newKulLen = *kulBufLen;
+    }
+
+    ret = SNMPERR_SUCCESS; /* most privTypes don't need extended kul */
+    switch (privType & (USM_PRIV_MASK_ALG | USM_PRIV_MASK_VARIANT)) {
+#ifdef NETSNMP_DRAFT_BLUMENTHAL_AES_04
+        case USM_CREATE_USER_PRIV_AES192:
+        case USM_CREATE_USER_PRIV_AES256:
+        {
+            int reeder = privType & USM_AES_REEDER_FLAG;
+            if (!reeder) {
+                ret = _kul_extend_blumenthal(needKeyLen, hashoid, hashoid_len,
+                                             newKul, &newKulLen, kulBufSize);
+                break;
+            }
+            /** fall through to reeder, if available */
+            ret = SNMPERR_GENERR; /* in case reeder not available */
+        }
+#endif
+#if defined(NETSNMP_DRAFT_REEDER_3DES) || defined(NETSNMP_DRAFT_BLUMENTHAL_AES_04)
+        case USM_CREATE_USER_PRIV_3DES:
+            ret = _kul_extend_reeder(needKeyLen, hashoid, hashoid_len,
+                                     engineID, engineIDLen,
+                                     newKul, &newKulLen, kulBufSize);
+            break;
+#endif
+         default:
+            DEBUGMSGTL(("usm:extend_kul", " unknown priv type 0x%x\n", privType));
+            ret = SNMPERR_GENERR;
+    }
+
+    if (SNMPERR_SUCCESS == ret) {
+        *kulBufLen = newKulLen;
+        if (newKul != *kulBuf) {
+            free(*kulBuf);
+            *kulBuf = newKul;
+        }
+    }
+    else {
+        if (newKul != *kulBuf)
+            free(newKul);
+    }
+
+    return ret;
+}
+
 /*******************************************************************-o-******
  * encode_keychange
  *

snmplib/snmpusm.c
@@ -143,16 +143,16 @@ static usm_alg_type_t usm_auth_type[] = {
     { "MD5", NETSNMP_USMAUTH_HMACMD5 },
 #endif
 #ifdef HAVE_EVP_SHA224
-    { "SHA-128", NETSNMP_USMAUTH_HMAC128SHA224 },
-    { "SHA128", NETSNMP_USMAUTH_HMAC128SHA224 },
-    { "SHA-192", NETSNMP_USMAUTH_HMAC192SHA256 },
-    { "SHA192", NETSNMP_USMAUTH_HMAC192SHA256 },
+    { "SHA-224", NETSNMP_USMAUTH_HMAC128SHA224 },
+    { "SHA224", NETSNMP_USMAUTH_HMAC128SHA224 },
+    { "SHA-256", NETSNMP_USMAUTH_HMAC192SHA256 },
+    { "SHA256", NETSNMP_USMAUTH_HMAC192SHA256 },
 #endif
 #ifdef HAVE_EVP_SHA384
-    { "SHA-256", NETSNMP_USMAUTH_HMAC256SHA384 },
-    { "SHA256", NETSNMP_USMAUTH_HMAC256SHA384 },
-    { "SHA-384",  NETSNMP_USMAUTH_HMAC384SHA512 },
-    { "SHA384",  NETSNMP_USMAUTH_HMAC384SHA512 },
+    { "SHA-384", NETSNMP_USMAUTH_HMAC256SHA384 },
+    { "SHA384", NETSNMP_USMAUTH_HMAC256SHA384 },
+    { "SHA-512",  NETSNMP_USMAUTH_HMAC384SHA512 },
+    { "SHA512",  NETSNMP_USMAUTH_HMAC384SHA512 },
 #endif
     { NULL, -1 }
 };
@@ -4602,9 +4602,9 @@ usm_create_usmUser_from_string(char *line, const char **errorMsg)
     struct usmUser *newuser;
     u_char          userKey[SNMP_MAXBUF_SMALL], *tmpp;
     size_t          userKeyLen = SNMP_MAXBUF_SMALL;
-    size_t          privKeyLen = 0;
+    size_t          privKeyLen = 0, privKeySize;
     size_t          ret;
-    int             ret2, properLen, priv_type;
+    int             ret2, properLen, properPrivKeyLen, priv_type;
     const oid      *def_auth_prot, *def_priv_prot;
     size_t          def_auth_prot_len, def_priv_prot_len;
 
@@ -4873,12 +4873,12 @@ usm_create_usmUser_from_string(char *line, const char **errorMsg)
     }
 #endif /* NETSNMP_FORCE_SYSTEM_V3_AUTHPRIV */
 
-    privKeyLen = sc_get_proper_priv_length_bytype(priv_type);
-    if (privKeyLen == 0)
+    properPrivKeyLen = sc_get_proper_priv_length_bytype(priv_type);
+    if (properPrivKeyLen == 0)
         DEBUGMSGTL(("usmUser",
                     "warning: unknown keyLen for privacy protocol\n"));
     if (USM_CREATE_USER_PRIV_DES == priv_type)
-        privKeyLen *= 2; /* ?? we store salt with key */
+        properPrivKeyLen *= 2; /* ?? we store salt with key */
 
     /*
      * READ: Encryption Pass Phrase or key
@@ -4889,7 +4889,7 @@ usm_create_usmUser_from_string(char *line, const char **errorMsg)
          */
         newuser->privKey = netsnmp_memdup(newuser->authKey,
                                           newuser->authKeyLen);
-        privKeyLen = newuser->privKeyLen = newuser->authKeyLen;
+        privKeySize = newuser->privKeyLen = newuser->authKeyLen;
         if (newuser->flags & USMUSER_FLAG_KEEP_MASTER_KEY) {
             newuser->privKeyKu = netsnmp_memdup(newuser->authKeyKu,
                                                 newuser->authKeyKuLen);
@@ -4930,10 +4930,12 @@ usm_create_usmUser_from_string(char *line, const char **errorMsg)
         }        
         
         /*
-         * And turn it into a localized key 
+         * And turn it into a localized key
+         * Allocate enough space for greater of auth mac and privKey len.
          */
-        newuser->privKey = (u_char *) malloc(privKeyLen);
-        newuser->privKeyLen = privKeyLen;
+        privKeySize = SNMP_MAX(properPrivKeyLen, properLen);
+        newuser->privKey = (u_char *) malloc(privKeySize);
+        newuser->privKeyLen = privKeySize;
 
         if (strcmp(buf,"-l") == 0) {
             /* a local key is directly specified */
@@ -4955,14 +4957,26 @@ usm_create_usmUser_from_string(char *line, const char **errorMsg)
                 goto fail;
             }
         }
+
+        if (newuser->privKeyLen < properPrivKeyLen) {
+            ret = usm_extend_user_kul(newuser, properPrivKeyLen);
+            if (ret != SNMPERR_SUCCESS) {
+                *errorMsg = "could not extend localized privacy key to required length.";
+                goto fail;
+            }
+        }
     }
 
-    if ((newuser->privKeyLen >= privKeyLen) || (privKeyLen == 0)){
-      newuser->privKeyLen = privKeyLen;
+    if ((newuser->privKeyLen >= properPrivKeyLen) || (properPrivKeyLen == 0)){
+        DEBUGMSGTL(("9:usmUser", "truncating privKeyLen from %d to %d\n",
+                    newuser->privKeyLen, properPrivKeyLen));
+        newuser->privKeyLen = properPrivKeyLen;
     }
     else {
-        DEBUGMSGTL(("usmUser", "privKey length %d is smaller than %d required by privProtoco\n", newuser->privKeyLen, privKeyLen));
-      *errorMsg = "privKey length is smaller than required by privProtocol";
+        DEBUGMSGTL(("usmUser",
+                    "privKey length %d < %d required by privProtocol\n",
+                    newuser->privKeyLen, properPrivKeyLen));
+      *errorMsg = "privKey length is less than required by privProtocol";
       goto fail;
     }
 

aclocal.m4
@@ -1,7 +1,7 @@
-# generated automatically by aclocal 1.11 -*- Autoconf -*-
+# generated automatically by aclocal 1.15 -*- Autoconf -*-
 
-# Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-# 2005, 2006, 2007, 2008, 2009  Free Software Foundation, Inc.
+# Copyright (C) 1996-2014 Free Software Foundation, Inc.
+
 # This file is free software; the Free Software Foundation
 # gives unlimited permission to copy and/or distribute it,
 # with or without modifications, as long as this notice is preserved.
@@ -11,6 +11,7 @@
 # even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 # PARTICULAR PURPOSE.
 
+m4_ifndef([AC_CONFIG_MACRO_DIRS], [m4_defun([_AM_CONFIG_MACRO_DIRS], [])m4_defun([AC_CONFIG_MACRO_DIRS], [_AM_CONFIG_MACRO_DIRS($@)])])
 m4_include([m4/ac_add_search_path.m4])
 m4_include([m4/ac_msg_cache.m4])
 m4_include([m4/ac_msg_module_dbg.m4])

configure.d/config_project_with_enable
@@ -410,6 +410,13 @@ if test "x$enable_internal_md5" = "xyes"; then
   fi
 fi
 
+NETSNMP_ARG_ENABLE(blumenthal-aes,
+[  --enable-blumenthal-aes         Enable AES-192/AES-256 (Blumenthal draft)])
+if test "x$enable_blumenthal_aes" = "xyes"; then
+  AC_DEFINE([NETSNMP_DRAFT_BLUMENTHAL_AES_04], 1,
+            [Define if AES-192/AES-256 encryption should be supported])
+fi
+
 
 ##
 #   Project: Library: Misc configuration

dist/autoconf-version
@@ -1,1 +1,1 @@
-2.68
+2.69

include/net-snmp/library/scapi.h
@@ -36,6 +36,11 @@ extern          "C" {
 #define SNMP_TRANS_PRIVLEN_1DES		64
 #define SNMP_TRANS_PRIVLEN_1DES_IV	64
 
+#ifdef NETSNMP_DRAFT_REEDER_3DES
+#define SNMP_TRANS_PRIVLEN_3DESEDE      256
+#define SNMP_TRANS_PRIVLEN_3DESEDE_IV   64
+#endif /* NETSNMP_DRAFT_REEDER_3DES */
+
 #define SNMP_TRANS_PRIVLEN_AES          128
 #define SNMP_TRANS_PRIVLEN_AES_IV       128
 
@@ -130,6 +135,10 @@ typedef struct netsnmp_priv_alg_info_s {
                                u_char * iv, u_int ivlen,
                                u_char * ciphertext, u_int ctlen,
                                u_char * plaintext, size_t * ptlen);
+
+    int             sc_hash_type(int auth_type, const u_char * buf,
+                                 size_t buf_len, u_char * MAC,
+                                 size_t * MAC_len);
 
     int             sc_hash(const oid * hashtype, size_t hashtypelen,
                             const u_char * buf, size_t buf_len,

include/net-snmp/net-snmp-config.h.in
@@ -1466,6 +1466,9 @@
 /* Define if SNMPv2c code should not be included */
 #undef NETSNMP_DISABLE_SNMPV2C
 
+/* Define if AES-192/AES-256 encryption should be supported */
+#undef NETSNMP_DRAFT_BLUMENTHAL_AES_04
+
 /* Define if you are embedding perl in the main agent. */
 #undef NETSNMP_EMBEDDED_PERL
 

commit 940888df4fa073a5db706fbffa6ec26a0de26de9

snmplib: Fix format specifiers and remove unused local variables
Fixes: commit 9e49de2e03b1 ("NEWS: snmplib: AES-192/AES-256 compatibility with SNMP Research / CISCO")

snmplib/keytools.c
@@ -671,8 +671,8 @@ netsnmp_extend_kul(u_int needKeyLen, oid *hashoid, u_int hashoid_len,
             return SNMPERR_SUCCESS;
     }
 
-    DEBUGMSGTL(("usm:extend_kul", " have %d bytes; need %d\n", *kulBufLen,
-                needKeyLen));
+    DEBUGMSGTL(("usm:extend_kul", " have %" NETSNMP_PRIz "d bytes; need %d\n",
+                *kulBufLen, needKeyLen));
 
     if (kulBufSize < needKeyLen) {
         newKul = calloc(1, needKeyLen);

snmplib/scapi.c
@@ -1473,7 +1473,6 @@ sc_decrypt(const oid * privtype, size_t privtypelen,
 #endif
     DES_cblock      key_struct;
 #endif
-    u_int           properlength = 0, properlength_iv = 0;
     netsnmp_priv_alg_info *pai = NULL;
 
     DEBUGTRACE;

snmplib/snmpusm.c
@@ -4602,7 +4602,7 @@ usm_create_usmUser_from_string(char *line, const char **errorMsg)
     struct usmUser *newuser;
     u_char          userKey[SNMP_MAXBUF_SMALL], *tmpp;
     size_t          userKeyLen = SNMP_MAXBUF_SMALL;
-    size_t          privKeyLen = 0, privKeySize;
+    size_t          privKeySize;
     size_t          ret;
     int             ret2, properLen, properPrivKeyLen, priv_type;
     const oid      *def_auth_prot, *def_priv_prot;
@@ -4710,7 +4710,6 @@ usm_create_usmUser_from_string(char *line, const char **errorMsg)
         newuser->authProtocolLen = def_auth_prot_len;
     } else {
         const oid *auth_prot;
-        size_t auth_prot_len;
         int auth_type = usm_lookup_auth_type(buf);
         if (auth_type < 0) {
             *errorMsg = "unknown authProtocol";
@@ -4968,13 +4967,13 @@ usm_create_usmUser_from_string(char *line, const char **errorMsg)
     }
 
     if ((newuser->privKeyLen >= properPrivKeyLen) || (properPrivKeyLen == 0)){
-        DEBUGMSGTL(("9:usmUser", "truncating privKeyLen from %d to %d\n",
+        DEBUGMSGTL(("9:usmUser", "truncating privKeyLen from %" NETSNMP_PRIz "d to %d\n",
                     newuser->privKeyLen, properPrivKeyLen));
         newuser->privKeyLen = properPrivKeyLen;
     }
     else {
         DEBUGMSGTL(("usmUser",
-                    "privKey length %d < %d required by privProtocol\n",
+                    "privKey length %" NETSNMP_PRIz "d < %d required by privProtocol\n",
                     newuser->privKeyLen, properPrivKeyLen));
       *errorMsg = "privKey length is less than required by privProtocol";
       goto fail;