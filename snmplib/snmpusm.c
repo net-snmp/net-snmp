c89aabfee0fe6a0dca691c588f8ecd6d803965d7
Robert Story <rstory@freesnmp.com>
2018-02-16 10:49:10 -0500

    match exact string length in usm_lookup_alg_type

--- snmplib/snmpusm.c
+++ snmplib/snmpusm.c
@@ -3413,9 +3413,10 @@ init_usm(void)
 int
 usm_lookup_alg_type(const char *str, usm_alg_type_t *types)
 {
-    int i;
+    int i, l;
+    l = strlen(str);
     for (i = 0; types[i].label; ++i) {
-        if (0 == strcasecmp(types[i].label, str))
+        if (0 == strncasecmp(types[i].label, str, l))
             return types[i].value;
     }
 
