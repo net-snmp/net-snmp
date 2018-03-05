net-snmp/net-snmp

commit 0598b77e1c3ac730ac07d92f79c1b0245b79352e

CHANGES: snmplib: add more openssl error cases where we check for local cert
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V5-6-patches@20117 06827809-a52a-0410-b366-d66718629ded

snmplib/transports/snmpTLSBaseDomain.c
@@ -83,6 +83,9 @@ int verify_callback(int ok, X509_STORE_CTX *ctx) {
        locally known fingerprint and then accept it */
     if (!ok &&
         (X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT == err ||
+         X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY == err ||
+         X509_V_ERR_CERT_UNTRUSTED == err ||
+         X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE == err ||
          X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN == err)) {
 
         cert = netsnmp_cert_find(NS_CERT_REMOTE_PEER, NS_CERTKEY_FINGERPRINT,

commit 2c682f6cbce8cc857acdef4dc145ae3ad5f98304

snmplib: Refuse certificates for which a trusted chain was not found the trust store
This patch fixes the noTrustCACert STlsServer test.

Fixes: 0598b77e1c3a ("CHANGES: snmplib: add more openssl error cases where we check for local cert")

snmplib/transports/snmpTLSBaseDomain.c
@@ -90,7 +90,6 @@ int verify_callback(int ok, X509_STORE_CTX *ctx) {
        locally known fingerprint and then accept it */
     if (!ok &&
         (X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT == err ||
-         X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY == err ||
          X509_V_ERR_CERT_UNTRUSTED == err ||
          X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE == err ||
          X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN == err)) {
2c682f6cbce8cc857acdef4dc145ae3ad5f98304
Bart Van Assche <bvanassche@acm.org>
2018-03-04 19:58:08 -0800

    snmplib: Refuse certificates for which a trusted chain was not found the trust store
    This patch fixes the noTrustCACert STlsServer test.
    
    Fixes: 0598b77e1c3a ("CHANGES: snmplib: add more openssl error cases where we check for local cert")

--- snmplib/transports/snmpTLSBaseDomain.c
+++ snmplib/transports/snmpTLSBaseDomain.c
@@ -90,7 +90,6 @@ int verify_callback(int ok, X509_STORE_CTX *ctx) {
        locally known fingerprint and then accept it */
     if (!ok &&
         (X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT == err ||
-         X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY == err ||
          X509_V_ERR_CERT_UNTRUSTED == err ||
          X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE == err ||
          X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN == err)) {
