net-snmp/net-snmp

commit 673f81051d58b7045eb9a38201eeaba878eebf3d

snmplib, SNMPv3: Fix two memory leaks in netsnmp_parse_args()
These were detected by Valgrind.

snmplib/snmp_parse_args.c
@@ -731,6 +731,7 @@ netsnmp_parse_args(int argc,
                     "Error generating a key (Ku) from the supplied authentication pass phrase. \n");
             return (NETSNMP_PARSE_ARGS_ERROR);
         }
+        free(Apsz);
     }
     if (Xpsz) {
         session->securityPrivKeyLen = USM_PRIV_KU_LEN;
@@ -770,6 +771,7 @@ netsnmp_parse_args(int argc,
                     "Error generating a key (Ku) from the supplied privacy pass phrase. \n");
             return (NETSNMP_PARSE_ARGS_ERROR);
         }
+        free(Xpsz);
     }
 #endif /* NETSNMP_SECMOD_USM */
 

agent/mibgroup/ucd-snmp/pass_persist.c
@@ -368,6 +368,8 @@ setPassPersist(int action,
                 sprint_mib_oid(buf, name, name_len);
             netsnmp_internal_pass_set_format(buf2, var_val, var_val_type,
                                              var_val_len);
+            free(persistpassthru->command);
+            persistpassthru->command = NULL;
             if (asprintf(&persistpassthru->command, "set\n%s\n%s\n", buf,
                          buf2) < 0) {
             }

commit c5d9cecda251ea1f4638f88d5506b1f1743d87c3

snmplib: Avoid that freeing the snmpv3_parse_arg() result can trigger a crash
Fixes: 673f81051d58 ("snmplib, SNMPv3: Fix two memory leaks in netsnmp_parse_args()")

snmplib/snmp_parse_args.c
@@ -608,7 +608,7 @@ netsnmp_parse_args(int argc,
 		    return NETSNMP_PARSE_ARGS_ERROR;
 		}
 	    } else {
-		Apsz = optarg;
+		Apsz = strdup(optarg);
 	    }
             break;
 
@@ -623,7 +623,7 @@ netsnmp_parse_args(int argc,
 		    return NETSNMP_PARSE_ARGS_ERROR;
 		}
 	    } else {
-		Xpsz = optarg;
+		Xpsz = strdup(optarg);
 	    }
             break;
 #endif                          /* SNMPV3_CMD_OPTIONS */

snmplib/snmpv3.c
@@ -315,11 +315,11 @@ snmpv3_options(char *optarg, netsnmp_session * session, char **Apsz,
         break;
 
     case 'A':
-        *Apsz = optarg;
+        *Apsz = strdup(optarg);
         break;
 
     case 'X':
-        *Xpsz = optarg;
+        *Xpsz = strdup(optarg);
         break;
 #endif /* NETSNMP_SECMOD_USM */
 