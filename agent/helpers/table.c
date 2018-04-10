4d1d375df29aa52b8c01893d687dfdd55204eb04
Bill Fenner <fenner@gmail.com>
2018-04-10 20:31:38 +0000

    CHANGES: snmpd: BUG: 2810: from "Minzhuan Gong": fix compile with --enable-read-only

--- agent/helpers/table.c
+++ agent/helpers/table.c
@@ -491,8 +491,9 @@ table_helper_handler(netsnmp_mib_handler *handler,
                 if (reqinfo->mode == MODE_SET_RESERVE1)
                     table_helper_cleanup(reqinfo, request,
                                          SNMP_ERR_NOTWRITABLE);
-                else if (reqinfo->mode == MODE_GET)
+                else
 #endif /* NETSNMP_NO_WRITE_SUPPORT */
+                if (reqinfo->mode == MODE_GET)
                     table_helper_cleanup(reqinfo, request,
                                          SNMP_NOSUCHOBJECT);
                 else
