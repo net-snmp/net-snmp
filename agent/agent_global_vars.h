net-snmp/net-snmp

commit 81b65f4d23a976d2106bbfc2262694c1c90f457c

Move declarations of global functions and variables from .c to .h
This allows the compiler to verify consistency of variable and function
declarations for variables and functions that are used in more than one
file.

agent/agent_index.c
@@ -65,8 +65,6 @@ struct snmp_index {
     struct snmp_index *next_idx;
 }              *snmp_index_head = NULL;
 
-extern netsnmp_session *main_session;
-
 /*
  * The caller is responsible for free()ing the memory returned by
  * this function.  

agent/agent_registry.c
@@ -59,6 +59,7 @@
 #include <net-snmp/agent/null.h>
 #include <net-snmp/agent/table.h>
 #include <net-snmp/agent/table_iterator.h>
+#include <net-snmp/agent/agent_index.h>
 #include <net-snmp/agent/agent_registry.h>
 
 #ifdef USING_AGENTX_SUBAGENT_MODULE
@@ -1257,7 +1258,6 @@ netsnmp_register_mib(const char *moduleName,
      */
     if (netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID, 
 			       NETSNMP_DS_AGENT_ROLE) != MASTER_AGENT) {
-        extern struct snmp_session *main_session;
         if (main_session == NULL) {
             register_mib_detach_node(subtree);
 	}
@@ -2236,7 +2236,6 @@ shutdown_tree(void) {
 
 }
 
-extern void     dump_idx_registry(void);
 void
 dump_registry(void)
 {

agent/mibgroup/agent/nsTransactionTable.c
@@ -95,8 +95,6 @@ init_nsTransactionTable(void)
     each appropriately according to the data matching the first row
     and return the put_index_data variable at the end of the function.
 */
-extern netsnmp_agent_session *agent_delegated_list;
-
 netsnmp_variable_list *
 nsTransactionTable_get_first_data_point(void **my_loop_context,
                                         void **my_data_context,

agent/mibgroup/agentx/client.c
@@ -104,8 +104,6 @@ int
 agentx_open_session(netsnmp_session * ss)
 {
     netsnmp_pdu    *pdu, *response;
-    extern oid      version_sysoid[];
-    extern int      version_sysoid_len;
     u_long 	    timeout;
 
     DEBUGMSGTL(("agentx/subagent", "opening session \n"));

agent/mibgroup/agentx/master_admin.c
@@ -35,6 +35,8 @@
 
 #include "agentx/protocol.h"
 #include "agentx/client.h"
+#include "agentx/subagent.h"
+#include "agentx/master_admin.h"
 
 #include <net-snmp/agent/agent_index.h>
 #include <net-snmp/agent/agent_trap.h>
@@ -408,8 +410,6 @@ agentx_notify(netsnmp_session * session, netsnmp_pdu *pdu)
 {
     netsnmp_session       *sp;
     netsnmp_variable_list *var;
-    extern const oid       sysuptime_oid[], snmptrap_oid[];
-    extern const size_t    sysuptime_oid_len, snmptrap_oid_len;
 
     sp = find_agentx_session(session, pdu->sessid);
     if (sp == NULL)

agent/mibgroup/agentx/subagent.c
@@ -31,6 +31,7 @@
 #endif
 
 #include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/agent_index.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/library/snmp_assert.h>
 
@@ -89,8 +90,6 @@ struct agent_netsnmp_set_info {
 static struct agent_netsnmp_set_info *Sets = NULL;
 
 netsnmp_session *agentx_callback_sess = NULL;
-extern int      callback_master_num;
-extern netsnmp_session *main_session;   /* from snmp_agent.c */
 
 int
 subagent_startup(int majorID, int minorID,

agent/mibgroup/agentx/subagent.h
@@ -11,6 +11,15 @@ config_require(agentx/agentx_config)
 config_error(agentx/subagent depends on the Callback transport)
 #endif
 
+     extern int             callback_master_num;
+
+     extern const oid       snmptrap_oid[];
+     extern const oid       snmptrapenterprise_oid[];
+     extern const oid       sysuptime_oid[];
+     extern const size_t    snmptrap_oid_len;
+     extern const size_t    snmptrapenterprise_oid_len;
+     extern const size_t    sysuptime_oid_len;
+
      int             subagent_init(void);
      int             handle_agentx_packet(int, netsnmp_session *, int,
                                           netsnmp_pdu *, void *);

agent/mibgroup/disman/event/mteEvent.c
@@ -7,6 +7,7 @@
 #include <net-snmp/net-snmp-features.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "agentx/subagent.h"
 #include "disman/event/mteEvent.h"
 #include "disman/event/mteTrigger.h"
 #include "disman/event/mteObjects.h"
@@ -349,8 +350,6 @@ _mteEvent_fire_notify( struct mteEvent   *entry,     /* The event to fire  */
                        oid *suffix, size_t sfx_len ) /* Matching instance  */
 {
     netsnmp_variable_list *var, *v2;
-    extern const oid       snmptrap_oid[];
-    extern const size_t    snmptrap_oid_len;
     netsnmp_session       *s;
 
          /*

commit f42aeb52e63147b5789cccde1ef997370191849e

CHANGES: BUG: 2831: Move libnetsnmpagent global variable declarations into a separate header file
Because commit 81b65f4d23a9 added declarations for several global variables
to public header files, applications that declare global variables with the
same names no longer build. Hence move the global variables that were added
by that commit to public header files into a new header file.

Fixes: 81b65f4d23a9 ("Move declarations of global functions and variables from .c to .h")

agent/agent_global_vars.h
@@ -1,0 +1,36 @@
+#ifndef _AGENT_GLOBAL_VARS_H_
+#define _AGENT_GLOBAL_VARS_H_
+
+/*
+ * Global variables defined in libnetsnmpagent.so that are used in MIB
+ * implementations. To do: avoid that MIB implementations depend on these
+ * variables and/or make sure that all these variables have a netsnmp_ prefix.
+ */
+
+/* Forward declarations. */
+
+struct snmp_session;
+struct netsnmp_agent_session_s;
+struct netsnmp_agent_session_s;
+
+/* Global variable declarations. */
+
+extern int netsnmp_running;
+extern int callback_master_num;
+extern long snmp_enableauthentraps;
+extern int snmp_enableauthentrapsset;
+
+extern struct snmp_session *main_session;
+extern struct netsnmp_agent_session_s *netsnmp_processing_set;
+extern struct netsnmp_agent_session_s *agent_delegated_list;
+
+extern const oid    snmptrap_oid[];
+extern const size_t snmptrap_oid_len;
+extern const oid    snmptrapenterprise_oid[];
+extern const size_t snmptrapenterprise_oid_len;
+extern const oid    sysuptime_oid[];
+extern const size_t sysuptime_oid_len;
+extern const oid    version_sysoid[];
+extern const int    version_sysoid_len;
+
+#endif /* _AGENT_GLOBAL_VARS_H_ */

agent/agent_index.c
@@ -39,6 +39,7 @@
 #include <net-snmp/agent/agent_index.h>
 
 #include "snmpd.h"
+#include "agent_global_vars.h"
 #include "mibgroup/struct.h"
 #include <net-snmp/agent/table.h>
 #include <net-snmp/agent/table_iterator.h>

agent/agent_registry.c
@@ -54,6 +54,7 @@
 #include <net-snmp/agent/agent_callbacks.h>
 
 #include "snmpd.h"
+#include "agent_global_vars.h"
 #include "mibgroup/struct.h"
 #include <net-snmp/agent/old_api.h>
 #include <net-snmp/agent/null.h>

agent/agent_trap.c
@@ -62,6 +62,7 @@
 #include <net-snmp/agent/agent_trap.h>
 #include <net-snmp/agent/snmp_agent.h>
 #include <net-snmp/agent/agent_callbacks.h>
+#include "agent_global_vars.h"
 
 #include <net-snmp/agent/agent_module_config.h>
 #include <net-snmp/agent/mib_module_config.h>

agent/mibgroup/agent/nsTransactionTable.c
@@ -6,6 +6,7 @@
 #include <net-snmp/net-snmp-features.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "agent_global_vars.h"
 
 #include <net-snmp/agent/table.h>
 #include <net-snmp/agent/table_iterator.h>

agent/mibgroup/agentx/client.c
@@ -37,6 +37,7 @@
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/agent/agent_index.h>
+#include "agent_global_vars.h"
 
 #include "agentx/protocol.h"
 #include "agentx/client.h"

agent/mibgroup/agentx/master_admin.c
@@ -32,6 +32,7 @@
 
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "agent_global_vars.h"
 
 #include "agentx/protocol.h"
 #include "agentx/client.h"

agent/mibgroup/agentx/subagent.c
@@ -36,6 +36,7 @@
 #include <net-snmp/library/snmp_assert.h>
 
 #include "snmpd.h"
+#include "agent_global_vars.h"
 #include "agentx/protocol.h"
 #include "agentx/client.h"
 #include "agentx/agentx_config.h"

agent/mibgroup/agentx/subagent.h
@@ -11,15 +11,6 @@ config_require(agentx/agentx_config)
 config_error(agentx/subagent depends on the Callback transport)
 #endif
 
-     extern int             callback_master_num;
-
-     extern const oid       snmptrap_oid[];
-     extern const oid       snmptrapenterprise_oid[];
-     extern const oid       sysuptime_oid[];
-     extern const size_t    snmptrap_oid_len;
-     extern const size_t    snmptrapenterprise_oid_len;
-     extern const size_t    sysuptime_oid_len;
-
      int             subagent_init(void);
      int             handle_agentx_packet(int, netsnmp_session *, int,
                                           netsnmp_pdu *, void *);

agent/mibgroup/disman/event/mteEvent.c
@@ -7,6 +7,7 @@
 #include <net-snmp/net-snmp-features.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "agent_global_vars.h"
 #include "agentx/subagent.h"
 #include "disman/event/mteEvent.h"
 #include "disman/event/mteTrigger.h"

agent/mibgroup/disman/event/mteTrigger.c
@@ -7,6 +7,7 @@
 #include <net-snmp/net-snmp-features.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "agent_global_vars.h"
 #include "disman/event/mteTrigger.h"
 #include "disman/event/mteEvent.h"
 

agent/mibgroup/mibII/snmp_mib.c
@@ -18,6 +18,7 @@
 #include "snmp_mib.h"
 #include "system_mib.h"
 #include "updates.h"
+#include "agent_global_vars.h"
 
 #ifndef NETSNMP_NO_WRITE_SUPPORT
 netsnmp_feature_require(check_vb_truthvalue)

agent/mibgroup/mibII/snmp_mib_5_5.c
@@ -8,6 +8,7 @@
 
 #include "snmp_mib_5_5.h"
 #include "updates.h"
+#include "agent_global_vars.h"
 
 netsnmp_feature_require(helper_statistics)
 #ifndef NETSNMP_NO_WRITE_SUPPORT
@@ -17,9 +18,6 @@ netsnmp_feature_require(check_vb_truthvalue)
 #define SNMP_OID 1, 3, 6, 1, 2, 1, 11
 
 static oid snmp_oid[] = { SNMP_OID };
-
-extern long snmp_enableauthentraps;
-extern int snmp_enableauthentrapsset;
 
 static int
 snmp_enableauthentraps_store(int a, int b, void *c, void *d)

agent/mibgroup/mibII/system_mib.c
@@ -48,6 +48,7 @@
 #include "util_funcs.h"
 #include "system_mib.h"
 #include "updates.h"
+#include "agent_global_vars.h"
 
 netsnmp_feature_require(watcher_read_only_int_scalar)
 

agent/mibgroup/notification/snmpNotifyTable.c
@@ -26,6 +26,7 @@
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 
+#include "agent_global_vars.h"
 #include "header_complex.h"
 #include "snmpNotifyTable.h"
 #include "snmpNotifyFilterProfileTable.h"
@@ -109,8 +110,6 @@ _checkFilter(const char* paramName, netsnmp_pdu *pdu)
     size_t                 profileNameLen;
     struct vacm_viewEntry *vp, *head;
     int                    vb_oid_excluded = 0;
-    extern const oid       snmptrap_oid[];
-    extern const size_t    snmptrap_oid_len;
 
     netsnmp_assert(NULL != paramName);
     netsnmp_assert(NULL != pdu);

agent/mibgroup/utilities/iquery.c
@@ -3,6 +3,7 @@
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 
+#include "agent_global_vars.h"
 #include "agentx/subagent.h"
 #include "utilities/iquery.h"
 

agent/snmp_agent.c
@@ -82,6 +82,7 @@ SOFTWARE.
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/library/snmp_assert.h>
+#include "agent_global_vars.h"
 
 #if HAVE_SYSLOG_H
 #include <syslog.h>

agent/snmp_vars.c
@@ -137,6 +137,7 @@ PERFORMANCE OF THIS SOFTWARE.
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/agent/mib_modules.h>
 #include <net-snmp/agent/agent_sysORTable.h>
+#include "agent_global_vars.h"
 #include "kernel.h"
 
 #include "mibgroup/struct.h"

agent/snmpd.c
@@ -135,6 +135,7 @@ typedef long    fd_mask;
 
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "agent_global_vars.h"
 
 #include <net-snmp/library/fd_event_manager.h>
 #include <net-snmp/library/large_fd_set.h>

apps/agentxtrap.c
@@ -10,6 +10,7 @@
 
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/ds_agent.h>
+#include "../agent_global_vars.h"
 
 #include "../agent/mibgroup/agentx/agentx_config.h"
 #include "../agent/mibgroup/agentx/client.h"

apps/snmptrapd.c
@@ -98,6 +98,7 @@ SOFTWARE.
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/library/fd_event_manager.h>
 #include <net-snmp/agent/netsnmp_close_fds.h>
+#include "../agent_global_vars.h"
 #include "../agent/mibgroup/snmpv3/snmpEngine.h"
 #include "../agent/mibgroup/snmpv3/usmUser.h"
 #include "../agent/mibgroup/agent/nsVacmAccessTable.h"

include/net-snmp/agent/agent_trap.h
@@ -10,14 +10,6 @@ struct agent_add_trap_args {
     int             confirm;
 };
 
-extern long snmp_enableauthentraps;
-extern int snmp_enableauthentrapsset;
-
-extern const oid       snmptrap_oid[];
-extern const oid       sysuptime_oid[];
-extern const size_t    snmptrap_oid_len;
-extern const size_t    sysuptime_oid_len;
-
 void            init_traps(void);
 void            send_easy_trap(int, int);
 void            send_trap_pdu(netsnmp_pdu *);

include/net-snmp/agent/snmp_agent.h
@@ -32,8 +32,6 @@ extern          "C" {
 #define SNMP_MAX_PDU_SIZE 64000 /* local constraint on PDU size sent by agent
                                  * (see also SNMP_MAX_MSG_SIZE in snmp_api.h) */
 
-    extern int      netsnmp_running;
-
     /*
      * If non-zero, causes the addresses of peers to be logged when receptions
      * occur.  
@@ -46,9 +44,6 @@ extern          "C" {
      */
 
     extern int      lastAddrAge;
-
-    extern const oid version_sysoid[];
-    extern const int version_sysoid_len;
 
     /** @typedef struct netsnmp_request_info_s netsnmp_request_info
      * Typedefs the netsnmp_request_info_s struct into
@@ -211,11 +206,6 @@ extern          "C" {
         netsnmp_cachemap *cache_store;
         int             vbcount;
     } netsnmp_agent_session;
-
-    extern netsnmp_session *main_session;
-
-    extern netsnmp_agent_session *netsnmp_processing_set;
-    extern netsnmp_agent_session *agent_delegated_list;
 
     /*
      * Address cache handling functions.  

include/net-snmp/agent/snmp_vars.h
@@ -43,8 +43,6 @@ PERFORMANCE OF THIS SOFTWARE.
  * distributed with the Net-SNMP package.
  */
 
-    extern int callback_master_num;
-
     struct variable;
 
     /**