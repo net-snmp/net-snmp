82bd09e3d9b94b2e3cc69a4f458c7dcd0bae0845
Bart Van Assche <bvanassche@acm.org>
2018-05-05 16:39:04 -0700

    SNMP-USM-DH-OBJECTS-MIB: Fix a compiler warning
    Avoid that the following warning is reported when building against an
    older openssl library:
    
    snmp-usm-dh-objects-mib/usmDHParameters/usmDHParameters.c: In function 'init_usmDHParameters':
    snmp-usm-dh-objects-mib/usmDHParameters/usmDHParameters.c:44:9: warning: implicit declaration of function 'DH_set0_pqg' [-Wimplicit-function-declaration]
             DH_set0_pqg(dh_params, p, NULL, g);
             ^
    
    See also commit 3317a7c66362 ("Remove duplicated function in usmDHParameters.c that breaks compilation with LibreSSL 2.7.2 (OpenBSD 6.3)")

--- agent/mibgroup/snmp-usm-dh-objects-mib/usmDHParameters/usmDHParameters.c
+++ agent/mibgroup/snmp-usm-dh-objects-mib/usmDHParameters/usmDHParameters.c
@@ -5,6 +5,7 @@
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/library/snmp_openssl.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include "usmDHParameters.h"
 
