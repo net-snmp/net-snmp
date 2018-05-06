net-snmp/net-snmp

commit 4ae6ed444687f32b69c04d5b7d1bc868fd67d195

call fsync after writing persistent config

snmplib/read_config.c
@@ -1507,6 +1507,9 @@ read_config_store(const char *type, const char *line)
 #ifdef NETSNMP_PERSISTENT_MASK
     mode_t          oldmask;
 #endif
+#if HAVE_FSYNC
+    int fd;
+#endif
 
     if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
                                NETSNMP_DS_LIB_DONT_PERSIST_STATE)
@@ -1538,6 +1541,10 @@ read_config_store(const char *type, const char *line)
             fprintf(fout, "\n");
         DEBUGMSGTL(("read_config:store", "storing: %s\n", line));
         fclose(fout);
+#if HAVE_FSYNC
+        fd = fileno(fout);
+        fsync(fd);
+#endif
     } else {
         if (strcmp(NETSNMP_APPLICATION_CONFIG_TYPE, type) != 0) {
             /*

configure
@@ -26332,7 +26332,7 @@ fi
 
 
 #  Agent:
-for ac_func in cgetnext        chown          execv                           forkall         gai_strerror   getgrnam                        gethostname     getloadavg                                     getpid          getpwnam                                       hasmntopt       initgroups                                     if_nameindex    if_indextoname if_nametoindex                  localtime_r     mkstemp        mktime                          poll            regcomp                                        setgid          setgroups                                      setmntent       setuid                                         sigaction       sigblock      sighold                          sigprocmask                                                    statfs          statvfs       stime                            strncasecmp     system                                         uname           usleep
+for ac_func in cgetnext        chown          execv                           forkall         fsync                                          gai_strerror    getgrnam                                       gethostname     getloadavg                                     getpid          getpwnam                                       hasmntopt       initgroups                                     if_nameindex    if_indextoname if_nametoindex                  localtime_r     mkstemp        mktime                          poll            regcomp                                        setgid          setgroups                                      setmntent       setuid                                         sigaction       sigblock      sighold                          sigprocmask                                                    statfs          statvfs       stime                            strncasecmp     system                                         uname           usleep
 do :
   as_ac_var=`$as_echo "ac_cv_func_$ac_func" | $as_tr_sh`
 ac_fn_c_check_func "$LINENO" "$ac_func" "$as_ac_var"

configure.d/config_os_functions
@@ -100,7 +100,8 @@ AC_REPLACE_FUNCS([getopt        inet_ntop     inet_pton        ] dnl
 
 #  Agent:
 AC_CHECK_FUNCS([cgetnext        chown          execv           ] dnl
-               [forkall         gai_strerror   getgrnam        ] dnl
+               [forkall         fsync                          ] dnl
+               [gai_strerror    getgrnam                       ] dnl
                [gethostname     getloadavg                     ] dnl
                [getpid          getpwnam                       ] dnl
                [hasmntopt       initgroups                     ] dnl

include/net-snmp/net-snmp-config.h.in
@@ -210,6 +210,9 @@
 /* Define to 1 if you have the <fstab.h> header file. */
 #undef HAVE_FSTAB_H
 
+/* Define to 1 if you have the `fsync' function. */
+#undef HAVE_FSYNC
+
 /* Define to 1 if you have the `funlockfile' function. */
 #undef HAVE_FUNLOCKFILE
 

commit 3da9c1c74e1f1284f981d0a4d5a24406ccb98f87

libsnmp: Flush persistent config to persistent storage
From the fsync() man page: "returns EBADF if fd is not a valid open file
descriptor". Hence call fsync() before closing 'fout' instead of after
closing it. Ensure that fsync() gets called when building with MSVC on
Windows. See also Smith, Stewart, and A. B. MySQL. "Eat My Data: How
everybody gets file I/O wrong." OSCON, Portland, Oregon (2008)
(http://mirror.linux.org.au/pub/linux.conf.au/2007/video/wednesday/278.pdf).

Fixes: 4ae6ed444687 ("call fsync after writing persistent config")

snmplib/read_config.c
@@ -113,6 +113,9 @@
 #include <netdb.h>
 #endif
 #include <errno.h>
+#if HAVE_IO_H
+#include <io.h>
+#endif
 
 #if HAVE_DIRENT_H
 # include <dirent.h>
@@ -1507,9 +1510,6 @@ read_config_store(const char *type, const char *line)
 #ifdef NETSNMP_PERSISTENT_MASK
     mode_t          oldmask;
 #endif
-#if HAVE_FSYNC
-    int fd;
-#endif
 
     if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
                                NETSNMP_DS_LIB_DONT_PERSIST_STATE)
@@ -1540,11 +1540,22 @@ read_config_store(const char *type, const char *line)
         if (line[strlen(line)] != '\n')
             fprintf(fout, "\n");
         DEBUGMSGTL(("read_config:store", "storing: %s\n", line));
-        fclose(fout);
-#if HAVE_FSYNC
-        fd = fileno(fout);
-        fsync(fd);
+        fflush(fout);
+#if defined(HAVE_FSYNC)
+        fsync(fileno(fout));
+#elif defined(WIN32) && !defined(cygwin)
+        {
+            int fd;
+            void *h;
+
+            fd = _fileno(fout);
+            netsnmp_assert(fd != -1);
+            h = _get_osfhandle(fd);
+            netsnmp_assert(h != INVALID_HANDLE_VALUE);
+            FlushFileBuffers(h);
+        }
 #endif
+        fclose(fout);
     } else {
         if (strcmp(NETSNMP_APPLICATION_CONFIG_TYPE, type) != 0) {
             /*