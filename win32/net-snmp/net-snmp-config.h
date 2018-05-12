c049798e159eb695149793295aafb8a89a509b04
Bart Van Assche <bvanassche@acm.org>
2018-05-12 13:21:45 -0700

    MSVC: Use <stdint.h>

--- win32/net-snmp/net-snmp-config.h
+++ win32/net-snmp/net-snmp-config.h
@@ -691,9 +691,7 @@
 /* #undef HAVE_STATVFS */
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#ifdef __MINGW32__
 #define HAVE_STDINT_H 1
-#endif
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
