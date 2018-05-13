234982ba5d30551490228f1384b893ae7a8c9f3e
Bart Van Assche <bvanassche@acm.org>
2018-05-13 07:27:07 -0700

    libsnmp: Fix the recursive dependency between <net-snmp/types.h> and <net-snmp/library/types.h>
    Since recursive dependencies between header files cause multiple kinds
    of maintainability problems, fix the recursive dependency between
    <net-snmp/types.h> and <net-snmp/library/types.h> by moving the content
    of the latter into the former. Remove netsnmp_ref_size_t since it is
    not used inside the Net-SNMP code base. Rename struct netsnmp_ref_void
    into struct netsnmp_ref_void_s to make this structure name consistent
    with the rest of the code base. Regenerate dependencies.

--- include/net-snmp/types.h
+++ include/net-snmp/types.h
@@ -441,8 +441,61 @@ struct snmp_session {
     void           *myvoid;
 };
 
+typedef struct netsnmp_index_s {
+    size_t          len;
+    oid            *oids;
+} netsnmp_index;
 
-#include <net-snmp/library/types.h>
+typedef struct netsnmp_void_array_s {
+    size_t          size;
+    void          **array;
+} netsnmp_void_array;
+
+/*
+ * references to various types
+ */
+typedef struct netsnmp_ref_void_s {
+    void           *val;
+} netsnmp_ref_void;
+
+typedef union {
+    u_long          ul;
+    u_int           ui;
+    u_short         us;
+    u_char          uc;
+    long            sl;
+    int             si;
+    short           ss;
+    char            sc;
+    char           *cp;
+    void           *vp;
+} netsnmp_cvalue;
+
+/*
+ * Structure for holding a set of file descriptors, similar to fd_set.
+ *
+ * This structure however can hold so-called large file descriptors
+ * (>= FD_SETSIZE or 1024) on Unix systems or more than FD_SETSIZE (64)
+ * sockets on Windows systems.
+ *
+ * It is safe to allocate this structure on the stack.
+ *
+ * This structure must be initialized by calling netsnmp_large_fd_set_init()
+ * and must be cleaned up via netsnmp_large_fd_set_cleanup(). If this last
+ * function is not called this may result in a memory leak.
+ *
+ * The members of this structure are:
+ * lfs_setsize: maximum set size.
+ * lsf_setptr:  points to lfs_set if lfs_setsize <= FD_SETSIZE, and otherwise
+ *              to dynamically allocated memory.
+ * lfs_set:     file descriptor / socket set data if lfs_setsize <= FD_SETSIZE.
+ */
+typedef struct netsnmp_large_fd_set_s {
+    unsigned        lfs_setsize;
+    fd_set         *lfs_setptr;
+    fd_set          lfs_set;
+} netsnmp_large_fd_set;
+
 #include <net-snmp/definitions.h>
 #include <net-snmp/library/snmp_api.h>
 
