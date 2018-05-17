net-snmp/net-snmp

commit 5103db3c189d87af1bd7e39c1d9e7bacc89cbf5e

mibII/at: Split into multiple source files
Split the implementations of ARP_Scan_Init(), ARP_Scan_Next()
and var_atEntry() into one function per platform and move these
implementations to separate source files. Additionally, move the
ARPTAB_SYMBOL and ARPTAB_SIZE_SYMBOL definitions from the
include/net-snmp/system/*.h include files into the mibII/at
implementation. This change reduces the number of #ifdefs in the
mibII/at code drastically.

agent/mibgroup/mibII/data_access/at_solaris.c
@@ -1,0 +1,165 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/data_access/interface.h>
+#include "kernel_sunos5.h"
+
+typedef struct if_ip {
+    int             ifIdx;
+    IpAddress       ipAddr;
+} if_ip_t;
+
+static int
+AT_Cmp(void *addr, void *ep)
+{
+    mib2_ipNetToMediaEntry_t *mp = (mib2_ipNetToMediaEntry_t *) ep;
+    int             ret = -1;
+    oid             index;
+
+#ifdef NETSNMP_INCLUDE_IFTABLE_REWRITES
+    mp->ipNetToMediaIfIndex.o_bytes[mp->ipNetToMediaIfIndex.o_length] = '\0';
+    index = netsnmp_access_interface_index_find(
+                    mp->ipNetToMediaIfIndex.o_bytes);
+#else
+    index = Interface_Index_By_Name(mp->ipNetToMediaIfIndex.o_bytes,
+                                    mp->ipNetToMediaIfIndex.o_length);
+#endif
+    DEBUGMSGTL(("mibII/at", "......... AT_Cmp %lx<>%lx %d<>%d (%.5s)\n",
+                (unsigned long)mp->ipNetToMediaNetAddress,
+                (unsigned long)((if_ip_t *) addr)->ipAddr,
+                ((if_ip_t *) addr)->ifIdx, index,
+                mp->ipNetToMediaIfIndex.o_bytes));
+    if (mp->ipNetToMediaNetAddress != ((if_ip_t *) addr)->ipAddr)
+        ret = 1;
+    else if (((if_ip_t *) addr)->ifIdx != index)
+        ret = 1;
+    else
+        ret = 0;
+    DEBUGMSGTL(("mibII/at", "......... AT_Cmp returns %d\n", ret));
+    return ret;
+}
+
+u_char         *
+var_atEntry(struct variable * vp,
+            oid * name,
+            size_t * length,
+            int exact, size_t * var_len, WriteMethod ** write_method)
+{
+    /*
+     * object identifier is of form:
+     * 1.3.6.1.2.1.3.1.1.1.interface.1.A.B.C.D,  where A.B.C.D is IP address.
+     * Interface is at offset 10,
+     * IPADDR starts at offset 12.
+     */
+#define AT_MAX_NAME_LENGTH	16
+#define AT_IFINDEX_OFF	10
+    u_char         *cp;
+    oid            *op;
+    oid             lowest[AT_MAX_NAME_LENGTH];
+    oid             current[AT_MAX_NAME_LENGTH];
+    if_ip_t         NextAddr;
+    mib2_ipNetToMediaEntry_t entry;
+    static mib2_ipNetToMediaEntry_t Lowentry;
+    int             Found = 0;
+    req_e           req_type;
+    int             offset, olength = 0;
+    static in_addr_t      addr_ret;
+
+    /*
+     * fill in object part of name for current (less sizeof instance part)
+     */
+
+    DEBUGMSGTL(("mibII/at", "var_atEntry: "));
+    DEBUGMSGOID(("mibII/at", vp->name, vp->namelen));
+    DEBUGMSG(("mibII/at", " %d\n", exact));
+
+    memset(&Lowentry, 0, sizeof(Lowentry));
+    memcpy((char *) current, (char *) vp->name, vp->namelen * sizeof(oid));
+    lowest[0] = 1024;
+    for (NextAddr.ipAddr = (u_long) - 1, NextAddr.ifIdx = 255, req_type =
+         GET_FIRST;;
+         NextAddr.ipAddr = entry.ipNetToMediaNetAddress, NextAddr.ifIdx =
+         current[AT_IFINDEX_OFF], req_type = GET_NEXT) {
+        if (getMibstat
+            (MIB_IP_NET, &entry, sizeof(mib2_ipNetToMediaEntry_t),
+             req_type, &AT_Cmp, &NextAddr) != 0)
+            break;
+#ifdef NETSNMP_INCLUDE_IFTABLE_REWRITES
+        entry.ipNetToMediaIfIndex.o_bytes[entry.ipNetToMediaIfIndex.o_length] = '\0';
+        current[AT_IFINDEX_OFF] = netsnmp_access_interface_index_find(
+           entry.ipNetToMediaIfIndex.o_bytes);
+#else
+        current[AT_IFINDEX_OFF] =
+           Interface_Index_By_Name(entry.ipNetToMediaIfIndex.o_bytes,
+                                   entry.ipNetToMediaIfIndex.o_length);
+#endif
+        if (current[6] == 3) {  /* AT group oid */
+            current[AT_IFINDEX_OFF + 1] = 1;
+            offset = AT_IFINDEX_OFF + 2;
+            olength = AT_IFINDEX_OFF + 6;
+        } else {
+            offset = AT_IFINDEX_OFF + 1;
+            olength = AT_IFINDEX_OFF + 5;
+        }
+        COPY_IPADDR(cp, (u_char *) & entry.ipNetToMediaNetAddress, op,
+                    current + offset);
+        if (exact) {
+            if (snmp_oid_compare(current, olength, name, *length) == 0) {
+                memcpy((char *) lowest, (char *) current,
+                       olength * sizeof(oid));
+                Lowentry = entry;
+                Found++;
+                break;          /* no need to search further */
+            }
+        } else {
+            if (snmp_oid_compare(current, olength, name, *length) > 0 &&
+                (Found == 0 ||
+                 snmp_oid_compare(current, olength, lowest, olength) < 0)) {
+                /*
+                 * if new one is greater than input and closer to input than
+                 * previous lowest, and is not equal to it, save this one as the "next" one.
+                 */
+                memcpy((char *) lowest, (char *) current,
+                       olength * sizeof(oid));
+                Lowentry = entry;
+                Found++;
+            }
+        }
+    }
+    DEBUGMSGTL(("mibII/at", "... Found = %d\n", Found));
+    if (Found == 0)
+        return (NULL);
+    memcpy((char *) name, (char *) lowest, olength * sizeof(oid));
+    *length = olength;
+    *write_method = 0;
+    switch (vp->magic) {
+    case IPMEDIAIFINDEX:
+        *var_len = sizeof long_return;
+#ifdef NETSNMP_INCLUDE_IFTABLE_REWRITES
+        Lowentry.ipNetToMediaIfIndex.o_bytes[
+            Lowentry.ipNetToMediaIfIndex.o_length] = '\0';
+        long_return = netsnmp_access_interface_index_find(
+            Lowentry.ipNetToMediaIfIndex.o_bytes);
+#else
+        long_return = Interface_Index_By_Name(
+            Lowentry.ipNetToMediaIfIndex.o_bytes,
+            Lowentry.ipNetToMediaIfIndex.o_length);
+#endif
+        return (u_char *) & long_return;
+    case IPMEDIAPHYSADDRESS:
+        *var_len = Lowentry.ipNetToMediaPhysAddress.o_length;
+        return Lowentry.ipNetToMediaPhysAddress.o_bytes;
+    case IPMEDIANETADDRESS:
+        *var_len = sizeof(addr_ret);
+        addr_ret = Lowentry.ipNetToMediaNetAddress;
+        return (u_char *) &addr_ret;
+    case IPMEDIATYPE:
+        *var_len = sizeof long_return;
+        long_return = Lowentry.ipNetToMediaType;
+        return (u_char *) & long_return;
+    default:
+        DEBUGMSGTL(("snmpd", "unknown sub-id %d in var_atEntry\n",
+                    vp->magic));
+    }
+    return NULL;
+}

agent/mibgroup/mibII/at.h
@@ -6,6 +6,10 @@
 #ifndef _MIBGROUP_AT_H
 #define _MIBGROUP_AT_H
 
+#if HAVE_NET_ROUTE_H
+#include <net/route.h>
+#endif
+
 config_arch_require(solaris2, kernel_sunos5)
 
      extern void     init_at(void);
@@ -21,18 +25,8 @@ config_arch_require(solaris2, kernel_sunos5)
 #define IPMEDIANETADDRESS       2
 #define IPMEDIATYPE             3
 
-/*
- * in case its missing: 
- */
-#ifndef ATF_PERM
-# define ATF_PERM	0x04
-#endif                          /*  ATF_PERM */
-#ifndef ATF_COM
-# define ATF_COM	0x02
-#endif                          /*  ATF_COM */
-
 /* InfiniBand uses HW addr > 6 */
-#define MAX_MAC_ADDR_LEN 32 
+#define MAX_MAC_ADDR_LEN 32
 
 #if defined(linux) || defined(irix6)
 /*
@@ -46,4 +40,25 @@ config_arch_require(solaris2, kernel_sunos5)
          int             if_index;
      };
 #endif
+
+#if !defined(WIN32) && !defined(cygwin) && !defined(solaris2)
+void     ARP_Scan_Init(void);
+int      ARP_Scan_Next(in_addr_t *, char *, int *, u_long *, u_short *);
+#endif
+
+#if defined(WIN32) || defined(cygwin)
+config_require(mibII/data_access/at_iphlpapi)
+#elif defined(solaris2)
+config_require(mibII/data_access/at_solaris)
+#elif defined(linux)
+config_require(mibII/data_access/at_linux)
+config_require(mibII/data_access/at_unix)
+#elif defined(HAVE_SYS_SYSCTL_H) && defined(RTF_LLINFO)
+config_require(mibII/data_access/at_sysctl)
+config_require(mibII/data_access/at_unix)
+#elif defined(HAVE_NLIST_H)
+config_require(mibII/data_access/at_nlist)
+config_require(mibII/data_access/at_unix)
+#endif
+
 #endif                          /* _MIBGROUP_AT_H */

agent/mibgroup/mibII/data_access/at_hpux.c
@@ -1,0 +1,149 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/data_access/interface.h>
+#include "../at.h"
+#if HAVE_STRING_H
+#include <string.h>
+#else
+#include <strings.h>
+#endif
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#if defined(NETSNMP_IFNET_NEEDS_KERNEL) && !defined(_KERNEL)
+#define _KERNEL 1
+#define _I_DEFINED_KERNEL
+#endif
+#include <sys/types.h>
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+#if HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+
+#if HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+#if HAVE_NET_IF_H
+#include <net/if.h>
+#endif
+#if HAVE_NET_IF_VAR_H
+#include <net/if_var.h>
+#endif
+#ifdef _I_DEFINED_KERNEL
+#undef _KERNEL
+#endif
+
+#if HAVE_NETINET_IF_ETHER_H
+#include <netinet/if_ether.h>
+#endif
+#if HAVE_INET_MIB2_H
+#include <inet/mib2.h>
+#endif
+#if HAVE_SYS_PARAM_H
+#include <sys/param.h>
+#endif
+#if HAVE_SYS_SYSCTL_H
+#include <sys/sysctl.h>
+#endif
+#if HAVE_NET_IF_DL_H
+#ifndef dynix
+#include <net/if_dl.h>
+#else
+#include <sys/net/if_dl.h>
+#endif
+#endif
+#if HAVE_SYS_STREAM_H
+#include <sys/stream.h>
+#endif
+#if HAVE_NET_ROUTE_H
+#include <net/route.h>
+#endif
+#ifdef solaris2
+#include "kernel_sunos5.h"
+#endif
+
+#ifdef hpux11
+#include <sys/mib.h>
+#include <netinet/mib_kern.h>
+#endif                          /* hpux11 */
+
+static int arptab_size, arptab_current;
+static mib_ipNetToMediaEnt *at;
+
+void
+ARP_Scan_Init(void)
+{
+    int             fd;
+    struct nmparms  p;
+    int             val;
+    unsigned int    ulen;
+    int             ret;
+
+    if (at)
+        free(at);
+    at = (mib_ipNetToMediaEnt *) 0;
+    arptab_size = 0;
+
+    if ((fd = open_mib("/dev/ip", O_RDONLY, 0, NM_ASYNC_OFF)) >= 0) {
+        p.objid = ID_ipNetToMediaTableNum;
+        p.buffer = (void *) &val;
+        ulen = sizeof(int);
+        p.len = &ulen;
+        if ((ret = get_mib_info(fd, &p)) == 0)
+            arptab_size = val;
+
+        if (arptab_size > 0) {
+            ulen = (unsigned) arptab_size *sizeof(mib_ipNetToMediaEnt);
+            at = (mib_ipNetToMediaEnt *) malloc(ulen);
+            memset(at, 0, ulen);
+            p.objid = ID_ipNetToMediaTable;
+            p.buffer = (void *) at;
+            p.len = &ulen;
+            if ((ret = get_mib_info(fd, &p)) < 0)
+                arptab_size = 0;
+            else
+                arptab_size = *p.len / sizeof(mib_ipNetToMediaEnt);
+        }
+
+        close_mib(fd);
+    }
+
+    arptab_current = 0;
+}
+
+int
+ARP_Scan_Next(in_addr_t * IPAddr, char *PhysAddr, int *PhysAddrLen,
+              u_long * ifType, u_short * ifIndex)
+{
+    *ifIndex = 0;
+
+    if (arptab_current < arptab_size) {
+        /*
+         * copy values
+         */
+        *IPAddr = at[arptab_current].NetAddr;
+        memcpy(PhysAddr, at[arptab_current].PhysAddr.o_bytes,
+               at[arptab_current].PhysAddr.o_length);
+        *ifType = at[arptab_current].Type;
+        *ifIndex = at[arptab_current].IfIndex;
+        *PhysAddrLen = at[arptab_current].PhysAddr.o_length;
+        /*
+         * increment to point next entry
+         */
+        arptab_current++;
+        /*
+         * return success
+         */
+        return (1);
+    }
+}

agent/mibgroup/mibII/data_access/at_iphlpapi.c
@@ -1,0 +1,429 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/data_access/interface.h>
+#if defined(cygwin)
+#include <windows.h>
+#endif
+#if defined(cygwin) || defined(mingw32)
+#include <winerror.h>
+#endif
+#include <iphlpapi.h>
+
+static WriteMethod write_arp;
+MIB_IPNETROW   *arp_row = NULL;
+int             create_flag = 0;
+
+u_char         *
+var_atEntry(struct variable *vp,
+            oid * name,
+            size_t * length,
+            int exact, size_t * var_len, WriteMethod ** write_method)
+{
+    /*
+     * Address Translation table object identifier is of form:
+     * 1.3.6.1.2.1.3.1.?.interface.1.A.B.C.D,  where A.B.C.D is IP address.
+     * Interface is at offset 10,
+     * IPADDR starts at offset 12.
+     *
+     * IP Net to Media table object identifier is of form:
+     * 1.3.6.1.2.1.4.22.1.?.interface.A.B.C.D,  where A.B.C.D is IP address.
+     * Interface is at offset 10,
+     * IPADDR starts at offset 11.
+     */
+    u_char         *cp;
+    oid            *op;
+    oid             lowest[16];
+    oid             current[16];
+    int             oid_length;
+    int             lowState = -1;      /* Don't have one yet */
+    PMIB_IPNETTABLE pIpNetTable = NULL;
+    DWORD           status = NO_ERROR;
+    DWORD           dwActualSize = 0;
+    UINT            i;
+    int             j;
+    u_char          dest_addr[4];
+    void           *result = NULL;
+    static in_addr_t	addr_ret;
+
+    /*
+     * fill in object part of name for current (less sizeof instance part)
+     */
+    memcpy((char *) current, (char *) vp->name,
+           (int) vp->namelen * sizeof(oid));
+
+    if (current[6] == 3) {      /* AT group oid */
+        oid_length = 16;
+    } else {                    /* IP NetToMedia group oid */
+        oid_length = 15;
+    }
+
+    status = GetIpNetTable(pIpNetTable, &dwActualSize, TRUE);
+    if (status == ERROR_INSUFFICIENT_BUFFER) {
+        pIpNetTable = malloc(dwActualSize);
+        if (pIpNetTable)
+            status = GetIpNetTable(pIpNetTable, &dwActualSize, TRUE);
+    }
+
+    i = -1;
+
+    if (status == NO_ERROR) {
+        for (i = 0; i < pIpNetTable->dwNumEntries; ++i) {
+            current[10] = pIpNetTable->table[i].dwIndex;
+
+
+            if (current[6] == 3) {      /* AT group oid */
+                current[11] = 1;
+                op = current + 12;
+            } else {            /* IP NetToMedia group oid */
+                op = current + 11;
+            }
+            cp = (u_char *) & pIpNetTable->table[i].dwAddr;
+            *op++ = *cp++;
+            *op++ = *cp++;
+            *op++ = *cp++;
+            *op++ = *cp++;
+
+            if (exact) {
+                if (snmp_oid_compare(current, oid_length, name, *length) ==
+                    0) {
+                    memcpy((char *) lowest, (char *) current,
+                           oid_length * sizeof(oid));
+                    lowState = 0;
+                    break;      /* no need to search further */
+                }
+            } else {
+                if (snmp_oid_compare(current, oid_length, name, *length) >
+                    0) {
+                    memcpy((char *) lowest, (char *) current,
+                           oid_length * sizeof(oid));
+                    lowState = 0;
+                    break;      /* As the table is sorted, no need to search further */
+                }
+            }
+        }
+    }
+    if (arp_row == NULL) {
+        /*
+         * Free allocated memory in case of SET request's FREE phase
+         */
+        arp_row = (PMIB_IPNETROW) malloc(sizeof(MIB_IPNETROW));
+    }
+
+    if (lowState < 0 || status != NO_ERROR) {
+        /*
+         * for creation of new row, only ipNetToMediaTable case is considered
+         */
+        if (*length == 15 || *length == 16) {
+            create_flag = 1;
+            *write_method = write_arp;
+            arp_row->dwIndex = name[10];
+
+            if (*length == 15) {        /* ipNetToMediaTable */
+                j = 11;
+            } else {            /* at Table */
+
+                j = 12;
+            }
+
+            dest_addr[0] = (u_char) name[j];
+            dest_addr[1] = (u_char) name[j + 1];
+            dest_addr[2] = (u_char) name[j + 2];
+            dest_addr[3] = (u_char) name[j + 3];
+            arp_row->dwAddr = *((DWORD *) dest_addr);
+
+            arp_row->dwType = 4;        /* Static */
+            arp_row->dwPhysAddrLen = 0;
+        }
+        goto out;
+    }
+
+    create_flag = 0;
+    memcpy((char *) name, (char *) lowest, oid_length * sizeof(oid));
+    *length = oid_length;
+    *write_method = write_arp;
+    netsnmp_assert(0 <= i && i < pIpNetTable->dwNumEntries);
+    *arp_row = pIpNetTable->table[i];
+
+    switch (vp->magic) {
+    case IPMEDIAIFINDEX:       /* also ATIFINDEX */
+        *var_len = sizeof long_return;
+        long_return = pIpNetTable->table[i].dwIndex;
+        result = &long_return;
+        break;
+    case IPMEDIAPHYSADDRESS:   /* also ATPHYSADDRESS */
+        *var_len = pIpNetTable->table[i].dwPhysAddrLen;
+        memcpy(return_buf, pIpNetTable->table[i].bPhysAddr, *var_len);
+        result = return_buf;
+        break;
+    case IPMEDIANETADDRESS:    /* also ATNETADDRESS */
+        *var_len = sizeof(addr_ret);
+        addr_ret = pIpNetTable->table[i].dwAddr;
+        result = &addr_ret;
+        break;
+    case IPMEDIATYPE:
+        *var_len = sizeof long_return;
+        long_return = pIpNetTable->table[i].dwType;
+        result = &long_return;
+        break;
+    default:
+        DEBUGMSGTL(("snmpd", "unknown sub-id %d in var_atEntry\n",
+                    vp->magic));
+        break;
+    }
+out:
+    free(pIpNetTable);
+    return result;
+}
+
+int
+write_arp(int action,
+          u_char * var_val,
+          u_char var_val_type,
+          size_t var_val_len, u_char * statP, oid * name, size_t length)
+{
+    int             var, retval = SNMP_ERR_NOERROR;
+    static PMIB_IPNETROW oldarp_row = NULL;
+    MIB_IPNETROW    temp_row;
+    DWORD           status = NO_ERROR;
+
+    /*
+     * IP Net to Media table object identifier is of form:
+     * 1.3.6.1.2.1.4.22.1.?.interface.A.B.C.D,  where A.B.C.D is IP address.
+     * Interface is at offset 10,
+     * IPADDR starts at offset 11.
+     */
+
+    if (name[6] == 3) {         /* AT group oid */
+        if (length != 16) {
+            snmp_log(LOG_ERR, "length error\n");
+            return SNMP_ERR_NOCREATION;
+        }
+    } else {                    /* IP NetToMedia group oid */
+        if (length != 15) {
+            snmp_log(LOG_ERR, "length error\n");
+            return SNMP_ERR_NOCREATION;
+        }
+    }
+
+
+    /*
+     * #define for ipNetToMediaTable entries are 1 less than corresponding sub-id in MIB
+     * * i.e. IPMEDIAIFINDEX defined as 0, but ipNetToMediaIfIndex registered as 1
+     */
+    var = name[9] - 1;
+    switch (action) {
+    case RESERVE1:
+        switch (var) {
+        case IPMEDIAIFINDEX:
+            if (var_val_type != ASN_INTEGER) {
+                snmp_log(LOG_ERR, "not integer\n");
+                return SNMP_ERR_WRONGTYPE;
+            }
+            if ((*((int *) var_val)) < 0) {
+                snmp_log(LOG_ERR, "invalid media ifIndex");
+                return SNMP_ERR_WRONGVALUE;
+            }
+            if (var_val_len > sizeof(int)) {
+                snmp_log(LOG_ERR, "bad length\n");
+                return SNMP_ERR_WRONGLENGTH;
+            }
+            break;
+        case IPMEDIANETADDRESS:
+            if (var_val_type != ASN_IPADDRESS) {
+                snmp_log(LOG_ERR, "not IP Address\n");
+                return SNMP_ERR_WRONGTYPE;
+            }
+            if ((*((int *) var_val)) < 0) {
+                snmp_log(LOG_ERR, "invalid media net address");
+                return SNMP_ERR_WRONGVALUE;
+            }
+            if (var_val_len > sizeof(DWORD)) {
+                snmp_log(LOG_ERR, "bad length\n");
+                return SNMP_ERR_WRONGLENGTH;
+            }
+            break;
+        case IPMEDIATYPE:
+            if (var_val_type != ASN_INTEGER) {
+                snmp_log(LOG_ERR, "not integer\n");
+                return SNMP_ERR_WRONGTYPE;
+            }
+            if ((*((int *) var_val)) < 1 || (*((int *) var_val)) > 4) {
+                snmp_log(LOG_ERR, "invalid media type");
+                return SNMP_ERR_WRONGVALUE;
+            }
+            if (var_val_len > sizeof(int)) {
+                snmp_log(LOG_ERR, "bad length\n");
+                return SNMP_ERR_WRONGLENGTH;
+            }
+            break;
+        case IPMEDIAPHYSADDRESS:
+            if (var_val_type != ASN_OCTET_STR) {
+                snmp_log(LOG_ERR, "not octet str");
+                return SNMP_ERR_WRONGTYPE;
+            }
+            if (var_val_len != 6) {
+                snmp_log(LOG_ERR, "not correct ipAddress length: %d",
+                         var_val_len);
+                return SNMP_ERR_WRONGLENGTH;
+            }
+            break;
+        default:
+            DEBUGMSGTL(("snmpd", "unknown sub-id %d in write_rte\n",
+                        var + 1));
+            return SNMP_ERR_NOTWRITABLE;
+        }
+        break;
+    case RESERVE2:
+        /*
+         * Save the old value, in case of UNDO
+         */
+        if (oldarp_row == NULL) {
+            oldarp_row = (PMIB_IPNETROW) malloc(sizeof(MIB_IPNETROW));
+            *oldarp_row = *arp_row;
+        }
+        break;
+    case ACTION:               /* Perform the SET action (if reversible) */
+        switch (var) {
+
+        case IPMEDIAIFINDEX:
+            temp_row = *arp_row;
+            arp_row->dwIndex = *((int *) var_val);
+            /*
+             * In case of new entry, physical address is mandatory.
+             * * SetIpNetEntry will be done in COMMIT case
+             */
+            if (!create_flag) {
+                if (SetIpNetEntry(arp_row) != NO_ERROR) {
+                    arp_row->dwIndex = temp_row.dwIndex;
+                    retval = SNMP_ERR_COMMITFAILED;
+                }
+                /*
+                 * Don't know yet, whether change in ifIndex creates new row or not
+                 */
+                /*
+                 * else{
+                 */
+                /*
+                 * temp_row.dwType = 2;
+                 */
+                /*
+                 * if(SetIpNetEntry(&temp_row) != NO_ERROR)
+                 */
+                /*
+                 * retval = SNMP_ERR_COMMITFAILED;
+                 */
+                /*
+                 * }
+                 */
+            }
+            break;
+        case IPMEDIANETADDRESS:
+            temp_row = *arp_row;
+            arp_row->dwAddr = *((int *) var_val);
+            if (!create_flag) {
+                if (SetIpNetEntry(arp_row) != NO_ERROR) {
+                    arp_row->dwAddr = oldarp_row->dwAddr;
+                    retval = SNMP_ERR_COMMITFAILED;
+                } else {
+                    temp_row.dwType = 2;
+                    if (SetIpNetEntry(&temp_row) != NO_ERROR) {
+                        snmp_log(LOG_ERR,
+                                 "Failed in ACTION, while deleting old row \n");
+                        retval = SNMP_ERR_COMMITFAILED;
+                    }
+                }
+            }
+            break;
+        case IPMEDIATYPE:
+            arp_row->dwType = *((int *) var_val);
+            if (!create_flag) {
+                if (SetIpNetEntry(arp_row) != NO_ERROR)
+                    retval = SNMP_ERR_COMMITFAILED;
+            }
+            break;
+        case IPMEDIAPHYSADDRESS:
+            memcpy(arp_row->bPhysAddr, var_val, var_val_len);
+            arp_row->dwPhysAddrLen = var_val_len;
+            if (!create_flag) {
+                if (SetIpNetEntry(arp_row) != NO_ERROR)
+                    retval = SNMP_ERR_COMMITFAILED;
+            }
+            break;
+        default:
+            DEBUGMSGTL(("snmpd", "unknown sub-id %d in write_arp\n",
+                        var + 1));
+            retval = SNMP_ERR_NOTWRITABLE;
+        }
+        return retval;
+    case UNDO:
+        /*
+         * Reverse the SET action and free resources
+         */
+        if (oldarp_row != NULL) {
+            /*
+             * UNDO the changes done for existing entry.
+             */
+            if (!create_flag) {
+                if ((status = SetIpNetEntry(oldarp_row)) != NO_ERROR) {
+                    snmp_log(LOG_ERR, "Error in case UNDO, status : %lu\n",
+                             status);
+                    retval = SNMP_ERR_UNDOFAILED;
+                }
+            }
+
+            if (oldarp_row->dwAddr != arp_row->dwAddr) {
+                arp_row->dwType = 2;    /*If row was added/created delete that row */
+
+                if ((status = SetIpNetEntry(arp_row)) != NO_ERROR) {
+                    snmp_log(LOG_ERR,
+                             "Error while deleting added row, status : %lu\n",
+                             status);
+                    retval = SNMP_ERR_UNDOFAILED;
+                }
+            }
+            free(oldarp_row);
+            oldarp_row = NULL;
+            free(arp_row);
+            arp_row = NULL;
+            return retval;
+        }
+        break;
+    case COMMIT:
+        /*
+         * if new entry and physical address specified, create new entry
+         */
+        if (create_flag) {
+            if (arp_row->dwPhysAddrLen != 0) {
+                if ((status = CreateIpNetEntry(arp_row)) != NO_ERROR) {
+                    snmp_log(LOG_ERR,
+                             "Inside COMMIT: CreateIpNetEntry failed, status %lu\n",
+                             status);
+                    retval = SNMP_ERR_COMMITFAILED;
+                }
+            } else {
+                /*
+                 * For new entry, physical address must be set.
+                 */
+                snmp_log(LOG_ERR,
+                         "Can't create new entry without physical address\n");
+                retval = SNMP_ERR_WRONGVALUE;
+            }
+            /*
+             * unset the create_flag, so that CreateIpNetEntry called only once
+             */
+            create_flag = 0;
+        }
+
+    case FREE:
+        /*
+         * Free any resources allocated
+         */
+        free(oldarp_row);
+        oldarp_row = NULL;
+        free(arp_row);
+        arp_row = NULL;
+        break;
+    }
+    return retval;
+}

commit e14027bce1615c57770a1eba18423b5a4b97eada

MIB-II, at: Fix Solaris build
Fixes: 5103db3c189d ("mibII/at: Split into multiple source files")

agent/mibgroup/mibII/data_access/at_solaris.c
@@ -19,6 +19,7 @@
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/data_access/interface.h>
 #include "kernel_sunos5.h"
+#include "../at.h"
 
 typedef struct if_ip {
     int             ifIdx;