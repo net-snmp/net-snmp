net-snmp/net-snmp

commit 7f05daa8e0e0f533b0f12a3e1cf07641519909d6

CHANGES: BUG: 3444939: BUG: 1796886: snmplib: Avoid that sprint_realloc_octet_string() embeds unprintable control characters or binary zeroes in its output. This behavior could cause truncated output in snmptrapd.

snmplib/mib.c
@@ -437,7 +437,7 @@ sprint_realloc_octet_string(u_char ** buf, size_t * buf_len,
     const char     *saved_hint = hint;
     int             hex = 0, x = 0;
     u_char         *cp;
-    int             output_format, len_needed;
+    int             output_format, cnt;
 
     if ((var->type != ASN_OCTET_STR) && 
         (!netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_QUICKE_PRINT))) {
@@ -531,18 +531,11 @@ sprint_realloc_octet_string(u_char ** buf, size_t * buf_len,
                     break;
                 case 't': /* new in rfc 3411 */
                 case 'a':
-                    /* A string hint gives the max size - we may not need this much */
-                    len_needed = SNMP_MIN( width, ecp-cp );
-                    while ((*out_len + len_needed + 1) >= *buf_len) {
-                        if (!(allow_realloc && snmp_realloc(buf, buf_len))) {
-                            return 0;
-                        }
-                    }
-                    for (x = 0; x < width && cp < ecp; x++) {
-                        *(*buf + *out_len) = *cp++;
-                        (*out_len)++;
-                    }
-                    *(*buf + *out_len) = '\0';
+                    cnt = SNMP_MIN(width, ecp - cp);
+                    if (!sprint_realloc_asciistring(buf, buf_len, out_len,
+                                                    allow_realloc, cp, cnt))
+                        return 0;
+                    cp += cnt;
                     break;
                 default:
                     *out_len = saved_out_len;

commit c56db2a5880fd0e9bde8eed98c2722a353d39eaa

snmplib: Avoid that sprint_realloc_octet_string() triggers a segmentation fault
strlcpy() implementations typically scan for the end of the source argument
passed to strlcpy(). Hence avoid passing an unterminated string to strlcpy().

Reported-by: Stuart Henderson <stu@spacehopper.org>
Fixes: 7f05daa8e0e0 ("CHANGES: BUG: 3444939: BUG: 1796886: snmplib: Avoid that sprint_realloc_octet_string() embeds unprintable control characters or binary zeroes in its output. This behavior could cause truncated output in snmptrapd.")

snmplib/mib.c
@@ -585,9 +585,10 @@ sprint_realloc_octet_string(u_char ** buf, size_t * buf_len,
                             return 0;
                     }
                     if (memchr(cp, '\0', cnt) == NULL) {
-                        /* No embedded '\0' - use strlcpy() to preserve UTF-8 */
-                        strlcpy((char *)(*buf + *out_len), (char *)cp, cnt + 1);
+                        /* No embedded '\0' - use memcpy() to preserve UTF-8 */
+                        memcpy(*buf + *out_len, cp, cnt);
                         *out_len += cnt;
+                        *(*buf + *out_len) = '\0';
                     } else if (!sprint_realloc_asciistring(buf, buf_len,
                                      out_len, allow_realloc, cp, cnt)) {
                         return 0;