net-snmp/net-snmp

commit 3bd8dc8b2d90bba775ff1a1fa2f4197fb20b180b

snmplib/transports: Avoid that the create_from_ostring methods truncate IPv6 addresses
This patch not only simplifies the code but also fixes a bug in the
DTLS-UDP and UDP-shared transports for IPv6, namely that IPv6 address
truncation no longer occurs. The truncation was caused by the following
code in the create_ostring implementations of these transports:

        memcpy(&addr6.sin6_addr.s6_addr, o, 4);

include/net-snmp/library/snmp_transport.h
@@ -118,12 +118,12 @@ typedef struct netsnmp_transport_s {
 
     /*  Local transport address (in relevant SNMP-style encoding).  */
     
-    unsigned char  *local;
+    void           *local;
     int             local_length;   /*  In octets.  */
 
     /*  Remote transport address (in relevant SNMP-style encoding).  */
 
-    unsigned char  *remote;
+    void           *remote;
     int             remote_length;  /*  In octets.  */
 
     /*  The actual socket.  */
@@ -209,7 +209,7 @@ typedef struct netsnmp_tdomain_s {
      */
     netsnmp_transport *(*f_create_from_tstring) (const char *, int);
 
-    netsnmp_transport *(*f_create_from_ostring) (const u_char *, size_t, int);
+    netsnmp_transport *(*f_create_from_ostring) (const void *, size_t, int);
 
     struct netsnmp_tdomain_s *next;
 

snmplib/transports/snmpAAL5PVCDomain.c
@@ -230,20 +230,12 @@ netsnmp_aal5pvc_transport(const struct sockaddr_atmpvc *addr, int local)
 
     if (local) {
 #ifndef NETSNMP_NO_LISTEN_SUPPORT
-        t->local = (unsigned char*)malloc(8);
+        t->local_length = sizeof(*addr);
+        t->local = netsnmp_memdup(addr, sizeof(*addr));
         if (t->local == NULL) {
             netsnmp_transport_free(t);
             return NULL;
         }
-        t->local[0] = (addr->sap_addr.itf & 0xff00) >> 8;
-        t->local[1] = (addr->sap_addr.itf & 0x00ff) >> 0;
-        t->local[2] = (addr->sap_addr.vpi & 0xff00) >> 8;
-        t->local[3] = (addr->sap_addr.vpi & 0x00ff) >> 0;
-        t->local[4] = (addr->sap_addr.vci & 0xff000000) >> 24;
-        t->local[5] = (addr->sap_addr.vci & 0x00ff0000) >> 16;
-        t->local[6] = (addr->sap_addr.vci & 0x0000ff00) >> 8;
-        t->local[7] = (addr->sap_addr.vci & 0x000000ff) >> 0;
-        t->local_length = 8;
 
         if (bind(t->sock, (const struct sockaddr *)addr,
                  sizeof(struct sockaddr_atmpvc)) < 0) {
@@ -257,20 +249,12 @@ netsnmp_aal5pvc_transport(const struct sockaddr_atmpvc *addr, int local)
         return NULL;
 #endif /* NETSNMP_NO_LISTEN_SUPPORT */
     } else {
-        t->remote = (unsigned char*)malloc(8);
+        t->remote_length = sizeof(*addr);
+        t->remote = netsnmp_memdup(addr, sizeof(*addr));
         if (t->remote == NULL) {
             netsnmp_transport_free(t);
             return NULL;
         }
-        t->remote[0] = (addr->sap_addr.itf & 0xff00) >> 8;
-        t->remote[1] = (addr->sap_addr.itf & 0x00ff) >> 0;
-        t->remote[2] = (addr->sap_addr.vpi & 0xff00) >> 8;
-        t->remote[3] = (addr->sap_addr.vpi & 0x00ff) >> 0;
-        t->remote[4] = (addr->sap_addr.vci & 0xff000000) >> 24;
-        t->remote[5] = (addr->sap_addr.vci & 0x00ff0000) >> 16;
-        t->remote[6] = (addr->sap_addr.vci & 0x0000ff00) >> 8;
-        t->remote[7] = (addr->sap_addr.vci & 0x000000ff) >> 0;
-        t->remote_length = 8;
 
         if (connect(t->sock, (const struct sockaddr *)addr,
                     sizeof(struct sockaddr_atmpvc)) < 0) {
@@ -340,20 +324,9 @@ netsnmp_aal5pvc_create_tstring(const char *str, int local,
 
 
 netsnmp_transport *
-netsnmp_aal5pvc_create_ostring(const u_char * o, size_t o_len, int local)
+netsnmp_aal5pvc_create_ostring(const void *o, size_t o_len, int local)
 {
-    struct sockaddr_atmpvc addr;
-
-    if (o_len == 8) {
-        addr.sap_family = AF_ATMPVC;
-        addr.sap_addr.itf = (o[0] << 8) + (o[1] << 0);
-        addr.sap_addr.vpi = (o[2] << 8) + (o[3] << 0);
-        addr.sap_addr.vci =
-	    (o[4] << 24) + (o[5] << 16) + (o[6] << 8) + (o[7] << 0);
-        return netsnmp_aal5pvc_transport(&addr, local);
-    }
-
-    return NULL;
+    return netsnmp_aal5pvc_transport(o, local);
 }
 
 

snmplib/transports/snmpDTLSUDPDomain.c
@@ -1535,28 +1535,16 @@ netsnmp_dtlsudp_create_tstring(const char *str, int isserver,
 
 
 netsnmp_transport *
-netsnmp_dtlsudp_create_ostring(const u_char * o, size_t o_len, int local)
+netsnmp_dtlsudp_create_ostring(const void *o, size_t o_len, int local)
 {
-    struct sockaddr_in addr;
-
-    if (o_len == 6) {
-        unsigned short porttmp = (o[4] << 8) + o[5];
-        addr.sin_family = AF_INET;
-        memcpy((u_char *) & (addr.sin_addr.s_addr), o, 4);
-        addr.sin_port = htons(porttmp);
-        return netsnmp_dtlsudp_transport(&addr, local);
-    }
+    if (o_len == sizeof(struct sockaddr_in))
+        return netsnmp_dtlsudp_transport(o, local);
 #ifdef NETSNMP_TRANSPORT_UDPIPV6_DOMAIN
-    else if (o_len == 18) {
-        struct sockaddr_in6 addr6;
-        unsigned short porttmp = (o[16] << 8) + o[17];
-        addr6.sin6_family = AF_INET6;
-        memcpy((u_char *) & (addr6.sin6_addr.s6_addr), o, 4);
-        addr6.sin6_port = htons(porttmp);
-        return netsnmp_dtlsudp6_transport(&addr6, local);
-    }
+    else if (o_len == sizeof(struct sockaddr_in6))
+        return netsnmp_dtlsudp6_transport(o, local);
 #endif
-    return NULL;
+    else
+        return NULL;
 }
 
 void

snmplib/transports/snmpIPXDomain.c
@@ -217,15 +217,12 @@ netsnmp_ipx_transport(const struct sockaddr_ipx *addr, int local)
 
     if (local) {
 #ifndef NETSNMP_NO_LISTEN_SUPPORT
-        t->local = (unsigned char*)malloc(12);
+        t->local_length = sizeof(*addr);
+        t->local = netsnmp_memdup(addr, sizeof(*addr));
         if (t->local == NULL) {
             netsnmp_transport_free(t);
             return NULL;
         }
-        memcpy(&(t->local[00]), (const u_char *)&addr->sipx_network, 4);
-        memcpy(&(t->local[04]), (const u_char *)&addr->sipx_node, 6);
-        memcpy(&(t->local[10]), (const u_char *)&addr->sipx_port, 2);
-        t->local_length = 12;
 
         /*
          * This session is inteneded as a server, so we must bind on to the
@@ -245,15 +242,12 @@ netsnmp_ipx_transport(const struct sockaddr_ipx *addr, int local)
         return NULL;
 #endif /* NETSNMP_NO_LISTEN_SUPPORT */
     } else {
-        t->remote = (unsigned char*)malloc(12);
+        t->remote_length = sizeof(*addr);
+        t->remote = netsnmp_memdup(addr, sizeof(*addr));
         if (t->remote == NULL) {
             netsnmp_transport_free(t);
             return NULL;
         }
-        memcpy(&(t->remote[00]), (const u_char *)&addr->sipx_network, 4);
-        memcpy(&(t->remote[04]), (const u_char *)&addr->sipx_node, 6);
-        memcpy(&(t->remote[10]), (const u_char *)&addr->sipx_port, 2);
-        t->remote_length = 12;
 
         /*
          * This is a client session.  Save the address in the
@@ -437,17 +431,11 @@ netsnmp_ipx_create_tstring(const char *str, int local,
 
 
 netsnmp_transport *
-netsnmp_ipx_create_ostring(const u_char * o, size_t o_len, int local)
+netsnmp_ipx_create_ostring(const void *o, size_t o_len, int local)
 {
-    struct sockaddr_ipx addr;
+    if (o_len == sizeof(struct sockaddr_ipx))
+        return netsnmp_ipx_transport(o, local);
 
-    if (o_len == 12) {
-        addr.sipx_family = AF_IPX;
-        memcpy((u_char *) & (addr.sipx_network), &(o[00]), 4);
-        memcpy((u_char *) & (addr.sipx_node), &(o[04]), 6);
-        memcpy((u_char *) & (addr.sipx_port), &(o[10]), 2);
-        return netsnmp_ipx_transport(&addr, local);
-    }
     return NULL;
 }
 

snmplib/transports/snmpSSHDomain.c
@@ -762,16 +762,13 @@ netsnmp_ssh_transport(const struct sockaddr_in *addr, int local)
             return NULL;
         }
 
-        t->remote = (u_char *)malloc(6);
-        if (t->remote == NULL) {
+        t->remote_length = sizeof(*addr);
+        t->remote = netsnmp_memdup(addr, sizeof(*addr));
+        if (!t->remote) {
             netsnmp_ssh_close(t);
             netsnmp_transport_free(t);
             return NULL;
         }
-        memcpy(t->remote, &addr->sin_addr.s_addr, 4);
-        t->remote[4] = (htons(addr->sin_port) & 0xff00) >> 8;
-        t->remote[5] = (htons(addr->sin_port) & 0x00ff) >> 0;
-        t->remote_length = 6;
 
         /*
          * This is a client-type session, so attempt to connect to the far
@@ -907,18 +904,9 @@ netsnmp_ssh_create_tstring(const char *str, int local,
 
 
 netsnmp_transport *
-netsnmp_ssh_create_ostring(const u_char * o, size_t o_len, int local)
+netsnmp_ssh_create_ostring(const void *o, size_t o_len, int local)
 {
-    struct sockaddr_in addr;
-
-    if (o_len == 6) {
-        unsigned short porttmp = (o[4] << 8) + o[5];
-        addr.sin_family = AF_INET;
-        memcpy((u_char *) & (addr.sin_addr.s_addr), o, 4);
-        addr.sin_port = htons(porttmp);
-        return netsnmp_ssh_transport(&addr, local);
-    }
-    return NULL;
+    return netsnmp_ssh_transport(o, local);
 }
 
 void

snmplib/transports/snmpSTDDomain.c
@@ -263,9 +263,9 @@ netsnmp_std_create_tstring(const char *instring, int local,
 }
 
 netsnmp_transport *
-netsnmp_std_create_ostring(const u_char * o, size_t o_len, int local)
+netsnmp_std_create_ostring(const void *o, size_t o_len, int local)
 {
-    return netsnmp_std_transport((const char*)o, o_len, NULL);
+    return netsnmp_std_transport(o, o_len, NULL);
 }
 
 void

commit 2b1d51aba50992549de03f76b77f045c63bd1296

SNMP-TARGET-MIB: Fix snmpTargetAddrTAddress
See also https://sourceforge.net/p/net-snmp/bugs/2871/.

Reported-by: Anders Wallin <wayline@users.sourceforge.net>
Fixes: 3bd8dc8b2d90 ("snmplib/transports: Avoid that the create_from_ostring methods truncate IPv6 addresses")

agent/mibgroup/notification/snmpNotifyTable_data.c
@@ -344,8 +344,10 @@ notifyTable_register_notifications(int major, int minor,
     ptr->nameData = netsnmp_memdup_nt(name, nameLen, &ptr->nameLen);
     memcpy(ptr->tDomain, t->domain, t->domain_length * sizeof(oid));
     ptr->tDomainLen = t->domain_length;
-    ptr->tAddressLen = t->remote_length;
-    ptr->tAddress = t->remote;
+    if (t->f_get_taddr)
+        t->f_get_taddr(t, &ptr->tAddress, &ptr->tAddressLen);
+    else
+        netsnmp_assert(0);
 
     ptr->timeout = ss->timeout / 1000;
     ptr->retryCount = ss->retries;

agent/mibgroup/target/snmpTargetAddrEntry.c
@@ -385,9 +385,9 @@ write_snmpTargetAddrTAddress(int action,
                 return SNMP_ERR_INCONSISTENTVALUE;
             }
 
-            old_addr = (char *) target->tAddress;
+            old_addr = target->tAddress;
             old_len = target->tAddressLen;
-            target->tAddress = (u_char *) malloc(var_val_len);
+            target->tAddress = malloc(var_val_len);
             if (target->tAddress == NULL) {
                 return SNMP_ERR_RESOURCEUNAVAILABLE;
             }
@@ -419,7 +419,7 @@ write_snmpTargetAddrTAddress(int action,
             if (target->storageType != SNMP_STORAGE_READONLY
                 && target->rowStatus != SNMP_ROW_ACTIVE) {
                 SNMP_FREE(target->tAddress);
-                target->tAddress = (u_char *) old_addr;
+                target->tAddress = old_addr;
                 target->tAddressLen = old_len;
                 if (target->rowStatus == SNMP_ROW_NOTINSERVICE &&
                     snmpTargetAddr_rowStatusCheck(target) == 0) {

agent/mibgroup/target/snmpTargetAddrEntry_data.h
@@ -44,7 +44,7 @@
          size_t          nameLen;
          oid             tDomain[MAX_OID_LEN];
          int             tDomainLen;
-         unsigned char  *tAddress;
+         void           *tAddress;
          size_t          tAddressLen;
          int             timeout;      /* Timeout in centiseconds */
          int             retryCount;

include/net-snmp/library/snmpIPv4BaseDomain.h
@@ -19,6 +19,10 @@ extern          "C" {
 
     char *netsnmp_ipv4_fmtaddr(const char *prefix, netsnmp_transport *t,
                                const void *data, int len);
+    void netsnmp_ipv4_get_taddr(struct netsnmp_transport_s *t, void **addr,
+                                size_t *addr_len);
+    int netsnmp_ipv4_ostring_to_sockaddr(struct sockaddr_in *sin,
+                                         const void *o, size_t o_len);
 
 /*
  * Convert a "traditional" peername into a sockaddr_in structure which is

include/net-snmp/library/snmpIPv6BaseDomain.h
@@ -21,6 +21,12 @@ extern          "C" {
     char *netsnmp_ipv6_fmtaddr(const char *prefix, netsnmp_transport *t,
                                const void *data, int len);
     NETSNMP_IMPORT
+    void netsnmp_ipv6_get_taddr(struct netsnmp_transport_s *t, void **addr,
+                                size_t *addr_len);
+    NETSNMP_IMPORT
+    int netsnmp_ipv6_ostring_to_sockaddr(struct sockaddr_in6 *sin6,
+                                         const void *o, size_t o_len);
+    NETSNMP_IMPORT
     int netsnmp_sockaddr_in6_2(struct sockaddr_in6 *addr,
                                const char *inpeername,
                                const char *default_target);

include/net-snmp/library/snmp_transport.h
@@ -213,6 +213,11 @@ typedef struct netsnmp_transport_s {
     /* allocated host name identifier; used by configuration system
        to load localhost.conf for host-specific configuration */
     u_char         *identifier; /* udp:localhost:161 -> "localhost" */
+
+    /* Duplicate the remote address in the format required by SNMP-TARGET-MIB */
+    void           (*f_get_taddr)(struct netsnmp_transport_s *t,
+                                  void **addr, size_t *addr_len);
+
 } netsnmp_transport;
 
 typedef struct netsnmp_transport_list_s {
@@ -232,7 +237,9 @@ typedef struct netsnmp_tdomain_s {
      */
     netsnmp_transport *(*f_create_from_tstring) (const char *, int);
 
-    netsnmp_transport *(*f_create_from_ostring) (const void *, size_t, int);
+    /* @o and @o_len define an address in the format used by SNMP-TARGET-MIB */
+    netsnmp_transport *(*f_create_from_ostring) (const void *o, size_t o_len,
+                                                 int local);
 
     struct netsnmp_tdomain_s *next;
 

snmplib/transports/snmpAAL5PVCDomain.c
@@ -67,7 +67,25 @@ netsnmp_aal5pvc_fmtaddr(netsnmp_transport *t, const void *data, int len)
     }
 }
 
+static void
+netsnmp_aal5pvc_get_taddr(netsnmp_transport *t, void **addr, size_t *addr_len)
+{
+    struct sockaddr_atmpvc *sa = t->remote;
+    unsigned char *p;
 
+    *addr_len = 8;
+    if (!(*addr = malloc(*addr_len)))
+        return;
+    p = *addr;
+    p[0] = sa->sap_addr.itf >> 8;
+    p[1] = sa->sap_addr.itf >> 0;
+    p[2] = sa->sap_addr.vpi >> 8;
+    p[3] = sa->sap_addr.vpi >> 0;
+    p[4] = sa->sap_addr.vci >> 24;
+    p[5] = sa->sap_addr.vci >> 16;
+    p[6] = sa->sap_addr.vci >> 8;
+    p[7] = sa->sap_addr.vci >> 0;
+}
 
 /*
  * You can write something into opaque that will subsequently get passed back 
@@ -285,6 +303,7 @@ netsnmp_aal5pvc_transport(const struct sockaddr_atmpvc *addr, int local)
     t->f_close    = netsnmp_aal5pvc_close;
     t->f_accept   = NULL;
     t->f_fmtaddr  = netsnmp_aal5pvc_fmtaddr;
+    t->f_get_taddr = netsnmp_aal5pvc_get_taddr;
 
     return t;
 }
@@ -322,12 +341,30 @@ netsnmp_aal5pvc_create_tstring(const char *str, int local,
     }
 }
 
+static int netsnmp_aal5pvc_ostring_to_sockaddr(struct sockaddr_atmpvc *sa,
+                                               const void *o, size_t o_len)
+{
+    const unsigned char *p = o;
 
+    if (o_len != 8)
+        return 0;
+
+    memset(sa, 0, sizeof(*sa));
+    sa->sap_family = AF_ATMPVC;
+    sa->sap_addr.itf = (p[0] << 8) + (p[1] << 0);
+    sa->sap_addr.vpi = (p[2] << 8) + (p[3] << 0);
+    sa->sap_addr.vci = (p[4] << 24) + (p[5] << 16) + (p[6] << 8) + (p[7] << 0);
+    return 1;
+}
 
 netsnmp_transport *
 netsnmp_aal5pvc_create_ostring(const void *o, size_t o_len, int local)
 {
-    return netsnmp_aal5pvc_transport(o, local);
+    struct sockaddr_atmpvc sa;
+
+    if (netsnmp_aal5pvc_ostring_to_sockaddr(&sa, o, o_len))
+        return netsnmp_aal5pvc_transport(&sa, local);
+    return NULL;
 }
 
 

snmplib/transports/snmpDTLSUDPDomain.c
@@ -1486,6 +1486,7 @@ _transport_common(netsnmp_transport *t, int local)
     t->f_setup_session = netsnmp_tlsbase_session_init;
     t->f_accept        = NULL;
     t->f_fmtaddr       = netsnmp_dtlsudp4_fmtaddr;
+    t->f_get_taddr     = netsnmp_ipv4_get_taddr;
 
     t->flags = NETSNMP_TRANSPORT_FLAG_TUNNELED;
 
@@ -1553,6 +1554,7 @@ netsnmp_dtlsudp6_transport(const struct sockaddr_in6 *addr, int local)
     /* XXX: and buf size */        
 
     t->f_fmtaddr       = netsnmp_dtlsudp6_fmtaddr;
+    t->f_get_taddr     = netsnmp_ipv6_get_taddr;
 
     return t;
 }
@@ -1601,11 +1603,14 @@ netsnmp_dtlsudp_create_tstring(const char *str, int isserver,
 netsnmp_transport *
 netsnmp_dtlsudp_create_ostring(const void *o, size_t o_len, int local)
 {
-    if (o_len == sizeof(struct sockaddr_in))
-        return netsnmp_dtlsudp_transport(o, local);
+    struct sockaddr_in sin;
+    struct sockaddr_in6 sin6;
+
+    if (netsnmp_ipv4_ostring_to_sockaddr(&sin, o, o_len))
+        return netsnmp_dtlsudp_transport(&sin, local);
 #ifdef NETSNMP_TRANSPORT_UDPIPV6_DOMAIN
-    else if (o_len == sizeof(struct sockaddr_in6))
-        return netsnmp_dtlsudp6_transport(o, local);
+    else if (netsnmp_ipv6_ostring_to_sockaddr(&sin6, o, o_len))
+        return netsnmp_dtlsudp6_transport(&sin6, local);
 #endif
     else
         return NULL;

snmplib/transports/snmpIPXDomain.c
@@ -33,6 +33,7 @@
 #include <net-snmp/output_api.h>
 #include <net-snmp/config_api.h>
 
+#include <net-snmp/library/snmp_assert.h>
 #include <net-snmp/library/snmp_transport.h>
 #include <net-snmp/library/tools.h>
 
@@ -69,7 +70,19 @@ netsnmp_ipx_fmtaddr(netsnmp_transport *t, const void *data, int len)
     }
 }
 
+static void netsnmp_ipx_get_taddr(struct netsnmp_transport_s *t,
+                                  void **addr, size_t *addr_len)
+{
+    struct sockaddr_ipx *sa = t->remote;
 
+    netsnmp_assert(t->remote_length == sizeof(*sa));
+    *addr_len = 12;
+    if ((*addr = malloc(*addr_len))) {
+        memcpy(*addr + 0,  &sa->sipx_network, 4);
+        memcpy(*addr + 4,  &sa->sipx_node,    6);
+        memcpy(*addr + 10, &sa->sipx_port,    2);
+    }
+}
 
 /*
  * You can write something into opaque that will subsequently get passed back 
@@ -276,6 +289,7 @@ netsnmp_ipx_transport(const struct sockaddr_ipx *addr, int local)
     t->f_close    = netsnmp_ipx_close;
     t->f_accept   = NULL;
     t->f_fmtaddr  = netsnmp_ipx_fmtaddr;
+    t->f_get_taddr = netsnmp_ipx_get_taddr;
 
     return t;
 }
@@ -430,13 +444,27 @@ netsnmp_ipx_create_tstring(const char *str, int local,
     }
 }
 
+static int netsnmp_ipx_ostring_to_sockaddr(struct sockaddr_ipx *sa,
+                                           const void *o, size_t o_len)
+{
+    if (o_len != 12)
+        return 0;
 
+    memset(sa, 0, sizeof(*sa));
+    sa->sipx_family = AF_IPX;
+    memcpy(&sa->sipx_network, o + 0, 4);
+    memcpy(&sa->sipx_node,    o + 4, 6);
+    memcpy(&sa->sipx_port,    o + 10, 2);
+    return 1;
+}
 
 netsnmp_transport *
 netsnmp_ipx_create_ostring(const void *o, size_t o_len, int local)
 {
-    if (o_len == sizeof(struct sockaddr_ipx))
-        return netsnmp_ipx_transport(o, local);
+    struct sockaddr_ipx sa;
+
+    if (netsnmp_ipx_ostring_to_sockaddr(&sa, o, o_len))
+        return netsnmp_ipx_transport(&sa, local);
 
     return NULL;
 }

snmplib/transports/snmpIPv4BaseDomain.c
@@ -242,3 +242,29 @@ netsnmp_ipv4_fmtaddr(const char *prefix, netsnmp_transport *t,
     return tmp;
 }
 
+void netsnmp_ipv4_get_taddr(struct netsnmp_transport_s *t, void **addr,
+                            size_t *addr_len)
+{
+    struct sockaddr_in *sin = t->remote;
+
+    netsnmp_assert(t->remote_length == sizeof(*sin));
+
+    *addr_len = 6;
+    if ((*addr = malloc(*addr_len))) {
+        memcpy(*addr,     &sin->sin_addr, 4);
+        memcpy(*addr + 4, &sin->sin_port, 2);
+    }
+}
+
+int netsnmp_ipv4_ostring_to_sockaddr(struct sockaddr_in *sin, const void *o,
+                                     size_t o_len)
+{
+    if (o_len != 6)
+        return 0;
+
+    memset(sin, 0, sizeof(*sin));
+    sin->sin_family = AF_INET;
+    memcpy(&sin->sin_addr, o + 0, 4);
+    memcpy(&sin->sin_port, o + 4, 2);
+    return 1;
+}

snmplib/transports/snmpIPv6BaseDomain.c
@@ -151,6 +151,33 @@ netsnmp_ipv6_fmtaddr(const char *prefix, netsnmp_transport *t,
     return tmp;
 }
 
+void netsnmp_ipv6_get_taddr(struct netsnmp_transport_s *t, void **addr,
+                            size_t *addr_len)
+{
+    struct sockaddr_in6 *sin6 = t->remote;
+
+    netsnmp_assert(t->remote_length == sizeof(*sin6));
+
+    *addr_len = 18;
+    if ((*addr = malloc(*addr_len))) {
+        memcpy(*addr,      &sin6->sin6_addr, 16);
+        memcpy(*addr + 16, &sin6->sin6_port, 2);
+    }
+}
+
+int netsnmp_ipv6_ostring_to_sockaddr(struct sockaddr_in6 *sin6, const void *o,
+                                     size_t o_len)
+{
+    if (o_len != 18)
+        return 0;
+
+    memset(sin6, 0, sizeof(*sin6));
+    sin6->sin6_family = AF_INET6;
+    memcpy(&sin6->sin6_addr, o + 0,  16);
+    memcpy(&sin6->sin6_port, o + 16, 2);
+    return 1;
+}
+
 int
 netsnmp_sockaddr_in6_2(struct sockaddr_in6 *addr,
                        const char *inpeername, const char *default_target)

snmplib/transports/snmpSSHDomain.c
@@ -1,6 +1,7 @@
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-features.h>
 
+#include <net-snmp/library/snmp_assert.h>
 #include <net-snmp/library/snmpSSHDomain.h>
 
 #include <stdio.h>
@@ -123,7 +124,18 @@ netsnmp_ssh_fmtaddr(netsnmp_transport *t, const void *data, int len)
     }
 }
 
-
+static void netsnmp_ssh_get_taddr(struct netsnmp_transport_s *t,
+                                  void **addr, size_t *addr_len)
+{
+    switch (t->remote_length) {
+    case sizeof(struct sockaddr_in):
+        netsnmp_ipv4_get_taddr(t, addr, addr_len);
+        break;
+    default:
+        *addr = NULL;
+        netsnmp_assert(0);
+    }
+}
 
 /*
  * You can write something into opaque that will subsequently get passed back 
@@ -883,6 +895,7 @@ netsnmp_ssh_transport(const struct sockaddr_in *addr, int local)
     t->f_close    = netsnmp_ssh_close;
     t->f_accept   = netsnmp_ssh_accept;
     t->f_fmtaddr  = netsnmp_ssh_fmtaddr;
+    t->f_get_taddr = netsnmp_ssh_get_taddr;
 
     return t;
 }
@@ -907,7 +920,13 @@ netsnmp_ssh_create_tstring(const char *str, int local,
 netsnmp_transport *
 netsnmp_ssh_create_ostring(const void *o, size_t o_len, int local)
 {
-    return netsnmp_ssh_transport(o, local);
+    struct sockaddr_in sin;
+
+    if (netsnmp_ipv4_ostring_to_sockaddr(&sin, o, o_len))
+        return netsnmp_ssh_transport(&sin, local);
+    else
+        netsnmp_assert(0);
+    return NULL;
 }
 
 void

snmplib/transports/snmpSTDDomain.c
@@ -53,7 +53,12 @@ netsnmp_std_fmtaddr(netsnmp_transport *t, const void *data, int len)
     return strdup("STDInOut");
 }
 
-
+static void
+netsnmp_std_get_taddr(netsnmp_transport *t, void **addr, size_t *addr_len)
+{
+    *addr_len = t->remote_length;
+    *addr = netsnmp_memdup(t->remote, *addr_len);
+}
 
 /*
  * You can write something into opaque that will subsequently get passed back 
@@ -176,6 +181,7 @@ netsnmp_std_transport(const char *instring, size_t instring_len,
     t->f_close    = netsnmp_std_close;
     t->f_accept   = netsnmp_std_accept;
     t->f_fmtaddr  = netsnmp_std_fmtaddr;
+    t->f_get_taddr = netsnmp_std_get_taddr;
 
     /*
      * if instring is not null length, it specifies a path to a prog

snmplib/transports/snmpTCPDomain.c
@@ -310,6 +310,7 @@ netsnmp_tcp_transport(const struct sockaddr_in *addr, int local)
     t->f_close    = netsnmp_socketbase_close;
     t->f_accept   = netsnmp_tcp_accept;
     t->f_fmtaddr  = netsnmp_tcp_fmtaddr;
+    t->f_get_taddr = netsnmp_ipv4_get_taddr;
 
     return t;
 }
@@ -334,7 +335,11 @@ netsnmp_tcp_create_tstring(const char *str, int local,
 netsnmp_transport *
 netsnmp_tcp_create_ostring(const void *o, size_t o_len, int local)
 {
-    return netsnmp_tcp_transport(o, local);
+    struct sockaddr_in sin;
+
+    if (netsnmp_ipv4_ostring_to_sockaddr(&sin, o, o_len))
+        return netsnmp_tcp_transport(&sin, local);
+    return NULL;
 }
 
 

snmplib/transports/snmpTCPIPv6Domain.c
@@ -316,6 +316,7 @@ netsnmp_tcp6_transport(const struct sockaddr_in6 *addr, int local)
     t->f_close    = netsnmp_socketbase_close;
     t->f_accept   = netsnmp_tcp6_accept;
     t->f_fmtaddr  = netsnmp_tcp6_fmtaddr;
+    t->f_get_taddr = netsnmp_ipv6_get_taddr;
 
     return t;
 }
@@ -346,7 +347,11 @@ netsnmp_tcp6_create_tstring(const char *str, int local,
 netsnmp_transport *
 netsnmp_tcp6_create_ostring(const void *o, size_t o_len, int local)
 {
-    return netsnmp_tcp6_transport(o, local);
+    struct sockaddr_in6 sin6;
+
+    if (netsnmp_ipv6_ostring_to_sockaddr(&sin6, o, o_len))
+        return netsnmp_tcp6_transport(&sin6, local);
+    return NULL;
 }
 
 

snmplib/transports/snmpTLSTCPDomain.c
@@ -124,6 +124,14 @@ netsnmp_tlstcp_fmtaddr(netsnmp_transport *t, const void *data, int len)
     }
     }
 }
+
+static void netsnmp_tlstcp_get_taddr(struct netsnmp_transport_s *t,
+                                     void **addr, size_t *addr_len)
+{
+    *addr_len = t->remote_length;
+    *addr = netsnmp_memdup(t->remote, *addr_len);
+}
+
 /*
  * You can write something into opaque that will subsequently get passed back 
  * to your send function if you like.  For instance, you might want to
@@ -986,6 +994,7 @@ netsnmp_tlstcp_transport(const char *addr_string, int isserver)
     t->f_config        = netsnmp_tlsbase_config;
     t->f_setup_session = netsnmp_tlsbase_session_init;
     t->f_fmtaddr       = netsnmp_tlstcp_fmtaddr;
+    t->f_get_taddr     = netsnmp_tlstcp_get_taddr;
 
     t->flags |= NETSNMP_TRANSPORT_FLAG_TUNNELED | NETSNMP_TRANSPORT_FLAG_STREAM;
 

snmplib/transports/snmpUDPDomain.c
@@ -146,6 +146,7 @@ netsnmp_udp_transport_base(netsnmp_transport *t)
     t->f_close    = netsnmp_socketbase_close;
     t->f_accept   = NULL;
     t->f_fmtaddr  = netsnmp_udp_fmtaddr;
+    t->f_get_taddr = netsnmp_ipv4_get_taddr;
 
     return t;
 }
@@ -628,7 +629,11 @@ netsnmp_udp_create_tspec(netsnmp_tdomain_spec *tspec)
 netsnmp_transport *
 netsnmp_udp_create_ostring(const void *o, size_t o_len, int local)
 {
-    return netsnmp_udp_transport(o, local);
+    struct sockaddr_in sin;
+
+    if (netsnmp_ipv4_ostring_to_sockaddr(&sin, o, o_len))
+        return netsnmp_udp_transport(&sin, local);
+    return NULL;
 }
 
 

snmplib/transports/snmpUDPIPv6Domain.c
@@ -263,6 +263,7 @@ netsnmp_udp6_transport_init(const struct sockaddr_in6 *addr, int flags)
     t->f_close    = netsnmp_socketbase_close;
     t->f_accept   = NULL;
     t->f_fmtaddr  = netsnmp_udp6_fmtaddr;
+    t->f_get_taddr = netsnmp_ipv6_get_taddr;
 
     t->domain = netsnmp_UDPIPv6Domain;
     t->domain_length =
@@ -951,7 +952,11 @@ netsnmp_udp6_create_tspec(netsnmp_tdomain_spec *tspec)
 netsnmp_transport *
 netsnmp_udp6_create_ostring(const void *o, size_t o_len, int local)
 {
-    return netsnmp_udp6_transport(o, local);
+    struct sockaddr_in6 sin6;
+
+    if (netsnmp_ipv6_ostring_to_sockaddr(&sin6, o, o_len))
+        return netsnmp_udp6_transport(&sin6, local);
+    return NULL;
 }
 
 

snmplib/transports/snmpUDPsharedDomain.c
@@ -178,6 +178,12 @@ _transport_common(netsnmp_transport *t)
     t->f_fmtaddr       = _udpshared_fmtaddr;
     t->f_setup_session = _setup_session;
     t->flags = NETSNMP_TRANSPORT_FLAG_SHARED;
+    if (t->base_transport->domain == netsnmpUDPDomain)
+        t->f_get_taddr = netsnmp_ipv4_get_taddr;
+    else if (t->base_transport->domain == netsnmp_UDPIPv6Domain)
+        t->f_get_taddr = netsnmp_ipv6_get_taddr;
+    else
+        netsnmp_assert(0);
 
     return t;
 }
@@ -340,13 +346,16 @@ netsnmp_udpshared6_transport_with_source(const struct sockaddr_in6 *addr6,
 netsnmp_transport *
 netsnmp_udpshared_create_ostring(const void *o, size_t o_len, int local)
 {
+    struct sockaddr_in sin;
+    struct sockaddr_in6 sin6;
+
     DEBUGMSGTL(("udpshared:create", "from ostring\n"));
 
-    if (o_len == sizeof(struct sockaddr_in))
-        return netsnmp_udpshared_transport(o, local);
+    if (netsnmp_ipv4_ostring_to_sockaddr(&sin, o, o_len))
+        return netsnmp_udpshared_transport(&sin, local);
 #ifdef NETSNMP_TRANSPORT_UDPIPV6_DOMAIN
-    else if (o_len == sizeof(struct sockaddr_in6))
-        return netsnmp_udpshared6_transport(o, local);
+    else if (netsnmp_ipv6_ostring_to_sockaddr(&sin6, o, o_len))
+        return netsnmp_udpshared6_transport(&sin6, local);
 #endif
     return NULL;
 }

snmplib/transports/snmpUnixDomain.c
@@ -107,7 +107,12 @@ netsnmp_unix_fmtaddr(netsnmp_transport *t, const void *data, int len)
     }
 }
 
-
+static void
+netsnmp_unix_get_taddr(netsnmp_transport *t, void **addr, size_t *addr_len)
+{
+    *addr_len = t->remote_length;
+    *addr = netsnmp_memdup(t->remote, *addr_len);
+}
 
 /*
  * You can write something into opaque that will subsequently get passed back
@@ -459,6 +464,7 @@ netsnmp_unix_transport(const struct sockaddr_un *addr, int local)
     t->f_close    = netsnmp_unix_close;
     t->f_accept   = netsnmp_unix_accept;
     t->f_fmtaddr  = netsnmp_unix_fmtaddr;
+    t->f_get_taddr = netsnmp_unix_get_taddr;
 
     return t;
 }