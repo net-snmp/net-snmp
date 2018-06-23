56d8662c867a47d012920537198de6300017fc02
Bart Van Assche <bvanassche@acm.org>
2018-06-23 08:17:21 -0700

    snmplib: Define LOG_DAEMON etc. once

--- agent/snmpd.c
+++ agent/snmpd.c
@@ -145,6 +145,8 @@
 #include <net-snmp/agent/table.h>
 #include <net-snmp/agent/table_iterator.h>
 
+#include "../snmplib/snmp_syslog.h"
+
 #include "mibgroup/util_funcs/restart.h"
 
 /*
@@ -206,44 +208,6 @@ static int      receive(void);
 #ifdef WIN32SERVICE
 static void     StopSnmpAgent(void);
 #endif
-
-/*
- * These definitions handle 4.2 systems without additional syslog facilities.
- */
-#ifndef LOG_CONS
-#define LOG_CONS	0       /* Don't bother if not defined... */
-#endif
-#ifndef LOG_PID
-#define LOG_PID		0       /* Don't bother if not defined... */
-#endif
-#ifndef LOG_LOCAL0
-#define LOG_LOCAL0	0
-#endif
-#ifndef LOG_LOCAL1
-#define LOG_LOCAL1	0
-#endif
-#ifndef LOG_LOCAL2
-#define LOG_LOCAL2	0
-#endif
-#ifndef LOG_LOCAL3
-#define LOG_LOCAL3	0
-#endif
-#ifndef LOG_LOCAL4
-#define LOG_LOCAL4	0
-#endif
-#ifndef LOG_LOCAL5
-#define LOG_LOCAL5	0
-#endif
-#ifndef LOG_LOCAL6
-#define LOG_LOCAL6	0
-#endif
-#ifndef LOG_LOCAL7
-#define LOG_LOCAL7	0
-#endif
-#ifndef LOG_DAEMON
-#define LOG_DAEMON	0
-#endif
-
 
 static void
 usage(char *prog)
