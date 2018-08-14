b14ff16eee753b29ef7659b34a67af89acee22f7
Bart Van Assche <bvanassche@acm.org>
2018-08-14 09:30:06 -0700

    snmplib, IPv6: Fix MSVC build
    Fixes: 4916ce007362 ("Fix Solaris compiler warnings")
    Reported-by: Gisle Vanem <gvanem@yahoo.no>

--- snmplib/transports/snmpUDPIPv6Domain.c
+++ snmplib/transports/snmpUDPIPv6Domain.c
@@ -34,11 +34,6 @@
 #if HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
 #endif
-
-#if defined(HAVE_WINSOCK_H) && !defined(mingw32)
-static const struct in6_addr in6addr_any = { } /*IN6ADDR_ANY_INIT*/;
-#endif
-
 #if HAVE_NETINET_IN_H
 #include <netinet/in.h>
 #endif
