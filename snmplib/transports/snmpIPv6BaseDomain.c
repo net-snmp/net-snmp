net-snmp/net-snmp

commit 4916ce00736296be79619d1debecbb74c7beee33

Fix Solaris compiler warnings
This patch does not change any functionality.

snmplib/transports/snmpIPv6BaseDomain.c
@@ -66,7 +66,7 @@
 
 
 #if defined(HAVE_WINSOCK_H) && !defined(mingw32)
-static const struct in6_addr in6addr_any = IN6ADDR_ANY_INIT;
+static const struct in6_addr in6addr_any = { } /*IN6ADDR_ANY_INIT*/;
 #endif
 
 

snmplib/transports/snmpUDPIPv6Domain.c
@@ -35,7 +35,7 @@
 #endif
 
 #if defined(HAVE_WINSOCK_H) && !defined(mingw32)
-static const struct in6_addr in6addr_any = IN6ADDR_ANY_INIT;
+static const struct in6_addr in6addr_any = { } /*IN6ADDR_ANY_INIT*/;
 #endif
 
 #if HAVE_NETINET_IN_H

agent/mibgroup/hardware/cpu/cpu_kstat.c
@@ -22,8 +22,9 @@ int _cpu_status(char *state);
      * Initialise the list of CPUs on the system
      *   (including descriptions)
      */
-void init_cpu_kstat( void ) {
-    int               i = 0, n = 0, clock, state_begin;
+void init_cpu_kstat(void)
+{
+    int               i = 0, n = 0, clock;
     char              ctype[16], ftype[16], state[10];
     kstat_t          *ksp;
     kstat_named_t    *ks_data;
@@ -49,8 +50,6 @@ void init_cpu_kstat( void ) {
             for (i=0, ks_data = ksp->ks_data; i < ksp->ks_ndata; i++, ks_data++) {
                 if ( strcmp( ks_data->name, "state" ) == 0 ) {
                     strlcpy(state, ks_data->value.c, sizeof(state));
-                } else if ( strcmp( ks_data->name, "state_begin" ) == 0 ) {
-                    state_begin = ks_data->value.i32;
                 } else if ( strcmp( ks_data->name, "cpu_type" ) == 0 ) {
                     strlcpy(ctype, ks_data->value.c, sizeof(ctype));
                 } else if ( strcmp( ks_data->name, "fpu_type" ) == 0 ) {

agent/mibgroup/hardware/fsys/fsys_mntent.c
@@ -212,7 +212,7 @@ netsnmp_fsys_arch_load( void )
         if ( _fsys_remote( entry->device, entry->type ))
             entry->flags |= NETSNMP_FS_FLAG_REMOTE;
 #if HAVE_HASMNTOPT
-        if (hasmntopt( m, "ro" ))
+        if (hasmntopt( m, NETSNMP_REMOVE_CONST(char *, "ro") ))
             entry->flags |= NETSNMP_FS_FLAG_RONLY;
         else
             entry->flags &= ~NETSNMP_FS_FLAG_RONLY;

agent/mibgroup/host/data_access/swinst_pkginfo.c
@@ -182,8 +182,8 @@ netsnmp_swinst_arch_load( netsnmp_container *container, u_int flags)
             CONTAINER_INSERT(container, entry);
 
 #ifdef HAVE_PKGINFO
-	    v = pkgparam( dp->d_name, "VERSION" );
-	    c = pkgparam( dp->d_name, "CATEGORY" );
+	    v = pkgparam( dp->d_name, NETSNMP_REMOVE_CONST(char *, "VERSION") );
+	    c = pkgparam( dp->d_name, NETSNMP_REMOVE_CONST(char *, "CATEGORY") );
 
 	    entry->swName_len = snprintf( entry->swName, sizeof(entry->swName),
 					  "%s-%s", dp->d_name, v );

agent/mibgroup/host/data_access/swrun_procfs_psinfo.c
@@ -56,7 +56,7 @@ netsnmp_arch_swrun_container_load( netsnmp_container *container, u_int flags)
     DIR                 *procdir = NULL;
     struct dirent       *procentry_p;
     psinfo_t             psinfo;
-    int                  pid, rc, fd;
+    int                  pid, fd;
     char                *cp, buf[512];
     netsnmp_swrun_entry *entry;
 
@@ -77,7 +77,7 @@ netsnmp_arch_swrun_container_load( netsnmp_container *container, u_int flags)
         entry = netsnmp_swrun_entry_create(pid);
         if (NULL == entry)
             continue;   /* error already logged by function */
-        rc = CONTAINER_INSERT(container, entry);
+        CONTAINER_INSERT(container, entry);
 
         /*
          * Now extract the interesting information

agent/mibgroup/host/hr_system.c
@@ -433,7 +433,7 @@ set_solaris_bootcommand_parameter(int action,
         case RESERVE2: {
             /* create copy of old value */
             if(statP) {
-                int old_val_len=strlen(statP);
+                int old_val_len = strlen((const char *)statP);
                 if(old_val_len >= sizeof(old_value)) {
                     p_old_value=(char *)malloc(old_val_len+1);
                     if(p_old_value==NULL) {
@@ -441,7 +441,7 @@ set_solaris_bootcommand_parameter(int action,
                         return SNMP_ERR_GENERR;
                     } 
                 }
-                strlcpy(p_old_value,statP,old_val_len+1);
+                strlcpy(p_old_value, (const char *)statP, old_val_len+1);
             } else { 
                 p_old_value=NULL;
             }
@@ -537,16 +537,16 @@ static long get_max_solaris_processes(void) {
     static long maxprocs=-1;
 
     /* assume only necessary to compute once, since /etc/system must be modified */
-    if (maxprocs == -1) {
-        if ( (ksc=kstat_open()) != NULL && 
-             (ks=kstat_lookup(ksc, "unix", 0, "var")) != NULL && 
-             (kstat_read(ksc, ks, &v) != -1)) {
+    if (maxprocs >= 0)
+        return maxprocs;
 
-            maxprocs=v.v_proc;
-        }
-        if(ksc) {
-            kstat_close(ksc);
-        }
+    ksc = kstat_open();
+    if (ksc) {
+        ks = kstat_lookup(ksc, NETSNMP_REMOVE_CONST(char *, "unix"), 0,
+                          NETSNMP_REMOVE_CONST(char *, "var"));
+        if (ks && kstat_read(ksc, ks, &v) != -1)
+            maxprocs = v.v_proc;
+        kstat_close(ksc);
     }
 
     return maxprocs;

agent/mibgroup/ip-forward-mib/data_access/route_solaris.c
@@ -20,8 +20,8 @@
 #include "if-mib/data_access/interface_ioctl.h"
 #include "route_private.h"
 
-static int _load_v4(netsnmp_container *container, u_long *count);
-static int _load_v6(netsnmp_container *container, u_long *count);
+static int _load_v4(netsnmp_container *container);
+static int _load_v6(netsnmp_container *container);
 
 /** arch specific load
  * @internal
@@ -34,7 +34,6 @@ int
 netsnmp_access_route_container_arch_load(netsnmp_container* container,
                                          u_int load_flags)
 {
-    u_long          count = 0;
     int             rc;
 
     DEBUGMSGTL(("access:route:container",
@@ -45,7 +44,7 @@ netsnmp_access_route_container_arch_load(netsnmp_container* container,
         return -1;
     }
 
-    rc = _load_v4(container, &count);
+    rc = _load_v4(container);
     
 #ifdef NETSNMP_ENABLE_IPV6
     if((0 != rc) || (load_flags & NETSNMP_ACCESS_ROUTE_LOAD_IPV4_ONLY))
@@ -55,7 +54,7 @@ netsnmp_access_route_container_arch_load(netsnmp_container* container,
      * load ipv6. ipv6 module might not be loaded,
      * so ignore -2 err (file not found)
      */
-    rc = _load_v6(container, &count);
+    rc = _load_v6(container);
     if (-2 == rc)
         rc = 0;
 #endif
@@ -148,7 +147,7 @@ IP6_Cmp_Route(void *addr, void *ep)
 }
 
 
-static int _load_v4(netsnmp_container *container, u_long *count)
+static int _load_v4(netsnmp_container *container)
 {
     netsnmp_route_entry *entry;
     mib2_ipRouteEntry_t Curentry, Nextentry;
@@ -197,13 +196,12 @@ static int _load_v4(netsnmp_container *container, u_long *count)
 	    netsnmp_access_route_entry_free(entry);
 	    continue;
 	}
-	*count++;
     }
     return 0;
 }
 
 
-static int _load_v6(netsnmp_container *container, u_long *count)
+static int _load_v6(netsnmp_container *container)
 {
     netsnmp_route_entry *entry;
     mib2_ipv6RouteEntry_t Curentry, Nextentry;
@@ -253,7 +251,6 @@ static int _load_v6(netsnmp_container *container, u_long *count)
 	    netsnmp_access_route_entry_free(entry);
 	    continue;
 	}
-	*count++;
     }
     return 0;
 }

commit 9e5d7a4475433295ec8a11356dcee69e8ac17a49

snmplib, IPv6: Fix MSVC build
Fixes: 4916ce007362 ("Fix Solaris compiler warnings")
Reported-by: Gisle Vanem <gvanem@yahoo.no>

snmplib/transports/snmpIPv6BaseDomain.c
@@ -66,7 +66,7 @@
 
 
 #if defined(HAVE_WINSOCK_H) && !defined(mingw32)
-static const struct in6_addr in6addr_any = { } /*IN6ADDR_ANY_INIT*/;
+static const struct in6_addr in6addr_any; /*IN6ADDR_ANY_INIT*/
 #endif
 
 

snmplib/transports/snmpUDPIPv6Domain.c
@@ -33,11 +33,6 @@
 #if HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
 #endif
-
-#if defined(HAVE_WINSOCK_H) && !defined(mingw32)
-static const struct in6_addr in6addr_any = { } /*IN6ADDR_ANY_INIT*/;
-#endif
-
 #if HAVE_NETINET_IN_H
 #include <netinet/in.h>
 #endif