net-snmp/net-snmp

commit 8ba7bc7d1c9ed68cea0f5ab3616904bbbedf51d0

apps: Use getnameinfo() instead of netsnmp_gethostbyaddr() where appropriate

apps/snmpnetstat/inet.c
@@ -599,7 +599,6 @@ inetname(const struct in_addr *inp)
 {
 	char *cp;
 	static char line[50];
-	struct hostent *hp;
 	struct netent *np;
 	static char domain[MAXHOSTNAMELEN];
 	static int first = 1;
@@ -613,44 +612,40 @@ inetname(const struct in_addr *inp)
 		else
 			domain[0] = '\0';
 	}
-	cp = NULL;
-	if (!nflag && inp->s_addr != INADDR_ANY) {
-		int net = inet_netof(*inp);
-		int lna = inet_lnaof(*inp);
-
-		if (lna == INADDR_ANY) {
-			np = getnetbyaddr(net, AF_INET);
+        if (inp->s_addr == INADDR_ANY) {
+                strlcpy(line, "*", sizeof(line));
+        } else {
+                cp = NULL;
+                if (!nflag && inet_lnaof(*inp) == INADDR_ANY) {
+			np = getnetbyaddr(inet_netof(*inp), AF_INET);
 			if (np)
 				cp = np->n_name;
 		}
 		if (cp == NULL) {
-			hp = netsnmp_gethostbyaddr(inp, sizeof (*inp),
-                                                   AF_INET);
-			if (hp) {
-				if ((cp = strchr(hp->h_name, '.')) &&
-				    !strcmp(cp + 1, domain))
-					*cp = '\0';
+                        struct sockaddr_in sin = { };
+                        char host[16];
+
+                        sin.sin_family = AF_INET;
+                        sin.sin_addr = *inp;
+                        if (getnameinfo((struct sockaddr*)&sin, sizeof(sin),
+                                        host, sizeof(host), NULL, 0,
+                                        nflag ? NI_NUMERICHOST : 0) < 0)
+                                strlcpy(host, "?", sizeof(host));
+                        if ((cp = strchr(host, '.')) && !strcmp(cp + 1, domain))
+                                *cp = '\0';
 #if defined (WIN32) || defined (cygwin)
-                                /*
-                                 * Windows insists on returning the computer
-                                 * name for 127.0.0.1 even if the hosts file
-                                 * lists something else such as 'localhost'.
-                                 * If we are trying to look up 127.0.0.1, just
-                                 * return 'localhost'.
-                                 */
-                                if (inp->s_addr == htonl(INADDR_LOOPBACK))
-                                        cp = "localhost";
-                                else
+                        /*
+                         * Windows insists on returning the computer name for
+                         * 127.0.0.1 even if the hosts file lists something
+                         * else such as 'localhost'.  If we are trying to look
+                         * up 127.0.0.1, just return 'localhost'.
+                         */
+                        if (inp->s_addr == htonl(INADDR_LOOPBACK))
+                                strlcpy(host, "localhost", sizeof(host));
 #endif                                                                          
-				cp = hp->h_name;
-			}
-		}
-	}
-	if (inp->s_addr == INADDR_ANY)
-		snprintf(line, sizeof line, "*");
-	else if (cp)
-		snprintf(line, sizeof line, "%s", cp);
-	else
-		strlcpy(line, inet_ntoa(*inp), sizeof(line));
+                        cp = host;
+                }
+		strlcpy(line, cp, sizeof(line));
+        }
 	return line;
 }

apps/snmpnetstat/inet6.c
@@ -451,12 +451,6 @@ inet6name(const struct in6_addr *in6p)
 	static char line[NI_MAXHOST];
 	static char domain[MAXHOSTNAMELEN];
 	static int first = 1;
-#ifdef NETSNMP_ENABLE_IPV6
-	struct hostent *hp;
-	char hbuf[NI_MAXHOST];
-	const int niflag = NI_NUMERICHOST;
-	struct sockaddr_in6 sin6;
-#endif
 
 	if (first && !nflag) {
 		first = 0;
@@ -467,26 +461,14 @@ inet6name(const struct in6_addr *in6p)
 			domain[0] = '\0';
 	}
 #ifdef NETSNMP_ENABLE_IPV6
-	cp = NULL;
-	if (!nflag && !IN6_IS_ADDR_UNSPECIFIED(in6p)) {
-		hp = netsnmp_gethostbyaddr((const char *)in6p, sizeof(*in6p),
-                                           AF_INET6);
-		if (hp) {
-			if ((cp = strchr(hp->h_name, '.')) &&
-			    !strcmp(cp + 1, domain))
-				*cp = 0;
-			cp = hp->h_name;
-		}
-	}
-	if (IN6_IS_ADDR_UNSPECIFIED(in6p))
+	if (IN6_IS_ADDR_UNSPECIFIED(in6p)) {
 		strlcpy(line, "*", sizeof(line));
-	else if (cp)
-		strlcpy(line, cp, sizeof(line));
-	else {
-		memset(&sin6, 0, sizeof(sin6));
-/*		sin6.sin6_len = sizeof(sin6);   */
-		sin6.sin6_family = AF_INET6;
-		sin6.sin6_addr = *in6p;
+        } else {
+                struct sockaddr_in6 sin6 = { };
+                char hbuf[NI_MAXHOST];
+
+                sin6.sin6_family = AF_INET6;
+                sin6.sin6_addr = *in6p;
 #ifdef __KAME__
 		if (IN6_IS_ADDR_LINKLOCAL(in6p) ||
 		    IN6_IS_ADDR_MC_LINKLOCAL(in6p)) {
@@ -497,9 +479,12 @@ inet6name(const struct in6_addr *in6p)
 		}
 #endif
 		if (getnameinfo((struct sockaddr *)&sin6, sizeof(sin6),
-		    hbuf, sizeof(hbuf), NULL, 0, niflag) != 0)
-			strlcpy(hbuf, "?", sizeof hbuf);
-		strlcpy(line, hbuf, sizeof(line));
+                                hbuf, sizeof(hbuf), NULL, 0,
+                                nflag ? NI_NUMERICHOST : 0) < 0)
+                        strlcpy(hbuf, "?", sizeof(hbuf));
+                if ((cp = strchr(hbuf, '.')) && strcmp(cp + 1, domain) == 0)
+                        *cp = 0;
+                strlcpy(line, hbuf, sizeof(line));
 	}
 #else
 	strlcpy(line, "[[XXX - inet6 address]]", sizeof(line));

apps/snmpnetstat/route.c
@@ -392,40 +392,31 @@ routename(in_addr_t in)
 {
 	char *cp;
 	static char line[MAXHOSTNAMELEN];
-	struct hostent *hp;
+	char host[MAXHOSTNAMELEN];
 	static char domain[MAXHOSTNAMELEN];
 	static int first = 1;
+        struct sockaddr_in sin = { };
 
 	if (first) {
 		first = 0;
 		if (gethostname(line, sizeof line) == 0 &&
 		    (cp = strchr(line, '.')))
-			(void) strlcpy(domain, cp + 1, sizeof domain);
+			strlcpy(domain, cp + 1, sizeof domain);
 		else
 			domain[0] = '\0';
 	}
-	cp = NULL;
-	if (!nflag) {
-		hp = netsnmp_gethostbyaddr((char *)&in, sizeof (struct in_addr),
-		    AF_INET);
-		if (hp) {
-			if ((cp = strchr(hp->h_name, '.')) &&
-			    !strcmp(cp + 1, domain))
-				*cp = '\0';
-			cp = hp->h_name;
-		}
-	}
-	if (cp) {
-		strlcpy(line, cp, sizeof(line));
-	} else {
-#define C(x)	(unsigned)((x) & 0xff)
-		in = ntohl(in);
-		snprintf(line, sizeof line, "%u.%u.%u.%u",
-		    C(in >> 24), C(in >> 16), C(in >> 8), C(in));
-	}
-	return (line);
+        sin.sin_family = AF_INET;
+        sin.sin_addr.s_addr = in;
+        if (getnameinfo((struct sockaddr *)&sin, sizeof(sin), host,
+                        sizeof(host), NULL, 0, nflag ? NI_NUMERICHOST : 0) < 0)
+            strlcpy(host, "?", sizeof(host));
+        if ((cp = strchr(host, '.')) && strcmp(cp + 1, domain) == 0)
+            *cp = '\0';
+        strlcpy(line, host, sizeof(line));
+	return line;
 }
 
+#define C(x) ((x) & 0xffU)
 /*
  * Return the name of the network whose address is given.
  * The address is assumed to be that of a net or subnet, not a host.

apps/snmpnetstat/routex.c
@@ -456,55 +456,29 @@ netxname(struct sockaddr_storage *in, int mask)
 }
 
 static char *
-routexname(struct sockaddr_storage *in)
+routexname(const struct sockaddr_storage *in)
 {
     char *cp;
     static char line[MAXHOSTNAMELEN];
-    struct hostent *hp = NULL;
+    char name[MAXHOSTNAMELEN];
     static char domain[MAXHOSTNAMELEN];
     static int first = 1;
-    struct sockaddr_in *sin = (struct sockaddr_in *)in;
-    struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)in;
+    const struct sockaddr *sa = (const void *)in;
 
     if (first) {
         first = 0;
-        if (gethostname(line, sizeof line) == 0 &&
-            (cp = strchr(line, '.')))
-            (void) strlcpy(domain, cp + 1, sizeof domain);
+        if (gethostname(line, sizeof(line)) == 0 && (cp = strchr(line, '.')))
+            strlcpy(domain, cp + 1, sizeof(domain));
         else
             domain[0] = '\0';
     }
-    cp = NULL;
-    if (!nflag) {
-        switch (in->ss_family) {
-        case AF_INET:
-            hp = netsnmp_gethostbyaddr(&sin->sin_addr,
-                    sizeof (struct in_addr), AF_INET);
-            break;
-        case AF_INET6:
-            hp = netsnmp_gethostbyaddr(&sin6->sin6_addr,
-                    sizeof (struct in6_addr), AF_INET6);
-            break;
-        }
-        if (hp) {
-            if ((cp = strchr(hp->h_name, '.')) && !strcmp(cp + 1, domain))
-                *cp = '\0';
-            cp = hp->h_name;
-        }
-    }
-    if (cp) {
-        strlcpy(line, cp, sizeof(line));
-    } else {
-        switch (in->ss_family) {
-        case AF_INET:
-            inet_ntop(sin->sin_family, &sin->sin_addr, line, sizeof(line));
-            break;
-        case AF_INET6:
-            inet_ntop(sin6->sin6_family, &sin6->sin6_addr, line, sizeof(line));
-            break;
-        }
-    }
-    return (line);
+    if (getnameinfo(sa, netsnmp_sockaddr_size(sa), name, sizeof(name),
+                    NULL, 0, nflag ? NI_NUMERICHOST : 0) < 0)
+        strlcpy(name, "?", sizeof(name));
+    if ((cp = strchr(name, '.')) && strcmp(cp + 1, domain) == 0)
+        *cp = '\0';
+    strlcpy(line, name, sizeof(line));
+    return line;
 }
 
 

apps/snmptrapd_log.c
@@ -587,6 +587,19 @@ realloc_handle_time_fmt(u_char ** buf, size_t * buf_len, size_t * out_len,
                                    (u_char **) & safe_bfr, options);
 }
 
+static
+void convert_agent_addr(struct in_addr agent_addr, char *name, size_t size)
+{
+    const int numeric = !netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID,
+                                                NETSNMP_DS_APP_NUMERIC_IP);
+    struct sockaddr_in sin = { };
+
+    sin.sin_family = AF_INET;
+    sin.sin_addr = agent_addr;
+    if (getnameinfo((struct sockaddr *)&sin, sizeof(sin), name, size, NULL, 0,
+                    numeric ? NI_NUMERICHOST : 0) < 0)
+        strlcpy(name, "?", sizeof(size));
+}
 
 static int
 realloc_handle_ip_fmt(u_char ** buf, size_t * buf_len, size_t * out_len,
@@ -609,7 +622,7 @@ realloc_handle_ip_fmt(u_char ** buf, size_t * buf_len, size_t * out_len,
       */
 {
     struct in_addr *agent_inaddr = (struct in_addr *) pdu->agent_addr;
-    struct hostent *host = NULL;       /* corresponding host name */
+    char            host[16];                   /* corresponding host name */
     char            fmt_cmd = options->cmd;     /* what we're formatting */
     u_char         *temp_buf = NULL;
     size_t          temp_buf_len = 64, temp_out_len = 0;
@@ -642,26 +655,14 @@ realloc_handle_ip_fmt(u_char ** buf, size_t * buf_len, size_t * out_len,
          * Try to resolve the agent_addr field as a hostname; fall back
          * to numerical address.  
          */
-        if (!netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID, 
-                                    NETSNMP_DS_APP_NUMERIC_IP)) {
-            host = netsnmp_gethostbyaddr((char *) pdu->agent_addr, 4, AF_INET);
-        }
-        if (host != NULL) {
-            if (!snmp_strcat(&temp_buf, &temp_buf_len, &temp_out_len, 1,
-                             (const u_char *)host->h_name)) {
-                if (temp_buf != NULL) {
-                    free(temp_buf);
-                }
-                return 0;
+        convert_agent_addr(*(struct in_addr *)pdu->agent_addr,
+                           host, sizeof(host));
+        if (!snmp_strcat(&temp_buf, &temp_buf_len, &temp_out_len, 1,
+                         (const u_char *)host)) {
+            if (temp_buf != NULL) {
+                free(temp_buf);
             }
-        } else {
-            if (!snmp_strcat(&temp_buf, &temp_buf_len, &temp_out_len, 1,
-                             (u_char *)inet_ntoa(*agent_inaddr))) {
-                if (temp_buf != NULL) {
-                    free(temp_buf);
-                }
-                return 0;
-            }
+            return 0;
         }
         break;
 
@@ -1335,7 +1336,7 @@ realloc_format_plain_trap(u_char ** buf, size_t * buf_len,
     struct tm      *now_parsed; /* time in struct format */
     char            safe_bfr[200];      /* holds other strings */
     struct in_addr *agent_inaddr = (struct in_addr *) pdu->agent_addr;
-    struct hostent *host = NULL;       /* host name */
+    char host[16];                      /* host name */
     netsnmp_variable_list *vars;        /* variables assoc with trap */
 
     if (buf == NULL) {
@@ -1362,36 +1363,18 @@ realloc_format_plain_trap(u_char ** buf, size_t * buf_len,
     /*
      * Get info about the sender.  
      */
-    if (!netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID, 
-                                NETSNMP_DS_APP_NUMERIC_IP)) {
-        host = netsnmp_gethostbyaddr((char *) pdu->agent_addr, 4, AF_INET);
-    }
-    if (host != (struct hostent *) NULL) {
-        if (!snmp_strcat
-            (buf, buf_len, out_len, allow_realloc,
-             (const u_char *) host->h_name)) {
-            return 0;
-        }
-        if (!snmp_strcat
-            (buf, buf_len, out_len, allow_realloc,
-             (const u_char *) " [")) {
-            return 0;
-        }
-        if (!snmp_strcat(buf, buf_len, out_len, allow_realloc,
-                         (const u_char *) inet_ntoa(*agent_inaddr))) {
-            return 0;
-        }
-        if (!snmp_strcat
-            (buf, buf_len, out_len, allow_realloc,
-             (const u_char *) "] ")) {
-            return 0;
-        }
-    } else {
-        if (!snmp_strcat(buf, buf_len, out_len, allow_realloc,
-                         (const u_char *) inet_ntoa(*agent_inaddr))) {
-            return 0;
-        }
-    }
+    convert_agent_addr(*(struct in_addr *)pdu->agent_addr, host, sizeof(host));
+    if (!snmp_strcat(buf, buf_len, out_len, allow_realloc, (u_char *)host))
+        return 0;
+    if (!snmp_strcat(buf, buf_len, out_len, allow_realloc,
+                      (const u_char *)" ["))
+        return 0;
+    if (!snmp_strcat(buf, buf_len, out_len, allow_realloc,
+                     (u_char *)inet_ntoa(*agent_inaddr)))
+        return 0;
+    if (!snmp_strcat(buf, buf_len, out_len, allow_realloc,
+                     (const u_char *)"] "))
+        return 0;
 
     /*
      * Append PDU transport info.  

commit af3d273e79d638f93c0c1b7b90f0c0155ac81007

apps: Fix MSVC build
Fixes: 8ba7bc7d1c9e ("apps: Use getnameinfo() instead of netsnmp_gethostbyaddr() where appropriate")

apps/snmpnetstat/inet.c
@@ -622,9 +622,10 @@ inetname(const struct in_addr *inp)
 				cp = np->n_name;
 		}
 		if (cp == NULL) {
-                        struct sockaddr_in sin = { };
+                        struct sockaddr_in sin;
                         char host[16];
 
+                        memset(&sin, 0, sizeof(sin));
                         sin.sin_family = AF_INET;
                         sin.sin_addr = *inp;
                         if (getnameinfo((struct sockaddr*)&sin, sizeof(sin),

apps/snmpnetstat/inet6.c
@@ -464,9 +464,10 @@ inet6name(const struct in6_addr *in6p)
 	if (IN6_IS_ADDR_UNSPECIFIED(in6p)) {
 		strlcpy(line, "*", sizeof(line));
         } else {
-                struct sockaddr_in6 sin6 = { };
+                struct sockaddr_in6 sin6;
                 char hbuf[NI_MAXHOST];
 
+                memset(&sin6, 0, sizeof(sin6));
                 sin6.sin6_family = AF_INET6;
                 sin6.sin6_addr = *in6p;
 #ifdef __KAME__

apps/snmpnetstat/route.c
@@ -395,8 +395,9 @@ routename(in_addr_t in)
 	char host[MAXHOSTNAMELEN];
 	static char domain[MAXHOSTNAMELEN];
 	static int first = 1;
-        struct sockaddr_in sin = { };
+        struct sockaddr_in sin;
 
+        memset(&sin, 0, sizeof(sin));
 	if (first) {
 		first = 0;
 		if (gethostname(line, sizeof line) == 0 &&

apps/snmpnetstat/routex.c
@@ -517,8 +517,9 @@ p_rtnodex( struct route_entry *rp )
     }
     else if (rp->af == AF_INET6) {
         struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)&rp->dst;
-        struct in6_addr in6_addr_any = { } /*IN6ADDR_ANY_INIT*/;
+        struct in6_addr in6_addr_any;
 
+        memset(&in6_addr_any, 0, sizeof(in6_addr_any)); /*IN6ADDR_ANY_INIT*/
         printf("%-*s ",
             WID_DST(AF_INET6),
                 memcmp(&sin6->sin6_addr, &in6_addr_any, sizeof(in6_addr_any)) == 0 ? "default" :

apps/snmptrapd_log.c
@@ -592,8 +592,9 @@ void convert_agent_addr(struct in_addr agent_addr, char *name, size_t size)
 {
     const int numeric = !netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID,
                                                 NETSNMP_DS_APP_NUMERIC_IP);
-    struct sockaddr_in sin = { };
+    struct sockaddr_in sin;
 
+    memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_addr = agent_addr;
     if (getnameinfo((struct sockaddr *)&sin, sizeof(sin), name, size, NULL, 0,

include/net-snmp/library/snmp_transport.h
@@ -267,6 +267,7 @@ int netsnmp_transport_add_to_list(netsnmp_transport_list **transport_list,
 				  netsnmp_transport *transport);
 int netsnmp_transport_remove_from_list(netsnmp_transport_list **transport_list,
 				       netsnmp_transport *transport);
+NETSNMP_IMPORT
 int netsnmp_sockaddr_size(const struct sockaddr *sa);
 
 