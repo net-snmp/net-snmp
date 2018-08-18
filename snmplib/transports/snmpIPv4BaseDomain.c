532071bb40738d7cd5b2994dcc13086dd0eca438
Bart Van Assche <bvanassche@acm.org>
2018-08-18 15:42:45 -0700

    snmplib, IPv4 transport: Move documentation from .h to .c
    This change makes it more likely that implementation and documentation
    will remain in sync.

--- snmplib/transports/snmpIPv4BaseDomain.c
+++ snmplib/transports/snmpIPv4BaseDomain.c
@@ -43,15 +43,28 @@
 #define INADDR_NONE     -1
 #endif
 
+/**
+ * Convert a "traditional" peername into a sockaddr_in structure which is
+ * written to *addr.
+ *
+ * @return 1 if the conversion was successful, or 0 if it failed.
+ */
 int
 netsnmp_sockaddr_in(struct sockaddr_in *addr,
                     const char *inpeername, int remote_port)
 {
     char buf[sizeof(int) * 3 + 2];
+
     sprintf(buf, ":%u", remote_port);
     return netsnmp_sockaddr_in2(addr, inpeername, remote_port ? buf : NULL);
 }
 
+/**
+ * Convert a "traditional" peername into a sockaddr_in structure which is
+ * written to *addr.
+ *
+ * @return 1 if the conversion was successful, or 0 if it failed.
+ */
 int
 netsnmp_sockaddr_in2(struct sockaddr_in *addr,
                      const char *inpeername, const char *default_target)
