net-snmp/net-snmp

commit 5d8372341594b5a7e0ba1b6ec76463bf6d926bbe

shared transport + udp shared domain
- refactor udp ipv4/6 base; break into smaller functions & export them
- add transport cache
- add tspec create for UDPDomain
- update tspec struct to use generic flags instead of just 'local'

Signed-off-by: Robert Story <rstory@freesnmp.com>

snmplib/snmp_transport.c
@@ -67,6 +67,9 @@
 #ifdef NETSNMP_TRANSPORT_TCPIPV6_DOMAIN
 #include <net-snmp/library/snmpTCPIPv6Domain.h>
 #endif
+#ifdef NETSNMP_TRANSPORT_UDPSHARED_DOMAIN
+#include <net-snmp/library/snmpUDPsharedDomain.h>
+#endif
 #include <net-snmp/library/snmp_api.h>
 #include <net-snmp/library/snmp_service.h>
 #include <net-snmp/library/read_config.h>
@@ -76,6 +79,7 @@ netsnmp_feature_child_of(transport_all, libnetsnmp)
 netsnmp_feature_child_of(tdomain_support, transport_all)
 netsnmp_feature_child_of(tdomain_transport_oid, transport_all)
 netsnmp_feature_child_of(sockaddr_size, transport_all)
+netsnmp_feature_child_of(transport_cache, transport_all)
 
 /*
  * Our list of supported transport domains.  
@@ -100,6 +104,7 @@ size_t          netsnmpDDPDomain_len = OID_LENGTH(netsnmpDDPDomain);
 oid             netsnmpIPXDomain[] = { 1, 3, 6, 1, 6, 1, 5 };
 size_t          netsnmpIPXDomain_len = OID_LENGTH(netsnmpIPXDomain);
 
+static netsnmp_container *_container = NULL;
 
 
 static void     netsnmp_tdomain_dump(void);
@@ -205,6 +210,12 @@ netsnmp_transport_free(netsnmp_transport *t)
 {
     if (NULL == t)
         return;
+
+#ifndef FEATURE_REMOVE_TRANSPORT_CACHE
+    /** don't free a transport that is currently shared */
+    if (netsnmp_transport_cache_remove(t) == 1)
+        return;
+#endif
 
     SNMP_FREE(t->local);
     SNMP_FREE(t->remote);
@@ -502,7 +513,7 @@ netsnmp_tdomain_transport_tspec(netsnmp_tdomain_spec *tspec)
 
     application = tspec->application;
     str = tspec->target;
-    local = tspec->local;
+    local = tspec->flags & NETSNMP_TSPEC_LOCAL;
     default_domain = tspec->default_domain;
     default_target = tspec->default_target;
     source = tspec->source;
@@ -706,7 +717,8 @@ netsnmp_tdomain_transport_full(const char *application,
     memset(&tspec, 0x0, sizeof(tspec));
     tspec.application = application;
     tspec.target = str;
-    tspec.local = local;
+    if (local)
+        tspec.flags |= NETSNMP_TSPEC_LOCAL;
     tspec.default_domain = default_domain;
     tspec.default_target = default_target;
     tspec.source = NULL;
@@ -722,7 +734,8 @@ netsnmp_tdomain_transport(const char *str, int local,
     memset(&tspec, 0x0, sizeof(tspec));
     tspec.application = "snmp";
     tspec.target = str;
-    tspec.local = local;
+    if (local)
+        tspec.flags |= NETSNMP_TSPEC_LOCAL;
     tspec.default_domain = default_domain;
     tspec.default_target = NULL;
     tspec.source = NULL;
@@ -836,3 +849,349 @@ netsnmp_transport_create_config(char *key, char *value) {
     entry->value = strdup(value);
     return entry;
 }
+
+#ifndef FEATURE_REMOVE_TRANSPORT_CACHE
+
+/* *************************************************************************
+ * transport caching by address family, type and use
+ */
+typedef struct trans_cache_s {
+    netsnmp_transport *t;
+    int af;
+    int type;
+    int local;
+    netsnmp_sockaddr_storage bind_addr;
+    int count; /* number of times this transport has been returned */
+} trans_cache;
+
+static void _tc_free_item(trans_cache *tc, void *context);
+static int _tc_compare(trans_cache *lhs, trans_cache *rhs);
+
+/** initialize transport cache */
+static int
+_tc_init(void)
+{
+    DEBUGMSGTL(("transport:cache:init", "%p\n", _container));
+
+    /** prevent double init */
+    if (NULL != _container)
+        return 0;
+
+    _container = netsnmp_container_find("trans_cache:binary_array");
+    if (NULL == _container) {
+        snmp_log(LOG_ERR, "failed to allocate trans_cache container\n");
+        return 1;
+    }
+
+    _container->container_name = strdup("trans_cache");
+    _container->free_item = (netsnmp_container_obj_func*) _tc_free_item;
+    _container->compare = (netsnmp_container_compare*) _tc_compare;
+
+    return 0;
+}
+
+/*
+ * container compare function
+ *
+ * sort by af, type, local
+ */
+static int
+_tc_compare(trans_cache *lhs, trans_cache *rhs)
+{
+    netsnmp_assert((lhs != NULL) && (rhs != NULL));
+
+    DEBUGMSGTL(("9:transport:cache:compare", "%p/%p\n", lhs, rhs));
+
+   if (lhs->af < rhs->af)
+        return -1;
+    else if (lhs->af > rhs->af)
+        return 1;
+
+    if (lhs->type < rhs->type)
+        return -1;
+    else if (lhs->type > rhs->type)
+        return 1;
+
+    if (lhs->local < rhs->local)
+        return -1;
+    else if (lhs->local > rhs->local)
+        return 1;
+
+    if (AF_INET == lhs->af) {
+        struct sockaddr_in *lha = &lhs->bind_addr.sin,
+            *rha = &rhs->bind_addr.sin;
+        if (lha->sin_addr.s_addr < rha->sin_addr.s_addr)
+            return -1;
+        else if (lha->sin_addr.s_addr > rha->sin_addr.s_addr)
+            return 1;
+
+        if (lha->sin_port < rha->sin_port)
+            return -1;
+        else if (lha->sin_port > rha->sin_port)
+            return 1;
+    } else if (AF_INET6 == lhs->af) {
+        struct sockaddr_in6 *lha = &lhs->bind_addr.sin6,
+            *rha = &rhs->bind_addr.sin6;
+        int rc = memcmp(lha->sin6_addr.s6_addr, rha->sin6_addr.s6_addr,
+                        sizeof(rha->sin6_addr.s6_addr));
+        if (rc)
+            return rc;
+
+        if (lha->sin6_port < rha->sin6_port)
+            return -1;
+        else if (lha->sin6_port > rha->sin6_port)
+            return 1;
+
+        if (lha->sin6_flowinfo < rha->sin6_flowinfo)
+            return -1;
+        else if (lha->sin6_flowinfo > rha->sin6_flowinfo)
+            return 1;
+
+        if (lha->sin6_scope_id < rha->sin6_scope_id)
+            return -1;
+        else if (lha->sin6_scope_id > rha->sin6_scope_id)
+            return 1;
+    }
+    return 0;
+}
+
+static void
+_tc_free(trans_cache *tc)
+{
+    if (NULL == tc)
+        return;
+
+    DEBUGMSGTL(("transport:cache:free", "%p %d/%d/%d/%p %d\n", tc, tc->af,
+                tc->type, tc->local, tc->t, tc->count));
+    netsnmp_transport_free(tc->t);
+    memset(tc, 0x0, sizeof(*tc));
+    free(tc);
+}
+
+static void
+_tc_free_item(trans_cache *tc, void *context)
+{
+    _tc_free(tc);
+}
+
+static void
+_tc_remove(trans_cache *tc)
+{
+    if (NULL == tc || NULL == _container)
+        return;
+
+    DEBUGMSGTL(("transport:cache:remove", "%p\n", tc));
+
+    CONTAINER_REMOVE(_container, tc);
+}
+
+static trans_cache *
+_tc_create(int af, int type, int local, netsnmp_sockaddr_storage *addr,
+           netsnmp_transport *t)
+{
+    trans_cache *tc = SNMP_MALLOC_TYPEDEF(trans_cache);
+    if (NULL == tc) {
+        snmp_log(LOG_ERR, "failed to allocate trans_cache\n");
+        return NULL;
+    }
+    DEBUGMSGTL(("transport:cache:create", "%p\n", tc));
+    tc->af = af;
+    tc->type = type;
+    tc->local = local;
+    tc->t = t;
+    if (addr)
+        memcpy(&tc->bind_addr, addr, sizeof(tc->bind_addr));
+    /** we only understand ipv6 and ipv6 sockaddrs in compare */
+    if (AF_INET != tc->af && AF_INET6 != tc->af)
+        NETSNMP_LOGONCE((LOG_WARNING, "transport cache not tested for af %d\n",
+                         tc->af));
+    return tc;
+}
+
+static trans_cache *
+_tc_add(int af, int type, int local, netsnmp_sockaddr_storage *addr,
+        netsnmp_transport *t)
+{
+    trans_cache *tc;
+    int rc;
+
+    DEBUGMSGTL(("transport:cache:add", "%d/%d/%d/%p\n", af, type, local, t));
+
+    if (NULL == _container) {
+        _tc_init();
+        if (NULL == _container)
+            return NULL;
+    }
+
+    tc = _tc_create(af, type, local, addr, t);
+    if (NULL == tc) {
+        DEBUGMSGTL(("transport:cache:add",
+                    "could not create transport cache\n"));
+        return NULL;
+    }
+
+    rc = CONTAINER_INSERT(_container, tc);
+    if (rc) {
+        DEBUGMSGTL(("transport:cache:add", "container insert failed\n"));
+        _tc_free(tc);
+        return NULL;
+    }
+
+    return tc;
+}
+
+trans_cache *
+_tc_find(int af, int type, int local, netsnmp_sockaddr_storage *addr)
+{
+    trans_cache tc, *rtn;
+
+    DEBUGMSGTL(("transport:cache:find", "%d/%d/%d\n", af, type, local));
+
+    if (NULL == _container)
+        return NULL;
+
+    memset(&tc, 0x00, sizeof(tc));
+    tc.af = af;
+    tc.type = type;
+    tc.local = local;
+    if (addr)
+        memcpy(&tc.bind_addr, addr, sizeof(tc.bind_addr));
+
+    rtn = CONTAINER_FIND(_container, &tc);
+    DEBUGMSGTL(("transport:cache:find", "%p\n", rtn));
+    return rtn;
+}
+
+trans_cache *
+_tc_find_transport(netsnmp_transport *t)
+{
+    /*
+     * we shouldn't really have that many transports, so instead of
+     * using an addition key, just iterate over the whole container.
+     */
+    netsnmp_iterator  *itr;
+    trans_cache *tc;
+
+    DEBUGMSGTL(("transport:cache:find_transport", "%p\n", t));
+
+    if (NULL == _container)
+        return NULL;
+
+    itr = CONTAINER_ITERATOR(_container);
+    if (NULL == itr) {
+        snmp_log(LOG_ERR, "could not get iterator for transport cache\n");
+        return NULL;
+    }
+
+    tc = ITERATOR_FIRST(itr);
+    for( ; tc; tc = ITERATOR_NEXT(itr))
+        if (tc->t == t)
+            break;
+    ITERATOR_RELEASE(itr);
+
+    DEBUGMSGT(("transport:cache:find_transport","found %p\n", tc));
+
+    return tc;
+}
+
+int
+netsnmp_transport_cache_remove(netsnmp_transport *t)
+{
+    trans_cache *tc;
+
+    DEBUGMSGTL(("transport:cache:close", "%p\n", t));
+
+    if (NULL == t)
+        return 0;
+
+    /** transport in cache? */
+    tc = _tc_find_transport(t);
+    if (NULL == tc) {
+        DEBUGMSGTL(("transport:cache:close", "%p not found in cache\n", t));
+        return 0;
+    }
+
+    --tc->count;
+
+    /** still in use? */
+    if (tc->count > 0) {
+        DEBUGMSGTL(("transport:cache:close", "still %d user(s) of %p\n",
+                    tc->count, t));
+        return 1;
+    }
+
+    /** unbalanced get/close? */
+    if (tc->count < 0)
+        snmp_log(LOG_WARNING, "transport cache get/close mismatch\n");
+
+    _tc_remove(tc);
+    _tc_free(tc); /* also does close */
+
+    return 0;
+}
+
+/*
+ * netsnmp_transport_get: get a (possibly duplicate, cached) transport
+ */
+netsnmp_transport *
+netsnmp_transport_cache_get(int af, int type, int local,
+                            netsnmp_sockaddr_storage *bind_addr)
+{
+    trans_cache       *tc;
+    netsnmp_transport *t;
+
+    DEBUGMSGTL(("transport:cache:get", "%d/%d/%d\n", af, type, local));
+
+#define USE_CACHE 1
+
+#if USE_CACHE
+    /** check for existing transport */
+    tc = _tc_find(af, type, local, bind_addr);
+    if (tc) {
+        DEBUGMSGTL(("transport:cache:get", "using existing transport %p\n",
+                    tc->t));
+        ++tc->count;
+        return tc->t;
+    }
+#endif
+    /** get transport */
+    t = NULL; /* _transport(af, type, 0);*/
+    if (NULL == t) {
+        snmp_log(LOG_ERR, "could not get new transport for %d/%d/%d\n", af,
+                 type, local);
+        return NULL;
+    }
+    DEBUGMSGTL(("transport:cache:get", "new transport %p\n", t));
+
+#if USE_CACHE
+    /** create transport cache for new transport */
+    tc = _tc_add(af, type, local, bind_addr, t);
+    if (NULL == tc) {
+        DEBUGMSGTL(("transport:cache:get", "could not create transport cache\n"));
+        /*
+         * hmmm.. this isn't really a critical error, is it? We have a
+         * transport, just no cache for it. Let's continue on and hope for the
+         * best.
+         */
+        /** return -1; */
+    }
+    tc->count = 1;
+#endif
+
+    return t;
+}
+
+int
+netsnmp_transport_cache_save(int af, int type, int local,
+                             netsnmp_sockaddr_storage *addr,
+                             netsnmp_transport *t)
+{
+    if (NULL == t)
+        return 1;
+
+    if (NULL == _tc_add(af, type, local, addr, t))
+        return 1;
+
+    return 0;
+}
+#endif /* FEATURE_REMOVE_TRANSPORT_CACHE */

agent/agent_trap.c
@@ -265,7 +265,6 @@ create_trap_session2(const char *sink, const char* sinkport,
         snprintf(tmp, sizeof(tmp)-1,"%s:%s", sink, sinkport);
         tspec.target = tmp;
     }
-    tspec.local = 0;
     tspec.default_domain = NULL;
     tspec.default_target = sinkport;
     t = netsnmp_tdomain_transport_tspec(&tspec);

configure
@@ -1837,6 +1837,9 @@ Miscellaneous:
                 This transport is available for Linux, Solaris and
                 FreeBSD at least.
                 This transport is compiled in by default if IPv6 support is enabled.
+    UDPshared   Allows a UDP port to be shared with multiple transports. It
+                enables multiple notification destinations to share a single
+                source address/port.
     TCPIPv6     support for SNMP over UDP/IPv6.
                 This transport is available for Linux, Solaris and
                 FreeBSD at least.

configure.d/config_net_snmp_config_h
@@ -346,6 +346,10 @@ AH_BOTTOM([/* end of definitions added by configure on-the-fly */
     available.   */
 #undef NETSNMP_TRANSPORT_UDP_DOMAIN
 
+/*  This is defined if support for the shared UDP/IP transport domain is
+    available.   */
+#undef NETSNMP_TRANSPORT_UDPSHARED_DOMAIN
+
 /*  This is defined if support for the "callback" transport domain is
     available.   */
 #undef NETSNMP_TRANSPORT_CALLBACK_DOMAIN

configure.d/config_project_with_enable
@@ -546,6 +546,9 @@ AC_ARG_WITH(out_transports,
                 This transport is available for Linux, Solaris and
                 FreeBSD at least.
                 This transport is compiled in by default if IPv6 support is enabled.
+    UDPshared   Allows a UDP port to be shared with multiple transports. It
+                enables multiple notification destinations to share a single
+                source address/port.
     TCPIPv6     support for SNMP over UDP/IPv6.
                 This transport is available for Linux, Solaris and
                 FreeBSD at least.

include/net-snmp/library/snmpUDPDomain.h
@@ -18,7 +18,16 @@ extern          "C" {
 config_require(UDPIPv4Base)
 #include <net-snmp/library/snmpUDPIPv4BaseDomain.h>
 
+NETSNMP_IMPORT
 netsnmp_transport *netsnmp_udp_transport(struct sockaddr_in *addr, int local);
+
+NETSNMP_IMPORT
+netsnmp_transport *netsnmp_udp_create_tspec(netsnmp_tdomain_spec *tspec);
+
+NETSNMP_IMPORT
+netsnmp_transport *
+netsnmp_udp_transport_with_source(struct sockaddr_in *addr, int local,
+                                  struct sockaddr_in *src_addr);
 
 #define C2SE_ERR_SUCCESS             0
 #define C2SE_ERR_MISSING_ARG        -1
@@ -42,7 +51,6 @@ void        netsnmp_udp_com2Sec_free(com2SecEntry *e);
 
 NETSNMP_IMPORT
 int         netsnmp_udp_com2SecList_remove(com2SecEntry *e);
-
 
 /*
  * Register any configuration tokens specific to the agent.  

include/net-snmp/library/snmpUDPIPv4BaseDomain.h
@@ -32,7 +32,24 @@ extern          "C" {
                                               int local,
                                               struct sockaddr_in *src_addr);
 
-#if defined(HAVE_IP_PKTINFO) || defined(HAVE_IP_RECVDSTADDR)
+    netsnmp_transport *
+    netsnmp_udpipv4base_tspec_transport(netsnmp_tdomain_spec *tspec);
+
+    /** internal functions for derivatives of udpipv4base */
+    netsnmp_transport *
+    netsnmp_udpipv4base_transport_init(struct sockaddr_in *addr, int local);
+
+    int
+    netsnmp_udpipv4base_transport_socket(int flags);
+
+    int
+    netsnmp_udpipv4base_transport_bind(netsnmp_transport *t,
+                                       struct sockaddr_in *addr, int flags);
+
+    void
+    netsnmp_udpipv4base_transport_get_bound_addr(netsnmp_transport *t);
+
+#if defined(IP_PKTINFO) || defined(IP_RECVDSTADDR)
     int netsnmp_udpipv4_recvfrom(int s, void *buf, int len,
                                  struct sockaddr *from, socklen_t *fromlen,
                                  struct sockaddr *dstip, socklen_t *dstlen,

include/net-snmp/library/snmpUDPIPv6Domain.h
@@ -25,6 +25,25 @@ NETSNMP_IMPORT oid      netsnmp_UDPIPv6Domain[];
 netsnmp_transport *netsnmp_udp6_transport(struct sockaddr_in6 *addr,
                                           int local);
 
+netsnmp_transport *
+netsnmp_udp6_transport_with_source(struct sockaddr_in6 *addr, int local,
+                                   struct sockaddr_in6 *src_addr);
+
+    /** internal functions for derivatives of udpipv6 */
+
+    netsnmp_transport *
+    netsnmp_udp6_transport_init(struct sockaddr_in6 *addr, int local);
+
+    int
+    netsnmp_udp6_transport_socket(int flags);
+
+    int
+    netsnmp_udp6_transport_bind(netsnmp_transport *t,
+                                struct sockaddr_in6 *addr, int flags);
+
+    void
+    netsnmp_udp6_transport_get_bound_addr(netsnmp_transport *t);
+
 
 /*
  * Convert a "traditional" peername into a sockaddr_in6 structure which is

commit 3ec15e836445dd85dc090bf0b40470d9193e0e0d

snmplib: Avoid dereferencing a NULL pointer if creating a cache entry fails
Fixes: 5d8372341594 ("shared transport + udp shared domain")

snmplib/snmp_transport.c
@@ -1303,13 +1303,12 @@ netsnmp_transport_cache_get(int af, int type, int local,
     /** create transport cache for new transport */
     tc = _tc_add(af, type, local, bind_addr, t);
     if (NULL == tc) {
-        DEBUGMSGTL(("transport:cache:get", "could not create transport cache\n"));
+        DEBUGMSGTL(("transport:cache:get", "could not create transport cache entry\n"));
         /*
-         * hmmm.. this isn't really a critical error, is it? We have a
-         * transport, just no cache for it. Let's continue on and hope for the
-         * best.
+         * We have a transport, just no cache for it. Let's continue on and
+         * hope for the best.
          */
-        /** return -1; */
+        return t;
     }
     tc->count = 1;
 #endif