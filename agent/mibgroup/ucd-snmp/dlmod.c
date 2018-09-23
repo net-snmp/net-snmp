net-snmp/net-snmp

commit 659ffc8d866154beafcfec03d0742d03b1ee2a74

AIX: Fix compiler warnings
Avoid that on AIX the compiler complains about assigning void * to a function
pointer or vice versa.

agent/mibgroup/ucd-snmp/dlmod.c
@@ -232,7 +232,7 @@ dlmod_load_module(struct dlmod *dlm)
         dl_function_ptr dl_init;
 
         snprintf(sym_init, sizeof(sym_init), "init_%s", dlm->name);
-        dl_init = dlmod_dlsym(dlm->handle, sym_init);
+        dl_init = (int (*)(void))dlmod_dlsym(dlm->handle, sym_init);
         if (dl_init == NULL) {
             dlmod_dlclose(dlm->handle);
             free(dlm->error);
@@ -259,10 +259,10 @@ dlmod_unload_module(struct dlmod *dlm)
         return;
 
     snprintf(sym_deinit, sizeof(sym_deinit), "deinit_%s", dlm->name);
-    dl_deinit = dlmod_dlsym(dlm->handle, sym_deinit);
+    dl_deinit = (int (*)(void))dlmod_dlsym(dlm->handle, sym_deinit);
     if (!dl_deinit) {
         snprintf(sym_deinit, sizeof(sym_deinit), "shutdown_%s", dlm->name);
-        dl_deinit = dlmod_dlsym(dlm->handle, sym_deinit);
+        dl_deinit = (int (*)(void))dlmod_dlsym(dlm->handle, sym_deinit);
     }
     if (dl_deinit) {
         DEBUGMSGTL(("dlmod", "Calling %s()\n", sym_deinit));

agent/mibgroup/utilities/iquery.c
@@ -229,7 +229,7 @@ netsnmp_session *netsnmp_iquery_session(char* secName,   int   version,
             ss->community = netsnmp_memdup(secName, strlen(secName));
             ss->community_len = strlen(secName);
         }
-        ss->myvoid = netsnmp_check_outstanding_agent_requests;
+        ss->myvoid = (void *)netsnmp_check_outstanding_agent_requests;
         ss->flags |= SNMP_FLAGS_RESP_CALLBACK | SNMP_FLAGS_DONT_PROBE;
     }
 #endif

commit f4dc13f888ad68df02f6730e0b17d657005d2fe8

MinGW32, ucd-snmp: Fix compiler warnings about assigning incompatible function pointers
Fixes: 659ffc8d8661 ("AIX: Fix compiler warnings")

agent/mibgroup/ucd-snmp/dlmod.c
@@ -232,7 +232,7 @@ dlmod_load_module(struct dlmod *dlm)
         dl_function_ptr dl_init;
 
         snprintf(sym_init, sizeof(sym_init), "init_%s", dlm->name);
-        dl_init = (int (*)(void))dlmod_dlsym(dlm->handle, sym_init);
+        dl_init = (dl_function_ptr)dlmod_dlsym(dlm->handle, sym_init);
         if (dl_init == NULL) {
             dlmod_dlclose(dlm->handle);
             free(dlm->error);
@@ -259,10 +259,10 @@ dlmod_unload_module(struct dlmod *dlm)
         return;
 
     snprintf(sym_deinit, sizeof(sym_deinit), "deinit_%s", dlm->name);
-    dl_deinit = (int (*)(void))dlmod_dlsym(dlm->handle, sym_deinit);
+    dl_deinit = (dl_function_ptr)dlmod_dlsym(dlm->handle, sym_deinit);
     if (!dl_deinit) {
         snprintf(sym_deinit, sizeof(sym_deinit), "shutdown_%s", dlm->name);
-        dl_deinit = (int (*)(void))dlmod_dlsym(dlm->handle, sym_deinit);
+        dl_deinit = (dl_function_ptr)dlmod_dlsym(dlm->handle, sym_deinit);
     }
     if (dl_deinit) {
         DEBUGMSGTL(("dlmod", "Calling %s()\n", sym_deinit));