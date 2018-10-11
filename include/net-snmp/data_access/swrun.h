net-snmp/net-snmp

commit d903d0d67109b4c9ca3fd5e6bd297ab8dd2864ce

ucd-snmp, HOST-MIB: Rework pcre abstraction
In previous versions of the pcre library the pcre typedef was an alias
for struct pcre. In the latest version of that library that typedef is
an alias for struct real_pcre8_or_16. This patch avoids that the
compiler complains about incompatible pointer assignments between these
two structure types.

include/net-snmp/data_access/swrun.h
@@ -90,8 +90,7 @@ extern "C" {
     int  swrun_count_processes_by_name( char *name );
 
 #ifndef NETSNMP_FEATURE_REMOVE_SWRUN_COUNT_PROCESSES_BY_REGEX
-    struct real_pcre;
-    int  swrun_count_processes_by_regex(char *name, struct real_pcre *regexp);
+    int  swrun_count_processes_by_regex(char *name, netsnmp_regex_ptr regexp);
 #endif
 
 #define NETSNMP_SWRUN_NOFLAGS            0x00000000

agent/mibgroup/host/data_access/swrun.c
@@ -102,7 +102,7 @@ swrun_max_processes( void )
 #ifndef NETSNMP_FEATURE_REMOVE_SWRUN_COUNT_PROCESSES_BY_REGEX
 #if HAVE_PCRE_H
 int
-swrun_count_processes_by_regex( char *name, struct real_pcre *regexp )
+swrun_count_processes_by_regex( char *name, netsnmp_regex_ptr regexp )
 {
     netsnmp_swrun_entry *entry;
     netsnmp_iterator  *it;
@@ -112,14 +112,14 @@ swrun_count_processes_by_regex( char *name, struct real_pcre *regexp )
     char fullCommand[64 + 128 + 128 + 3];
 
     netsnmp_cache_check_and_reload(swrun_cache);
-    if ( !swrun_container || !name || !regexp )
+    if ( !swrun_container || !name || !regexp.regex_ptr )
         return 0;    /* or -1 */
 
     it = CONTAINER_ITERATOR( swrun_container );
     while ((entry = (netsnmp_swrun_entry*)ITERATOR_NEXT( it )) != NULL) {
         /* need to assemble full command back so regexps can get full picture */
         sprintf(fullCommand, "%s %s", entry->hrSWRunPath, entry->hrSWRunParameters);
-        found = pcre_exec(regexp, NULL, fullCommand, strlen(fullCommand), 0, 0, found_ndx, 30);
+        found = pcre_exec(regexp.regex_ptr, NULL, fullCommand, strlen(fullCommand), 0, 0, found_ndx, 30);
         if (found > 0) {
             i++;
         }

agent/mibgroup/struct.h
@@ -28,13 +28,6 @@ struct extensible {
 #endif /* USING_SINGLE_COMMON_PASSPERSIST_INSTANCE */
 };
 
-#if HAVE_PCRE_H
-/* Pointer to pcre struct. Abstract pcre native pointer so all *.c files */
-/* do not have to include pcre.h */
-struct real_pcre;
-typedef struct real_pcre *netsnmp_regex_ptr;
-#endif
-
 struct myproc {
     char            name[STRMAX];
 #if HAVE_PCRE_H

agent/mibgroup/ucd-snmp/proc.c
@@ -134,9 +134,7 @@ proc_free_config(void)
         ptmp2 = ptmp;
         ptmp = ptmp->next;
 #if HAVE_PCRE_H
-        if (ptmp2->regexp) {
-            free(ptmp2->regexp);
-        }
+        free(ptmp2->regexp.regex_ptr);
 #endif
         free(ptmp2);
     }
@@ -210,7 +208,7 @@ proc_parse_config(const char *token, char *cptr)
         return;                 /* memory alloc error */
     numprocs++;
 #if HAVE_PCRE_H
-    (*procp)->regexp = NULL;
+    (*procp)->regexp.regex_ptr = NULL;
 #endif
     /*
      * not blank and not a comment 
@@ -229,8 +227,9 @@ proc_parse_config(const char *token, char *cptr)
                 int pcre_error_offset;
 
                 DEBUGMSGTL(("ucd-snmp/regexp_proc", "Loading regex %s\n", cptr));
-                (*procp)->regexp = pcre_compile(cptr, 0,  &pcre_error, &pcre_error_offset, NULL);
-                if ((*procp)->regexp == NULL) {
+                (*procp)->regexp.regex_ptr =
+                    pcre_compile(cptr, 0,  &pcre_error, &pcre_error_offset, NULL);
+                if ((*procp)->regexp.regex_ptr == NULL) {
                     config_perror(pcre_error);
                 }
             }
@@ -392,7 +391,7 @@ sh_count_myprocs(struct myproc *proc)
         return 0;
 
 #if defined(USING_HOST_DATA_ACCESS_SWRUN_MODULE) && defined(HAVE_PCRE_H)
-    if (proc->regexp != NULL)
+    if (proc->regexp.regex_ptr != NULL)
       return sh_count_procs_by_regex(proc->name, proc->regexp);
 #endif
 

include/net-snmp/types.h
@@ -88,6 +88,16 @@ typedef long ssize_t;
 typedef unsigned long int nfds_t;
 #endif
 
+#ifdef HAVE_PCRE_H
+/*
+ * Abstract the pcre typedef such that not all *.c files have to include
+ * <pcre.h>.
+ */
+typedef struct {
+    void *regex_ptr;
+} netsnmp_regex_ptr;
+#endif
+ 
     /*
      *  For the initial release, this will just refer to the
      *  relevant UCD header files.

commit 553a9f9a3d939faab68dd6a76b94d70ef8943e72

Fix the build if <pcre.h> is not present
Fixes: d903d0d67109 ("ucd-snmp, HOST-MIB: Rework pcre abstraction")

include/net-snmp/data_access/swrun.h
@@ -89,7 +89,8 @@ extern "C" {
     int  swrun_max_processes(   void );
     int  swrun_count_processes_by_name( char *name );
 
-#ifndef NETSNMP_FEATURE_REMOVE_SWRUN_COUNT_PROCESSES_BY_REGEX
+#if !defined(NETSNMP_FEATURE_REMOVE_SWRUN_COUNT_PROCESSES_BY_REGEX) \
+    && defined(HAVE_PCRE_H)
     int  swrun_count_processes_by_regex(char *name, netsnmp_regex_ptr regexp);
 #endif
 