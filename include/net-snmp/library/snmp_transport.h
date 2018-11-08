net-snmp/net-snmp

commit 5d8372341594b5a7e0ba1b6ec76463bf6d926bbe

shared transport + udp shared domain
- refactor udp ipv4/6 base; break into smaller functions & export them
- add transport cache
- add tspec create for UDPDomain
- update tspec struct to use generic flags instead of just 'local'

Signed-off-by: Robert Story <rstory@freesnmp.com>

include/net-snmp/library/snmp_transport.h
@@ -48,6 +48,7 @@ extern          "C" {
                                                           TSM tmStateReference */
 #define		NETSNMP_TRANSPORT_FLAG_EMPTY_PKT 0x10
 #define		NETSNMP_TRANSPORT_FLAG_OPENED	 0x20  /* f_open called */
+#define		NETSNMP_TRANSPORT_FLAG_SHARED	 0x40
 #define		NETSNMP_TRANSPORT_FLAG_HOSTNAME	 0x80  /* for fmtaddr hook */
 
 /*  The standard SNMP domains.  */
@@ -106,11 +107,15 @@ typedef struct netsnmp_tmStateReference_s {
    void *otherTransportOpaque; /* XXX: May have mem leak issues */
 } netsnmp_tmStateReference;
 
+#define NETSNMP_TSPEC_LOCAL                     0x01 /* 1=server, 0=client */
+#define NETSNMP_TSPEC_SHARED                    0x02
+#define NETSNMP_TSPEC_NO_DFTL_CLIENT_ADDR       0x04
+
 struct netsnmp_container; /* forward decl */
 typedef struct netsnmp_tdomain_spec_s {
     const char *application;             /* application name */
     const char *target;                  /* target as string */
-    int         local;                   /* 1=local/server, 0=remote/client */
+    u_int       flags;
     const char *default_domain;          /* default domain */
     const char *default_target;          /* default target */
     const char *source;                  /* source as string iff remote */
@@ -262,6 +267,23 @@ netsnmp_transport *netsnmp_transport_copy(netsnmp_transport *t);
 NETSNMP_IMPORT
 void            netsnmp_transport_free(netsnmp_transport *t);
 
+#ifndef FEATURE_REMOVE_TRANSPORT_CACHE
+
+/**  transport cache support */
+
+NETSNMP_IMPORT
+netsnmp_transport *netsnmp_transport_cache_get(int af, int type, int local,
+                                               netsnmp_sockaddr_storage *bind_addr);
+
+NETSNMP_IMPORT
+int                netsnmp_transport_cache_save(int af, int type, int local,
+                                                netsnmp_sockaddr_storage *addr,
+                                                netsnmp_transport *t);
+
+NETSNMP_IMPORT
+int                netsnmp_transport_cache_remove(netsnmp_transport *t);
+
+#endif /* FEATURE_REMOVE_TRANSPORT_CACHE */
 
 /*
  * If the passed oid (in_oid, in_len) corresponds to a supported transport

agent/agent_trap.c
@@ -265,7 +265,6 @@ create_trap_session2(const char *sink, const char* sinkport,
         snprintf(tmp, sizeof(tmp)-1,"%s:%s", sink, sinkport);
         tspec.target = tmp;
     }
-    tspec.local = 0;
     tspec.default_domain = NULL;
     tspec.default_target = sinkport;
     t = netsnmp_tdomain_transport_tspec(&tspec);

configure
@@ -1837,6 +1837,9 @@ Miscellaneous:
                 This transport is available for Linux, Solaris and
                 FreeBSD at least.
                 This transport is compiled in by default if IPv6 support is enabled.
+    UDPshared   Allows a UDP port to be shared with multiple transports. It
+                enables multiple notification destinations to share a single
+                source address/port.
     TCPIPv6     support for SNMP over UDP/IPv6.
                 This transport is available for Linux, Solaris and
                 FreeBSD at least.

configure.d/config_net_snmp_config_h
@@ -346,6 +346,10 @@ AH_BOTTOM([/* end of definitions added by configure on-the-fly */
     available.   */
 #undef NETSNMP_TRANSPORT_UDP_DOMAIN
 
+/*  This is defined if support for the shared UDP/IP transport domain is
+    available.   */
+#undef NETSNMP_TRANSPORT_UDPSHARED_DOMAIN
+
 /*  This is defined if support for the "callback" transport domain is
     available.   */
 #undef NETSNMP_TRANSPORT_CALLBACK_DOMAIN

configure.d/config_project_with_enable
@@ -546,6 +546,9 @@ AC_ARG_WITH(out_transports,
                 This transport is available for Linux, Solaris and
                 FreeBSD at least.
                 This transport is compiled in by default if IPv6 support is enabled.
+    UDPshared   Allows a UDP port to be shared with multiple transports. It
+                enables multiple notification destinations to share a single
+                source address/port.
     TCPIPv6     support for SNMP over UDP/IPv6.
                 This transport is available for Linux, Solaris and
                 FreeBSD at least.

include/net-snmp/library/snmpUDPDomain.h
@@ -18,7 +18,16 @@ extern          "C" {
 config_require(UDPIPv4Base)
 #include <net-snmp/library/snmpUDPIPv4BaseDomain.h>
 
+NETSNMP_IMPORT
 netsnmp_transport *netsnmp_udp_transport(struct sockaddr_in *addr, int local);
+
+NETSNMP_IMPORT
+netsnmp_transport *netsnmp_udp_create_tspec(netsnmp_tdomain_spec *tspec);
+
+NETSNMP_IMPORT
+netsnmp_transport *
+netsnmp_udp_transport_with_source(struct sockaddr_in *addr, int local,
+                                  struct sockaddr_in *src_addr);
 
 #define C2SE_ERR_SUCCESS             0
 #define C2SE_ERR_MISSING_ARG        -1
@@ -42,7 +51,6 @@ void        netsnmp_udp_com2Sec_free(com2SecEntry *e);
 
 NETSNMP_IMPORT
 int         netsnmp_udp_com2SecList_remove(com2SecEntry *e);
-
 
 /*
  * Register any configuration tokens specific to the agent.  

include/net-snmp/library/snmpUDPIPv4BaseDomain.h
@@ -32,7 +32,24 @@ extern          "C" {
                                               int local,
                                               struct sockaddr_in *src_addr);
 
-#if defined(HAVE_IP_PKTINFO) || defined(HAVE_IP_RECVDSTADDR)
+    netsnmp_transport *
+    netsnmp_udpipv4base_tspec_transport(netsnmp_tdomain_spec *tspec);
+
+    /** internal functions for derivatives of udpipv4base */
+    netsnmp_transport *
+    netsnmp_udpipv4base_transport_init(struct sockaddr_in *addr, int local);
+
+    int
+    netsnmp_udpipv4base_transport_socket(int flags);
+
+    int
+    netsnmp_udpipv4base_transport_bind(netsnmp_transport *t,
+                                       struct sockaddr_in *addr, int flags);
+
+    void
+    netsnmp_udpipv4base_transport_get_bound_addr(netsnmp_transport *t);
+
+#if defined(IP_PKTINFO) || defined(IP_RECVDSTADDR)
     int netsnmp_udpipv4_recvfrom(int s, void *buf, int len,
                                  struct sockaddr *from, socklen_t *fromlen,
                                  struct sockaddr *dstip, socklen_t *dstlen,

include/net-snmp/library/snmpUDPIPv6Domain.h
@@ -25,6 +25,25 @@ NETSNMP_IMPORT oid      netsnmp_UDPIPv6Domain[];
 netsnmp_transport *netsnmp_udp6_transport(struct sockaddr_in6 *addr,
                                           int local);
 
+netsnmp_transport *
+netsnmp_udp6_transport_with_source(struct sockaddr_in6 *addr, int local,
+                                   struct sockaddr_in6 *src_addr);
+
+    /** internal functions for derivatives of udpipv6 */
+
+    netsnmp_transport *
+    netsnmp_udp6_transport_init(struct sockaddr_in6 *addr, int local);
+
+    int
+    netsnmp_udp6_transport_socket(int flags);
+
+    int
+    netsnmp_udp6_transport_bind(netsnmp_transport *t,
+                                struct sockaddr_in6 *addr, int flags);
+
+    void
+    netsnmp_udp6_transport_get_bound_addr(netsnmp_transport *t);
+
 
 /*
  * Convert a "traditional" peername into a sockaddr_in6 structure which is

commit a240e46cdaf189f2c74b6b465ffbc09d6c697088

snmplib, transports: Remove NETSNMP_TSPEC_SHARED
Since this flag is not used anywhere in the Net-SNMP code base, remove it.

Fixes: 5d8372341594 ("shared transport + udp shared domain")

include/net-snmp/library/snmp_transport.h
@@ -114,7 +114,6 @@ typedef struct netsnmp_tmStateReference_s {
 } netsnmp_tmStateReference;
 
 #define NETSNMP_TSPEC_LOCAL                     0x01 /* 1=server, 0=client */
-#define NETSNMP_TSPEC_SHARED                    0x02
 #define NETSNMP_TSPEC_NO_DFTL_CLIENT_ADDR       0x04
 
 struct netsnmp_container_s; /* forward decl */