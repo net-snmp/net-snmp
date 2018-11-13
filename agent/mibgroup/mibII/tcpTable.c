7f3abe7515d585e089ddddf9a30762451b95aa44
Niels Baggesen <nba@users.sourceforge.net>
2018-11-13 21:51:24 +0100

    Patch for tcpTable for FreeBSD 12
    Patch taken from Freebsd ports collection.

--- agent/mibgroup/mibII/tcpTable.c
+++ agent/mibgroup/mibII/tcpTable.c
@@ -87,7 +87,11 @@ struct netsnmp_tcpConnEntry_s {
 
 typedef struct netsnmp_inpcb_s netsnmp_inpcb;
 struct netsnmp_inpcb_s {
+#if __FreeBSD_version >= 1200026
+    struct xinpcb   pcb;
+#else
     struct inpcb    pcb;
+#endif
     int             state;
     netsnmp_inpcb  *inp_next;
 };
@@ -289,8 +293,10 @@ tcpTable_handler(netsnmp_mib_handler          *handler,
 
 int
 TCP_Count_Connections( void ) {
+#if (defined(CAN_USE_SYSCTL) && defined(TCPCTL_PCBLIST))
     tcpTable_load(NULL, NULL);
     return tcp_estab;
+#endif
 }
 
 	/*
@@ -767,12 +773,20 @@ tcpTable_load(netsnmp_cache *cache, void *vmagic)
         nnew = SNMP_MALLOC_TYPEDEF(netsnmp_inpcb);
         if (!nnew)
             break;
+#if __FreeBSD_version >= 1200026
+        nnew->state = StateMap[((NS_ELEM *) xig)->t_state];
+#else
         nnew->state = StateMap[((NS_ELEM *) xig)->xt_tp.t_state];
+#endif
         if (nnew->state == 5 /* established */ ||
             nnew->state == 8 /*  closeWait  */ )
             tcp_estab++;
         memcpy(&(nnew->pcb), &(((NS_ELEM *) xig)->xt_inp),
+#if __FreeBSD_version >= 1200026
+                           sizeof(struct xinpcb));
+#else
                            sizeof(struct inpcb));
+#endif
 
 	if (nnew->pcb.inp_vflag & INP_IPV6)
 	    free(nnew);
