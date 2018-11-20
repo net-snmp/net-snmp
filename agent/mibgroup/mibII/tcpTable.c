net-snmp/net-snmp

commit 61c373a9a3571ea3f63a2762e43ddb0c9273ce64

Forward port FreeBSD 12 patches

agent/mibgroup/mibII/tcpTable.c
@@ -120,7 +120,11 @@ struct netsnmp_tcpConnEntry_s {
 
 typedef struct netsnmp_inpcb_s netsnmp_inpcb;
 struct netsnmp_inpcb_s {
+#if __FreeBSD_version >= 1200026
+    struct xinpcb   pcb;
+#else
     struct inpcb    pcb;
+#endif
     int             state;
     netsnmp_inpcb  *inp_next;
 };
@@ -330,8 +334,10 @@ tcpTable_handler(netsnmp_mib_handler          *handler,
 #ifndef NETSNMP_FEATURE_REMOVE_TCP_COUNT_CONNECTIONS
 int
 TCP_Count_Connections( void ) {
+#if (defined(CAN_USE_SYSCTL) && defined(TCPCTL_PCBLIST))
     tcpTable_load(NULL, NULL);
     return tcp_estab;
+#endif
 }
 #endif /* NETSNMP_FEATURE_REMOVE_TCP_COUNT_CONNECTIONS */
 
@@ -988,12 +994,20 @@ tcpTable_load(netsnmp_cache *cache, void *vmagic)
         nnew = SNMP_MALLOC_TYPEDEF(netsnmp_inpcb);
         if (!nnew)
             break;
+#if __FreeBSD_version >= 1200026
+        nnew->state = StateMap[((NS_ELEM *) xig)->t_state];
+#else
         nnew->state = StateMap[((NS_ELEM *) xig)->xt_tp.t_state];
+#endif
         if (nnew->state == 5 /* established */ ||
             nnew->state == 8 /*  closeWait  */ )
             tcp_estab++;
         memcpy(&(nnew->pcb), &(((NS_ELEM *) xig)->xt_inp),
+#if __FreeBSD_version >= 1200026
+                           sizeof(struct xinpcb));
+#else
                            sizeof(struct inpcb));
+#endif
 
 #ifdef INP_ISIPV6
 	if (INP_ISIPV6(&nnew->pcb))

agent/mibgroup/mibII/udpTable.c
@@ -97,7 +97,11 @@ struct netsnmp_udpEntry_s {
 #if defined(freebsd4) || defined(darwin) || defined(osf5)
 typedef struct netsnmp_inpcb_s netsnmp_inpcb;
 struct netsnmp_inpcb_s {
+#if __FreeBSD_version >= 1200026
+    struct xinpcb   pcb;
+#else
     struct inpcb    pcb;
+#endif
     int             state;
     netsnmp_inpcb  *inp_next;
 };
@@ -708,7 +712,11 @@ udpTable_load(netsnmp_cache *cache, void *vmagic)
         nnew = SNMP_MALLOC_TYPEDEF(UDPTABLE_ENTRY_TYPE);
         if (!nnew)
             break;
+#if __FreeBSD_version >= 1200026
+        memcpy(&nnew->pcb, xig, sizeof(struct xinpcb));
+#else
         memcpy(&nnew->pcb, &((struct xinpcb *) xig)->xi_inp, sizeof(struct inpcb));
+#endif
 	nnew->inp_next = udp_head;
 	udp_head   = nnew;
 #if defined(dragonfly)

agent/mibgroup/tcp-mib/data_access/tcpConn_freebsd4.c
@@ -163,7 +163,11 @@ _load(netsnmp_container *container, u_int load_flags)
 #else
 	xig = (struct xinpgen *) ((char *) xig + xig->xig_len);
 #endif
+#if __FreeBSD_version >= 1200026
+	state = StateMap[pcb.t_state];
+#else
 	state = StateMap[pcb.xt_tp.t_state];
+#endif
 
 	if (load_flags) {
 	    if (state == TCPCONNECTIONSTATE_LISTEN) {

agent/mibgroup/udp-mib/data_access/udp_endpoint_freebsd4.c
@@ -168,9 +168,24 @@ _load(netsnmp_container *container, u_int load_flags)
         }
 
         /** oddly enough, these appear to already be in network order */
+#if __FreeBSD_version >= 1200026
+        entry->loc_port = htons(pcb.inp_lport);
+        entry->rmt_port = htons(pcb.inp_fport);
+        
+        /** the addr string may need work */
+	if (pcb.inp_vflag & INP_IPV6) {
+	    entry->loc_addr_len = entry->rmt_addr_len = 16;
+	    memcpy(entry->loc_addr, &pcb.in6p_laddr, 16);
+	    memcpy(entry->rmt_addr, &pcb.in6p_faddr, 16);
+	}
+	else {
+	    entry->loc_addr_len = entry->rmt_addr_len = 4;
+	    memcpy(entry->loc_addr, &pcb.inp_laddr, 4);
+	    memcpy(entry->rmt_addr, &pcb.inp_faddr, 4);
+	}
+#else
         entry->loc_port = htons(pcb.xi_inp.inp_lport);
         entry->rmt_port = htons(pcb.xi_inp.inp_fport);
-        entry->pid = 0;
         
         /** the addr string may need work */
 #ifdef INP_ISIPV6
@@ -187,6 +202,8 @@ _load(netsnmp_container *container, u_int load_flags)
 	    memcpy(entry->loc_addr, &pcb.xi_inp.inp_laddr, 4);
 	    memcpy(entry->rmt_addr, &pcb.xi_inp.inp_faddr, 4);
 	}
+#endif
+        entry->pid = 0;
 
         /*
          * add entry to container

commit b0f497b84aa28a754ad7ce260c63092dd85a584d

snmpd, MIB-II: Fix a recently introduced compiler warning
Avoid that the compiler reports the following on Linux:

mibII/tcpTable.c:341:1: warning: control reaches end of non-void function [-Wreturn-type]
 }
 ^

Fixes: 61c373a9a357 ("Forward port FreeBSD 12 patches")

agent/mibgroup/mibII/tcpTable.c
@@ -336,8 +336,8 @@ int
 TCP_Count_Connections( void ) {
 #if (defined(CAN_USE_SYSCTL) && defined(TCPCTL_PCBLIST))
     tcpTable_load(NULL, NULL);
-    return tcp_estab;
 #endif
+    return tcp_estab;
 }
 #endif /* NETSNMP_FEATURE_REMOVE_TCP_COUNT_CONNECTIONS */
 