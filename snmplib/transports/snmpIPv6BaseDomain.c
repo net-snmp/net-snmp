net-snmp/net-snmp

commit f3d96c8c45f91a193883c7b669b78eec6d2b3242

libsnmp: Add two additional debug statements in the code for parsing IPv6 addresses

snmplib/transports/snmpIPv6BaseDomain.c
@@ -314,13 +314,17 @@ netsnmp_sockaddr_in6_3(struct netsnmp_ep *ep,
 #endif
         }
         if (!inet_pton(AF_INET6, ep_str.addr, &addr->sin6_addr) &&
-            !netsnmp_resolve_v6_hostname(&addr->sin6_addr, ep_str.addr))
+            !netsnmp_resolve_v6_hostname(&addr->sin6_addr, ep_str.addr)) {
+            DEBUGMSGTL(("netsnmp_sockaddr_in6", "failed to parse %s\n",
+                        ep_str.addr));
             return 0;
+        }
     }
 
-    DEBUGMSGTL(("netsnmp_sockaddr_in6", "return { AF_INET6, [%s]:%hu }\n",
+    DEBUGMSGTL(("netsnmp_sockaddr_in6", "return { AF_INET6, [%s%%%d]:%hu }\n",
                 inet_ntop(AF_INET6, &addr->sin6_addr, debug_addr,
-                          sizeof(debug_addr)), ntohs(addr->sin6_port)));
+                          sizeof(debug_addr)), addr->sin6_scope_id,
+                ntohs(addr->sin6_port)));
     return 1;
 }
 

commit 29f03b11f527c874cc01afa5293eb9bb790b9798

libsnmp, Windows: Fix a recently introduced compiler warning
Since sin6_scope_id has been declared as unsigned long on Windows,
cast it to int before passing it to the %d format specifier.

Fixes: f3d96c8c45f9 ("libsnmp: Add two additional debug statements in the code for parsing IPv6 addresses")

snmplib/transports/snmpIPv6BaseDomain.c
@@ -323,7 +323,7 @@ netsnmp_sockaddr_in6_3(struct netsnmp_ep *ep,
 
     DEBUGMSGTL(("netsnmp_sockaddr_in6", "return { AF_INET6, [%s%%%d]:%hu }\n",
                 inet_ntop(AF_INET6, &addr->sin6_addr, debug_addr,
-                          sizeof(debug_addr)), addr->sin6_scope_id,
+                          sizeof(debug_addr)), (int)addr->sin6_scope_id,
                 ntohs(addr->sin6_port)));
     return 1;
 }