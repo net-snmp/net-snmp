net-snmp/net-snmp

commit 52e33fb6117738d3e10b394a4370991aa74d31c7

ltmain.sh, MinGW64: Fix a linker error
Avoid that linking fails as follows with certain MinGW64 versions:

./.libs/lt-snmpps.c: In function 'main':
./.libs/lt-snmpps.c:315:10: warning: implicit declaration of function '_spawnv' [-Wimplicit-function-declaration]
   rval = _spawnv (_P_WAIT, lt_argv_zero, (const char * const *) newargz);
          ^~~~~~~
./.libs/lt-snmpps.c:315:19: error: '_P_WAIT' undeclared (first use in this function); did you mean '__VALIST'?
   rval = _spawnv (_P_WAIT, lt_argv_zero, (const char * const *) newargz);
                   ^~~~~~~
                   __VALIST

ltmain.sh
@@ -4515,6 +4515,8 @@ EOF
 
 	    case $host_os in
 	      mingw*)
+		case "$(uname)" in
+		  MINGW32*)
 		cat <<"EOF"
   /* execv doesn't actually work on mingw as expected on unix */
   newargz = prepare_spawn (newargz);
@@ -4529,6 +4531,8 @@ EOF
     }
   return rval;
 EOF
+		;;
+		esac
 		;;
 	      *)
 		cat <<"EOF"

commit 9cd7d5c60318426a65b083d23c356a8804a3a38e

ltmain.sh: Improve MinGW32 detection code
Fixes: 52e33fb61177 ("ltmain.sh, MinGW64: Fix a linker error")

ltmain.sh
@@ -4515,8 +4515,8 @@ EOF
 
 	    case $host_os in
 	      mingw*)
-		case "$(uname)" in
-		  MINGW32*)
+		case "$($LTCC -v | sed -n 's/^Target:[[:blank:]]*//')" in
+		  i386-w32-mingw32*)
 		cat <<"EOF"
   /* execv doesn't actually work on mingw as expected on unix */
   newargz = prepare_spawn (newargz);