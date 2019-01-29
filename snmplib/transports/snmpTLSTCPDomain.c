net-snmp/net-snmp

commit d686bc3502f137f10e38fe719921c765cefa38fb

CHANGES: snmplib: Add support for OpenSSL 1.1.0

snmplib/transports/snmpTLSTCPDomain.c
@@ -688,8 +688,7 @@ netsnmp_tlstcp_open(netsnmp_transport *t)
         /* XXX: check securityLevel and ensure no NULL fingerprints are used */
 
         /* set up the needed SSL context */
-        tlsdata->ssl_context = ctx =
-            sslctx_client_setup(TLSv1_method(), tlsdata);
+        tlsdata->ssl_context = ctx = sslctx_client_setup(TLS_method(), tlsdata);
         if (!ctx) {
             snmp_log(LOG_ERR, "failed to create TLS context\n");
             return NULL;
@@ -894,8 +893,7 @@ netsnmp_tlstcp_open(netsnmp_transport *t)
         }
 
         /* create the OpenSSL TLS context */
-        tlsdata->ssl_context =
-            sslctx_server_setup(TLSv1_method());
+        tlsdata->ssl_context = sslctx_server_setup(TLS_method());
 
         t->sock = BIO_get_fd(tlsdata->accept_bio, NULL);
         t->flags |= NETSNMP_TRANSPORT_FLAG_LISTEN;

configure
@@ -33121,7 +33121,11 @@ _ACEOF
 
 
 
-for ac_func in AES_cfb128_encrypt                           EVP_MD_CTX_create EVP_MD_CTX_destroy                           EVP_MD_CTX_new    EVP_MD_CTX_free                              DH_set0_pqg DH_get0_pqg DH_get0_key
+
+
+
+
+for ac_func in AES_cfb128_encrypt                           EVP_MD_CTX_create EVP_MD_CTX_destroy                           EVP_MD_CTX_new    EVP_MD_CTX_free                              DH_set0_pqg DH_get0_pqg DH_get0_key                           ASN1_STRING_get0_data X509_NAME_ENTRY_get_object                           X509_NAME_ENTRY_get_data X509_get_signature_nid
 do
 as_ac_var=`$as_echo "ac_cv_func_$ac_func" | $as_tr_sh`
 { $as_echo "$as_me:$LINENO: checking for $ac_func" >&5
@@ -33221,22 +33225,107 @@ _ACEOF
 fi
 done
 
-            LIBS="$netsnmp_save_LIBS"
-        fi
-        if echo " $transport_result_list " | $GREP "DTLS" > /dev/null; then
-	    { $as_echo "$as_me:$LINENO: checking for DTLSv1_method in -lssl" >&5
-$as_echo_n "checking for DTLSv1_method in -lssl... " >&6; }
-if test "${ac_cv_lib_ssl_DTLSv1_method+set}" = set; then
-  $as_echo_n "(cached) " >&6
-else
-  ac_check_lib_save_LIBS=$LIBS
-LIBS="-lssl -lcrypto $LIBS"
-cat >conftest.$ac_ext <<_ACEOF
+            { $as_echo "$as_me:$LINENO: checking whether 2nd app_verify_cookie_cb() arg is const" >&5
+$as_echo_n "checking whether 2nd app_verify_cookie_cb() arg is const... " >&6; }
+            netsnmp_save_CFLAGS="$CFLAGS"
+            CFLAGS="$CFLAGS -Werror"
+            cat >conftest.$ac_ext <<_ACEOF
 /* confdefs.h.  */
 _ACEOF
 cat confdefs.h >>conftest.$ac_ext
 cat >>conftest.$ac_ext <<_ACEOF
 /* end confdefs.h.  */
+
+              #include <openssl/ssl.h>
+
+int
+main ()
+{
+
+              int (*app_verify_cookie_cb)(SSL *ssl,
+                                          const unsigned char *cookie,
+                                          unsigned int cookie_len) = NULL;
+              SSL_CTX_set_cookie_verify_cb(NULL, app_verify_cookie_cb);
+
+  ;
+  return 0;
+}
+_ACEOF
+rm -f conftest.$ac_objext
+if { (ac_try="$ac_compile"
+case "(($ac_try" in
+  *\"* | *\`* | *\\*) ac_try_echo=\$ac_try;;
+  *) ac_try_echo=$ac_try;;
+esac
+eval ac_try_echo="\"\$as_me:$LINENO: $ac_try_echo\""
+$as_echo "$ac_try_echo") >&5
+  (eval "$ac_compile") 2>conftest.er1
+  ac_status=$?
+  grep -v '^ *+' conftest.er1 >conftest.err
+  rm -f conftest.er1
+  cat conftest.err >&5
+  $as_echo "$as_me:$LINENO: \$? = $ac_status" >&5
+  (exit $ac_status); } && {
+	 test -z "$ac_c_werror_flag" ||
+	 test ! -s conftest.err
+       } && test -s conftest.$ac_objext; then
+  { $as_echo "$as_me:$LINENO: result: yes" >&5
+$as_echo "yes" >&6; }; ac_cv_saccaq=const
+else
+  $as_echo "$as_me: failed program was:" >&5
+sed 's/^/| /' conftest.$ac_ext >&5
+
+	{ $as_echo "$as_me:$LINENO: result: no" >&5
+$as_echo "no" >&6; }
+fi
+
+rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
+
+cat >>confdefs.h <<_ACEOF
+#define SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER $ac_cv_saccaq
+_ACEOF
+
+            CFLAGS="$netsnmp_save_CFLAGS"
+            LIBS="$netsnmp_save_LIBS"
+        fi
+        netsnmp_save_LIBS="$LIBS"
+        LIBS="-lssl"
+
+
+
+
+
+
+for ac_func in TLS_method TLSv1_method DTLS_method DTLSv1_method                       SSL_library_init SSL_load_error_strings
+do
+as_ac_var=`$as_echo "ac_cv_func_$ac_func" | $as_tr_sh`
+{ $as_echo "$as_me:$LINENO: checking for $ac_func" >&5
+$as_echo_n "checking for $ac_func... " >&6; }
+if { as_var=$as_ac_var; eval "test \"\${$as_var+set}\" = set"; }; then
+  $as_echo_n "(cached) " >&6
+else
+  cat >conftest.$ac_ext <<_ACEOF
+/* confdefs.h.  */
+_ACEOF
+cat confdefs.h >>conftest.$ac_ext
+cat >>conftest.$ac_ext <<_ACEOF
+/* end confdefs.h.  */
+/* Define $ac_func to an innocuous variant, in case <limits.h> declares $ac_func.
+   For example, HP-UX 11i <limits.h> declares gettimeofday.  */
+#define $ac_func innocuous_$ac_func
+
+/* System header to define __stub macros and hopefully few prototypes,
+    which can conflict with char $ac_func (); below.
+    Prefer <limits.h> to <assert.h> if __STDC__ is defined, since
+    <limits.h> exists even on freestanding compilers.  */
+
+#ifdef __STDC__
+# include <limits.h>
+#else
+# include <assert.h>
+#endif
+
+#undef $ac_func
 
 /* Override any GCC internal prototype to avoid an error.
    Use char because int might match the return type of a GCC
@@ -33244,11 +33333,18 @@ cat >>conftest.$ac_ext <<_ACEOF
 #ifdef __cplusplus
 extern "C"
 #endif
-char DTLSv1_method ();
+char $ac_func ();
+/* The GNU C library defines this for functions which it implements
+    to always fail with ENOSYS.  Some functions are actually named
+    something starting with __ and the normal name is an alias.  */
+#if defined __stub_$ac_func || defined __stub___$ac_func
+choke me
+#endif
+
 int
 main ()
 {
-return DTLSv1_method ();
+return $ac_func ();
   ;
   return 0;
 }
@@ -33274,115 +33370,63 @@ $as_echo "$ac_try_echo") >&5
 	 test "$cross_compiling" = yes ||
 	 $as_test_x conftest$ac_exeext
        }; then
-  ac_cv_lib_ssl_DTLSv1_method=yes
+  eval "$as_ac_var=yes"
 else
   $as_echo "$as_me: failed program was:" >&5
 sed 's/^/| /' conftest.$ac_ext >&5
 
-	ac_cv_lib_ssl_DTLSv1_method=no
+	eval "$as_ac_var=no"
 fi
 
 rm -rf conftest.dSYM
 rm -f core conftest.err conftest.$ac_objext conftest_ipa8_conftest.oo \
       conftest$ac_exeext conftest.$ac_ext
-LIBS=$ac_check_lib_save_LIBS
 fi
-{ $as_echo "$as_me:$LINENO: result: $ac_cv_lib_ssl_DTLSv1_method" >&5
-$as_echo "$ac_cv_lib_ssl_DTLSv1_method" >&6; }
-if test "x$ac_cv_lib_ssl_DTLSv1_method" = x""yes; then
+ac_res=`eval 'as_val=${'$as_ac_var'}
+		 $as_echo "$as_val"'`
+	       { $as_echo "$as_me:$LINENO: result: $ac_res" >&5
+$as_echo "$ac_res" >&6; }
+as_val=`eval 'as_val=${'$as_ac_var'}
+		 $as_echo "$as_val"'`
+   if test "x$as_val" = x""yes; then
+  cat >>confdefs.h <<_ACEOF
+#define `$as_echo "HAVE_$ac_func" | $as_tr_cpp` 1
+_ACEOF
+
+fi
+done
+
+        LIBS="$netsnmp_save_LIBS"
+        if echo " $transport_result_list " | $GREP "DTLS" > /dev/null; then
+	    if test x$ac_cv_func_DTLSv1_method = xyes -o \
+                    x$ac_cv_func_DTLS_method = xyes; then
 
 cat >>confdefs.h <<\_ACEOF
 #define HAVE_LIBSSL_DTLS 1
 _ACEOF
 
                 LIBCRYPTO=" -lssl $LIBCRYPTO"
-else
-  { { $as_echo "$as_me:$LINENO: error: The DTLS based transports require the libssl library from OpenSSL to be available and support DTLS" >&5
+	    else
+		{ { $as_echo "$as_me:$LINENO: error: The DTLS based transports require the libssl library from OpenSSL to be available and support DTLS" >&5
 $as_echo "$as_me: error: The DTLS based transports require the libssl library from OpenSSL to be available and support DTLS" >&2;}
    { (exit 1); exit 1; }; }
-fi
-
+            fi
             TLSPROG=yes
         fi
         if echo " $transport_result_list " | $GREP "TLS" > /dev/null; then
-	    { $as_echo "$as_me:$LINENO: checking for SSL_library_init in -lssl" >&5
-$as_echo_n "checking for SSL_library_init in -lssl... " >&6; }
-if test "${ac_cv_lib_ssl_SSL_library_init+set}" = set; then
-  $as_echo_n "(cached) " >&6
-else
-  ac_check_lib_save_LIBS=$LIBS
-LIBS="-lssl -lcrypto $LIBS"
-cat >conftest.$ac_ext <<_ACEOF
-/* confdefs.h.  */
-_ACEOF
-cat confdefs.h >>conftest.$ac_ext
-cat >>conftest.$ac_ext <<_ACEOF
-/* end confdefs.h.  */
-
-/* Override any GCC internal prototype to avoid an error.
-   Use char because int might match the return type of a GCC
-   builtin and then its argument prototype would still apply.  */
-#ifdef __cplusplus
-extern "C"
-#endif
-char SSL_library_init ();
-int
-main ()
-{
-return SSL_library_init ();
-  ;
-  return 0;
-}
-_ACEOF
-rm -f conftest.$ac_objext conftest$ac_exeext
-if { (ac_try="$ac_link"
-case "(($ac_try" in
-  *\"* | *\`* | *\\*) ac_try_echo=\$ac_try;;
-  *) ac_try_echo=$ac_try;;
-esac
-eval ac_try_echo="\"\$as_me:$LINENO: $ac_try_echo\""
-$as_echo "$ac_try_echo") >&5
-  (eval "$ac_link") 2>conftest.er1
-  ac_status=$?
-  grep -v '^ *+' conftest.er1 >conftest.err
-  rm -f conftest.er1
-  cat conftest.err >&5
-  $as_echo "$as_me:$LINENO: \$? = $ac_status" >&5
-  (exit $ac_status); } && {
-	 test -z "$ac_c_werror_flag" ||
-	 test ! -s conftest.err
-       } && test -s conftest$ac_exeext && {
-	 test "$cross_compiling" = yes ||
-	 $as_test_x conftest$ac_exeext
-       }; then
-  ac_cv_lib_ssl_SSL_library_init=yes
-else
-  $as_echo "$as_me: failed program was:" >&5
-sed 's/^/| /' conftest.$ac_ext >&5
-
-	ac_cv_lib_ssl_SSL_library_init=no
-fi
-
-rm -rf conftest.dSYM
-rm -f core conftest.err conftest.$ac_objext conftest_ipa8_conftest.oo \
-      conftest$ac_exeext conftest.$ac_ext
-LIBS=$ac_check_lib_save_LIBS
-fi
-{ $as_echo "$as_me:$LINENO: result: $ac_cv_lib_ssl_SSL_library_init" >&5
-$as_echo "$ac_cv_lib_ssl_SSL_library_init" >&6; }
-if test "x$ac_cv_lib_ssl_SSL_library_init" = x""yes; then
+	    if test x$ac_cv_func_TLSv1_method = xyes -o \
+                    x$ac_cv_func_TLS_method = xyes; then
 
 cat >>confdefs.h <<\_ACEOF
 #define HAVE_LIBSSL 1
 _ACEOF
 
                 LIBCRYPTO=" -lssl $LIBCRYPTO"
-else
-  { { $as_echo "$as_me:$LINENO: error: The DTLS based transports require the libssl library from OpenSSL to be available" >&5
+            else
+		{ { $as_echo "$as_me:$LINENO: error: The DTLS based transports require the libssl library from OpenSSL to be available" >&5
 $as_echo "$as_me: error: The DTLS based transports require the libssl library from OpenSSL to be available" >&2;}
    { (exit 1); exit 1; }; }
-fi
-
+            fi
             TLSPROG=yes
         fi
         if echo " $transport_result_list " | $GREP " SSH " > /dev/null; then

configure.d/config_os_libs2
@@ -301,23 +301,53 @@ if test "x$tryopenssl" != "xno" -a "x$tryopenssl" != "xinternal"; then
             AC_CHECK_FUNCS([AES_cfb128_encrypt]dnl
                            [EVP_MD_CTX_create EVP_MD_CTX_destroy]dnl
                            [EVP_MD_CTX_new    EVP_MD_CTX_free   ]dnl
-                           [DH_set0_pqg DH_get0_pqg DH_get0_key])
+                           [DH_set0_pqg DH_get0_pqg DH_get0_key]dnl
+                           [ASN1_STRING_get0_data X509_NAME_ENTRY_get_object]dnl
+                           [X509_NAME_ENTRY_get_data X509_get_signature_nid])
+            AC_MSG_CHECKING([whether 2nd app_verify_cookie_cb() arg is const])
+            netsnmp_save_CFLAGS="$CFLAGS"
+            CFLAGS="$CFLAGS -Werror"
+            AC_COMPILE_IFELSE(AC_LANG_PROGRAM([
+              #include <openssl/ssl.h>
+            ],[
+              int (*app_verify_cookie_cb)(SSL *ssl,
+                                          const unsigned char *cookie,
+                                          unsigned int cookie_len) = NULL;
+              SSL_CTX_set_cookie_verify_cb(NULL, app_verify_cookie_cb);
+            ]),
+            [AC_MSG_RESULT(yes); ac_cv_saccaq=const],
+            [AC_MSG_RESULT(no)])
+            AC_DEFINE_UNQUOTED([SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER],
+                               [$ac_cv_saccaq],
+                               [Qualifier for second argument of the appverify_cookie_cb() callback function])
+            CFLAGS="$netsnmp_save_CFLAGS"
             LIBS="$netsnmp_save_LIBS"
         fi
+        netsnmp_save_LIBS="$LIBS"
+        LIBS="-lssl"
+        AC_CHECK_FUNCS([TLS_method TLSv1_method DTLS_method DTLSv1_method]dnl
+                       [SSL_library_init SSL_load_error_strings])
+        LIBS="$netsnmp_save_LIBS"
         if echo " $transport_result_list " | $GREP "DTLS" > /dev/null; then
-	    AC_CHECK_LIB(ssl, DTLSv1_method,
+	    if test x$ac_cv_func_DTLSv1_method = xyes -o \
+                    x$ac_cv_func_DTLS_method = xyes; then
                 AC_DEFINE(HAVE_LIBSSL_DTLS, 1,
                     [Define to 1 if your `ssl' library supports DTLS (-lssl).])
-                LIBCRYPTO=" -lssl $LIBCRYPTO",	
-		AC_MSG_ERROR([The DTLS based transports require the libssl library from OpenSSL to be available and support DTLS])	,-lcrypto)
+                LIBCRYPTO=" -lssl $LIBCRYPTO"
+	    else
+		AC_MSG_ERROR([The DTLS based transports require the libssl library from OpenSSL to be available and support DTLS])
+            fi
             TLSPROG=yes
         fi
         if echo " $transport_result_list " | $GREP "TLS" > /dev/null; then
-	    AC_CHECK_LIB(ssl, SSL_library_init,
+	    if test x$ac_cv_func_TLSv1_method = xyes -o \
+                    x$ac_cv_func_TLS_method = xyes; then
                 AC_DEFINE(HAVE_LIBSSL, 1,
                     [Define to 1 if you have the `ssl' library (-lssl).])
-                LIBCRYPTO=" -lssl $LIBCRYPTO",	
-		AC_MSG_ERROR([The DTLS based transports require the libssl library from OpenSSL to be available])	,-lcrypto)
+                LIBCRYPTO=" -lssl $LIBCRYPTO"
+            else
+		AC_MSG_ERROR([The DTLS based transports require the libssl library from OpenSSL to be available])
+            fi
             TLSPROG=yes
         fi
         if echo " $transport_result_list " | $GREP " SSH " > /dev/null; then

include/net-snmp/library/snmp_openssl.h
@@ -6,10 +6,13 @@
 #define SNMP_OPENSSL_H
 
 #include <openssl/dh.h>
+#include <openssl/ssl.h>
 
 #ifdef __cplusplus
 extern          "C" {
 #endif
+
+    struct netsnmp_cert_map_s;
 
     NETSNMP_IMPORT
     void netsnmp_init_openssl(void);
@@ -27,8 +30,8 @@ extern          "C" {
 
     int netsnmp_openssl_cert_issued_by(X509 *issuer, X509 *cert);
 
-    char *netsnmp_openssl_extract_secname(netsnmp_cert_map *cert_map,
-                                          netsnmp_cert_map *peer_cert);
+    char *netsnmp_openssl_extract_secname(struct netsnmp_cert_map_s *cert_map,
+                                          struct netsnmp_cert_map_s *peer_cert);
 
     char *netsnmp_openssl_cert_get_subjectAltName(X509 *, char **buf, int *len);
 
@@ -54,6 +57,30 @@ extern          "C" {
     NETSNMP_IMPORT
     void DH_get0_key(const DH *dh, const BIGNUM **pub_key,
                      const BIGNUM **priv_key);
+#ifndef ASN1_STRING_GET0_DATA
+    NETSNMP_IMPORT
+    const unsigned char *ASN1_STRING_get0_data(const ASN1_STRING *x);
+#endif
+#ifndef HAVE_X509_NAME_ENTRY_GET_OBJECT
+    NETSNMP_IMPORT
+    ASN1_OBJECT *X509_NAME_ENTRY_get_object(const X509_NAME_ENTRY *ne);
+#endif
+#ifndef HAVE_X509_NAME_ENTRY_GET_DATA
+    NETSNMP_IMPORT
+    ASN1_STRING *X509_NAME_ENTRY_get_data(const X509_NAME_ENTRY *ne);
+#endif
+#ifndef HAVE_X509_GET_SIGNATURE_NID
+    NETSNMP_IMPORT
+    int X509_get_signature_nid(const X509_REQ *req);
+#endif
+#ifndef HAVE_TLS_METHOD
+    NETSNMP_IMPORT
+    const SSL_METHOD *TLS_method(void);
+#endif
+#ifndef HAVE_DTLS_METHOD
+    NETSNMP_IMPORT
+    const SSL_METHOD *DTLS_method(void);
+#endif
 
 #ifdef __cplusplus
 }

include/net-snmp/net-snmp-config.h.in
@@ -94,6 +94,9 @@
 /* Define to 1 if you have the <asm/types.h> header file. */
 #undef HAVE_ASM_TYPES_H
 
+/* Define to 1 if you have the `ASN1_STRING_get0_data' function. */
+#undef HAVE_ASN1_STRING_GET0_DATA
+
 /* Define to 1 if you have the `cgetnext' function. */
 #undef HAVE_CGETNEXT
 
@@ -143,6 +146,12 @@
 
 /* Set if the dpkg-query command is available */
 #undef HAVE_DPKG_QUERY
+
+/* Define to 1 if you have the `DTLSv1_method' function. */
+#undef HAVE_DTLSV1_METHOD
+
+/* Define to 1 if you have the `DTLS_method' function. */
+#undef HAVE_DTLS_METHOD
 
 /* Define to 1 if you have the <err.h> header file. */
 #undef HAVE_ERR_H
@@ -747,6 +756,12 @@
 /* Define to 1 if the system has the type `ssize_t'. */
 #undef HAVE_SSIZE_T
 
+/* Define to 1 if you have the `SSL_library_init' function. */
+#undef HAVE_SSL_LIBRARY_INIT
+
+/* Define to 1 if you have the `SSL_load_error_strings' function. */
+#undef HAVE_SSL_LOAD_ERROR_STRINGS
+
 /* Define to 1 if you have the `statfs' function. */
 #undef HAVE_STATFS
 
@@ -1203,6 +1218,12 @@
 /* Define if a timezone variable is declared in <sys/time.h> */
 #undef HAVE_TIMEZONE_VARIABLE
 
+/* Define to 1 if you have the `TLSv1_method' function. */
+#undef HAVE_TLSV1_METHOD
+
+/* Define to 1 if you have the `TLS_method' function. */
+#undef HAVE_TLS_METHOD
+
 /* Define to 1 if you have the <ufs/ffs/fs.h> header file. */
 #undef HAVE_UFS_FFS_FS_H
 
@@ -1274,6 +1295,15 @@
 
 /* Define to 1 if you have the <ws2tcpip.h> header file. */
 #undef HAVE_WS2TCPIP_H
+
+/* Define to 1 if you have the `X509_get_signature_nid' function. */
+#undef HAVE_X509_GET_SIGNATURE_NID
+
+/* Define to 1 if you have the `X509_NAME_ENTRY_get_data' function. */
+#undef HAVE_X509_NAME_ENTRY_GET_DATA
+
+/* Define to 1 if you have the `X509_NAME_ENTRY_get_object' function. */
+#undef HAVE_X509_NAME_ENTRY_GET_OBJECT
 
 /* Define to 1 if you have the <xti.h> header file. */
 #undef HAVE_XTI_H
@@ -1453,6 +1483,10 @@
 
 /* Use BSD 4.4 routing table entries? */
 #undef RTENTRY_4_4
+
+/* Qualifier for second argument of the appverify_cookie_cb() callback
+   function */
+#undef SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER
 
 /* The size of `int', as computed by sizeof. */
 #undef SIZEOF_INT

snmplib/snmp_openssl.c
@@ -38,8 +38,12 @@ void netsnmp_init_openssl(void) {
     DEBUGMSGTL(("snmp_openssl", "initializing\n"));
 
     /* Initializing OpenSSL */
+#ifdef HAVE_SSL_LIBRARY_INIT
     SSL_library_init();
+#endif
+#ifdef HAVE_SSL_LOAD_ERROR_STRINGS
     SSL_load_error_strings();
+#endif
     ERR_load_BIO_strings();
     OpenSSL_add_all_algorithms();
 }
@@ -154,11 +158,12 @@ netsnmp_openssl_cert_dump_names(X509 *ocert)
         oname_entry = X509_NAME_get_entry(osubj_name, i);
         netsnmp_assert(NULL != oname_entry);
 
-        if (oname_entry->value->type != V_ASN1_PRINTABLESTRING)
+        if (X509_NAME_ENTRY_get_data(oname_entry)->type !=
+            V_ASN1_PRINTABLESTRING)
             continue;
 
         /** get NID */
-        onid = OBJ_obj2nid(oname_entry->object);
+        onid = OBJ_obj2nid(X509_NAME_ENTRY_get_object(oname_entry));
         if (onid == NID_undef) {
             prefix_long = prefix_short = "UNKNOWN";
         }
@@ -169,9 +174,10 @@ netsnmp_openssl_cert_dump_names(X509 *ocert)
 
         DEBUGMSGT(("9:cert:dump:names",
                    "[%02d] NID type %d, ASN type %d\n", i, onid,
-                   oname_entry->value->type));
+                   X509_NAME_ENTRY_get_data(oname_entry)->type));
         DEBUGMSGT(("9:cert:dump:names", "%s/%s: '%s'\n", prefix_long,
-                   prefix_short, ASN1_STRING_data(oname_entry->value)));
+                   prefix_short,
+                   ASN1_STRING_get0_data(X509_NAME_ENTRY_get_data(oname_entry))));
     }
 }
 
@@ -457,7 +463,7 @@ netsnmp_openssl_cert_get_hash_type(X509 *ocert)
     if (NULL == ocert)
         return 0;
 
-    return _nid2ht(OBJ_obj2nid(ocert->sig_alg->algorithm));
+    return _nid2ht(X509_get_signature_nid(ocert));
 }
 
 /**
@@ -474,7 +480,7 @@ netsnmp_openssl_cert_get_fingerprint(X509 *ocert, int alg)
     if (NULL == ocert)
         return NULL;
 
-    nid = OBJ_obj2nid(ocert->sig_alg->algorithm);
+    nid = X509_get_signature_nid(ocert);
     DEBUGMSGT(("9:openssl:fingerprint", "alg %d, cert nid %d (%d)\n", alg, nid,
                _nid2ht(nid)));
         
@@ -915,6 +921,52 @@ DH_get0_key(const DH *dh, const BIGNUM **pub_key, const BIGNUM **priv_key)
        *pub_key = dh->pub_key;
    if (priv_key != NULL)
        *priv_key = dh->priv_key;
+}
+#endif
+
+#ifndef ASN1_STRING_GET0_DATA
+const unsigned char *ASN1_STRING_get0_data(const ASN1_STRING *x)
+{
+    return x->data;
+}
+#endif
+
+#ifndef HAVE_X509_NAME_ENTRY_GET_OBJECT
+ASN1_OBJECT *X509_NAME_ENTRY_get_object(const X509_NAME_ENTRY *ne)
+{
+    if (ne == NULL)
+        return NULL;
+    return ne->object;
+}
+#endif
+
+#ifndef HAVE_X509_NAME_ENTRY_GET_DATA
+ASN1_STRING *X509_NAME_ENTRY_get_data(const X509_NAME_ENTRY *ne)
+{
+    if (ne == NULL)
+        return NULL;
+    return ne->value;
+}
+#endif
+
+#ifndef HAVE_X509_GET_SIGNATURE_NID
+int X509_get_signature_nid(const X509_REQ *req)
+{
+    return OBJ_obj2nid(req->sig_alg.algorithm);
+}
+#endif
+
+#ifndef HAVE_TLS_METHOD
+const SSL_METHOD *TLS_method(void)
+{
+    return TLSv1_method();
+}
+#endif
+
+#ifndef HAVE_DTLS_METHOD
+const SSL_METHOD *DTLS_method(void)
+{
+    return DTLSv1_method();
 }
 #endif
 

snmplib/transports/snmpDTLSUDPDomain.c
@@ -118,7 +118,9 @@ static bio_cache *biocache = NULL;
 
 static int openssl_addr_index = 0;
 
-static int netsnmp_dtls_verify_cookie(SSL *ssl, unsigned char *cookie,
+static int netsnmp_dtls_verify_cookie(SSL *ssl,
+                                      SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER
+                                      unsigned char *cookie,
                                       unsigned int cookie_len);
 static int netsnmp_dtls_gen_cookie(SSL *ssl, unsigned char *cookie,
                                    unsigned int *cookie_len);
@@ -295,12 +297,12 @@ start_new_cached_connection(netsnmp_transport *t,
         DEBUGMSGTL(("dtlsudp",
                     "starting a new connection as a client to sock: %d\n",
                     t->sock));
-        tlsdata->ssl = SSL_new(sslctx_client_setup(DTLSv1_method(), tlsdata));
+        tlsdata->ssl = SSL_new(sslctx_client_setup(DTLS_method(), tlsdata));
 
         /* XXX: session setting 735 */
     } else {
         /* we're the server */
-        SSL_CTX *ctx = sslctx_server_setup(DTLSv1_method());
+        SSL_CTX *ctx = sslctx_server_setup(DTLS_method());
         if (!ctx) {
             BIO_free(cachep->read_bio);
             BIO_free(cachep->write_bio);
@@ -1537,7 +1539,9 @@ int netsnmp_dtls_gen_cookie(SSL *ssl, unsigned char *cookie,
     return 1;
 }
 
-int netsnmp_dtls_verify_cookie(SSL *ssl, unsigned char *cookie,
+int netsnmp_dtls_verify_cookie(SSL *ssl,
+                               SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER
+                               unsigned char *cookie,
                                unsigned int cookie_len)
 {
     unsigned char *buffer, result[EVP_MAX_MD_SIZE];

commit e926b1cec44ee769618440719dbcc92bb68f7aa7

libsnmp, TLS: Change default TLS version from v1.3 into v1.1
See also https://sourceforge.net/p/net-snmp/bugs/2920/.

Fixes: d686bc3502f1 ("CHANGES: snmplib: Add support for OpenSSL 1.1.0")

snmplib/transports/snmpTLSTCPDomain.c
@@ -722,7 +722,7 @@ netsnmp_tlstcp_open(netsnmp_transport *t)
         /* XXX: check securityLevel and ensure no NULL fingerprints are used */
 
         /* set up the needed SSL context */
-        tlsdata->ssl_context = ctx = sslctx_client_setup(TLS_method(), tlsdata);
+        tlsdata->ssl_context = ctx = sslctx_client_setup(TLSv1_method(), tlsdata);
         if (!ctx) {
             snmp_log(LOG_ERR, "failed to create TLS context\n");
             return NULL;
@@ -912,7 +912,7 @@ netsnmp_tlstcp_open(netsnmp_transport *t)
         }
 
         /* create the OpenSSL TLS context */
-        tlsdata->ssl_context = sslctx_server_setup(TLS_method());
+        tlsdata->ssl_context = sslctx_server_setup(TLSv1_method());
 
         t->sock = BIO_get_fd(tlsdata->accept_bio, NULL);
         t->flags |= NETSNMP_TRANSPORT_FLAG_LISTEN;