net-snmp/net-snmp

commit e422fbfb09cd7a7927d02deb35f890a84f351691

more checks for disabling protocol versions at runtime
Signed-off-by: Robert Story <rstory@freesnmp.com>

include/net-snmp/library/default_store.h
@@ -180,6 +180,64 @@ extern          "C" {
      * end storage definitions 
      */
 
+    /*
+     * macros for dynamic protocol switches
+     */
+#ifndef NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION
+
+#if !defined(NETSNMP_DISABLE_SNMPV1) || !defined(NETSNMP_DISABLE_SNMPV2C)
+
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver) \
+    ((pc_ver == SNMP_VERSION_1) &&                                     \
+     netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,                      \
+                            NETSNMP_DS_LIB_DISABLE_V1))
+
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver) \
+    ((pc_ver == SNMP_VERSION_2c) &&                                     \
+     netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,                      \
+                            NETSNMP_DS_LIB_DISABLE_V2c))
+
+#define NETSNMP_RUNTIME_PROTOCOL_CHECK_V1V2(pc_ver, pc_target) do {    \
+        if (NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver) ||                \
+            NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver))                  \
+            goto pc_target;                                            \
+    } while(0)
+#else
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver) (0)
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver) (0)
+#define NETSNMP_RUNTIME_PROTOCOL_CHECK_V1V2(ver, gt) do { ; } while(0)
+#endif
+
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V3(pc_ver) \
+    ((pc_ver == SNMP_VERSION_3) &&                                   \
+     netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,                     \
+                            NETSNMP_DS_LIB_DISABLE_V3))
+
+#define NETSNMP_RUNTIME_PROTOCOL_CHECK_V3(pc_ver, pc_target) do {      \
+        if (NETSNMP_RUNTIME_PROTOCOL_SKIP_V3(pc_ver))                  \
+            goto pc_target;                                            \
+    } while(0)
+
+#define NETSNMP_RUNTIME_PROTOCOL_CHECK(pc_ver, pc_target) do {         \
+        NETSNMP_RUNTIME_PROTOCOL_CHECK_V1V2(pc_ver, pc_target);            \
+        NETSNMP_RUNTIME_PROTOCOL_CHECK_V3(pc_ver, pc_target);            \
+    } while(0)
+
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP(pc_ver) \
+    (NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver) ||        \
+     NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver) ||        \
+     NETSNMP_RUNTIME_PROTOCOL_SKIP_V3(pc_ver))
+
+#else /* NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION */
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver) (0)
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver) (0)
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V3(pc_ver) (0)
+#define NETSNMP_RUNTIME_PROTOCOL_CHECK(ver, gt) do { ; } while(0)
+#define NETSNMP_RUNTIME_PROTOCOL_CHECK_V1V2(ver, gt) do { ; } while(0)
+#define NETSNMP_RUNTIME_PROTOCOL_CHECK_V3(ver, gt) do { ; } while(0)
+#endif /* NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION */
+
+
     NETSNMP_IMPORT
     int             netsnmp_ds_set_boolean(int storeid, int which, int value);
     NETSNMP_IMPORT

agent/agent_registry.c
@@ -2063,11 +2063,13 @@ in_a_view(oid *name, size_t *namelen, netsnmp_pdu *pdu, int type)
 #ifndef NETSNMP_DISABLE_SNMPV2C
     case SNMP_VERSION_2c:
 #endif
-    case SNMP_VERSION_3:
+   case SNMP_VERSION_3:
+        NETSNMP_RUNTIME_PROTOCOL_CHECK(pdu->version,unsupported_version);
         snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
                             SNMPD_CALLBACK_ACM_CHECK, &view_parms);
         return view_parms.errorcode;
     }
+  unsupported_version:
     return VACM_NOSECNAME;
 }
 
@@ -2104,10 +2106,12 @@ check_access(netsnmp_pdu *pdu)
     case SNMP_VERSION_2c:
 #endif
     case SNMP_VERSION_3:
+        NETSNMP_RUNTIME_PROTOCOL_CHECK(pdu->version,unsupported_version);
         snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
                             SNMPD_CALLBACK_ACM_CHECK_INITIAL, &view_parms);
         return view_parms.errorcode;
     }
+  unsupported_version:
     return 1;
 }
 
@@ -2148,10 +2152,12 @@ netsnmp_acm_check_subtree(netsnmp_pdu *pdu, oid *name, size_t namelen)
     case SNMP_VERSION_2c:
 #endif
     case SNMP_VERSION_3:
+        NETSNMP_RUNTIME_PROTOCOL_CHECK(pdu->version,unsupported_version);
         snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
                             SNMPD_CALLBACK_ACM_CHECK_SUBTREE, &view_parms);
         return view_parms.errorcode;
     }
+  unsupported_version:
     return 1;
 }
 

agent/agent_trap.c
@@ -1035,10 +1035,14 @@ send_trap_to_sess(netsnmp_session * sess, netsnmp_pdu *template_pdu)
 
 #ifndef NETSNMP_DISABLE_SNMPV1
     if (sess->version == SNMP_VERSION_1 &&
-        (template_pdu->command != SNMP_MSG_TRAP))
+        ((template_pdu->command != SNMP_MSG_TRAP) ||
+         netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                NETSNMP_DS_LIB_DISABLE_V1)))
         return;                 /* Skip v1 sinks for v2 only traps */
     if (sess->version != SNMP_VERSION_1 &&
-        (template_pdu->command == SNMP_MSG_TRAP))
+        ((template_pdu->command == SNMP_MSG_TRAP) ||
+         netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                NETSNMP_DS_LIB_DISABLE_V1)))
         return;                 /* Skip v2+ sinks for v1 only traps */
 #endif
     template_pdu->version = sess->version;
@@ -1274,6 +1278,11 @@ _parse_config_sink(const char *token, char *cptr, int version, int type)
     char           *st, *name = NULL, *tag = NULL, *profile = NULL;
     int            done = 0;
 
+    if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                               NETSNMP_DS_LIB_DISABLE_V1)) {
+        netsnmp_config_error("SNMPv1 disabled, cannot create trapsinks");
+        return;
+    }
     if (!snmp_trapcommunity)
         snmp_trapcommunity = strdup("public");
     sp = strtok_r(cptr, " \t\n", &st);
@@ -1323,6 +1332,11 @@ snmpd_parse_config_trapsink(const char *token, char *cptr)
 void
 snmpd_parse_config_trap2sink(const char *word, char *cptr)
 {
+    if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                               NETSNMP_DS_LIB_DISABLE_V1)) {
+        netsnmp_config_error("SNMPv2c disabled, cannot create trap2sinks");
+        return;
+    }
     _parse_config_sink(word, cptr, SNMP_VERSION_2c, SNMP_MSG_TRAP2);
 }
 
@@ -1590,7 +1604,9 @@ snmpd_parse_config_trapsess(const char *word, char *cptr)
     }
 
 #ifndef NETSNMP_DISABLE_SNMPV1
-    if (ss->version == SNMP_VERSION_1)
+    if ((ss->version == SNMP_VERSION_1) &&
+        !netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                NETSNMP_DS_LIB_DISABLE_V1))
         traptype = SNMP_MSG_TRAP;
 #endif
     netsnmp_add_notification_session(ss, traptype,

agent/mibgroup/ucd-snmp/proxy.c
@@ -305,16 +305,18 @@ proxy_fill_in_session(netsnmp_mib_handler *handler,
     }
 
 #if !defined(NETSNMP_DISABLE_SNMPV1) || !defined(NETSNMP_DISABLE_SNMPV2C)
-#if defined(NETSNMP_DISABLE_SNMPV1)
-    if (session->version == SNMP_VERSION_2c) {
-#else
-#if defined(NETSNMP_DISABLE_SNMPV2C)
-    if (session->version == SNMP_VERSION_1) {
-#else
-    if (session->version == SNMP_VERSION_1 ||
-        session->version == SNMP_VERSION_2c) {
+    if (
+#ifndef NETSNMP_DISABLE_SNMPV1
+        ((session->version == SNMP_VERSION_1) &&
+         !netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                 NETSNMP_DS_LIB_DISABLE_V1)) ||
 #endif
+#ifndef NETSNMP_DISABLE_SNMPV2C
+        ((session->version == SNMP_VERSION_2c) &&
+         !netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                 NETSNMP_DS_LIB_DISABLE_V2c)) ||
 #endif
+        0 ) { /* 0 to terminate '||' above */
 
         /*
          * Check if session has community string defined for it.

agent/mibgroup/utilities/iquery.c
@@ -19,6 +19,12 @@ netsnmp_parse_iquerySecLevel(const char *token, char *line)
 {
     int secLevel;
 
+#ifndef NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION
+    if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                               NETSNMP_DS_LIB_DISABLE_V3)) {
+        netsnmp_config_error("SNMPv3 disabled");
+    } else
+#endif
     if ((secLevel = parse_secLevel_conf( token, line )) >= 0 ) {
         netsnmp_ds_set_int(NETSNMP_DS_APPLICATION_ID,
                            NETSNMP_DS_AGENT_INTERNAL_SECLEVEL, secLevel);
@@ -31,22 +37,37 @@ void
 netsnmp_parse_iqueryVersion(const char *token, char *line)
 {
 #ifndef NETSNMP_DISABLE_SNMPV1
-    if (!strcmp( line, "1" ))
+    if (!strcmp( line, "1" )
+#ifndef NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION
+        && !netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                NETSNMP_DS_LIB_DISABLE_V1)
+#endif
+        )
         netsnmp_ds_set_int(NETSNMP_DS_APPLICATION_ID,
                            NETSNMP_DS_AGENT_INTERNAL_VERSION, SNMP_VERSION_1);
     else 
 #endif
 #ifndef NETSNMP_DISABLE_SNMPV2C
-         if (!strcmp( line, "2"  ) || !strcasecmp( line, "2c" ))
+        if ((!strcmp( line, "2"  ) || !strcasecmp( line, "2c" ))
+#ifndef NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION
+            && !netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                       NETSNMP_DS_LIB_DISABLE_V2c)
+#endif
+            )
         netsnmp_ds_set_int(NETSNMP_DS_APPLICATION_ID,
                            NETSNMP_DS_AGENT_INTERNAL_VERSION, SNMP_VERSION_2c);
     else 
 #endif
-         if (!strcmp( line, "3" ))
+         if (!strcmp( line, "3" )
+#ifndef NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION
+             && !netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                        NETSNMP_DS_LIB_DISABLE_V3)
+#endif
+             )
         netsnmp_ds_set_int(NETSNMP_DS_APPLICATION_ID,
                            NETSNMP_DS_AGENT_INTERNAL_VERSION, SNMP_VERSION_3);
     else {
-	netsnmp_config_error("Unknown version: %s", line);
+	netsnmp_config_error("Unknown/disabled version: %s", line);
     }
 }
 
@@ -132,7 +153,7 @@ void init_iquery(void){
 
 #ifndef NETSNMP_FEATURE_REMOVE_IQUERY_PDU_SESSION
 netsnmp_session *netsnmp_iquery_pdu_session(netsnmp_pdu* pdu) {
-    if (!pdu)
+    if (!pdu || NETSNMP_RUNTIME_PROTOCOL_SKIP(pdu->version))
        return NULL;
     if (pdu->version == SNMP_VERSION_3)
         return netsnmp_iquery_session( pdu->securityName, 
@@ -143,6 +164,7 @@ netsnmp_session *netsnmp_iquery_pdu_session(netsnmp_pdu* pdu) {
                            pdu->securityEngineIDLen);
     else
         return netsnmp_iquery_session((char *) pdu->community, 
+
                            pdu->version,
                            pdu->version+1,
                            SNMP_SEC_LEVEL_NOAUTH,
@@ -183,6 +205,8 @@ netsnmp_session *netsnmp_iquery_session(char* secName,   int   version,
     extern int callback_master_num;
     netsnmp_session *ss = NULL;
 
+    NETSNMP_RUNTIME_PROTOCOL_CHECK(version, unsupported_version);
+
 #ifdef NETSNMP_TRANSPORT_CALLBACK_DOMAIN
     ss = netsnmp_callback_open( callback_master_num, NULL, NULL, NULL);
     if (ss) {
@@ -203,6 +227,7 @@ netsnmp_session *netsnmp_iquery_session(char* secName,   int   version,
     }
 #endif
 
+  unsupported_version:
     return ss;
 }
 

snmplib/snmp_api.c
@@ -4113,22 +4113,14 @@ _snmp_parse(void *sessp,
     }
 
     switch (pdu->version) {
+#if !defined(NETSNMP_DISABLE_SNMPV1) || !defined(NETSNMP_DISABLE_SNMPV2C)
 #ifndef NETSNMP_DISABLE_SNMPV1
     case SNMP_VERSION_1:
 #endif
 #ifndef NETSNMP_DISABLE_SNMPV2C
     case SNMP_VERSION_2c:
 #endif
-#if !defined(NETSNMP_DISABLE_SNMPV1) || !defined(NETSNMP_DISABLE_SNMPV2C)
-#ifndef NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION
-        if (((pdu->version == SNMP_VERSION_1) &&
-             netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
-                                    NETSNMP_DS_LIB_DISABLE_V1)) ||
-            ((pdu->version == SNMP_VERSION_2c) &&
-             netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
-                                    NETSNMP_DS_LIB_DISABLE_V2c)))
-            goto unsupported_version;
-#endif
+        NETSNMP_RUNTIME_PROTOCOL_CHECK_V1V2(pdu->version,unsupported_version);
         DEBUGMSGTL(("snmp_api", "Parsing SNMPv%ld message...\n",
                     (1 + pdu->version)));
 
@@ -4199,11 +4191,7 @@ _snmp_parse(void *sessp,
 #endif /* support for community based SNMP */
 
     case SNMP_VERSION_3:
-#ifndef NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION
-        if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
-                                   NETSNMP_DS_LIB_DISABLE_V3))
-            goto unsupported_version;
-#endif
+        NETSNMP_RUNTIME_PROTOCOL_CHECK_V3(SNMP_VERSION_3,unsupported_version);
         result = snmpv3_parse(pdu, data, &length, NULL, session);
         DEBUGMSGTL(("snmp_parse",
                     "Parsed SNMPv3 message (secName:%s, secLevel:%s): %s\n",

commit 69253ba63cfaaaacc10942aad360449b2a081681

snmpd: Fix --disable-snmpv1 build
See also https://sourceforge.net/p/net-snmp/bugs/2924/.

Fixes: e422fbfb09cd ("more checks for disabling protocol versions at runtime").

include/net-snmp/library/default_store.h
@@ -193,17 +193,25 @@ extern          "C" {
      */
 #ifndef NETSNMP_FEATURE_REMOVE_RUNTIME_DISABLE_VERSION
 
-#if !defined(NETSNMP_DISABLE_SNMPV1) || !defined(NETSNMP_DISABLE_SNMPV2C)
-
-#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver) \
-    ((pc_ver == SNMP_VERSION_1) &&                                     \
+#if defined(NETSNMP_DISABLE_SNMPV1)
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver)                        \
+    ((pc_ver) == 0/*SNMP_VERSION_1*/)
+#else
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver)                        \
+    ((pc_ver) == SNMP_VERSION_1 &&                                      \
      netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,                      \
                             NETSNMP_DS_LIB_DISABLE_V1))
+#endif
 
-#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver) \
-    ((pc_ver == SNMP_VERSION_2c) &&                                     \
+#if defined(NETSNMP_DISABLE_SNMPV2C)
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver)                        \
+    ((pc_ver) == 1/*SNMP_VERSION_2c*/)
+#else
+#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver)                        \
+    ((pc_ver) == SNMP_VERSION_2c &&                                     \
      netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,                      \
                             NETSNMP_DS_LIB_DISABLE_V2c))
+#endif
 
 #define NETSNMP_RUNTIME_PROTOCOL_CHECK_V1V2(pc_ver, pc_target) do {    \
         if (NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver) ||                \
@@ -212,11 +220,6 @@ extern          "C" {
             goto pc_target;                                            \
         }                                                              \
     } while(0)
-#else
-#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V1(pc_ver) (0)
-#define NETSNMP_RUNTIME_PROTOCOL_SKIP_V2(pc_ver) (0)
-#define NETSNMP_RUNTIME_PROTOCOL_CHECK_V1V2(ver, gt) do { ; } while(0)
-#endif
 
 #define NETSNMP_RUNTIME_PROTOCOL_SKIP_V3(pc_ver) \
     ((pc_ver == SNMP_VERSION_3) &&                                   \

testing/fulltests/unit-tests/T102pdu_build_clib.c
@@ -10,7 +10,13 @@ int rc;
 
 init_snmp("testing");
 snmp_sess_init(&session);
+#if !defined(NETSNMP_DISABLE_SNMPV2C)
 session.version = SNMP_VERSION_2c;
+#elif !defined(NETSNMP_DISABLE_SNMPV1)
+session.version = SNMP_VERSION_1;
+#else
+#error This test does not (yet?) support SNMPv3
+#endif
 session.peername = strdup("udp:127.0.0.1"); /* we won't actually connect */
 session.community = (u_char *) strdup("bogus");
 session.community_len = strlen((char *) session.community);