d773eac3e0e4ea39d247895d849cbaa5c84922a2
Niels Baggesen <nba@users.sourceforge.net>
2019-04-24 21:58:56 +0200

    hr_disk_bsd: OpenBSD needs to include sys/dkio.h

--- agent/mibgroup/host/data_access/hr_disk_bsd.c
+++ agent/mibgroup/host/data_access/hr_disk_bsd.c
@@ -5,6 +5,12 @@
 #define DKTYPENAMES
 #include <sys/disklabel.h>
 #include <sys/disk.h>
+#if HAVE_SYS_IOCTL_H
+#include <sys/ioctl.h>
+#endif
+#if HAVE_SYS_DKIO_H
+#include <sys/dkio.h>
+#endif
 #include "../hr_disk.h"
 
 static struct disklabel HRD_info;
