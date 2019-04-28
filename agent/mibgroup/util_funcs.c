net-snmp/net-snmp

commit 9f4f13c6cb42f1161eac6d7fa780467d650137cb

CHANGES: Win32: BUG: 2779541 Fixed handle leak in pass_persist.
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@17568 06827809-a52a-0410-b366-d66718629ded

agent/mibgroup/util_funcs.c
@@ -631,10 +631,17 @@ get_exec_pipes(char *cmd, int *fdIn, int *fdOut, int *pid)
       return 0;
     }
     
+    DEBUGMSGTL(("util_funcs","child hProcess (stored in pid): %d\n",(int)pi.hProcess));
+    DEBUGMSGTL(("util_funcs","child dwProcessId (task manager): %d\n",(int)pi.dwProcessId));
+
     /* Set global child process handle */
     *pid = (int)pi.hProcess;
 
-    /* Close pipe handles to make sure that no handles to the write end of the
+    /* Cleanup */
+    if (!CloseHandle(pi.hThread))
+      DEBUGMSGTL(("util_funcs","get_exec_pipes CloseHandle pi.hThread: %d\n",cmd));
+
+   /* Close pipe handles to make sure that no handles to the write end of the
      * output pipe are maintained in this process or else the pipe will
      * not close when the child process exits and any calls to ReadFile 
      * will hang.

agent/mibgroup/ucd-snmp/pass_persist.c
@@ -808,6 +808,13 @@ close_persist_pipe(int iindex)
         close(persist_pipes[iindex].fdIn);
         persist_pipes[iindex].fdIn = -1;
     }
+
+#if defined(WIN32) && !defined (mingw32) && !defined (HAVE_SIGNAL)
+    if (!CloseHandle(persist_pipes[iindex].pid)) {
+          DEBUGMSGTL(("ucd-snmp/pass_persist","close_persist_pipe pid: close error\n"));
+        } 
+#endif
+    
 #ifdef __uClinux__
 	/*remove the pipes*/
 	unlink(fifo_in_path);

commit 21b0ea844db3e45ec170ddec0bab1cea083e3709

agent/mibgroup/util_funcs.c: Fix Windows debug statements
Fixes: 9f4f13c6cb42 ("CHANGES: Win32: BUG: 2779541 Fixed handle leak in pass_persist.") # v5.7.rc2.

agent/mibgroup/util_funcs.c
@@ -646,7 +646,9 @@ get_exec_pipes(char *cmd, int *fdIn, int *fdOut, netsnmp_pid_t *pid)
 
     /* Cleanup */
     if (!CloseHandle(pi.hThread))
-      DEBUGMSGTL(("util_funcs","get_exec_pipes CloseHandle pi.hThread: %d\n",cmd));
+      DEBUGMSGTL(("util_funcs",
+		  "get_exec_pipes(%s) CloseHandle pi.hThread: %d\n",
+		  cmd, (unsigned int)GetLastError()));
 
    /* Close pipe handles to make sure that no handles to the write end of the
      * output pipe are maintained in this process or else the pipe will
@@ -655,17 +657,20 @@ get_exec_pipes(char *cmd, int *fdIn, int *fdOut, netsnmp_pid_t *pid)
      */
 
     if (!CloseHandle(hOutputWrite)){
-      DEBUGMSGTL(("util_funcs","get_exec_pipes CloseHandle hOutputWrite: %d\n",
+      DEBUGMSGTL(("util_funcs",
+		  "get_exec_pipes(%s) CloseHandle hOutputWrite: %d\n",
                   cmd, (unsigned int)GetLastError()));
       return 0;
     }
     if (!CloseHandle(hInputRead)) {
-      DEBUGMSGTL(("util_funcs","get_exec_pipes CloseHandle hInputRead: %d\n",
+      DEBUGMSGTL(("util_funcs",
+		  "get_exec_pipes(%s) CloseHandle hInputRead: %d\n",
                   cmd, (unsigned int)GetLastError()));
       return 0;
     }
     if (!CloseHandle(hErrorWrite)) {
-      DEBUGMSGTL(("util_funcs","get_exec_pipes CloseHandle hErrorWrite: %d\n",
+      DEBUGMSGTL(("util_funcs",
+		  "get_exec_pipes(%s) CloseHandle hErrorWrite: %d\n",
                   cmd, (unsigned int)GetLastError()));
       return 0;
     }