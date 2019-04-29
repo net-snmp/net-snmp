net-snmp/net-snmp

commit 484524cda94838dfec8eb1ae92b069ea8ce448f6

configure: Determine endianness at compile time instead of at configure time
The new test supports cross-compilation. In other words, --with-endianness
no longer has to be specified during cross-compilation.

win32/net-snmp/net-snmp-config.h
@@ -1024,10 +1024,6 @@
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
 /* #undef TIME_WITH_SYS_TIME */
 
-/* Define to 1 if your processor stores words with the most significant byte
-   first (like Motorola and SPARC, unlike Intel and VAX). */
-/* #undef WORDS_BIGENDIAN */
-
 /* Define to 1 if on AIX 3.
    System headers sometimes define this.
    We just want to avoid a redefinition error message.  */

win32/net-snmp/net-snmp-config.h.in
@@ -1024,10 +1024,6 @@
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
 /* #undef TIME_WITH_SYS_TIME */
 
-/* Define to 1 if your processor stores words with the most significant byte
-   first (like Motorola and SPARC, unlike Intel and VAX). */
-/* #undef WORDS_BIGENDIAN */
-
 /* Define to 1 if on AIX 3.
    System headers sometimes define this.
    We just want to avoid a redefinition error message.  */

include/net-snmp/net-snmp-config.h.in
@@ -54,9 +54,6 @@
 
 #define config_parse_dot_conf(w,x,y,z)
 
-/* Define if building universal (internal helper macro) */
-#undef AC_APPLE_UNIVERSAL_BUILD
-
 /* Define to one of `_getb67', `GETB67', `getb67' for Cray-2 and Cray-YMP
    systems. This function is required for `alloca.c' support on those systems.
    */
@@ -1737,18 +1734,6 @@
 #endif
 
 
-/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
-#if defined AC_APPLE_UNIVERSAL_BUILD
-# if defined __BIG_ENDIAN__
-#  define WORDS_BIGENDIAN 1
-# endif
-#else
-# ifndef WORDS_BIGENDIAN
-#  undef WORDS_BIGENDIAN
-# endif
-#endif
-
 /* Define to 1 if on MINIX. */
 #undef _MINIX
 
@@ -1861,6 +1846,33 @@
 #endif
 
 #endif /* NETSNMP_NO_AUTOCONF_DEFINITIONS */
+
+/*
+ * Note: GCC 4.6 added __BYTE_ORDER__, __ORDER_BIG_ENDIAN__,
+ * __ORDER_LITTLE_ENDIAN__ and __ORDER_PDP_ENDIAN__ in SVN r165881. For older
+ * gcc versions and also for other compilers, determine the endianness using
+ * an expression that can be evaluated at compile time. To do: add support for
+ * other compilers than gcc that do not support statement expressions.
+ */
+#if defined(__BYTE_ORDER__)
+# if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#  define NETSNMP_BIGENDIAN 1
+# elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#  define NETSNMP_BIGENDIAN 0
+# else
+#  error Unknown byte order
+# endif
+#else
+# define NETSNMP_BIGENDIAN                              \
+    (__extension__                                      \
+     ({                                                 \
+         union { unsigned long l; unsigned char c; } u; \
+                                                        \
+         u.l = 1;                                       \
+         u.c == 0;                                      \
+     })                                                 \
+    )
+#endif
 
 
 

agent/mibgroup/agentx/protocol.c
@@ -98,9 +98,7 @@ agentx_realloc_build_int(u_char ** buf, size_t * buf_len, size_t * out_len,
 {
     unsigned int    ivalue = value;
     size_t          ilen = *out_len;
-#ifdef WORDS_BIGENDIAN
     unsigned int    i = 0;
-#endif
 
     while ((*out_len + 4) >= *buf_len) {
         if (!(allow_realloc && snmp_realloc(buf, buf_len))) {
@@ -109,22 +107,21 @@ agentx_realloc_build_int(u_char ** buf, size_t * buf_len, size_t * out_len,
     }
 
     if (network_order) {
-#ifndef WORDS_BIGENDIAN
-        value = ntohl(value);
-#endif
+        if (!NETSNMP_BIGENDIAN)
+            value = ntohl(value);
         memmove((*buf + *out_len), &value, 4);
         *out_len += 4;
     } else {
-#ifndef WORDS_BIGENDIAN
-        memmove((*buf + *out_len), &value, 4);
-        *out_len += 4;
-#else
-        for (i = 0; i < 4; i++) {
-            *(*buf + *out_len) = (u_char) value & 0xff;
-            (*out_len)++;
-            value >>= 8;
+        if (!NETSNMP_BIGENDIAN) {
+            memmove((*buf + *out_len), &value, 4);
+            *out_len += 4;
+        } else {
+            for (i = 0; i < 4; i++) {
+                *(*buf + *out_len) = (u_char) value & 0xff;
+                (*out_len)++;
+                value >>= 8;
+            }
         }
-#endif
     }
     DEBUGDUMPSETUP("send", (*buf + ilen), 4);
     DEBUGMSG(("dumpv_send", "  Integer:\t%u (0x%.2X)\n", ivalue,
@@ -139,25 +136,24 @@ agentx_build_int(u_char * bufp, u_int value, int network_byte_order)
     u_int           orig_val = value;
 
     if (network_byte_order) {
-#ifndef WORDS_BIGENDIAN
-        value = ntohl(value);
-#endif
+        if (!NETSNMP_BIGENDIAN)
+            value = ntohl(value);
         memmove(bufp, &value, 4);
     } else {
-#ifndef WORDS_BIGENDIAN
-        memmove(bufp, &value, 4);
-#else
-        *bufp = (u_char) value & 0xff;
-        value >>= 8;
-        bufp++;
-        *bufp = (u_char) value & 0xff;
-        value >>= 8;
-        bufp++;
-        *bufp = (u_char) value & 0xff;
-        value >>= 8;
-        bufp++;
-        *bufp = (u_char) value & 0xff;
-#endif
+        if (!NETSNMP_BIGENDIAN) {
+            memmove(bufp, &value, 4);
+        } else {
+            *bufp = (u_char) value & 0xff;
+            value >>= 8;
+            bufp++;
+            *bufp = (u_char) value & 0xff;
+            value >>= 8;
+            bufp++;
+            *bufp = (u_char) value & 0xff;
+            value >>= 8;
+            bufp++;
+            *bufp = (u_char) value & 0xff;
+        }
     }
     DEBUGDUMPSETUP("send", orig_bufp, 4);
     DEBUGMSG(("dumpv_send", "  Integer:\t%u (0x%.2X)\n", orig_val,
@@ -171,9 +167,7 @@ agentx_realloc_build_short(u_char ** buf, size_t * buf_len,
 {
     unsigned short  ivalue = value;
     size_t          ilen = *out_len;
-#ifdef WORDS_BIGENDIAN
     unsigned short  i = 0;
-#endif
 
     while ((*out_len + 2) >= *buf_len) {
         if (!(allow_realloc && snmp_realloc(buf, buf_len))) {
@@ -182,22 +176,21 @@ agentx_realloc_build_short(u_char ** buf, size_t * buf_len,
     }
 
     if (network_order) {
-#ifndef WORDS_BIGENDIAN
-        value = ntohs(value);
-#endif
+        if (!NETSNMP_BIGENDIAN)
+            value = ntohs(value);
         memmove((*buf + *out_len), &value, 2);
         *out_len += 2;
     } else {
-#ifndef WORDS_BIGENDIAN
-        memmove((*buf + *out_len), &value, 2);
-        *out_len += 2;
-#else
-        for (i = 0; i < 2; i++) {
-            *(*buf + *out_len) = (u_char) value & 0xff;
-            (*out_len)++;
-            value >>= 8;
+        if (!NETSNMP_BIGENDIAN) {
+            memmove((*buf + *out_len), &value, 2);
+            *out_len += 2;
+        } else {
+            for (i = 0; i < 2; i++) {
+                *(*buf + *out_len) = (u_char) value & 0xff;
+                (*out_len)++;
+                value >>= 8;
+            }
         }
-#endif
     }
     DEBUGDUMPSETUP("send", (*buf + ilen), 2);
     DEBUGMSG(("dumpv_send", "  Short:\t%hu (0x%.2hX)\n", ivalue, ivalue));
@@ -1045,25 +1038,24 @@ agentx_parse_int(const u_char *data, u_int network_byte_order)
     DEBUGDUMPSETUP("recv", data, 4);
     if (network_byte_order) {
         memmove(&value, data, 4);
-#ifndef WORDS_BIGENDIAN
-        value = ntohl(value);
-#endif
+        if (!NETSNMP_BIGENDIAN)
+            value = ntohl(value);
     } else {
-#ifndef WORDS_BIGENDIAN
-        memmove(&value, data, 4);
-#else
-        /*
-         * The equivalent of the 'ntohl()' macro,
-         * except this macro is null on big-endian systems 
-         */
-        value += data[3];
-        value <<= 8;
-        value += data[2];
-        value <<= 8;
-        value += data[1];
-        value <<= 8;
-        value += data[0];
-#endif
+        if (!NETSNMP_BIGENDIAN) {
+            memmove(&value, data, 4);
+        } else {
+            /*
+             * The equivalent of the 'ntohl()' macro,
+             * except this macro is null on big-endian systems
+             */
+            value += data[3];
+            value <<= 8;
+            value += data[2];
+            value <<= 8;
+            value += data[1];
+            value <<= 8;
+            value += data[0];
+        }
     }
     DEBUGMSG(("dumpv_recv", "  Integer:\t%u (0x%.2X)\n", value, value));
 
@@ -1078,21 +1070,20 @@ agentx_parse_short(const u_char *data, u_int network_byte_order)
 
     if (network_byte_order) {
         memmove(&value, data, 2);
-#ifndef WORDS_BIGENDIAN
-        value = ntohs(value);
-#endif
+        if (!NETSNMP_BIGENDIAN)
+            value = ntohs(value);
     } else {
-#ifndef WORDS_BIGENDIAN
-        memmove(&value, data, 2);
-#else
-        /*
-         * The equivalent of the 'ntohs()' macro,
-         * except this macro is null on big-endian systems 
-         */
-        value += data[1];
-        value <<= 8;
-        value += data[0];
-#endif
+        if (!NETSNMP_BIGENDIAN) {
+            memmove(&value, data, 2);
+        } else {
+            /*
+             * The equivalent of the 'ntohs()' macro,
+             * except this macro is null on big-endian systems
+             */
+            value += data[1];
+            value <<= 8;
+            value += data[0];
+        }
     }
 
     DEBUGDUMPSETUP("recv", data, 2);

configure
@@ -4625,6 +4625,9 @@ fi
 if test "${with_endianness+set}" = set; then :
   withval=$with_endianness; if test $with_endianness != "big" -a $with_endianness != "little" ; then
        as_fn_error $? "Endianness must be big or little, not \"$with_endianness\"." "$LINENO" 5;
+     else
+       { $as_echo "$as_me:${as_lineno-$LINENO}: WARNING: --with-endianness is obsolete" >&5
+$as_echo "$as_me: WARNING: --with-endianness is obsolete" >&2;}
      fi
 
 fi
@@ -26705,236 +26708,6 @@ CFLAGS=$netsnmp_save_CFLAGS
 #   Compilation characteristics
 ##
 
-#       Byte order
-#
-if test "$with_endianness" = "big"; then
-    ac_cv_c_bigendian=yes
-elif test "$with_endianness" = "little" ; then
-    ac_cv_c_bigendian=no
-fi
- { $as_echo "$as_me:${as_lineno-$LINENO}: checking whether byte ordering is bigendian" >&5
-$as_echo_n "checking whether byte ordering is bigendian... " >&6; }
-if ${ac_cv_c_bigendian+:} false; then :
-  $as_echo_n "(cached) " >&6
-else
-  ac_cv_c_bigendian=unknown
-    # See if we're dealing with a universal compiler.
-    cat confdefs.h - <<_ACEOF >conftest.$ac_ext
-/* end confdefs.h.  */
-#ifndef __APPLE_CC__
-	       not a universal capable compiler
-	     #endif
-	     typedef int dummy;
-
-_ACEOF
-if ac_fn_c_try_compile "$LINENO"; then :
-
-	# Check for potential -arch flags.  It is not universal unless
-	# there are at least two -arch flags with different values.
-	ac_arch=
-	ac_prev=
-	for ac_word in $CC $CFLAGS $CPPFLAGS $LDFLAGS; do
-	 if test -n "$ac_prev"; then
-	   case $ac_word in
-	     i?86 | x86_64 | ppc | ppc64)
-	       if test -z "$ac_arch" || test "$ac_arch" = "$ac_word"; then
-		 ac_arch=$ac_word
-	       else
-		 ac_cv_c_bigendian=universal
-		 break
-	       fi
-	       ;;
-	   esac
-	   ac_prev=
-	 elif test "x$ac_word" = "x-arch"; then
-	   ac_prev=arch
-	 fi
-       done
-fi
-rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
-    if test $ac_cv_c_bigendian = unknown; then
-      # See if sys/param.h defines the BYTE_ORDER macro.
-      cat confdefs.h - <<_ACEOF >conftest.$ac_ext
-/* end confdefs.h.  */
-#include <sys/types.h>
-	     #include <sys/param.h>
-
-int
-main ()
-{
-#if ! (defined BYTE_ORDER && defined BIG_ENDIAN \
-		     && defined LITTLE_ENDIAN && BYTE_ORDER && BIG_ENDIAN \
-		     && LITTLE_ENDIAN)
-	      bogus endian macros
-	     #endif
-
-  ;
-  return 0;
-}
-_ACEOF
-if ac_fn_c_try_compile "$LINENO"; then :
-  # It does; now see whether it defined to BIG_ENDIAN or not.
-	 cat confdefs.h - <<_ACEOF >conftest.$ac_ext
-/* end confdefs.h.  */
-#include <sys/types.h>
-		#include <sys/param.h>
-
-int
-main ()
-{
-#if BYTE_ORDER != BIG_ENDIAN
-		 not big endian
-		#endif
-
-  ;
-  return 0;
-}
-_ACEOF
-if ac_fn_c_try_compile "$LINENO"; then :
-  ac_cv_c_bigendian=yes
-else
-  ac_cv_c_bigendian=no
-fi
-rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
-fi
-rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
-    fi
-    if test $ac_cv_c_bigendian = unknown; then
-      # See if <limits.h> defines _LITTLE_ENDIAN or _BIG_ENDIAN (e.g., Solaris).
-      cat confdefs.h - <<_ACEOF >conftest.$ac_ext
-/* end confdefs.h.  */
-#include <limits.h>
-
-int
-main ()
-{
-#if ! (defined _LITTLE_ENDIAN || defined _BIG_ENDIAN)
-	      bogus endian macros
-	     #endif
-
-  ;
-  return 0;
-}
-_ACEOF
-if ac_fn_c_try_compile "$LINENO"; then :
-  # It does; now see whether it defined to _BIG_ENDIAN or not.
-	 cat confdefs.h - <<_ACEOF >conftest.$ac_ext
-/* end confdefs.h.  */
-#include <limits.h>
-
-int
-main ()
-{
-#ifndef _BIG_ENDIAN
-		 not big endian
-		#endif
-
-  ;
-  return 0;
-}
-_ACEOF
-if ac_fn_c_try_compile "$LINENO"; then :
-  ac_cv_c_bigendian=yes
-else
-  ac_cv_c_bigendian=no
-fi
-rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
-fi
-rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
-    fi
-    if test $ac_cv_c_bigendian = unknown; then
-      # Compile a test program.
-      if test "$cross_compiling" = yes; then :
-  # Try to guess by grepping values from an object file.
-	 cat confdefs.h - <<_ACEOF >conftest.$ac_ext
-/* end confdefs.h.  */
-short int ascii_mm[] =
-		  { 0x4249, 0x4765, 0x6E44, 0x6961, 0x6E53, 0x7953, 0 };
-		short int ascii_ii[] =
-		  { 0x694C, 0x5454, 0x656C, 0x6E45, 0x6944, 0x6E61, 0 };
-		int use_ascii (int i) {
-		  return ascii_mm[i] + ascii_ii[i];
-		}
-		short int ebcdic_ii[] =
-		  { 0x89D3, 0xE3E3, 0x8593, 0x95C5, 0x89C4, 0x9581, 0 };
-		short int ebcdic_mm[] =
-		  { 0xC2C9, 0xC785, 0x95C4, 0x8981, 0x95E2, 0xA8E2, 0 };
-		int use_ebcdic (int i) {
-		  return ebcdic_mm[i] + ebcdic_ii[i];
-		}
-		extern int foo;
-
-int
-main ()
-{
-return use_ascii (foo) == use_ebcdic (foo);
-  ;
-  return 0;
-}
-_ACEOF
-if ac_fn_c_try_compile "$LINENO"; then :
-  if grep BIGenDianSyS conftest.$ac_objext >/dev/null; then
-	      ac_cv_c_bigendian=yes
-	    fi
-	    if grep LiTTleEnDian conftest.$ac_objext >/dev/null ; then
-	      if test "$ac_cv_c_bigendian" = unknown; then
-		ac_cv_c_bigendian=no
-	      else
-		# finding both strings is unlikely to happen, but who knows?
-		ac_cv_c_bigendian=unknown
-	      fi
-	    fi
-fi
-rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
-else
-  cat confdefs.h - <<_ACEOF >conftest.$ac_ext
-/* end confdefs.h.  */
-$ac_includes_default
-int
-main ()
-{
-
-	     /* Are we little or big endian?  From Harbison&Steele.  */
-	     union
-	     {
-	       long int l;
-	       char c[sizeof (long int)];
-	     } u;
-	     u.l = 1;
-	     return u.c[sizeof (long int) - 1] == 1;
-
-  ;
-  return 0;
-}
-_ACEOF
-if ac_fn_c_try_run "$LINENO"; then :
-  ac_cv_c_bigendian=no
-else
-  ac_cv_c_bigendian=yes
-fi
-rm -f core *.core core.conftest.* gmon.out bb.out conftest$ac_exeext \
-  conftest.$ac_objext conftest.beam conftest.$ac_ext
-fi
-
-    fi
-fi
-{ $as_echo "$as_me:${as_lineno-$LINENO}: result: $ac_cv_c_bigendian" >&5
-$as_echo "$ac_cv_c_bigendian" >&6; }
- case $ac_cv_c_bigendian in #(
-   yes)
-     $as_echo "#define WORDS_BIGENDIAN 1" >>confdefs.h
-;; #(
-   no)
-      ;; #(
-   universal)
-
-$as_echo "#define AC_APPLE_UNIVERSAL_BUILD 1" >>confdefs.h
-
-     ;; #(
-   *)
-     as_fn_error $? "Failed to figure out host endianness, please use --with-endianness to specify it." "$LINENO" 5 ;;
- esac
-
 
 cat >confcache <<\_ACEOF
 # This file is a shell script that caches the results of configure
@@ -32116,7 +31889,6 @@ done
 LIBOBJS=$ac_libobjs
 
 LTLIBOBJS=$ac_ltlibobjs
-
 
 
 

configure.d/config_net_snmp_config_h
@@ -56,6 +56,33 @@ AH_BOTTOM([/* end of definitions added by configure on-the-fly */
 
 #endif /* NETSNMP_NO_AUTOCONF_DEFINITIONS */
 
+/*
+ * Note: GCC 4.6 added __BYTE_ORDER__, __ORDER_BIG_ENDIAN__,
+ * __ORDER_LITTLE_ENDIAN__ and __ORDER_PDP_ENDIAN__ in SVN r165881. For older
+ * gcc versions and also for other compilers, determine the endianness using
+ * an expression that can be evaluated at compile time. To do: add support for
+ * other compilers than gcc that do not support statement expressions.
+ */
+#if defined(__BYTE_ORDER__)
+# if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#  define NETSNMP_BIGENDIAN 1
+# elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#  define NETSNMP_BIGENDIAN 0
+# else
+#  error Unknown byte order
+# endif
+#else
+# define NETSNMP_BIGENDIAN                              \
+    (__extension__                                      \
+     ({                                                 \
+         union { unsigned long l; unsigned char c; } u; \
+                                                        \
+         u.l = 1;                                       \
+         u.c == 0;                                      \
+     })                                                 \
+    )
+#endif
+
 
 
 

configure.d/config_os_misc3
@@ -9,12 +9,3 @@
 #   Compilation characteristics
 ##
 
-#       Byte order
-#
-if test "$with_endianness" = "big"; then
-    ac_cv_c_bigendian=yes
-elif test "$with_endianness" = "little" ; then
-    ac_cv_c_bigendian=no
-fi
-AC_C_BIGENDIAN(,,
-    [AC_MSG_ERROR([Failed to figure out host endianness, please use --with-endianness to specify it.])])

configure.d/config_project_with_enable
@@ -27,6 +27,8 @@ AC_ARG_WITH(endianness,
                                   cross-compiling.],
     [if test $with_endianness != "big" -a $with_endianness != "little" ; then
        AC_MSG_ERROR([Endianness must be big or little, not "$with_endianness".]);
+     else
+       AC_MSG_WARN([--with-endianness is obsolete])
      fi
 ])
 

commit f39d5486bd66adde244fb0c6fbaf8d55e22918cd

MSVC: Define the NETSNMP_BIGENDIAN macro
Fixes: 484524cda948 ("configure: Determine endianness at compile time instead of at configure time")

win32/net-snmp/net-snmp-config.h
@@ -1176,6 +1176,14 @@
 /* define if SIOCGIFADDR exists in sys/ioctl.h */
 /* #undef SYS_IOCTL_H_HAS_SIOCGIFADDR */
 
+#if defined(_M_PPC) || defined(_M_MPPC)
+# define NETSNMP_BIGENDIAN 1
+#elif defined(_M_IX86) || defined(_M_X64)
+# define NETSNMP_BIGENDIAN 0
+#else
+#  error Unknown byte order
+#endif
+
 /* Mib-2 tree Info */
 /* These are the system information variables. */
 

win32/net-snmp/net-snmp-config.h.in
@@ -1176,6 +1176,14 @@
 /* define if SIOCGIFADDR exists in sys/ioctl.h */
 /* #undef SYS_IOCTL_H_HAS_SIOCGIFADDR */
 
+#if defined(_M_PPC) || defined(_M_MPPC)
+# define NETSNMP_BIGENDIAN 1
+#elif defined(_M_IX86) || defined(_M_X64)
+# define NETSNMP_BIGENDIAN 0
+#else
+#  error Unknown byte order
+#endif
+
 /* Mib-2 tree Info */
 /* These are the system information variables. */
 