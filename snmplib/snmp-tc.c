net-snmp/net-snmp

commit af3fc499f6007f56623f3b1b046512e985505a1e

snmplib: Initialize tm_isdst if available
This patch fixes Coverity ID 144417.

configure
@@ -27824,6 +27824,29 @@ _ACEOF
 
 
 fi
+ac_fn_c_check_member "$LINENO" "struct time" "tm_isdst" "ac_cv_member_struct_time_tm_isdst" "
+    $ac_includes_default
+
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
+"
+if test "x$ac_cv_member_struct_time_tm_isdst" = xyes; then :
+
+cat >>confdefs.h <<_ACEOF
+#define HAVE_STRUCT_TIME_TM_ISDST 1
+_ACEOF
+
+
+fi
 
 
 #       extern timezone

configure.d/config_os_struct_members
@@ -306,7 +306,7 @@ AC_CHECK_MEMBERS([struct tcpstat.tcp_rcvmemdrop],,,[
 #       struct tm
 #   Library:
 #
-AC_CHECK_MEMBERS([struct tm.tm_gmtoff],,,[
+AC_CHECK_MEMBERS([struct tm.tm_gmtoff, struct time.tm_isdst],,,[
     AC_INCLUDES_DEFAULT()
     [
 #if TIME_WITH_SYS_TIME

include/net-snmp/net-snmp-config.h.in
@@ -1064,6 +1064,9 @@
 /* Define to 1 if `tz_dsttime' is a member of `struct timezone'. */
 #undef HAVE_STRUCT_TIMEZONE_TZ_DSTTIME
 
+/* Define to 1 if `tm_isdst' is a member of `struct time'. */
+#undef HAVE_STRUCT_TIME_TM_ISDST
+
 /* Define to 1 if `tm_gmtoff' is a member of `struct tm'. */
 #undef HAVE_STRUCT_TM_TM_GMTOFF
 

snmplib/snmp-tc.c
@@ -254,13 +254,9 @@ ctime_to_timet(const char *str)
      *  Cope with timezone and DST
      */
 
-#ifdef SYSV
-    if (daylight)
-        tm.tm_isdst = 1;
-
+#ifdef HAVE_STRUCT_TIME_TM_ISDST
+    tm.tm_isdst = !!daylight;
     tm.tm_sec -= timezone;
-#else
-    tm.tm_isdst = 0;
 #endif
 
     return (mktime(&tm));

commit 39121a995d09484766ee5e86137ade1123bea7ab

snmplib: Fix tm.tm_isdst check in ctime_to_timet()
This patch fixes Coverity issue 144417.

Fixes: af3fc499f600 ("snmplib: Initialize tm_isdst if available") # v5.8.

configure
@@ -29110,7 +29110,7 @@ _ACEOF
 
 
 fi
-ac_fn_c_check_member "$LINENO" "struct time" "tm_isdst" "ac_cv_member_struct_time_tm_isdst" "
+ac_fn_c_check_member "$LINENO" "struct tm" "tm_isdst" "ac_cv_member_struct_tm_tm_isdst" "
     $ac_includes_default
 
 #if TIME_WITH_SYS_TIME
@@ -29125,10 +29125,10 @@ ac_fn_c_check_member "$LINENO" "struct time" "tm_isdst" "ac_cv_member_struct_tim
 #endif
 
 "
-if test "x$ac_cv_member_struct_time_tm_isdst" = xyes; then :
+if test "x$ac_cv_member_struct_tm_tm_isdst" = xyes; then :
 
 cat >>confdefs.h <<_ACEOF
-#define HAVE_STRUCT_TIME_TM_ISDST 1
+#define HAVE_STRUCT_TM_TM_ISDST 1
 _ACEOF
 
 

configure.d/config_os_struct_members
@@ -306,7 +306,7 @@ AC_CHECK_MEMBERS([struct tcpstat.tcp_rcvmemdrop],,,[
 #       struct tm
 #   Library:
 #
-AC_CHECK_MEMBERS([struct tm.tm_gmtoff, struct time.tm_isdst],,,[
+AC_CHECK_MEMBERS([struct tm.tm_gmtoff, struct tm.tm_isdst],,,[
     AC_INCLUDES_DEFAULT()
     [
 #if TIME_WITH_SYS_TIME

include/net-snmp/net-snmp-config.h.in
@@ -1093,11 +1093,11 @@
 /* Define to 1 if `tz_dsttime' is a member of `struct timezone'. */
 #undef HAVE_STRUCT_TIMEZONE_TZ_DSTTIME
 
-/* Define to 1 if `tm_isdst' is a member of `struct time'. */
-#undef HAVE_STRUCT_TIME_TM_ISDST
-
 /* Define to 1 if `tm_gmtoff' is a member of `struct tm'. */
 #undef HAVE_STRUCT_TM_TM_GMTOFF
+
+/* Define to 1 if `tm_isdst' is a member of `struct tm'. */
+#undef HAVE_STRUCT_TM_TM_ISDST
 
 /* Define to 1 if `udps_discard' is a member of `struct udpstat'. */
 #undef HAVE_STRUCT_UDPSTAT_UDPS_DISCARD

snmplib/snmp-tc.c
@@ -254,7 +254,7 @@ ctime_to_timet(const char *str)
      *  Cope with timezone and DST
      */
 
-#ifdef HAVE_STRUCT_TIME_TM_ISDST
+#ifdef HAVE_STRUCT_TM_TM_ISDST
     tm.tm_isdst = !!daylight;
     tm.tm_sec -= timezone;
 #endif