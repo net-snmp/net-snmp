8f92cadfecedc4bc6bc56c24d8282d11f82b06c3
Bart Van Assche <bvanassche@acm.org>
2019-05-06 21:20:13 -0700

    ci: Improve the code that supports various Windows builds
    Install openssl-devel again on MSYS2. Fix the MSYS2 and MinGW32 code for
    detecting the OpenSSL version. Disable Python support again for MSYS2.

--- ci/build.sh
+++ ci/build.sh
@@ -3,8 +3,18 @@
 scriptdir="$(dirname "$0")"
 export NOAUTODEPS=1
 export SNMP_VERBOSE=1
-case $(uname) in
-    MINGW64*|MSYS*)
+case "$(uname -a)" in
+    MSYS*x86_64*)
+	pacman --noconfirm --remove mingw-w64-x86_64-gcc-ada
+	pacman --noconfirm --remove mingw-w64-x86_64-gcc-fortran
+	pacman --noconfirm --remove mingw-w64-x86_64-gcc-libgfortran
+	pacman --noconfirm --remove mingw-w64-x86_64-gcc-objc
+	pacman --noconfirm --sync --refresh
+	pacman --noconfirm --sync --needed diffutils
+	pacman --noconfirm --sync --needed make
+	pacman --noconfirm --sync --needed openssl-devel
+	;;
+    MINGW64*)
 	pacman --noconfirm --remove mingw-w64-x86_64-gcc-ada
 	pacman --noconfirm --remove mingw-w64-x86_64-gcc-fortran
 	pacman --noconfirm --remove mingw-w64-x86_64-gcc-libgfortran
@@ -15,15 +25,7 @@ case $(uname) in
 	pacman --noconfirm --sync --needed mingw-w64-x86_64-libmariadbclient
 	pacman --noconfirm --sync --needed mingw-w64-x86_64-gcc
 	pacman --noconfirm --sync --needed mingw-w64-x86_64-openssl
-	;;
-esac
-case "${BUILD}" in
-    MSYS2)
-	;;
-    MinGW64)
 	export PATH="/mingw64/bin:$PATH"
-	;;
-    *)
 	;;
 esac
 echo "compiler path: $(type -p gcc)"
