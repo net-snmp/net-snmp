net-snmp/net-snmp

commit b3f3cb8b4948a14663b5071348b95e43a16a4360

ci/build.sh: Dump config.log if 'make' fails

ci/build.sh
@@ -30,7 +30,8 @@ case "$(uname -a)" in
 esac
 echo "compiler path: $(type -p gcc)"
 "${scriptdir}"/net-snmp-configure master || exit $?
-make -s                                  || exit $?
+make -s                                  ||
+    { echo "config.log:"; cat config.log; exit $?; }
 case "$MODE" in
     disable-set|mini*|read-only)
         exit 0;;

commit c77172ae94bde978c18cf02437e0f2d3875ff75a

ci/build.sh: Fail the build if "make" fails
Fixes: b3f3cb8b4948 ("ci/build.sh: Dump config.log if 'make' fails")

ci/build.sh
@@ -31,7 +31,7 @@ esac
 echo "compiler path: $(type -p gcc)"
 "${scriptdir}"/net-snmp-configure master || exit $?
 make -s                                  ||
-    { echo "config.log:"; cat config.log; exit $?; }
+    { echo "config.log:"; cat config.log; exit 1; }
 case "$MODE" in
     disable-set|mini*|read-only)
         exit 0;;