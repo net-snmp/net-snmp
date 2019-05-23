net-snmp/net-snmp

commit 48593bb6abd20f4817504206ac672ee396c02e47

include/net-snmp/types.h: Include <windows.h> explicitly

include/net-snmp/types.h
@@ -62,7 +62,8 @@
 # endif
 #endif
 
-#if defined(WIN32) && !defined(cygwin)
+#ifdef HAVE_WINDOWS_H
+#include <windows.h>
 typedef HANDLE netsnmp_pid_t;
 #define NETSNMP_NO_SUCH_PROCESS INVALID_HANDLE_VALUE
 #else

win32/net-snmp/net-snmp-config.h
@@ -958,6 +958,9 @@
 /* Define to 1 if you have the `vsnprintf' function. */
 #define HAVE_VSNPRINTF 1
 
+/* Define to 1 if you have the <windows.h> header file. */
+#define HAVE_WINDOWS_H 1
+
 /* Define to 1 if you have the <winsock.h> header file. */
 #define HAVE_WINSOCK_H 1
 

win32/net-snmp/net-snmp-config.h.in
@@ -958,6 +958,9 @@
 /* Define to 1 if you have the `vsnprintf' function. */
 #define HAVE_VSNPRINTF 1
 
+/* Define to 1 if you have the <windows.h> header file. */
+#define HAVE_WINDOWS_H 1
+
 /* Define to 1 if you have the <winsock.h> header file. */
 #define HAVE_WINSOCK_H 1
 

commit f100abcd4dfd386f8c5be2169beed199756404a9

Cygwin: Unbreak the build
Fixes: 48593bb6abd2 ("include/net-snmp/types.h: Include <windows.h> explicitly")

include/net-snmp/types.h
@@ -62,7 +62,7 @@
 # endif
 #endif
 
-#ifdef HAVE_WINDOWS_H
+#if defined(HAVE_WINDOWS_H) && !defined(__CYGWIN__)
 #include <windows.h>
 typedef HANDLE netsnmp_pid_t;
 #define NETSNMP_NO_SUCH_PROCESS INVALID_HANDLE_VALUE