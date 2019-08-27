net-snmp/net-snmp

commit 58ae9e6b11d8586528249c3a5b62ad221e91f67b

update all notification tables to handle embedded nulls in names
Signed-off-by: Robert Story <rstory@freesnmp.com>

agent/mibgroup/target/snmpTargetAddrEntry_data.c
@@ -37,11 +37,12 @@ get_addrTable(void)
 }
 
 struct targetAddrTable_struct *
-get_addrForName(const char *name)
+get_addrForName2(const char *name, size_t nameLen)
 {
     struct targetAddrTable_struct *ptr;
     for (ptr = aAddrTable; ptr != NULL; ptr = ptr->next) {
-        if (ptr->name && strcmp(ptr->name, name) == 0)
+        if (ptr->nameLen == nameLen && ptr->nameData &&
+            memcmp(ptr->nameData, name, nameLen) == 0)
             return ptr;
     }
     return NULL;
@@ -65,7 +66,8 @@ snmpTargetAddrTable_create(void)
         newEntry->timeout = 1500;
         newEntry->retryCount = 3;
 
-        newEntry->tagList = strdup("");
+        newEntry->tagListData = strdup("");
+        newEntry->tagListLen = 0;
 
         newEntry->storageType = SNMP_STORAGE_NONVOLATILE;
         newEntry->rowStatus = SNMP_ROW_NONEXISTENT;
@@ -89,9 +91,9 @@ snmpTargetAddrTable_dispose(struct targetAddrTable_struct *reaped)
         snmp_close(reaped->sess);
     else
         SNMP_FREE(reaped->tAddress);
-    SNMP_FREE(reaped->name);
-    SNMP_FREE(reaped->tagList);
-    SNMP_FREE(reaped->params);
+    SNMP_FREE(reaped->nameData);
+    SNMP_FREE(reaped->tagListData);
+    SNMP_FREE(reaped->paramsData);
 
     SNMP_FREE(reaped);
     --_active;
@@ -110,8 +112,6 @@ snmpTargetAddrTable_addToList(struct targetAddrTable_struct *newEntry,
 {
     static struct targetAddrTable_struct *curr_struct, *prev_struct;
     int             i;
-    size_t          newOIDLen = 0, currOIDLen = 0;
-    oid             newOID[128], currOID[128];
 
     /*
      * if the list is empty, add the new entry to the top
@@ -121,23 +121,13 @@ snmpTargetAddrTable_addToList(struct targetAddrTable_struct *newEntry,
         return;
     } else {
         /*
-         * get the 'OID' value of the new entry
-         */
-        newOIDLen = strlen(newEntry->name);
-        for (i = 0; i < (int) newOIDLen; i++) {
-            newOID[i] = newEntry->name[i];
-        }
-
-        /*
          * search through the list for an equal or greater OID value
          */
         while (curr_struct != NULL) {
-            currOIDLen = strlen(curr_struct->name);
-            for (i = 0; i < (int) currOIDLen; i++) {
-                currOID[i] = curr_struct->name[i];
-            }
-
-            i = snmp_oid_compare(newOID, newOIDLen, currOID, currOIDLen);
+            i = netsnmp_compare_mem(newEntry->nameData,
+                                    newEntry->nameLen,
+                                    curr_struct->nameData,
+                                    curr_struct->nameLen);
             if (i == 0) {       /* Exact match, overwrite with new struct */
                 newEntry->next = curr_struct->next;
                 /*
@@ -232,10 +222,10 @@ search_snmpTargetAddrTable(oid * baseName,
 
     for (temp_struct = aAddrTable; temp_struct != NULL;
          temp_struct = temp_struct->next) {
-        for (i = 0; i < (int) strlen(temp_struct->name); i++) {
-            newNum[baseNameLen + i] = temp_struct->name[i];
+        for (i = 0; i < temp_struct->nameLen; i++) {
+            newNum[baseNameLen + i] = temp_struct->nameData[i];
         }
-        myOIDLen = baseNameLen + strlen(temp_struct->name);
+        myOIDLen = baseNameLen + i;
         i = snmp_oid_compare(name, *length, newNum, myOIDLen);
         /*
          * Assumes that the linked list sorted by OID, low to high
@@ -328,7 +318,8 @@ store_snmpTargetAddrEntry(int majorID, int minorID, void *serverarg,
              curr_struct->rowStatus == SNMP_ROW_NOTINSERVICE)) {
             cur = line + snprintf(line, sizeof(line), "targetAddr ");
             cur = read_config_save_octet_string(
-                cur, (const u_char*)curr_struct->tDomain, curr_struct->tDomainLen);
+                 cur, (const u_char*)curr_struct->nameData,
+                 curr_struct->nameLen);
             *cur++ = ' ';
             for (i = 0; i < curr_struct->tDomainLen; i++) {
                 cur += snprintf(cur, ep - cur, ".%i",
@@ -339,8 +330,8 @@ store_snmpTargetAddrEntry(int majorID, int minorID, void *serverarg,
                 cur, curr_struct->tAddress, curr_struct->tAddressLen);
             cur += snprintf(cur, ep - cur, " %i %i \"%s\" %s %i %i",
                             curr_struct->timeout,
-                            curr_struct->retryCount, curr_struct->tagList,
-                            curr_struct->params, curr_struct->storageType,
+                            curr_struct->retryCount, curr_struct->tagListData,
+                            curr_struct->paramsData, curr_struct->storageType,
                             curr_struct->rowStatus);
             line[ sizeof(line)-1 ] = 0;
 
@@ -456,8 +447,9 @@ snmpTargetAddr_addTagList(struct targetAddrTable_struct *entry, char *cptr)
                         "ERROR snmpTargetAddrEntry: tag list out of range in config string\n"));
             return (0);
         }
-        SNMP_FREE(entry->tagList);
-        entry->tagList = strdup(cptr);
+        SNMP_FREE(entry->tagListData);
+        entry->tagListData = strdup(cptr);
+        entry->tagListLen = strlen(cptr);
     }
     return (1);
 }                               /* snmpTargetAddr_addTagList */
@@ -481,7 +473,8 @@ snmpTargetAddr_addParams(struct targetAddrTable_struct *entry, char *cptr)
                         "ERROR snmpTargetAddrEntry: params out of range in config string\n"));
             return (0);
         }
-        entry->params = strdup(cptr);
+        entry->paramsData = strdup(cptr);
+        entry->paramsLen = strlen(cptr);
     }
     return (1);
 }                               /* snmpTargetAddr_addParams */
@@ -579,7 +572,7 @@ snmpd_parse_config_targetAddr(const char *token, char *char_ptr)
 
     bufl = 0;
     cptr = read_config_read_octet_string_const(cptr,
-                                               (u_char**)&newEntry->tDomain,
+                                               (u_char**)&newEntry->nameData,
                                                &bufl);
     if (bufl < 1 || bufl > 32) {
         DEBUGMSGTL(("snmpTargetAddrEntry",
@@ -588,7 +581,7 @@ snmpd_parse_config_targetAddr(const char *token, char *char_ptr)
         snmpTargetAddrTable_dispose(newEntry);
         return;
     }
-    newEntry->tDomainLen = bufl;
+    newEntry->nameLen = bufl;
 
     cptr = copy_nword_const(cptr, buff, sizeof(buff));
     if (snmpTargetAddr_addTDomain(newEntry, buff) == 0) {
@@ -638,8 +631,8 @@ snmpd_parse_config_targetAddr(const char *token, char *char_ptr)
     }
     bptr = buff;
     bptr += sprintf(bptr, "snmp_parse_config_targetAddr, read: ");
-    bptr = read_config_save_octet_string(bptr, (u_char*)newEntry->tDomain,
-                                         newEntry->tDomainLen);
+    bptr = read_config_save_octet_string(bptr, (u_char*)newEntry->nameData,
+                                         newEntry->nameLen);
     *bptr++ = '\n';
     for (i = 0; i < newEntry->tDomainLen; i++) {
         bptr += snprintf(bptr, buff + sizeof(buff) - bptr,
@@ -648,8 +641,8 @@ snmpd_parse_config_targetAddr(const char *token, char *char_ptr)
     bptr += snprintf(bptr, buff + sizeof(buff) - bptr,
                      " %s %d %d %s %s %d %d\n",
                      newEntry->tAddress, newEntry->timeout,
-                     newEntry->retryCount, newEntry->tagList,
-                     newEntry->params, newEntry->storageType,
+                     newEntry->retryCount, newEntry->tagListData,
+                     newEntry->paramsData, newEntry->storageType,
                      newEntry->rowStatus);
     buff[ sizeof(buff) - 1 ] = 0;
     DEBUGMSGTL(("snmpTargetAddrEntry", "%s", buff));

agent/mibgroup/target/snmpTargetParamsEntry_data.c
@@ -39,23 +39,21 @@ snmpTargetParamTable_create(void)
     struct targetParamTable_struct *newEntry;
 
     newEntry = (struct targetParamTable_struct *)
-        malloc(sizeof(struct targetParamTable_struct));
+        calloc(1, sizeof(struct targetParamTable_struct));
 
     if (NULL == newEntry)
         return NULL;
 
     ++_active;
 
-    newEntry->paramName = NULL;
     newEntry->mpModel = -1;
 
     newEntry->secModel = -1;
-    newEntry->secName = NULL;
     newEntry->secLevel = -1;
 
     newEntry->storageType = SNMP_STORAGE_NONVOLATILE;
     newEntry->rowStatus = SNMP_ROW_NONEXISTENT;
-    newEntry->next = NULL;
+
     return newEntry;
 }
 
@@ -67,8 +65,8 @@ snmpTargetParamTable_create(void)
 void
 snmpTargetParamTable_dispose(struct targetParamTable_struct *reaped)
 {
-    SNMP_FREE(reaped->paramName);
-    SNMP_FREE(reaped->secName);
+    SNMP_FREE(reaped->paramNameData);
+    SNMP_FREE(reaped->secNameData);
 
     free(reaped);
     --_active;
@@ -88,8 +86,6 @@ snmpTargetParamTable_addToList(struct targetParamTable_struct *newEntry,
 {
     static struct targetParamTable_struct *curr_struct, *prev_struct;
     int             i;
-    size_t          newOIDLen = 0, currOIDLen = 0;
-    oid             newOID[128], currOID[128];
 
     snmp_store_needed(NULL);
 
@@ -101,23 +97,13 @@ snmpTargetParamTable_addToList(struct targetParamTable_struct *newEntry,
         return;
     } else {
         /*
-         * get the 'OID' value of the new entry
-         */
-        newOIDLen = strlen(newEntry->paramName);
-        for (i = 0; i < (int) newOIDLen; i++) {
-            newOID[i] = newEntry->paramName[i];
-        }
-
-        /*
          * search through the list for an equal or greater OID value
          */
         while (curr_struct != NULL) {
-            currOIDLen = strlen(curr_struct->paramName);
-            for (i = 0; i < (int) currOIDLen; i++) {
-                currOID[i] = curr_struct->paramName[i];
-            }
-
-            i = snmp_oid_compare(newOID, newOIDLen, currOID, currOIDLen);
+            i = netsnmp_compare_mem(newEntry->paramNameData,
+                                    newEntry->paramNameLen,
+                                    curr_struct->paramNameData,
+                                    curr_struct->paramNameLen);
             if (i == 0) {       /* Exact match, overwrite with new struct */
                 newEntry->next = curr_struct->next;
                 /*
@@ -211,10 +197,10 @@ search_snmpTargetParamsTable(oid * baseName,
 
     for (temp_struct = aPTable; temp_struct != NULL;
          temp_struct = temp_struct->next) {
-        for (i = 0; i < (int) strlen(temp_struct->paramName); i++) {
-            newNum[baseNameLen + i] = temp_struct->paramName[i];
+        for (i = 0; i < temp_struct->paramNameLen; i++) {
+            newNum[baseNameLen + i] = temp_struct->paramNameData[i];
         }
-        myOIDLen = baseNameLen + strlen(temp_struct->paramName);
+        myOIDLen = baseNameLen + temp_struct->paramNameLen;
         i = snmp_oid_compare(name, *length, newNum, myOIDLen);
         /*
          * Assumes that the linked list sorted by OID, low to high
@@ -232,11 +218,12 @@ search_snmpTargetParamsTable(oid * baseName,
 
 
 struct targetParamTable_struct *
-get_paramEntry(const char *name)
+get_paramEntry2(const char *name, size_t nameLen)
 {
     static struct targetParamTable_struct *ptr;
     for (ptr = aPTable; ptr; ptr = ptr->next) {
-        if (strcmp(ptr->paramName, name) == 0) {
+        if (nameLen == ptr->paramNameLen &&
+            memcmp(ptr->paramNameData, name, nameLen) == 0) {
             return ptr;
         }
     }
@@ -310,8 +297,8 @@ store_snmpTargetParamsEntry(int majorID, int minorID, void *serverarg,
                  curr_struct->rowStatus == SNMP_ROW_NOTINSERVICE)) {
                 snprintf(line, sizeof(line),
                         "targetParams %s %i %i %s %i %i %i\n",
-                        curr_struct->paramName, curr_struct->mpModel,
-                        curr_struct->secModel, curr_struct->secName,
+                        curr_struct->paramNameData, curr_struct->mpModel,
+                        curr_struct->secModel, curr_struct->secNameData,
                         curr_struct->secLevel, curr_struct->storageType,
                         curr_struct->rowStatus);
                 line[ sizeof(line)-1 ] = 0;

agent/agent_trap.c
@@ -217,9 +217,12 @@ netsnmp_add_notification_session(netsnmp_session * ss, int pdutype,
         DEBUGMSGTL(("trap", "adding callback trap sink (%p)\n", ss));
         args.ss = ss;
         args.confirm = confirm;
-        args.name = name;
-        args.tag = tag;
-        args.profile = profile;
+        args.nameData = name;
+        args.nameLen = (NULL == name) ? 0 : strlen(name);
+        args.tagData = tag;
+        args.tagLen = (NULL == tag) ? 0 : strlen(tag);
+        args.profileData = profile;
+        args.profileLen = (NULL == profile) ? 0: strlen(profile);
         snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
                             SNMPD_CALLBACK_REGISTER_NOTIFICATIONS,
                             (void *) &args);

agent/mibgroup/notification/snmpNotifyTable_data.c
@@ -67,7 +67,8 @@ _checkFilter(const char* paramName, netsnmp_pdu *pdu)
     netsnmp_assert(NULL != paramName);
     netsnmp_assert(NULL != pdu);
 
-    DEBUGMSGTL(("send_notifications", "checking filters...\n"));
+    DEBUGMSGTL(("send_notifications", "checking filters for '%s'...\n",
+                paramName));
 
     /*
    A notification originator uses the snmpNotifyFilterTable to filter
@@ -79,6 +80,8 @@ _checkFilter(const char* paramName, netsnmp_pdu *pdu)
    snmpNotifyFilterProfileTable, no filtering is performed for that
    management target.
     */
+    /** xxx paramName from session doesn't have length. will be
+     * trouble with paramNames with embedded NULL */
     profileName = get_FilterProfileName(paramName, strlen(paramName),
                                         &profileNameLen);
     if (NULL == profileName) /* try default */
@@ -270,6 +273,7 @@ notifyTable_register_notifications(int major, int minor,
         (struct agent_add_trap_args *) serverarg;
     netsnmp_session *ss;
     const char      *name, *tag, *notifyProfile;
+    int             nameLen, tagLen, notifyProfileLen;
 
     if (!args || !(args->ss)) {
         return (0);
@@ -277,17 +281,23 @@ notifyTable_register_notifications(int major, int minor,
     args->rc = SNMPERR_GENERR;
     confirm = args->confirm;
     ss = args->ss;
-    name = args->name;
-    tag = args->tag;
-    notifyProfile = args->profile;
+    name = args->nameData;
+    nameLen = args->nameLen;
+    tag = args->tagData;
+    tagLen = args->tagLen;
+    notifyProfile = args->profileData;
+    notifyProfileLen = args->profileLen;
 
     /*
      * XXX: START move target creation to target code 
      */
     if (NULL == name) {
+        int len;
         for (i = 0; i < MAX_ENTRIES; i++) {
             sprintf(buf, "internal%d", i);
-            if (get_addrForName(buf) == NULL && get_paramEntry(buf) == NULL)
+            len = strlen(buf);
+            if ((get_addrForName2(buf,len) == NULL) &&
+                (get_paramEntry2(buf,len) == NULL))
                 break;
         }
         if (i == MAX_ENTRIES) {
@@ -298,11 +308,16 @@ notifyTable_register_notifications(int major, int minor,
             return (0);
         }
         name = buf;
-        if (NULL == tag)
+        nameLen = len;
+        if (NULL == tag) {
             tag = buf;
+            tagLen = len;
+        }
     } else {
-        if (NULL == tag)
+        if (NULL == tag) {
             tag = name;
+            tagLen = nameLen;
+        }
     }
 
     /*
@@ -318,7 +333,7 @@ notifyTable_register_notifications(int major, int minor,
     ptr = snmpTargetAddrTable_create();
     if (!ptr)
         goto bail;
-    ptr->name = strdup(name);
+    ptr->nameData = netsnmp_memdup_nt(name, nameLen, &ptr->nameLen);
     memcpy(ptr->tDomain, t->domain, t->domain_length * sizeof(oid));
     ptr->tDomainLen = t->domain_length;
     ptr->tAddressLen = t->remote_length;
@@ -326,15 +341,16 @@ notifyTable_register_notifications(int major, int minor,
 
     ptr->timeout = ss->timeout / 1000;
     ptr->retryCount = ss->retries;
-    SNMP_FREE(ptr->tagList);
-    ptr->tagList = strdup(tag);
-    ptr->params = strdup(ptr->name); /* link to target param table */
-    if (!ptr->params || !ptr->tagList || !ptr->name)
+    SNMP_FREE(ptr->tagListData);
+    ptr->tagListData = netsnmp_memdup_nt(tag, tagLen, &ptr->tagListLen);
+    /** link to target param table */
+    ptr->paramsData = netsnmp_memdup_nt(name, nameLen, &ptr->paramsLen);
+    if (!ptr->paramsData || !ptr->tagListData || !ptr->nameData)
         goto bail;
     ptr->storageType = ST_READONLY;
     ptr->rowStatus = RS_ACTIVE;
     ptr->sess = ss;
-    DEBUGMSGTL(("trapsess", "adding %s to trap table\n", ptr->name));
+    DEBUGMSGTL(("trapsess", "adding %s to trap table\n", ptr->nameData));
     snmpTargetAddrTable_add(ptr);
 
     /*
@@ -343,19 +359,20 @@ notifyTable_register_notifications(int major, int minor,
     pptr = snmpTargetParamTable_create();
     if (NULL == pptr)
         goto bail;
-    pptr->paramName = strdup(ptr->params); /* link from target addr table */
-    if (!pptr->paramName)
+    /** link from target addr table */
+    pptr->paramNameData = netsnmp_memdup_nt(ptr->paramsData, ptr->paramsLen,
+                                            &pptr->paramNameLen);
+    if (!pptr->paramNameData)
         goto bail;
     pptr->mpModel = ss->version;
     if (ss->version == SNMP_VERSION_3) {
         pptr->secModel = ss->securityModel;
         pptr->secLevel = ss->securityLevel;
-        pptr->secName = (char *) malloc(ss->securityNameLen + 1);
-        if (pptr->secName == NULL)
+        pptr->secNameData = netsnmp_memdup_nt(ss->securityName,
+                                              ss->securityNameLen,
+                                              &pptr->secNameLen);
+        if (pptr->secNameData == NULL)
             goto bail;
-        memcpy((void *) pptr->secName, (void *) ss->securityName,
-               ss->securityNameLen);
-        pptr->secName[ss->securityNameLen] = 0;
     }
 #if !defined(NETSNMP_DISABLE_SNMPV1) || !defined(NETSNMP_DISABLE_SNMPV2C)
        else {
@@ -365,14 +382,13 @@ notifyTable_register_notifications(int major, int minor,
 #endif
                                              SNMP_SEC_MODEL_SNMPv2c;
         pptr->secLevel = SNMP_SEC_LEVEL_NOAUTH;
-        pptr->secName = NULL;
+        pptr->secNameData = NULL;
         if (ss->community && (ss->community_len > 0)) {
-            pptr->secName = (char *) malloc(ss->community_len + 1);
-            if (pptr->secName == NULL)
+            pptr->secNameData = netsnmp_memdup_nt(ss->community,
+                                                  ss->community_len,
+                                                  &pptr->secNameLen);
+            if (pptr->secNameData == NULL)
                 goto bail;
-            memcpy((void *) pptr->secName, (void *) ss->community,
-                   ss->community_len);
-            pptr->secName[ss->community_len] = 0;
         }
     }
 #endif
@@ -390,10 +406,11 @@ notifyTable_register_notifications(int major, int minor,
     if (nptr == NULL)
         goto bail;
     ++_active;
-    nptr->snmpNotifyName = strdup(name);
-    nptr->snmpNotifyNameLen = strlen(name);
-    nptr->snmpNotifyTag = strdup(tag); /* selects target addr */
-    nptr->snmpNotifyTagLen = strlen(nptr->snmpNotifyTag);
+    nptr->snmpNotifyName = netsnmp_memdup_nt(name, nameLen,
+                                             &nptr->snmpNotifyNameLen);
+    /** selects target addr */
+    nptr->snmpNotifyTag = netsnmp_memdup_nt(tag, tagLen,
+                                            &nptr->snmpNotifyTagLen);
     if (!nptr->snmpNotifyName || !nptr->snmpNotifyTag)
         goto bail;
     nptr->snmpNotifyType = confirm ?
@@ -412,10 +429,10 @@ notifyTable_register_notifications(int major, int minor,
      */
     if (NULL != notifyProfile) {
         struct snmpNotifyFilterProfileTable_data *profile;
-        profile = snmpNotifyFilterProfileTable_create(ptr->params,
-                                                      strlen(ptr->params),
+        profile = snmpNotifyFilterProfileTable_create(ptr->paramsData,
+                                                      ptr->paramsLen,
                                                       notifyProfile,
-                                                      strlen(notifyProfile));
+                                                      notifyProfileLen);
         if (NULL == profile) {
             snmp_log(LOG_ERR, "couldn't create notify filter profile\n");
             goto bail;
@@ -624,13 +641,14 @@ snmpNotifyTable_remove(struct snmpNotifyTable_data *thedata)
 }
 
 struct snmpNotifyTable_data *
-get_notifyTable(const char *name)
+get_notifyTable2(const char *name, size_t nameLen)
 {
     struct header_complex_index *hptr;
 
     for (hptr = snmpNotifyTableStorage; hptr; hptr = hptr->next) {
         struct snmpNotifyTable_data *nptr = hptr->data;
-        if (nptr->snmpNotifyName && strcmp(nptr->snmpNotifyName, name) == 0)
+        if (nptr->snmpNotifyNameLen == nameLen && nptr->snmpNotifyName &&
+            memcmp(nptr->snmpNotifyName, name, nameLen) == 0)
             return nptr;
     }
     return NULL;
@@ -648,12 +666,13 @@ find_row_notifyTable(struct variable *vp, oid * name, size_t * len, int exact,
 }
 
 void
-notifyTable_unregister_notification(const char *name)
+notifyTable_unregister_notification(const char *name, unsigned char nameLen)
 {
-    struct targetAddrTable_struct *ta = get_addrForName(name);
-    struct targetParamTable_struct *tp = get_paramEntry(name);
-    struct snmpNotifyTable_data *nt = get_notifyTable(name);
-    struct snmpNotifyFilterProfileTable_data *fp = get_FilterProfile(name);
+    struct targetAddrTable_struct *ta = get_addrForName2(name,nameLen);
+    struct targetParamTable_struct *tp = get_paramEntry2(name,nameLen);
+    struct snmpNotifyTable_data *nt = get_notifyTable2(name,nameLen);
+    struct snmpNotifyFilterProfileTable_data *fp =
+        snmpNotifyFilterProfileTable_find(name, nameLen);
 
     DEBUGMSGTL(("trapsess", "removing %s from trap tables\n", name));
 

agent/mibgroup/notification/snmpNotifyTable_data.h
@@ -51,7 +51,7 @@ struct snmpNotifyTable_data *
                 snmpNotifyTable_extract(struct snmpNotifyTable_data *thedata);
 void            snmpNotifyTable_dispose(struct snmpNotifyTable_data *thedata);
 
-struct snmpNotifyTable_data *get_notifyTable(const char *name);
+struct snmpNotifyTable_data *get_notifyTable2(const char *name, size_t len);
 struct snmpNotifyTable_data *find_row_notifyTable(struct variable *vp,
                                                   oid * name, size_t * length,
                                                   int exact, size_t * var_len,

commit 37e498fdcde98ee93875a750dbcd5a8a9ba336b1

SNMP-TARGET-MIB: express features used (broken by 58ae9e6b11)

agent/mibgroup/target/snmpTargetAddrEntry_data.c
@@ -25,6 +25,8 @@
 #include "snmpTargetAddrEntry_data.h"
 #include "util_funcs/header_generic.h"
 
+netsnmp_feature_require(container_compare_mem);
+
 static struct targetAddrTable_struct *aAddrTable = NULL;
 static int _active = 0;
 

agent/mibgroup/target/snmpTargetParamsEntry_data.c
@@ -23,6 +23,8 @@
 
 #include "snmpTargetParamsEntry_data.h"
 
+netsnmp_feature_require(container_compare_mem);
+
 static struct targetParamTable_struct *aPTable = NULL;
 static int _active = 0;
 