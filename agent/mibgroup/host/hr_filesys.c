a2334651b84878222b6a758154b283bcf8b233ef
Bart Van Assche <bvanassche@acm.org>
2019-09-06 14:49:32 -0700

    HOST-MIB, hr_filesys: Clean up HRFS_ignores[]
    Declare the HRFS_ignores array static. Make sure that "autofs" occurs once.
    Define MNTTYPE_AUTOFS if necessary.

--- agent/mibgroup/host/hr_filesys.c
+++ agent/mibgroup/host/hr_filesys.c
@@ -68,6 +68,10 @@ netsnmp_feature_require(se_find_free_value_in_slist);
 netsnmp_feature_require(date_n_time);
 netsnmp_feature_require(ctime_to_timet);
 
+#ifndef MNTTYPE_AUTOFS
+#define MNTTYPE_AUTOFS	"autofs"
+#endif
+
 #if defined(bsdi4) || defined(freebsd3) || defined(freebsd4) || defined(freebsd5) || defined(darwin)
 #if HAVE_GETFSSTAT && defined(MFSNAMELEN)
 #define MOUNT_NFS	"nfs"
@@ -664,7 +668,7 @@ Init_HR_FileSys(void)
 #endif
 }
 
-const char     *HRFS_ignores[] = {
+static const char *HRFS_ignores[] = {
 #ifdef MNTTYPE_IGNORE
     MNTTYPE_IGNORE,
 #endif
@@ -677,13 +681,8 @@ const char     *HRFS_ignores[] = {
 #ifdef MNTTYPE_PROCFS
     MNTTYPE_PROCFS,
 #endif
-#ifdef MNTTYPE_AUTOFS
     MNTTYPE_AUTOFS,
-#else
-    "autofs",
-#endif
 #ifdef linux
-    "autofs",
     "bdev",
     "binfmt_misc",
     "cpuset",
