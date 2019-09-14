2633eedde135c2e69c92b82f702da783608c6009
Bart Van Assche <bvanassche@acm.org>
2019-09-14 10:13:40 -0700

    CHANGES: snmpd: Reduce the time needed to execute "pass" scripts on BSD systems
    See also https://github.com/net-snmp/net-snmp/issues/8.

--- agent/netsnmp_close_fds.c
+++ agent/netsnmp_close_fds.c
@@ -23,6 +23,7 @@ void netsnmp_close_fds(int fd)
     if (fd < -1)
         fd = -1;
 
+#ifdef __linux__
     if ((dir = opendir("/proc/self/fd"))) {
         while ((ent = readdir(dir))) {
             if (sscanf(ent->d_name, "%d", &i) == 1) {
@@ -31,9 +32,20 @@ void netsnmp_close_fds(int fd)
             }
         }
         closedir(dir);
-    } else {
-        largest_fd = getdtablesize() - 1;
     }
+#elif defined(__FreeBSD__) || defined(__OpenBSD__)
+    if ((dir = opendir("/dev/fd"))) {
+        while ((ent = readdir(dir))) {
+            if (sscanf(ent->d_name, "%d", &i) == 1) {
+                if (i > largest_fd)
+                    largest_fd = i;
+            }
+        }
+        closedir(dir);
+    }
+#endif
+    if (largest_fd < 0)
+        largest_fd = getdtablesize() - 1;
 
     for (i = largest_fd; i > fd && i >= 0; i--)
         close(i);
