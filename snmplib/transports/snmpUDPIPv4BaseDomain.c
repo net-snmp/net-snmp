8e762a11ad868f2c1c122a77f5e27aadaca60b8a
Sam Tannous <stannous@cumulusnetworks.com>
2019-09-29 07:26:25 -0700

    From review comments, corrected debugs and removed check on t->sock.
    Signed-off-by: Sam Tannous <stannous@cumulusnetworks.com>

--- snmplib/transports/snmpUDPIPv4BaseDomain.c
+++ snmplib/transports/snmpUDPIPv4BaseDomain.c
@@ -302,13 +302,13 @@ netsnmp_udpipv4base_transport_with_source(const struct netsnmp_ep *ep,
         return t;
 
     /* for Linux VRF Traps we try to bind the iface if clientaddr is not set */
-    if (ep && ep->iface && t->sock) {
+    if (ep && ep->iface) {
         rc =  netsnmp_bindtodevice(t->sock, ep->iface);
         if (rc)
-            DEBUGMSGTL(("netsnmp_udpbase", "VRF: Could not bind socket %d bound to %s\n",
+            DEBUGMSGTL(("netsnmp_udpbase", "VRF: Could not bind socket %d to %s\n",
                         t->sock, ep->iface));
         else
-            DEBUGMSGTL(("netsnmp_udpbase", "VRF: Bound socket %d bound to %s\n",
+            DEBUGMSGTL(("netsnmp_udpbase", "VRF: Bound socket %d to %s\n",
                         t->sock, ep->iface));
     }
 
