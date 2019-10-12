net-snmp/net-snmp

commit 9b9c0e287b4dd4619da3e3ca80e1832dc1c81699

MIBs: Use asprintf() instead of snprintf() to prevent truncation
This patch addresses most gcc 7 warnings about output buffer truncation.

agent/mibgroup/host/hrh_filesys.c
@@ -218,7 +218,7 @@ var_hrhfilesys(struct variable *vp,
               int exact, size_t * var_len, WriteMethod ** write_method)
 {
     int             fsys_idx;
-    static char     string[1024];
+    static char    *string;
 
     fsys_idx =
         header_hrhfilesys(vp, name, length, exact, var_len, write_method);
@@ -230,17 +230,22 @@ var_hrhfilesys(struct variable *vp,
         long_return = fsys_idx;
         return (u_char *) & long_return;
     case HRFSYS_MOUNT:
-        snprintf(string, sizeof(string), "%s", HRFS_entry->path);
-        string[ sizeof(string)-1 ] = 0;
-        *var_len = strlen(string);
+        free(string);
+        string = NULL;
+        *var_len = 0;
+        if (asprintf(&string, "%s", HRFS_entry->path) >= 0)
+            *var_len = strlen(string);
         return (u_char *) string;
     case HRFSYS_RMOUNT:
+        free(string);
+        string = NULL;
         if (HRFS_entry->flags & NETSNMP_FS_FLAG_REMOTE) {
-            snprintf(string, sizeof(string), "%s", HRFS_entry->device);
-            string[ sizeof(string)-1 ] = 0;
-        } else
-            string[0] = '\0';
-        *var_len = strlen(string);
+            if (asprintf(&string, "%s", HRFS_entry->device) < 0) {
+            }
+        } else {
+            string = strdup("");
+        }
+        *var_len = string ? strlen(string) : 0;
         return (u_char *) string;
 
     case HRFSYS_TYPE:

agent/mibgroup/ucd-snmp/disk.c
@@ -821,10 +821,10 @@ var_extensible_disk(struct variable *vp,
                     size_t * var_len, WriteMethod ** write_method)
 {
     int             ret;
-	unsigned int	disknum = 0;
+    unsigned int    disknum = 0;
     struct dsk_entry entry;
     static long     long_ret;
-    static char     errmsg[300];
+    static char    *errmsg;
 
 tryAgain:
     if (header_simple_table
@@ -911,22 +911,22 @@ tryAgain:
         return ((u_char *) (&long_ret));
 
     case ERRORMSG:
+        free(errmsg);
+        errmsg = NULL;
+        *var_len = 0;
         if (entry.dskErrorFlag) {
-            if (disks[disknum].minimumspace >= 0)
-                snprintf(errmsg, sizeof(errmsg),
-                        "%s: less than %d free (= %d)",
-                        disks[disknum].path, disks[disknum].minimumspace,
-                        (int) entry.dskAvail);
-            else
-                snprintf(errmsg, sizeof(errmsg),
-                        "%s: less than %d%% free (= %d%%)",
-                        disks[disknum].path, disks[disknum].minpercent,
-                        (int)entry.dskPercent);
-            errmsg[ sizeof(errmsg)-1 ] = 0;
-        } else
-            errmsg[0] = 0;
-        *var_len = strlen(errmsg);
-        return ((u_char *) (errmsg));
+            if ((disks[disknum].minimumspace >= 0 &&
+                 asprintf(&errmsg, "%s: less than %d free (= %d)",
+                          disks[disknum].path, disks[disknum].minimumspace,
+                          (int) entry.dskAvail) >= 0) ||
+                (disks[disknum].minimumspace < 0 &&
+                 asprintf(&errmsg, "%s: less than %d%% free (= %d%%)",
+                          disks[disknum].path, disks[disknum].minpercent,
+                          (int)entry.dskPercent) >= 0)) {
+                *var_len = strlen(errmsg);
+            }
+        }
+        return (u_char *) (errmsg);
     }
     return NULL;
 }

agent/mibgroup/ucd-snmp/disk_hw.c
@@ -305,7 +305,7 @@ var_extensible_disk(struct variable *vp,
   netsnmp_fsys_info *entry;
     unsigned long long val;
     static long     long_ret;
-    static char     errmsg[300];
+    static char    *errmsg;
     netsnmp_cache  *cache;
 
     /* Update the fsys H/W module */
@@ -403,23 +403,21 @@ tryAgain:
         return ((u_char *) (&long_ret));
 
     case ERRORMSG:
-        errmsg[0] = 0;
+        free(errmsg);
+        errmsg = NULL;
+        *var_len = 0;
         val = netsnmp_fsys_avail_ull(entry);
-        if (( entry->minspace >= 0 ) &&
-            ( val < entry->minspace ))
-                snprintf(errmsg, sizeof(errmsg),
-                        "%s: less than %d free (= %d)",
-                        entry->path, entry->minspace,
-                        (int) val);
-        else if (( entry->minpercent >= 0 ) &&
-                 (_percent( entry->avail, entry->size ) < entry->minpercent ))
-                snprintf(errmsg, sizeof(errmsg),
-                        "%s: less than %d%% free (= %d%%)",
-                        entry->path, entry->minpercent,
-                        _percent( entry->avail, entry->size ));
-        errmsg[ sizeof(errmsg)-1 ] = 0;
-        *var_len = strlen(errmsg);
-        return ((u_char *) (errmsg));
+        if ((entry->minspace >= 0 && val < entry->minspace &&
+             asprintf(&errmsg, "%s: less than %d free (= %d)", entry->path,
+                      entry->minspace, (int) val) >= 0) ||
+            (entry->minpercent >= 0 &&
+             _percent(entry->avail, entry->size) < entry->minpercent &&
+             asprintf(&errmsg, "%s: less than %d%% free (= %d%%)", entry->path,
+                      entry->minpercent, _percent(entry->avail, entry->size))
+             >= 0)) {
+            *var_len = strlen(errmsg);
+        }
+        return (u_char *) errmsg;
     }
     return NULL;
 }

agent/mibgroup/ucd-snmp/proc.c
@@ -238,7 +238,7 @@ var_extensible_proc(struct variable *vp,
 
     struct myproc  *proc;
     static long     long_ret;
-    static char     errmsg[300];
+    static char    *errmsg;
 
 
     if (header_simple_table
@@ -277,29 +277,31 @@ var_extensible_proc(struct variable *vp,
             }
             return ((u_char *) (&long_ret));
         case ERRORMSG:
+            free(errmsg);
+            errmsg = NULL;
             long_ret = sh_count_procs(proc->name);
             if (long_ret < 0) {
-                errmsg[0] = 0;  /* catch out of mem errors return 0 count */
+                /* catch out of mem errors return 0 count */
             } else if (proc->min && long_ret < proc->min) {
-                if ( long_ret > 0 )
-                    snprintf(errmsg, sizeof(errmsg),
-                        "Too few %s running (# = %d)",
-                        proc->name, (int) long_ret);
-                else
-                    snprintf(errmsg, sizeof(errmsg),
-                        "No %s process running", proc->name);
+                if (long_ret > 0) {
+                    if (asprintf(&errmsg, "Too few %s running (# = %d)",
+                                 proc->name, (int) long_ret) < 0) {
+                    }
+                } else {
+                    if (asprintf(&errmsg, "No %s process running", proc->name)
+                        < 0) {
+                    }
+                }
             } else if (proc->max && long_ret > proc->max) {
-                snprintf(errmsg, sizeof(errmsg),
-                        "Too many %s running (# = %d)",
-                        proc->name, (int) long_ret);
+                if (asprintf(&errmsg, "Too many %s running (# = %d)",
+                             proc->name, (int) long_ret) < 0) {
+                }
             } else if (proc->min == 0 && proc->max == 0 && long_ret > 0) {
-                snprintf(errmsg, sizeof(errmsg),
-                        "%s process should not be running.", proc->name);
-            } else {
-                errmsg[0] = 0;
+                if (asprintf(&errmsg, "%s process should not be running.",
+                             proc->name) < 0) {
+                }
             }
-            errmsg[ sizeof(errmsg)-1 ] = 0;
-            *var_len = strlen(errmsg);
+            *var_len = errmsg ? strlen(errmsg) : 0;
             return ((u_char *) errmsg);
         case ERRORFIX:
             *write_method = fixProcError;
@@ -338,7 +340,8 @@ fixProcError(int action,
         tmp = *((long *) var_val);
         if (tmp == 1 && action == COMMIT) {
             if (proc->fixcmd[0]) {
-                strcpy(fixproc.command, proc->fixcmd);
+                free(fixproc.command);
+                fixproc.command = strdup(proc->fixcmd);
                 exec_command(&fixproc);
             }
         }

agent/mibgroup/hardware/fsys/fsys_mntent.c
@@ -174,15 +174,16 @@ netsnmp_fsys_arch_load( void )
 #endif
     struct NSFS_STATFS stat_buf;
     netsnmp_fsys_info *entry;
-    char               tmpbuf[1024];
+    char              *tmpbuf = NULL;
 
     /*
      * Retrieve information about the currently mounted filesystems...
      */
     fp = fopen( ETC_MNTTAB, "r" );   /* OR setmntent()?? */
     if ( !fp ) {
-        snprintf( tmpbuf, sizeof(tmpbuf), "Cannot open %s", ETC_MNTTAB );
-        snmp_log_perror( tmpbuf );
+        if (asprintf(&tmpbuf, "Cannot open %s", ETC_MNTTAB) >= 0)
+            snmp_log_perror(tmpbuf);
+        free(tmpbuf);
         return;
     }
 
@@ -243,8 +244,10 @@ netsnmp_fsys_arch_load( void )
         if ( NSFS_STATFS( entry->path, &stat_buf ) < 0 )
 #endif
         {
-            snprintf( tmpbuf, sizeof(tmpbuf), "Cannot statfs %s", entry->path );
-            snmp_log_perror( tmpbuf );
+            tmpbuf = NULL;
+            if (asprintf(&tmpbuf, "Cannot statfs %s", entry->path) >= 0)
+                snmp_log_perror(tmpbuf);
+            free(tmpbuf);
             entry->units = stat_buf.NSFS_SIZE;
             entry->size  = 0;
             entry->used  = 0;

agent/mibgroup/host/hr_print.c
@@ -361,10 +361,9 @@ run_lpstat(int *fd)
     struct extensible ex;
 
     memset(&ex, 0, sizeof(ex));
-    strcpy(ex.command, LPSTAT_PATH " -v");
-    if ((*fd = get_exec_output(&ex)) < 0)
-        return NULL;
-
-    return fdopen(*fd, "r");
+    ex.command = strdup(LPSTAT_PATH " -v");
+    *fd = get_exec_output(&ex);
+    free(ex.command);
+    return *fd >= 0 ? fdopen(*fd, "r") : NULL;
 }
 #endif

agent/mibgroup/mibII/system_mib.c
@@ -249,7 +249,9 @@ init_system_mib(void)
     /*
      * set default values of system stuff 
      */
-    sprintf(extmp.command, "%s -a", UNAMEPROG);
+    extmp.command = NULL;
+    if (asprintf(&extmp.command, "%s -a", UNAMEPROG) < 0) {
+    }
     /*
      * setup defaults 
      */
@@ -275,7 +277,9 @@ init_system_mib(void)
     strlcpy(sysName, utsName.nodename, sizeof(sysName));
 #else
 #if defined (HAVE_EXECV) && !defined (mingw32)
-    sprintf(extmp.command, "%s -n", UNAMEPROG);
+    extmp.command = NULL;
+    if (asprintf(&extmp.command, "%s -n", UNAMEPROG) < 0) {
+    }
     /*
      * setup defaults 
      */

agent/mibgroup/struct.h
@@ -10,7 +10,7 @@
 
 struct extensible {
     char            name[STRMAX];
-    char            command[STRMAX];
+    char           *command;
     char            fixcmd[STRMAX];
     int             type;
     int             result;

commit d0787a2c86a80e31756965c436fac67b7d1c0f9b

HOST-RESOURCES-MIB, UCD-SNMP-MIB: Report empty strings correctly
See also https://github.com/net-snmp/net-snmp/issues/26.

Fixes: 9b9c0e287b4d ("MIBs: Use asprintf() instead of snprintf() to prevent truncation")

agent/mibgroup/host/hrh_filesys.c
@@ -219,6 +219,7 @@ var_hrhfilesys(struct variable *vp,
 {
     int             fsys_idx;
     static char    *string;
+    static char     empty_str[1];
 
     fsys_idx =
         header_hrhfilesys(vp, name, length, exact, var_len, write_method);
@@ -235,7 +236,7 @@ var_hrhfilesys(struct variable *vp,
         *var_len = 0;
         if (asprintf(&string, "%s", HRFS_entry->path) >= 0)
             *var_len = strlen(string);
-        return (u_char *) string;
+        return (u_char *)(string ? string : empty_str);
     case HRFSYS_RMOUNT:
         free(string);
         if (HRFS_entry->flags & NETSNMP_FS_FLAG_REMOTE) {
@@ -245,7 +246,7 @@ var_hrhfilesys(struct variable *vp,
             string = strdup("");
         }
         *var_len = string ? strlen(string) : 0;
-        return (u_char *) string;
+        return (u_char *)(string ? string : empty_str);
 
     case HRFSYS_TYPE:
         fsys_type_id[fsys_type_len - 1] = 

agent/mibgroup/ucd-snmp/disk.c
@@ -825,6 +825,7 @@ var_extensible_disk(struct variable *vp,
     struct dsk_entry entry;
     static long     long_ret;
     static char    *errmsg;
+    static char     empty_str[1];
 
 tryAgain:
     if (header_simple_table
@@ -926,7 +927,7 @@ tryAgain:
                 *var_len = strlen(errmsg);
             }
         }
-        return (u_char *) (errmsg);
+        return (u_char *)(errmsg ? errmsg : empty_str);
     }
     return NULL;
 }

agent/mibgroup/ucd-snmp/disk_hw.c
@@ -314,6 +314,7 @@ var_extensible_disk(struct variable *vp,
     unsigned long long val;
     static long     long_ret;
     static char    *errmsg;
+    static char     empty_str[1];
     netsnmp_cache  *cache;
 
     /* Update the fsys H/W module */
@@ -432,7 +433,7 @@ tryAgain:
              >= 0)) {
             *var_len = strlen(errmsg);
         }
-        return (u_char *) errmsg;
+        return (u_char *)(errmsg ? errmsg : empty_str);
     }
     return NULL;
 }

agent/mibgroup/ucd-snmp/proc.c
@@ -265,7 +265,7 @@ var_extensible_proc(struct variable *vp,
     struct myproc  *proc;
     static long     long_ret;
     static char    *errmsg;
-
+    static char     empty_str[1];
 
     if (header_simple_table
         (vp, name, length, exact, var_len, write_method, numprocs))
@@ -328,7 +328,7 @@ var_extensible_proc(struct variable *vp,
                 }
             }
             *var_len = errmsg ? strlen(errmsg) : 0;
-            return ((u_char *) errmsg);
+            return (u_char *)(errmsg ? errmsg : empty_str);
         case ERRORFIX:
             *write_method = fixProcError;
             long_return = fixproc.result;