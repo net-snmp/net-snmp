net-snmp/net-snmp

commit a680e9ad5ac9411b6f095714f0182f246ef92f04

NET-SNMP-AGENT-MIB: Make nsDebugOutputAll enable all debug tokens
Signed-off-by: Anders Wallin <wallinux@gmail.com>
[ bvanassche: edited patch description ]

snmplib/snmp_debug.c
@@ -552,6 +552,18 @@ snmp_get_do_debugging(void)
 }
 
 void
+snmp_set_do_debugoutputall(int val)
+{
+    debug_print_everything = val;
+}
+
+int
+snmp_get_do_debugoutputall(void)
+{
+    return debug_print_everything;
+}
+
+void
 snmp_debug_shutdown(void)
 {
     int i;
@@ -675,6 +687,16 @@ int
 snmp_get_do_debugging(void)
 {
     return 0;
+}
+
+void
+snmp_set_do_debugoutputall(int val)
+{ }
+
+int
+snmp_get_do_debugoutputall(void)
+{
+    return 0
 }
 
 void

agent/mibgroup/agent/nsDebug.c
@@ -169,7 +169,7 @@ handle_nsDebugOutputAll(netsnmp_mib_handler *handler,
     switch (reqinfo->mode) {
 
     case MODE_GET:
-	enabled = snmp_get_do_debugging();
+	enabled = snmp_get_do_debugoutputall();
 	if ( enabled==0 )
 	    enabled=2;		/* false */
 	for (request = requests; request; request=request->next) {
@@ -205,7 +205,7 @@ handle_nsDebugOutputAll(netsnmp_mib_handler *handler,
         enabled = *requests->requestvb->val.integer;
 	if (enabled == 2 )	/* false */
 	    enabled = 0;
-	snmp_set_do_debugging( enabled );
+	snmp_set_do_debugoutputall( enabled );
         break;
 #endif /* !NETSNMP_NO_WRITE_SUPPORT */
     }

include/net-snmp/output_api.h
@@ -149,6 +149,10 @@ netsnmp_debug_no_dumpsetup(const char *token, const void *buf, size_t len)
     void            snmp_set_do_debugging(int);
     NETSNMP_IMPORT
     int             snmp_get_do_debugging(void);
+    NETSNMP_IMPORT
+    void            snmp_set_do_debugoutputall(int);
+    NETSNMP_IMPORT
+    int             snmp_get_do_debugoutputall(void);
 #ifndef NETSNMP_DISABLE_DYNAMIC_LOG_LEVEL
     NETSNMP_IMPORT
     void            netsnmp_set_debug_log_level(int val);

commit d75bd39844c89f9cb6ffa03a2c43ec3f8576e735

libsnmp: Add a missing semicolon
See also https://github.com/net-snmp/net-snmp/issues/30.

Fixes: a680e9ad5ac9 ("NET-SNMP-AGENT-MIB: Make nsDebugOutputAll enable all debug tokens")

snmplib/snmp_debug.c
@@ -696,7 +696,7 @@ snmp_set_do_debugoutputall(int val)
 int
 snmp_get_do_debugoutputall(void)
 {
-    return 0
+    return 0;
 }
 
 void