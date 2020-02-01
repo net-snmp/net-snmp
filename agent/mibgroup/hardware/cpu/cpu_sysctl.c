net-snmp/net-snmp

commit 2bbad43c53ffe040f02bc6269e32368edd7c6ca7

Patch for cpu_sysctl for FreeBSD 12.
Patch taken from FreeBSD ports collection.

agent/mibgroup/hardware/cpu/cpu_sysctl.c
@@ -133,8 +133,21 @@ void init_cpu_sysctl( void ) {
 #endif  /* VM_UVMEXP2 || VM_UVMEXP */
 
 #elif defined(__FreeBSD__)                /* FreeBSD */
+#if __FreeBSD_version >= 1200028
+#define	VMMETER_TYPE	uint64_t
+#else
+#define	VMMETER_TYPE	u_int
+#endif
+struct __vmmeter {
+	VMMETER_TYPE	v_intr;
+	VMMETER_TYPE	v_swtch;
+	VMMETER_TYPE	v_swappgsin;
+	VMMETER_TYPE	v_swappgsout;
+	VMMETER_TYPE	v_swapin;
+	VMMETER_TYPE	v_swapout;
+};
 #define NETSNMP_VM_STATS       VM_METER
-#define NETSNMP_VM_STATS_TYPE  struct vmmeter
+#define NETSNMP_VM_STATS_TYPE  struct __vmmeter
     #define NS_VM_INTR		v_intr
     #define NS_VM_SWTCH		v_swtch
     #define NS_VM_PAGEIN	v_swappgsin
@@ -209,7 +222,23 @@ int netsnmp_cpu_arch_load( netsnmp_cache *cache, void *magic ) {
          * Interrupt/Context Switch statistics
          *   XXX - Do these really belong here ?
          */
+#ifdef __FreeBSD__
+#define	GET_VM_STATS(space, name) sysctlbyname("vm.stats." #space "." #name, &mem_stats.name, &len, NULL, 0)
+    {
+	size_t len;
+
+	len = sizeof(VMMETER_TYPE);
+	GET_VM_STATS(sys, v_intr);
+	GET_VM_STATS(sys, v_swtch);
+	GET_VM_STATS(vm, v_swappgsin);
+	GET_VM_STATS(vm, v_swappgsout);
+	GET_VM_STATS(vm, v_swapin);
+	GET_VM_STATS(vm, v_swapout);
+    }
+#undef GET_VM_STATS
+#else
     sysctl(mem_mib, 2, &mem_stats, &mem_size, NULL, 0);
+#endif
     cpu->nInterrupts  = mem_stats.NS_VM_INTR;
     cpu->nCtxSwitches = mem_stats.NS_VM_SWTCH;
     cpu->swapIn       = mem_stats.NS_VM_SWAPIN;

commit dd6cb372db7c1d7ca606c0d13c48f20148da8038

FreeBSD, HOST-RESOURCES-MIB: Fix two compiler warnings
Fix the following compiler warnings:

hardware/cpu/cpu_sysctl.c:204:20: warning: unused variable 'mem_mib' [-Wunused-variable]
    int            mem_mib[] = { CTL_VM, NETSNMP_VM_STATS };
                   ^
hardware/cpu/cpu_sysctl.c:205:20: warning: unused variable 'mem_size' [-Wunused-variable]
    size_t         mem_size  = sizeof(NETSNMP_VM_STATS_TYPE);
                   ^

Fixes: 2bbad43c53ff ("Patch for cpu_sysctl for FreeBSD 12")

agent/mibgroup/hardware/cpu/cpu_sysctl.c
@@ -201,8 +201,6 @@ int netsnmp_cpu_arch_load( netsnmp_cache *cache, void *magic ) {
     size_t         mcpu_size;
 #endif
     NETSNMP_VM_STATS_TYPE mem_stats;
-    int            mem_mib[] = { CTL_VM, NETSNMP_VM_STATS };
-    size_t         mem_size  = sizeof(NETSNMP_VM_STATS_TYPE);
     netsnmp_cpu_info *cpu = netsnmp_cpu_get_byIdx( -1, 0 );
 
 #if defined(__FreeBSD__) || defined(__NetBSD__)
@@ -233,7 +231,14 @@ int netsnmp_cpu_arch_load( netsnmp_cache *cache, void *magic ) {
     }
 #undef GET_VM_STATS
 #else
-    sysctl(mem_mib, 2, &mem_stats, &mem_size, NULL, 0);
+    {
+        static const int mem_mib[] = { CTL_VM, NETSNMP_VM_STATS };
+        size_t           mem_size  = sizeof(mem_stats);
+
+        sysctl(mem_mib, sizeof(mem_mib) / sizeof(mem_mib[0]), &mem_stats,
+               &mem_size, NULL, 0);
+        netsnmp_assert(mem_size == sizeof(mem_stats));
+    }
 #endif
     /*
      * Interrupt/Context Switch statistics