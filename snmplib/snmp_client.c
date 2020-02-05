net-snmp/net-snmp

commit 8e7458661b0eeb72d787a69cb915c16b151beee1

- lots of 64 bit checks added iff SIZEOF_LONG != 4   - check counter64 high/low overflows (int64.c)   - truncate received values to 32 bits (asn1.c)   - truncate sent values to 32 bits before encoding (asn1.c) - update set_var_value (snmp_client.c)   - truncate varbind value sets to 32 bits   - handle set of integers of various lengths (1, 2, 4, 8 bytes)
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12648 06827809-a52a-0410-b366-d66718629ded

snmplib/snmp_client.c
@@ -742,9 +742,6 @@ snmp_set_var_value(netsnmp_variable_list * vars,
                    const u_char * value, size_t len)
 {
     int             largeval = 1;
-    const long     *val_long = NULL;
-    const int      *val_int  = NULL;
-    const u_int    *val_uint  = NULL;
 
     /*
      * xxx-rks: why the unconditional free? why not use existing
@@ -777,23 +774,68 @@ snmp_set_var_value(netsnmp_variable_list * vars,
     case ASN_IPADDRESS:
     case ASN_COUNTER:
         if (value) {
-            if (largeval) {
+            if (vars->val_len == sizeof(int)) {
+                if (ASN_INTEGER == vars->type) {
+                    const int      *val_int 
+                        = (const int *) value;
+                    *(vars->val.integer) = (long) *val_int;
+                } else {
+                    const u_int    *val_uint
+                        = (const u_int *) value;
+                    *(vars->val.integer) = (unsigned long) *val_uint;
+                }
+            }
+#if SIZEOF_LONG != SIZEOF_INT
+            else if (vars->val_len == sizeof(long)){
+                const u_long   *val_ulong
+                    = (const u_long *) value;
+                *(vars->val.integer) = *val_ulong;
+                if (*(vars->val.integer) > 0xffffffff) {
+                    snmp_log(LOG_ERR,"truncating integer value > 32 bits\n");
+                    *(vars->val.integer) &= 0xffffffff;
+                }
+            }
+#endif
+#if SIZEOF_LONG != SIZEOF_LONG_LONG
+            else if (vars->val_len == sizeof(long long)){
+                const unsigned long long   *val_ullong
+                    = (const unsigned long long *) value;
+                *(vars->val.integer) = (long) *val_ullong;
+                if (*(vars->val.integer) > 0xffffffff) {
+                    snmp_log(LOG_ERR,"truncating integer value > 32 bits\n");
+                    *(vars->val.integer) &= 0xffffffff;
+                }
+            }
+#endif
+#if SIZEOF_SHORT != SIZEOF_INT
+            else if (vars->val_len == sizeof(short)) {
+                if (ASN_INTEGER == vars->type) {
+                    const short      *val_short 
+                        = (const short *) value;
+                    *(vars->val.integer) = (long) *val_short;
+                } else {
+                    const u_short    *val_ushort
+                        = (const u_short *) value;
+                    *(vars->val.integer) = (unsigned long) *val_ushort;
+                }
+            }
+#endif
+            else if (vars->val_len == sizeof(char)) {
+                if (ASN_INTEGER == vars->type) {
+                    const char      *val_char 
+                        = (const char *) value;
+                    *(vars->val.integer) = (long) *val_char;
+                } else {
+                    *(vars->val.integer) = (unsigned long) *value;
+                }
+            }
+            else {
                 snmp_log(LOG_ERR,"bad size for integer-like type (%d)\n",
                          vars->val_len);
                 return (1);
-            } else if (vars->val_len == sizeof(int)) {
-                if (ASN_INTEGER == vars->type) {
-                    val_int = (const int *) value;
-                    *(vars->val.integer) = (long) *val_int;
-                } else {
-                    val_uint = (const u_int *) value;
-                    *(vars->val.integer) = (long) *val_uint;
-                }
-            } else {
-                val_long = (const long *) value;
-                *(vars->val.integer) = *val_long;
             }
-        }
+        } else
+            *(vars->val.integer) = 0;
         vars->val_len = sizeof(long);
         break;
 

snmplib/asn1.c
@@ -470,6 +470,13 @@ asn_parse_int(u_char * data,
     while (asn_length--)
         value = (value << 8) | *bufp++;
 
+#if SIZEOF_LONG != 4
+    if ((unsigned long)value > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating integer value to 32 bits\n");
+        value &= 0xffffffff;
+    }
+#endif
+
     DEBUGMSG(("dumpv_recv", "  Integer:\t%ld (0x%.2X)\n", value, value));
 
     *intp = value;
@@ -535,6 +542,13 @@ asn_parse_unsigned_int(u_char * data,
     while (asn_length--)
         value = (value << 8) | *bufp++;
 
+#if SIZEOF_LONG != 4
+    if (value > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating uinteger value to 32 bits\n");
+        value &= 0xffffffff;
+    }
+#endif
+
     DEBUGMSG(("dumpv_recv", "  UInteger:\t%ld (0x%.2X)\n", value, value));
 
     *intp = value;
@@ -584,6 +598,12 @@ asn_build_int(u_char * data,
         return NULL;
     }
     integer = *intp;
+#if SIZEOF_LONG != 4
+    if ((unsigned long)integer > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating integer value to 32 bits\n");
+        integer &= 0xffffffff;
+    }
+#endif
     /*
      * Truncate "unnecessary" bytes off of the most significant end of this
      * 2's complement integer.  There should be no sequence of 9
@@ -663,6 +683,12 @@ asn_build_unsigned_int(u_char * data,
         return NULL;
     }
     integer = *intp;
+#if SIZEOF_LONG != 4
+    if (integer > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating uinteger value to 32 bits\n");
+        integer &= 0xffffffff;
+    }
+#endif
     mask = ((u_long) 0xFF) << (8 * (sizeof(long) - 1));
     /*
      * mask is 0xFF000000 on a big-endian machine 
@@ -1286,18 +1312,13 @@ asn_parse_objid(u_char * data,
                 (subidentifier << 7) + (*(u_char *) bufp & ~ASN_BIT8);
             length--;
         } while (*(u_char *) bufp++ & ASN_BIT8);        /* last byte has high bit clear */
-        /*
-         * ?? note, this test will never be true, since the largest value
-         * of subidentifier is the value of MAX_SUBID! 
-         *
-         * Yes: PC-LINT says the same thing
-         * No!: the agent can be configured to use a char instead of a long
-         *      for OIDs, in which case this test is essential.
-         */
+
+#if defined(EIGHTBIT_SUBIDS) || (SIZEOF_LONG != 4)
         if (subidentifier > (u_long) MAX_SUBID) {
             ERROR_MSG("subidentifier too large");
             return NULL;
         }
+#endif
         *oidp++ = (oid) subidentifier;
     }
 
@@ -1439,6 +1460,12 @@ asn_build_objid(u_char * data,
         if (i >= (int) objidlength)
             break;
         objid_val = *op++;	/* XXX - doesn't handle 2.X (X > 40) */
+#if SIZEOF_LONG != 4
+        if (objid_val > 0xffffffff) {
+            snmp_log(LOG_ERR,"truncating objid value to 32 bits\n");
+            objid_val &= 0xffffffff;
+        }
+#endif
     }
 
     /*
@@ -1454,8 +1481,13 @@ asn_build_objid(u_char * data,
      */
     for (i = 1, objid_val = first_objid_val, op = objid + 2;
          i < (int) objidlength; i++) {
-        if (i != 1)
+        if (i != 1) {
             objid_val = *op++;
+#if SIZEOF_LONG != 4
+            if (objid_val > 0xffffffff) /* already logged warning above */
+                objid_val &= 0xffffffff;
+#endif
+        }
         switch (objid_size[i]) {
         case 1:
             *data++ = (u_char) objid_val;
@@ -1783,6 +1815,17 @@ asn_parse_unsigned_int64(u_char * data,
         low = (low << 8) | *bufp++;
     }
 
+#if SIZEOF_LONG != 4
+    if (high > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 high value to 32 bits\n");
+        high &= 0xffffffff;
+    }
+    if (low > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 low value to 32 bits\n");
+        low &= 0xffffffff;
+    }
+#endif
+
     cp->low = low;
     cp->high = high;
 
@@ -1843,6 +1886,16 @@ asn_build_unsigned_int64(u_char * data,
     intsize = 8;
     low = cp->low;
     high = cp->high;
+#if SIZEOF_LONG != 4
+    if (high > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 high value to 32 bits\n");
+        high &= 0xffffffff;
+    }
+    if (low > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 low value to 32 bits\n");
+        low &= 0xffffffff;
+    }
+#endif
     mask = ((u_long) 0xFF) << (8 * (sizeof(long) - 1));
     /*
      * mask is 0xFF000000 on a big-endian machine 
@@ -2041,6 +2094,17 @@ asn_parse_signed_int64(u_char * data,
         low = (low << 8) | *bufp++;
     }
 
+#if SIZEOF_LONG != 4
+    if (high > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 high value to 32 bits\n");
+        high &= 0xffffffff;
+    }
+    if (low > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 low value to 32 bits\n");
+        low &= 0xffffffff;
+    }
+#endif
+
     cp->low = low;
     cp->high = high;
 
@@ -2103,6 +2167,16 @@ asn_build_signed_int64(u_char * data,
     memcpy(&c64, cp, sizeof(struct counter64)); /* we're may modify it */
     low = c64.low;
     high = c64.high;
+#if SIZEOF_LONG != 4
+    if (high > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 high value to 32 bits\n");
+        high &= 0xffffffff;
+    }
+    if (low > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 low value to 32 bits\n");
+        low &= 0xffffffff;
+    }
+#endif
 
     /*
      * Truncate "unnecessary" bytes off of the most significant end of this
@@ -2673,6 +2747,12 @@ asn_realloc_rbuild_int(u_char ** pkt, size_t * pkt_len,
         _asn_size_err(errpre, intsize, sizeof(long));
         return 0;
     }
+#if SIZEOF_LONG != 4
+    if ((unsigned long)integer > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating integer value to 32 bits\n");
+        integer &= 0xffffffff;
+    }
+#endif
 
     if (((*pkt_len - *offset) < 1) && !(r && asn_realloc(pkt, pkt_len))) {
         return 0;
@@ -2825,6 +2905,13 @@ asn_realloc_rbuild_unsigned_int(u_char ** pkt, size_t * pkt_len,
         return 0;
     }
 
+#if SIZEOF_LONG != 4
+    if (integer > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating uinteger value to 32 bits\n");
+        integer &= 0xffffffff;
+    }
+#endif
+
     if (((*pkt_len - *offset) < 1) && !(r && asn_realloc(pkt, pkt_len))) {
         return 0;
     }
@@ -2960,6 +3047,12 @@ asn_realloc_rbuild_objid(u_char ** pkt, size_t * pkt_len,
     } else {
         for (i = objidlength; i > 2; i--) {
             tmpint = objid[i - 1];
+#if SIZEOF_LONG != 4
+            if ((unsigned long)tmpint > 0xffffffff) {
+                snmp_log(LOG_ERR,"truncating oid subid to 32 bits\n");
+                tmpint &= 0xffffffff;
+            }
+#endif
 
             if (((*pkt_len - *offset) < 1)
                 && !(r && asn_realloc(pkt, pkt_len))) {
@@ -3176,6 +3269,16 @@ asn_realloc_rbuild_unsigned_int64(u_char ** pkt, size_t * pkt_len,
                       sizeof(struct counter64));
         return 0;
     }
+#if SIZEOF_LONG != 4
+    if (high > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 high value to 32 bits\n");
+        high &= 0xffffffff;
+    }
+    if (low > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 low value to 32 bits\n");
+        low &= 0xffffffff;
+    }
+#endif
 
     /*
      * Encode the low 4 bytes first.  
@@ -3358,6 +3461,17 @@ asn_realloc_rbuild_signed_int64(u_char ** pkt, size_t * pkt_len,
                       sizeof(struct counter64));
         return 0;
     }
+
+#if SIZEOF_LONG != 4
+    if (high > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 high value to 32 bits\n");
+        high &= 0xffffffff;
+    }
+    if (low > 0xffffffff) {
+        snmp_log(LOG_ERR,"truncating counter64 low value to 32 bits\n");
+        low &= 0xffffffff;
+    }
+#endif
 
     /*
      * Encode the low 4 bytes first.  

snmplib/int64.c
@@ -179,7 +179,9 @@ incrByU16(U64 * pu64, unsigned int u16)
      */
     pu64->low = (ulT1 + u16) & 0x0FFFFFFFFL;
     pu64->high++;
-
+#if SIZEOF_LONG != 4
+    pu64->high &= 0xffffffff;
+#endif
 }                               /* incrByV16 */
 
 void
@@ -188,8 +190,15 @@ incrByU32(U64 * pu64, unsigned int u32)
     unsigned int    tmp;
     tmp = pu64->low;
     pu64->low += u32;
-    if (pu64->low < tmp)
+#if SIZEOF_LONG != 4
+    pu64->low &= 0xffffffff;
+#endif
+    if (pu64->low < tmp) {
         pu64->high++;
+#if SIZEOF_LONG != 4
+        pu64->high &= 0xffffffff;
+#endif
+    }
 }
 
 /**
@@ -214,6 +223,9 @@ void
 u64Incr(U64 * pu64out, const U64 * pu64one)
 {
     pu64out->high += pu64one->high;
+#if SIZEOF_LONG != 4
+    pu64out->high &= 0xffffffff;
+#endif
     incrByU32(pu64out, pu64one->low);
 }
 
@@ -247,7 +259,6 @@ u64Copy(U64 * pu64one, const U64 * pu64two)
 void
 zeroU64(U64 * pu64)
 {
-
     pu64->low = 0;
     pu64->high = 0;
 }                               /* zeroU64 */
@@ -315,8 +326,12 @@ netsnmp_c64_check_for_32bit_wrap(struct counter64 *old_val,
      */
     if (new_val->high == old_val->high) {
         DEBUGMSGTL(("c64:check_wrap", "32 bit wrap\n"));
-        if (adjust)
+        if (adjust) {
             ++new_val->high;
+#if SIZEOF_LONG != 4
+            new_val->high &= 0xffffffff;
+#endif
+        }
         return 32;
     }
     else if ((new_val->high == (old_val->high + 1)) ||

commit b4f334a85e5be08e2c867586ec81a6802e8cd0b1

libsnmp: Cast signed char to signed int when setting value
We have an INT8 in a MIB, an Integer32(-128..127) to be more specific.
In a custom software we call 'snmp_pdu_add_variable()' with
type == ASN_INTEGER and len == 1. Deeper down the code path, we come to
'snmp_set_var_value()' and this part:

        else if (vars->val_len == sizeof(char)) {
            if (ASN_INTEGER == vars->type) {
                const char      *val_char
                    = (const char *) value;
                *(vars->val.integer) = (long) *val_char;
            } else {
                    const u_char    *val_uchar
                    = (const u_char *) value;
                *(vars->val.integer) = (unsigned long) *val_uchar;
            }

Obviously the intent is to cast depending on whether the integer is
supposed to be signed or not, but although he jumps to the right code
path here, the result in vars->val.integer is wrong then. It does not
work with type 'char' like above.

We got values looking unsigned added to our pdu which we set to our
net-snmp server then and got them back unsigned on SNMP get requests
later, for example we fed an int8_t with value -32 we read back 224.

So this patch explicitly uses type 'signed char' which works for us.

Compiler is gcc 4.7 and we're cross-compiling for
arm-v5te-linux-gnueabi on Linux.

Fixes: 8e7458661b0eeb72d787a69cb915c16b151beee1
Signed-off-by: Alexander Dahl <ada@thorsis.com>

snmplib/snmp_client.c
@@ -888,8 +888,8 @@ snmp_set_var_value(netsnmp_variable_list * vars,
         }
         else if (vars->val_len == sizeof(char)) {
             if (ASN_INTEGER == vars->type) {
-                const char      *val_char 
-                    = (const char *) value;
+                const signed char   *val_char
+                    = (const signed char *) value;
                 *(vars->val.integer) = (long) *val_char;
             } else {
                     const u_char    *val_uchar