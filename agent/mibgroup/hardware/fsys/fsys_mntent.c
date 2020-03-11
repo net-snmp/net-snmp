8c02e747d35d1df12c3482895879caf38e448c44
Niels Baggesen <nba@users.sourceforge.net>
2020-03-11 20:38:50 +0100

    fsys_mrtent: add glusterfs as a recognized filesystem type. 	Thanks to Josef Ricky.

--- agent/mibgroup/hardware/fsys/fsys_mntent.c
+++ agent/mibgroup/hardware/fsys/fsys_mntent.c
@@ -135,6 +135,8 @@ _fsys_type( char *typename )
               !strcmp(typename, MNTTYPE_TMPFS) ||
               !strcmp(typename, MNTTYPE_GFS) ||
               !strcmp(typename, MNTTYPE_GFS2) ||
+              !strcmp(typename, MNTTYPE_GLUSTERFS) ||
+              !strcmp(typename, MNTTYPE_FUSEGLUSTERFS) ||    
               !strcmp(typename, MNTTYPE_XFS) ||
               !strcmp(typename, MNTTYPE_JFS) ||
               !strcmp(typename, MNTTYPE_VXFS) ||
