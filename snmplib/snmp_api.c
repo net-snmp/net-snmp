b83b0ddcf8fa27108b198bbb0fe247e18d2dae4f
Bart Van Assche <bvanassche@acm.org>
2020-03-21 20:01:18 -0700

    libsnmp: Improve snmp_async_send() and snmp_sess_async_send() documentation

--- snmplib/snmp_api.c
+++ snmplib/snmp_api.c
@@ -5252,11 +5252,12 @@ snmp_async_send(netsnmp_session * session,
  * @param[in] pdu      PDU to send.
  * @param[in] callback Callback function called after processing of the PDU
  *                     finished. This function is called if the PDU has not
- *                     been sent or after a response has been received.
+ *                     been sent or after a response has been received. Must
+ *                     not free @pdu.
  * @param[in] cb_data  Will be passed as fifth argument to @callback.
  *
- * @return Returns 0 if sending failed or a non-zero request ID if sending
- *   succeeded.
+ * @return If successful, returns the request id of @pdu and frees @pdu.
+ * If not successful, returns zero and expects the caller to free @pdu.
  */
 static int
 _sess_async_send(void *sessp,
@@ -5419,6 +5420,20 @@ _sess_async_send(void *sessp,
     return reqid;
 }
 
+/**
+ * Send a PDU asynchronously.
+ *
+ * @param[in] slp      Session pointer.
+ * @param[in] pdu      PDU to send.
+ * @param[in] callback Callback function called after processing of the PDU
+ *                     finished. This function is called if the PDU has not
+ *                     been sent or after a response has been received. Must
+ *                     not free @pdu.
+ * @param[in] cb_data  Will be passed as fifth argument to @callback.
+ *
+ * @return If successful, returns the request id of @pdu and frees @pdu.
+ * If not successful, returns zero and expects the caller to free @pdu.
+ */
 int
 snmp_sess_async_send(void *sessp,
                      netsnmp_pdu *pdu,
