net-snmp/net-snmp

commit 8fbaffcf3a5ca62819c1148a571d7c69f741b4c8

HOST-RESOURCES-MIB: Move platform-specific code into data_access/hr_disk_${platform}.c
This makes it much easier to tell which functionality is implemented
on which platform and which functionality has not been implemented.
The only functional change in this patch is that support for the
DIOCGMEDIASIZE ioctl has been added. The DIOCGDINFO ioctl has namely
been removed from FreeBSD. See also
https://lists.freebsd.org/pipermail/freebsd-current/2015-January/053963.html.

agent/mibgroup/host/hr_disk.h
@@ -7,13 +7,62 @@
 
 config_require(host/hr_device)
 
+#if defined(__linux__)
+config_require(host/data_access/hr_disk_linux)
+#elif defined(__APPLE__) && defined(__MACH__)
+config_require(host/data_access/hr_disk_darwin)
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || \
+    defined(__DragonFly__) || defined(__bsdi__)
+config_require(host/data_access/hr_disk_bsd)
+#elif defined(__sun)
+config_require(host/data_access/hr_disk_solaris)
+#elif defined(_AIX)
+config_require(host/data_access/hr_disk_aix)
+#elif defined(__hpux)
+config_require(host/data_access/hr_disk_hpux)
+#endif
+
+/*************************************************************
+ * constants for enums for the MIB node
+ * hrDiskStorageAccess (INTEGER / ASN_INTEGER)
+ */
+#define HRDISKSTORAGEACCESS_READWRITE  1
+#define HRDISKSTORAGEACCESS_READONLY  2
+
+
+/*************************************************************
+ * constants for enums for the MIB node
+ * hrDiskStorageMedia (INTEGER / ASN_INTEGER)
+ */
+#define HRDISKSTORAGEMEDIA_OTHER  1
+#define HRDISKSTORAGEMEDIA_UNKNOWN  2
+#define HRDISKSTORAGEMEDIA_HARDDISK  3
+#define HRDISKSTORAGEMEDIA_FLOPPYDISK  4
+#define HRDISKSTORAGEMEDIA_OPTICALDISKROM  5
+#define HRDISKSTORAGEMEDIA_OPTICALDISKWORM  6
+#define HRDISKSTORAGEMEDIA_OPTICALDISKRW  7
+#define HRDISKSTORAGEMEDIA_RAMDISK  8
+
 extern void     init_hr_disk(void);
+extern void     init_hr_disk_entries(void);
 extern void     shutdown_hr_disk(void);
 
 extern void     Init_HR_Disk(void);
+extern void     Add_HR_Disk_entry(const char *, int, int, int, int,
+                                  const char *, int, int);
 extern int      Get_Next_HR_Disk(void);
 extern int      Get_Next_HR_Disk_Partition(char *, size_t, int);
+extern int      Query_Disk(int, const char *);
+extern void     Save_HR_Disk_General(void);
+extern void     Save_HR_Disk_Specific(void);
+extern int      Is_It_Writeable(void);
+extern int      What_Type_Disk(void);
+extern int      Is_It_Removeable(void);
 extern FindVarMethod var_hrdisk;
+extern long     HRD_savedCapacity;
+#define HRD_SAVED_MODEL_SIZE 40
+extern char     HRD_savedModel[];
+extern int      HRD_type_index;
 
 
 #endif                          /* _MIBGROUP_HRDISK_H */

agent/mibgroup/host/data_access/hr_disk_aix.c
@@ -1,0 +1,42 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/snmp_agent.h>
+#include <net-snmp/agent/snmp_vars.h>
+#include "../hr_disk.h"
+
+void init_hr_disk_entries(void)
+{
+}
+
+void shutdown_hr_disk(void)
+{
+}
+
+void Save_HR_Disk_Specific(void)
+{
+}
+
+void Save_HR_Disk_General(void)
+{
+}
+
+int Query_Disk(int fd, const char *devfull)
+{
+    return -1;
+}
+
+int Is_It_Writeable(void)
+{
+    return 1;                 /* read-write */
+}
+
+int What_Type_Disk(void)
+{
+    return 2;                 /* Unknown */
+}
+
+int Is_It_Removeable(void)
+{
+    return 2;                 /* false */
+}
+

agent/mibgroup/host/data_access/hr_disk_bsd.c
@@ -1,0 +1,74 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/snmp_agent.h>
+#include <net-snmp/agent/snmp_vars.h>
+#define DKTYPENAMES
+#include <sys/disklabel.h>
+#include "../hr_disk.h"
+
+static struct disklabel HRD_info;
+
+void init_hr_disk_entries(void)
+{
+#if defined(freebsd4) || defined(freebsd5)
+    Add_HR_Disk_entry("/dev/ad%ds%d%c", 0, 1, 1, 4, "/dev/ad%ds%d", 'a', 'h');
+    Add_HR_Disk_entry("/dev/da%ds%d%c", 0, 1, 1, 4, "/dev/da%ds%d", 'a', 'h');
+#elif defined(freebsd3)
+    Add_HR_Disk_entry("/dev/wd%ds%d%c", 0, 1, 1, 4, "/dev/wd%ds%d", 'a',
+                      'h');
+    Add_HR_Disk_entry("/dev/sd%ds%d%c", 0, 1, 1, 4, "/dev/sd%ds%d", 'a',
+                      'h');
+#elif defined(freebsd2)
+    Add_HR_Disk_entry("/dev/wd%d%c", -1, -1, 0, 3, "/dev/wd%d", 'a', 'h');
+    Add_HR_Disk_entry("/dev/sd%d%c", -1, -1, 0, 3, "/dev/sd%d", 'a', 'h');
+#elif defined(netbsd1)
+    Add_HR_Disk_entry("/dev/wd%d%c", -1, -1, 0, 3, "/dev/wd%dc", 'a', 'h');
+    Add_HR_Disk_entry("/dev/sd%d%c", -1, -1, 0, 3, "/dev/sd%dc", 'a', 'h');
+#endif
+}
+
+void shutdown_hr_disk(void)
+{
+}
+
+void Save_HR_Disk_Specific(void)
+{
+    HRD_savedCapacity = HRD_info.d_secperunit / 2;
+}
+
+void Save_HR_Disk_General(void)
+{
+    strlcpy(HRD_savedModel, dktypenames[HRD_info.d_type], HRD_SAVED_MODEL_SIZE);
+}
+
+
+int Query_Disk(int fd, const char *devfull)
+{
+#if defined(DIOCGMEDIASIZE)
+    unsigned long long size64;
+
+    if (ioctl(fd, DIOCGMEDIASIZE, &size64) < 0)
+        return -1;
+    HRD_info.d_secperunit = size64 / 512;
+    return 0;
+#elif defined(DIOCGDINFO)
+    return ioctl(fd, DIOCGDINFO, &HRD_info);
+#else
+    return -1;
+#endif
+}
+
+int Is_It_Writeable(void)
+{
+    return 1;                 /* read-write */
+}
+
+int What_Type_Disk(void)
+{
+    return 2;                 /* Unknown */
+}
+
+int Is_It_Removeable(void)
+{
+    return 2;                 /* false */
+}

agent/mibgroup/host/data_access/hr_disk_darwin.c
@@ -1,0 +1,204 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/snmp_agent.h>
+#include <net-snmp/agent/snmp_vars.h>
+#include "../hr_disk.h"
+#include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/IOKitLib.h>
+#include <IOKit/storage/IOBlockStorageDriver.h>
+#include <IOKit/storage/IOMedia.h>
+#include <IOKit/IOBSD.h>
+#include <DiskArbitration/DADisk.h>
+
+static int64_t  HRD_cap;
+static int      HRD_access;
+static int      HRD_type;
+static int      HRD_removable;
+static char     HRD_model[40];
+static int      HRD_saved_access;
+static int      HRD_saved_type;
+static int      HRD_saved_removeble;
+
+typedef struct type_value_map_s {
+     const char *type;
+     uint32_t    value;
+} type_value_map;
+
+static type_value_map media_type_map[] = {
+    { "CD-ROM", HRDISKSTORAGEMEDIA_OPTICALDISKROM},
+    { "DVD-R", HRDISKSTORAGEMEDIA_OPTICALDISKWORM},
+    { "DVD+R", HRDISKSTORAGEMEDIA_OPTICALDISKWORM},
+};
+static const int media_types = sizeof(media_type_map)/sizeof(media_type_map[0]);
+
+static int
+_get_type_value(const char *str_type)
+{
+    int           i, len;
+
+    if (NULL == str_type)
+        return HRDISKSTORAGEMEDIA_UNKNOWN;
+
+    len = strlen(str_type);
+    for (i = 0; i < media_types; ++i) {
+        if (0 == strcmp(media_type_map[i].type, str_type))
+            return media_type_map[i].value;
+    }
+
+    return HRDISKSTORAGEMEDIA_UNKNOWN;
+}
+
+static type_value_map proto_map[] = {
+    { "ATA", HRDISKSTORAGEMEDIA_HARDDISK},
+    { "ATAPI", HRDISKSTORAGEMEDIA_OPTICALDISKROM}
+};
+static int proto_maps = sizeof(proto_map)/sizeof(proto_map[0]);
+
+static int _get_type_from_protocol(const char *prot)
+{
+    int           i, len;
+
+    if (NULL == prot)
+        return TV_FALSE;
+
+    len = strlen(prot);
+    for (i = 0; i < proto_maps; ++i) {
+        if (0 == strcmp(proto_map[i].type, prot))
+            return proto_map[i].value;
+    }
+
+    return HRDISKSTORAGEMEDIA_UNKNOWN;
+}
+
+void init_hr_disk_entries(void)
+{
+    Add_HR_Disk_entry("/dev/disk%ds%d", -1, -1, 0, 32, "/dev/disk%d", 1, 32);
+}
+
+void shutdown_hr_disk(void)
+{
+}
+
+void Save_HR_Disk_Specific(void)
+{
+    HRD_savedCapacity = HRD_cap / 1024;
+    HRD_saved_access = HRD_access;
+    HRD_saved_type = HRD_type;
+    HRD_saved_removeble = HRD_removable;
+}
+
+void Save_HR_Disk_General(void)
+{
+    strlcpy(HRD_savedModel, HRD_model, HRD_SAVED_MODEL_SIZE);
+}
+
+int Query_Disk(int fd, const char *devfull)
+{
+    DASessionRef        sess_ref;
+    DADiskRef           disk;
+    CFDictionaryRef     desc;
+    CFStringRef         str_ref;
+    CFNumberRef         number_ref;
+    CFBooleanRef        bool_ref;
+    CFStringEncoding    sys_encoding = CFStringGetSystemEncoding();
+
+    sess_ref = DASessionCreate(NULL);
+    if (NULL == sess_ref)
+        return -1;
+
+    disk = DADiskCreateFromBSDName(NULL, sess_ref, devfull);
+    if (NULL == disk) {
+        CFRelease(sess_ref);
+        return -1;
+    }
+
+    desc = DADiskCopyDescription(disk);
+    if (NULL == desc) {
+        CFRelease(disk);
+        CFRelease(sess_ref);
+        return -1;
+    }
+
+    number_ref = (CFNumberRef)
+        CFDictionaryGetValue(desc, kDADiskDescriptionMediaSizeKey);
+    if (number_ref)
+        CFNumberGetValue(number_ref, kCFNumberSInt64Type, &HRD_cap);
+    else
+        HRD_cap = 0;
+    DEBUGMSGTL(("verbose:diskmgr:darwin", " size %lld\n", HRD_cap));
+
+    /** writable?  */
+    bool_ref = (CFBooleanRef)
+        CFDictionaryGetValue(desc, kDADiskDescriptionMediaWritableKey);
+    if (bool_ref)
+        HRD_access = CFBooleanGetValue(bool_ref);
+    else
+        HRD_access = 0;
+    DEBUGMSGTL(("verbose:diskmgr:darwin", " writable %d\n",
+                HRD_access));
+
+    /** removable?  */
+    bool_ref = (CFBooleanRef)
+        CFDictionaryGetValue(desc, kDADiskDescriptionMediaRemovableKey);
+    if (bool_ref)
+        HRD_removable = CFBooleanGetValue(bool_ref);
+    else
+        HRD_removable = 0;
+    DEBUGMSGTL(("verbose:diskmgr:darwin", " removable %d\n",
+                HRD_removable));
+
+    /** get type */
+    str_ref = (CFStringRef)
+        CFDictionaryGetValue(desc, kDADiskDescriptionMediaTypeKey);
+    if (str_ref) {
+        HRD_type = _get_type_value(CFStringGetCStringPtr(str_ref,
+                                                         sys_encoding));
+        DEBUGMSGTL(("verbose:diskmgr:darwin", " type %s / %d\n",
+                    CFStringGetCStringPtr(str_ref, sys_encoding),
+                    HRD_type));
+    } else {
+        str_ref = (CFStringRef)
+            CFDictionaryGetValue(desc, kDADiskDescriptionDeviceProtocolKey);
+        if (str_ref) {
+            HRD_type =
+                _get_type_from_protocol(CFStringGetCStringPtr(str_ref,
+                                                              sys_encoding));
+            DEBUGMSGTL(("verbose:diskmgr:darwin", " type %s / %d\n",
+                        CFStringGetCStringPtr(str_ref, sys_encoding),
+                        HRD_type));
+        }
+        else
+            HRD_type = HRDISKSTORAGEMEDIA_UNKNOWN;
+    }
+
+    /** model */
+    str_ref = (CFStringRef)
+        CFDictionaryGetValue(desc, kDADiskDescriptionDeviceModelKey);
+    if (str_ref) {
+        strlcpy(HRD_model, CFStringGetCStringPtr(str_ref, sys_encoding),
+                sizeof(HRD_model));
+        DEBUGMSGTL(("verbose:diskmgr:darwin", " model %s\n", HRD_model));
+    } else {
+        HRD_model[0] = 0;
+    }
+    CFRelease(disk);
+    CFRelease(desc);
+    CFRelease(sess_ref);
+
+    return 0;
+}
+
+int Is_It_Writeable(void)
+{
+    return HRD_access ? 1 /* read-write */ : 2 /* read-only */;
+}
+
+int Is_It_Removeable(void)
+{
+    return HRD_removable ? 1 /* true */ : 2 /* false */;
+}
+
+int What_Type_Disk(void)
+{
+    return HRD_type;
+}

agent/mibgroup/host/data_access/hr_disk_hpux.c
@@ -1,0 +1,96 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/snmp_agent.h>
+#include <net-snmp/agent/snmp_vars.h>
+#include <fcntl.h>
+#include <sys/diskio.h>
+#include <sys/scsi.h>
+#include "../hr_disk.h"
+
+static disk_describe_type HRD_info;
+static capacity_type HRD_cap;
+
+static int      HRD_savedIntf_type;
+static int      HRD_savedDev_type;
+static int      HRD_savedFlags;
+
+void init_hr_disk_entries(void)
+{
+#if defined(hpux10) || defined(hpux11)
+    Add_HR_Disk_entry("/dev/rdsk/c%dt%xd%d", 0, 1, 0, 15,
+                      "/dev/rdsk/c%dt%xd0", 0, 4);
+#else                           /* hpux9 */
+    Add_HR_Disk_entry("/dev/rdsk/c%dd%xs%d", 201, 201, 0, 15,
+                      "/dev/rdsk/c%dd%xs0", 0, 4);
+#endif
+}
+
+void shutdown_hr_disk(void)
+{
+}
+
+void Save_HR_Disk_Specific(void)
+{
+    HRD_savedIntf_type = HRD_info.intf_type;
+    HRD_savedDev_type = HRD_info.dev_type;
+    HRD_savedFlags = HRD_info.flags;
+    HRD_savedCapacity = HRD_cap.lba / 2;
+}
+
+void Save_HR_Disk_General(void)
+{
+    strlcpy(HRD_savedModel, HRD_info.model_num, HRD_SAVED_MODEL_SIZE);
+}
+
+int Query_Disk(int fd, const char *devfull)
+{
+    if (ioctl(fd, DIOC_DESCRIBE, &HRD_info) < 0)
+        return -1;
+    return ioctl(fd, DIOC_CAPACITY, &HRD_cap);
+}
+
+int Is_It_Writeable(void)
+{
+    if (HRD_savedFlags & WRITE_PROTECT_FLAG ||
+        HRD_savedDev_type == CDROM_DEV_TYPE)
+        return 2;             /* read only */
+
+    return 1;                 /* read-write */
+}
+
+int What_Type_Disk(void)
+{
+    switch (HRD_savedDev_type) {
+    case DISK_DEV_TYPE:
+        if (HRD_savedIntf_type == PC_FDC_INTF)
+            return 4;         /* Floppy Disk */
+        else
+            return 3;         /* Hard Disk */
+        break;
+    case CDROM_DEV_TYPE:
+        return 5;             /* Optical RO */
+        break;
+    case WORM_DEV_TYPE:
+        return 6;             /* Optical WORM */
+        break;
+    case MO_DEV_TYPE:
+        return 7;             /* Optical R/W */
+        break;
+    default:
+        return 2;             /* Unknown */
+        break;
+    }
+
+    return 2;                 /* Unknown */
+}
+
+int Is_It_Removeable(void)
+{
+    if (HRD_savedIntf_type == PC_FDC_INTF ||
+        HRD_savedDev_type == WORM_DEV_TYPE ||
+        HRD_savedDev_type == MO_DEV_TYPE ||
+        HRD_savedDev_type == CDROM_DEV_TYPE)
+        return 1;             /* true */
+
+    return 2;                 /* false */
+}

agent/mibgroup/host/data_access/hr_disk_linux.c
@@ -1,0 +1,172 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/snmp_agent.h>
+#include <net-snmp/agent/snmp_vars.h>
+#include "../hr_disk.h"
+#if HAVE_DIRENT_H
+#include <dirent.h>
+#endif
+#if defined(HAVE_REGEX_H) && defined(HAVE_REGCOMP)
+#include <regex.h>
+#endif
+#include <linux/fs.h>
+#include <linux/hdreg.h>
+#include <sys/ioctl.h>
+
+#define MAX_NUMBER_DISK_TYPES 16
+
+#if defined(HAVE_REGEX_H) && defined(HAVE_REGCOMP)
+static char    *lvm_device_names[MAX_NUMBER_DISK_TYPES];
+static int      lvm_device_count;
+#endif
+
+static struct hd_driveid HRD_info;
+static int      HRD_savedFlags;
+
+static void Add_LVM_Disks(void)
+{
+#if defined(HAVE_REGEX_H) && defined(HAVE_REGCOMP)
+    /*
+     * LVM devices are harder because their name can be almost anything (see
+     * regexp below). Each logical volume is interpreted as its own device with
+     * one partition, even if two logical volumes share common volume group.
+     */
+    regex_t         lvol;
+    int             res;
+    DIR            *dir;
+    struct dirent  *d;
+
+    res =
+        regcomp(&lvol, "[0-9a-zA-Z+_\\.-]+-[0-9a-zA-Z+_\\.-]+",
+                REG_EXTENDED | REG_NOSUB);
+    if (res != 0) {
+        char            error[200];
+        regerror(res, &lvol, error, sizeof(error)-1);
+        DEBUGMSGTL(("host/hr_disk",
+                    "Add_LVM_Disks: cannot compile regexp: %s", error));
+        return;
+    }
+
+    dir = opendir("/dev/mapper/");
+    if (dir == NULL) {
+        DEBUGMSGTL(("host/hr_disk",
+                    "Add_LVM_Disks: cannot open /dev/mapper"));
+        regfree(&lvol);
+        return;
+    }
+
+    while ((d = readdir(dir)) != NULL) {
+        res = regexec(&lvol, d->d_name, 0, NULL, 0);
+        if (res == 0) {
+            char *path = (char*)malloc(PATH_MAX + 1);
+            if (path == NULL) {
+                DEBUGMSGTL(("host/hr_disk",
+                            "Add_LVM_Disks: cannot allocate memory for device %s",
+                            d->d_name));
+                break;
+            }
+            snprintf(path, PATH_MAX-1, "/dev/mapper/%s", d->d_name);
+            Add_HR_Disk_entry(path, -1, -1, 0, 0, path, 0, 0);
+
+            /*
+             * store the device name so we can free it in Remove_LVM_Disks
+             */
+            lvm_device_names[lvm_device_count] = path;
+            ++lvm_device_count;
+            if (lvm_device_count >= MAX_NUMBER_DISK_TYPES) {
+                DEBUGMSGTL(("host/hr_disk",
+                            "Add_LVM_Disks: maximum count of LVM devices reached"));
+                break;
+            }
+        }
+    }
+    closedir(dir);
+    regfree(&lvol);
+#endif
+}
+
+static void Remove_LVM_Disks(void)
+{
+#if defined(HAVE_REGEX_H) && defined(HAVE_REGCOMP)
+    /*
+     * just free the device names allocated in add_lvm_disks
+     */
+    int             i;
+    for (i = 0; i < lvm_device_count; i++) {
+        free(lvm_device_names[i]);
+        lvm_device_names[i] = NULL;
+    }
+    lvm_device_count = 0;
+#endif
+}
+
+void init_hr_disk_entries(void)
+{
+    Add_HR_Disk_entry("/dev/hd%c%d", -1, -1, 'a', 'l', "/dev/hd%c", 1, 15);
+    Add_HR_Disk_entry("/dev/sd%c%d", -1, -1, 'a', 'p', "/dev/sd%c", 1, 15);
+    Add_HR_Disk_entry("/dev/md%d", -1, -1, 0, 3, "/dev/md%d", 0, 0);
+    Add_HR_Disk_entry("/dev/fd%d", -1, -1, 0, 1, "/dev/fd%d", 0, 0);
+
+    Add_LVM_Disks();
+}
+
+void shutdown_hr_disk(void)
+{
+    Remove_LVM_Disks();
+}
+
+void Save_HR_Disk_Specific(void)
+{
+    HRD_savedCapacity = HRD_info.lba_capacity / 2;
+    HRD_savedFlags = HRD_info.config;
+}
+
+void Save_HR_Disk_General(void)
+{
+    strlcpy(HRD_savedModel, (const char *) HRD_info.model,
+            HRD_SAVED_MODEL_SIZE);
+}
+
+int Query_Disk(int fd, const char *devfull)
+{
+    if (HRD_type_index == 0)    /* IDE hard disk */
+        return ioctl(fd, HDIO_GET_IDENTITY, &HRD_info);
+    else if (HRD_type_index != 3) {     /* SCSI hard disk, md and LVM devices */
+        long            h;
+
+        if (ioctl(fd, BLKGETSIZE, &h) < 0)
+            return -1;
+        if (HRD_type_index == 2 && h == 0L)
+            return -1;        /* ignore empty md devices */
+
+        HRD_info.lba_capacity = h;
+        if (HRD_type_index == 1)
+            snprintf((char *)HRD_info.model, sizeof(HRD_info.model),
+                     "SCSI disk (%s)", devfull);
+        else if (HRD_type_index >= 4)
+            snprintf((char *)HRD_info.model, sizeof(HRD_info.model),
+                     "LVM volume (%s)", devfull + strlen("/dev/mapper/"));
+        else
+            snprintf((char *)HRD_info.model, sizeof(HRD_info.model),
+                     "RAID disk (%s)", devfull);
+        HRD_info.config = 0;
+    }
+
+    return 0;
+}
+
+int Is_It_Writeable(void)
+{
+    return 1;                 /* read-write */
+}
+
+int What_Type_Disk(void)
+{
+    return 2;                 /* Unknown */
+}
+
+int Is_It_Removeable(void)
+{
+    return HRD_savedFlags & 0x80 ? 1 /* true */ : 2 /* false */;
+}
+

agent/mibgroup/host/data_access/hr_disk_solaris.c
@@ -1,0 +1,108 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/snmp_agent.h>
+#include <net-snmp/agent/snmp_vars.h>
+#include <sys/dkio.h>
+#include "../hr_disk.h"
+
+static struct dk_cinfo HRD_info;
+static struct dk_geom HRD_cap;
+
+static int      HRD_savedCtrl_type;
+static int      HRD_savedFlags;
+
+void init_hr_disk_entries(void)
+{
+    Add_HR_Disk_entry("/dev/rdsk/c%dt%dd0s%d", 0, 7, 0, 15,
+                      "/dev/rdsk/c%dt%dd0s0", 0, 7);
+    Add_HR_Disk_entry("/dev/rdsk/c%dd%ds%d", 0, 7, 0, 15,
+                      "/dev/rdsk/c%dd%ds0", 0, 7);
+}
+
+void shutdown_hr_disk(void)
+{
+}
+
+void Save_HR_Disk_Specific(void)
+{
+    HRD_savedCtrl_type = HRD_info.dki_ctype;
+    HRD_savedFlags = HRD_info.dki_flags;
+    HRD_savedCapacity = HRD_cap.dkg_ncyl * HRD_cap.dkg_nhead * HRD_cap.dkg_nsect / 2;   /* ??? */
+}
+
+void Save_HR_Disk_General(void)
+{
+    strlcpy(HRD_savedModel, HRD_info.dki_dname, HRD_SAVED_MODEL_SIZE);
+}
+
+int Query_Disk(int fd, const char *devfull)
+{
+    if (ioctl(fd, DKIOCINFO, &HRD_info) < 0)
+        return -1;
+    return ioctl(fd, DKIOCGGEOM, &HRD_cap);
+}
+
+int Is_It_Writeable(void)
+{
+    return HRD_savedCtrl_type == DKC_CDROM ? 2 /* read only */ :
+        1 /* read-write */;
+}
+
+int What_Type_Disk(void)
+{
+    switch (HRD_savedCtrl_type) {
+    case DKC_WDC2880:
+    case DKC_DSD5215:
+#ifdef DKC_XY450
+    case DKC_XY450:
+#endif
+    case DKC_ACB4000:
+    case DKC_MD21:
+#ifdef DKC_XD7053
+    case DKC_XD7053:
+#endif
+    case DKC_SCSI_CCS:
+#ifdef DKC_PANTHER
+    case DKC_PANTHER:
+#endif
+#ifdef DKC_CDC_9057
+    case DKC_CDC_9057:
+#endif
+#ifdef DKC_FJ_M1060
+    case DKC_FJ_M1060:
+#endif
+    case DKC_DIRECT:
+    case DKC_PCMCIA_ATA:
+        return 3;             /* Hard Disk */
+        break;
+    case DKC_NCRFLOPPY:
+    case DKC_SMSFLOPPY:
+    case DKC_INTEL82077:
+        return 4;             /* Floppy Disk */
+        break;
+    case DKC_CDROM:
+        return 5;             /* Optical RO */
+        break;
+    case DKC_PCMCIA_MEM:
+        return 8;             /* RAM disk */
+        break;
+    case DKC_MD:               /* "meta-disk" driver */
+        return 1;             /* Other */
+        break;
+    }
+
+    return 2;                 /* Unknown */
+}
+
+int Is_It_Removeable(void)
+{
+    if (HRD_savedCtrl_type == DKC_CDROM ||
+        HRD_savedCtrl_type == DKC_NCRFLOPPY ||
+        HRD_savedCtrl_type == DKC_SMSFLOPPY ||
+        HRD_savedCtrl_type == DKC_INTEL82077 ||
+        HRD_savedCtrl_type == DKC_PCMCIA_MEM ||
+        HRD_savedCtrl_type == DKC_PCMCIA_ATA)
+        return 1;             /* true */
+
+    return 2;                 /* false */
+}

commit 532c1b264eb7f68172cf03e6964bb92167b2873a

Win32, HOST-MIB: Provide a dummy implementation of this MIB
This patch fixes a build error for MinGW.

Fixes: 8fbaffcf3a5c ("HOST-RESOURCES-MIB: Move platform-specific code into data_access/hr_disk_${platform}.c")

agent/mibgroup/host/data_access/hr_disk_windows.c
@@ -1,0 +1,49 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/snmp_agent.h>
+#include <net-snmp/agent/snmp_vars.h>
+#include "../hr_disk.h"
+
+void
+init_hr_disk_entries(void)
+{
+}
+
+void
+shutdown_hr_disk(void)
+{
+}
+
+void
+Save_HR_Disk_Specific(void)
+{
+}
+
+void
+Save_HR_Disk_General(void)
+{
+}
+
+int
+Query_Disk(int fd, const char *devfull)
+{
+    return -1;
+}
+
+int
+Is_It_Writeable(void)
+{
+    return 1;                   /* read-write */
+}
+
+int
+What_Type_Disk(void)
+{
+    return 2;                   /* Unknown */
+}
+
+int
+Is_It_Removeable(void)
+{
+    return 2;                   /* false */
+}

agent/mibgroup/host/hr_disk.h
@@ -22,6 +22,8 @@ config_require(host/data_access/hr_disk_solaris)
 config_require(host/data_access/hr_disk_aix)
 #elif defined(__hpux)
 config_require(host/data_access/hr_disk_hpux)
+#elif defined(_WIN32)
+config_require(host/data_access/hr_disk_windows)
 #endif
 
 /*************************************************************