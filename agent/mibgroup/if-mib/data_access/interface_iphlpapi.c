net-snmp/net-snmp

commit c33140b0110cead76039da6d985878ea9d05b2fd

Win32, IF-MIB: Implement interface table support
On Windows Vista, Windows 2008 and later the ifOperStatus information is
more accurate than on older Windows versions.

agent/mibgroup/if-mib/data_access/interface_iphlpapi.c
@@ -1,0 +1,200 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-features.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/snmp_agent.h>
+#include <net-snmp/agent/snmp_vars.h>
+#include "interface_private.h"
+#include "net-snmp/data_access/interface.h"
+#include <iphlpapi.h>
+
+/* For Cygwin, MinGW32 and MSYS */
+#ifndef NETIO_STATUS
+#define NETIO_STATUS DWORD
+#endif
+
+static void (*pFreeMibTable)(void *Table);
+static NETIO_STATUS (*pGetIfEntry2)(MIB_IF_ROW2 *Row);
+static NETIO_STATUS (*pGetIfTable2)(MIB_IF_TABLE2 **Table);
+static NETIO_STATUS (*pSetIfEntry2)(MIB_IF_ROW2 *Row);
+
+void
+netsnmp_arch_interface_init(void)
+{
+#ifdef HAVE_MIB_IF_TABLE2
+    HANDLE dll_handle = LoadLibrary("iphlpapi.dll");
+
+    pFreeMibTable = (void(*)(void*))(uintptr_t)
+        GetProcAddress(dll_handle, "FreeMibTable");
+    pGetIfEntry2 = (NETIO_STATUS(*)(MIB_IF_ROW2*))(uintptr_t)
+        GetProcAddress(dll_handle, "GetIfEntry2");
+    pGetIfTable2 = (NETIO_STATUS(*)(MIB_IF_TABLE2**))(uintptr_t)
+        GetProcAddress(dll_handle, "GetIfTable2");
+    pSetIfEntry2 = (NETIO_STATUS(*)(MIB_IF_ROW2*))(uintptr_t)
+        GetProcAddress(dll_handle, "SetIfEntry2");
+#endif
+}
+
+/* Load interface data using GetIfTable() (Windows 2000 and later). */
+static int netsnmp_arch_interface_load_old(netsnmp_container *container)
+{
+    DWORD           status;
+    DWORD           dwActualSize = 0;
+    MIB_IFTABLE    *iftable = NULL;
+    MIB_IFROW      *row, *end;
+
+    status = GetIfTable(iftable, &dwActualSize, /*bOrder=*/FALSE);
+    if (status == NO_ERROR)
+        return 0;
+    if (status != ERROR_INSUFFICIENT_BUFFER)
+        return SNMP_ERR_GENERR;
+    iftable = malloc(dwActualSize);
+    if (!iftable)
+        return SNMP_ERR_GENERR;
+    status = GetIfTable(iftable, &dwActualSize, /*bOrder=*/FALSE);
+    if (status != NO_ERROR)
+        return SNMP_ERR_GENERR;
+    end = iftable->table + iftable->dwNumEntries;
+    for (row = iftable->table; row < end; row++) {
+        netsnmp_interface_entry *entry;
+
+        entry = calloc(sizeof(*entry), 1);
+        if (!entry)
+            continue;
+        entry->name = strdup((const char *) row->wszName);
+        entry->oid_index.len = 1;
+        entry->oid_index.oids = &entry->index;
+        entry->index = row->dwIndex;
+        entry->descr = netsnmp_memdup(row->bDescr, row->dwDescrLen);
+        entry->type = row->dwType;
+        entry->mtu = row->dwMtu;
+        entry->speed = row->dwSpeed;
+        entry->paddr_len = row->dwPhysAddrLen;
+        entry->paddr = netsnmp_memdup(row->bPhysAddr, row->dwPhysAddrLen);
+        entry->admin_status = row->dwAdminStatus;
+        entry->oper_status = row->dwOperStatus ==
+            MIB_IF_OPER_STATUS_OPERATIONAL ? 1/*up*/ : 2/*down*/;
+        entry->lastchange = 0;  /* row->dwLastChange is not a Unix time */
+        entry->stats.ibytes.low = row->dwInOctets;
+        entry->stats.iucast.low = row->dwInUcastPkts;
+        entry->stats.inucast = row->dwInNUcastPkts;
+        entry->stats.ierrors = row->dwInErrors;
+        entry->stats.idiscards = row->dwInDiscards;
+        entry->stats.iunknown_protos = row->dwInUnknownProtos;
+        entry->stats.obytes.low = row->dwOutOctets;
+        entry->stats.oucast.low = row->dwOutUcastPkts;
+        entry->stats.oerrors = row->dwOutErrors;
+        entry->stats.oqlen = row->dwOutQLen;
+        entry->stats.onucast = row->dwOutNUcastPkts;
+        CONTAINER_INSERT(container, entry);
+    }
+
+    free(iftable);
+    return 0;
+}
+
+#ifdef HAVE_MIB_IF_TABLE2
+/*
+ * Load interface data using GetIfTable2(). GetIfTable2() is available on
+ * Windows Vista, Windows Server 2008 and later.
+ */
+static int netsnmp_arch_interface_load_new(netsnmp_container *container)
+{
+    DWORD           status;
+    MIB_IF_TABLE2  *iftable = NULL;
+    MIB_IF_ROW2    *row, *end;
+    char           *descr;
+    unsigned        descr_len;
+
+    status = pGetIfTable2(&iftable);
+    if (status != NO_ERROR)
+        return SNMP_ERR_GENERR;
+    end = iftable->Table + iftable->NumEntries;
+    for (row = iftable->Table; row < end; row++) {
+        netsnmp_interface_entry *entry;
+
+        entry = calloc(sizeof(*entry), 1);
+        if (!entry)
+            continue;
+        entry->name = strdup((const char *) row->Alias);
+        entry->oid_index.len = 1;
+        entry->oid_index.oids = &entry->index;
+        entry->index = row->InterfaceIndex;
+        descr_len = WideCharToMultiByte(CP_UTF8, 0, row->Description, -1, NULL,
+                                        0, NULL, NULL);
+        descr = malloc(descr_len);
+        if (row && WideCharToMultiByte(CP_UTF8, 0, row->Description, -1, descr,
+                                       descr_len, NULL, NULL) > 0) {
+            entry->descr = descr;
+        } else {
+            free(row);
+        }
+        entry->type = row->Type;
+        entry->mtu = row->Mtu;
+        entry->speed = row->TransmitLinkSpeed;
+        entry->paddr_len = row->PhysicalAddressLength;
+        entry->paddr = netsnmp_memdup(row->PhysicalAddress,
+                                      row->PhysicalAddressLength);
+        entry->admin_status = row->AdminStatus;
+        entry->oper_status = row->OperStatus;
+        entry->lastchange = 0;  /* row->dwLastChange is not a Unix time */
+        entry->stats.ibytes.low = row->InOctets;
+        entry->stats.iucast.low = row->InUcastPkts;
+        entry->stats.inucast = row->InNUcastPkts;
+        entry->stats.ierrors = row->InErrors;
+        entry->stats.idiscards = row->InDiscards;
+        entry->stats.iunknown_protos = row->InUnknownProtos;
+        entry->stats.obytes.low = row->OutOctets;
+        entry->stats.oucast.low = row->OutUcastPkts;
+        entry->stats.oerrors = row->OutErrors;
+        entry->stats.oqlen = row->OutQLen;
+        entry->stats.onucast = row->OutNUcastPkts;
+        CONTAINER_INSERT(container, entry);
+    }
+
+    pFreeMibTable(iftable);
+    return 0;
+}
+#else
+static int netsnmp_arch_interface_load_new(netsnmp_container *container)
+{
+    return SNMP_ERR_GENERR;
+}
+#endif
+
+int
+netsnmp_arch_interface_container_load(netsnmp_container *container,
+                                      u_int load_flags)
+{
+    return pGetIfTable2 ? netsnmp_arch_interface_load_new(container) :
+        netsnmp_arch_interface_load_old(container);
+}
+
+oid
+netsnmp_arch_interface_index_find(const char *name)
+{
+    /* To do: implement this function. */
+    return 0;
+}
+
+int
+netsnmp_arch_set_admin_status(struct netsnmp_interface_entry_s *entry,
+                              int ifAdminStatus_val)
+{
+    MIB_IFROW       row;
+
+    memset(&row, 0, sizeof(row));
+    row.dwIndex = entry->index;
+    if (GetIfEntry(&row) != NO_ERROR) {
+        snmp_log(LOG_ERR, "GetIfEntry() failed for index %" NETSNMP_PRIo "u",
+                 entry->index);
+        return SNMP_ERR_GENERR;
+    }
+    row.dwAdminStatus = ifAdminStatus_val;
+    if (SetIfEntry(&row) != NO_ERROR) {
+        snmp_log(LOG_ERR,
+                 "SetIfEntry() failed for index %" NETSNMP_PRIo "u and admin status %u\n",
+                 entry->index, ifAdminStatus_val);
+        return SNMP_ERR_GENERR;
+    }
+    return 0;
+}

agent/mibgroup/if-mib/data_access/interface.h
@@ -39,8 +39,7 @@ config_require(if-mib/data_access/interface_sysctl)
 #elif defined(solaris2)
 config_require(if-mib/data_access/interface_solaris2)
 #elif defined(HAVE_IPHLPAPI_H)
-config_warning(This platform does not yet support the IF-MIB)
-config_require(if-mib/data_access/interface_unsup)
+config_require(if-mib/data_access/interface_iphlpapi)
 #else
 config_error(This platform does not yet support the IF-MIB)
 config_require(if-mib/data_access/interface_unsup)

configure
@@ -23692,6 +23692,40 @@ fi
 rm -f core conftest.err conftest.$ac_objext \
     conftest$ac_exeext conftest.$ac_ext
 
+{ $as_echo "$as_me:${as_lineno-$LINENO}: checking for MIB_IF_TABLE2" >&5
+$as_echo_n "checking for MIB_IF_TABLE2... " >&6; }
+cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+  #define _WIN32_WINNT 0xffff
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
+  #include <iphlpapi.h>
+
+int
+main ()
+{
+
+  return sizeof(MIB_IF_TABLE2)
+
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_link "$LINENO"; then :
+
+  { $as_echo "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+$as_echo "yes" >&6; }
+
+$as_echo "#define HAVE_MIB_IF_TABLE2 1" >>confdefs.h
+
+else
+  { $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5
+$as_echo "no" >&6; }
+fi
+rm -f core conftest.err conftest.$ac_objext \
+    conftest$ac_exeext conftest.$ac_ext
+
 
 #   libregex                                            (MinGW)
 #

configure.d/config_os_libs2
@@ -93,6 +93,20 @@ AC_LINK_IFELSE([AC_LANG_PROGRAM([
   esac
 ])
 
+AC_MSG_CHECKING([for MIB_IF_TABLE2])
+AC_LINK_IFELSE([AC_LANG_PROGRAM([
+  #define _WIN32_WINNT 0xffff
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
+  #include <iphlpapi.h>
+],[
+  return sizeof(MIB_IF_TABLE2)
+])],[
+  AC_MSG_RESULT([yes])
+  AC_DEFINE([HAVE_MIB_IF_TABLE2], 1,
+    [Define if MIB_IF_TABLE2 is available])],
+  [AC_MSG_RESULT([no])])
+
 
 #   libregex                                            (MinGW)
 #

include/net-snmp/net-snmp-config.h.in
@@ -512,6 +512,9 @@
 /* Define to 1 if the system has the type `mib2_ipIfStatsEntry_t'. */
 #undef HAVE_MIB2_IPIFSTATSENTRY_T
 
+/* Define if MIB_IF_TABLE2 is available */
+#undef HAVE_MIB_IF_TABLE2
+
 /* Define to 1 if you have the `mkstemp' function. */
 #undef HAVE_MKSTEMP
 

commit e981bcbab5034ccd9cf8445d4ba60d1420b23b3d

IF-MIB: Unbreak the MinGW32 build
Fixes: c33140b0110c ("Win32, IF-MIB: Implement interface table support")

agent/mibgroup/if-mib/data_access/interface_iphlpapi.c
@@ -5,6 +5,7 @@
 #include <net-snmp/agent/snmp_vars.h>
 #include "interface_private.h"
 #include "net-snmp/data_access/interface.h"
+#include <ws2tcpip.h>
 #include <iphlpapi.h>
 
 /* For Cygwin, MinGW32 and MSYS */
@@ -12,10 +13,12 @@
 #define NETIO_STATUS DWORD
 #endif
 
+#ifdef HAVE_MIB_IF_TABLE2
 static void (*pFreeMibTable)(void *Table);
 static NETIO_STATUS (*pGetIfEntry2)(MIB_IF_ROW2 *Row);
 static NETIO_STATUS (*pGetIfTable2)(MIB_IF_TABLE2 **Table);
 static NETIO_STATUS (*pSetIfEntry2)(MIB_IF_ROW2 *Row);
+#endif
 
 void
 netsnmp_arch_interface_init(void)
@@ -165,8 +168,12 @@ int
 netsnmp_arch_interface_container_load(netsnmp_container *container,
                                       u_int load_flags)
 {
+#ifdef HAVE_MIB_IF_TABLE2
     return pGetIfTable2 ? netsnmp_arch_interface_load_new(container) :
         netsnmp_arch_interface_load_old(container);
+#else
+    return netsnmp_arch_interface_load_old(container);
+#endif
 }
 
 oid