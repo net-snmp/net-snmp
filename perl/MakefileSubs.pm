net-snmp/net-snmp

commit 273cd33796133a1642dcfbcae33935fce776911e

perl Makefiles: MinGW / MSYS fixes
This patch improves but does not fix the MinGW / MSYS Perl module build
entirely.

perl/MakefileSubs.pm
@@ -3,6 +3,8 @@ package MakefileSubs;
 use strict;
 use warnings;
 use Config;
+use Cwd 'abs_path';
+use File::Basename;
 use Getopt::Long;
 use Exporter;
 use vars qw(@ISA @EXPORT_OK);
@@ -19,32 +21,6 @@ sub NetSNMPGetOpts {
     $rootpath .= '/' if ($rootpath !~ /\/$/);
 
     if ($Config{'osname'} eq 'MSWin32' && !defined($ENV{'OSTYPE'})) {
-
-      # Grab command line options first.  Only used if environment variables are not set
-      GetOptions("NET-SNMP-IN-SOURCE=s" => \$ret{'insource'},
-        "NET-SNMP-PATH=s"      => \$ret{'prefix'},
-        "NET-SNMP-DEBUG=s"     => \$ret{'debug'});
-
-      if ($ENV{'NET-SNMP-IN-SOURCE'})
-      {
-	$ret{'insource'} = $ENV{'NET-SNMP-IN-SOURCE'};
-        undef ($ret{'prefix'});
-      }
-      elsif ($ENV{'NET-SNMP-PATH'})
-      {
-	$ret{'prefix'} = $ENV{'NET-SNMP-PATH'};
-      }
-
-      if ($ENV{'NET-SNMP-DEBUG'})
-      {
-	$ret{'debug'} = $ENV{'NET-SNMP-DEBUG'};
-      }
-
-      # Update environment variables in case they are needed
-      $ENV{'NET-SNMP-IN-SOURCE'}    = $ret{'insource'};
-      $ENV{'NET-SNMP-PATH'}         = $ret{'prefix'};
-      $ENV{'NET-SNMP-DEBUG'}        = $ret{'debug'};
-
       $basedir = abs_path($0);
       while (1) {
           my $basename = basename($basedir);
@@ -58,32 +34,34 @@ sub NetSNMPGetOpts {
             "supported\nPlease rename the folder and try again.\n\n";
       }
     }
-    else
-    {
-      if ($ENV{'NET-SNMP-CONFIG'} &&
-        $ENV{'NET-SNMP-IN-SOURCE'}) {
+
+    if ($ENV{'NET-SNMP-CONFIG'} && $ENV{'NET-SNMP-IN-SOURCE'}) {
 	# have env vars, pull from there
 	$ret{'nsconfig'} = $ENV{'NET-SNMP-CONFIG'};
 	$ret{'insource'} = $ENV{'NET-SNMP-IN-SOURCE'};
-      } else {
+    } else {
 	# don't have env vars, pull from command line and put there
 	GetOptions("NET-SNMP-CONFIG=s" => \$ret{'nsconfig'},
 	           "NET-SNMP-IN-SOURCE=s" => \$ret{'insource'});
 
-	if (defined($ret{'insource'}) && lc($ret{'insource'}) eq "true" &&
-	    !defined($ret{'nsconfig'})) {
-	    $ret{'nsconfig'}="sh ROOTPATH../net-snmp-config";
+	my $use_default_nsconfig;
+
+	if ($ret{'insource'}) {
+	    if (lc($ret{'insource'}) eq "true" && !defined($ret{'nsconfig'})) {
+		$use_default_nsconfig = 1;
+	    }
+	}
+
+	if ($use_default_nsconfig) {
+	    $ret{'nsconfig'}="sh ${rootpath}../net-snmp-config";
 	} elsif (!defined($ret{'nsconfig'})) {
 	    $ret{'nsconfig'}="net-snmp-config";
 	}
 
 	$ENV{'NET-SNMP-CONFIG'}    = $ret{'nsconfig'};
 	$ENV{'NET-SNMP-IN-SOURCE'} = $ret{'insource'};
-      }
     }
-
-    $ret{'nsconfig'} =~ s/ROOTPATH/$rootpath/;
-
+    
     $ret{'rootpath'} = $rootpath;
 
     \%ret;

perl/ASN/Makefile.PL
@@ -31,8 +31,9 @@ sub AsnInitMakeParams {
                   
     my ($snmp_lib, $snmp_llib, $sep);
 
+    $opts = NetSNMPGetOpts("../");
+
     if ($Config{'osname'} eq 'MSWin32' && !defined($ENV{'OSTYPE'})) {
-      $opts = NetSNMPGetOpts();	
       $Params{'DEFINE'} = "-DMSVC_PERL -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS";
       $sep = '\\';
       my $snmp_lib_file = 'netsnmp.lib';
@@ -77,7 +78,6 @@ sub AsnInitMakeParams {
     }
     else
     {
-      $opts = NetSNMPGetOpts();
       $Params{'LDDLFLAGS'} = "$Config{lddlflags} " . `$opts->{'nsconfig'} --ldflags`;
       $Params{'LIBS'}    = `$opts->{'nsconfig'} --libs`;
       chomp($Params{'LIBS'});

perl/OID/Makefile.PL
@@ -28,8 +28,10 @@ sub OidInitMakeParams {
     }
                   
     my ($snmp_lib, $snmp_llib, $sep);
+
+    $opts = NetSNMPGetOpts("../");
+
     if ($Config{'osname'} eq 'MSWin32' && !defined($ENV{'OSTYPE'})) {
-      $opts = NetSNMPGetOpts();	
       $Params{'DEFINE'} = "-DMSVC_PERL -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS";
       $sep = '\\';
       my $snmp_lib_file = 'netsnmp.lib';
@@ -73,7 +75,6 @@ sub OidInitMakeParams {
       $Params{'INC'} = "-I" . $MakefileSubs::basedir . "\\include\\ -I" . $MakefileSubs::basedir . "\\include\\net-snmp\\ -I" . $MakefileSubs::basedir . "\\win32\\ ";
     }
     else {
-	$opts = NetSNMPGetOpts();
 	$Params{'LDDLFLAGS'} = "$Config{lddlflags} " . `$opts->{'nsconfig'} --ldflags`;
  	$Params{'LIBS'}    = `$opts->{'nsconfig'} --libs`;
 	chomp($Params{'LIBS'});

perl/SNMP/Makefile.PL
@@ -33,8 +33,10 @@ sub SnmpInitMakeParams {
     }
                                     
     my ($snmp_lib, $snmp_llib, $sep);
+
+    $opts = NetSNMPGetOpts("../");
+
     if ($Config{'osname'} eq 'MSWin32' && !defined($ENV{'OSTYPE'})) {
-      $opts = NetSNMPGetOpts();	
       $Params{'DEFINE'} = "-DMSVC_PERL -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS";
       $sep = '\\';
       my $snmp_lib_file = 'netsnmp.lib';
@@ -78,7 +80,6 @@ sub SnmpInitMakeParams {
       $Params{'INC'} = "-I" . $MakefileSubs::basedir . "\\include\\ -I" . $MakefileSubs::basedir . "\\include\\net-snmp\\ -I" . $MakefileSubs::basedir . "\\win32\\ ";
     }
     else {
-	$opts = NetSNMPGetOpts();
 	$Params{'LDDLFLAGS'} = "$Config{lddlflags} " . `$opts->{'nsconfig'} --ldflags`;
 	if (!$ENV{'NETSNMP_LIBS'}) {
 	    $Params{'LIBS'}    = `$opts->{'nsconfig'} --libs`;

perl/TrapReceiver/Makefile.PL
@@ -57,8 +57,10 @@ sub TrapReceiverInitMakeParams {
     }
 
     my ($snmp_lib, $snmp_llib, $sep);
+
+    $opts = NetSNMPGetOpts("../");
+
     if ($Config{'osname'} eq 'MSWin32' && !defined($ENV{'OSTYPE'})) {
-      $opts = NetSNMPGetOpts();	
       $Params{'DEFINE'} = "-DMSVC_PERL -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS";
       $sep = '\\';
       my $snmp_lib_file = 'netsnmp.lib';
@@ -106,7 +108,6 @@ sub TrapReceiverInitMakeParams {
       $Params{'INC'} = "-I" . $MakefileSubs::basedir . "\\include\\ -I" . $MakefileSubs::basedir . "\\include\\net-snmp\\ -I" . $MakefileSubs::basedir . "\\win32\\ ";
     }
     else {
-	$opts = NetSNMPGetOpts();
 	$Params{'LDDLFLAGS'} = "$Config{lddlflags} " . `$opts->{'nsconfig'} --ldflags`;
 	$Params{'LIBS'} = '-lnetsnmptrapd ' . `$opts->{'nsconfig'} --base-agent-libs`;
 	chomp($Params{'LIBS'});

perl/agent/Makefile.PL
@@ -30,8 +30,10 @@ sub AgentInitMakeParams {
     }
                                    
     my ($snmp_lib, $snmp_llib, $sep);
+
+    $opts = NetSNMPGetOpts("../");
+
     if ($Config{'osname'} eq 'MSWin32' && !defined($ENV{'OSTYPE'})) {
-      $opts = NetSNMPGetOpts();	
       $Params{'DEFINE'} = "-DMSVC_PERL -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS";
       $sep = '\\';
       my $snmp_lib_file = 'netsnmp.lib';
@@ -78,7 +80,6 @@ sub AgentInitMakeParams {
       $Params{'INC'} = "-I" . $MakefileSubs::basedir . "\\include\\ -I" . $MakefileSubs::basedir . "\\include\\net-snmp\\ -I" . $MakefileSubs::basedir . "\\win32\\ ";
     }
     else {
-	$opts = NetSNMPGetOpts();
 	$Params{'LDDLFLAGS'} = "$Config{lddlflags} " . `$opts->{'nsconfig'} --ldflags`;
 	$Params{'LIBS'}   = `$opts->{'nsconfig'} --base-agent-libs`;
 	chomp($Params{'LIBS'});

perl/agent/Support/Makefile.PL
@@ -29,8 +29,9 @@ sub SupportInitMakeParams {
                   
     my ($snmp_lib, $snmp_llib, $sep);
 
+    $opts = NetSNMPGetOpts("../../");
+
     if ($Config{'osname'} eq 'MSWin32' && !defined($ENV{'OSTYPE'})) {
-      $opts = NetSNMPGetOpts();	
       $Params{'DEFINE'} = "-DMSVC_PERL -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS";
       $sep = '\\';
       my $snmp_lib_file = 'netsnmp.lib';
@@ -75,7 +76,6 @@ sub SupportInitMakeParams {
     }
     else
     {
-      $opts = NetSNMPGetOpts("../../");
       $Params{'LIBS'}    = `$opts->{'nsconfig'} --libs`;
       chomp($Params{'LIBS'});
       $Params{'CCFLAGS'} = `$opts->{'nsconfig'} --cflags`;

perl/agent/default_store/Makefile.PL
@@ -24,8 +24,10 @@ sub AgentDefaultStoreInitMakeParams {
     }
                                    
     my ($snmp_lib, $snmp_llib, $sep);
+
+    $opts = NetSNMPGetOpts("../../");
+
     if ($Config{'osname'} eq 'MSWin32' && !defined($ENV{'OSTYPE'})) {
-      $opts = NetSNMPGetOpts();	
       $Params{'DEFINE'} = "-DMSVC_PERL -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS";
       $sep = '\\';
       my $snmp_lib_file = 'netsnmp.lib';
@@ -69,7 +71,6 @@ sub AgentDefaultStoreInitMakeParams {
       $Params{'INC'} = "-I" . $MakefileSubs::basedir . "\\include\\ -I" . $MakefileSubs::basedir . "\\include\\net-snmp\\ -I" . $MakefileSubs::basedir . "\\win32\\ ";
     }
     else {
-	$opts = NetSNMPGetOpts("../../");
 	$Params{'LIBS'}    = `$opts->{'nsconfig'} --libs`;
 	chomp($Params{'LIBS'});
 	$Params{'CCFLAGS'} = `$opts->{'nsconfig'} --cflags`;

commit 4d614474f9157809d8a5ab8c265246e5b2e56374

perl/MakefileSubs.pm: Restore support for the -NET-SNMP-PATH option
Fixes: 273cd3379613 ("perl Makefiles: MinGW / MSYS fixes")

perl/MakefileSubs.pm
@@ -46,7 +46,8 @@ sub NetSNMPGetOpts {
 	           "NET-SNMP-IN-SOURCE=s" => \$ret{'insource'},
 		   "NET-SNMP-DEFINE=s"    => \$ret{'define'},
 		   "NET-SNMP-INC=s"       => \$ret{'inc'},
-		   "NET-SNMP-CFLAGS=s"    => \$ret{'cflags'});
+		   "NET-SNMP-CFLAGS=s"    => \$ret{'cflags'},
+		   "NET-SNMP-PATH=s"      => \$ret{'prefix'});
 
 	my $use_default_nsconfig;
 
@@ -67,6 +68,7 @@ sub NetSNMPGetOpts {
 	$ENV{'NET-SNMP-DEFINE'}    = $ret{'define'};
 	$ENV{'NET-SNMP-INC'}       = $ret{'inc'};
 	$ENV{'NET-SNMP-CFLAGS'}    = $ret{'cflags'};
+	$ENV{'NET-SNMP-PATH'}      = $ret{'prefix'};
     }
     
     $ret{'rootpath'} = $rootpath;