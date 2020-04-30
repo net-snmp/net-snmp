net-snmp/net-snmp

commit f1647e3db24241f0c42449506aee4b406a37b1b5

snmpd: Do not open /dev/kmem on Linux
The Net-SNMP agent only uses /dev/kmem to access kernel data structures on
BSD systems. Hence do not open /dev/kmem on Linux. A side effect of this
patch is that it enables cross-compilation from Linux to MinGW. /dev/kmem
exists on Linux but the code in agent/kernel.c that is surrounded with
 #ifdef HAVE_KMEM / #endif does not build with the MinGW compiler.

agent/kernel.c
@@ -44,13 +44,8 @@
 #include "kernel.h"
 #include <net-snmp/agent/ds_agent.h>
 
-#ifndef NULL
-#define NULL 0
-#endif
-
-
 #if HAVE_KVM_H
-kvm_t *kd = NULL;
+kvm_t *kd;
 
 /**
  * Initialize the support for accessing kernel virtual memory.
@@ -135,9 +130,7 @@ free_kmem(void)
     }
 }
 
-#else                           /* HAVE_KVM_H */
-
-#ifdef HAVE_KMEM
+#elif defined(HAVE_NLIST_H)
 
 static off_t    klseek(off_t);
 static int      klread(char *, int);
@@ -259,6 +252,15 @@ free_kmem(void)
     }
 }
 
-#endif                          /* HAVE_KMEM */
+#else
+int
+init_kmem(const char *file)
+{
+    return 0;
+}
 
-#endif                          /* HAVE_KVM_H */
+void
+free_kmem(void)
+{
+}
+#endif

agent/mibgroup/hardware/memory/memory_freebsd.c
@@ -3,6 +3,7 @@
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/agent/auto_nlist.h>
 #include <net-snmp/agent/hardware/memory.h>
+#include "../../../kernel.h"
 
 #include <unistd.h>
 #include <sys/param.h>
@@ -237,8 +238,6 @@ swapmode(long pagesize)
  */
 
 #include <sys/conf.h>
-
-extern kvm_t *kd;
 
 static int
 swapmode(long pagesize)

agent/mibgroup/host/data_access/swrun_kinfo.c
@@ -48,9 +48,7 @@
 #include <net-snmp/library/snmp_debug.h>
 #include <net-snmp/data_access/swrun.h>
 #include "swrun_private.h"
-
-extern kvm_t *kd;
-
+#include "../../../kernel.h"
 
 #if defined(freebsd5) && __FreeBSD_version >= 500014
     /*

agent/snmp_vars.c
@@ -292,9 +292,7 @@ init_agent(const char *app)
     netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, 
 			   NETSNMP_DS_LIB_ALARM_DONT_USE_SIG, 1);
 
-#ifdef HAVE_KMEM
     r = init_kmem("/dev/kmem") ? 0 : -EACCES;
-#endif
 
     setup_tree();
 
@@ -384,9 +382,7 @@ shutdown_agent(void) {
     clear_callback();
     shutdown_secmod();
     netsnmp_addrcache_destroy();
-#ifdef HAVE_KMEM
     free_kmem();
-#endif
 
     done_init_agent = 0;
 }

configure
@@ -31246,33 +31246,6 @@ _ACEOF
 
 
 
-#       /dev/kmem existence
-#	    used in agent only
-#
-{ $as_echo "$as_me:${as_lineno-$LINENO}: checking for /dev/kmem" >&5
-$as_echo_n "checking for /dev/kmem... " >&6; }
-if ${ac_cv_HAVE_KMEM+:} false; then :
-  $as_echo_n "(cached) " >&6
-else
-  if test $CDEV_TEST_FLAG /dev/kmem; then
-        ac_cv_HAVE_KMEM="/dev/kmem"
-    fi
-
-fi
-{ $as_echo "$as_me:${as_lineno-$LINENO}: result: $ac_cv_HAVE_KMEM" >&5
-$as_echo "$ac_cv_HAVE_KMEM" >&6; }
-#
-#  Note that it's OK if this device is not found
-#
-if test "x$ac_cv_HAVE_KMEM" = "x/dev/kmem"; then
-
-cat >>confdefs.h <<_ACEOF
-#define HAVE_KMEM "$ac_cv_HAVE_KMEM"
-_ACEOF
-
-fi
-
-
 #       Swap device Location
 #           (/dev/dmem or /dev/drum)
 #	    used in agent only

configure.d/config_os_misc4
@@ -49,23 +49,6 @@ AC_CACHE_CHECK([for location of system kernel],
 AC_DEFINE_UNQUOTED(KERNEL_LOC,"$ac_cv_KERNEL_LOC", [location of UNIX kernel])
 
 
-#       /dev/kmem existence
-#	    used in agent only
-#
-AC_CACHE_CHECK([for /dev/kmem],
-    ac_cv_HAVE_KMEM,
-   [if test $CDEV_TEST_FLAG /dev/kmem; then
-        ac_cv_HAVE_KMEM="/dev/kmem"
-    fi
-])
-#
-#  Note that it's OK if this device is not found
-#
-if test "x$ac_cv_HAVE_KMEM" = "x/dev/kmem"; then
-    AC_DEFINE_UNQUOTED(HAVE_KMEM,"$ac_cv_HAVE_KMEM", [Define if you have /dev/kmem])
-fi
-
-
 #       Swap device Location
 #           (/dev/dmem or /dev/drum)
 #	    used in agent only

include/net-snmp/net-snmp-config.h.in
@@ -361,9 +361,6 @@
 /* Define to 1 if you have the `kinfo_get_cpus' function. */
 #undef HAVE_KINFO_GET_CPUS
 
-/* Define if you have /dev/kmem */
-#undef HAVE_KMEM
-
 /* Define to 1 if you have the `knlist' function. */
 #undef HAVE_KNLIST
 

win32/libagent/Makefile.in
@@ -29,6 +29,7 @@ LIB32_OBJS= \
 	"$(INTDIR)\cache_handler.obj" \
 	"$(INTDIR)\debug_handler.obj" \
 	"$(INTDIR)\instance.obj" \
+	"$(INTDIR)\kernel.obj" \
 	"$(INTDIR)\mode_end_call.obj" \
 	"$(INTDIR)\multiplexer.obj" \
 	"$(INTDIR)\null.obj" \

commit 59e8b5b28db8bed05d68c4d508a89932437e9cb6

snmpd: Make init_kmem() report "success" if /dev/kmem is not opened
Fixes: f1647e3db242 ("snmpd: Do not open /dev/kmem on Linux")

agent/kernel.c
@@ -256,7 +256,7 @@ free_kmem(void)
 int
 init_kmem(const char *file)
 {
-    return 0;
+    return 1;  /* success */
 }
 
 void