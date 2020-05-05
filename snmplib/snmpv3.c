net-snmp/net-snmp

commit 670d4234b5196875036053ffb6c76c08a2fd25a5

First pass at an SNMPv3 "combined-option" command-line flag (-3x). (In *addition* to the current command-line flags, I hasten to add!)
Supports  "... -3u{secName} ..." and "... -3u={secName} ..." syntax,
but not the separate "... -3u {secName} ..." style.

I haven't touched the documentation or usage message to mention this.


git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@6408 06827809-a52a-0410-b366-d66718629ded

snmplib/snmpv3.c
@@ -25,6 +25,7 @@
 #else
 #include <strings.h>
 #endif
+#include <ctype.h>
 #if HAVE_NETINET_IN_H
 #include <netinet/in.h>
 #endif
@@ -173,6 +174,134 @@ snmpv3_secLevel_conf(const char *word, char *cptr)
   }
   DEBUGMSGTL(("snmpv3","default secLevel set to: %s = %d\n", cptr,
               ds_get_int(DS_LIBRARY_ID, DS_LIB_SECLEVEL)));
+}
+
+
+int
+snmpv3_options(char *optarg, struct snmp_session *session, char **Apsz, char **Xpsz)
+{
+   char *cp = optarg;
+   optarg++;
+   if (*optarg == '=') {
+       optarg++;
+   }
+
+   switch (*cp) {
+
+      case 'Z':
+        session->engineBoots = strtoul(optarg, NULL, 10);
+        if (session->engineBoots == 0 || !isdigit(optarg[0])) {
+          fprintf(stderr,"Need engine boots value after -3Z flag.\n");
+          return(-1);
+        }
+        cp = strchr(optarg,',');
+        if (cp && *(++cp) && isdigit(*cp))
+          session->engineTime = strtoul(cp, NULL, 10);
+        else {
+          fprintf(stderr,"Need engine time value after -3Z flag.\n");
+          return(-1);
+        }
+        break;
+
+      case 'e': {
+	size_t ebuf_len = 32, eout_len = 0;
+	u_char *ebuf = (u_char *)malloc(ebuf_len);
+	
+	if (ebuf == NULL) {
+	  fprintf(stderr, "malloc failure processing -3e flag.\n");
+	  return(-1);
+	}
+	if (!snmp_hex_to_binary(&ebuf, &ebuf_len, &eout_len, 1, optarg)) {
+          fprintf(stderr, "Bad engine ID value after -3e flag.\n");
+	  free(ebuf);
+          return(-1);
+	}
+	session->securityEngineID = ebuf;
+	session->securityEngineIDLen = eout_len;
+        break;
+      }
+
+      case 'E': {
+	size_t ebuf_len = 32, eout_len = 0;
+	u_char *ebuf = (u_char *)malloc(ebuf_len);
+	
+	if (ebuf == NULL) {
+	  fprintf(stderr, "malloc failure processing -3E flag.\n");
+	  return(-1);
+	}
+	if (!snmp_hex_to_binary(&ebuf, &ebuf_len, &eout_len, 1, optarg)) {
+          fprintf(stderr, "Bad engine ID value after -3E flag.\n");
+	  free(ebuf);
+          return(-1);
+	}
+	session->contextEngineID = ebuf;
+	session->contextEngineIDLen = eout_len;
+        break;
+      }
+
+      case 'n':
+	session->contextName = optarg;
+	session->contextNameLen = strlen(optarg);
+        break;
+
+      case 'u':
+	session->securityName = optarg;
+	session->securityNameLen = strlen(optarg);
+        break;
+
+      case 'l':
+        if (!strcasecmp(optarg,"noAuthNoPriv") || !strcmp(optarg,"1") ||
+            !strcasecmp(optarg,"nanp")) {
+          session->securityLevel = SNMP_SEC_LEVEL_NOAUTH;
+        } else if (!strcasecmp(optarg,"authNoPriv") || !strcmp(optarg,"2") ||
+            !strcasecmp(optarg,"anp")) {
+          session->securityLevel = SNMP_SEC_LEVEL_AUTHNOPRIV;
+        } else if (!strcasecmp(optarg,"authPriv") || !strcmp(optarg,"3") ||
+            !strcasecmp(optarg,"ap")) {
+          session->securityLevel = SNMP_SEC_LEVEL_AUTHPRIV;
+        } else {
+          fprintf(stderr,"Invalid security level specified after -3l flag: %s\n", optarg);
+          return(-1);
+        }
+
+        break;
+
+      case 'a':
+        if (!strcasecmp(optarg,"MD5")) {
+          session->securityAuthProto = usmHMACMD5AuthProtocol;
+          session->securityAuthProtoLen = USM_AUTH_PROTO_MD5_LEN;
+        } else if (!strcasecmp(optarg,"SHA")) {
+          session->securityAuthProto = usmHMACSHA1AuthProtocol;
+          session->securityAuthProtoLen = USM_AUTH_PROTO_SHA_LEN;
+        } else {
+          fprintf(stderr,"Invalid authentication protocol specified after -3a flag: %s\n", optarg);
+          return(-1);
+        }
+        break;
+
+      case 'x':
+        if (!strcasecmp(optarg,"DES")) {
+          session->securityPrivProto = usmDESPrivProtocol;
+          session->securityPrivProtoLen = USM_PRIV_PROTO_DES_LEN;
+        } else {
+          fprintf(stderr,"Invalid privacy protocol specified after -3x flag: %s\n", optarg);
+          return(-1);
+        }
+        break;
+
+      case 'A':
+	*Apsz = optarg;
+	break;
+
+      case 'X':
+        *Xpsz = optarg;
+        break;
+
+      default:
+        fprintf(stderr,"Unknown SNMPv3 option passed to -3: %c.\n", *cp);
+        return -1;
+   }
+   return 0;
 }
 
 /*******************************************************************-o-******

snmplib/snmp_parse_args.c
@@ -66,15 +66,9 @@
 #include <net-snmp/version.h>
 #include <net-snmp/library/parse.h>
 #include <net-snmp/library/snmpv3.h>
+#include <net-snmp/library/transform_oids.h>
 
 int random_access = 0;
-
-#define USM_AUTH_PROTO_MD5_LEN 10
-static oid usmHMACMD5AuthProtocol[]  = { 1,3,6,1,6,3,10,1,1,2 };
-#define USM_AUTH_PROTO_SHA_LEN 10
-static oid usmHMACSHA1AuthProtocol[] = { 1,3,6,1,6,3,10,1,1,3 };
-#define USM_PRIV_PROTO_DES_LEN 10
-static oid usmDESPrivProtocol[]      = { 1,3,6,1,6,3,10,1,2,2 };
 
 void
 snmp_parse_args_usage(FILE *outf)
@@ -148,6 +142,7 @@ handle_long_opt(const char *myoptarg)
     free(cp);
 }
 
+extern int snmpv3_options(char *optarg, struct snmp_session *session, char **Apsz, char **Xpsz);
 int
 snmp_parse_args(int argc, 
 		char *const *argv, 
@@ -163,7 +158,7 @@ snmp_parse_args(int argc,
 
   /* initialize session to default values */
   snmp_sess_init( session );
-  strcpy(Opts, "Y:VhHm:M:O:I:P:D:dv:r:t:c:Z:e:E:n:u:l:x:X:a:A:p:T:-:");
+  strcpy(Opts, "Y:VhHm:M:O:I:P:D:dv:r:t:c:Z:e:E:n:u:l:x:X:a:A:p:T:-:3:");
   if (localOpts) strcat(Opts, localOpts);
 
   /* get the options */
@@ -292,6 +287,14 @@ snmp_parse_args(int argc,
 	Cpsz = optarg;
 	break;
 
+      case '3':
+        if (snmpv3_options(optarg, session, &Apsz, &Xpsz) < 0 ) {
+          return(-1);
+        }
+        break;
+
+#define SNMPV3_CMD_OPTIONS
+#ifdef  SNMPV3_CMD_OPTIONS
       case 'Z':
         session->engineBoots = strtoul(optarg, NULL, 10);
         if (session->engineBoots == 0 || !isdigit(optarg[0])) {
@@ -403,6 +406,7 @@ snmp_parse_args(int argc,
       case 'X':
         Xpsz = optarg;
         break;
+#endif /* SNMPV3_CMD_OPTIONS */
 
       case '?':
         return(-1);

commit a510b92eb8b015e478d4d052bca3d277744f42d0

libsnmp: Fix handling of the -a and -x app command line options
Since snmp_free_session() frees securityAuthProto and securityPrivProto,
allocate these two session members dynamically.

Fixes: 670d4234b519 ("First pass at an SNMPv3 "combined-option" command-line flag (-3x)").

snmplib/snmpv3.c
@@ -266,8 +266,12 @@ snmpv3_parse_arg(int arg, char *optarg, netsnmp_session *session, char **Apsz,
     case 'a': {
         int auth_type = usm_lookup_auth_type(optarg);
         if (auth_type > 0) {
-            session->securityAuthProto =
-                sc_get_auth_oid(auth_type, &session->securityAuthProtoLen);
+            const oid *auth_proto;
+
+            auth_proto = sc_get_auth_oid(auth_type,
+                                         &session->securityAuthProtoLen);
+            session->securityAuthProto = snmp_duplicate_objid(auth_proto,
+                                             session->securityAuthProtoLen);
          } else {
             fprintf(stderr,
                     "Invalid authentication protocol specified after -3a flag: %s\n",
@@ -277,7 +281,9 @@ snmpv3_parse_arg(int arg, char *optarg, netsnmp_session *session, char **Apsz,
     }
         break;
 
-    case 'x':
+    case 'x': {
+        const oid *priv_proto;
+
         priv_type = usm_lookup_priv_type(optarg);
         if (priv_type < 0) {
             fprintf(stderr,
@@ -285,10 +291,11 @@ snmpv3_parse_arg(int arg, char *optarg, netsnmp_session *session, char **Apsz,
                     optarg);
             return (-1);
         }
-        session->securityPrivProto =
-            sc_get_priv_oid(priv_type, &session->securityPrivProtoLen);
+        priv_proto = sc_get_priv_oid(priv_type, &session->securityPrivProtoLen);
+        session->securityPrivProto = snmp_duplicate_objid(priv_proto,
+                                         session->securityPrivProtoLen);
         break;
-
+    }
     case 'A':
         *Apsz = strdup(optarg);
         if (NULL == *Apsz) {