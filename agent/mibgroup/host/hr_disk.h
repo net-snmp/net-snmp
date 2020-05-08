net-snmp/net-snmp

commit 06037d0c8f2af2ed0d2e9d6010a6ee6f3926f699

FreeBSD: Use the freebsd3 macro instead of __FreeBSD__
Use the 'freebsd3' macro to guard code that is FreeBSD-specific. Note:
that macro is defined if the FreeBSD version is >= 3.

agent/mibgroup/host/hr_disk.h
@@ -11,7 +11,7 @@ config_require(host/hr_device)
 config_require(host/data_access/hr_disk_linux)
 #elif defined(__APPLE__) && defined(__MACH__)
 config_require(host/data_access/hr_disk_darwin)
-#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || \
+#elif defined(freebsd3) || defined(__NetBSD__) || defined(__OpenBSD__) || \
     defined(__bsdi__)
 config_require(host/data_access/hr_disk_bsd)
 #elif defined(__DragonFly__)

commit 489a2ef0e2905f5a830edc5fdbc9a1d45acdbeae

HOST-MIB: Fix the DragonFly build
Since the 'freebsd3' macro is defined on DragonFly systems, test the
__DragonFly__ macro before testing the freebsd3 macro.

Fixes: 06037d0c8f2a ("FreeBSD: Use the freebsd3 macro instead of __FreeBSD__")

agent/mibgroup/host/hr_disk.h
@@ -11,11 +11,11 @@ config_require(host/hr_device)
 config_require(host/data_access/hr_disk_linux)
 #elif defined(__APPLE__) && defined(__MACH__)
 config_require(host/data_access/hr_disk_darwin)
+#elif defined(__DragonFly__)
+config_require(host/data_access/hr_disk_dfly)
 #elif defined(freebsd3) || defined(__NetBSD__) || defined(__OpenBSD__) || \
     defined(__bsdi__)
 config_require(host/data_access/hr_disk_bsd)
-#elif defined(__DragonFly__)
-config_require(host/data_access/hr_disk_dfly)
 #elif defined(__sun)
 config_require(host/data_access/hr_disk_solaris)
 #elif defined(_AIX)