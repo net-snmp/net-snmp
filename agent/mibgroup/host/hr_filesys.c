net-snmp/net-snmp

commit 2dfdd7cb73ec8bc670888939568f2bfa49f73943

Better file system types
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9384 06827809-a52a-0410-b366-d66718629ded

agent/mibgroup/host/hr_filesys.c
@@ -115,6 +115,16 @@ struct mntent  *HRFS_entry;
 #define	HRFS_type	mnt_type
 #define	HRFS_statfs	statfs
 
+#ifdef linux
+#define MNTTYPE_CD9660	"iso9660"
+#define MNTTYPE_EXT2FS	"ext2"
+#define MNTTYPE_EXT3FS	"ext3"
+#define MNTTYPE_SMBFS	"smbfs"
+#define MNTTYPE_MSDOS	"msdos"
+#define MNTTYPE_FAT32	"vfat"
+#define MNTTYPE_NTFS	"ntfs"
+#endif	/* linux */
+
 #endif
 
 #define	FULL_DUMP	0
@@ -363,7 +373,7 @@ var_hrfilesys(struct variable *vp,
 #endif
 #ifdef MNTTYPE_UFS
         else if (!strcmp(mnt_type, MNTTYPE_UFS))
-#if defined(BerkelyFS) && !defined(MNTTYPE_HFS)
+#if (defined(BerkelyFS) && !defined(MNTTYPE_HFS)) || defined(solaris2)
             fsys_type_id[fsys_type_len - 1] = 3;
 #else                           /* SysV */
             fsys_type_id[fsys_type_len - 1] = 4;        /* or 3? XXX */
@@ -381,6 +391,10 @@ var_hrfilesys(struct variable *vp,
         else if (!strcmp(mnt_type, MNTTYPE_MSDOS))
             fsys_type_id[fsys_type_len - 1] = 5;
 #endif
+#ifdef MNTTYPE_FAT32
+        else if (!strcmp(mnt_type, MNTTYPE_FAT32))
+            fsys_type_id[fsys_type_len - 1] = 22;
+#endif
 #ifdef MNTTYPE_CDFS
         else if (!strcmp(mnt_type, MNTTYPE_CDFS))
 #ifdef RockRidge
@@ -389,9 +403,21 @@ var_hrfilesys(struct variable *vp,
             fsys_type_id[fsys_type_len - 1] = 12;
 #endif
 #endif
+#ifdef MNTTYPE_HSFS
+        else if (!strcmp(mnt_type, MNTTYPE_HSFS))
+            fsys_type_id[fsys_type_len - 1] = 13;
+#endif
 #ifdef MNTTYPE_ISO9660
         else if (!strcmp(mnt_type, MNTTYPE_ISO9660))
             fsys_type_id[fsys_type_len - 1] = 12;
+#endif
+#ifdef MNTTYPE_CD9660
+        else if (!strcmp(mnt_type, MNTTYPE_CD9660))
+            fsys_type_id[fsys_type_len - 1] = 12;
+#endif
+#ifdef MNTTYPE_SMBFS
+        else if (!strcmp(mnt_type, MNTTYPE_SMBFS))
+            fsys_type_id[fsys_type_len - 1] = 14;
 #endif
 #ifdef MNTTYPE_NFS
         else if (!strcmp(mnt_type, MNTTYPE_NFS))
@@ -407,6 +433,10 @@ var_hrfilesys(struct variable *vp,
 #endif
 #ifdef MNTTYPE_EXT2FS
         else if (!strcmp(mnt_type, MNTTYPE_EXT2FS))
+            fsys_type_id[fsys_type_len - 1] = 23;
+#endif
+#ifdef MNTTYPE_EXT3FS
+        else if (!strcmp(mnt_type, MNTTYPE_EXT3FS))
             fsys_type_id[fsys_type_len - 1] = 23;
 #endif
 #ifdef MNTTYPE_NTFS
@@ -495,15 +525,24 @@ const char     *HRFS_ignores[] = {
 #ifdef MNTTYPE_PROC
     MNTTYPE_PROC,
 #endif
+#ifdef MNTTYPE_PROCFS
+    MNTTYPE_PROCFS,
+#endif
 #ifdef MNTTYPE_AUTOFS
     MNTTYPE_AUTOFS,
-#endif
+#else
     "autofs",
+#endif
 #ifdef linux
     "devpts",
+    "devfs",
+    "usbdevfs",
+    "tmpfs",
     "shm",
 #endif
 #ifdef solaris2
+    "mntfs",
+    "proc",
     "fd",
 #endif
     0
@@ -576,6 +615,9 @@ Check_HR_FileSys_NFS (void)
 #endif
 #if defined(MNTTYPE_NFS3)
 	    !strcmp( HRFS_entry->HRFS_type, MNTTYPE_NFS3) ||
+#endif
+#if defined(MNTTYPE_SMBFS)
+	    !strcmp( HRFS_entry->HRFS_type, MNTTYPE_SMBFS) ||
 #endif
 #if defined(MNTTYPE_LOFS)
 	    !strcmp( HRFS_entry->HRFS_type, MNTTYPE_LOFS) ||

commit 327079a4ae20e2d4766fe549a6e4e57fbe947bcd

HOST-MIB, hr_filesys: Also monitor tmpfs
See also https://sourceforge.net/p/net-snmp/feature-requests/165/,
https://bugs.debian.org/685877 and
https://gitlab.alpinelinux.org/alpine/aports/issues/932.

Fixes: 2dfdd7cb73ec ("Better file system types") # v5.7
Reported-by: Craig Small <csmall@dropbear.xyz>

agent/mibgroup/host/hr_filesys.c
@@ -704,7 +704,6 @@ static const char *HRFS_ignores[] = {
     "shm",
     "sockfs",
     "sysfs",
-    "tmpfs",
     "usbdevfs",
     "usbfs",
 #endif