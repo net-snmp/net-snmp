net-snmp/net-snmp

commit 4253c5431631ea9009e5f8ebb3ce57ce6a3b473a

- make mibs, mibdirs and mibfiles handling consistent:   - '-XXX' adds XXX before existing   - '+XXX' adds XXX after existing     NOTE: this restores original behavior of '+' to that of 5.0.1. After           5.0.2, if I read the code right, '+' behaviour on the cmd line and           in MIBDIRS was reversed and not consistent with handle_mibs_conf.
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@12322 06827809-a52a-0410-b366-d66718629ded

snmplib/mib.c
@@ -2075,12 +2075,20 @@ handle_mibdirs_conf(const char *token, char *line)
                 sprintf(ctmp, "%s%c%s", line, ENV_SEPARATOR_CHAR, confmibdir);
         } else {
             ctmp = strdup(line);
+            if (!ctmp) {
+                DEBUGMSGTL(("read_config:initmib", "mibs conf malloc failed"));
+                return;
+            }
         }
         SNMP_FREE(confmibdir);
-        confmibdir = ctmp;
     } else {
-        confmibdir = strdup(line);
+        ctmp = strdup(line);
+        if (!ctmp) {
+            DEBUGMSGTL(("read_config:initmib", "mibs conf malloc failed"));
+            return;
+        }
     }
+    confmibdir = ctmp;
     DEBUGMSGTL(("read_config:initmib", "using mibdirs: %s\n", confmibdir));
 }
 
@@ -2090,19 +2098,32 @@ handle_mibs_conf(const char *token, char *line)
     char           *ctmp;
 
     if (confmibs) {
-        ctmp = (char *) malloc(strlen(confmibs) + strlen(line) + 2);
+        if ((*line == '+') || (*line == '-')) {
+            ctmp = (char *) malloc(strlen(confmibs) + strlen(line) + 2);
+            if (!ctmp) {
+                DEBUGMSGTL(("read_config:initmib", "mibs conf malloc failed"));
+                return;
+            }
+            if(*line++ == '+')
+                sprintf(ctmp, "%s%c%s", confmibs, ENV_SEPARATOR_CHAR, line);
+            else
+                sprintf(ctmp, "%s%c%s", line, ENV_SEPARATOR_CHAR, confmibdir);
+        } else {
+            ctmp = strdup(line);
+            if (!ctmp) {
+                DEBUGMSGTL(("read_config:initmib", "mibs conf malloc failed"));
+                return;
+            }
+        }
+        SNMP_FREE(confmibs);
+    } else {
+        ctmp = strdup(line);
         if (!ctmp) {
             DEBUGMSGTL(("read_config:initmib", "mibs conf malloc failed"));
             return;
         }
-        if (*line == '+')
-            line++;
-        sprintf(ctmp, "%s%c%s", confmibs, ENV_SEPARATOR_CHAR, line);
-        SNMP_FREE(confmibs);
-        confmibs = ctmp;
-    } else {
-        confmibs = strdup(line);
     }
+    confmibs = ctmp;
     DEBUGMSGTL(("read_config:initmib", "using mibs: %s\n", confmibs));
 }
 
@@ -2591,15 +2612,20 @@ init_mib(void)
     } else {
         env_var = strdup(env_var);
     }
-    if (env_var && *env_var == '+') {
+    if (env_var && ((*env_var == '+') || (*env_var == '-'))) {
         entry =
             (char *) malloc(strlen(DEFAULT_MIBS) + strlen(env_var) + 2);
         if (!entry) {
             DEBUGMSGTL(("init_mib", "env mibs malloc failed"));
             return;
-        } else
-            sprintf(entry, "%s%c%s", DEFAULT_MIBS, ENV_SEPARATOR_CHAR,
-                env_var + 1);
+        } else {
+            if (*env_var++ == '+')
+                sprintf(entry, "%s%c%s", DEFAULT_MIBS, ENV_SEPARATOR_CHAR,
+                        env_var);
+            else
+                sprintf(entry, "%s%c%s", env_var, ENV_SEPARATOR_CHAR,
+                        DEFAULT_MIBS );
+        }
         SNMP_FREE(env_var);
         env_var = entry;
     }
@@ -2623,16 +2649,21 @@ init_mib(void)
 
     env_var = netsnmp_getenv("MIBFILES");
     if (env_var != NULL) {
-        if (*env_var == '+') {
+        if ((*env_var == '+') || (*env_var == '-')) {
 #ifdef DEFAULT_MIBFILES
             entry =
                 (char *) malloc(strlen(DEFAULT_MIBFILES) +
                                 strlen(env_var) + 2);
             if (!entry) {
                 DEBUGMSGTL(("init_mib", "env mibfiles malloc failed"));
-            } else
-                sprintf(entry, "%s%c%s", DEFAULT_MIBFILES, ENV_SEPARATOR_CHAR,
-                    env_var + 1);
+            } else {
+                if (*env_var++ == '+')
+                    sprintf(entry, "%s%c%s", DEFAULT_MIBFILES, ENV_SEPARATOR_CHAR,
+                            env_var );
+                else
+                    sprintf(entry, "%s%c%s", env_var, ENV_SEPARATOR_CHAR,
+                            DEFAULT_MIBFILES );
+            }
             SNMP_FREE(env_var);
             env_var = entry;
 #else

commit ec28fff5c0dede00767b50cebe8959369e6d7035

libsnmp: Fix handling of the minus character in mib configuration directives
Change one occurrence of 'confmibdir' into 'confmibs'. It seems like the
original code is the result of a copy/paste error. See also
https://github.com/net-snmp/net-snmp/issues/170.

Fixes: 4253c5431631 ("make mibs, mibdirs and mibfiles handling consistent")

snmplib/mib.c
@@ -2222,15 +2222,17 @@ handle_mibs_conf(const char *token, char *line)
 
     if (confmibs) {
         if ((*line == '+') || (*line == '-')) {
-            ctmp = (char *) malloc(strlen(confmibs) + strlen(line) + 2);
-            if (!ctmp) {
+            int res;
+
+            res = *line++ == '+' ?
+                /* Add specified dirs after existing ones */
+                asprintf(&ctmp, "%s%c%s", confmibs, ENV_SEPARATOR_CHAR, line) :
+                /* Add specified dirs before existing ones */
+                asprintf(&ctmp, "%s%c%s", line, ENV_SEPARATOR_CHAR, confmibs);
+            if (res < 0) {
                 DEBUGMSGTL(("read_config:initmib", "mibs conf malloc failed"));
                 return;
             }
-            if(*line++ == '+')
-                sprintf(ctmp, "%s%c%s", confmibs, ENV_SEPARATOR_CHAR, line);
-            else
-                sprintf(ctmp, "%s%c%s", line, ENV_SEPARATOR_CHAR, confmibdir);
         } else {
             ctmp = strdup(line);
             if (!ctmp) {
@@ -2488,7 +2490,7 @@ register_mib_handlers(void)
                                     "[mib-dirs|+mib-dirs|-mib-dirs]");
     register_prenetsnmp_mib_handler("snmp", "mibs",
                                     handle_mibs_conf, NULL,
-                                    "[mib-tokens|+mib-tokens]");
+                                    "[mib-tokens|+mib-tokens|-mib-tokens]");
     register_config_handler("snmp", "mibfile",
                             handle_mibfile_conf, NULL, "mibfile-to-read");
     /*