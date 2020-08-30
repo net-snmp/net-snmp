net-snmp/net-snmp

commit ad653fb4618dc70a463d169f68eef868d2240109

new data access files
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9985 06827809-a52a-0410-b366-d66718629ded

agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -1,0 +1,203 @@
+/*
+ *  Interface MIB architecture support
+ *
+ * $Id$
+ */
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include "mibII/mibII_common.h"
+
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/data_access/interface.h>
+
+/*
+ *
+ * @retval  0 success
+ * @retval -1 no container specified
+ * @retval -2 could not open /proc/net/dev
+ * @retval -3 could not create entry (probably malloc)
+ */
+int
+netsnmp_access_interface_container_arch_load(netsnmp_container* container,
+                                             u_int load_flags)
+{
+    FILE           *devin;
+    char            line[256];
+    const char     *scan_line_2_2 =
+        "%llu %llu %llu %llu %*llu %*llu %*llu %*llu %llu %llu %llu %llu %*llu %llu";
+    const char     *scan_line_2_0 =
+        "%llu %llu %*llu %*llu %*llu %llu %llu %*llu %*llu %llu";
+    static const char     *scan_line_to_use = NULL;
+    static char     scan_expected;
+    int             scan_count;
+    unsigned long long rec_pkt, rec_oct, rec_err, rec_drop;
+    unsigned long long snd_pkt, snd_oct, snd_err, snd_drop, coll;
+    netsnmp_interface_entry *entry = NULL;
+
+    DEBUGMSGTL(("access:interface", "ifcontainer_arch_load (flags %p)\n",
+                load_flags));
+
+    if (NULL == container) {
+        snmp_log(LOG_ERR, "no container specified/found for access_interface_\n");
+        return -1;
+    }
+
+    if (!(devin = fopen("/proc/net/dev", "r"))) {
+        DEBUGMSGTL(("access:interface",
+                    "Failed to load Interface Table (linux1)\n"));
+        snmp_log(LOG_ERR, "snmpd: cannot open /proc/net/dev ...\n");
+        return -2;
+    }
+
+    /*
+     * Read the first two lines of the file, containing the header
+     * This indicates which version of the kernel we're working with,
+     * and hence which statistics are actually available.
+     *
+     * Wes originally suggested parsing the field names in this header
+     * to detect the position of individual fields directly,
+     * but I suspect this is probably more trouble than it's worth.
+     */
+    fgets(line, sizeof(line), devin);
+    fgets(line, sizeof(line), devin);
+    /*
+     * XXX - What's the format for the 2.6 kernel ?
+     */
+    if( NULL == scan_line_to_use ) {
+        if (strstr(line, "compressed")) {
+            scan_line_to_use = scan_line_2_2;
+            scan_expected = 9;
+            DEBUGMSGTL(("access:interface",
+                        "using linux 2.2 kernel /proc/net/dev\n"));
+        } else {
+            scan_line_to_use = scan_line_2_0;
+            scan_expected = 5;
+            DEBUGMSGTL(("access:interface",
+                        "using linux 2.0 kernel /proc/net/dev\n"));
+        }
+    }
+
+    /*
+     * The rest of the file provides the statistics for each interface.
+     * Read in each line in turn, isolate the interface name
+     *   and retrieve (or create) the corresponding data structure.
+     */
+    while (fgets(line, sizeof(line), devin)) {
+        char           *stats, *ifstart = line;
+
+        if (line[strlen(line) - 1] == '\n')
+            line[strlen(line) - 1] = '\0';
+
+        while (*ifstart && *ifstart == ' ')
+            ifstart++;
+
+        if ((!*ifstart) || ((stats = strrchr(ifstart, ':')) == NULL)) {
+            snmp_log(LOG_ERR,
+                     "interface data format error 1, line ==|%s|\n", line);
+            continue;
+        }
+        if ((scan_line_to_use == scan_line_2_2) && ((stats - line) < 6)) {
+            snmp_log(LOG_ERR,
+                     "interface data format error 2 (%d < 6), line ==|%s|\n",
+                     line, stats - line);
+        }
+
+        DEBUGMSGTL(("9:access:ifcontainer", "processing '%s'\n", ifstart));
+
+        /*
+         * If we've met this interface before, use the same index.
+         * Otherwise find an unused index value and use that.
+         */
+        *stats++ = 0; /* null terminate name */
+        entry = netsnmp_access_interface_entry_create(ifstart);
+        if(NULL == entry) {
+            netsnmp_access_interface_container_free(container,
+                                                    NETSNMP_ACCESS_INTERFACE_FREE_NOFLAGS);
+            fclose(devin);
+            return -3;
+        }
+        entry->if_speed = 10000000; // xxx-rks: lookup token?
+        entry->if_type = 6;
+
+
+        /*
+         * OK - we've now got (or created) the data structure for
+         *      this interface, including any "static" information.
+         * Now parse the rest of the line (i.e. starting from 'stats')
+         *      to extract the relevant statistics, and populate
+         *      data structure accordingly.
+         * Use the ifentry flags field to indicate which counters are valid
+         */
+        while (*stats == ' ')
+            stats++;
+
+        rec_pkt = rec_oct = rec_err = rec_drop = 0;
+        snd_pkt = snd_oct = snd_err = snd_drop = coll = 0;
+        if (scan_line_to_use == scan_line_2_2) {
+            scan_count = sscanf(stats, scan_line_to_use,
+                                &rec_oct, &rec_pkt, &rec_err, &rec_drop,
+                                &snd_oct, &snd_pkt, &snd_err, &snd_drop,
+                                &coll);
+            if (scan_count == scan_expected) {
+                entry->flags |= NETSNMP_INTERFACE_FLAGS_HAS_BYTES;
+                entry->flags |= NETSNMP_INTERFACE_FLAGS_HAS_DROPS;
+                /*
+                 *  2.4 kernel includes a single multicast (input) counter?
+                 */
+                entry->flags |= NETSNMP_INTERFACE_FLAGS_HAS_MCAST_PKTS;
+                entry->flags |= NETSNMP_INTERFACE_FLAGS_HAS_HIGH_SPEED;
+                entry->flags |= NETSNMP_INTERFACE_FLAGS_HAS_HIGH_BYTES;
+                entry->flags |= NETSNMP_INTERFACE_FLAGS_HAS_HIGH_PACKETS;
+            }
+        } else {
+            scan_count = sscanf(stats, scan_line_to_use,
+                                &rec_pkt, &rec_err,
+                                &snd_pkt, &snd_err, &coll);
+            if (scan_count == scan_expected) {
+                entry->flags &= ~NETSNMP_INTERFACE_FLAGS_HAS_MCAST_PKTS;
+                rec_oct = rec_drop = 0;
+                snd_oct = snd_drop = 0;
+            }
+        }
+        if(scan_count != scan_expected) {
+            snmp_log(LOG_ERR,
+                     "error scanning interface data (expected %d, got %d)\n",
+                     scan_expected, scan_count);
+            netsnmp_access_interface_entry_free(entry);
+            continue;
+        }
+        entry->flags |= NETSNMP_INTERFACE_FLAGS_ACTIVE;
+
+        /*
+         * linux previous to 1.3.~13 may miss transmitted loopback pkts: 
+         */
+        if (!strcmp(entry->if_name, "lo") && rec_pkt > 0 && !snd_pkt)
+            snd_pkt = rec_pkt;
+
+        /*
+         * //xxx-rks: get descr by linking mem from /proc/pci and /proc/iomem
+         */
+
+
+        entry->if_ibytes.low = rec_oct & 0xffffffff;
+        entry->if_ibytes.high = rec_oct >> 32;
+        entry->if_iucast.low = rec_pkt & 0xffffffff;
+        entry->if_iucast.high = rec_pkt >> 32;
+        entry->if_ierrors = rec_err;
+        entry->if_idiscards = rec_drop;
+        entry->if_obytes.low = snd_oct & 0xffffffff;
+        entry->if_obytes.high = snd_oct >> 32;
+        entry->if_oucast.low = snd_pkt & 0xffffffff;
+        entry->if_oucast.high = snd_pkt >> 32;
+        entry->if_oerrors = snd_err;
+        entry->if_odiscards = snd_drop;
+        entry->if_collisions = coll;
+        
+        /*
+         * add to container
+         */
+        CONTAINER_INSERT(container, entry);
+    }
+    fclose(devin);
+    return 0;
+}

agent/mibgroup/if-mib/data_access/interface.h
@@ -1,0 +1,36 @@
+/*
+ * interface data access header
+ *
+ * $Id$
+ */
+/**---------------------------------------------------------------------*/
+/*
+ * configure required files
+ *
+ * Notes:
+ *
+ * 1) prefer functionality over platform, where possible. If a method
+ *    is available for multiple platforms, test that first. That way
+ *    when a new platform is ported, it won't need a new test here.
+ *
+ * 2) don't do detail requirements here. If, for example,
+ *    HPUX11 had different reuirements than other HPUX, that should
+ *    be handled in the *_hpux.h header file.
+ */
+config_require(if-mib/data_access/interface_common)
+#if defined( linux )
+config_require(if-mib/data_access/interface_linux);
+#elif defined ( solaris )
+config_require(if-mib/data_access/interface_solaris);
+#elif defined ( hpux )
+config_require(if-mib/data_access/interface_hpux);
+#elif defined ( bsd )
+config_require(if-mib/data_access/interface_bsd);
+#else
+/*
+ * couldn't determine the correct file!
+ * require a bogus file to generate an error.
+ */
+configure_require(if-mib/data_access/interface-unknown-arch);
+#endif
+

agent/mibgroup/if-mib/data_access/interface_common.c
@@ -1,0 +1,277 @@
+/*
+ *  Interface MIB architecture support
+ *
+ * $Id$
+ */
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include "mibII/mibII_common.h"
+
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/data_access/interface.h>
+
+/**---------------------------------------------------------------------*/
+/*
+ * local static prototypes
+ */
+static int _access_interface_entry_compare_name(const void *lhs,
+                                                const void *rhs);
+static void _access_interface_entry_release(netsnmp_interface_entry * entry,
+                                            void *unused);
+static void _access_interface_entry_set_index(netsnmp_interface_entry *entry,
+                                              const char *name);
+
+/**---------------------------------------------------------------------*/
+/*
+ * external per-architecture functions prototypes
+ *
+ * These shouldn't be called by the general public, so they aren't in
+ * the header file.
+ */
+extern int
+netsnmp_access_interface_container_arch_load(netsnmp_container* container,
+                                             u_int load_flags);
+
+
+/**---------------------------------------------------------------------*/
+/*
+ * container functions
+ */
+/**
+ */
+netsnmp_container *
+netsnmp_access_interface_container_init(u_int flags)
+{
+    netsnmp_container *container1;
+
+    DEBUGMSGTL(("access:interface:container", "init\n"));
+
+    /*
+     * create the containers. one indexed by ifIndex, the other
+     * indexed by ifName.
+     */
+    container1 = netsnmp_container_find("ifTable:table_container");
+    if (NULL == container1)
+        return NULL;
+
+    if (flags & NETSNMP_ACCESS_INTERFACE_INIT_ADDL_IDX_BY_NAME) {
+        netsnmp_container *container2 =
+            netsnmp_container_find("ifTable_by_name:ifTable:table_container");
+        if (NULL == container2)
+            return NULL;
+
+        container2->compare = _access_interface_entry_compare_name;
+        
+        netsnmp_container_add_index(container1, container2);
+    }
+
+    return container1;
+}
+
+/**
+ * @retval NULL  error
+ * @retval !NULL pointer to container
+ */
+netsnmp_container*
+netsnmp_access_interface_container_load(netsnmp_container* container, u_int load_flags)
+{
+    int rc;
+
+    DEBUGMSGTL(("access:interface:container", "load\n"));
+
+    if (NULL == container)
+        container = netsnmp_container_find("access:interface:table_container");
+    if (NULL == container) {
+        snmp_log(LOG_ERR, "no container specified/found for access_interface\n");
+        return NULL;
+    }
+
+    rc =  netsnmp_access_interface_container_arch_load(container, load_flags);
+    if (0 != rc) {
+        netsnmp_access_interface_container_free(container,
+                                                NETSNMP_ACCESS_INTERFACE_FREE_NOFLAGS);
+        container = NULL;
+    }
+
+    return container;
+}
+
+void
+netsnmp_access_interface_container_free(netsnmp_container *container, u_int free_flags)
+{
+    DEBUGMSGTL(("access:interface:container", "free\n"));
+
+    if (NULL == container) {
+        snmp_log(LOG_ERR, "invalid container for netsnmp_access_interface_free\n");
+        return;
+    }
+
+    if(! (free_flags & NETSNMP_ACCESS_INTERFACE_FREE_DONT_CLEAR)) {
+        /*
+         * free all items.
+         */
+        CONTAINER_CLEAR(container,
+                        (netsnmp_container_obj_func*)_access_interface_entry_release,
+                        NULL);
+    }
+
+    CONTAINER_FREE(container);
+}
+
+/**---------------------------------------------------------------------*/
+/*
+ * ifentry functions
+ */
+/**
+ */
+netsnmp_interface_entry *
+netsnmp_access_interface_entry_get_by_index(netsnmp_container *container, oid index)
+{
+    netsnmp_index   tmp;
+
+    if (NULL == container) {
+        snmp_log(LOG_ERR,
+                 "invalid container for netsnmp_access_interface_entry_get_by_index\n");
+        return NULL;
+    }
+
+    tmp.len = 1;
+    tmp.oids = &index;
+
+    return (netsnmp_interface_entry *) CONTAINER_FIND(container, &tmp);
+}
+
+/**
+ */
+netsnmp_interface_entry *
+netsnmp_access_interface_entry_get_by_name(netsnmp_container *container,
+                                const char *name)
+{
+    netsnmp_interface_entry tmp;
+
+    if (NULL == container) {
+        snmp_log(LOG_ERR,
+                 "invalid container for netsnmp_access_interface_entry_get_by_name\n");
+        return NULL;
+    }
+
+    if (NULL == container->next) {
+        snmp_log(LOG_ERR,
+                 "invalid container for netsnmp_access_interface_entry_get_by_name\n");
+        return NULL;
+    }
+
+    tmp.if_name = name;
+    return CONTAINER_FIND(container->next, &tmp);
+}
+
+/**
+ * @retval 0  interface not found
+ */
+oid
+netsnmp_access_interface_index_find(const char *name)
+{
+    oid index = se_find_value_in_slist("interfaces", name);
+    if (index == SE_DNE)
+        return 0;
+
+    return index;
+}
+
+/**
+ */
+netsnmp_interface_entry *
+netsnmp_access_interface_entry_create(const char *name)
+{
+    netsnmp_interface_entry *entry = SNMP_MALLOC_TYPEDEF(netsnmp_interface_entry);
+
+    if(NULL != name)
+        entry->if_name = strdup(name);
+
+    _access_interface_entry_set_index(entry, name);
+
+    entry->if_descr = strdup("unknown");
+
+    entry->if_connector_present = 1; /* assume yes */
+
+    entry->oid_index.len = 1;
+    entry->oid_index.oids = (oid *) & entry->index;
+
+    // xxx-rks: do this stuff.. ifspeed, etc..
+    /*
+     * XXX - initialise the "static" information
+     *  a) Using the configure overrides
+     *  b) Via (architecture-specific) utility routines
+     */
+    snmp_log(LOG_ERR, "netsnmp_access_interface_entry_info_init(entry);\n");
+
+    return entry;
+}
+
+/**
+ */
+void
+netsnmp_access_interface_entry_free(netsnmp_interface_entry * entry)
+{
+    if (NULL == entry)
+        return;
+
+    if (NULL != entry->if_name)
+        free(entry->if_name);
+
+    if (NULL != entry->if_descr)
+        free(entry->if_descr);
+
+    if (NULL != entry->if_alias)
+        free(entry->if_alias);
+
+    if (NULL != entry->if_old_alias)
+        free(entry->if_old_alias);
+
+    if (NULL != entry->if_paddr)
+        free(entry->if_paddr);
+
+    free(entry);
+}
+
+/**---------------------------------------------------------------------*/
+/*
+ * Utility routines
+ */
+
+/**
+ */
+static int
+_access_interface_entry_compare_name(const void *lhs, const void *rhs)
+{
+    return strcmp(((const netsnmp_interface_entry *) lhs)->if_name,
+                  ((const netsnmp_interface_entry *) rhs)->if_name);
+}
+
+/**
+ */
+void
+_access_interface_entry_release(netsnmp_interface_entry * entry, void *context)
+{
+    netsnmp_access_interface_entry_free(entry);
+}
+
+/**
+ */
+void
+_access_interface_entry_set_index(netsnmp_interface_entry *entry, const char *name)
+{
+    if(NULL != name) {
+        entry->index = netsnmp_access_interface_index_find(name);
+        if (entry->index == 0) {
+            entry->index = se_find_free_value_in_slist("interfaces");
+            if (entry->index == SE_DNE)
+                entry->index = 1;       /* Completely new list! */
+            se_add_pair_to_slist("interfaces", strdup(name), entry->index);
+            DEBUGMSGTL(("access:interface:ifIndex", "new ifIndex %d for %s\n",
+                        entry->index, name));
+        }
+    }
+    else
+        entry->index = 0;
+}

agent/mibgroup/ip-forward-mib/data_access/route.h
@@ -1,0 +1,39 @@
+/*
+ * route data access header
+ *
+ * $Id$
+ */
+/**---------------------------------------------------------------------*/
+/*
+ * configure required files
+ *
+ * Notes:
+ *
+ * 1) prefer functionality over platform, where possible. If a method
+ *    is available for multiple platforms, test that first. That way
+ *    when a new platform is ported, it won't need a new test here.
+ *
+ * 2) don't do detail requirements here. If, for example,
+ *    HPUX11 had different reuirements than other HPUX, that should
+ *    be handled in the *_hpux.h header file.
+ */
+config_require(ip-forward-mib/data_access/route_common)
+#if defined( linux )
+config_require(ip-forward-mib/data_access/route_linux);
+#elif defined ( solaris )
+config_require(ip-forward-mib/data_access/route_solaris);
+#elif defined ( hpux )
+config_require(ip-forward-mib/data_access/route_hpux);
+#elif defined ( bsd )
+config_require(ip-forward-mib/data_access/route_bsd);
+#else
+/*
+ * couldn't determine the correct file!
+ * require a bogus file to generate an error.
+ */
+configure_require(ip-forward-mib/data_access/route_unknown-arch);
+#endif
+
+/** need interface for ifIndex */
+config_require(if-mib/data_access/interface);
+

agent/mibgroup/ip-forward-mib/data_access/route_common.c
@@ -1,0 +1,158 @@
+/*
+ *  Interface MIB architecture support
+ *
+ * $Id$
+ */
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include "mibII/mibII_common.h"
+
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/data_access/route.h>
+
+/**---------------------------------------------------------------------*/
+/*
+ * local static prototypes
+ */
+static void _access_route_entry_release(netsnmp_route_entry * entry, void *unused);
+static int _access_route_entry_compare(const netsnmp_route_entry *lhs,
+                                       const netsnmp_route_entry *rhs);
+
+/**---------------------------------------------------------------------*/
+/*
+ * external per-architecture functions prototypes
+ *
+ * These shouldn't be called by the general public, so they aren't in
+ * the header file.
+ */
+extern int netsnmp_access_route_container_arch_load(netsnmp_container* container,
+                                                    u_int load_flags);
+
+
+/**---------------------------------------------------------------------*/
+/*
+ * container functions
+ */
+
+/**
+ * @retval NULL  error
+ * @retval !NULL pointer to container
+ */
+netsnmp_container*
+netsnmp_access_route_container_load(netsnmp_container* container, u_int load_flags)
+{
+    int rc;
+
+    DEBUGMSGTL(("access:route:container", "load\n"));
+
+    if (NULL == container)
+        container = netsnmp_container_find("access:_route:table_container");
+    if (NULL == container) {
+        snmp_log(LOG_ERR, "no container specified/found for access_route\n");
+        return NULL;
+    }
+    container->compare = _access_route_entry_compare;
+
+    rc =  netsnmp_access_route_container_arch_load(container, load_flags);
+    if (0 != rc) {
+        netsnmp_access_route_container_free(container, NETSNMP_ACCESS_ROUTE_FREE_NOFLAGS);
+        container = NULL;
+    }
+
+    return container;
+}
+
+void
+netsnmp_access_route_container_free(netsnmp_container *container, u_int free_flags)
+{
+    DEBUGMSGTL(("access:route:container", "free\n"));
+
+    if (NULL == container) {
+        snmp_log(LOG_ERR, "invalid container for netsnmp_access_route_free\n");
+        return;
+    }
+
+    if(! (free_flags & NETSNMP_ACCESS_ROUTE_FREE_DONT_CLEAR)) {
+        /*
+         * free all items.
+         */
+        CONTAINER_CLEAR(container,
+                        (netsnmp_container_obj_func*)_access_route_entry_release,
+                        NULL);
+    }
+
+    if(! (free_flags & NETSNMP_ACCESS_ROUTE_FREE_KEEP_CONTAINER))
+        CONTAINER_FREE(container);
+}
+
+/**---------------------------------------------------------------------*/
+/*
+ * ifentry functions
+ */
+/**
+ */
+netsnmp_route_entry *
+netsnmp_access_route_entry_create(void)
+{
+    netsnmp_route_entry *entry = SNMP_MALLOC_TYPEDEF(netsnmp_route_entry);
+    if(NULL == entry) {
+        snmp_log(LOG_ERR, "could not allocate route entry\n");
+        return NULL;
+    }
+
+    entry->oid_index.oids = entry->rt_indexes;
+    entry->oid_index.len = 4;
+
+    entry->rt_metric1 = -1;
+    entry->rt_metric2 = -1;
+    entry->rt_metric3 = -1;
+    entry->rt_metric4 = -1;
+    entry->rt_metric5 = -1;
+
+    /** entry->row_status? */
+
+    return entry;
+}
+
+/**
+ */
+void
+netsnmp_access_route_entry_free(netsnmp_route_entry * entry)
+{
+    if (NULL == entry)
+        return;
+
+    if (NULL != entry->rt_info)
+        free(entry->rt_info);
+
+    free(entry);
+}
+
+/**---------------------------------------------------------------------*/
+/*
+ * Utility routines
+ */
+
+/**
+ */
+static int
+_access_route_entry_compare(const netsnmp_route_entry *lhs,
+                            const netsnmp_route_entry *rhs)
+{
+    int i;
+    for( i = 0; i < 4; ++i ) {
+        if(lhs->rt_indexes[i] < rhs->rt_indexes[i] )
+            return -1;
+        else if (lhs->rt_indexes[i] > rhs->rt_indexes[i] )
+            return 1;
+    }
+    return 0;
+}
+
+/**
+ */
+void
+_access_route_entry_release(netsnmp_route_entry * entry, void *context)
+{
+    netsnmp_access_route_entry_free(entry);
+}

agent/mibgroup/ip-forward-mib/data_access/route_linux.c
@@ -1,0 +1,114 @@
+/*
+ *  Interface MIB architecture support
+ *
+ * $Id$
+ */
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include "mibII/mibII_common.h"
+
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/data_access/interface.h>
+#include <net-snmp/data_access/route.h>
+
+#include "ip-forward-mib/ipCidrRouteTable/ipCidrRouteTable_constants.h"
+
+/**
+ *
+ * @retval  0 success
+ * @retval -1 no container specified
+ * @retval -2 could not open /proc/net/route
+ */
+int
+netsnmp_access_route_container_arch_load(netsnmp_container* container,
+                                         u_int load_flags)
+{
+    FILE           *in;
+    char            line[256];
+    netsnmp_route_entry *entry = NULL;
+    char            name[16];
+
+    DEBUGMSGTL(("access:route:container",
+                "route_container_arch_load (flags %p)\n", load_flags));
+
+    if (NULL == container) {
+        snmp_log(LOG_ERR, "no container specified/found for access_if\n");
+        return -1;
+    }
+
+    /*
+     * fetch routes from the proc file-system:
+     */
+    if (!(in = fopen("/proc/net/route", "r"))) {
+        snmp_log(LOG_ERR, "cannot open /proc/net/route\n");
+        return -2;
+    }
+
+    fgets(line, sizeof(line), in); /* skip header */
+
+    while (fgets(line, sizeof(line), in)) {
+        char            rtent_name[32];
+        int             refcnt, flags, rc;
+        unsigned        use;
+
+        entry = netsnmp_access_route_entry_create();
+
+        /*
+         * as with 1.99.14:
+         * Iface Dest     GW       Flags RefCnt Use Metric Mask     MTU  Win IRTT
+         * eth0  0A0A0A0A 00000000 05    0      0   0      FFFFFFFF 1500 0   0 
+         */
+        rc = sscanf(line, "%s %x %x %x %u %d %d %x %*d %*d %*d\n",
+                    rtent_name, &entry->rt_dest, &entry->rt_nexthop,
+                    /*
+                     * XXX: fix type of the args 
+                     */
+                    &flags, &refcnt, &use, &entry->rt_metric1,
+                    &entry->rt_mask);
+        DEBUGMSGTL(("9:access:route:container", "line |%s|\n", line));
+        if (8 != rc) {
+            snmp_log(LOG_ERR,
+                     "/proc/net/route data format error (%d!=8), line ==|%s|",
+                     rc, line);
+            continue;
+        }
+
+        strncpy(name, rtent_name, sizeof(name));
+        name[ sizeof(name)-1 ] = 0;
+        /*
+         * linux says ``lo'', but the interface is stored as ``lo0'': 
+         * xxx-rks: sez who? not on 2.4.20?
+         */
+        //if (!strcmp(name, "lo"))
+        //   strcat(name, "0");
+
+        // xxx-rks: how to make sure interfaces has been loaded?
+        entry->if_index = se_find_value_in_slist("interfaces", name);
+        if(SE_DNE == entry->if_index) {
+            snmp_log(LOG_ERR,"unknown interface '%s' in /proc/net/route\n",
+                     name);
+            netsnmp_access_route_entry_free(entry);
+            continue;
+        }
+
+        if (flags & RTF_UP) {
+            if (flags & RTF_GATEWAY) {
+                entry->rt_type = IPCIDRROUTETYPE_REMOTE;
+            } else {
+                entry->rt_type = IPCIDRROUTETYPE_LOCAL;
+            }
+        } else 
+            entry->rt_type = IPCIDRROUTETYPE_REJECT;
+        
+        entry->rt_proto = (flags & RTF_DYNAMIC)
+            ? IPCIDRROUTEPROTO_ICMP : IPCIDRROUTEPROTO_LOCAL;
+
+        // xxx-rks: does this belong here? probably not...
+        entry->row_status = ROWSTATUS_ACTIVE;
+
+        CONTAINER_INSERT(container, entry);
+    }
+
+    fclose(in);
+    return 0;
+}

commit 205c69a652d742d4914a4332ecc8915b52217719

snmpd: avoid container double free in error cases
Fixes: ad653fb4618d ("new data access files")
[ bvanassche: Added "Fixes" tag ]

agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -757,8 +757,6 @@ netsnmp_arch_interface_container_load(netsnmp_container* container,
 #ifdef NETSNMP_ENABLE_IPV6
             netsnmp_access_ipaddress_container_free(addr_container, 0);
 #endif
-            netsnmp_access_interface_container_free(container,
-                                                    NETSNMP_ACCESS_INTERFACE_FREE_NOFLAGS);
             fclose(devin);
             close(fd);
             free(ifc.ifc_buf);

agent/mibgroup/if-mib/data_access/interface_openbsd.c
@@ -143,8 +143,6 @@ netsnmp_arch_interface_container_load(netsnmp_container* container,
 
         entry = netsnmp_access_interface_entry_create(if_name, ifp->ifm_index);
         if(NULL == entry) {
-            netsnmp_access_interface_container_free(container,
-                                                    NETSNMP_ACCESS_INTERFACE_FREE_NOFLAGS);
             free(if_list);
             return -3;
         }

agent/mibgroup/if-mib/data_access/interface_solaris2.c
@@ -211,8 +211,6 @@ netsnmp_arch_interface_container_load(netsnmp_container* container,
     if (error) {
         DEBUGMSGTL(("access:interface:container:arch", 
                     "error %d, free container\n", error));
-        netsnmp_access_interface_container_free(container,
-            NETSNMP_ACCESS_INTERFACE_FREE_NOFLAGS);
         return -2;
     }
 

agent/mibgroup/if-mib/data_access/interface_sysctl.c
@@ -546,8 +546,6 @@ netsnmp_arch_interface_container_load(netsnmp_container* container,
         entry = netsnmp_access_interface_entry_create(if_name, ifp->ifm_index);
         free(if_name);
         if(NULL == entry) {
-            netsnmp_access_interface_container_free(container,
-                                                    NETSNMP_ACCESS_INTERFACE_FREE_NOFLAGS);
             free(if_list);
             return -3;
         }