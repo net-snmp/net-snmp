net-snmp/net-snmp

commit 22e1371bb1fd4a502fc1065fb5aa6b6df8a6d0d2

HOST-MIB, hr_filesys: Convert recursion into iteration

agent/mibgroup/host/hr_filesys.c
@@ -719,6 +719,7 @@ static const char *HRFS_ignores[] = {
 int
 Get_Next_HR_FileSys(void)
 {
+next:
 #if HAVE_GETFSSTAT
     if (HRFS_index >= fscount)
         return -1;
@@ -745,8 +746,7 @@ Get_Next_HR_FileSys(void)
         case MNT_PROCFS:
 #endif
         case MNT_SFS:
-            return Get_Next_HR_FileSys();
-            break;
+            goto next;
     }
     return HRFS_index++;
 #else
@@ -766,7 +766,7 @@ Get_Next_HR_FileSys(void)
 
     for (cpp = HRFS_ignores; *cpp != NULL; ++cpp)
         if (!strcmp(HRFS_entry->HRFS_type, *cpp))
-            return Get_Next_HR_FileSys();
+            goto next;
 
     /*
      * Try and ensure that index values are persistent

commit 9588b5c9c27239b1f8c02f0bf417f13735e93225

HOST-MIB, hr_filesys: fix compile error
On non-AIX systems without getfsstat, a variable is being declared right
after a label. This is a violation of the C language standard, and
causes the following compile error:

host/hr_filesys.c: In function 'Get_Next_HR_FileSys':
host/hr_filesys.c:752:5: error: a label can only be part of a statement and a declaration is not a statement
     const char    **cpp;
          ^~~~~

Fix the problem by adding an empty statement after the label.

Fixes: 22e1371bb1fd ("HOST-MIB, hr_filesys: Convert recursion into iteration")

Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

agent/mibgroup/host/hr_filesys.c
@@ -718,7 +718,7 @@ static const char *HRFS_ignores[] = {
 int
 Get_Next_HR_FileSys(void)
 {
-next:
+next: ;
 #if HAVE_GETFSSTAT
     if (HRFS_index >= fscount)
         return -1;