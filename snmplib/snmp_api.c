net-snmp/net-snmp

commit 6066c6e21991ee31ae4fd5eaf791911a31c3432f

libsnmp, snmpv3_packet_build(): Avoid triggering a buffer overflow

snmplib/snmp_api.c
@@ -2743,6 +2743,11 @@ snmpv3_packet_build(netsnmp_session * session, netsnmp_pdu *pdu,
      */
     DEBUGPRINTPDUTYPE("send", ((pdu_data) ? *pdu_data : 0x00));
     if (pdu_data) {
+        if (cp + pdu_data_len > spdu_buf + sizeof(spdu_buf)) {
+            snmp_log(LOG_ERR, "%s: PDU too big (%" NETSNMP_PRIz "d > %" NETSNMP_PRIz "d)\n",
+                     __func__, pdu_data_len, sizeof(spdu_buf));
+            return -1;
+        }
         memcpy(cp, pdu_data, pdu_data_len);
         cp += pdu_data_len;
     } else {

commit cc3e0505f58d2c5d03be7a0b4c70afd01f7deb3c

Windows: Yet another Visual Studio 2005 build fix
Microsoft Visual Studio 2013 and before do not support __func__. Hence
use __FUNCTION__ instead of __func__ when building with Visual Studio.

Reported-by: Christophe Cassignol
Fixes: 6066c6e21991 ("libsnmp, snmpv3_packet_build(): Avoid triggering a buffer overflow")
Fixes: 20725d58ca76 ("DTLSUDP transport: Check length of from address")

snmplib/snmp_api.c
@@ -2742,7 +2742,7 @@ snmpv3_packet_build(netsnmp_session * session, netsnmp_pdu *pdu,
     if (pdu_data) {
         if (cp + pdu_data_len > spdu_buf + sizeof(spdu_buf)) {
             snmp_log(LOG_ERR, "%s: PDU too big (%" NETSNMP_PRIz "d > %" NETSNMP_PRIz "d)\n",
-                     __func__, pdu_data_len, sizeof(spdu_buf));
+                     NETSNMP_FUNCTION, pdu_data_len, sizeof(spdu_buf));
             return -1;
         }
         memcpy(cp, pdu_data, pdu_data_len);

snmplib/transports/snmpDTLSUDPDomain.c
@@ -667,7 +667,7 @@ netsnmp_dtlsudp_recv(netsnmp_transport *t, void *buf, int size,
         if (rc > 0) {
             if (olen > sizeof(*addr_pair))
                 snmp_log(LOG_ERR, "%s: from address length %d > %d\n",
-                         __func__, olen, (int)sizeof(*addr_pair));
+                         NETSNMP_FUNCTION, olen, (int)sizeof(*addr_pair));
             memcpy(addr_pair, opaque, SNMP_MIN(sizeof(*addr_pair), olen));
         }
         SNMP_FREE(opaque);

win32/net-snmp/net-snmp-config.h
@@ -210,6 +210,9 @@
 /* If we don't want to use kmem. */
 /* #undef NETSNMP_NO_KMEM_USAGE */
 
+/* Should evaluate to the name of the current function if defined */
+#define NETSNMP_FUNCTION __FUNCTION__
+
 /* If you don't want the agent to report on variables it doesn't have data for */
 #define NETSNMP_NO_DUMMY_VALUES 1
 

win32/net-snmp/net-snmp-config.h.in
@@ -210,6 +210,9 @@
 /* If we don't want to use kmem. */
 /* #undef NETSNMP_NO_KMEM_USAGE */
 
+/* Should evaluate to the name of the current function if defined */
+#define NETSNMP_FUNCTION __FUNCTION__
+
 /* If you don't want the agent to report on variables it doesn't have data for */
 #define NETSNMP_NO_DUMMY_VALUES 1
 