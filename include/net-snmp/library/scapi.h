net-snmp/net-snmp

commit 329a9d3c9d63e0dc2c3bc3374f52611d264fa1c3

revamp auth/priv protocol constants handling
- move constants into arrays
- remove hardcoded protocol OID lengths

include/net-snmp/library/scapi.h
@@ -52,22 +52,57 @@ extern          "C" {
 #define SNMP_TRANS_PRIVLEN_AES256_IV    256
 #define SNMP_TRANS_AES_AES256_PADSIZE   128
 
+
+typedef struct netsnmp_auth_alg_info_s {
+    int          type;
+    const char * name;
+    oid *        alg_oid;
+    int          oid_len;
+    int          proper_length;
+    int          mac_length;
+} netsnmp_auth_alg_info;
+
+typedef struct netsnmp_priv_alg_info_s {
+    int          type;
+    const char * name;
+    oid *        alg_oid;
+    int          oid_len;
+    int          proper_length;
+    int         iv_length;
+    int         pad_size;
+    const EVP_CIPHER *  cipher;
+} netsnmp_priv_alg_info;
+
     /*
      * Prototypes.
      */
-    int             sc_get_authtype(const oid * hashtype, u_int hashtype_len);
-    int             sc_get_proper_auth_length(int hashtype);
+    int             sc_get_authtype(const oid * hashoid, u_int hashoid_len);
+    int             sc_get_proper_auth_length_bytype(int auth_type);
     int             sc_get_auth_maclen(int auth_type);
+    const char*     sc_get_auth_name(int auth_type);
+    oid *           sc_get_auth_oid(int auth_type, size_t *oid_len);
+    netsnmp_auth_alg_info * sc_get_auth_alg_byoid(const oid *oid, u_int len);
+    netsnmp_auth_alg_info * sc_get_auth_alg_bytype(u_int type);
+    netsnmp_auth_alg_info * sc_get_auth_alg_byindex(u_int index);
 
-    /** deprectated, use sc_get_authtype() + sc_get_proper_auth_length() */
+    /** deprectated, use
+     *        sc_get_authtype() + sc_get_proper_auth_length_bytype() */
     int             sc_get_properlength(const oid * hashtype,
                                         u_int hashtype_len);
 
-    int             sc_get_proper_priv_length(const oid * privtype,
-                                              u_int privtype_len);
 #ifdef NETSNMP_USE_OPENSSL
     const EVP_MD *sc_get_openssl_hashfn(int auth_type);
+    const EVP_CIPHER *sc_get_openssl_privfn(int priv_type);
 #endif
+
+    int             sc_get_privtype(const oid * privtype, u_int privtype_len);
+    oid *           sc_get_priv_oid(int type, size_t *oid_len);
+    int             sc_get_proper_priv_length(const oid * privtype,
+                                              u_int privtype_len);
+    int             sc_get_proper_priv_length_bytype(int privtype);
+    netsnmp_priv_alg_info * sc_get_priv_alg_byoid(const oid *oid, u_int len);
+    netsnmp_priv_alg_info * sc_get_priv_alg_bytype(u_int type);
+    netsnmp_priv_alg_info * sc_get_priv_alg_byindex(u_int index);
 
     NETSNMP_IMPORT
     int             sc_init(void);

apps/encode_keychange.c
@@ -83,7 +83,7 @@ char           *newpass = NULL, *oldpass = NULL;
 char           *transform_type_input = NULL;
 
 const oid      *transform_type = NULL;  /* Type of HMAC hash to use.      */
-
+size_t          transform_type_len = 0;
 
 
 /*
@@ -120,7 +120,7 @@ main(int argc, char **argv)
         oldkul[SNMP_MAXBUF_SMALL],
         newkul[SNMP_MAXBUF_SMALL], keychange[SNMP_MAXBUF_SMALL];
 
-    int             i;
+    int             i, auth_type;
     int             arg = 1;
 
     local_progname = argv[0];
@@ -188,16 +188,9 @@ main(int argc, char **argv)
     /*
      * Convert and error check transform_type.
      */
-#ifndef NETSNMP_DISABLE_MD5
-    if (!strcmp(transform_type_input, "md5")) {
-        transform_type = usmHMACMD5AuthProtocol;
-
-    } else
-#endif
-        if (!strcmp(transform_type_input, "sha1")) {
-        transform_type = usmHMACSHA1AuthProtocol;
-
-    } else {
+    auth_type = usm_lookup_auth_type(transform_type_input);
+    transform_type = sc_get_auth_oid( auth_type, &transform_type_len );
+    if (NULL == transform_type) {
         fprintf(stderr,
                 "Unrecognized hash transform: \"%s\".\n",
                 transform_type_input);
@@ -206,13 +199,7 @@ main(int argc, char **argv)
     }
 
     if (verbose) {
-        fprintf(stderr, "Hash:\t\t%s\n",
-#ifndef NETSNMP_DISABLE_MD5
-                (transform_type == usmHMACMD5AuthProtocol)
-                ? "usmHMACMD5AuthProtocol" :
-#endif
-                "usmHMACSHA1AuthProtocol"
-            );
+        fprintf(stderr, "Hash:\t\t%s\n", sc_get_auth_name(auth_type));
     }
 
 
@@ -280,13 +267,13 @@ main(int argc, char **argv)
     QUITFUN(rval, main_quit);
 
 
-    rval = generate_Ku(transform_type, USM_LENGTH_OID_TRANSFORM,
+    rval = generate_Ku(transform_type, transform_type_len,
                        (u_char *) oldpass, strlen(oldpass),
                        oldKu, &oldKu_len);
     QUITFUN(rval, main_quit);
 
 
-    rval = generate_Ku(transform_type, USM_LENGTH_OID_TRANSFORM,
+    rval = generate_Ku(transform_type, transform_type_len,
                        (u_char *) newpass, strlen(newpass),
                        newKu, &newKu_len);
     QUITFUN(rval, main_quit);
@@ -303,7 +290,7 @@ main(int argc, char **argv)
         DEBUGMSGTL(("encode_keychange", "%02x", (int) (oldKu[i])));
     DEBUGMSGTL(("encode_keychange", "\n"));
 
-    rval = generate_kul(transform_type, USM_LENGTH_OID_TRANSFORM,
+    rval = generate_kul(transform_type, transform_type_len,
                         engineid, engineid_len,
                         oldKu, oldKu_len, oldkul, &oldkul_len);
     QUITFUN(rval, main_quit);
@@ -315,7 +302,7 @@ main(int argc, char **argv)
         DEBUGMSGTL(("encode_keychange", "%02x", (int) (oldkul[i])));
     DEBUGMSGTL(("encode_keychange", "\n"));
 
-    rval = generate_kul(transform_type, USM_LENGTH_OID_TRANSFORM,
+    rval = generate_kul(transform_type, transform_type_len,
                         engineid, engineid_len,
                         newKu, newKu_len, newkul, &newkul_len);
     QUITFUN(rval, main_quit);
@@ -326,7 +313,7 @@ main(int argc, char **argv)
         DEBUGMSGTL(("encode_keychange", "%02x", newkul[i]));
     DEBUGMSGTL(("encode_keychange", "\n"));
 
-    rval = encode_keychange(transform_type, USM_LENGTH_OID_TRANSFORM,
+    rval = encode_keychange(transform_type, transform_type_len,
                             oldkul, oldkul_len,
                             newkul, newkul_len, keychange, &keychange_len);
     QUITFUN(rval, main_quit);

apps/snmpusm.c
@@ -620,26 +620,21 @@ main(int argc, char *argv[])
          * they're the same lengths.
          */
         if (doprivkey) {
+            int privtype;
             if (!session.securityPrivProto) {
                 snmp_log(LOG_ERR, "no encryption type specified, which I need in order to know to change the key\n");
                 goto close_session;
             }
-                
-#ifndef NETSNMP_DISABLE_DES
-            if (ISTRANSFORM(session.securityPrivProto, DESPriv)) {
-                /* DES uses a 128 bit key, 64 bits of which is a salt */
-                oldkulpriv_len = newkulpriv_len = 16;
-            }
-#endif
-#ifdef HAVE_AES
-            if (ISTRANSFORM(session.securityPrivProto, AESPriv)) {
-                oldkulpriv_len = newkulpriv_len = 16;
-            }
-#endif
+
+            privtype = sc_get_privtype(session.securityPrivProto,
+                                       session.securityPrivProtoLen);
+            oldkulpriv_len = sc_get_proper_priv_length_bytype(privtype);
+            if (USM_CREATE_USER_PRIV_DES == privtype)
+                oldkulpriv_len *= 2; /* ?? we store salt with key */
+            newkulpriv_len = oldkulpriv_len;
             memcpy(oldkulpriv, oldkul, oldkulpriv_len);
             memcpy(newkulpriv, newkul, newkulpriv_len);
         }
-            
 
         /*
          * create the keychange string 
@@ -939,18 +934,12 @@ main(int argc, char *argv[])
             vars = vars->next_variable;
         }
         if (doprivkey) {
-	    size_t dhprivKeyLen = 0;
-#ifndef NETSNMP_DISABLE_DES
-	    if (ISTRANSFORM(ss->securityPrivProto, DESPriv)) {
-                /* DES uses a 128 bit key, 64 bits of which is a salt */
-	        dhprivKeyLen = 16;
-	    }
-#endif
-#ifdef HAVE_AES
-	    if (ISTRANSFORM(ss->securityPrivProto, AESPriv)) {
-	        dhprivKeyLen = 16;
-	    }
-#endif
+            size_t dhprivKeyLen = 0;
+            int privtype = sc_get_privtype(ss->securityPrivProto,
+                                           ss->securityPrivProtoLen);
+            dhprivKeyLen = sc_get_proper_priv_length_bytype(privtype);
+            if (USM_CREATE_USER_PRIV_DES == privtype)
+                dhprivKeyLen *= 2; /* ?? we store salt with key */
             if (get_USM_DH_key(vars, dhvar,
                                dhprivKeyLen,
                                pdu, "priv",

include/net-snmp/library/snmp_api.h
@@ -114,13 +114,13 @@ typedef struct request_list {
 #else
 #define SNMP_DEFAULT_AUTH_PROTO     usmHMACSHA1AuthProtocol
 #endif
-#define SNMP_DEFAULT_AUTH_PROTOLEN  USM_LENGTH_OID_TRANSFORM
+#define SNMP_DEFAULT_AUTH_PROTOLEN  OID_LENGTH(SNMP_DEFAULT_AUTH_PROTO)
 #ifndef NETSNMP_DISABLE_DES
 #define SNMP_DEFAULT_PRIV_PROTO     usmDESPrivProtocol
 #else
 #define SNMP_DEFAULT_PRIV_PROTO     usmAESPrivProtocol
 #endif
-#define SNMP_DEFAULT_PRIV_PROTOLEN  USM_LENGTH_OID_TRANSFORM
+#define SNMP_DEFAULT_PRIV_PROTOLEN  OID_LENGTH(SNMP_DEFAULT_PRIV_PROTO)
 
     NETSNMP_IMPORT void     snmp_set_detail(const char *);
 

include/net-snmp/library/snmpusm.h
@@ -277,10 +277,13 @@ extern          "C" {
     NETSNMP_IMPORT
     void            shutdown_usm(void);
 
-    void            usm_init_auth_types(void);
     int             usm_lookup_auth_type(const char *str);
     const char     *usm_lookup_auth_str(int value);
     oid            *usm_get_auth_oid(int auth_type, size_t *oid_len);
+
+    int             usm_lookup_priv_type(const char *str);
+    const char     *usm_lookup_priv_str(int value);
+    oid            *usm_get_priv_oid(int priv_type, size_t *oid_len);
 
 
 #define USM_CREATE_USER_AUTH_DFLT -1
@@ -293,16 +296,42 @@ extern          "C" {
 #define USM_CREATE_USER_AUTH_SHA256  NETSNMP_USMAUTH_HMAC192SHA256
 #define USM_CREATE_USER_AUTH_SHA224  NETSNMP_USMAUTH_HMAC128SHA224
 
-#define USM_CREATE_USER_PRIV_DFLT -1
-#define USM_CREATE_USER_PRIV_NONE 0
-#ifndef NETSNMP_DISABLE_DES
-#define USM_CREATE_USER_PRIV_DES  1
-#endif
-#ifdef HAVE_AES
-#define USM_CREATE_USER_PRIV_AES  2
-#endif
+    /** flags for variants fo priv algorithsm */
+#define USM_DES_FLAG_3                      0x000100
 
-    NETSNMP_IMPORT
+#define USM_AES_FLAG_192                    0x000100
+#define USM_AES_FLAG_256                    0x000200
+
+#define USM_AES_REEDER_FLAG                 0x030000
+#define USM_AES_FLAG_CISCO                  0x100000
+
+#define USM_PRIV_MASK_ALG                   0x0000ff
+#define USM_PRIV_MASK_VARIANT               0x00ff00
+
+#define USM_CREATE_USER_PRIV_DFLT          -1
+#define USM_CREATE_USER_PRIV_NONE           0
+
+#define USM_CREATE_USER_PRIV_DES            0x01
+#define USM_CREATE_USER_PRIV_3DES           \
+    (USM_CREATE_USER_PRIV_DES | USM_DES_FLAG_3)
+
+#define USM_CREATE_USER_PRIV_AES            0x02
+#define USM_CREATE_USER_PRIV_AES192         \
+    (USM_CREATE_USER_PRIV_AES | USM_AES_FLAG_192)
+#define USM_CREATE_USER_PRIV_AES256         \
+    (USM_CREATE_USER_PRIV_AES | USM_AES_FLAG_256)
+
+#define USM_CREATE_USER_PRIV_AES192_CISCO   \
+    (USM_CREATE_USER_PRIV_AES | USM_AES_FLAG_192 | USM_AES_FLAG_CISCO)
+#define USM_CREATE_USER_PRIV_AES256_CISCO   \
+    (USM_CREATE_USER_PRIV_AES | USM_AES_FLAG_256 | USM_AES_FLAG_CISCO)
+
+#define USM_CREATE_USER_PRIV_AES192_CISCO2  \
+    (USM_CREATE_USER_PRIV_AES192_CISCO | USM_AES_REEDER_FLAG)
+#define USM_CREATE_USER_PRIV_AES256_CISCO2  \
+    (USM_CREATE_USER_PRIV_AES256_CISCO | USM_AES_REEDER_FLAG)
+
+
     struct usmUser *usm_create_usmUser(const char *userName,
                                        const char *engineID, u_int flags,
                                        int authType, const char *authPass,

include/net-snmp/library/tools.h
@@ -183,17 +183,6 @@ extern          "C" {
     }                                                           \
 }
 
-
-    /*
-     * ISTRANSFORM
-     * ASSUMES the minimum length for ttype and toid.
-     */
-#define USM_LENGTH_OID_TRANSFORM	10
-
-#define ISTRANSFORM(ttype, toid)					\
-	!snmp_oid_compare(ttype, USM_LENGTH_OID_TRANSFORM,		\
-		usm ## toid ## Protocol, USM_LENGTH_OID_TRANSFORM)
-
 #define ENGINETIME_MAX	2147483647      /* ((2^31)-1) */
 #define ENGINEBOOT_MAX	2147483647      /* ((2^31)-1) */
 

include/net-snmp/library/transform_oids.h
@@ -26,11 +26,13 @@ extern          "C" {
 #define NETSNMP_USMAUTH_HMAC256SHA384     6 /* RFC 7860; OPTIONAL */
 #define NETSNMP_USMAUTH_HMAC384SHA512     7 /* RFC 7860; SHOULD */
 
-NETSNMP_IMPORT oid      usmNoAuthProtocol[10];  /* == { NETSNMP_USMAUTH_BASE,1 }; */
+NETSNMP_IMPORT oid      usmNoAuthProtocol[10];
+
 #ifndef NETSNMP_DISABLE_MD5
-NETSNMP_IMPORT oid      usmHMACMD5AuthProtocol[10];     /* == { NETSNMP_USMAUTH_BASE,2 }; */
+NETSNMP_IMPORT oid      usmHMACMD5AuthProtocol[10];
 #endif
-NETSNMP_IMPORT oid      usmHMACSHA1AuthProtocol[10];    /* == { NETSNMP_USMAUTH_BASE,3 }; */
+
+NETSNMP_IMPORT oid      usmHMACSHA1AuthProtocol[10];
 
 NETSNMP_IMPORT oid      usmHMAC128SHA224AuthProtocol[10];
 NETSNMP_IMPORT oid      usmHMAC192SHA256AuthProtocol[10];
@@ -38,31 +40,24 @@ NETSNMP_IMPORT oid      usmHMAC256SHA384AuthProtocol[10];
 NETSNMP_IMPORT oid      usmHMAC384SHA512AuthProtocol[10];
 
 /** priv */
-NETSNMP_IMPORT oid      usmNoPrivProtocol[10];  /* == { 1,3,6,1,6,3,10,1,2,1 }; */
+NETSNMP_IMPORT oid      usmNoPrivProtocol[10];
+
 #ifndef NETSNMP_DISABLE_DES
-NETSNMP_IMPORT oid      usmDESPrivProtocol[10]; /* == { 1,3,6,1,6,3,10,1,2,2 }; */
+NETSNMP_IMPORT oid      usmDESPrivProtocol[10];
 #endif
 
-NETSNMP_IMPORT oid      usmAESPrivProtocol[10]; /* == { 1,3,6,1,6,3,10,1,2,4 }; */
+NETSNMP_IMPORT oid      usmAESPrivProtocol[10];
 NETSNMP_IMPORT oid      *usmAES128PrivProtocol; /* backwards compat */
 
 #ifdef NETSNMP_DRAFT_BLUMENTHAL_AES_04
-    /* OIDs from http://www.snmp.com/eso/esoConsortiumMIB.txt */
-NETSNMP_IMPORT oid      usmAES192PrivProtocol[10]; /* == { 1,3,6,1,4,1,14832,1,3 }; */
-NETSNMP_IMPORT oid      usmAES256PrivProtocol[10]; /* == { 1,3,6,1,4,1,14832,1,4 }; */
-#endif /* NETSNMP_DRAFT_BLUMENTHAL_AES_04 */
+NETSNMP_IMPORT oid      usmAES192PrivProtocol[9];
+NETSNMP_IMPORT oid      usmAES256PrivProtocol[9];
 
-#define USM_AUTH_PROTO_NOAUTH_LEN USM_LENGTH_OID_TRANSFORM
-#define USM_AUTH_PROTO_MD5_LEN    USM_LENGTH_OID_TRANSFORM
-#define USM_AUTH_PROTO_SHA_LEN    USM_LENGTH_OID_TRANSFORM
-#define USM_PRIV_PROTO_NOPRIV_LEN USM_LENGTH_OID_TRANSFORM
-#define USM_PRIV_PROTO_DES_LEN    USM_LENGTH_OID_TRANSFORM
+NETSNMP_IMPORT oid      usmAES192CiscoPrivProtocol[11];
+NETSNMP_IMPORT oid      usmAES256CiscoPrivProtocol[11];
 
-#define USM_PRIV_PROTO_AES_LEN    USM_LENGTH_OID_TRANSFORM
-#define USM_PRIV_PROTO_AES128_LEN USM_LENGTH_OID_TRANSFORM /* backwards compat */
-#ifdef NETSNMP_DRAFT_BLUMENTHAL_AES_04
-#define USM_PRIV_PROTO_AES192_LEN 9
-#define USM_PRIV_PROTO_AES256_LEN 9
+NETSNMP_IMPORT oid      usmAES192Cisco2PrivProtocol[11];
+NETSNMP_IMPORT oid      usmAES256Cisco2PrivProtocol[11];
 #endif /* NETSNMP_DRAFT_BLUMENTHAL_AES_04 */
 
 

python/netsnmp/client_intf.c
@@ -1092,15 +1092,15 @@ netsnmp_create_session_v3(PyObject *self, PyObject *args)
   if (!strcmp(auth_proto, "MD5")) {
     session.securityAuthProto =
       snmp_duplicate_objid(usmHMACMD5AuthProtocol,
-			   USM_AUTH_PROTO_MD5_LEN);
-    session.securityAuthProtoLen = USM_AUTH_PROTO_MD5_LEN;
+                           OID_LENGTH(usmHMACMD5AuthProtocol));
+    session.securityAuthProtoLen = OID_LENGTH(usmHMACMD5AuthProtocol);
   } else
 #endif
     if (!strcmp(auth_proto, "SHA")) {
       session.securityAuthProto =
 	snmp_duplicate_objid(usmHMACSHA1AuthProtocol,
-			     USM_AUTH_PROTO_SHA_LEN);
-      session.securityAuthProtoLen = USM_AUTH_PROTO_SHA_LEN;
+                             OID_LENGTH(usmHMACSHA1AuthProtocol));
+      session.securityAuthProtoLen = OID_LENGTH(usmHMACSHA1AuthProtocol);
     } else if (!strcmp(auth_proto, "DEFAULT")) {
       const oid* a = get_default_authtype(&session.securityAuthProtoLen);
       session.securityAuthProto
@@ -1128,15 +1128,15 @@ netsnmp_create_session_v3(PyObject *self, PyObject *args)
   if (!strcmp(priv_proto, "DES")) {
     session.securityPrivProto =
       snmp_duplicate_objid(usmDESPrivProtocol,
-			   USM_PRIV_PROTO_DES_LEN);
-    session.securityPrivProtoLen = USM_PRIV_PROTO_DES_LEN;
+                           OID_LENGTH(usmDESPrivProtocol));
+    session.securityPrivProtoLen = OID_LENGTH(usmDESPrivProtocol);
   } else
 #endif
     if (!strncmp(priv_proto, "AES", 3)) {
       session.securityPrivProto =
 	snmp_duplicate_objid(usmAESPrivProtocol,
-			     USM_PRIV_PROTO_AES_LEN);
-      session.securityPrivProtoLen = USM_PRIV_PROTO_AES_LEN;
+                             OID_LENGTH(usmAESPrivProtocol));
+      session.securityPrivProtoLen = OID_LENGTH(usmAESPrivProtocol);
     } else if (!strcmp(priv_proto, "DEFAULT")) {
       const oid *p = get_default_privtype(&session.securityPrivProtoLen);
       session.securityPrivProto

commit 92012951687cb18c8e58fede015e4bc0a9222e73

libsnmp: Export sc_find_auth_alg_byoid() and sc_find_auth_alg_bytype()
Fix two typos in include/net-snmp/library/scapi.h.

Fixes: 329a9d3c9d63 ("revamp auth/priv protocol constants handling")

include/net-snmp/library/scapi.h
@@ -87,9 +87,9 @@ typedef struct netsnmp_priv_alg_info_s {
     NETSNMP_IMPORT
     oid *           sc_get_auth_oid(int auth_type, size_t *oid_len);
     NETSNMP_IMPORT
-    netsnmp_auth_alg_info * sc_get_auth_alg_byoid(const oid *oid, u_int len);
+    netsnmp_auth_alg_info * sc_find_auth_alg_byoid(const oid *oid, u_int len);
     NETSNMP_IMPORT
-    netsnmp_auth_alg_info * sc_get_auth_alg_bytype(u_int type);
+    netsnmp_auth_alg_info * sc_find_auth_alg_bytype(u_int type);
     NETSNMP_IMPORT
     netsnmp_auth_alg_info * sc_get_auth_alg_byindex(u_int index);
 