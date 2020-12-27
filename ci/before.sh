22f0630371231787f59522f4a3c4d1bb04594c66
Bart Van Assche <bvanassche@acm.org>
2020-12-26 16:19:37 -0800

    ci: Overwrite /etc/hosts
    Overwrite /etc/hosts instead of using the /etc/hosts file provided by the
    CI service. This should fix sporadic OS/X test failures.

--- ci/before.sh
+++ ci/before.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+# Instead of relying on the hosts file provided by the CI host, replace it.
+# See also
+# https://blog.justincarmony.com/2011/07/27/mac-os-x-lion-etc-hosts-bugs-and-dns-resolution/.
+sudo sh -c 'printf "127.0.0.1 ipv4-loopback\n::1 localhost ipv6-localhost ipv6-loopback\n" >/etc/hosts'
+
 case "${TRAVIS_OS_NAME}" in
     linux)
 	sudo sh -c 'apt-get install -y libmariadbclient-dev || sudo apt-get install -y libmariadb-client-lgpl-dev'
 
 	# Add an IPv6 config - see the corresponding Travis issue
 	# https://github.com/travis-ci/travis-ci/issues/8361
-	sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6; printf "\n::1 localhost ipv6-localhost ipv6-loopback\n" >>/etc/hosts';;
+	sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6';;
     osx)
 	# Upgrade openssl such that Net-SNMP can be built with Blumenthal
 	# AES support. Disabled because this upgrade takes long and even
@@ -17,11 +22,6 @@ case "${TRAVIS_OS_NAME}" in
 esac
 
 if [ -n "$CIRRUS_CI" ]; then
-    cat <<EOF >>/etc/hosts
-127.0.0.1 localhost
-::1 localhost ipv6-localhost ipv6-loopback
-EOF
-
     pkg install -y bash
     pkg install -y gawk
     pkg install -y krb5 krb5-appl krb5-devel
