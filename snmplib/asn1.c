net-snmp/net-snmp

commit a9850f4445cf4bebf8f04f0d2168042a3726fed1

asn parse: add NULL checks, check length lengths
- that is, make sure we have enough data to parse length before
  parsing length.

Signed-off-by: Robert Story <rstory@freesnmp.com>

snmplib/asn1.c
@@ -288,13 +288,88 @@ _asn_length_err(const char *str, size_t wrongsize, size_t rightsize)
 
 /**
  * @internal
+ * output an error for a wrong length
+ *
+ * @param str        error string
+ * @param wrongsize  wrong  length
+ * @param rightsize  expected length
+ */
+static
+    void
+_asn_short_err(const char *str, size_t wrongsize, size_t rightsize)
+{
+    char            ebuf[128];
+
+    snprintf(ebuf, sizeof(ebuf),
+            "%s length %lu too short: need %lu", str,
+	    (unsigned long)wrongsize, (unsigned long)rightsize);
+    ebuf[ sizeof(ebuf)-1 ] = 0;
+    ERROR_MSG(ebuf);
+}
+
+/**
+ * @internal
+ * checks a buffer with a length + data to see if it is big enough for
+ *    the length encoding and the data of the parsed length.
+ *
+ * @param IN  pkt      The buffer
+ * @param IN  pkt_len  The length of the bugger
+ * @param OUT data_len Pointer to size of data
+ *
+ * @return Pointer to start of data or NULL if pkt isn't long enough
+ *
+ * pkt = get_buf(..., &pkt_len);
+ * data = asn_parse_nlength(pkt, pkt_len, &data_len);
+ * if (NULL == data) { handle_error(); }
+ *
+ */
+u_char *
+asn_parse_nlength(u_char *pkt, size_t pkt_len, size_t *data_len)
+{
+    int len_len;
+
+    if (pkt_len < 1)
+        return NULL;               /* always too short */
+
+    if (NULL == pkt || NULL == data_len || NULL == data_len)
+        return NULL;
+
+    *data_len = 0;
+
+    if (*pkt & 0x80) {
+        /*
+         * long length; first byte is length of length (after masking high bit)
+         */
+        len_len = (int) ((*pkt & ~0x80) + 1);
+        if ((int) pkt_len <= len_len )
+            return NULL;           /* still too short for length and data */
+
+        /* now we know we have enough data to parse length */
+        asn_parse_length(pkt, data_len);
+    } else {
+        /*
+         * short length; first byte is the length
+         */
+        len_len = 1;
+        *data_len = *pkt;
+    }
+
+    if ((*data_len + len_len) > pkt_len)
+        return NULL;
+
+    return (pkt + len_len);
+}
+
+#if 0
+/**
+ * @internal
  * call after asn_parse_length to verify result.
  * 
  * @param str  error string
  * @param bufp start of buffer
  * @param data start of data
- * @param plen  ?
- * @param dlen  ?
+ * @param plen  ? parsed length
+ * @param dlen  ? data/buf length
  * 
  * @return 1 on error 0 on success
  */
@@ -325,6 +400,7 @@ _asn_parse_length_check(const char *str,
     }
     return 0;
 }
+#endif
 
 
 /**
@@ -492,20 +568,33 @@ asn_parse_int(u_char * data,
     u_long          asn_length;
     register long   value = 0;
 
+    if (NULL == data || NULL == datalength || NULL == type || NULL == intp) {
+        ERROR_MSG("parse int: NULL pointer");
+        return NULL;
+    }
+
     if (intsize != sizeof(long)) {
         _asn_size_err(errpre, intsize, sizeof(long));
         return NULL;
     }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0)  */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 2);
+        return NULL;
+    }
+
     *type = *bufp++;
     if (*type != ASN_INTEGER) {
         _asn_type_err(errpre, *type);
         return NULL;
     }
 
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check
-        (errpre, bufp, data, asn_length, *datalength))
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
         return NULL;
+    }
 
     if ((size_t) asn_length > intsize || (int) asn_length == 0) {
         _asn_length_err(errpre, (size_t) asn_length, intsize);
@@ -564,20 +653,34 @@ asn_parse_unsigned_int(u_char * data,
     u_long          asn_length;
     register u_long value = 0;
 
+    if (NULL == data || NULL == datalength || NULL == type || NULL == intp) {
+        ERROR_MSG("parse uint: NULL pointer");
+        return NULL;
+    }
+
     if (intsize != sizeof(long)) {
         _asn_size_err(errpre, intsize, sizeof(long));
         return NULL;
     }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0)  */
+    if (*datalength <= 2) {
+        _asn_short_err(errpre, *datalength, 2);
+        return NULL;
+    }
+
     *type = *bufp++;
     if (*type != ASN_COUNTER && *type != ASN_GAUGE && *type != ASN_TIMETICKS
             && *type != ASN_UINTEGER) {
         _asn_type_err(errpre, *type);
         return NULL;
     }
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check
-        (errpre, bufp, data, asn_length, *datalength))
+
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
         return NULL;
+    }
 
     if ((asn_length > (intsize + 1)) || ((int) asn_length == 0) ||
         ((asn_length == intsize + 1) && *bufp != 0x00)) {
@@ -812,6 +915,18 @@ asn_parse_string(u_char * data,
     u_char         *bufp = data;
     u_long          asn_length;
 
+    if (NULL == data || NULL == datalength || NULL == type || NULL == str ||
+        NULL == strlength) {
+        ERROR_MSG("parse string: NULL pointer");
+        return NULL;
+    }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0)  */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 2);
+        return NULL;
+    }
+
     *type = *bufp++;
     if (*type != ASN_OCTET_STR && *type != ASN_IPADDRESS && *type != ASN_OPAQUE
             && *type != ASN_NSAP) {
@@ -819,9 +934,9 @@ asn_parse_string(u_char * data,
         return NULL;
     }
 
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check
-        (errpre, bufp, data, asn_length, *datalength)) {
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
         return NULL;
     }
 
@@ -959,12 +1074,20 @@ u_char         *
 asn_parse_header(u_char * data, size_t * datalength, u_char * type)
 {
     register u_char *bufp;
-    u_long          asn_length;
+    u_long          asn_length = 0;
+    const char      *errpre = "parse header";
 
     if (!data || !datalength || !type) {
         ERROR_MSG("parse header: NULL pointer");
         return NULL;
     }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0) */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 2);
+        return NULL;
+    }
+
     bufp = data;
     /*
      * this only works on data types < 30, i.e. no extension octets 
@@ -973,12 +1096,13 @@ asn_parse_header(u_char * data, size_t * datalength, u_char * type)
         ERROR_MSG("can't process ID >= 30");
         return NULL;
     }
-    *type = *bufp;
-    bufp = asn_parse_length(bufp + 1, &asn_length);
+    *type = *bufp++;
 
-    if (_asn_parse_length_check
-        ("parse header", bufp, data, asn_length, *datalength))
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
         return NULL;
+    }
 
 #ifdef DUMP_PRINT_HEADERS
     DEBUGDUMPSETUP("recv", data, (bufp - data));
@@ -1017,10 +1141,11 @@ asn_parse_header(u_char * data, size_t * datalength, u_char * type)
         /*
          * value is encoded as special format 
          */
-        bufp = asn_parse_length(bufp + 2, &asn_length);
-        if (_asn_parse_length_check("parse opaque header", bufp, data,
-                                    asn_length, *datalength))
+        bufp = asn_parse_nlength(bufp+2, *datalength - 2, &asn_length);
+        if (NULL == bufp) {
+            _asn_short_err("parse opaque header", *datalength, asn_length);
             return NULL;
+        }
     }
 #endif                          /* NETSNMP_WITH_OPAQUE_SPECIAL_TYPES */
 
@@ -1165,6 +1290,9 @@ asn_build_sequence(u_char * data,
  *  @return Returns a pointer to the first byte after this length
  *          field (aka: the start of the data field).
  *          Returns NULL on any error.
+ *
+ * WARNING: this function does not know the length of the data
+*           buffer, so it can go past the end of a short buffer.
  */
 u_char         *
 asn_parse_length(u_char * data, u_long * length)
@@ -1331,15 +1459,28 @@ asn_parse_objid(u_char * data,
     u_long          asn_length;
     size_t          original_length = *objidlength;
 
+    if (NULL == data || NULL == datalength || NULL == type || NULL == objid ||
+        NULL == objidlength) {
+        ERROR_MSG("parse objid: NULL pointer");
+        return NULL;
+    }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0)  */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 3);
+        return NULL;
+    }
+
     *type = *bufp++;
     if (*type != ASN_OBJECT_ID) {
         _asn_type_err(errpre, *type);
         return NULL;
     }
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check("parse objid", bufp, data,
-                                asn_length, *datalength))
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
         return NULL;
+    }
 
     *datalength -= (int) asn_length + (bufp - data);
 
@@ -1613,11 +1754,23 @@ asn_parse_null(u_char * data, size_t * datalength, u_char * type)
      */
     register u_char *bufp = data;
     u_long          asn_length;
+    static const char *errpre = "parse null";
+
+    if (NULL == data || NULL == datalength || NULL == type) {
+        ERROR_MSG("parse null: NULL pointer");
+        return NULL;
+    }
+
+    /** need at least 2 bytes to work with: type, length  (which should be 0) */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 2);
+        return NULL;
+    }
 
     *type = *bufp++;
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (bufp == NULL) {
-        ERROR_MSG("parse null: bad length");
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
         return NULL;
     }
     if (asn_length != 0) {
@@ -1704,15 +1857,29 @@ asn_parse_bitstring(u_char * data,
     register u_char *bufp = data;
     u_long          asn_length;
 
+    if (NULL == data || NULL == datalength || NULL == type ||
+        NULL == str || NULL == strlength) {
+        ERROR_MSG("parse bitstring: NULL pointer");
+        return NULL;
+    }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0)  */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 3);
+        return NULL;
+    }
+
     *type = *bufp++;
     if (*type != ASN_BIT_STR) {
         _asn_type_err(errpre, *type);
         return NULL;
     }
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check(errpre, bufp, data,
-                                asn_length, *datalength))
+
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
         return NULL;
+    }
 
     if ((size_t) asn_length > *strlength) {
         _asn_length_err(errpre, (size_t) asn_length, *strlength);
@@ -1828,6 +1995,18 @@ asn_parse_unsigned_int64(u_char * data,
         _asn_size_err(errpre, countersize, sizeof(struct counter64));
         return NULL;
     }
+
+    if (NULL == data || NULL == datalength || NULL == type || NULL == cp) {
+        ERROR_MSG("parse uint64: NULL pointer");
+        return NULL;
+    }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0)  */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 3);
+        return NULL;
+    }
+
     *type = *bufp++;
     if (*type != ASN_COUNTER64
 #ifdef NETSNMP_WITH_OPAQUE_SPECIAL_TYPES
@@ -1837,10 +2016,11 @@ asn_parse_unsigned_int64(u_char * data,
         _asn_type_err(errpre, *type);
         return NULL;
     }
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check
-        (errpre, bufp, data, asn_length, *datalength))
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
         return NULL;
+    }
 
     DEBUGDUMPSETUP("recv", data, bufp - data);
 #ifdef NETSNMP_WITH_OPAQUE_SPECIAL_TYPES
@@ -1859,10 +2039,11 @@ asn_parse_unsigned_int64(u_char * data,
         /*
          * value is encoded as special format 
          */
-        bufp = asn_parse_length(bufp + 2, &asn_length);
-        if (_asn_parse_length_check("parse opaque uint64", bufp, data,
-                                    asn_length, *datalength))
+        bufp = asn_parse_nlength(bufp+2, *datalength - 2, &asn_length);
+        if (NULL == bufp) {
+            _asn_short_err("parse opaque uint64", *datalength, asn_length);
             return NULL;
+        }
     }
 #endif                          /* NETSNMP_WITH_OPAQUE_SPECIAL_TYPES */
     if (((int) asn_length > uint64sizelimit) ||
@@ -2084,11 +2265,24 @@ asn_parse_signed_int64(u_char * data,
         _asn_size_err(errpre, countersize, sizeof(struct counter64));
         return NULL;
     }
-    *type = *bufp++;
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check
-        (errpre, bufp, data, asn_length, *datalength))
+
+    if (NULL == data || NULL == datalength || NULL == type || NULL == cp) {
+        ERROR_MSG("parse int64: NULL pointer");
         return NULL;
+    }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0) */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 3);
+        return NULL;
+    }
+
+    *type = *bufp++;
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
+        return NULL;
+    }
 
     DEBUGDUMPSETUP("recv", data, bufp - data);
     if ((*type == ASN_OPAQUE) &&
@@ -2101,10 +2295,11 @@ asn_parse_signed_int64(u_char * data,
         /*
          * value is encoded as special format 
          */
-        bufp = asn_parse_length(bufp + 2, &asn_length);
-        if (_asn_parse_length_check("parse opaque int64", bufp, data,
-                                    asn_length, *datalength))
+        bufp = asn_parse_nlength(bufp+2, *datalength - 2, &asn_length);
+        if (NULL == bufp) {
+            _asn_short_err("parse opaque int64", *datalength, asn_length);
             return NULL;
+        }
     }
     /*
      * this should always have been true until snmp gets int64 PDU types 
@@ -2285,11 +2480,24 @@ asn_parse_float(u_char * data,
         _asn_size_err("parse float", floatsize, sizeof(float));
         return NULL;
     }
-    *type = *bufp++;
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check("parse float", bufp, data,
-                                asn_length, *datalength))
+
+    if (NULL == data || NULL == datalength || NULL == type || NULL == floatp) {
+        ERROR_MSG("parse float: NULL pointer");
         return NULL;
+    }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0)  */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 3);
+        return NULL;
+    }
+
+    *type = *bufp++;
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
+        return NULL;
+    }
 
     DEBUGDUMPSETUP("recv", data, bufp - data + asn_length);
     /*
@@ -2302,11 +2510,11 @@ asn_parse_float(u_char * data,
         /*
          * value is encoded as special format 
          */
-        bufp = asn_parse_length(bufp + 2, &asn_length);
-        if (_asn_parse_length_check("parse opaque float", bufp, data,
-                                    asn_length, *datalength))
+        bufp = asn_parse_nlength(bufp+2, *datalength - 2, &asn_length);
+        if (NULL == bufp) {
+            _asn_short_err("parse opaque float", *datalength, asn_length);
             return NULL;
-
+        }
         /*
          * change type to Float 
          */
@@ -2458,11 +2666,24 @@ asn_parse_double(u_char * data,
         _asn_size_err("parse double", doublesize, sizeof(double));
         return NULL;
     }
-    *type = *bufp++;
-    bufp = asn_parse_length(bufp, &asn_length);
-    if (_asn_parse_length_check("parse double", bufp, data,
-                                asn_length, *datalength))
+
+    if (NULL == data || NULL == datalength || NULL == type || NULL == doublep) {
+        ERROR_MSG("parse double: NULL pointer");
         return NULL;
+    }
+
+    /** need at least 2 bytes to work with: type, length (which might be 0)  */
+    if (*datalength < 2) {
+        _asn_short_err(errpre, *datalength, 3);
+        return NULL;
+    }
+
+    *type = *bufp++;
+    bufp = asn_parse_nlength(bufp, *datalength - 1, &asn_length);
+    if (NULL == bufp) {
+        _asn_short_err(errpre, *datalength, asn_length);
+        return NULL;
+    }
 
     DEBUGDUMPSETUP("recv", data, bufp - data + asn_length);
     /*
@@ -2475,10 +2696,11 @@ asn_parse_double(u_char * data,
         /*
          * value is encoded as special format 
          */
-        bufp = asn_parse_length(bufp + 2, &asn_length);
-        if (_asn_parse_length_check("parse opaque double", bufp, data,
-                                    asn_length, *datalength))
+        bufp = asn_parse_nlength(bufp+2, *datalength - 2, &asn_length);
+        if (NULL == bufp) {
+            _asn_short_err("parse opaque double", *datalength, asn_length);
             return NULL;
+        }
 
         /*
          * change type to Double 

commit 92f0fe9e0dc3cf7ab6e8cc94d7962df83d0ddbec

libsnmp: Fix asn_parse_nlength()
Handle length zero correctly.

Fixes: https://github.com/net-snmp/net-snmp/issues/253
Fixes: a9850f4445cf ("asn parse: add NULL checks, check length lengths")

snmplib/asn1.c
@@ -345,7 +345,7 @@ asn_parse_nlength(u_char *pkt, size_t pkt_len, u_long *data_len)
          * long length; first byte is length of length (after masking high bit)
          */
         len_len = (int) ((*pkt & ~0x80) + 1);
-        if ((int) pkt_len <= len_len )
+        if (pkt_len < len_len)
             return NULL;           /* still too short for length and data */
 
         /* now we know we have enough data to parse length */