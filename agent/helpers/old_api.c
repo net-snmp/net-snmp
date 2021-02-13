net-snmp/net-snmp

commit c308ba800be4395223240d336a79655ef3567d81

agent: expose netsnmp_agent_request_info_s before snmp feeding
The snmp api v4 API to register mib does not provide any parameter
to know which session the mib is for. One way to mitigate this
limitation is to create a callback service, so that the
netsnmp_agent_request_info_s is available. This permits to know the used
netsnmp_agent_session_s, and the underlying netsnmp_session used.

This solution is not perfect, but may be a correct trade off, assuming that
the transition from old v4 API code to new v5 API may be not neglectible.

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

agent/helpers/old_api.c
@@ -289,6 +289,10 @@ netsnmp_old_api_helper(netsnmp_mib_handler *handler,
 
     vp = (struct variable *) handler->myvoid;
 
+    if (reqinfo)
+      snmp_call_callbacks(SNMP_CALLBACK_LIBRARY,
+			  SNMP_CALLBACK_MIB_REQUEST_INFO,
+			  reqinfo);
     /*
      * create old variable structure with right information 
      */

include/net-snmp/library/callback.h
@@ -29,6 +29,7 @@ extern          "C" {
 #define SNMP_CALLBACK_SESSION_INIT		5
 #define SNMP_CALLBACK_PRE_READ_CONFIG	        7
 #define SNMP_CALLBACK_PRE_PREMIB_READ_CONFIG	8
+#define SNMP_CALLBACK_MIB_REQUEST_INFO		9
 
 
     /*

commit 4fd73557818e26511e4c793df48beed50c867245

snmpd: Remove a superfluous pointer check
This patch fixes the following Coverity complaint:

CID 366544 (#1 of 1): Dereference after null check (FORWARD_NULL)
3. var_deref_op: Dereferencing null pointer reqinfo.

Fixes: c308ba800be4 ("agent: expose netsnmp_agent_request_info_s before snmp feeding")

agent/helpers/old_api.c
@@ -289,10 +289,9 @@ netsnmp_old_api_helper(netsnmp_mib_handler *handler,
 
     vp = (struct variable *) handler->myvoid;
 
-    if (reqinfo)
-      snmp_call_callbacks(SNMP_CALLBACK_LIBRARY,
-			  SNMP_CALLBACK_MIB_REQUEST_INFO,
-			  reqinfo);
+    snmp_call_callbacks(SNMP_CALLBACK_LIBRARY,
+                        SNMP_CALLBACK_MIB_REQUEST_INFO,
+                        reqinfo);
     /*
      * create old variable structure with right information 
      */