net-snmp/net-snmp

commit aed4ba2b896c17c36cfc55ee5b6beb6a792be6bd

declarations for a generic factory
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7277 06827809-a52a-0410-b366-d66718629ded

include/net-snmp/library/factory.h
@@ -1,0 +1,72 @@
+#ifndef NETSNMP_FACTORY_H
+#define NETSNMP_FACTORY_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    typedef void * (netsnmp_factory_produce_f)(void);
+    typedef int (netsnmp_factory_produce_noalloc_f)(void *);
+
+    typedef struct netsnmp_factory_s {
+        /*
+         * a string describing the product the factory creates
+         */
+        const char                           *product;
+
+        /*
+         * a function to create an object in newly allcoated memory
+         */
+        netsnmp_factory_produce_f            *produce;
+
+        /*
+         * a function to create an object in previously allcoated memory
+         */
+        netsnmp_factory_produce_noalloc_f    *produce_noalloc;
+
+    } netsnmp_factory;
+
+    /*
+     * init factory registry
+     */
+    void netsnmp_factory_init(void);
+
+    /*
+     * register a factory type
+     */
+    int  netsnmp_factory_register(netsnmp_factory *f);
+
+    /*
+     * get a factory
+     */
+    netsnmp_factory* netsnmp_factory_get(const char* product);
+
+    /*
+     * ask a factory to produce an object
+     */
+    void * netsnmp_factory_produce(const char* product);
+
+    /*
+     * ask a factory to produce an object in the provided memory
+     */
+    int netsnmp_factory_produce_noalloc(const char *product, void *memory);
+
+    /*
+     * factory return codes
+     */
+    enum {
+        FACTORY_NOERROR = 0,
+        FACTORY_EXISTS,
+        FACTORY_NOTFOUND,
+        FACTORY_NOMEMORY,
+        FACTORY_GENERR,
+        FACTORY_MAXIMUM_ERROR
+    };
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif /* NETSNMP_FACTORY_H */

commit c576e7dd3bec2086d55b5261566a267942dd704e

include/net-snmp/library/factory.h: Fix a typo in a source code comment
Fixes: aed4ba2b896c ("declarations for a generic factory")

include/net-snmp/library/factory.h
@@ -16,7 +16,7 @@ extern "C" {
         const char                           *product;
 
         /*
-         * a function to create an object in newly allcoated memory
+         * a function to create an object in newly allocated memory
          */
         netsnmp_factory_produce_f            *produce;
 