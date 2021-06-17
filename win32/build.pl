net-snmp/net-snmp

commit bbbf866c1c261615e71e52ad4cdfcac2504c92fb

Windows: Always use the 'Platform SDK'
The 'Platform SDK' is an optional download for Visual Studio 6, released in
1998. Since that version of Visual Studio is no longer supported, remove the
corresponding menu option.

win32/build.pl
@@ -28,7 +28,7 @@ my $default_openssllibdir = $default_openssldir . "\\lib\\VC";
 my $openssllibdir = $default_openssllibdir;
 my $b_ipv6 = false;
 my $b_winextdll = false;
-my $sdk = false;
+my $sdk = true;
 my $default_install_base = "c:/usr";
 my $install_base = $default_install_base;
 my $install = true;
@@ -58,23 +58,22 @@ while (1) {
   print "1.  OpenSSL support:                " . ($openssl ? "enabled" : "disabled"). "\n";
   print "2.  OpenSSL include directory:      " . $opensslincdir. "\n";
   print "3.  OpenSSL library director:       " . $openssllibdir. "\n";
-  print "4.  Platform SDK support:           " . ($sdk ? "enabled" : "disabled") . "\n";
   print "\n";
-  print "5.  Install path:                   " . $install_base . "\n";
-  print "6.  Install after build:            " . ($install ? "enabled" : "disabled") . "\n";
+  print "4.  Install path:                   " . $install_base . "\n";
+  print "5.  Install after build:            " . ($install ? "enabled" : "disabled") . "\n";
   print "\n";
-  print "7.  Perl modules:                   " . ($perl ? "enabled" : "disabled") . "\n";
-  print "8.  Install perl modules:           " . ($perl_install ? "enabled" : "disabled") . "\n";
+  print "6.  Perl modules:                   " . ($perl ? "enabled" : "disabled") . "\n";
+  print "7.  Install perl modules:           " . ($perl_install ? "enabled" : "disabled") . "\n";
   print "\n";
-  print "9.  Quiet build (logged):           " . ($logging ? "enabled" : "disabled") . "\n";
-  print "10. Debug mode:                     " . ($debug ? "enabled" : "disabled") . "\n";
+  print "8.  Quiet build (logged):           " . ($logging ? "enabled" : "disabled") . "\n";
+  print "9. Debug mode:                     " . ($debug ? "enabled" : "disabled") . "\n";
   print "\n";
-  print "11. IPv6 transports (requires SDK): " . ($b_ipv6 ? "enabled" : "disabled") . "\n";
-  print "12. winExtDLL agent (requires SDK): " . ($b_winextdll ? "enabled" : "disabled") . "\n";
+  print "10. IPv6 transports (requires SDK): " . ($b_ipv6 ? "enabled" : "disabled") . "\n";
+  print "11. winExtDLL agent (requires SDK): " . ($b_winextdll ? "enabled" : "disabled") . "\n";
   print "\n";
-  print "13. Link type:                      " . ($link_dynamic ? "dynamic" : "static") . "\n";
+  print "12. Link type:                      " . ($link_dynamic ? "dynamic" : "static") . "\n";
   print "\n";
-  print "14. Install development files       " . ($install_devel ? "enabled" : "disabled") . "\n";
+  print "13. Install development files       " . ($install_devel ? "enabled" : "disabled") . "\n";
   print "\nF.  Finished - start build\n";
   print "Q.  Quit - abort build\n\n";
   print "Select option to set / toggle: ";

commit 3fb026f068c3010b29c0be44998fae3d93feccbc

win32/build.pl: Fix menu system
Fixes: bbbf866c1c26 ("Windows: Always use the 'Platform SDK'")

win32/build.pl
@@ -94,48 +94,45 @@ while (1) {
     $openssllibdir =~ s/\\/\//g;
     $openssllibdir = $default_openssllibdir if ($openssllibdir eq "");
   }
-  elsif ($option eq "4") {
-    $sdk = !$sdk;
-  }
-  elsif ($option eq "11") {
+  elsif ($option eq "10") {
     $b_ipv6 = !$b_ipv6;
     if ($b_ipv6 && !$sdk) {
       print "\n\n* SDK required for IPv6 and has been automatically enabled";
       $sdk = true;
     }
   }
-  elsif ($option eq "12") {
+  elsif ($option eq "11") {
     $b_winextdll = !$b_winextdll;
     if ($b_winextdll && !$sdk) {
       print "\n\n* SDK required for IPv6 and has been automatically enabled";
       $sdk = true;
     }
   }
-  elsif ($option eq "5") {
+  elsif ($option eq "4") {
     print "Please enter the new install path [$default_install_base]: ";
     chomp ($install_base = <>);
     $install_base =~ s/\\/\//g;
     $install_base = $default_install_base if ($install_base eq "");
   }
-  elsif ($option eq "6") {
+  elsif ($option eq "5") {
     $install = !$install;
   }
-  elsif ($option eq "14") {
+  elsif ($option eq "13") {
     $install_devel = !$install_devel;
   }
-  elsif ($option eq "7") {
+  elsif ($option eq "6") {
     $perl = !$perl;
   }
-  elsif ($option eq "8") {
+  elsif ($option eq "7") {
     $perl_install = !$perl_install;
   }
-  elsif ($option eq "9") {
+  elsif ($option eq "8") {
     $logging = !$logging;
   }
-  elsif ($option eq "10") {
+  elsif ($option eq "9") {
     $debug = !$debug;
   }
-  elsif ($option eq "13") {
+  elsif ($option eq "12") {
     $link_dynamic = !$link_dynamic;
   }
   elsif (lc($option) eq "f") {