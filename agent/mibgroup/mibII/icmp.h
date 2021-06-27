f84d62d3b38251fa5e01c2cc95e12e139474e3c4
Bart Van Assche <bvanassche@acm.org>
2021-06-27 09:44:53 -0700

    Darwin: MIB-II: Include mibII/kernel_sysctl for kernel versions >= 10
    This patch breaks support for Darwin kernel versions < 10. If it ever would
    be necessary to restore support for Darwin kernel versions < 10 that can be
    done by modifying the agent/mibgroup/mibII/kernel_sysctl.c source file.
    
    Fixes: f01c45c53cdc ("Darwin: Unify system header files")
    Fixes: https://github.com/net-snmp/net-snmp/issues/226

--- agent/mibgroup/mibII/icmp.h
+++ agent/mibgroup/mibII/icmp.h
@@ -10,7 +10,7 @@ config_require(kernel_sunos5)
 #elif defined(linux)
 config_require(mibII/kernel_linux)
 #elif defined(freebsd4) || defined(openbsd4) || defined(dragonfly2) || \
-    defined(darwin10)
+    defined(darwin)
 config_require(mibII/kernel_sysctl)
 #elif defined(netbsd) || defined(netbsdelf)
 config_require(mibII/kernel_netbsd)
