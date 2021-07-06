net-snmp/net-snmp

commit e772b8017d7e235437a45622bf8aa05f3ef3d084

fix issue #99: read stored ifXTable config at startup

agent/mibgroup/if-mib/ifTable/ifTable.c
@@ -109,7 +109,7 @@ void
 init_ifTable(void)
 {
     snmp_register_callback(SNMP_CALLBACK_LIBRARY,
-                           SNMP_CALLBACK_POST_READ_CONFIG,
+                           SNMP_CALLBACK_PRE_READ_CONFIG,
                            _init_ifTable, NULL);
 }
 

commit 829445d1478bc86f627d019e4debf6df00dc9786

IF-MIB: Fix a source code comment
Fixes: e772b8017d7e ("fix issue #99: read stored ifXTable config at startup")

agent/mibgroup/if-mib/ifTable/ifTable.c
@@ -59,7 +59,7 @@ _if_number_handler(netsnmp_mib_handler *handler,
 
 
 /**
- * Initializes the ifTable module. Called after the snmpd configuration has
+ * Initializes the ifTable module. Called before the snmpd configuration has
  * been read.
  */
 static int