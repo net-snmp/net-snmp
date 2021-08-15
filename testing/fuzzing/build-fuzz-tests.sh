e15203778d7f1fcfbcff20f0c29e5a61548b3a51
Bart Van Assche <bvanassche@acm.org>
2021-08-15 12:33:15 -0700

    testing/fuzzing/build-fuzz-tests.sh: Fix krb5 compatibility

--- testing/fuzzing/build-fuzz-tests.sh
+++ testing/fuzzing/build-fuzz-tests.sh
@@ -13,11 +13,15 @@ fuzzers=(
     snmp_pdu_parse
     snmp_scoped_pdu_parse
 )
+krb5_libs=""
+if type -p krb5-config >&/dev/null; then
+    krb5_libs=$(krb5-config --libs)
+fi
 for fuzzname in "${fuzzers[@]}"; do
   $CC $CFLAGS -c -Iinclude -Iagent/mibgroup/agentx $SRC/${fuzzname}_fuzzer.c -o $WORK/${fuzzname}_fuzzer.o
   $CXX $CXXFLAGS $WORK/${fuzzname}_fuzzer.o \
         $LIB_FUZZING_ENGINE snmplib/.libs/libnetsnmp.a \
         agent/.libs/libnetsnmpagent.a \
-        -lcrypto -lm \
+        -Wl,-no-undefined ${krb5_libs} -lcrypto -lm \
         -o $OUT/${fuzzname}_fuzzer
 done
