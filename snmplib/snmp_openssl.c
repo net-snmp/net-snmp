19e75743173cb8d49d49fd685b8e0249e83cc820
Bart Van Assche <bvanassche@acm.org>
2021-09-08 20:39:42 -0700

    libsnmp: Fix the build against OpenSSL 3.0
    Fixes: https://github.com/net-snmp/net-snmp/issues/343

--- snmplib/snmp_openssl.c
+++ snmplib/snmp_openssl.c
@@ -899,6 +899,11 @@ netsnmp_openssl_cert_issued_by(X509 *issuer, X509 *cert)
 
 
 #ifndef NETSNMP_FEATURE_REMOVE_OPENSSL_ERR_LOG
+#ifndef ERR_GET_FUNC
+/* removed in OpenSSL 3.0 */
+#define ERR_GET_FUNC(e) -1
+#endif
+
 void
 netsnmp_openssl_err_log(const char *prefix)
 {
