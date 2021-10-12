eb75d03aae27aa34b5107b9b3e93f97f35f3025d
David Korczynski <david@adalogics.com>
2021-10-12 15:41:34 +0100

    testing/fuzzing: Add agent end-to-end fuzzer
    Add an end-to-end fuzzer focused on the agent code. More work can be
    done on this fuzzer to exercise more code and this is a goal for the
    near future.
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- testing/fuzzing/build-fuzz-tests.sh
+++ testing/fuzzing/build-fuzz-tests.sh
@@ -9,7 +9,10 @@ for fuzzname in testing/fuzzing/*_fuzzer.c; do
     $CC $CFLAGS -c -Iinclude -Iagent/mibgroup/agentx \
 	testing/fuzzing/${fuzzname}_fuzzer.c -o $WORK/${fuzzname}_fuzzer.o
     $CXX $CXXFLAGS $WORK/${fuzzname}_fuzzer.o \
-        $LIB_FUZZING_ENGINE snmplib/.libs/libnetsnmp.a \
-        agent/.libs/libnetsnmpagent.a ${libs} \
+        $LIB_FUZZING_ENGINE -Wl,--start-group snmplib/.libs/libnetsnmp.a \
+        agent/helpers/.libs/libnetsnmphelpers.a \
+        agent/.libs/libnetsnmpmibs.a \
+        apps/.libs/libnetsnmptrapd.a \
+        agent/.libs/libnetsnmpagent.a -Wl,--end-group ${libs} \
         -o "$OUT/${fuzzname}_fuzzer"
 done
