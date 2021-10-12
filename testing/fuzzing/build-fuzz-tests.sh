803ab9347edf968b90a2acb53a46bb4e017e0f4a
David Korczynski <david@adalogics.com>
2021-10-12 15:41:34 +0100

    testing/fuzzing: Add agent end-to-end fuzzer
    Add an end-to-end fuzzer focused on the agent code. More work can be
    done on this fuzzer to exercise more code and this is a goal for the
    near future.
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    [ bvanassche: fixed the build-fuzz-tests.sh script ]

--- testing/fuzzing/build-fuzz-tests.sh
+++ testing/fuzzing/build-fuzz-tests.sh
@@ -2,14 +2,19 @@
 
 # build fuzzers. To do: switch from dynamic to static linking for external
 # libraries.
-libs=$(./net-snmp-config --external-libs)
+libs=$(sed -n 's/^NSC_LNETSNMPLIBS="\(.*\)"$/\1/p' ./net-snmp-config;
+       sed -n "s/^PERLLDOPTS_FOR_LIBS='\(.*\)'/\1/p" ./config.log)
 for fuzzname in testing/fuzzing/*_fuzzer.c; do
     fuzzname=${fuzzname%_fuzzer.c}
     fuzzname=${fuzzname#testing/fuzzing/}
     $CC $CFLAGS -c -Iinclude -Iagent/mibgroup/agentx \
 	testing/fuzzing/${fuzzname}_fuzzer.c -o $WORK/${fuzzname}_fuzzer.o
     $CXX $CXXFLAGS $WORK/${fuzzname}_fuzzer.o \
-        $LIB_FUZZING_ENGINE snmplib/.libs/libnetsnmp.a \
-        agent/.libs/libnetsnmpagent.a ${libs} \
+        $LIB_FUZZING_ENGINE \
+        apps/.libs/libnetsnmptrapd.a \
+        agent/.libs/libnetsnmpagent.a \
+        agent/.libs/libnetsnmpmibs.a \
+        agent/helpers/.libs/libnetsnmphelpers.a \
+	snmplib/.libs/libnetsnmp.a ${libs} \
         -o "$OUT/${fuzzname}_fuzzer"
 done
