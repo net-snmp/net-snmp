net-snmp/net-snmp

commit b4ec525601709d4004d5c0746f0d6f62111b5006

libsnmp: Port to OpenSSL 1.1.1e
See also OpenSSL commit b13342e933c5 ("Modernise the ERR functionality
further (new functions and deprecations)").

snmplib/transports/snmpTLSBaseDomain.c
@@ -57,6 +57,17 @@ netsnmp_feature_require(cert_util);
 
 int openssl_local_index;
 
+#ifndef HAVE_ERR_GET_ERROR_ALL
+/* A backport of the OpenSSL 1.1.1e ERR_get_error_all() function. */
+static unsigned long ERR_get_error_all(const char **file, int *line,
+                                       const char **func,
+                                       const char **data, int *flags)
+{
+    *func = NULL;
+    return ERR_get_error_line_data(file, line, data, flags);
+}
+#endif
+
 /* this is called during negotiation */
 int verify_callback(int ok, X509_STORE_CTX *ctx) {
     int err, depth;
@@ -1104,7 +1115,7 @@ const char * _x509_get_error(int x509failvalue, const char *location) {
 #endif /* NETSNMP_FEATURE_REMOVE__X509_GET_ERROR */
 
 void _openssl_log_error(int rc, SSL *con, const char *location) {
-    const char     *reason, *file, *data;
+    const char     *reason, *file, *func, *data;
     unsigned long   numerical_reason;
     int             flags, line;
 
@@ -1170,9 +1181,9 @@ void _openssl_log_error(int rc, SSL *con, const char *location) {
 
     /* other errors */
     while ((numerical_reason =
-            ERR_get_error_line_data(&file, &line, &data, &flags)) != 0) {
-        snmp_log(LOG_ERR, " error: #%lu (file %s, line %d)\n",
-                 numerical_reason, file, line);
+            ERR_get_error_all(&file, &line, &func, &data, &flags)) != 0) {
+        snmp_log(LOG_ERR, "%s (file %s, func %s, line %d)\n",
+                 ERR_error_string(numerical_reason, NULL), file, func, line);
 
         /* if we have a text translation: */
         if (data && (flags & ERR_TXT_STRING)) {

configure
@@ -24446,7 +24446,7 @@ _ACEOF
         fi
         netsnmp_save_LIBS="$LIBS"
         LIBS="-lssl"
-        for ac_func in TLS_method TLSv1_method DTLS_method DTLSv1_method                       SSL_library_init SSL_load_error_strings
+        for ac_func in TLS_method TLSv1_method DTLS_method DTLSv1_method                       SSL_library_init SSL_load_error_strings		       ERR_get_error_all
 do :
   as_ac_var=`$as_echo "ac_cv_func_$ac_func" | $as_tr_sh`
 ac_fn_c_check_func "$LINENO" "$ac_func" "$as_ac_var"

configure.d/config_os_libs2
@@ -344,7 +344,8 @@ if test "x$tryopenssl" != "xno" -a "x$tryopenssl" != "xinternal"; then
         netsnmp_save_LIBS="$LIBS"
         LIBS="-lssl"
         AC_CHECK_FUNCS([TLS_method TLSv1_method DTLS_method DTLSv1_method]dnl
-                       [SSL_library_init SSL_load_error_strings])
+                       [SSL_library_init SSL_load_error_strings]dnl
+		       [ERR_get_error_all])
         LIBS="$netsnmp_save_LIBS"
         if echo " $transport_result_list " | $GREP "DTLS" > /dev/null; then
 	    if test x$ac_cv_func_DTLSv1_method = xyes -o \

include/net-snmp/net-snmp-config.h.in
@@ -175,6 +175,9 @@
 /* Define to 1 if you have the `DTLS_method' function. */
 #undef HAVE_DTLS_METHOD
 
+/* Define to 1 if you have the `ERR_get_error_all' function. */
+#undef HAVE_ERR_GET_ERROR_ALL
+
 /* Define to 1 if you have the <err.h> header file. */
 #undef HAVE_ERR_H
 

commit e49f81d24299f5196ac06b74ff54595f4fb7ed55

libsnmp: Fix a compiler warning
Fix the following compiler warning:

transports/snmpTLSBaseDomain.c: In function ‘_openssl_log_error’:
transports/snmpTLSBaseDomain.c:1194:9: warning: ‘%s’ directive argument is null [-Wformat-overflow=]
 1194 |         snmp_log(LOG_ERR, "%s (file %s, func %s, line %d)\n",
      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 1195 |                  ERR_error_string(numerical_reason, NULL), file, func, line);
      |                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Fixes: b4ec52560170 ("libsnmp: Port to OpenSSL 1.1.1e")

snmplib/transports/snmpTLSBaseDomain.c
@@ -60,7 +60,7 @@ static unsigned long ERR_get_error_all(const char **file, int *line,
                                        const char **func,
                                        const char **data, int *flags)
 {
-    *func = NULL;
+    *func = "(?)";
     return ERR_get_error_line_data(file, line, data, flags);
 }
 #endif