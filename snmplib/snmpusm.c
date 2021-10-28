net-snmp/net-snmp

commit 9d7b9669a57757ba1b01d67cc0658b04002698be

snmpd: Fix memory leaks in USM and DH algorithms
[ bvanassche: made small additional changes and edited the patch description ]

snmplib/snmpusm.c
@@ -4,7 +4,7 @@
  */
 /*
  * Portions of this file are copyrighted by:
- * Copyright © 2003 Sun Microsystems, Inc. All rights reserved.
+ * Copyright Â© 2003 Sun Microsystems, Inc. All rights reserved.
  * Use is subject to license terms specified in the COPYING file
  * distributed with the Net-SNMP package.
  *
@@ -75,6 +75,8 @@
 #include <net-snmp/library/snmpusm.h>
 #include <net-snmp/library/transform_oids.h>
 #include <net-snmp/library/snmp_enum.h>
+
+#include <openssl/dh.h>
 
 netsnmp_feature_child_of(usm_all, libnetsnmp);
 netsnmp_feature_child_of(usm_support, usm_all);
@@ -762,14 +764,25 @@ usm_free_user(struct usmUser *user)
         SNMP_FREE(user->privKeyKu);
     }
 
+    if (user->usmDHUserAuthKeyChange)
+    {
+        DH_free(user->usmDHUserAuthKeyChange);
+        user->usmDHUserAuthKeyChange = NULL;
+    }
+
+    if (user->usmDHUserPrivKeyChange)
+    {
+        DH_free(user->usmDHUserPrivKeyChange);
+        user->usmDHUserPrivKeyChange = NULL;
+    }
 
     /*
      * FIX  Why not put this check *first?*
      */
-    if (user->prev != NULL) {   /* ack, this shouldn't happen */
+    if (user->prev != NULL && user->prev != (struct usmUser *)-1) {   /* ack, this shouldn't happen */
         user->prev->next = user->next;
     }
-    if (user->next != NULL) {
+    if (user->next != NULL && user->next != (struct usmUser *)-1) {
         user->next->prev = user->prev;
         if (user->prev != NULL) /* ack this is really bad, because it means
                                  * * we'll loose the head of some structure tree */

agent/agent_trap.c
@@ -1662,6 +1662,7 @@ netsnmp_create_v3user_notification_session(const char *dest, const char *user,
     /** free any allocated mem in session */
     SNMP_FREE(session.securityAuthProto);
     SNMP_FREE(session.securityPrivProto);
+    SNMP_FREE(session.contextEngineID);
 
     return ss;
 }

agent/mibgroup/snmp-usm-dh-objects-mib/usmDHUserKeyTable/usmDHUserKeyTable_data_get.c
@@ -62,8 +62,8 @@ int
 usmDHGetUserKeyChange(struct usmUser *user, int for_auth_key,
                       u_char **keyobj, size_t *keyobj_len)
 {
-    DH             *dh;
-    const BIGNUM   *pub_key;
+    DH             *dh = NULL;
+    const BIGNUM   *pub_key = NULL;
 
     dh = usmDHGetUserDHptr(user, for_auth_key);
 
@@ -155,15 +155,7 @@ usmDHUserKeyTable_release_data(usmDHUserKeyTable_data * data)
     netsnmp_assert(user->next == (struct usmUser *) -1);
     netsnmp_assert(user->prev == (struct usmUser *) -1);
 
-    /*
-     * TODO:202:r: |-> release memory for the usmDHUserKeyTable data context.
-     */
-    if (user) {
-        SNMP_FREE(user->authKey);
-        SNMP_FREE(user->privKey);
-    }
-
-    free(data);
+    usm_free_user(user);
 }                               /* usmDHUserKeyTable_release_data */
 
 

agent/mibgroup/snmp-usm-dh-objects-mib/usmDHUserKeyTable/usmDHUserKeyTable_data_set.c
@@ -28,8 +28,9 @@ usmDHUserCheckValue(struct usmUser *user, int for_auth_key,
      * The set value must be composed of 2 parts, the first being the
      * current value 
      */
-    u_char         *current_value;
-    size_t          current_value_len;
+    u_char         *current_value = NULL;
+    size_t          current_value_len = 0;
+    int             ret = MFD_SUCCESS;
 
     DEBUGMSGTL(("verbose:usmDHUserKeyTable:usmDHUserCheckValue",
                 "called\n"));
@@ -40,22 +41,23 @@ usmDHUserCheckValue(struct usmUser *user, int for_auth_key,
         return MFD_ERROR;
 
     if (val_len != current_value_len * 2)
-        return MFD_NOT_VALID_NOW;
+        ret = MFD_NOT_VALID_NOW;
 
-    if (memcmp(current_value, val, current_value_len) != 0)
-        return SNMP_ERR_WRONGVALUE;     /* mandated error string */
+    else if (memcmp(current_value, val, current_value_len) != 0)
+        ret = SNMP_ERR_WRONGVALUE;     /* mandated error string */
 
-    return MFD_SUCCESS;
+    SNMP_FREE(current_value);
+    return ret;
 }
 
 int
 usmDHSetKey(struct usmUser *user, int for_auth_key,
             u_char *val, size_t val_len)
 {
-    DH             *dh;
-    BIGNUM         *other_pub;
-    u_char         *key;
-    size_t          key_len;
+    DH             *dh = NULL;
+    BIGNUM         *other_pub = NULL;
+    u_char         *key = NULL;
+    size_t          key_len = 0;
 
     DEBUGMSGTL(("verbose:usmDHUserKeyTable:usmDHSetKey", "called\n"));
     /*
@@ -76,11 +78,16 @@ usmDHSetKey(struct usmUser *user, int for_auth_key,
     key_len = DH_size(dh);
     key = malloc(DH_size(dh));
     if (!key)
+    {
+        BN_clear_free(other_pub);
         return MFD_ERROR;
+    }
 
     if (DH_compute_key(key, other_pub, dh)) {
         u_char        **replkey;
         size_t          replkey_size;
+
+        BN_clear_free(other_pub);
 
         if (for_auth_key) {
             replkey_size = user->authKeyLen;
@@ -94,16 +101,22 @@ usmDHSetKey(struct usmUser *user, int for_auth_key,
          * is it large enough? 
          */
         if (key_len < replkey_size)
+        {
+            SNMP_FREE(key);
             return MFD_ERROR;
+        }
 
         /*
          * copy right most bits, per the object requirements 
          */
         SNMP_FREE(*replkey);
         *replkey = netsnmp_memdup(key + key_len - replkey_size, replkey_size);
+        SNMP_FREE(key);
 
         return MFD_SUCCESS;
     }
+    SNMP_FREE(other_pub);
+    SNMP_FREE(key);
 
     return MFD_ERROR;
 }

commit 17a1691b66b96ed7b5b863621966871bc66047e3

libsnmp: Unbreak the build if OpenSSL support is disabled
Fixes: 9d7b9669a577 ("snmpd: Fix memory leaks in USM and DH algorithms")

snmplib/snmpusm.c
@@ -766,6 +766,7 @@ usm_free_user(struct usmUser *user)
         SNMP_FREE(user->privKeyKu);
     }
 
+#ifdef NETSNMP_USE_OPENSSL
     if (user->usmDHUserAuthKeyChange)
     {
         DH_free(user->usmDHUserAuthKeyChange);
@@ -777,6 +778,7 @@ usm_free_user(struct usmUser *user)
         DH_free(user->usmDHUserPrivKeyChange);
         user->usmDHUserPrivKeyChange = NULL;
     }
+#endif
 
     /*
      * FIX  Why not put this check *first?*