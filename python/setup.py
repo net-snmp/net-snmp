467324982ef0040df5dbd3adec6a18caccb5f35b
Bart Van Assche <bvanassche@acm.org>
2022-07-30 15:47:38 -0700

    python/setup.py: Pass -D and -L options to the module build system

--- python/setup.py
+++ python/setup.py
@@ -5,6 +5,8 @@ import sys
 
 intree=0
 basedir = ""
+defines = []
+
 args = sys.argv[:]
 for arg in args:
     if arg.find('--basedir=') == 0:
@@ -13,8 +15,20 @@ for arg in args:
         intree=1
 
 netsnmp_libs = os.popen(basedir + 'net-snmp-config --libs').read()
+base_cflags = os.popen(basedir + 'net-snmp-config --base-cflags').read()
+for opt in base_cflags.split(' '):
+    if opt[0:2] == "-D":
+        e = opt[2:].split('=')
+        if e[0] != "linux":
+            if len(e) == 1:
+                defines += [(e[0], 1)]
+            else:
+                defines += [(e[0], e[1])]
+
 if intree:
     libdir = os.popen(basedir + 'net-snmp-config --build-lib-dirs ' + basedir).read()
+    libdir += " "
+    libdir += os.popen(basedir + 'net-snmp-config --ldflags').read()
     incdir = os.popen(basedir + 'net-snmp-config --build-includes ' + basedir).read()
     incdir += " "
     incdir += os.popen(basedir + 'net-snmp-config --base-cflags ').read()
@@ -38,6 +52,7 @@ setup(
 
     ext_modules = [
        Extension("netsnmp.client_intf", ["netsnmp/client_intf.c"],
+                 define_macros=defines,
                  library_dirs=libdirs,
                  include_dirs=incdirs,
                  libraries=libs )
