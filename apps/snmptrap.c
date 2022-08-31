net-snmp/net-snmp

commit 0b5e7d6f1532799865df5fde8ed35c02680e716d

apps: Use netsnmp_cleanup_session()
Free the memory allocated by netsnmp_parse_args() before exiting. This reduces
the number of memory leaks reported by tools like LeakSanitizer.

Ensure that netsnmp_parse_args() only stores pointers to dynamically allocated
memory in the session object.

apps/snmptrap.c
@@ -381,6 +381,8 @@ close_session:
     snmp_shutdown(NETSNMP_APPLICATION_CONFIG_TYPE);
 
 out:
+    if (ss == NULL)
+        netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

agent/agent_trap.c
@@ -1839,9 +1839,7 @@ snmpd_parse_config_trapsess(const char *word, char *cptr)
                                               ss->version, name, tag, profile);
 
   cleanup:
-    SNMP_FREE(session.community);
-    if (session.securityEngineIDLen > 0)
-        SNMP_FREE(session.securityEngineID);
+    netsnmp_cleanup_session(&session);
     SNMP_FREE(clientaddr_save);
     SNMP_FREE(profile);
     SNMP_FREE(name);

apps/snmpbulkget.c
@@ -249,6 +249,7 @@ main(int argc, char *argv[])
     snmp_close(ss);
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

apps/snmpbulkwalk.c
@@ -385,6 +385,7 @@ main(int argc, char *argv[])
     }
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

apps/snmpdelta.c
@@ -748,6 +748,7 @@ close_session:
     snmp_close(ss);
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return (exit_code);
 }

apps/snmpdf.c
@@ -466,6 +466,7 @@ close_session:
     snmp_close(ss);
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exit_code;
 }                               /* end main() */

apps/snmpget.c
@@ -249,6 +249,7 @@ close_session:
     snmp_close(ss);
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }                               /* end main() */

apps/snmpgetnext.c
@@ -234,6 +234,7 @@ close_session:
     snmp_close(ss);
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

commit 620b346e000882a03009d4afc0cee3cddc7977aa

apps/snmptrap: Initialize the variable 'ss'
Fixes: 0b5e7d6f1532 ("apps: Use netsnmp_cleanup_session()")

apps/snmptrap.c
@@ -119,7 +119,7 @@ optProc(int argc, char *const *argv, int opt)
 int
 main(int argc, char *argv[])
 {
-    netsnmp_session session, *ss;
+    netsnmp_session session, *ss = NULL;
     netsnmp_pdu    *pdu, *response;
     oid             name[MAX_OID_LEN];
     size_t          name_length;