net-snmp/net-snmp

commit add994166e264b15f7c5232d28e432c16b5b6b9c

CHANGES: snmplib: PATCHES: 3183000: Fix calculation of common OID prefix length CHANGES: snmplib: BUGS: 3182985: Fix calculation of common OID prefix length
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@20182 06827809-a52a-0410-b366-d66718629ded

snmplib/snmp_api.c
@@ -6561,7 +6561,8 @@ netsnmp_oid_is_subtree(const oid * in_name1,
  * @param len1     Length of the first oid.
  * @param in_name2 A pointer to the second oid.
  * @param len2     Length of the second oid.
- * @return length of largest common index of commonality.  1 = first, 0 if none *         or -1 on error.
+ * @return         length of common prefix
+ *                 0 if no common prefix, -1 on error.
  */
 int
 netsnmp_oid_find_prefix(const oid * in_name1, size_t len1,
@@ -6578,9 +6579,12 @@ netsnmp_oid_find_prefix(const oid * in_name1, size_t len1,
     min_size = SNMP_MIN(len1, len2);
     for(i = 0; i < (int)min_size; i++) {
         if (in_name1[i] != in_name2[i])
-            return i + 1;    /* Why +1 ?? */
+            return i;    /* 'í' is the first differing subidentifier
+                            So the common prefix is 0..(i-1), of length i */
     }
-    return min_size;	/* or +1? - the spec isn't totally clear */
+    return min_size;	/* The shorter OID is a prefix of the longer, and
+                           hence is precisely the common prefix of the two.
+                           Return its length. */
 }
 
 static int _check_range(struct tree *tp, long ltmp, int *resptr,

commit 0c0c54806e7046cf4ad496b4dc0c3196ac48071b

libsnmp: Fix a variable name in a source code comment
Fixes: add994166e26 ("CHANGES: snmplib: PATCHES: 3183000: Fix calculation of common OID prefix length CHANGES: snmplib: BUGS: 3182985: Fix calculation of common OID prefix length")
Reported-by: Eldin Zenderink <e.zenderink@gmail.com>

snmplib/snmp_api.c
@@ -7146,7 +7146,7 @@ netsnmp_oid_find_prefix(const oid * in_name1, size_t len1,
     min_size = SNMP_MIN(len1, len2);
     for(i = 0; i < (int)min_size; i++) {
         if (in_name1[i] != in_name2[i])
-            return i;    /* 'í' is the first differing subidentifier
+            return i;    /* 'i' is the first differing subidentifier
                             So the common prefix is 0..(i-1), of length i */
     }
     return min_size;	/* The shorter OID is a prefix of the longer, and