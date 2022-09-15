44a4e7672a73384cdf2beb27fb7b652b76ddbb2f
Bart Van Assche <bvanassche@acm.org>
2022-09-15 14:18:17 -0700

    testhandler: Fix memory leaks

--- agent/mibgroup/testhandler.h
+++ agent/mibgroup/testhandler.h
@@ -3,6 +3,7 @@
  */
 
 void            init_testhandler(void);
+void            cleanup_testhandler(void);
 Netsnmp_Node_Handler my_test_handler;
 Netsnmp_Node_Handler my_test_table_handler;
 Netsnmp_Node_Handler my_data_table_handler;
