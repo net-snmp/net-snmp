net-snmp/net-snmp

commit 9b9c0e287b4dd4619da3e3ca80e1832dc1c81699

MIBs: Use asprintf() instead of snprintf() to prevent truncation
This patch addresses most gcc 7 warnings about output buffer truncation.

agent/mibgroup/ucd-snmp/pass_persist.c
@@ -143,7 +143,7 @@ pass_persist_parse_config(const char *token, char *cptr)
 
     while (*ppass != NULL)
         ppass = &((*ppass)->next);
-    (*ppass) = (struct extensible *) malloc(sizeof(struct extensible));
+    *ppass = calloc(1, sizeof(**ppass));
     if (*ppass == NULL)
         return;
     (*ppass)->type = PASSTHRU_PERSIST;
@@ -155,14 +155,19 @@ pass_persist_parse_config(const char *token, char *cptr)
     /*
      * path
      */
+    free((*ppass)->command);
+    (*ppass)->command = NULL;
     cptr = skip_white(cptr);
     if (cptr == NULL) {
         config_perror("No command specified on pass_persist line");
-        (*ppass)->command[0] = 0;
+        if (asprintf(&(*ppass)->command, "%s", "") < 0) {
+        }
     } else {
         for (tcptr = cptr; *tcptr != 0 && *tcptr != '#' && *tcptr != ';';
              tcptr++);
-        sprintf((*ppass)->command, "%.*s", (int) (tcptr - cptr), cptr);
+        if (asprintf(&(*ppass)->command, "%.*s", (int)(tcptr - cptr), cptr)
+            < 0) {
+        }
     }
     strlcpy((*ppass)->name, (*ppass)->command, sizeof((*ppass)->name));
     (*ppass)->next = NULL;
@@ -259,13 +264,11 @@ var_extensible_pass_persist(struct variable *vp,
                 return (NULL);
             }
 
-            if (exact)
-                snprintf(persistpassthru->command,
-                  sizeof(persistpassthru->command), "get\n%s\n", buf);
-            else
-                snprintf(persistpassthru->command,
-                  sizeof(persistpassthru->command), "getnext\n%s\n", buf);
-            persistpassthru->command[ sizeof(persistpassthru->command)-1 ] = 0;
+            free(persistpassthru->command);
+            persistpassthru->command = NULL;
+            if (asprintf(&persistpassthru->command, "%s\n%s\n",
+                         exact ? "get" : "getnext", buf) < 0) {
+            }
 
             DEBUGMSGTL(("ucd-snmp/pass_persist",
                         "persistpass-sending:\n%s",
@@ -363,14 +366,11 @@ setPassPersist(int action,
                                persistpassthru->miblen);
             else
                 sprint_mib_oid(buf, name, name_len);
-            snprintf(persistpassthru->command,
-                     sizeof(persistpassthru->command), "set\n%s\n", buf);
-            persistpassthru->command[ sizeof(persistpassthru->command)-1 ] = 0;
-            netsnmp_internal_pass_set_format(buf, var_val, var_val_type, var_val_len);
-            strlcat(persistpassthru->command, buf,
-                    sizeof(persistpassthru->command));
-            persistpassthru->command[ sizeof(persistpassthru->command)-2 ] = '\n';
-            persistpassthru->command[ sizeof(persistpassthru->command)-1 ] = 0;
+            netsnmp_internal_pass_set_format(buf2, var_val, var_val_type,
+                                             var_val_len);
+            if (asprintf(&persistpassthru->command, "set\n%s\n%s\n", buf,
+                         buf2) < 0) {
+            }
 
             if (!open_persist_pipe(i, persistpassthru->name)) {
                 return SNMP_ERR_NOTWRITABLE;

agent/mibgroup/hardware/fsys/fsys_mntent.c
@@ -174,15 +174,16 @@ netsnmp_fsys_arch_load( void )
 #endif
     struct NSFS_STATFS stat_buf;
     netsnmp_fsys_info *entry;
-    char               tmpbuf[1024];
+    char              *tmpbuf = NULL;
 
     /*
      * Retrieve information about the currently mounted filesystems...
      */
     fp = fopen( ETC_MNTTAB, "r" );   /* OR setmntent()?? */
     if ( !fp ) {
-        snprintf( tmpbuf, sizeof(tmpbuf), "Cannot open %s", ETC_MNTTAB );
-        snmp_log_perror( tmpbuf );
+        if (asprintf(&tmpbuf, "Cannot open %s", ETC_MNTTAB) >= 0)
+            snmp_log_perror(tmpbuf);
+        free(tmpbuf);
         return;
     }
 
@@ -243,8 +244,10 @@ netsnmp_fsys_arch_load( void )
         if ( NSFS_STATFS( entry->path, &stat_buf ) < 0 )
 #endif
         {
-            snprintf( tmpbuf, sizeof(tmpbuf), "Cannot statfs %s", entry->path );
-            snmp_log_perror( tmpbuf );
+            tmpbuf = NULL;
+            if (asprintf(&tmpbuf, "Cannot statfs %s", entry->path) >= 0)
+                snmp_log_perror(tmpbuf);
+            free(tmpbuf);
             entry->units = stat_buf.NSFS_SIZE;
             entry->size  = 0;
             entry->used  = 0;

agent/mibgroup/host/hr_print.c
@@ -361,10 +361,9 @@ run_lpstat(int *fd)
     struct extensible ex;
 
     memset(&ex, 0, sizeof(ex));
-    strcpy(ex.command, LPSTAT_PATH " -v");
-    if ((*fd = get_exec_output(&ex)) < 0)
-        return NULL;
-
-    return fdopen(*fd, "r");
+    ex.command = strdup(LPSTAT_PATH " -v");
+    *fd = get_exec_output(&ex);
+    free(ex.command);
+    return *fd >= 0 ? fdopen(*fd, "r") : NULL;
 }
 #endif

agent/mibgroup/host/hrh_filesys.c
@@ -218,7 +218,7 @@ var_hrhfilesys(struct variable *vp,
               int exact, size_t * var_len, WriteMethod ** write_method)
 {
     int             fsys_idx;
-    static char     string[1024];
+    static char    *string;
 
     fsys_idx =
         header_hrhfilesys(vp, name, length, exact, var_len, write_method);
@@ -230,17 +230,22 @@ var_hrhfilesys(struct variable *vp,
         long_return = fsys_idx;
         return (u_char *) & long_return;
     case HRFSYS_MOUNT:
-        snprintf(string, sizeof(string), "%s", HRFS_entry->path);
-        string[ sizeof(string)-1 ] = 0;
-        *var_len = strlen(string);
+        free(string);
+        string = NULL;
+        *var_len = 0;
+        if (asprintf(&string, "%s", HRFS_entry->path) >= 0)
+            *var_len = strlen(string);
         return (u_char *) string;
     case HRFSYS_RMOUNT:
+        free(string);
+        string = NULL;
         if (HRFS_entry->flags & NETSNMP_FS_FLAG_REMOTE) {
-            snprintf(string, sizeof(string), "%s", HRFS_entry->device);
-            string[ sizeof(string)-1 ] = 0;
-        } else
-            string[0] = '\0';
-        *var_len = strlen(string);
+            if (asprintf(&string, "%s", HRFS_entry->device) < 0) {
+            }
+        } else {
+            string = strdup("");
+        }
+        *var_len = string ? strlen(string) : 0;
         return (u_char *) string;
 
     case HRFSYS_TYPE:

agent/mibgroup/mibII/system_mib.c
@@ -249,7 +249,9 @@ init_system_mib(void)
     /*
      * set default values of system stuff 
      */
-    sprintf(extmp.command, "%s -a", UNAMEPROG);
+    extmp.command = NULL;
+    if (asprintf(&extmp.command, "%s -a", UNAMEPROG) < 0) {
+    }
     /*
      * setup defaults 
      */
@@ -275,7 +277,9 @@ init_system_mib(void)
     strlcpy(sysName, utsName.nodename, sizeof(sysName));
 #else
 #if defined (HAVE_EXECV) && !defined (mingw32)
-    sprintf(extmp.command, "%s -n", UNAMEPROG);
+    extmp.command = NULL;
+    if (asprintf(&extmp.command, "%s -n", UNAMEPROG) < 0) {
+    }
     /*
      * setup defaults 
      */

agent/mibgroup/struct.h
@@ -10,7 +10,7 @@
 
 struct extensible {
     char            name[STRMAX];
-    char            command[STRMAX];
+    char           *command;
     char            fixcmd[STRMAX];
     int             type;
     int             result;

agent/mibgroup/tlstm-mib/snmpTlstmAddrTable/snmpTlstmAddrTable.c
@@ -1234,7 +1234,7 @@ _tlstmAddr_init_persistence(void)
 static int
 _save_entry(tlstmAddrTable_entry *entry, void *type)
 {
-    char   buf[SNMP_MAXBUF_SMALL], *hashType;
+    char *buf = NULL, *hashType;
 
     hashType = se_find_label_in_slist("cert_hash_alg", entry->hashType);
     if (NULL == hashType) {
@@ -1250,15 +1250,17 @@ _save_entry(tlstmAddrTable_entry *entry, void *type)
                        entry->snmpTargetAddrName_len]);
     netsnmp_assert(0 == entry->tlstmAddrServerFingerprint[
                        entry->tlstmAddrServerFingerprint_len]);
-    snprintf(buf, sizeof(buf), "%s %s --%s %s %s %d", mib_token,
-             entry->snmpTargetAddrName, hashType,
-             entry->tlstmAddrServerFingerprint,
-             entry->tlstmAddrServerIdentity,
-             entry->tlstmAddrRowStatus);
-    buf[sizeof(buf)-1] = 0;
+    if (asprintf(&buf, "%s %s --%s %s %s %d", mib_token,
+                 entry->snmpTargetAddrName, hashType,
+                 entry->tlstmAddrServerFingerprint,
+                 entry->tlstmAddrServerIdentity,
+                 entry->tlstmAddrRowStatus) < 0) {
+        return SNMP_ERR_GENERR;
+    }
 
     read_config_store(type, buf);
     DEBUGMSGTL(("tlstmAddrTable:row:save", "saving entry '%s'\n", buf));
+    free(buf);
 
     return SNMP_ERR_NOERROR;
 }

agent/mibgroup/tlstm-mib/snmpTlstmCertToTSNTable/snmpTlstmCertToTSNTable.c
@@ -1200,7 +1200,7 @@ _parse_mib_maps(const char *token, char *line)
 static int
 _save_entry(certToTSN_entry *entry, void *app_type)
 {
-    char buf[SNMP_MAXBUF_SMALL], *hashType, *mapType, *data = NULL;
+    char *buf = NULL, *hashType, *mapType, *data = NULL;
 
     if (NULL == entry)
         return SNMP_ERR_GENERR;
@@ -1214,12 +1214,15 @@ _save_entry(certToTSN_entry *entry, void *app_type)
     mapType = se_find_label_in_slist("cert_map_type", entry->mapType);
     if (TSNM_tlstmCertSpecified == entry->mapType)
         data = entry->data;
-    snprintf(buf, sizeof(buf), "%s %ld --%s %s --%s %s %d",
-             MAP_MIB_CONFIG_TOKEN, entry->tlstmCertToTSNID, hashType,
-             entry->fingerprint, mapType, data ? data : "", entry->rowStatus);
+    if (asprintf(&buf, "%s %ld --%s %s --%s %s %d", MAP_MIB_CONFIG_TOKEN,
+                 entry->tlstmCertToTSNID, hashType, entry->fingerprint,
+                 mapType, data ? data : "", entry->rowStatus) < 0) {
+        return SNMP_ERR_GENERR;
+    }
 
     DEBUGMSGTL(("tlstmCertToTSNTable:save", "saving '%s'\n", buf));
     read_config_store(app_type, buf);
+    free(buf);
 
     return SNMP_ERR_NOERROR;
 }

commit dffe26c0bd9a4f62dc9cd7b267fa7dded867ceee

agent/mibgroup/ucd-snmp/pass_persist: Fix a regression in pass_persist SET handling
Fixes: 9b9c0e287b4d ("MIBs: Use asprintf() instead of snprintf() to prevent truncation")
Fixes: https://github.com/net-snmp/net-snmp/issues/461

agent/mibgroup/ucd-snmp/pass_persist.c
@@ -452,7 +452,7 @@ setPassPersist(int action,
             netsnmp_internal_pass_set_format(buf2, var_val, var_val_type,
                                              var_val_len);
             free(persistpassthru->command);
-            if (asprintf(&persistpassthru->command, "set\n%s\n%s\n", buf,
+            if (asprintf(&persistpassthru->command, "set\n%s\n%s", buf,
                          buf2) < 0) {
                 persistpassthru->command = NULL;
                 return SNMP_ERR_GENERR;