71ed5bab10a9922c2b0e1c84f0518fa762998c08
Bart Van Assche <bvanassche@acm.org>
2022-10-09 16:18:39 -0700

    testing/fulltests/unit-tests/T028_objid: Add this unit test

---
+++ testing/fulltests/unit-tests/T028_objid_clib.c
@@ -0,0 +1,95 @@
+/* HEADER ASN OBJECT ID parsing and encoding */
+
+static const struct testdata_s {
+    oid      objid[4];
+    uint16_t objid_length;
+    u_char   encoded[8];
+    uint16_t encoded_length;
+} testdata[] = {
+    { {},                0, { 6, 1, 0 },       3 },
+    { { 0 },             1, { 6, 1, 0 },       3 },
+    { { 0, 0 },          2, { 6, 1, 0 },       3 },
+    { { 1, 3 },          2, { 6, 1, 0x2b },    3 },
+    { { 1, 3, 4 },       3, { 6, 2, 0x2b, 4 }, 4 },
+    { { 1, 3, 4444444 }, 3, { 6, 5, 0x2b, 0x82, 0x8f, 0xa2, 0x1c }, 7 },
+};
+
+int i, j;
+
+for (i = 0; i < sizeof(testdata) / sizeof(testdata[0]); i++) {
+    const struct testdata_s *t = &testdata[i];
+    {
+        uint8_t data[16];
+        size_t datalength = sizeof(data);
+        uint8_t *res = asn_build_objid(data, &datalength, ASN_OBJECT_ID,
+                                       t->objid, t->objid_length);
+        OKF(res != NULL, ("[%d] asn_build_objid()", i));
+        if (res != NULL) {
+            uint16_t encoded_length = sizeof(data) - datalength;
+            OKF(t->encoded_length == encoded_length,
+                ("[%d] encoded length %d <> %d", i, t->encoded_length,
+                 encoded_length));
+            if (t->encoded_length == encoded_length) {
+                int cmp_res = memcmp(data, t->encoded, t->encoded_length);
+                OKF(cmp_res == 0, ("[%d] asn_build_objid() memcmp()", i));
+                if (cmp_res != 0) {
+                    for (j = 0; j < encoded_length; j++)
+                        printf("%02x ", data[j]);
+                    printf("\n");
+                }
+            }
+        }
+    }
+    {
+        uint8_t *pkt = NULL;
+        size_t pkt_len = 0, offset = 0;
+        int res = asn_realloc_rbuild_objid(&pkt, &pkt_len, &offset, TRUE,
+                                           ASN_OBJECT_ID, t->objid,
+                                           t->objid_length);
+        OKF(res != 0, ("[%d] asn_realloc_rbuild_objid()", i));
+        if (res != 0) {
+            OKF(t->encoded_length == offset,
+                ("[%d] encoded length %d <> %" NETSNMP_PRIz "d", i,
+                 t->encoded_length, offset));
+            if (t->encoded_length == offset) {
+                const uint8_t *const start = pkt + pkt_len - offset;
+                int cmp_res = memcmp(start, t->encoded, t->encoded_length);
+                OKF(cmp_res == 0, ("[%d] asn_rbuild_objid() memcmp()", i));
+                if (cmp_res != 0) {
+                    for (j = 0; j < offset; j++)
+                        printf("%02x ", start[j]);
+                    printf("\n");
+                }
+            }
+        }
+        free(pkt);
+    }
+    {
+        size_t datalength = t->encoded_length;
+        u_char type;
+        oid objid[8];
+        size_t objid_len = sizeof(objid) / sizeof(objid[0]);
+        uint8_t *end =
+            asn_parse_objid(NETSNMP_REMOVE_CONST(uint8_t *, t->encoded),
+                            &datalength, &type, objid, &objid_len);
+        OKF(end != NULL, ("[%d] asn_parse_objid()", i));
+        if (end != NULL) {
+            OKF(datalength == 0, ("[%d] datalength %zu", i, datalength));
+            OKF(type == ASN_OBJECT_ID, ("[%d] type %u", i, type));
+            const uint32_t exp_len = t->objid_length >= 2 ?
+                t->objid_length : 2;
+            OKF(exp_len == objid_len, ("[%d] objid len %d <> %zd", i, exp_len,
+                                       objid_len));
+            if (exp_len == objid_len) {
+                int cmp_res = memcmp(t->objid, objid, objid_len * sizeof(oid));
+                OKF(cmp_res == 0, ("[%d] asn_parse_objid() memcmp()", i));
+                if (cmp_res != 0) {
+                    for (j = 0; j < objid_len; j++)
+                        printf("%" NETSNMP_PRIo "d ", objid[j]);
+                    printf("\n");
+                }
+            }
+        }
+    }
+    
+}
