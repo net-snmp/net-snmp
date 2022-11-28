67efde4d63477eedd0f1a096a2d8e9e2a3055d18
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2022-11-28 09:12:59 +0300

    mibgroup: removed dead code

--- agent/mibgroup/mibII/kernel_linux.c
+++ agent/mibgroup/mibII/kernel_linux.c
@@ -438,9 +438,6 @@ linux_read_icmp_msg_stat(struct icmp_mib *icmpstat,
 
     if ((ret = linux_read_mibII_stats()) == -1) {
         return -1;
-    } else if (ret) {
-       memcpy(icmpmsgstat, &cached_icmp4_msg_mib, sizeof(*icmpmsgstat));
-       *flag = 1; /* We have a valid icmpmsg */
     }
 
     memcpy(icmpstat, &cached_icmp_mib, sizeof(*icmpstat));
