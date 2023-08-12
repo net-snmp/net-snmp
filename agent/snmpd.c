45a64224f3e52307bbcc4cd00705398f27de44cb
Feroz Ahmed <feroz.afshan@gmail.com>
2023-08-12 07:09:37 +0000

    Handle SIGHUP for engine-id change
    SNMP operations continue to work with old engine-id with SIGHUP.

--- agent/snmpd.c
+++ agent/snmpd.c
@@ -1168,6 +1168,7 @@ snmpd_reconfig(void)
     netsnmp_logging_restart();
     snmp_log(LOG_INFO, "NET-SNMP version %s restarted\n",
              netsnmp_get_version());
+    read_premib_configs();
     update_config();
     send_easy_trap(SNMP_TRAP_ENTERPRISESPECIFIC, 3);
 #ifdef HAVE_SIGPROCMASK
