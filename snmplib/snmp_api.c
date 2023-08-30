net-snmp/net-snmp

commit 9d28612ac03b1bd16ccaf12627e9e1eff0ce8cef

add packet filtering by source ip
Signed-off-by: Robert Story <rstory@freesnmp.com>

snmplib/snmp_api.c
@@ -5430,6 +5430,7 @@ _sess_process_packet_parse_pdu(void *sessp, netsnmp_session * sp,
 {
   netsnmp_pdu    *pdu;
   int             ret = 0;
+  int             dump = 0, filter = 0;
 
   debug_indent_reset();
 
@@ -5437,12 +5438,44 @@ _sess_process_packet_parse_pdu(void *sessp, netsnmp_session * sp,
 	      "session %p fd %d pkt %p length %d\n", sessp,
 	      transport->sock, packetptr, length));
 
-  if (netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,NETSNMP_DS_LIB_DUMP_PACKET)) {
+  dump = netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                NETSNMP_DS_LIB_DUMP_PACKET);
+#ifndef NETSNMP_FEATURE_REMOVE_FILTER_SOURCE
+  filter = netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID,
+                                  NETSNMP_DS_LIB_FILTER_SOURCE);
+#endif
+  if (dump || filter) {
+      int drop = 0;
       char *addrtxt = netsnmp_transport_peer_string(transport, opaque, olength);
       snmp_log(LOG_DEBUG, "\nReceived %d byte packet from %s\n",
                length, addrtxt);
+
+      if (dump)
+          xdump(packetptr, length, "");
+#ifndef NETSNMP_FEATURE_REMOVE_FILTER_SOURCE
+      if (filter) {
+          char *txt, *c = strchr(addrtxt, '[');
+          if (c) {
+              txt = ++c;
+              c = strchr(txt, ']');
+              if (c)
+                  *c = 0;
+              drop = netsnmp_transport_filter_should_drop(txt);
+              if (drop)
+                  DEBUGMSGTL(("sess_process_packet:drop",
+                              "dropping packet from %s\n", txt));
+          }
+      }
+#endif
+
       SNMP_FREE(addrtxt);
-      xdump(packetptr, length, "");
+
+#ifndef NETSNMP_FEATURE_REMOVE_FILTER_SOURCE
+      if (drop) {
+          SNMP_FREE(opaque);
+          return NULL;
+      }
+#endif
   }
 
   /*

include/net-snmp/library/default_store.h
@@ -103,6 +103,7 @@ extern          "C" {
 #define NETSNMP_DS_LIB_DISABLE_V1          43 /* disable SNMPv1 */
 #define NETSNMP_DS_LIB_DISABLE_V2c         44 /* disable SNMPv2c */
 #define NETSNMP_DS_LIB_DISABLE_V3          45 /* disable SNMPv3 */
+#define NETSNMP_DS_LIB_FILTER_SOURCE       46 /* filter pkt by source IP */
 #define NETSNMP_DS_LIB_MAX_BOOL_ID         48 /* match NETSNMP_DS_MAX_SUBIDS */
 
     /*

include/net-snmp/library/snmp_transport.h
@@ -356,6 +356,22 @@ int netsnmp_transport_config_compare(netsnmp_transport_config *left,
 NETSNMP_IMPORT
 netsnmp_transport_config *netsnmp_transport_create_config(char *key,
                                                           char *value);
+
+#ifndef NETSNMP_FEATURE_REMOVE_FILTER_SOURCE
+NETSNMP_IMPORT
+int netsnmp_transport_filter_add(const char *addrtxt);
+
+NETSNMP_IMPORT
+int netsnmp_transport_filter_remove(const char *addrtxt);
+
+NETSNMP_IMPORT
+int netsnmp_transport_filter_should_drop(const char *addrtxt);
+
+NETSNMP_IMPORT
+void netsnmp_transport_filter_cleanup(void);
+
+#endif /* NETSNMP_FEATURE_REMOVE_FILTER_SOURCE */
+
 #ifdef __cplusplus
 }
 #endif

snmplib/snmp_transport.c
@@ -254,6 +254,102 @@ netsnmp_transport_peer_string(netsnmp_transport *t, void *data, int len)
     return str;
 }
 
+#ifndef NETSNMP_FEATURE_REMOVE_FILTER_SOURCE
+static netsnmp_container * filtered = NULL;
+
+void netsnmp_transport_parse_filter(const char *word, char *cptr);
+void netsnmp_transport_filter_cleanup(void);
+
+static int _transport_filter_init(void)
+{
+    if (filtered)
+        return 0;
+
+    filtered = netsnmp_container_find("transport_filter:cstring");
+    if (NULL == filtered) {
+        NETSNMP_LOGONCE((LOG_WARNING,
+                         "couldn't allocate container for transport_filter list\n"));
+        return -1;
+    }
+    filtered->container_name = strdup("transport_filter list");
+    netsnmp_ds_register_config(ASN_BOOLEAN,
+                               "snmp", "enableSourceFiltering",
+                               NETSNMP_DS_LIBRARY_ID,
+                               NETSNMP_DS_LIB_FILTER_SOURCE);
+    register_app_config_handler("filtersource",
+                                netsnmp_transport_parse_filter,
+                                netsnmp_transport_filter_cleanup,
+                                "host");
+
+    return 0;
+}
+
+int
+netsnmp_transport_filter_add(const char *addrtxt)
+{
+    char *tmp;
+
+    /*
+     * create the container, if needed
+     */
+    if (_transport_filter_init()) {
+        snmp_log(LOG_ERR,"netsnmp_transport_filter_add %s failed\n",
+                 addrtxt);
+        return (-1);
+    }
+    tmp = strdup(addrtxt);
+    if (NULL == tmp) {
+        snmp_log(LOG_ERR,"netsnmp_transport_filter_add strdup failed\n");
+        return(-1);
+    }
+    return CONTAINER_INSERT(filtered, tmp);
+}
+
+int
+netsnmp_transport_filter_remove(const char *addrtxt)
+{
+    /*
+     * create the container, if needed
+     */
+    if (NULL == filtered)
+        return -1;
+    return CONTAINER_REMOVE(filtered, addrtxt);
+}
+
+/*
+ * netsnmp_transport_filter_should_drop
+ *
+ * returns 1 if packets from the specified address string should be dropped
+ */
+int
+netsnmp_transport_filter_should_drop(const char *addrtxt)
+{
+    char *addr;
+    if (NULL == filtered)
+        return 0;
+    addr = CONTAINER_FIND(filtered, addrtxt);
+    return addr ? 1 : 0;
+}
+
+void
+netsnmp_transport_parse_filter(const char *word, char *cptr)
+{
+    if (netsnmp_transport_filter_add(cptr))
+        netsnmp_config_error("cannot create source filter: %s", cptr);
+}
+
+void
+netsnmp_transport_filter_cleanup(void)
+{
+    if (NULL == filtered)
+        return;
+    CONTAINER_CLEAR(filtered, filtered->free_item, NULL);
+    CONTAINER_FREE(filtered);
+    filtered = NULL;
+}
+#endif /* NETSNMP_FEATURE_REMOVE_FILTER_SOURCE */
+
+
 #ifndef NETSNMP_FEATURE_REMOVE_SOCKADDR_SIZE
 int
 netsnmp_sockaddr_size(struct sockaddr *sa)
@@ -375,6 +471,18 @@ netsnmp_tdomain_init(void)
 #include "transports/snmp_transport_inits.h"
 
     netsnmp_tdomain_dump();
+
+#ifndef NETSNMP_FEATURE_REMOVE_FILTER_SOURCE
+    netsnmp_ds_register_config(ASN_BOOLEAN,
+                               "snmp", "enableSourceFiltering",
+                               NETSNMP_DS_LIBRARY_ID,
+                               NETSNMP_DS_LIB_FILTER_SOURCE);
+    register_app_config_handler("filtersource",
+                                netsnmp_transport_parse_filter,
+                                netsnmp_transport_filter_cleanup,
+                                "host");
+#endif /* NETSNMP_FEATURE_REMOVE_FILTER_SOURCE */
+
 }
 
 void

commit 636c20802050b08b9037a7d5e0a9c9938f72fff0

snmplib: do not filter internal request
sourceFilterType and sourceFilterAddress options in snmpd.conf. However,
once activated, internal requests are also blocked. The filtering is
based on IP in the string returned by the function "fmtaddr". Internal
requests do not have a source IP, instead of an IP address, the function
returns a string that starts with "callback" following a file descriptor
numero. These internal requests are generated by alarms to define event
triggers. If they are blocked, no traps will be sent.

Therefore, do not filter internal requests by checking if the string
returned by "fmtaddr" starts with "callback".

Fixes: 9d28612ac03b ("add packet filtering by source ip")
Signed-off-by: Loïc Sang <loic.sang@6wind.com>

snmplib/snmp_api.c
@@ -5651,6 +5651,13 @@ _sess_process_packet_parse_pdu(struct session_list *slp, netsnmp_session * sp,
                   *c = 0;
               filtered = netsnmp_transport_filter_check(sourceaddr);
           }
+          else if (!strncmp(addrtxt, "callback", 8)) {
+              /* do not filter internal request */
+              DEBUGMSGTL(("sess_process_packet:filter",
+                          "bypass packet from %s \n",
+                          addrtxt));
+              filtered = 1;
+          }
           if ((filter == -1) && filtered)
               dropstr = "matched blacklist";
           else if ((filter == 1) && !filtered)