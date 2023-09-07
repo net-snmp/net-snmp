net-snmp/net-snmp

commit e6e11ebcc493bfa2aa4576754edcb39f38e4c033

Only schedule automatic loading once for any given cache.
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@13462 06827809-a52a-0410-b366-d66718629ded

agent/helpers/cache_handler.c
@@ -214,6 +214,7 @@ netsnmp_cache_timer_start(netsnmp_cache *cache)
         return 0;
     }
 
+    cache->flags &= ~NETSNMP_CACHE_AUTO_RELOAD;
     DEBUGMSGT(("cache_timer:start",
                "starting timer %d for cache %p\n", cache->timer_id, cache));
     return cache->timer_id;
@@ -235,6 +236,7 @@ netsnmp_cache_timer_stop(netsnmp_cache *cache)
                "stopping timer %d for cache %p\n", cache->timer_id, cache));
 
     snmp_alarm_unregister(cache->timer_id);
+    cache->flags |= NETSNMP_CACHE_AUTO_RELOAD;
 }
 
 

commit 3771cc60ce51c0232bd4fe659c72a4c6fb678ff6

agent: keep cache flags unchanged
Cache flags are managed by the MIB implementation.

Do not clear/set NETSNMP_CACHE_AUTO_RELOAD flag to ensure
scheduling auto-reload only once. This is already managed
by netsnmp_cache_timer_start() via cache timer_id check.

Instead, check the timer_id validity in the cache handler
before calling netsnmp_cache_timer_start() to avoid warnings
if auto-reload is already scheduled.

Fixes: e6e11ebcc493 ("Only schedule automatic loading once for any given cache.")

Signed-off-by: Ilyes Ben Hamouda <ilyes.ben_hamouda@6wind.com>

agent/helpers/cache_handler.c
@@ -347,7 +347,6 @@ netsnmp_cache_timer_start(netsnmp_cache *cache)
         return 0;
     }
 
-    cache->flags &= ~NETSNMP_CACHE_AUTO_RELOAD;
     DEBUGMSGT(("cache_timer:start",
                "starting timer %lu for cache %p\n", cache->timer_id, cache));
     return cache->timer_id;
@@ -370,7 +369,6 @@ netsnmp_cache_timer_stop(netsnmp_cache *cache)
 
     snmp_alarm_unregister(cache->timer_id);
     cache->timer_id = 0;
-    cache->flags |= NETSNMP_CACHE_AUTO_RELOAD;
 }
 
 
@@ -395,7 +393,7 @@ netsnmp_cache_handler_get(netsnmp_cache* cache)
                  */
                 (void)_cache_load(cache);
             }
-            if (cache->flags & NETSNMP_CACHE_AUTO_RELOAD)
+            if (cache->flags & NETSNMP_CACHE_AUTO_RELOAD && !cache->timer_id)
                 netsnmp_cache_timer_start(cache);
             
         }