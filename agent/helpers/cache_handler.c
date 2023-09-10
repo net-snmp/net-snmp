net-snmp/net-snmp

commit 585d35cc51e3eea066293d65ff5e80373df07a3e

agent: release memory back to the system after freeing cache data
The heap memory increases, when retrieving IP-FORWARD MIB objects, in
proportion to the number of entries in the routing table.
It is not given back to the system when freed due to a glibc limitation
(See the link below).

The IP-FORWARD MIB implementation as well as other MIB implementations
use data caching.
Cache management depends on cache flags. For instance, based on
NETSNMP_CACHE_AUTO_RELOAD flag, some caches are reloaded automatically,
while others are reloaded on request.
Each cache has also a timeout. The timeout is checked on cache access.
If expired, the cache is reloaded.
A garbage collector also checks the timeout expiry every minute to
release cache resources.

Call malloc_trim() to return freed memory to the system after the
garbage collector has freed cache data only if the cache will not be
automatically reloaded.

In the best-case scenario, malloc_trim will only be called once after
cache accesses have been completed.

Worst-case, the GC detects the timeout expiry just before a request.
Hence, there will be a wasteful trim, as the cache will be immediately
loaded later. There is a way to avoid this behavior by introducing
NETSNMP_CACHE_RESET_TIMER_ON_USE flag which will reset cache timeout
on each cache access but this may lead to a stale cache if the polling
happens too fast.

Avoid introducing this flag since it depends on polling period.

Note: malloc_trim has a linear complexity, it seems to be safe but it may
softly degrade performance when frequently called.

Link: https://sourceware.org/bugzilla/show_bug.cgi?id=15321

Signed-off-by: Ilyes Ben Hamouda <ilyes.ben_hamouda@6wind.com>

agent/helpers/cache_handler.c
@@ -16,6 +16,8 @@
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-features.h>
 
+#include <malloc.h>
+
 #ifdef HAVE_STRING_H
 #include <string.h>
 #else
@@ -769,6 +771,7 @@ void
 release_cached_resources(unsigned int regNo, void *clientargs)
 {
     netsnmp_cache  *cache = NULL;
+    int do_trim = 0;
 
     cache_outstanding_valid = 0;
     DEBUGMSGTL(("helper:cache_handler", "running auto-release\n"));
@@ -785,13 +788,21 @@ release_cached_resources(unsigned int regNo, void *clientargs)
              *   least one active cache.
              */
             if (netsnmp_cache_check_expired(cache)) {
-                if(! (cache->flags & NETSNMP_CACHE_DONT_FREE_EXPIRED))
+                if(! (cache->flags & NETSNMP_CACHE_DONT_FREE_EXPIRED)) {
                     _cache_free(cache);
+                    if (cache->free_cache && !cache->timer_id)
+                        do_trim = 1;
+                }
             } else {
                 cache_outstanding_valid = 1;
             }
         }
     }
+
+    if (do_trim)
+        /* Release freed memory back to the system */
+        malloc_trim(0);
+
     /*
      * If there are any caches still valid & active,
      *   then schedule another pass.

commit 0ff409ddb429535a4829301d9dc9c0a0077ffee2

snmpd: Unbreak the build
malloc_trim() is defined by glibc but is not available on BSD systems.

Fixes: 585d35cc51e3 ("agent: release memory back to the system after freeing cache data")

agent/helpers/cache_handler.c
@@ -16,7 +16,9 @@
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-features.h>
 
+#ifdef HAVE_MALLOC_H
 #include <malloc.h>
+#endif
 
 #ifdef HAVE_STRING_H
 #include <string.h>
@@ -799,9 +801,11 @@ release_cached_resources(unsigned int regNo, void *clientargs)
         }
     }
 
-    if (do_trim)
-        /* Release freed memory back to the system */
+    if (do_trim) {
+#ifdef HAVE_MALLOC_TRIM
         malloc_trim(0);
+#endif
+    }
 
     /*
      * If there are any caches still valid & active,

configure
@@ -31340,6 +31340,12 @@ then :
   printf "%s\n" "#define HAVE_IF_NAMETOINDEX 1" >>confdefs.h
 
 fi
+ac_fn_c_check_func "$LINENO" "malloc_trim" "ac_cv_func_malloc_trim"
+if test "x$ac_cv_func_malloc_trim" = xyes
+then :
+  printf "%s\n" "#define HAVE_MALLOC_TRIM 1" >>confdefs.h
+
+fi
 ac_fn_c_check_func "$LINENO" "mkstemp" "ac_cv_func_mkstemp"
 if test "x$ac_cv_func_mkstemp" = xyes
 then :

configure.d/config_os_functions
@@ -29,7 +29,7 @@ AC_CHECK_FUNCS([asprintf        closedir        endnetgrent      ] dnl
                [fgetc_unlocked                                   ] dnl
                [flockfile       funlockfile     getipnodebyname  ] dnl
                [gettimeofday    getlogin        getnetgrent      ] dnl
-               [if_nametoindex  mkstemp                          ] dnl
+               [if_nametoindex  malloc_trim     mkstemp          ] dnl
                [opendir         readdir         regcomp          ] dnl
                [setenv          setitimer       setlocale        ] dnl
                [setnetgrent                                      ] dnl

include/net-snmp/net-snmp-config.h.in
@@ -538,6 +538,9 @@
 /* Define to 1 if you have the <malloc.h> header file. */
 #undef HAVE_MALLOC_H
 
+/* Define to 1 if you have the `malloc_trim' function. */
+#undef HAVE_MALLOC_TRIM
+
 /* Define to 1 if the system has the type `mib2_ipIfStatsEntry_t'. */
 #undef HAVE_MIB2_IPIFSTATSENTRY_T
 