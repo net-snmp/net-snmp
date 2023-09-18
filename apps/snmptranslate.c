net-snmp/net-snmp

commit 8e8d56b7b9471a5427491af7cd881db892deeaf2

libsnmp: Fix a crash in netsnmp_parse_args()
Prevent that netsnmp_parse_args() crashes if one of the following
argument lists is passed: -IS or -Is.

Fixes: https://github.com/net-snmp/net-snmp/issues/692

snmplib/mib.c
@@ -2381,18 +2381,22 @@ snmp_in_options(char *optarg, int argc, char *const *argv)
             netsnmp_ds_toggle_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_READ_UCD_STYLE_OID);
             break;
         case 's':
-            /* What if argc/argv are null ? */
-            if (!*(++cp))
+            if (!*(++cp)) {
                 cp = argv[optind++];
+                if (!cp)
+                    return "?";
+            }
             netsnmp_ds_set_string(NETSNMP_DS_LIBRARY_ID,
                                   NETSNMP_DS_LIB_OIDSUFFIX,
                                   cp);
             return NULL;  /* -Is... is a standalone option, so we're done here */
 
         case 'S':
-            /* What if argc/argv are null ? */
-            if (!*(++cp))
+            if (!*(++cp)) {
                 cp = argv[optind++];
+                if (!cp)
+                    return "?";
+            }
             netsnmp_ds_set_string(NETSNMP_DS_LIBRARY_ID,
                                   NETSNMP_DS_LIB_OIDPREFIX,
                                   cp);

commit 3cfa5e013b3033c224549c4fe6451063899fc857

libsnmp: Fix recently introduced compiler warnings
Fixes: 8e8d56b7b947 ("libsnmp: Fix a crash in netsnmp_parse_args()")

apps/snmptranslate.c
@@ -170,7 +170,8 @@ int
 main(int argc, char *argv[])
 {
     int             arg;
-    char           *current_name = NULL, *cp = NULL;
+    char           *current_name = NULL;
+    const char     *cp = NULL;
     oid             name[MAX_OID_LEN];
     size_t          name_length;
     int             description = 0;

include/net-snmp/library/mib.h
@@ -491,9 +491,9 @@ SOFTWARE.
     NETSNMP_IMPORT
     void            snmp_out_toggle_options_usage(const char *, FILE *);
     NETSNMP_IMPORT
-    char           *snmp_in_toggle_options(char *);
+    const char     *snmp_in_toggle_options(char *);
     NETSNMP_IMPORT
-    char           *snmp_in_options(char *, int, char * const *);
+    const char     *snmp_in_options(char *, int, char * const *);
     NETSNMP_IMPORT
     void            snmp_in_toggle_options_usage(const char *, FILE *);
     NETSNMP_IMPORT

snmplib/mib.c
@@ -2358,7 +2358,7 @@ snmp_out_toggle_options_usage(const char *lead, FILE * outf)
     fprintf(outf, "%sX:  extended index format\n", lead);
 }
 
-char *
+const char *
 snmp_in_options(char *optarg, int argc, char *const *argv)
 {
     char *cp;
@@ -2413,7 +2413,7 @@ snmp_in_options(char *optarg, int argc, char *const *argv)
     return NULL;
 }
 
-char           *
+const char     *
 snmp_in_toggle_options(char *options)
 {
     return snmp_in_options( options, 0, NULL );

snmplib/snmp_parse_args.c
@@ -202,7 +202,7 @@ netsnmp_parse_args(int argc,
                    int flags)
 {
     int             arg, ret, sp = 0;
-    char           *cp;
+    const char     *cp;
     char           *Apsz = NULL;
     char           *Xpsz = NULL;
     char           *Cpsz = NULL;