07cccb8d940daf5b41f5783b9c460248ac5c21e7
Moshe Kaplan <me@moshekaplan.com>
2023-09-26 17:14:05 -0400

    More spelling and grammar fixes

--- agent/mibgroup/snmp-notification-mib/snmpNotifyFilterTable/snmpNotifyFilterTable_interface.c
+++ agent/mibgroup/snmp-notification-mib/snmpNotifyFilterTable/snmpNotifyFilterTable_interface.c
@@ -1632,7 +1632,7 @@ _mfd_snmpNotifyFilterTable_commit(netsnmp_mib_handler *handler,
 
     if (rowreq_ctx->rowreq_flags & MFD_ROW_DIRTY) {
         /*
-         * if we successfully commited this row, set the dirty flag. Use the
+         * if we successfully committed this row, set the dirty flag. Use the
          * current value + 1 (i.e. dirty = # rows changed).
          * this is checked in post_request...
          */
