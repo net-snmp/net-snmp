56795de0a9cd1987007ac9b90868c7972c77f57f
Bart Van Assche <bvanassche@acm.org>
2023-10-08 06:50:25 -0700

    MinGW: Unbreak the Perl module build

--- perl/MakefileSubs.pm
+++ perl/MakefileSubs.pm
@@ -114,9 +114,10 @@ sub AddCommonParams {
 	my $cflags = `$opts->{'nsconfig'} --cflags` or
 	    die "net-snmp-config failed\n";
 	chomp($cflags);
-	# Remove -Wimplicit-fallthrough since it is not supported by older
-	# versions of gcc.
-	$cflags =~ s/-Wimplicit-fallthrough=[0-9]//g;
+	# Remove -Wimplicit-fallthrough and -Wcast-function-type since these are
+	# not supported by older versions of gcc.
+	$cflags =~ s/-Wimplicit-fallthrough=*[0-9]*//g;
+	$cflags =~ s/-Wcast-function-type//g;
 	append($Params->{'CCFLAGS'}, $cflags);
 	append($Params->{'CCFLAGS'}, $Config{'ccflags'});
 	# Suppress known Perl header shortcomings.
