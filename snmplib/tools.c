net-snmp/net-snmp

commit 4b7919e60cd485d3d48a80f78e74ac6874cae479

Apply patch #898464 to fix bugs #896030 and #773412
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9655 06827809-a52a-0410-b366-d66718629ded

snmplib/tools.c
@@ -2,6 +2,8 @@
  * tools.c
  */
 
+#define NETSNMP_TOOLS_C 1 /* dont re-define malloc wrappers here */
+
 #include <net-snmp/net-snmp-config.h>
 
 #include <ctype.h>
@@ -55,6 +57,48 @@
 #include <net-snmp/library/mib.h>
 #include <net-snmp/library/scapi.h>
 
+#ifdef WIN32
+/**
+ * This function is a wrapper for the strdup function.
+ */
+char * netsnmp_strdup( const char * ptr)
+{
+    return strdup(ptr);
+}
+/**
+ * This function is a wrapper for the calloc function.
+ */
+void * netsnmp_calloc(size_t nmemb, size_t size)
+{
+    return calloc(nmemb, size);
+}
+
+/**
+ * This function is a wrapper for the malloc function.
+ */
+void * netsnmp_malloc(size_t size)
+{
+    return malloc(size);
+}
+
+/**
+ * This function is a wrapper for the realloc function.
+ */
+void * netsnmp_realloc( void * ptr, size_t size)
+{
+    return realloc(ptr, size);
+}
+
+/**
+ * This function is a wrapper for the free function.
+ * It calls free only if the calling parameter has a non-zero value.
+ */
+void netsnmp_free( void * ptr)
+{
+    if (ptr)
+        free(ptr);
+}
+#endif /* WIN32 */
 
 /**
  * This function increase the size of the buffer pointed at by *buf, which is

README.win32
@@ -358,42 +358,84 @@ C:\net-snmp-5.1.1>
 *
 ***************************************************************************
 
-Prerequisites 
+ #### Preparation -- These are done one time ####
 
-b. Install ActivePerl in C:\Perl .   Unless you need the advanced features
-   of Perl 5.8, consider that Perl 5.6 is better supported in some circles.
+1. Open the Net-SNMP source, goto the win32 sub-directory
+1.1. Edit net-snmp/net-snmp-config.h and install-net-snmp.bat so that
+   the directories created by the .BAT file are the same as the ones
+   defined in the header file (DEFAULT_MIBDIRS, PERSISTENT_DIRECTORY,
+   SNMPSHAREPATH, and SNMPCONFPATH must be properly formed).
+   The nmakeperl.bat script must also point to the SNMPLIBPATH setting.
 
-1. Open the Net-SNMP source, goto the win32 sub-directory.
-   NOTE: Make sure that you did NOT previous build in this directory,
-   as for instance, for Cygwin or for Mingw.  The reason for this is to
-   avoid reading header files specific to those platforms, which would cause
-   MSVC to fail compiling the modules.
+1.2. Cd to the net-snmp subdirectory.  copy net-snmp-config.h
+     to static-config.h.  Copy net-snmp-config.h to dll-config.h.
+     Remove the line in dll-config.h that defines NETSNMP_NO_DLL
 
-2. Review the "Building with MS VC++" section of this document, then
-   make sure that nmakeperl.bat agrees with install-net-snmp.bat to
-   locate the ".lib" products from the MSVC build, which would be
-   deposited into the SNMPLIBPATH directory.
 
-   If the libraries are in their installed location, you may proceed
-   to step 5 in this procedure.
+ #### Build and install statically linked programs ####
 
-3. Build Net-SNMP using win32.dsw or win32sdk.dsw
-   Close the workspace and exit Visual Studio.
+2. Copy win32\net-snmp\static-config.h win32\net-snmp\net-snmp-config.h
+2.1. Using Visual Studio, open win32sdk.dsw (or win32.dsw).
+   MSVC++ .Net only:  Click 'Yes to All'
+   Click Build - Set Active Configuration (MSVC++ 6.0) and select Release
+     or
+   Click Build - Configuration Manager (MSVC++ .Net) and select Release for
+     the 'Active Solution Configuration'
+   Right Click 'libsnmp' and select Build
+   Click "Build->Batch Build...",
+   Enable all projects for Release then click "Rebuild (All)"
+   If there are no errors, then proceed to the next step.
 
-4. Open a command prompt window using "cmd.exe".
-   Go to the Net-SNMP source base directory.
-   Invoke win32\install-net-snmp to install the built programs.
+2.2. Using a command prompt window, cd to the source base directory.
+   Type:  win32\install-net-snmp to install the programs.
 
-5. Set the environment PATH to locate "nmake", "cl", and "link".
+2.3. Using Visual Studio, Click Build->"Batch Build..."
+   Click Clean to remove the programs from the build directories.
+   Click "File->Close Workspace" (MSVC++ 6.0)
+     or 
+   Click "File->Close Solution" (MSVC++ .Net)
+
+
+ #### Build and install dynamically linked libraries ####
+
+3. Copy win32\net-snmp\dll-config.h win32\net-snmp\net-snmp-config.h
+3.1. Using Visual Studio, open libsdll.dsw
+   MSVC++ .Net only:  Click 'Yes to All'
+   Click "Build->Batch Build...", 
+   Enable all projects for Rlease then click "Rebuild (All)"
+   The "locally defined symbol ... imported" warnings can be ignored.
+   If there are no errors, then proceed to the next step.
+
+3.2. Using a command prompt window, cd to the source base directory.
+   Type:  win32\install-net-snmp to install the programs.
+
+3.3. Using Visual Studio, Click Build->"Batch Build..."
+   Click Clean to remove the programs from the build directories.
+   Click "File->Close Workspace" (MSVC++ 6.0)
+     or 
+   Click "File->Close Solution" (MSVC++ .Net)
+
+
+ #### Build and test Perl SNMP modules ####
+
+4. Using a command prompt window, cd to the source base directory.
+
+4.1. Set the environment PATH to locate "nmake", "cl", and "link".
    Visual Studio installs a VCVARS32.BAT for this purpose.
 
-6. Invoke the nmakeperl.bat to build the Perl SNMP modules.
+4.2. Invoke win32\nmakeperl.bat to build the Perl SNMP modules.
    If you see errors, review the "nmake.out" file first.  If no errors there,
    then the modules built correctly, but the tests did not rigourously
    prove the mettle of the modules.  Review "nmaketest.out".  If the
    first three sections mostly pass, the modules are well formed.
 
-7. The final step is to invoke "nmake install".  If no errors occurred,
+NOTE: If the tests fail, there may be a perl application left hanging.
+  Use the Task Manager to remove any stale perl or snmp*.exe process,
+
+
+ #### Install Perl SNMP modules ####
+
+5. The final step is to invoke "nmake install".  If no errors occurred,
    then the SNMP modules are available for use by your Perl programs.
 
 

acconfig.h
@@ -671,6 +671,10 @@
 #   define NETSNMP_STATIC_INLINE static
 #endif
 
+#ifndef NETSNMP_IMPORT
+#  define NETSNMP_IMPORT extern
+#endif
+
 #if defined(HAVE_NLIST) && defined(STRUCT_NLIST_HAS_N_VALUE) && !defined(DONT_USE_NLIST) && !defined(NO_KMEM_USAGE)
 #define CAN_USE_NLIST
 #endif

include/net-snmp/library/getopt.h
@@ -5,9 +5,9 @@
 extern          "C" {
 #endif
 
-    extern int      getopt(int, char *const *, const char *);
-    extern char    *optarg;
-    extern int      optind, opterr, optopt, optreset;
+NETSNMP_IMPORT int   getopt(int, char *const *, const char *);
+NETSNMP_IMPORT char *optarg;
+NETSNMP_IMPORT int   optind, opterr, optopt, optreset;
 
 #ifdef __cplusplus
 }

include/net-snmp/net-snmp-config.h.in
@@ -1417,6 +1417,10 @@
 #   define NETSNMP_STATIC_INLINE static
 #endif
 
+#ifndef NETSNMP_IMPORT
+#  define NETSNMP_IMPORT extern
+#endif
+
 #if defined(HAVE_NLIST) && defined(STRUCT_NLIST_HAS_N_VALUE) && !defined(DONT_USE_NLIST) && !defined(NO_KMEM_USAGE)
 #define CAN_USE_NLIST
 #endif

win32/libsdll.dsw
@@ -1,0 +1,65 @@
+Microsoft Developer Studio Workspace File, Format Version 6.00
+# WARNING: DO NOT EDIT OR DELETE THIS WORKSPACE FILE!
+
+###############################################################################
+
+Project: "libagent"=".\libagent\libagent.dsp" - Package Owner=<4>
+
+Package=<5>
+{{{
+}}}
+
+Package=<4>
+{{{
+}}}
+
+###############################################################################
+
+Project: "libhelpers"=".\libhelpers\libhelpers.dsp" - Package Owner=<4>
+
+Package=<5>
+{{{
+}}}
+
+Package=<4>
+{{{
+}}}
+
+###############################################################################
+
+Project: "libsnmp_dll"=".\libsnmp_dll\libsnmp_dll.dsp" - Package Owner=<4>
+
+Package=<5>
+{{{
+}}}
+
+Package=<4>
+{{{
+}}}
+
+###############################################################################
+
+Project: "netsnmpmibs"=".\netsnmpmibs\netsnmpmibs.dsp" - Package Owner=<4>
+
+Package=<5>
+{{{
+}}}
+
+Package=<4>
+{{{
+}}}
+
+###############################################################################
+
+Global:
+
+Package=<5>
+{{{
+}}}
+
+Package=<3>
+{{{
+}}}
+
+###############################################################################
+

win32/libsnmp_dll/libsnmp.def
@@ -391,4 +391,11 @@ EXPORTS
 	get_logh_head
 	dbg_tokens				DATA
 	debug_num_tokens		DATA
+	create_winpipe_transport
+	get_temp_file_pattern
+	netsnmp_strdup
+	netsnmp_calloc
+	netsnmp_malloc
+	netsnmp_realloc
+	netsnmp_free
 

win32/net-snmp/net-snmp-config.h
@@ -310,9 +310,6 @@ extern "C" {
 /* Define if you have the strchr function.  */
 #define HAVE_STRCHR 1
 
-/* use win32 strdup */
-#define strdup _strdup
-
 /* Define if you have the strtol function.  */
 #define HAVE_STRTOL 1
 
@@ -1005,6 +1002,65 @@ typedef unsigned short mode_t;
 #   undef  NETSNMP_STATIC_INLINE
 #   define NETSNMP_STATIC_INLINE static
 #endif
+
+#ifdef WIN32
+
+/* define NETSNMP_NO_DLL if building non-DLL netsnmp */
+#define NETSNMP_NO_DLL 1
+
+  #ifndef NETSNMP_NO_DLL
+    #ifndef NETSNMP_DLL
+
+      #if defined(_MSC_VER)
+        #define NETSNMP_IMPORT __declspec(dllimport)
+      #endif
+    #else
+      #if defined(_MSC_VER)
+        #define NETSNMP_IMPORT __declspec(dllexport)
+      #endif
+
+    #endif   /* NETSNMP_DLL */
+  #endif     /* ! NETSNMP_NO_DLL */
+
+/*
+ * DLL decoration, if used at all, must be consistent.
+ * This is why NETSNMP_IMPORT is really an export decoration
+ * when it is encountered in a header file that is included
+ * during the compilation of a library source file.
+ * NETSNMP_DLL is set by the MSVC libsnmp_dll project
+ *  in order to signal that the library sources are being compiled.
+ * NETSNMP_NO_DLL ignores the preceding, and renders
+ *  the NETSNMP_IMPORT definitions harmless.
+ */
+
+#endif       /* WIN32 */
+
+#ifndef NETSNMP_IMPORT
+#  define NETSNMP_IMPORT extern
+#endif
+
+
+/* wrap alloc functions to use DLL's memory heap */
+/* This is not done in tools.c, where these wrappers are defined */
+
+#ifdef WIN32
+
+  #ifndef NETSNMP_NO_DLL
+    #ifndef NETSNMP_TOOLS_C
+      #define strdup    netsnmp_strdup
+      #define calloc    netsnmp_calloc
+      #define malloc    netsnmp_malloc
+      #define realloc   netsnmp_realloc
+      #define free      netsnmp_free
+    #endif
+  #endif
+
+  /* XXX next definition may be needed only for MSVC */
+  #ifndef strdup
+    #define strdup _strdup
+  #endif
+
+#endif /* WIN32 */
 
 #ifdef __cplusplus
 }

commit c4732b8dd51e9b1e77839ad410773a847c4b8746

libsnmp: Remove a #define directive
No code ever tests the NETSNMP_TOOLS_C macro. Hence remove the directive
that defines it.

Fixes: 4b7919e60cd4 ("Apply patch #898464 to fix bugs #896030 and #773412")

snmplib/tools.c
@@ -7,8 +7,6 @@
  * distributed with the Net-SNMP package.
  */
 
-#define NETSNMP_TOOLS_C 1 /* don't re-define malloc wrappers here */
-
 #ifdef HAVE_CRTDBG_H
 /*
  * Define _CRTDBG_MAP_ALLOC such that in debug builds (when _DEBUG has been