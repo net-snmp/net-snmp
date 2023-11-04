net-snmp/net-snmp

commit 8c301c1e06004aa4095f956bff5f399104e71127

IF-MIB: Do not scan for PCI devices if no PCI bus is available
Fixes: https://github.com/net-snmp/net-snmp/issues/271

agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -143,10 +143,10 @@ static void init_libpci(void)
     struct stat stbuf;
 
     /*
-     * When snmpd is run inside an OpenVZ container /proc/bus/pci is not
-     * available.
+     * When snmpd is run inside an OpenVZ container or on a Raspberry Pi system
+     * /proc/bus/pci is not available.
      */
-    if (stat("/proc/vz", &stbuf) == 0)
+    if (stat("/proc/bus/pci", &stbuf) == 0)
         return;
 
     pci_access = pci_alloc();

commit ea6c54f508ca8897961f4faa8d7b04793151db78

IF-MIB: Fix the test for skipping PCI bus scanning
Reported-by: https://github.com/abutcher-gh
Fixes: 8c301c1e0600 ("IF-MIB: Do not scan for PCI devices if no PCI bus is available")

agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -143,7 +143,7 @@ static void init_libpci(void)
      * When snmpd is run inside an OpenVZ container or on a Raspberry Pi system
      * /proc/bus/pci is not available.
      */
-    if (stat("/proc/bus/pci", &stbuf) == 0)
+    if (stat("/proc/bus/pci", &stbuf) < 0)
         return;
 
     pci_access = pci_alloc();