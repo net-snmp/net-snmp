net-snmp/net-snmp

commit 460a97c2ba626594ac1aa548bfc6b8377451d0d9

Give memory back to system post snmp operation

snmplib/container.c
@@ -509,6 +509,7 @@ void CONTAINER_CLEAR(netsnmp_container *x, netsnmp_container_obj_func *f,
         x = x->prev;
     }
     x->clear(x, f, c);
+    malloc_trim(0);
 }
 
 #ifndef NETSNMP_FEATURE_REMOVE_CONTAINER_FREE_ALL

commit 759421b42563832fcd28559d438649b0f97f0389

libsnmp: Add a missing include directive
Fixes: 460a97c2ba62 ("Give memory back to system post snmp operation")

snmplib/container.c
@@ -22,6 +22,7 @@
 #include <net-snmp/library/container_null.h>
 #include "factory.h"
 
+#include <malloc.h>
 #include <stdint.h>
 
 netsnmp_feature_child_of(container_all, libnetsnmp);