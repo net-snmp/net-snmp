net-snmp/net-snmp

commit e5d60f0c46c58b99130e8ae8098dc5b192091ef6

DISMAN-EVENT-MIB: Initialize 'name' and 'name_buf'
This patch suppresses the following two Coverity reports:

CID 407579:  Uninitialized variables  (UNINIT)
Using uninitialized value "*name_buf" when calling "memcpy". [Note: The source code implementation of the function has been overridden by a builtin model.]

CID 407578:  Uninitialized variables  (UNINIT)
Using uninitialized value "name_buf_len".

agent/mibgroup/disman/event/mteTriggerConf.c
@@ -166,8 +166,8 @@ parse_mteMonitor(const char *token, const char *line)
 
     int    seen_name = 0;
     char   oid_name_buf[SPRINT_MAX_LEN];
-    oid    name_buf[MAX_OID_LEN];
-    size_t name_buf_len;
+    oid    name_buf[MAX_OID_LEN] = { };
+    size_t name_buf_len = 0;
     u_char op    = 0;
     long   value = 0;
 

commit 47a550defd0209d57d34e50290942fb6b1c286f5

DISMAN-EVENT-MIB: Unbreak the MSVC build
Fixes: e5d60f0c46c5 ("DISMAN-EVENT-MIB: Initialize 'name' and 'name_buf'")

agent/mibgroup/disman/event/mteTriggerConf.c
@@ -166,7 +166,7 @@ parse_mteMonitor(const char *token, const char *line)
 
     int    seen_name = 0;
     char   oid_name_buf[SPRINT_MAX_LEN];
-    oid    name_buf[MAX_OID_LEN] = { };
+    oid    name_buf[MAX_OID_LEN];
     size_t name_buf_len = 0;
     u_char op    = 0;
     long   value = 0;
@@ -188,6 +188,7 @@ parse_mteMonitor(const char *token, const char *line)
     memset( buf,   0, sizeof(buf));
     memset( tname, 0, sizeof(tname));
     memset( ename, 0, sizeof(ename));
+    memset(name_buf, 0, sizeof(name_buf));
     for (cp = copy_nword_const(line, buf, SPRINT_MAX_LEN);
          ;
          cp = copy_nword_const(cp,   buf, SPRINT_MAX_LEN)) {