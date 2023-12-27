net-snmp/net-snmp

commit 460a97c2ba626594ac1aa548bfc6b8377451d0d9

Give memory back to system post snmp operation

snmplib/container.c
@@ -509,6 +509,7 @@ void CONTAINER_CLEAR(netsnmp_container *x, netsnmp_container_obj_func *f,
         x = x->prev;
     }
     x->clear(x, f, c);
+    malloc_trim(0);
 }
 
 #ifndef NETSNMP_FEATURE_REMOVE_CONTAINER_FREE_ALL

commit 72bd53ff5a10cb85d37f9d65c4bd5869a38b7760

libsnmp: Unbreak the MSVC build
Fixes: 460a97c2ba62 ("Give memory back to system post snmp operation")

snmplib/container.c
@@ -22,7 +22,9 @@
 #include <net-snmp/library/container_null.h>
 #include "factory.h"
 
+#ifdef HAVE_MALLOC_H
 #include <malloc.h>
+#endif
 #include <stdint.h>
 
 netsnmp_feature_child_of(container_all, libnetsnmp);