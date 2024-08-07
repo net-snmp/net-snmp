net-snmp/net-snmp

commit 5ad6dce71c380c10be2a9420856466fa5a33ced9

Linux: IF-MIB: Handle tun interfaces correctly
Prevent that snmpd crashes on tun interfaces.

Fixes: b78b674f90cb ("Linux: IF-MIB: Use libnl-route-3 instead of reading /proc/net/dev")
Fixes: https://github.com/net-snmp/net-snmp/issues/836

agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -663,20 +663,22 @@ static void netsnmp_retrieve_one_link_info(struct rtnl_link *rtnl_link, int fd,
                                            int load_stats)
 {
     struct nl_addr *nl_addr = rtnl_link_get_addr(rtnl_link);
-    void *paddr = nl_addr_get_binary_addr(nl_addr);
-    int paddr_len = nl_addr_get_len(nl_addr);
-    unsigned int link_flags;
 
-    free(entry->paddr);
-    entry->paddr = netsnmp_memdup(paddr, paddr_len);
-    entry->paddr_len = paddr_len;
+    if (nl_addr) {
+        void *paddr = nl_addr_get_binary_addr(nl_addr);
+        int paddr_len = nl_addr_get_len(nl_addr);
+
+        free(entry->paddr);
+        entry->paddr = netsnmp_memdup(paddr, paddr_len);
+        entry->paddr_len = paddr_len;
+    }
     entry->type = netsnmp_convert_arphrd_type(
                               rtnl_link_get_arptype(rtnl_link));
     if (entry->type == 0)
         netsnmp_guess_interface_type(entry);
     netsnmp_derive_interface_id(entry);
     /* IFF_* flags */
-    link_flags = rtnl_link_get_flags(rtnl_link);
+    const unsigned int link_flags = rtnl_link_get_flags(rtnl_link);
     netsnmp_process_link_flags(entry, link_flags);
     /* MTU */
     entry->mtu = rtnl_link_get_mtu(rtnl_link);

commit 49a5127b5dec3455f103867165510a7a4b64e5ae

Linux: IF-MIB: Improve handling of tun interfaces further
Prevent that ifPhysAddress_get() triggers a NULL pointer dereference if
no hardware address is present.

Fixes: 5ad6dce71c38 ("Linux: IF-MIB: Handle tun interfaces correctly")

agent/mibgroup/if-mib/data_access/interface_linux.c
@@ -663,15 +663,12 @@ static void netsnmp_retrieve_one_link_info(struct rtnl_link *rtnl_link, int fd,
                                            int load_stats)
 {
     struct nl_addr *nl_addr = rtnl_link_get_addr(rtnl_link);
+    void *paddr = nl_addr ? nl_addr_get_binary_addr(nl_addr) : NULL;
+    int paddr_len = nl_addr ? nl_addr_get_len(nl_addr) : 0;
 
-    if (nl_addr) {
-        void *paddr = nl_addr_get_binary_addr(nl_addr);
-        int paddr_len = nl_addr_get_len(nl_addr);
-
-        free(entry->paddr);
-        entry->paddr = netsnmp_memdup(paddr, paddr_len);
-        entry->paddr_len = paddr_len;
-    }
+    free(entry->paddr);
+    entry->paddr = netsnmp_memdup(paddr, paddr_len);
+    entry->paddr_len = paddr_len;
     entry->type = netsnmp_convert_arphrd_type(
                               rtnl_link_get_arptype(rtnl_link));
     if (entry->type == 0)

agent/mibgroup/if-mib/ifTable/ifTable.c
@@ -591,6 +591,17 @@ ifSpeed_get(ifTable_rowreq_ctx * rowreq_ctx, u_long * ifSpeed_val_ptr)
     return MFD_SUCCESS;
 }                               /* ifSpeed_get */
 
+static int is_zero(const char *p, unsigned int len)
+{
+    unsigned int i;
+
+    for (i = 0; i < len; i++)
+        if (p[i])
+            return FALSE;
+
+    return TRUE;
+}
+
 /*---------------------------------------------------------------------
  * IF-MIB::ifEntry.ifPhysAddress
  * ifPhysAddress is subid 6 of ifEntry.
@@ -659,12 +670,8 @@ ifPhysAddress_get(ifTable_rowreq_ctx * rowreq_ctx,
 
     netsnmp_assert(NULL != rowreq_ctx);
 
-    if ((rowreq_ctx->data.ifPhysAddress[0] == 0) &&
-        (rowreq_ctx->data.ifPhysAddress[1] == 0) &&
-        (rowreq_ctx->data.ifPhysAddress[2] == 0) &&
-        (rowreq_ctx->data.ifPhysAddress[3] == 0) &&
-        (rowreq_ctx->data.ifPhysAddress[4] == 0) &&
-        (rowreq_ctx->data.ifPhysAddress[5] == 0)) {
+    if (is_zero(rowreq_ctx->data.ifPhysAddress,
+                rowreq_ctx->data.ifPhysAddress_len)) {
         /*
          * all 0s = empty string
          */