net-snmp/net-snmp

commit 589e9c56fad29fbbb5b3adb265cc4644d1aca6b8

libsnmp: Make OID_LENGTH() more strict
Make OID_LENGTH() trigger a build error if its argument is a pointer.

include/net-snmp/library/asn1.h
@@ -47,7 +47,18 @@ SOFTWARE.
 #define MAX_NAME_LEN	    MAX_OID_LEN /* obsolete. use MAX_OID_LEN */
 #endif
 
-#define OID_LENGTH(x)  (sizeof(x)/sizeof(oid))
+#ifdef __GNUC__
+/*
+ * If x is an array, x and &(x)[0] have different types. If x is a pointer,
+ * x and &(x)[0] have the same type. Trigger a build error if x is a pointer
+ * by making the compiler evaluate sizeof(int[-1]).
+ */
+#define OID_LENGTH(x)                                                   \
+    (sizeof(x) / sizeof((x)[0]) +                                       \
+     sizeof(int[-__builtin_types_compatible_p(typeof(x), typeof(&(x)[0]))]))
+#else
+#define OID_LENGTH(x)  (sizeof(x) / sizeof((x)[0]))
+#endif
 
 
 #define ASN_BOOLEAN	    ((u_char)0x01)

commit d65c48a62a32a5e22afe599ccd3985305c638c6e

libsnmp: Fix OID_LENGTH macro for build with -pedantic
Fixes: 589e9c56fad29fbbb5b3adb265cc4644d1aca6b8

include/net-snmp/library/asn1.h
@@ -54,7 +54,7 @@ SOFTWARE.
 #define MAX_NAME_LEN	    MAX_OID_LEN /* obsolete. use MAX_OID_LEN */
 #endif
 
-#ifdef __GNUC__
+#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
 /*
  * If x is an array, x and &(x)[0] have different types. If x is a pointer,
  * x and &(x)[0] have the same type. Trigger a build error if x is a pointer