net-snmp/net-snmp

commit 993728c977d416b15ea74ae8b0b03ebdf1c07ec8

libsnmp: Handle integer overflows in asn_predict_length()

snmplib/snmpusm.c
@@ -990,6 +990,9 @@ asn_predict_int_length(int type, long number, size_t len)
 static int
 asn_predict_length(int type, u_char * ptr, size_t u_char_len)
 {
+    /* Check for integer overflow. */
+    if (1 + 3 + u_char_len < 1 + 3)
+        return -1;
 
     if (type & ASN_SEQUENCE)
         return 1 + 3 + u_char_len;
@@ -999,6 +1002,10 @@ asn_predict_length(int type, u_char * ptr, size_t u_char_len)
         memcpy(&value, ptr, u_char_len);
         u_char_len = asn_predict_int_length(type, value, u_char_len);
     }
+
+    /* Check for integer overflow. */
+    if (1 + 3 + u_char_len < 1 + 3)
+        return -1;
 
     if (u_char_len < 0x80)
         return 1 + 1 + u_char_len;

commit e500d823c1242d3859f671ec5dd51c9fc6b9eeb5

libsnmp: Rework an integer overflow check
Rework an integer overflow check such that it no longer triggers a Coverity
complaint.

Fixes: 993728c977d4 ("libsnmp: Handle integer overflows in asn_predict_length()")

snmplib/snmpusm.c
@@ -990,7 +990,7 @@ static int
 asn_predict_length(int type, u_char * ptr, size_t u_char_len)
 {
     /* Check for integer overflow. */
-    if (1 + 3 + u_char_len < 1 + 3)
+    if (u_char_len > SIZE_MAX - (1 + 3))
         return -1;
 
     if (type & ASN_SEQUENCE)