net-snmp/net-snmp

commit 84aba8f8f878b977e98c46af0584723885660a3e

libsnmp: Fix a memory leak in the MIB parser
Fixes: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=62448

snmplib/parse.c
@@ -4343,6 +4343,16 @@ scan_objlist(struct node *root, struct module *mp, struct objgroup *list, const 
     mibLine = oLine;
 }
 
+static void free_objgroup(struct objgroup *o)
+{
+    while (o) {
+        struct objgroup *next = o->next;
+
+        free(o);
+        o = next;
+    }
+}
+
 /*
  * Parses a mib file and returns a linked list of nodes found in the file.
  * Returns NULL on error.
@@ -4630,6 +4640,12 @@ free_mib:
         np = root->next;
         free_node(root);
     }
+    free_objgroup(objgroups);
+    objgroups = NULL;
+    free_objgroup(objects);
+    objects = NULL;
+    free_objgroup(notifs);
+    notifs = NULL;
     return NULL;
 }
 

commit 9425b05b663c55e12bc4fccec45a520c333ee343

libsnmp: Fix a memory leak in free_objgroup()
Fixes: 84aba8f8f878 ("libsnmp: Fix a memory leak in the MIB parser")
Fixes: https://issues.oss-fuzz.com/issues/380322456

snmplib/parse.c
@@ -4377,6 +4377,7 @@ static void free_objgroup(struct objgroup *o)
     while (o) {
         struct objgroup *next = o->next;
 
+        free(o->name);
         free(o);
         o = next;
     }