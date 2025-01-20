net-snmp/net-snmp

commit 1c6e66b3122b7fd878d17f40664adf831d1a2c7a

libsnmp: Fix homedir lookup also for configuration

include/net-snmp/library/tools.h
@@ -290,6 +290,9 @@ extern          "C" {
     NETSNMP_IMPORT
     int             netsnmp_string_time_to_secs(const char *time_string);
 
+    NETSNMP_IMPORT
+    const char      *netsnmp_gethomedir();
+
 #ifdef __cplusplus
 }
 #endif

snmplib/tools.c
@@ -1413,3 +1413,12 @@ netsnmp_string_time_to_secs(const char *time_string) {
     return secs;
 }
 #endif /* NETSNMP_FEATURE_REMOVE_STRING_TIME_TO_SECS */
+
+const char *netsnmp_gethomedir() {
+    const char *homepath = netsnmp_getenv("HOME");
+#ifdef _WIN32
+    if (!homepath)
+        homepath = netsnmp_getenv("USERPROFILE");
+#endif
+    return homepath;
+}

apps/encode_keychange.c
@@ -375,7 +375,7 @@ usage_synopsis(FILE * ofp)
 void
 usage_to_file(FILE * ofp)
 {
-    char           *s;
+    const char *s;
 
     usage_synopsis(ofp);
 
@@ -388,7 +388,7 @@ usage_to_file(FILE * ofp)
     Ku=>Kul, and to hash the old Kul with the random bits.\n\
 \n\
     Passphrase will be taken from the first successful source as follows:\n",
-    (s = getenv("HOME")) ? s : "$HOME", local_passphrase_filename,
+    (s = netsnmp_gethomedir()) ? s : "$HOME", local_passphrase_filename,
    "-f will require reading from the stdin/terminal, ignoring a) and b).\n\
     -P will prevent prompts for passphrases to stdout from being printed.\n\
 \n\
@@ -461,7 +461,8 @@ get_user_passphrases(void)
 
     char           *obuf = NULL, *nbuf = NULL;
 
-    char            path[SNMP_MAXBUF], buf[SNMP_MAXBUF], *s = NULL;
+    char            path[SNMP_MAXBUF], buf[SNMP_MAXBUF];
+    const char     *s = NULL;
 
     struct stat     statbuf;
     FILE           *fp = NULL;
@@ -484,7 +485,7 @@ get_user_passphrases(void)
      * path for existence and access first.  Refuse to read
      * if the permissions are wrong.
      */
-    s = getenv("HOME");
+    s = netsnmp_gethomedir();
     snprintf(path, sizeof(path), "%s/%s", s, PASSPHRASE_DIR);
     path[ sizeof(path)-1 ] = 0;
 

snmplib/mib.c
@@ -2622,17 +2622,13 @@ netsnmp_get_mib_directory(void)
 void
 netsnmp_fixup_mib_directory(void)
 {
-    char *homepath = netsnmp_getenv("HOME");
+    const char *homepath = netsnmp_gethomedir();
     char *mibpath = netsnmp_get_mib_directory();
     char *oldmibpath = NULL;
     char *ptr_home;
     char *new_mibpath;
 
     DEBUGTRACE;
-#ifdef _WIN32
-    if (!homepath)
-      homepath = netsnmp_getenv("USERPROFILE");
-#endif
     if (homepath && mibpath) {
         DEBUGMSGTL(("fixup_mib_directory", "mib directories '%s'\n", mibpath));
         while ((ptr_home = strstr(mibpath, "$HOME"))) {

snmplib/read_config.c
@@ -1151,11 +1151,11 @@ const char     *
 get_configuration_directory(void)
 {
     char            defaultPath[SPRINT_MAX_LEN];
-    char           *homepath;
+    const char     *homepath;
 
     if (NULL == netsnmp_ds_get_string(NETSNMP_DS_LIBRARY_ID, 
 				      NETSNMP_DS_LIB_CONFIGURATION_DIR)) {
-        homepath = netsnmp_getenv("HOME");
+        homepath = netsnmp_gethomedir();
         snprintf(defaultPath, sizeof(defaultPath), "%s%c%s%c%s%s%s%s",
                 SNMPCONFPATH, ENV_SEPARATOR_CHAR,
                 SNMPSHAREPATH, ENV_SEPARATOR_CHAR, SNMPLIBPATH,

commit 0e27a7d8c2f9c84fc641cc61cf24791da8a8198f

libsnmp: Fix the netsnmp_gethomedir() declaration
Fixes: 1c6e66b3122b ("libsnmp: Fix homedir lookup also for configuration")

include/net-snmp/library/tools.h
@@ -291,7 +291,7 @@ extern          "C" {
     int             netsnmp_string_time_to_secs(const char *time_string);
 
     NETSNMP_IMPORT
-    const char      *netsnmp_gethomedir();
+    const char      *netsnmp_gethomedir(void);
 
 #ifdef __cplusplus
 }

snmplib/tools.c
@@ -1414,7 +1414,7 @@ netsnmp_string_time_to_secs(const char *time_string) {
 }
 #endif /* NETSNMP_FEATURE_REMOVE_STRING_TIME_TO_SECS */
 
-const char *netsnmp_gethomedir() {
+const char *netsnmp_gethomedir(void) {
     const char *homepath = netsnmp_getenv("HOME");
 #ifdef _WIN32
     if (!homepath)