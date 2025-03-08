net-snmp/net-snmp

commit 8c9ec0108c242acea173386e58ab4b8ee75fed7b

move fuzz-test target to just inside testing

ci/build.sh
@@ -54,7 +54,7 @@ case "$MODE" in
 	fi;;
 esac
 make -s -j"${nproc}" || exit $?
-make -s fuzz-test || exit $?
+make -C testing -s fuzz-test || exit $?
 case "$MODE" in
     disable-set|mini*|read-only)
         exit 0;;

Makefile.in
@@ -79,13 +79,6 @@ FTOTHERTARGS=@PERLFEATURES@ @PYTHONFEATURES@
 #
 all:    sedscript EXAMPLE.conf  @FEATURETARGS@ standardall net-snmp-config-x net-snmp-create-v3-user @PERLTARGS@ @PYTHONTARGS@
 
-fuzz-test: all
-	if grep mibgroup/agentx/protocol.o agent/Makefile > /dev/null 2>&1 &&	\
-	   ! grep NETSNMP_FEATURE_REMOVE_MIB_SNPRINT		\
-	   include/net-snmp/library/features.h >/dev/null 2>&1; then\
-		cd testing && make fuzz-tests;			\
-	fi
-
 start-flag:
 	@touch build-in-progress-flag
 

commit 46f98a838fc13c6570a58debb69b193412e088a8

ci/build.sh: Fix the CI build
Only build the fuzz tests in regular mode. Fix the name of the Makefile target.

Fixes: 8c9ec0108c24 ("move fuzz-test target to just inside testing")

ci/build.sh
@@ -54,8 +54,11 @@ case "$MODE" in
 	fi;;
 esac
 make -s -j"${nproc}" || exit $?
-make -C testing -s fuzz-test || exit $?
 case "$MODE" in
     disable-set|mini*|read-only)
-        exit 0;;
+	exit 0
+	;;
+    *)
+	make -C testing -s fuzz-tests || exit $?
+	;;
 esac