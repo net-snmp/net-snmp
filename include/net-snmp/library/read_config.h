net-snmp/net-snmp

commit 5130d0cdc6d928b2209b33229da027aea527dbc3

libsnmp: Suppress a compiler warning
Suppress the following (false positive?) compiler warning:

read_config.c: In function ‘read_config_save_objid’:
read_config.c:2130:9: warning: null destination pointer [-Wformat-overflow=]
 2130 |         sprintf(saveto, ".%" NETSNMP_PRIo "d", objid[i]);
      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

include/net-snmp/library/read_config.h
@@ -107,7 +107,8 @@ extern          "C" {
                                                  size_t * len);
     NETSNMP_IMPORT
     char           *read_config_save_objid(char *saveto, oid * objid,
-                                           size_t len);
+                                           size_t len)
+        __attribute__((nonnull (1, 2)));
     NETSNMP_IMPORT
     char           *read_config_read_data(int type, char *readfrom,
                                           void *dataptr, size_t * len);

commit 29f15dcede9c650014e5ba083cf1bfa83b74eeeb

libsnmp: Fix an incorrect NONNULL annotation
Without this change, the following appears when running test
T031snmpv3dynamicinform with the undefined behavior sanitizer enabled:

snmpusm.c:4212:9: runtime error: null pointer passed as argument 2, which is declared to never be null

Fixes: 5130d0cdc6d9 ("libsnmp: Suppress a compiler warning")

include/net-snmp/library/read_config.h
@@ -107,7 +107,7 @@ extern          "C" {
                                                  size_t * len);
     NETSNMP_IMPORT
     char           *read_config_save_objid(char *saveto, const oid * objid,
-                                           size_t len) NETSNMP_NONNULL(1, 2);
+                                           size_t len) NETSNMP_NONNULL(1);
     NETSNMP_IMPORT
     char           *read_config_read_data(int type, char *readfrom,
                                           void *dataptr, size_t * len);