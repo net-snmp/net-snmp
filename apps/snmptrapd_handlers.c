net-snmp/net-snmp

commit ce1c9d4455a405d2ed8a2ac7b26b778e008e86d5

snmptrapd: Fix a buffer overflow triggered by an empty enterprise OID
Fixes: https://issues.oss-fuzz.com/u/1/issues/457106694

apps/snmptrapd_handlers.c
@@ -1115,7 +1115,7 @@ snmp_input(int op, netsnmp_session *session,
             if (pdu->trap_type == SNMP_TRAP_ENTERPRISESPECIFIC) {
                 trapOidLen = pdu->enterprise_length;
                 memcpy(trapOid, pdu->enterprise, sizeof(oid) * trapOidLen);
-                if (trapOid[trapOidLen - 1] != 0) {
+                if (trapOidLen >= 0 && trapOid[trapOidLen - 1] != 0) {
                     trapOid[trapOidLen++] = 0;
                 }
                 trapOid[trapOidLen++] = pdu->specific_type;

commit ca51e5808b707f7d317d2503600df81fb52cba02

snmptrapd: Really fix a buffer overflow triggered by an empty enterprise OID
Fixes: ce1c9d4455a4 ("snmptrapd: Fix a buffer overflow triggered by an empty enterprise OID")
Fixes: https://issues.oss-fuzz.com/issues/471518953

apps/snmptrapd_handlers.c
@@ -1115,7 +1115,7 @@ snmp_input(int op, netsnmp_session *session,
             if (pdu->trap_type == SNMP_TRAP_ENTERPRISESPECIFIC) {
                 trapOidLen = pdu->enterprise_length;
                 memcpy(trapOid, pdu->enterprise, sizeof(oid) * trapOidLen);
-                if (trapOidLen >= 0 && trapOid[trapOidLen - 1] != 0) {
+                if (trapOidLen > 0 && trapOid[trapOidLen - 1] != 0) {
                     trapOid[trapOidLen++] = 0;
                 }
                 trapOid[trapOidLen++] = pdu->specific_type;