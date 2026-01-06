net-snmp/net-snmp

commit f975916a847de75ad257383dd4f6af653d0c946b

apps: Use netsnmp_cleanup_session()
The changes compared to an earlier attempt (commit 21b16eba6024 ("apps: Use
netsnmp_cleanup_session()")) are as follows:
- netsnmp_cleanup_session() is now called for all session objects allocated
  on the stack instead of only for a subset of these objects.
- A copy of strings is assigned to netsnmp_session members.
- If an snmp_close() call is followed by a netsnmp_cleanup_session() call,
  the netsnmp_session object is cleared to prevent a double free.

This patch should not reintroduce
https://github.com/net-snmp/net-snmp/issues/707.

apps/snmpget.c
@@ -249,8 +249,10 @@ main(int argc, char *argv[])
 
 close_session:
     snmp_close(ss);
+    memset(&session, 0, sizeof(session));
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }                               /* end main() */

apps/snmpset.c
@@ -281,8 +281,10 @@ main(int argc, char *argv[])
 
 close_session:
     snmp_close(ss);
+    memset(&session, 0, sizeof(session));
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

apps/snmptrap.c
@@ -378,9 +378,11 @@ main(int argc, char *argv[])
 
 close_session:
     snmp_close(ss);
+    memset(&session, 0, sizeof(session));
     snmp_shutdown(NETSNMP_APPLICATION_CONFIG_TYPE);
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

agent/agent_trap.c
@@ -367,7 +367,7 @@ netsnmp_create_v1v2_notification_session(const char *sink, const char* sinkport,
     session.callback = handle_disconnect_packet;
     session.version = version;
     if (com) {
-        session.community = (u_char *) NETSNMP_REMOVE_CONST(char *, com);
+        session.community = (u_char *)strdup(com);
         session.community_len = strlen(com);
     }
 
@@ -396,7 +396,7 @@ netsnmp_create_v1v2_notification_session(const char *sink, const char* sinkport,
             client_addr = "localhost";
         tspec.source = client_addr;
     }
-    session.localname = NETSNMP_REMOVE_CONST(char *,tspec.source);
+    session.localname = tspec.source ? strdup(tspec.source) : NULL;
 
     tspec.application = "snmptrap";
     if (NULL == sinkport)
@@ -413,9 +413,12 @@ netsnmp_create_v1v2_notification_session(const char *sink, const char* sinkport,
         /** diagnose snmp_open errors with the input netsnmp_session pointer */
         snmp_sess_perror("snmpd: netsnmp_create_notification_session",
                          &session);
+        netsnmp_cleanup_session(&session);
         /* transport freed by snmp_add */
         return NULL;
     }
+
+    netsnmp_cleanup_session(&session);
 
     rc = netsnmp_add_closable_notification_session(sesp,
                                                    !(t->flags & NETSNMP_TRANSPORT_FLAG_STREAM),
@@ -1611,13 +1614,13 @@ netsnmp_create_v3user_notification_session(const char *dest, const char *user,
 
     session.version = SNMP_VERSION_3;
 
-    session.peername = NETSNMP_REMOVE_CONST(char*,dest);
+    session.peername = strdup(dest);
 
-    session.securityName = NETSNMP_REMOVE_CONST(char*,user);
+    session.securityName = strdup(user);
     session.securityNameLen = strlen(user);
 
-    if (NULL != context) {
-        session.contextName = NETSNMP_REMOVE_CONST(char*,context);
+    if (context) {
+        session.contextName = strdup(context);
         session.contextNameLen = strlen(context);
     }
 
@@ -1701,15 +1704,12 @@ netsnmp_create_v3user_notification_session(const char *dest, const char *user,
                                                   notif_profile) != 1) {
         DEBUGMSGTL(("trap:v3user_notif_sess", "add notification failed\n"));
         snmp_close(ss);
-        ss = NULL;
-        goto bail;
+        return NULL;
     }
 
   bail:
     /** free any allocated mem in session */
-    SNMP_FREE(session.securityAuthProto);
-    SNMP_FREE(session.securityPrivProto);
-    SNMP_FREE(session.contextEngineID);
+    netsnmp_cleanup_session(&session);
 
     return ss;
 }
@@ -1841,9 +1841,7 @@ snmpd_parse_config_trapsess(const char *word, char *cptr)
                                               ss->version, name, tag, profile);
 
   cleanup:
-    SNMP_FREE(session.community);
-    if (session.securityEngineIDLen > 0)
-        SNMP_FREE(session.securityEngineID);
+    netsnmp_cleanup_session(&session);
     SNMP_FREE(clientaddr_save);
     SNMP_FREE(profile);
     SNMP_FREE(name);

agent/mibgroup/ucd-snmp/proxy.c
@@ -132,6 +132,8 @@ proxy_parse_config(const char *token, char *line)
                     argv[arg]));
     }
 
+    memset(&session, 0, sizeof(session));
+
     DEBUGMSGTL(("proxy_config", "parsing args: %d\n", argn));
     /* Call special parse_args that allows for no specified community string */
     arg = netsnmp_parse_args(argn, argv, &session, "C:", proxyOptProc,
@@ -249,6 +251,7 @@ out:
     /* Free the memory allocated */
     while(argn--)
         SNMP_FREE(argv[argn]);
+    netsnmp_cleanup_session(&session);
 }
 
 void

apps/snmpbulkget.c
@@ -247,8 +247,10 @@ main(int argc, char *argv[])
         snmp_free_pdu(response);
 
     snmp_close(ss);
+    memset(&session, 0, sizeof(session));
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

apps/snmpbulkwalk.c
@@ -378,6 +378,7 @@ main(int argc, char *argv[])
         snmp_get_and_print(ss, root, rootlen);
     }
     snmp_close(ss);
+    memset(&session, 0, sizeof(session));
 
     if (netsnmp_ds_get_boolean(NETSNMP_DS_APPLICATION_ID, 
 			       NETSNMP_DS_WALK_PRINT_STATISTICS)) {
@@ -385,6 +386,7 @@ main(int argc, char *argv[])
     }
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

apps/snmpdelta.c
@@ -746,8 +746,10 @@ main(int argc, char *argv[])
 
 close_session:
     snmp_close(ss);
+    memset(&session, 0, sizeof(session));
 
 out:
+    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return (exit_code);
 }

commit be7b3c89267d5f037c0ccb4d7adcbc43d894f29a

apps: Fix crashes during shutdown
Fixes: f975916a847d ("apps: Use netsnmp_cleanup_session()")

apps/snmpget.c
@@ -251,7 +251,6 @@ close_session:
     snmp_close(ss);
 
 out:
-    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }                               /* end main() */

apps/snmpset.c
@@ -286,7 +286,6 @@ close_session:
 
 out:
     free(posix_env);
-    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }

apps/snmptrap.c
@@ -384,7 +384,6 @@ close_session:
 
 out:
     free(posix_env);
-    netsnmp_cleanup_session(&session);
     SOCK_CLEANUP;
     return exitval;
 }