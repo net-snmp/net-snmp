net-snmp/net-snmp

commit 232a335ffaad29289f74f334a980ecd42532154d

Fix memory corruption in agentx_got_response
we encounter the following crash (onfield)

(gdb) bt
    at ./nptl/pthread_kill.c:44
    at ./nptl/pthread_kill.c:78
    at ./nptl/pthread_kill.c:89
    at ../sysdeps/posix/raise.c:26
    fmt=fmt@entry=0x7e22cf9dbb77 "%s\n") at ../sysdeps/posix/libc_fatal.c:156
    str=str@entry=0x7e22cf9de7b0 "double free or corruption (!prev)")
    at ./malloc/malloc.c:5664
    p=0x56fc941985c0, have_lock=<optimized out>) at ./malloc/malloc.c:4591
    at ./malloc/malloc.c:3391
    dcache=dcache@entry=0x56fc941985d0) at agent_handler.c:934
    session=0x56fc941a5240, reqid=<optimized out>, pdu=<optimized out>,
    magic=0x56fc941985d0) at mibgroup/agentx/master.c:220
--Type <RET> for more, q to quit, c to continue without paging--
    at snmp_api.c:1984
    numfds=0x7fff49e28ee4, fdset=0x7fff49e28f00, timeout=0x7fff49e28ef0,
    block=0x7fff49e28eec, flags=0) at snmp_api.c:6580

in "agentx_got_response" delegated_cache is no more valid and freeing
it encounter following error "double free or corruption (!prev)".

memory inspection shows that delegated_cache had been freed and
replace by another variable of another type.

thus a copy of that pointer exists and a free occured on it.

Fix: add a refcount in delegated_cache, and increase it when a copy occurs.
     separates the contents from the container by creating a box which has
     a refcount. This ensures that the container is freed when the refcount
     reaches 0.

Signed-off-by: Francois Dumontet <francois.dumontet@6wind.com>

agent/mibgroup/agentx/master.c
@@ -203,6 +203,34 @@ real_init_master(void)
     DEBUGMSGTL(("agentx/master", "initializing...   DONE\n"));
 }
 
+
+static netsnmp_refcnt_void *
+netsnmp_create_delegated_box(void *val_ptr)
+{
+    netsnmp_refcnt_void * box = NULL;
+
+    box = SNMP_MALLOC_TYPEDEF(netsnmp_refcnt_void);
+    if (box) {
+        box->refcnt = 1;
+        box->val = val_ptr;
+    }
+      
+    return box;
+}
+
+static
+netsnmp_delete_delegated_box(netsnmp_refcnt_void * box)
+{
+    if (box) {
+        box->refcnt--;
+        box->val = 0;
+        if (box->refcnt <= 0)
+	    SNMP_FREE(box);
+    }
+    return;
+}
+
+
         /*
          * Handle the response from an AgentX subagent,
          *   merging the answers back into the original query
@@ -212,7 +240,8 @@ agentx_got_response(int operation,
                     netsnmp_session * session,
                     int reqid, netsnmp_pdu *pdu, void *magic)
 {
-    netsnmp_delegated_cache *cache = (netsnmp_delegated_cache *) magic;
+    netsnmp_refcnt_void *cache_box = (netsnmp_refcnt_void *) magic;
+    netsnmp_delegated_cache *cache = (netsnmp_delegated_cache *) cache_box->val;
     int             i, ret;
     netsnmp_request_info *requests, *request;
     netsnmp_variable_list *var;
@@ -222,8 +251,10 @@ agentx_got_response(int operation,
         DEBUGMSGTL(("agentx/master", "response too late on session %8p\n",
                     session));
         /* response is too late, free the cache */
-        if (magic)
-            netsnmp_free_delegated_cache((netsnmp_delegated_cache*) magic);
+        if (magic) {
+            netsnmp_free_delegated_cache(cache);
+            netsnmp_delete_delegated_box(cache_box);
+	}
         return 1;
     }
     requests = cache->requests;
@@ -261,6 +292,7 @@ agentx_got_response(int operation,
                 DEBUGMSGTL(("agentx/master", "NULL sess_pointer??\n"));
             }
             netsnmp_free_delegated_cache(cache);
+            netsnmp_delete_delegated_box(cache_box);
             return 0;
         }
 
@@ -279,6 +311,7 @@ agentx_got_response(int operation,
                                   SNMP_ERR_GENERR);
         close_agentx_session(session, -1);
         netsnmp_free_delegated_cache(cache);
+        netsnmp_delete_delegated_box(cache_box);
         return 0;
 
     case NETSNMP_CALLBACK_OP_RESEND:
@@ -296,6 +329,7 @@ agentx_got_response(int operation,
         snmp_log(LOG_ERR, "Unknown operation %d in agentx_got_response\n",
                  operation);
         netsnmp_free_delegated_cache(cache);
+        netsnmp_delete_delegated_box(cache_box);
         return 0;
     }
 
@@ -359,6 +393,7 @@ agentx_got_response(int operation,
                                       SNMP_ERR_GENERR);
         }
         netsnmp_free_delegated_cache(cache);
+        netsnmp_delete_delegated_box(cache_box);
         DEBUGMSGTL(("agentx/master", "end error branch\n"));
         return 1;
     } else if (cache->reqinfo->mode == MODE_GET ||
@@ -420,8 +455,10 @@ agentx_got_response(int operation,
     DEBUGMSGTL(("agentx/master",
                 "handle_agentx_response() finishing...\n"));
     netsnmp_free_delegated_cache(cache);
+    netsnmp_delete_delegated_box(cache_box);
     return 1;
 }
+
 
 /*
  *
@@ -446,7 +483,8 @@ agentx_master_handler(netsnmp_mib_handler *handler,
     netsnmp_request_info *request = requests;
     netsnmp_pdu    *pdu;
     void           *cb_data;
-    int             result;
+    void           *cb_data_box;
+    int            result;
 
     DEBUGMSGTL(("agentx/master",
                 "agentx master handler starting, mode = 0x%02x\n",
@@ -606,12 +644,14 @@ agentx_master_handler(netsnmp_mib_handler *handler,
     else
         cb_data = NULL;
 
+    cb_data_box  = netsnmp_create_delegated_box(cb_data);
+
     /*
      * send the requests out.
      */
     DEBUGMSGTL(("agentx/master", "sending pdu (req=0x%x,trans=0x%x,sess=0x%x)\n",
                 (unsigned)pdu->reqid, (unsigned)pdu->transid, (unsigned)pdu->sessid));
-    result = snmp_async_send(ax_session, pdu, agentx_got_response, cb_data);
+    result = snmp_async_send_cp(ax_session, pdu, agentx_got_response, cb_data_box, 1);
     if (result == 0) {
         snmp_free_pdu(pdu);
     }

include/net-snmp/session_api.h
@@ -102,6 +102,28 @@ extern          "C" {
 
 
     /*
+     * int snmp_async_send_cp(session, pdu, callback, cb_data, cp_inc)
+     *     netsnmp_session *session;
+     *     netsnmp_pdu      *pdu;
+     *     netsnmp_callback callback;
+     *     void             *cb_data;
+     *     int              cp_inc;
+     *
+     * Sends the input pdu on the session after calling snmp_build to create
+     * a serialized packet.  If necessary, set some of the pdu data from the
+     * session defaults.  Add a request corresponding to this pdu to the list
+     * of outstanding requests on this session and store callback and data,
+     * then send the pdu.
+     * Returns the request id of the generated packet if applicable, otherwise 1.
+     * On any error, 0 is returned.
+     * The pdu is freed by snmp_send() unless a failure occured.
+     */
+    NETSNMP_IMPORT
+    int             snmp_async_send_cp(netsnmp_session *, netsnmp_pdu *,
+                                       netsnmp_callback, void *, int);
+
+
+    /*
      * void snmp_read(fdset)
      *     fd_set  *fdset;
      *
@@ -248,6 +270,9 @@ extern          "C" {
     NETSNMP_IMPORT
     int             snmp_sess_async_send(struct session_list *, netsnmp_pdu *,
                                          netsnmp_callback, void *);
+    NETSNMP_IMPORT
+    int             snmp_sess_async_send_cp(struct session_list *, netsnmp_pdu *,
+                                            netsnmp_callback, void *, int); 
     NETSNMP_IMPORT
     int             snmp_sess_select_info(struct session_list *, int *, fd_set *,
                                           struct timeval *, int *);

include/net-snmp/types.h
@@ -445,6 +445,11 @@ typedef struct netsnmp_ref_void_s {
     void           *val;
 } netsnmp_ref_void;
 
+typedef struct netsnmp_refcnt_void {
+    size_t         refcnt;
+    void           *val;
+} netsnmp_refcnt_void;
+
 typedef union {
     u_long          ul;
     u_int           ui;

snmplib/snmp_api.c
@@ -5353,6 +5353,14 @@ snmp_async_send(netsnmp_session * session,
     return snmp_sess_async_send(sessp, pdu, callback, cb_data);
 }
 
+int
+snmp_async_send_cp(netsnmp_session * session, netsnmp_pdu *pdu,
+                   snmp_callback callback, void *cb_data, int cp_inc)
+{
+    void           *sessp = snmp_sess_pointer(session);
+    return snmp_sess_async_send_cp(sessp, pdu, callback, cb_data, cp_inc);
+}
+
 /**
  * Send a PDU asynchronously.
  *
@@ -5369,7 +5377,8 @@ snmp_async_send(netsnmp_session * session,
  */
 static int
 _sess_async_send(struct session_list *slp,
-                 netsnmp_pdu *pdu, snmp_callback callback, void *cb_data)
+                 netsnmp_pdu *pdu, snmp_callback callback, void *cb_data,
+                 int cp_inc)
 {
     netsnmp_session *session;
     struct snmp_internal_session *isp;
@@ -5484,6 +5493,10 @@ _sess_async_send(struct session_list *slp,
         rp->request_id = pdu->reqid;
         rp->message_id = pdu->msgid;
         rp->callback = callback;
+        if (cp_inc) {
+            netsnmp_refcnt_void *aux_cb_data = (netsnmp_refcnt_void*) cb_data;
+            aux_cb_data->refcnt++;
+	}
         rp->cb_data = cb_data;
         rp->retries = 0;
         if (pdu->flags & UCD_MSG_FLAG_PDU_TIMEOUT) {
@@ -5545,6 +5558,15 @@ snmp_sess_async_send(struct session_list *slp,
                      netsnmp_pdu *pdu,
                      snmp_callback callback, void *cb_data)
 {
+
+    return snmp_sess_async_send_cp(slp, pdu, callback, cb_data, 0);
+}
+
+int
+snmp_sess_async_send_cp(struct session_list *slp, netsnmp_pdu *pdu,
+                        snmp_callback callback, void *cb_data,
+                        int cp_inc)
+{
     int             rc;
 
     if (slp == NULL) {
@@ -5554,7 +5576,8 @@ snmp_sess_async_send(struct session_list *slp,
     /*
      * send pdu
      */
-    rc = _sess_async_send(slp, pdu, callback, cb_data);
+
+    rc = _sess_async_send(slp, pdu, callback, cb_data, cp_inc);
     if (rc == 0)
         SET_SNMP_ERROR(slp->session->s_snmp_errno);
     return rc;

commit 7f93f087090fed9a26043f033f1b8b80aee61207

snmpd: Fix a recently introduced compiler warning
Fixes: 232a335ffaad ("Fix memory corruption in agentx_got_response")

agent/mibgroup/agentx/master.c
@@ -218,7 +218,7 @@ netsnmp_create_delegated_box(void *val_ptr)
     return box;
 }
 
-static
+static void
 netsnmp_delete_delegated_box(netsnmp_refcnt_void * box)
 {
     if (box) {
@@ -227,7 +227,6 @@ netsnmp_delete_delegated_box(netsnmp_refcnt_void * box)
         if (box->refcnt <= 0)
 	    SNMP_FREE(box);
     }
-    return;
 }
 
 