net-snmp/net-snmp

commit d9b63c81518c049314932096cf1bb0f1eda68730

snmptrapd: Protect startup-registered trap handlers on config reload
Introduce NETSNMP_TRAPHANDLER_FLAG_BUILTIN to mark built-in startup-registered
handlers (syslog, print, notification, and authorization). Modify
snmptrapd_free_traphandle to only free dynamically registered handlers.
Add snmptrapd_free_all_traphandlers to strip this flag and free all handlers
on final shutdown.

This prevents losing core built-in handlers when configuration is reloaded,
preserving handler execution after SIGHUP.

apps/snmptrapd.c
@@ -1015,12 +1015,16 @@ main(int argc, char *argv[])
 #ifndef NETSNMP_FEATURE_REMOVE_LOGGING_SYSLOG
         traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_PRE_HANDLER,
                                                syslog_handler);
+        if (traph)
+            traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
         traph->authtypes = TRAP_AUTH_LOG;
         snmp_enable_syslog();
 #else /* NETSNMP_FEATURE_REMOVE_LOGGING_SYSLOG */
 #ifndef NETSNMP_FEATURE_REMOVE_LOGGING_STDIO
         traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_PRE_HANDLER,
                                                print_handler);
+        if (traph)
+            traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
         traph->authtypes = TRAP_AUTH_LOG;
         snmp_enable_stderr();
 #endif /* NETSNMP_FEATURE_REMOVE_LOGGING_STDIO */
@@ -1028,6 +1032,8 @@ main(int argc, char *argv[])
     } else {
         traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_PRE_HANDLER,
                                                print_handler);
+        if (traph)
+            traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
         traph->authtypes = TRAP_AUTH_LOG;
     }
 
@@ -1077,6 +1083,8 @@ main(int argc, char *argv[])
                               "snmptrapd");
             traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_POST_HANDLER,
                                                    notification_handler);
+            if (traph)
+                traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
             traph->authtypes = TRAP_AUTH_LOG;
             init_notification_log();
         }
@@ -1360,6 +1368,7 @@ main(int argc, char *argv[])
 #endif
     snmptrapd_close_sessions(sess_list);
     snmp_shutdown("snmptrapd");
+    snmptrapd_free_all_traphandlers();
 #ifdef WIN32SERVICE
     trapd_status = SNMPTRAPD_STOPPED;
 #endif

apps/snmptrapd_auth.c
@@ -44,6 +44,8 @@ init_netsnmp_trapd_auth(void)
     netsnmp_trapd_handler *traph;
     traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_AUTH_HANDLER,
                                            netsnmp_trapd_auth);
+    if (traph)
+        traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
     traph->authtypes = TRAP_AUTH_NONE;
 
 #ifdef USING_MIBII_VACM_CONF_MODULE

apps/snmptrapd_handlers.c
@@ -517,6 +517,32 @@ netsnmp_add_traphandler(Netsnmp_Trap_Handler* handler,
     return traph;
 }
 
+static void
+free_non_builtin_handlers(netsnmp_trapd_handler **list_head)
+{
+    netsnmp_trapd_handler *traph = *list_head;
+    netsnmp_trapd_handler *prev = NULL;
+    netsnmp_trapd_handler *next;
+
+    while (traph) {
+        next = traph->nexth;
+        if (!(traph->flags & NETSNMP_TRAPHANDLER_FLAG_BUILTIN)) {
+            /* Unlink */
+            if (prev)
+                prev->nexth = next;
+            else
+                *list_head = next;
+
+            /* Free */
+            SNMP_FREE(traph->token);
+            SNMP_FREE(traph);
+        } else {
+            prev = traph;
+        }
+        traph = next;
+    }
+}
+
 void
 snmptrapd_free_traphandle(void)
 {
@@ -524,19 +550,10 @@ snmptrapd_free_traphandle(void)
 
     DEBUGMSGTL(("snmptrapd", "Freeing trap handler lists\n"));
 
-    /*
-     * Free default trap handlers
-     */
-    traph = netsnmp_default_traphandlers;
-   /* loop over handlers */
-    while (traph) {
-       DEBUGMSG(("snmptrapd", "Freeing default trap handler\n"));
-	nexth = traph->nexth;
-	SNMP_FREE(traph->token);
-	SNMP_FREE(traph);
-	traph = nexth;
-    }
-    netsnmp_default_traphandlers = NULL;
+    free_non_builtin_handlers(&netsnmp_auth_global_traphandlers);
+    free_non_builtin_handlers(&netsnmp_pre_global_traphandlers);
+    free_non_builtin_handlers(&netsnmp_post_global_traphandlers);
+    free_non_builtin_handlers(&netsnmp_default_traphandlers);
 
     /* 
      * Free specific trap handlers
@@ -557,6 +574,23 @@ snmptrapd_free_traphandle(void)
 	traph = nextt;
     }
     netsnmp_specific_traphandlers = NULL;
+}
+
+void
+snmptrapd_free_all_traphandlers(void)
+{
+    netsnmp_trapd_handler *traph;
+
+    for (traph = netsnmp_auth_global_traphandlers; traph; traph = traph->nexth)
+        traph->flags &= ~NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
+    for (traph = netsnmp_pre_global_traphandlers; traph; traph = traph->nexth)
+        traph->flags &= ~NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
+    for (traph = netsnmp_post_global_traphandlers; traph; traph = traph->nexth)
+        traph->flags &= ~NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
+    for (traph = netsnmp_default_traphandlers; traph; traph = traph->nexth)
+        traph->flags &= ~NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
+
+    snmptrapd_free_traphandle();
 }
 
 /*

apps/snmptrapd_handlers.h
@@ -10,6 +10,7 @@ typedef int (Netsnmp_Trap_Handler)(netsnmp_pdu           *pdu,
 
 #define NETSNMP_TRAPHANDLER_FLAG_MATCH_TREE     0x1
 #define NETSNMP_TRAPHANDLER_FLAG_STRICT_SUBTREE 0x2
+#define NETSNMP_TRAPHANDLER_FLAG_BUILTIN        0x4
 
 struct netsnmp_trapd_handler_s {
      oid  *trapoid;
@@ -60,6 +61,7 @@ netsnmp_trapd_handler *netsnmp_add_default_traphandler(Netsnmp_Trap_Handler* han
 netsnmp_trapd_handler *netsnmp_add_traphandler(Netsnmp_Trap_Handler* handler,
                         oid *trapOid, int trapOidLen);
 netsnmp_trapd_handler *netsnmp_get_traphandler(oid *trapOid, int trapOidLen);
+void snmptrapd_free_all_traphandlers(void);
 
 const char *trap_description(int trap);
 int snmp_input(int op, netsnmp_session *session,

commit 398b9e48d7fa10e4c835f7eb2c30dae8d466b235

snmptrapd: Guard traph dereferences
Wrap traph dereferences in `if (traph)` blocks to avoid potential NULL
pointer dereference if `netsnmp_add_global_traphandler` fails.

Fixes: d9b63c81518c ("snmptrapd: Protect startup-registered trap handlers on config reload")

apps/snmptrapd.c
@@ -1015,26 +1015,29 @@ main(int argc, char *argv[])
 #ifndef NETSNMP_FEATURE_REMOVE_LOGGING_SYSLOG
         traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_PRE_HANDLER,
                                                syslog_handler);
-        if (traph)
+        if (traph) {
             traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
-        traph->authtypes = TRAP_AUTH_LOG;
+            traph->authtypes = TRAP_AUTH_LOG;
+        }
         snmp_enable_syslog();
 #else /* NETSNMP_FEATURE_REMOVE_LOGGING_SYSLOG */
 #ifndef NETSNMP_FEATURE_REMOVE_LOGGING_STDIO
         traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_PRE_HANDLER,
                                                print_handler);
-        if (traph)
+        if (traph) {
             traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
-        traph->authtypes = TRAP_AUTH_LOG;
+            traph->authtypes = TRAP_AUTH_LOG;
+        }
         snmp_enable_stderr();
 #endif /* NETSNMP_FEATURE_REMOVE_LOGGING_STDIO */
 #endif /* NETSNMP_FEATURE_REMOVE_LOGGING_SYSLOG */
     } else {
         traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_PRE_HANDLER,
                                                print_handler);
-        if (traph)
+        if (traph) {
             traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
-        traph->authtypes = TRAP_AUTH_LOG;
+            traph->authtypes = TRAP_AUTH_LOG;
+        }
     }
 
 #if defined(USING_AGENTX_SUBAGENT_MODULE) && !defined(NETSNMP_SNMPTRAPD_DISABLE_AGENTX)
@@ -1083,9 +1086,10 @@ main(int argc, char *argv[])
                               "snmptrapd");
             traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_POST_HANDLER,
                                                    notification_handler);
-            if (traph)
+            if (traph) {
                 traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
-            traph->authtypes = TRAP_AUTH_LOG;
+                traph->authtypes = TRAP_AUTH_LOG;
+            }
             init_notification_log();
         }
 #endif

apps/snmptrapd_auth.c
@@ -44,9 +44,10 @@ init_netsnmp_trapd_auth(void)
     netsnmp_trapd_handler *traph;
     traph = netsnmp_add_global_traphandler(NETSNMPTRAPD_AUTH_HANDLER,
                                            netsnmp_trapd_auth);
-    if (traph)
+    if (traph) {
         traph->flags |= NETSNMP_TRAPHANDLER_FLAG_BUILTIN;
-    traph->authtypes = TRAP_AUTH_NONE;
+        traph->authtypes = TRAP_AUTH_NONE;
+    }
 
 #ifdef USING_MIBII_VACM_CONF_MODULE
     /* register our configuration tokens for VACM configs */