net-snmp/net-snmp

commit b0a4ae309dad0ed05f6915838b9d5d1fecb94b91

[TSM] TLS TCP Support the configuration of min/max tls version

snmplib/transports/snmpTLSBaseDomain.c
@@ -462,6 +462,9 @@ SSL_CTX *
 _sslctx_common_setup(SSL_CTX *the_ctx, _netsnmpTLSBaseData *tlsbase) {
     char         *crlFile;
     char         *cipherList;
+    char         *tlsMinVersion;
+    char         *tlsMaxVersion;
+    int          tlsVersion;
     X509_LOOKUP  *lookup;
     X509_STORE   *cert_store = NULL;
 
@@ -484,6 +487,65 @@ _sslctx_common_setup(SSL_CTX *the_ctx, _netsnmpTLSBaseData *tlsbase) {
         X509_STORE_set_flags(cert_store,
                              X509_V_FLAG_CRL_CHECK | X509_V_FLAG_CRL_CHECK_ALL);
     }
+
+#ifdef SSL_CTX_set_min_proto_version
+    tlsVersion = TLS1_2_VERSION;
+    tlsMinVersion = "tls1_2";
+    tlsMinVersion = netsnmp_ds_get_string(NETSNMP_DS_LIBRARY_ID,
+                                          NETSNMP_DS_LIB_TLS_MIN_VERSION);
+    if (NULL != tlsMinVersion) {
+        if (strcmp("tls1",tlsMinVersion) == 0) {
+           tlsVersion = TLS1_VERSION;
+        }
+        else if (strcmp("tls1_1",tlsMinVersion) == 0) {
+           tlsVersion = TLS1_1_VERSION;
+        }
+        else if (strcmp("tls1_2",tlsMinVersion) == 0) {
+           tlsVersion = TLS1_2_VERSION;
+        }
+        else if (strcmp("tls1_3",tlsMinVersion) == 0) {
+           tlsVersion = TLS1_3_VERSION;
+        }
+        else {
+            LOGANDDIE("Invalid tlsMinVersion value");
+        }
+    }
+    if (1 == SSL_CTX_set_min_proto_version(the_ctx, tlsVersion)) {
+        snmp_log(LOG_INFO,"Set tlsMinVersion to '%s'\n", tlsMinVersion);
+    }
+    else {
+        LOGANDDIE("Set tlsMinVersion failed");
+    }
+
+    tlsVersion = TLS1_3_VERSION;
+    tlsMaxVersion = "tls1_3";
+    tlsMaxVersion = netsnmp_ds_get_string(NETSNMP_DS_LIBRARY_ID,
+                                          NETSNMP_DS_LIB_TLS_MAX_VERSION);
+    if (NULL != tlsMaxVersion) {
+        if (strcmp("tls1",tlsMaxVersion) == 0) {
+            tlsVersion = TLS1_VERSION;
+        }
+        else if (strcmp("tls1_1",tlsMaxVersion) == 0) {
+            tlsVersion = TLS1_1_VERSION;
+        }
+        else if (strcmp("tls1_2",tlsMaxVersion) == 0) {
+            tlsVersion = TLS1_2_VERSION;
+        }
+        else if (strcmp("tls1_3",tlsMaxVersion) == 0) {
+            tlsVersion = TLS1_3_VERSION;
+        }
+        else {
+            LOGANDDIE("Invalid tlsMaxVersion value");
+        }
+    }
+
+    if (1 == SSL_CTX_set_max_proto_version(the_ctx, tlsVersion)) {
+        snmp_log(LOG_INFO,"Set tlsMaxVersion to '%s'\n", tlsMaxVersion);
+    }
+    else {
+        LOGANDDIE("Set tlsMaxVersion failed");
+    }
+#endif
 
     cipherList = netsnmp_ds_get_string(NETSNMP_DS_LIBRARY_ID,
                                        NETSNMP_DS_LIB_TLS_ALGORITMS);
@@ -773,6 +835,16 @@ netsnmp_tlsbase_ctor(void) {
     netsnmp_ds_register_config(ASN_OCTET_STR, "snmp", "tlsAlgorithms",
                                NETSNMP_DS_LIBRARY_ID,
                                NETSNMP_DS_LIB_TLS_ALGORITMS);
+
+    /* What TLS version should be used at least */
+    netsnmp_ds_register_config(ASN_OCTET_STR, "snmp", "tlsMinVersion",
+                               NETSNMP_DS_LIBRARY_ID,
+                               NETSNMP_DS_LIB_TLS_MIN_VERSION);
+
+    /* What TLS version should be used at max */
+    netsnmp_ds_register_config(ASN_OCTET_STR, "snmp", "tlsMaxVersion",
+                               NETSNMP_DS_LIBRARY_ID,
+                               NETSNMP_DS_LIB_TLS_MAX_VERSION);
 
     /*
      * for the client

include/net-snmp/library/default_store.h
@@ -4,7 +4,7 @@
  */
 /*
  * Portions of this file are copyrighted by:
- * Copyright © 2003 Sun Microsystems, Inc. All rights reserved.
+ * Copyright Â© 2003 Sun Microsystems, Inc. All rights reserved.
  * Use is subject to license terms specified in the COPYING file
  * distributed with the Net-SNMP package.
  *
@@ -183,6 +183,8 @@ extern          "C" {
 #define NETSNMP_DS_LIB_SSH_PUBKEY        33
 #define NETSNMP_DS_LIB_SSH_PRIVKEY       34
 #define NETSNMP_DS_LIB_OUTPUT_PRECISION  35
+#define NETSNMP_DS_LIB_TLS_MIN_VERSION   36
+#define NETSNMP_DS_LIB_TLS_MAX_VERSION   37
 #define NETSNMP_DS_LIB_MAX_STR_ID        48 /* match NETSNMP_DS_MAX_SUBIDS */
 
     /*

man/snmpd.conf.5.def
@@ -203,6 +203,12 @@ HIGH:!AES128\-SHA
 .RE
 .IP
 The default value is whatever openssl itself was configured with.
+.IP "tlsMinVersion STRING"
+The function sets the minimum supported TLS protocol version. 
+OPTION can be one of < tls1 | tls1_1| tls1_2 | tls1_3 >.
+.IP "tlsMaxVersion STRING"
+The function sets the maximum supported TLS protocol version. 
+OPTION can be one of < tls1 | tls1_1| tls1_2 | tls1_3 >.
 .IP "[snmp] x509CRLFile"
 If you are using a Certificate Authority (CA) that publishes a
 Certificate Revocation List (CRL) then this token can be used to

snmplib/transports/snmpTLSTCPDomain.c
@@ -719,10 +719,6 @@ netsnmp_tlstcp_open_client(netsnmp_transport *t)
         return NULL;
     }
 
-#ifdef SSL_CTX_set_max_proto_version
-    SSL_CTX_set_max_proto_version(tlsdata->ssl_context, TLS1_VERSION);
-#endif
-
     /* RFC5953 Section 5.3.1:  Establishing a Session as a Client
        3)  Using the destTransportDomain and destTransportAddress values,
        the client will initiate the (D)TLS handshake protocol to
@@ -919,10 +915,6 @@ netsnmp_tlstcp_open_server(netsnmp_transport *t)
 
     /* create the OpenSSL TLS context */
     tlsdata->ssl_context = sslctx_server_setup(TLS_method());
-#ifdef SSL_CTX_set_max_proto_version
-    if (tlsdata->ssl_context)
-        SSL_CTX_set_max_proto_version(tlsdata->ssl_context, TLS1_VERSION);
-#endif
 
     t->sock = BIO_get_fd(tlsdata->accept_bio, NULL);
     t->flags |= NETSNMP_TRANSPORT_FLAG_LISTEN;

commit 58624fface25185f55a49a85e84fbe7995068130

libsnmp: Only set the TLS version for TLS connections
Fixes: b0a4ae309dad ("[TSM] TLS TCP Support the configuration of min/max tls version")

snmplib/transports/snmpTLSBaseDomain.c
@@ -614,6 +614,7 @@ int _sslctx_set_tls_version(SSL_CTX *the_ctx)
 SSL_CTX *
 _sslctx_common_setup(SSL_CTX *the_ctx, _netsnmpTLSBaseData *tlsbase) {
     char         *crlFile;
+    const SSL_METHOD *method;
     char         *cipherList;
     X509_LOOKUP  *lookup;
     X509_STORE   *cert_store = NULL;
@@ -638,7 +639,10 @@ _sslctx_common_setup(SSL_CTX *the_ctx, _netsnmpTLSBaseData *tlsbase) {
                              X509_V_FLAG_CRL_CHECK | X509_V_FLAG_CRL_CHECK_ALL);
     }
 
-    if (_sslctx_set_tls_version(the_ctx) == 0)
+    method = SSL_CTX_get_ssl_method(the_ctx);
+    if ((method == TLS_method() || method == TLS_client_method() ||
+         method == TLS_server_method()) &&
+        _sslctx_set_tls_version(the_ctx) == 0)
         return NULL;
 
     cipherList = netsnmp_ds_get_string(NETSNMP_DS_LIBRARY_ID,