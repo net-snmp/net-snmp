c76934681f66744aa164c91c9f9c9a6578f4eaea
Bart Van Assche <bvanassche@acm.org>
2026-07-30 14:39:49 -0700

    ci: Reduce verbosity of the code for downloading OpenSSL

--- ci/openssl.bat
+++ ci/openssl.bat
@@ -1,8 +1,10 @@
 REM Download and install OpenSSL
-rmdir /s /q C:\OpenSSL-Win32
-rmdir /s /q C:\OpenSSL-v11-Win32
-rmdir /s /q C:\OpenSSL-Win64
-rmdir /s /q C:\OpenSSL-v11-Win64
+set "FOLDERS=C:\OpenSSL-Win32 C:\OpenSSL-v11-Win32 C:\OpenSSL-Win64 C:\OpenSSL-v11-Win64"
+for %%D in (%FOLDERS%) do (
+    if exist "%%D\" (
+        rmdir /s /q "%%D" 2>nul
+    )
+)
 powershell -ExecutionPolicy Bypass -File "%~dp0openssl.ps1"
 if %errorlevel% neq 0 exit /b %errorlevel%
 .\openssl.exe /suppressmsgboxes /silent /norestart /nocloseapplications /log=openssl-installation-log.txt /dir=C:\OpenSSL-Win64
--- ci/openssl.ps1
+++ ci/openssl.ps1
@@ -26,4 +26,4 @@ if ($null -eq $latest) {
 Write-Host "Latest OpenSSL Installer found: $($latest.url)"
 Write-Host "Downloading OpenSSL installer..."
 #Invoke-WebRequest -Uri $latest.url -OutFile "openssl.exe"
-curl.exe -L $latest.url -o "openssl.exe"
+curl.exe -s -S -L $latest.url -o "openssl.exe"
