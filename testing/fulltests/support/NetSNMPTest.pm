946c1e3d966d0af8d55f315ca4a88d198bb37837
Bart Van Assche <bvanassche@acm.org>
2026-08-07 10:36:54 -0700

    testing, Perl: Validate PID and wait for process termination in NetSNMPTest.pm
    Check that the agent PID file exists and contains a valid numeric PID
    before calling kill("TERM", $pid) in stop_agent(). This prevents an
    uncaught "Can't kill a non-numeric process ID" exception when snmpd
    fails to start. Also wait for the snmpd process to fully terminate
    before returning from stop_agent() so that directory cleanup in
    DESTROY() does not fail with a "Directory not empty" error.

--- testing/fulltests/support/NetSNMPTest.pm
+++ testing/fulltests/support/NetSNMPTest.pm
@@ -82,9 +82,20 @@ sub start_agent {
 sub stop_agent {
     my ($self) = @_;
     my $pidfile = new IO::File "$self->{'snmpd.pid'}";
-    my $pid = <$pidfile>;
-    kill("TERM", $pid);
-    $self->wait_for($self->{'snmpd.log'}, 'shutting down');
+    if ($pidfile) {
+        my $pid = <$pidfile>;
+        if (defined($pid)) {
+            chomp($pid);
+            if ($pid =~ /^\d+$/) {
+                kill("TERM", $pid);
+                $self->wait_for($self->{'snmpd.log'}, 'shutting down');
+                for (my $i = 0; $i < 50; ++$i) {
+                    last if !kill(0, $pid);
+                    select(undef, undef, undef, 0.1);
+                }
+            }
+        }
+    }
 }
 
 # returns 1 on success, 0 on failure
