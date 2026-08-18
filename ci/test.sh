net-snmp/net-snmp

commit 1244c333ea009bd77514cc5bcf81fb8de9e08c01

ci/test.sh: Run the regression tests in all modes
Run the regression tests in all modes except the modes in which Net-SNMP is
cross-compiled.

ci/test.sh
@@ -9,20 +9,12 @@ case "$(uname)" in
 	;;
 esac
 
-head -n 999 /etc/hosts
-
 scriptdir="$(dirname "$0")"
 
-# To do: fix the tests for the disable-set, mini and read-only modes and delete
-# the case statement below.
 case "$MODE" in
-    "")
-	;;
-    asan|debug|disable-ipv6|regular)
-        ;;
-    *)
+    Android)
 	exit 0
-        ;;
+	;;
 esac
 
 case $(uname) in

commit 44b49548b20ffcf0340563082c17c7d15a51c4ed

ci/test.sh: Detect cross-compilation from config.log
Instead of hardcoding a check for MODE=Android to skip tests, extract the
--build=... and --host=... arguments from the configure invocation in
config.log. Only execute the regression tests if the build and host
arguments are identical, indicating a native build.

Fixes: 1244c333ea00 ("ci/test.sh: Run the regression tests in all modes")

ci/test.sh
@@ -11,16 +11,9 @@ esac
 
 scriptdir="$(dirname "$0")"
 
-case "$MODE" in
-    Android)
-	exit 0
-	;;
-esac
+build=$(sed -n '/^  \$ \.\/configure/ s/.*--build=\([^ ]*\).*/\1/p' config.log | tr -d "\"'" )
+host=$(sed -n '/^  \$ \.\/configure/ s/.*--host=\([^ ]*\).*/\1/p' config.log | tr -d "\"'" )
 
-case $(uname) in
-    MinGW)
-	;;
-    *)
-	"${scriptdir}"/net-snmp-run-tests
-	;;
-esac
+if [ "$build" = "$host" ]; then
+    "${scriptdir}"/net-snmp-run-tests
+fi