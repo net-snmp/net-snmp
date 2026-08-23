84d77136d9d3a9648886cffbe81e088500da2a56
Bart Van Assche <bvanassche@acm.org>
2026-08-23 08:14:40 -0700

    ci/AppVeyor: Add Visual Studio 2026 MSVCDYNAMIC64 build
    Add the Visual Studio 2026 build worker image with the MSVCDYNAMIC64
    build configuration to .appveyor.yml. Place it at the beginning of the
    matrix to ensure it is built first.

--- ci/find_vc.bat
+++ ci/find_vc.bat
@@ -1,5 +1,16 @@
 @echo off
+set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if exist "%vswhere%" (
+    for /f "usebackq tokens=*" %%i in (`"%vswhere%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+        if exist "%%i\VC\Auxiliary\Build\vcvars64.bat" (
+            set "VCVARSPATH=%%i\VC\Auxiliary\Build"
+            goto :eof
+        )
+    )
+)
 set p=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build
 if exist "%p%" set "VCVARSPATH=%p%"
-set p=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary/Build
+set p=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
+if exist "%p%" set "VCVARSPATH=%p%"
+set p=C:\Program Files\Microsoft Visual Studio\2026\Community\VC\Auxiliary\Build
 if exist "%p%" set "VCVARSPATH=%p%"
