net-snmp/net-snmp

commit afc396ab1618a8595a2519fbc25f7a498e174411

Unbreak netsnmp_mktemp() and make_temp().
These were broken by commit bb8fcbb874998000f945f058d1aa6fe14e925899.
Also replace a strncpy() call by a call to strlcpy() and replace the
make_tempfile() implementation by a call to netsnmp_mktemp().

snmplib/system.c
@@ -1112,11 +1112,11 @@ netsnmp_mktemp(void)
 #endif
     int             fd = -1;
 
-    strncpy(name, get_temp_file_pattern(), sizeof(name));
-    name[sizeof(name)-1] = '\0';
+    strlcpy(name, get_temp_file_pattern(), sizeof(name));
 #ifdef HAVE_MKSTEMP
     {
-        mode_t oldmask = umask(S_IRUSR | S_IWUSR);
+        mode_t oldmask = umask(~(S_IRUSR | S_IWUSR));
+        netsnmp_assert(oldmask != -1);
         fd = mkstemp(name);
         umask(oldmask);
     }

agent/mibgroup/util_funcs.c
@@ -135,36 +135,7 @@ Exit(int var)
 const char *
 make_tempfile(void)
 {
-    static char     name[32];
-    int             fd = -1;
-
-    strncpy(name, get_temp_file_pattern(), sizeof(name));
-    name[sizeof(name)-1] = '\0';
-#ifdef HAVE_MKSTEMP
-    {
-        mode_t oldmask = umask(S_IRUSR | S_IWUSR);
-        fd = mkstemp(name);
-        umask(oldmask);
-    }
-#else
-    if (mktemp(name)) {
-# ifndef WIN32        
-        fd = open(name, O_CREAT | O_EXCL | O_WRONLY, S_IRUSR | S_IWUSR);
-# else
-        /*
-          Win32 needs _S_IREAD | _S_IWRITE to set permissions on file after closing
-        */
-        fd = _open(name, _O_CREAT | _O_EXCL | _O_WRONLY, _S_IREAD | _S_IWRITE);
-# endif
-    }
-#endif
-    if (fd >= 0) {
-        close(fd);
-        DEBUGMSGTL(("make_tempfile", "temp file created: %s\n", name));
-        return name;
-    }
-    snmp_log(LOG_ERR,"make_tempfile: error creating file %s\n", name);
-    return NULL;
+    return netsnmp_mktemp();
 }
 
 int

commit 9b8d38c25cab8c4242293f0dd25b4ffbbe416573

libsnmp: Use octal 0177 for umask in netsnmp_mktemp()
In netsnmp_mktemp(), replace ~(S_IRUSR | S_IWUSR) with 0177.
Bitwise negation of (S_IRUSR | S_IWUSR) promotes to signed int and
evaluates to -385 (0xFFFFFE7F), inverting higher bits outside the
permission mask. Using 0177 octal explicitly specifies the umask
that ensures temporary files created by mkstemp() are readable and
writable only by the owner (0600 mode).

Fixes: afc396ab1618 ("Unbreak netsnmp_mktemp() and make_temp().")

snmplib/system.c
@@ -1336,7 +1336,7 @@ netsnmp_mktemp(void)
     strlcpy(name, get_temp_file_pattern(), sizeof(name));
 #ifdef HAVE_MKSTEMP
     {
-        mode_t oldmask = umask(~(S_IRUSR | S_IWUSR));
+        mode_t oldmask = umask(0177);
         netsnmp_assert(oldmask != (mode_t)(-1));
         fd = mkstemp(name);
         umask(oldmask);