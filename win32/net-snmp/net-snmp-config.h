net-snmp/net-snmp

commit 20e2bb7d75c391f5cfde1eb8b8676aff68f3a5f5

Add '@' netgroup functionality
Allow access control via netgroups defined in /etc/netgroup or NIS/LDAP
via the '@' sign inside the configuration file. Same as IP addresses and
host names.

configure
@@ -31222,6 +31222,12 @@ then :
   printf "%s\n" "#define HAVE_CLOSEDIR 1" >>confdefs.h
 
 fi
+ac_fn_c_check_func "$LINENO" "endnetgrent" "ac_cv_func_endnetgrent"
+if test "x$ac_cv_func_endnetgrent" = xyes
+then :
+  printf "%s\n" "#define HAVE_ENDNETGRENT 1" >>confdefs.h
+
+fi
 ac_fn_c_check_func "$LINENO" "fgetc_unlocked" "ac_cv_func_fgetc_unlocked"
 if test "x$ac_cv_func_fgetc_unlocked" = xyes
 then :
@@ -31256,6 +31262,12 @@ ac_fn_c_check_func "$LINENO" "getlogin" "ac_cv_func_getlogin"
 if test "x$ac_cv_func_getlogin" = xyes
 then :
   printf "%s\n" "#define HAVE_GETLOGIN 1" >>confdefs.h
+
+fi
+ac_fn_c_check_func "$LINENO" "getnetgrent" "ac_cv_func_getnetgrent"
+if test "x$ac_cv_func_getnetgrent" = xyes
+then :
+  printf "%s\n" "#define HAVE_GETNETGRENT 1" >>confdefs.h
 
 fi
 ac_fn_c_check_func "$LINENO" "if_nametoindex" "ac_cv_func_if_nametoindex"
@@ -31304,6 +31316,12 @@ ac_fn_c_check_func "$LINENO" "setlocale" "ac_cv_func_setlocale"
 if test "x$ac_cv_func_setlocale" = xyes
 then :
   printf "%s\n" "#define HAVE_SETLOCALE 1" >>confdefs.h
+
+fi
+ac_fn_c_check_func "$LINENO" "setnetgrent" "ac_cv_func_setnetgrent"
+if test "x$ac_cv_func_setnetgrent" = xyes
+then :
+  printf "%s\n" "#define HAVE_SETNETGRENT 1" >>confdefs.h
 
 fi
 ac_fn_c_check_func "$LINENO" "setsid" "ac_cv_func_setsid"

configure.d/config_os_functions
@@ -25,12 +25,14 @@ AC_TYPE_SIGNAL
 AC_CHECK_FUNCS([rand   random srand srandom lrand48 srand48])
 
 #  Library:
-AC_CHECK_FUNCS([asprintf        closedir        fgetc_unlocked   ] dnl
+AC_CHECK_FUNCS([asprintf        closedir        endnetgrent      ] dnl
+               [fgetc_unlocked                                   ] dnl
                [flockfile       funlockfile     getipnodebyname  ] dnl
-               [gettimeofday    getlogin                         ] dnl
+               [gettimeofday    getlogin        getnetgrent      ] dnl
                [if_nametoindex  mkstemp                          ] dnl
                [opendir         readdir         regcomp          ] dnl
                [setenv          setitimer       setlocale        ] dnl
+               [setnetgrent                                      ] dnl
                [setsid          snprintf        strcasestr       ] dnl
                [strdup          strerror        strncasecmp      ] dnl
                [sysconf         times           vsnprintf        ] )

include/net-snmp/net-snmp-config.h.in
@@ -183,6 +183,9 @@
 /* Define to 1 if you have the `endfsent' function. */
 #undef HAVE_ENDFSENT
 
+/* Define to 1 if you have the `endnetgrent' function. */
+#undef HAVE_ENDNETGRENT
+
 /* Define to 1 if you have the `ERR_get_error_all' function. */
 #undef HAVE_ERR_GET_ERROR_ALL
 
@@ -293,6 +296,9 @@
 
 /* Define to 1 if you have the `getmntinfo' function. */
 #undef HAVE_GETMNTINFO
+
+/* Define to 1 if you have the `getnetgrent' function. */
+#undef HAVE_GETNETGRENT
 
 /* Define to 1 if you have the `getopt' function. */
 #undef HAVE_GETOPT
@@ -882,6 +888,9 @@
 
 /* Define to 1 if you have the `setmntent' function. */
 #undef HAVE_SETMNTENT
+
+/* Define to 1 if you have the `setnetgrent' function. */
+#undef HAVE_SETNETGRENT
 
 /* Define to 1 if you have the `setsid' function. */
 #undef HAVE_SETSID

man/snmpd.conf.5.def
@@ -434,6 +434,14 @@ com2sec sec1 10.0.0.0/8 public
 .IP
 Access from outside of 10.0.0.0/8 would still be denied.
 .IP
+It is also possible to reference a specific \fInetgroup\fR starting with an
+'@' character (e.g.  @adminhosts). The \fInetgroup\fR lookup is running
+through the NSS (Name Services Switch) making it possible to define the
+group locally or via NIS/LDAP.
+.IP
+Note: The hostname DNS lookup and \fInetgroup\fR resolution is done only
+during snmpd start or reload.
+.IP
 The same community string can be specified in several separate directives
 (presumably with different source tokens), and the first source/community
 combination that matches the incoming request will be selected.

snmplib/transports/snmpUDPDomain.c
@@ -445,6 +445,10 @@ netsnmp_udp_parse_security(const char *token, char *param)
         network.s_addr = 0;
         mask.s_addr = 0;
         negate = 0;
+        /* Create a new com2Sec entry. */
+        rc = netsnmp_udp_com2SecEntry_create(NULL, community, secName, contextName,
+                                             &network, &mask, negate);
+        netsnmp_udp_com2SecEntry_check_return_code(rc);
     } else {
         char *strmask;
         if (*source == '!') {
@@ -454,20 +458,44 @@ netsnmp_udp_parse_security(const char *token, char *param)
             negate = 0;
             sourcep = source;
         }
-        /* Parse source address and network mask. */
-        if(netsnmp_udp_resolve_source(sourcep, &network, &mask)) {
-            config_perror("source address/network mask parsing issue");
-            return;
+#if HAVE_ENDNETGRENT && HAVE_GETNETGRENT && HAVE_SETNETGRENT
+        /* Interpret as netgroup */
+        if (*sourcep == '@') {
+            char *netgroup = sourcep+1;
+            char *host, *user, *domain;
+            if(setnetgrent(netgroup)) {
+                while (getnetgrent(&host, &user, &domain)) {
+                    /* Parse source address and network mask for each netgroup host. */
+                    if (netsnmp_udp_resolve_source(host, &network, &mask) == 0) {
+                        /* Create a new com2Sec entry. */
+                        rc = netsnmp_udp_com2SecEntry_create(NULL, community, secName, contextName,
+                                                             &network, &mask, negate);
+                        netsnmp_udp_com2SecEntry_check_return_code(rc);
+                    } else {
+                        config_perror("netgroup host address parsing issue");
+                        break;
+                    }
+                }
+                endnetgrent();
+            } else {
+                config_perror("netgroup could not be found");
+            }
+        }
+        /* Without '@' it has to be an address or hostname */
+        else
+#endif
+        {
+            /* Parse source address and network mask. */
+            if(netsnmp_udp_resolve_source(sourcep, &network, &mask) == 0) {
+                /* Create a new com2Sec entry. */
+                rc = netsnmp_udp_com2SecEntry_create(NULL, community, secName, contextName,
+                                                     &network, &mask, negate);
+                netsnmp_udp_com2SecEntry_check_return_code(rc);
+            } else {
+                config_perror("source address/network mask parsing issue");
+            }
         }
     }
-
-    /*
-     * Everything is okay.  Copy the parameters to the structure allocated
-     * above and add it to END of the list.
-     */
-    rc = netsnmp_udp_com2SecEntry_create(NULL, community, secName, contextName,
-                                         &network, &mask, negate);
-    netsnmp_udp_com2SecEntry_check_return_code(rc);
 }
 
 void

commit 0aff9e9c878c9fd96095954ef4e6002a13f8cb35

configure: Add const checks for X509_NAME_get_text_by_NID and getnetgrent
Add autoconf checks to detect whether the first argument of
X509_NAME_get_text_by_NID() and getnetgrent() is a const pointer:
- In configure.d/config_os_functions, test whether getnetgrent() accepts
  a const char ** as its first argument and define
  GETNETGRENT_ARG1_IS_CONST if so.
- In configure.d/config_os_libs2, test whether
  X509_NAME_get_text_by_NID() accepts a const X509_NAME * as its first
  argument and define X509_NAME_GET_TEXT_BY_NID_ARG1_IS_CONST if so.
- Regenerate configure and include/net-snmp/net-snmp-config.h.in.
- Define X509_NAME_GET_TEXT_BY_NID_ARG1_IS_CONST in
  win32/net-snmp/net-snmp-config.h and
  win32/net-snmp/net-snmp-config.h.in.

Fixes: de18a4b6e2ac ("Win32, MSVC: Fix compiler and linker warnings")
Fixes: 20e2bb7d75c3 ("Add '@' netgroup functionality")

configure
@@ -30933,6 +30933,40 @@ rm -f core conftest.err conftest.$ac_objext conftest.beam conftest.$ac_ext
 
 printf "%s\n" "#define SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER $ac_cv_saccaq" >>confdefs.h
 
+
+        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking whether the first argument of X509_NAME_get_text_by_NID() is a const pointer" >&5
+printf %s "checking whether the first argument of X509_NAME_get_text_by_NID() is a const pointer... " >&6; }
+        cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+          #ifdef HAVE_OPENSSL_X509_H
+          #include <openssl/x509.h>
+          #endif
+
+int
+main (void)
+{
+
+          const X509_NAME *name = NULL;
+          return X509_NAME_get_text_by_NID(name, 0, NULL, 0);
+
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_compile "$LINENO"
+then :
+  { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+printf "%s\n" "yes" >&6; }
+
+printf "%s\n" "#define X509_NAME_GET_TEXT_BY_NID_ARG1_IS_CONST 1" >>confdefs.h
+
+else case e in #(
+  e) { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: no" >&5
+printf "%s\n" "no" >&6; } ;;
+esac
+fi
+rm -f core conftest.err conftest.$ac_objext conftest.beam conftest.$ac_ext
         CFLAGS="$netsnmp_save_CFLAGS"
 
         if echo " $transport_result_list " | $GREP "DTLS" > /dev/null; then
@@ -33724,6 +33758,50 @@ printf "%s\n" "void" >&6; } ;;
 esac
 fi
 rm -f core conftest.err conftest.$ac_objext conftest.beam conftest.$ac_ext
+fi
+
+if test x$ac_cv_func_getnetgrent = xyes; then
+{ printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking whether the first argument of getnetgrent() is a const pointer" >&5
+printf %s "checking whether the first argument of getnetgrent() is a const pointer... " >&6; }
+netsnmp_save_CFLAGS="$CFLAGS"
+CFLAGS="$CFLAGS -Werror"
+cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+#ifdef HAVE_NETDB_H
+    #include <netdb.h>
+#endif
+#ifdef HAVE_NETGROUP_H
+    #include <netgroup.h>
+#endif
+
+int
+main (void)
+{
+
+    const char *host = NULL;
+    char *user = NULL, *domain = NULL;
+    return getnetgrent(&host, (void *)&user, (void *)&domain);
+
+
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_compile "$LINENO"
+then :
+  { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+printf "%s\n" "yes" >&6; }
+
+printf "%s\n" "#define GETNETGRENT_ARG1_IS_CONST 1" >>confdefs.h
+
+else case e in #(
+  e) { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: no" >&5
+printf "%s\n" "no" >&6; } ;;
+esac
+fi
+rm -f core conftest.err conftest.$ac_objext conftest.beam conftest.$ac_ext
+CFLAGS="$netsnmp_save_CFLAGS"
 fi
 
 { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking for _NSGetArgv()" >&5

configure.d/config_os_functions
@@ -54,6 +54,30 @@ AC_COMPILE_IFELSE([AC_LANG_PROGRAM(
     [AC_MSG_RESULT([void])])
 fi
 
+if test x$ac_cv_func_getnetgrent = xyes; then
+AC_MSG_CHECKING([whether the first argument of getnetgrent() is a const pointer])
+netsnmp_save_CFLAGS="$CFLAGS"
+CFLAGS="$CFLAGS -Werror"
+AC_COMPILE_IFELSE([AC_LANG_PROGRAM(
+        [[
+#ifdef HAVE_NETDB_H
+    #include <netdb.h>
+#endif
+#ifdef HAVE_NETGROUP_H
+    #include <netgroup.h>
+#endif
+]],[[
+    const char *host = NULL;
+    char *user = NULL, *domain = NULL;
+    return getnetgrent(&host, (void *)&user, (void *)&domain);
+]]
+    )], [AC_MSG_RESULT([yes])
+	 AC_DEFINE([GETNETGRENT_ARG1_IS_CONST], [1],
+            [Define if the first argument of getnetgrent() is a const pointer])],
+    [AC_MSG_RESULT([no])])
+CFLAGS="$netsnmp_save_CFLAGS"
+fi
+
 AC_MSG_CHECKING([for _NSGetArgv()])
 AC_LINK_IFELSE(
   [AC_LANG_PROGRAM([#include <crt_externs.h>], [return _NSGetArgv])],

configure.d/config_os_libs2
@@ -440,6 +440,20 @@ if test "x$tryopenssl" != "xno" -a "x$tryopenssl" != "xinternal"; then
         AC_DEFINE_UNQUOTED([SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER],
                            [$ac_cv_saccaq],
                            [Qualifier for second argument of the appverify_cookie_cb() callback function])
+
+        AC_MSG_CHECKING([whether the first argument of X509_NAME_get_text_by_NID() is a const pointer])
+        AC_COMPILE_IFELSE([AC_LANG_PROGRAM([[
+          #ifdef HAVE_OPENSSL_X509_H
+          #include <openssl/x509.h>
+          #endif
+        ]],[[
+          const X509_NAME *name = NULL;
+          return X509_NAME_get_text_by_NID(name, 0, NULL, 0);
+        ]])],
+        [AC_MSG_RESULT([yes])
+         AC_DEFINE([X509_NAME_GET_TEXT_BY_NID_ARG1_IS_CONST], [1],
+            [Define if the first argument of X509_NAME_get_text_by_NID() is a const pointer])],
+        [AC_MSG_RESULT([no])])
         CFLAGS="$netsnmp_save_CFLAGS"
 
         if echo " $transport_result_list " | $GREP "DTLS" > /dev/null; then

include/net-snmp/net-snmp-config.h.in
@@ -71,6 +71,9 @@
 /* location of mount table list */
 #undef ETC_MNTTAB
 
+/* Define if the first argument of getnetgrent() is a const pointer */
+#undef GETNETGRENT_ARG1_IS_CONST
+
 /* Define to 1 if you have the `AES_cfb128_encrypt' function. */
 #undef HAVE_AES_CFB128_ENCRYPT
 
@@ -1857,6 +1860,10 @@
 
 /* Where is the uname command */
 #undef UNAMEPROG
+
+/* Define if the first argument of X509_NAME_get_text_by_NID() is a const
+   pointer */
+#undef X509_NAME_GET_TEXT_BY_NID_ARG1_IS_CONST
 
 /* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the

win32/net-snmp/net-snmp-config.h
@@ -1365,6 +1365,8 @@
 
 #define SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER const
 
+#define X509_NAME_GET_TEXT_BY_NID_ARG1_IS_CONST 1
+
 /* Define to 1 if you have the <openssl/aes.h> header file. */
 #define HAVE_OPENSSL_AES_H 1
 

win32/net-snmp/net-snmp-config.h.in
@@ -1365,6 +1365,8 @@
 
 #define SECOND_APPVERIFY_COOKIE_CB_ARG_QUALIFIER const
 
+#define X509_NAME_GET_TEXT_BY_NID_ARG1_IS_CONST 1
+
 /* Define to 1 if you have the <openssl/aes.h> header file. */
 #define HAVE_OPENSSL_AES_H 1
 