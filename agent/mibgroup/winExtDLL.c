net-snmp/net-snmp

commit d9aad5695ed8964c142867a6d7888c9dd90c7361

libsnmp, perl, apps: Fix -Wdeclaration-after-statement warnings

agent/snmp_perl.c
@@ -1,5 +1,6 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #include <EXTERN.h>
 #include "perl.h"
 #pragma GCC diagnostic pop

perl/ASN/ASN.xs
@@ -1,8 +1,12 @@
 /* -*- C -*- */
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#pragma GCC diagnostic pop
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/library/asn1.h>

perl/OID/OID.xs
@@ -1,8 +1,12 @@
 /* -*- C -*- */
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#pragma GCC diagnostic pop
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>

perl/SNMP/SNMP.xs
@@ -8,9 +8,13 @@
      modify it under the same terms as Perl itself.
 */
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#pragma GCC diagnostic pop
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>

perl/TrapReceiver/TrapReceiver.xs
@@ -1,8 +1,12 @@
 /* -*- c -*- */
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#pragma GCC diagnostic pop
 
 #include "ppport.h"
 

perl/agent/agent.xs
@@ -1,8 +1,13 @@
 /* -*- C -*- */
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#pragma GCC diagnostic pop
+
 #include <netdb.h>
 #include <sys/socket.h>
 

perl/agent/default_store/default_store.xs
@@ -1,6 +1,10 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#pragma GCC diagnostic pop
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/agent/ds_agent.h>

perl/default_store/default_store.xs
@@ -1,6 +1,10 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#pragma GCC diagnostic pop
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/library/default_store.h>

commit 290c350131e8eae22d76e856cade8e8bcd1d6d16

Win32, MSVC: Fix compiler and linker warnings
Fix multiple compiler and linker warnings reported during Windows MSVC
builds:

- In snmplib/snmp_openssl.c, add const qualifiers to X509_NAME,
  X509_NAME_ENTRY, ASN1_STRING, and X509_EXTENSION pointers matching
  modern OpenSSL 3.x/4.x API signatures to eliminate MSVC warning C4090
  ('=': different 'const' qualifiers). Use NETSNMP_REMOVE_CONST where
  casting is necessary.
- In win32/agent_module_includes.h and win32/mib_module_includes.h,
  include mibgroup/utilities/iquery.h,
  mibgroup/if-mib/ifTable/ifTable.h, mibgroup/if-mib/ifXTable/ifXTable.h,
  and net-snmp/data_access/interface.h to provide declarations for
  init_iquery(), init_ifTable(), init_ifXTable(), and init_interface(),
  resolving MSVC warning C4013.
- In perl/*/*.xs, guard #pragma GCC diagnostic blocks with
  #ifdef __GNUC__ to prevent MSVC warning C4068 (unknown pragma 'GCC').
- In perl/SNMP/SNMP.xs, nest the #if __has_attribute check inside
  #if defined(__has_attribute) to avoid MSVC preprocessor warning C4067.
- In agent/mibgroup/winExtDLL.c, suppress MSVC warning C4996 for the
  deprecated GetVersionEx() call using #pragma warning directives.
- In win32/Configure, remove the unsupported /MANIFEST:EMBED switch from
  LDFLAGS to eliminate MSVC linker warning LNK4044. Embedding manifests
  was essential only for MSVC 8 (Visual Studio 2005) and MSVC 9 (Visual
  Studio 2008) due to C runtime WinSxS assembly dependencies, but is
  unnecessary for MSVC 10 (Visual Studio 2010) and later.

Fixes: d9aad5695ed8 ("libsnmp, perl, apps: Fix -Wdeclaration-after-statement warnings")
Fixes: d1372b63fdf0 ("Perl: SNMP: Suppress a compiler warning")
Fixes: 73f2db7f6695 ("Win32, MSVC: Use the /MANIFEST:EMBED linker option instead of mt.exe")

agent/mibgroup/winExtDLL.c
@@ -316,7 +316,14 @@ init_winExtDLL(void)
 
     DEBUGMSG(("winExtDLL", "init_winExtDLL started.\n"));
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4996)
+#endif
     GetVersionEx(&s_versioninfo);
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
     IsWow64Process = (pfIsWow64Process)(uintptr_t)
 	GetProcAddress(GetModuleHandle("kernel32"), "IsWow64Process");

agent/snmp_perl.c
@@ -1,9 +1,16 @@
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wundef"
+#endif
 #include <EXTERN.h>
 #include "perl.h"
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>

perl/ASN/ASN.xs
@@ -1,12 +1,19 @@
 /* -*- C -*- */
 
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wundef"
+#endif
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/library/asn1.h>

perl/OID/OID.xs
@@ -1,12 +1,19 @@
 /* -*- C -*- */
 
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wundef"
+#endif
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>

perl/SNMP/SNMP.xs
@@ -31,13 +31,20 @@
     #pragma push_macro("select")
 #endif
 
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wundef"
+#endif
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 #ifdef WIN32
     /* 5. Restore Winsock macro definitions, overriding Perl's redefinitions */
@@ -460,8 +467,10 @@ int type;
 #define USE_ENUMS 1
 #define USE_SPRINT_VALUE 2
 static int
-#if defined(__has_attribute) && __has_attribute(nonnull)
+#if defined(__has_attribute)
+#if __has_attribute(nonnull)
 __attribute__((nonnull(1)))
+#endif
 #endif
 __snprint_value(char *buf, size_t buf_len, netsnmp_variable_list *var,
                 struct tree *tp, int type, int flag)

perl/TrapReceiver/TrapReceiver.xs
@@ -1,12 +1,19 @@
 /* -*- c -*- */
 
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wundef"
+#endif
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 #include "ppport.h"
 

perl/agent/agent.xs
@@ -1,12 +1,19 @@
 /* -*- C -*- */
 
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wundef"
+#endif
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 #include <float.h>
 #include <math.h>

perl/agent/default_store/default_store.xs
@@ -1,10 +1,17 @@
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wundef"
+#endif
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/agent/ds_agent.h>

perl/default_store/default_store.xs
@@ -1,10 +1,17 @@
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wundef"
+#endif
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/library/default_store.h>

snmplib/snmp_openssl.c
@@ -191,7 +191,7 @@ _cert_get_name(X509 *ocert, int which, char **buf, int *len, int flags)
 char *
 netsnmp_openssl_cert_get_subjectName(X509 *ocert, char **buf, int *len)
 {
-    X509_NAME       *osubj_name;
+    const X509_NAME *osubj_name;
     int              space;
     char            *buf_ptr;
 
@@ -237,12 +237,12 @@ netsnmp_openssl_cert_get_commonName(X509 *ocert, char **buf, int *len)
 void
 netsnmp_openssl_cert_dump_names(X509 *ocert)
 {
-    int              i, onid;
-    int              oname_value_type;
-    X509_NAME_ENTRY *oname_entry;
-    ASN1_STRING     *oname_value;
-    X509_NAME       *osubj_name;
-    const char      *prefix_short, *prefix_long;
+    int                    i, onid;
+    int                    oname_value_type;
+    const X509_NAME_ENTRY *oname_entry;
+    const ASN1_STRING     *oname_value;
+    const X509_NAME       *osubj_name;
+    const char            *prefix_short, *prefix_long;
 
     if (NULL == ocert)
         return;
@@ -282,7 +282,7 @@ netsnmp_openssl_cert_dump_names(X509 *ocert)
 #endif /* NETSNMP_FEATURE_REMOVE_CERT_DUMP_NAMES */
 
 static char *
-_cert_get_extension(X509_EXTENSION  *oext, char **buf, int *len, int flags)
+_cert_get_extension(const X509_EXTENSION  *oext, char **buf, int *len, int flags)
 {
     int              space;
     char            *buf_ptr = NULL;
@@ -297,7 +297,7 @@ _cert_get_extension(X509_EXTENSION  *oext, char **buf, int *len, int flags)
         snmp_log(LOG_ERR, "could not get bio for extension\n");
         return NULL;
     }
-    if (X509V3_EXT_print(bio, oext, 0, 0) != 1) {
+    if (X509V3_EXT_print(bio, NETSNMP_REMOVE_CONST(X509_EXTENSION *, oext), 0, 0) != 1) {
         snmp_log(LOG_ERR, "could not print extension!\n");
         goto out;
     }
@@ -336,7 +336,7 @@ out:
 X509_EXTENSION  *
 _cert_get_extension_at(X509 *ocert, int pos, char **buf, int *len, int flags)
 {
-    X509_EXTENSION  *oext;
+    const X509_EXTENSION  *oext;
 
     if ((NULL == ocert) || ((buf && !len) || (len && !buf)))
         return NULL;
@@ -348,7 +348,7 @@ _cert_get_extension_at(X509 *ocert, int pos, char **buf, int *len, int flags)
         return NULL;
     }
 
-    return oext;
+    return NETSNMP_REMOVE_CONST(X509_EXTENSION *, oext);
 }
 
 /** netsnmp_openssl_cert_get_extension: get extension field from cert
@@ -360,7 +360,7 @@ static char *
 _cert_get_extension_str_at(X509 *ocert, int pos, char **buf, int *len,
                            int flags)
 {
-    X509_EXTENSION  *oext;
+    const X509_EXTENSION  *oext;
 
     if ((NULL == ocert) || ((buf && !len) || (len && !buf)))
         return NULL;
@@ -494,10 +494,10 @@ netsnmp_openssl_cert_get_subjectAltNames(X509 *ocert, char **buf, int *len)
 void
 netsnmp_openssl_cert_dump_extensions(X509 *ocert)
 {
-    X509_EXTENSION  *extension;
-    const char      *extension_name;
-    char             buf[SNMP_MAXBUF], *buf_ptr = buf, *str, *lf;
-    int              i, num_extensions, buf_len, nid;
+    const X509_EXTENSION *extension;
+    const char           *extension_name;
+    char                  buf[SNMP_MAXBUF], *buf_ptr = buf, *str, *lf;
+    int                   i, num_extensions, buf_len, nid;
 
     if (NULL == ocert)
         return;
@@ -512,7 +512,7 @@ netsnmp_openssl_cert_dump_extensions(X509 *ocert)
         DEBUGMSGT(("9:cert:dump", "    0 extensions\n"));
     for(i = 0; i < num_extensions; i++) {
         extension = X509_get_ext(ocert, i);
-        nid = OBJ_obj2nid(X509_EXTENSION_get_object(extension));
+        nid = OBJ_obj2nid(X509_EXTENSION_get_object(NETSNMP_REMOVE_CONST(X509_EXTENSION *, extension)));
         extension_name = OBJ_nid2sn(nid);
         buf_len = sizeof(buf);
         str = _cert_get_extension_str_at(ocert, i, &buf_ptr, &buf_len, 0);

win32/Configure
@@ -228,8 +228,7 @@ my %makefile_subs = (
     "lfl" => [ "^LDFLAGS=",    "LDFLAGS=" .
                                ($config eq "debug" ? "/debug " : "") .
                                ($openssllibdir ? "/libpath:$openssllibdir" :
-                                "") . " " .
-                               "/MANIFEST:EMBED"
+                                "")
              ],
     "rsc" => [ "^RSCFLAGS=",   "RSCFLAGS=/l 0x409 " .
                                ($config eq "release" ? "/d NDEBUG" :

win32/agent_module_includes.h
@@ -1,5 +1,6 @@
 /* This file is automatically generated by configure.  Do not modify by hand. */
 #include "mibgroup/snmpv3/usmConf.h"
+#include "mibgroup/utilities/iquery.h"
 #include "mibgroup/agentx/master.h"
 #include "mibgroup/agentx/subagent.h"
 #include "mibgroup/mibII/vacm_conf.h"

win32/mib_module_includes.h
@@ -46,6 +46,9 @@
 #ifdef HAVE_WIN32_PLATFORM_SDK
 #include "mibgroup/mibII/at.h"
 #include "mibgroup/mibII/interfaces.h"
+#include "mibgroup/if-mib/ifTable/ifTable.h"
+#include "mibgroup/if-mib/ifXTable/ifXTable.h"
+#include <net-snmp/data_access/interface.h>
 #include "mibgroup/mibII/tcp.h"
 #include "mibgroup/mibII/icmp.h"
 #include "mibgroup/mibII/ip.h"