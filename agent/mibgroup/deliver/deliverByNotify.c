net-snmp/net-snmp

commit fefe3a0ed809830a620ec692a750b03bfe8590b5

beginning template code for delivery of data by notifications
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@20197 06827809-a52a-0410-b366-d66718629ded

agent/mibgroup/deliver/deliverByNotify.c
@@ -1,0 +1,30 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-features.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+
+#include "deliveryByNotify.h"
+
+void parse_deliver_config(const char *, char *);
+void free_deliver_config(void);
+
+/** Initializes the mteTrigger module */
+void
+init_mteTrigger(void)
+{
+    snmpd_register_config_handler("deliver",
+                                  &parse_deliver_config, &free_deliver_config,
+                                  "foo");
+}
+
+void
+parse_deliver_config(const char *token, char *line) {
+}
+
+void
+free_deliver_config(void) {
+}
+
+void
+deliver_execute(void) {
+}

agent/mibgroup/deliver/deliverByNotify.h
@@ -1,0 +1,11 @@
+#ifndef DELIVERBYNOTIFY_H
+#define DELIVERBYNOTIFY_H 1
+
+void init_mteTrigger(void);
+
+void parse_deliver_config(const char *, char *);
+void free_deliver_config(void);
+
+void deliver_execute(void);
+
+#endif /* deliverByNotify_h */

commit 4d7ca03f47272e60aa4381eb8fc86cc33943c232

snmpd: Remove unused static variables in deliverByNotify and hr_partition
Remove unused static variables:
- In agent/mibgroup/deliver/deliverByNotify.c, remove default_max_size
  and its unused assignments.
- In agent/mibgroup/host/hr_partition.c, remove HRP_savedDiskIndex and
  HRP_savedPartIndex in Save_HR_Partition().

Fixes: fefe3a0ed809 ("beginning template code for delivery of data by notifications")
Fixes: cd704ad86274 ("- (configure, configure.in, agent/mibgroup/*):")

agent/mibgroup/deliver/deliverByNotify.c
@@ -45,7 +45,6 @@ size_t netsnmp_max_message_number_oid_len = 13;
 oid objid_snmptrap[] = { 1, 3, 6, 1, 6, 3, 1, 1, 4, 1, 0 };
 
 #define DEFAULT_MAX_DELIVER_SIZE -1
-static int default_max_size;
 unsigned int alarm_reg;
 static netsnmp_container *deliver_container;
 
@@ -103,9 +102,6 @@ init_deliverByNotify(void)
     }
     deliver_container->container_name = strdup("deliverByNotify");
     deliver_container->compare = _deliver_compare;
-
-    /* set the defaults */
-    default_max_size = DEFAULT_MAX_DELIVER_SIZE;
 
     alarm_reg = 0;
 }
@@ -238,7 +234,6 @@ parse_deliver_config(const char *token, char *line) {
 
 void
 parse_deliver_maxsize_config(const char *token, char *line) {
-    default_max_size = atoi(line);
 }
 
 static void
@@ -253,7 +248,6 @@ _free_deliver_obj(void *p, void *context)
 
 void
 free_deliver_config(void) {
-    default_max_size = DEFAULT_MAX_DELIVER_SIZE;
     CONTAINER_CLEAR(deliver_container, _free_deliver_obj, NULL);
     if (alarm_reg) {
         snmp_alarm_unregister(alarm_reg);

agent/mibgroup/host/hr_partition.c
@@ -41,8 +41,6 @@
 	 *
 	 *********************/
 
-static int      HRP_savedDiskIndex;
-static int      HRP_savedPartIndex;
 static char     HRP_savedName[1024];
 static char     HRP_savedLabel[1024];
 
@@ -329,8 +327,6 @@ Get_Next_HR_Partition(void)
 static void
 Save_HR_Partition(int disk_idx, int part_idx)
 {
-    HRP_savedDiskIndex = disk_idx;
-    HRP_savedPartIndex = part_idx;
     (void) Get_Next_HR_Disk_Partition(HRP_savedName, sizeof(HRP_savedName), HRP_index);
     (void) Get_HR_Disk_Label(HRP_savedLabel, sizeof(HRP_savedLabel), HRP_savedName);
 }