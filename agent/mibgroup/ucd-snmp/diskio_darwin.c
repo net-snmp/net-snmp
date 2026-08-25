net-snmp/net-snmp

commit 1059ad306040bfb2604101e53ac6642de8362951

agent/mibgroup/ucd-snmp/diskio.c: Split this file
Split this source file into one source file per supported operating system.

agent/mibgroup/ucd-snmp/diskio_darwin.c
@@ -1,0 +1,276 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/agent/agent_callbacks.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/IOKitLib.h>
+#include <IOKit/storage/IOBlockStorageDriver.h>
+#include <IOKit/storage/IOMedia.h>
+#include <IOKit/IOBSD.h>
+#include "struct.h"
+#include "diskio.h"
+#include "diskio_darwin.h"
+#include "util_funcs/header_simple_table.h"
+
+#define MAXDRIVES	16	/* most drives we will record */
+#define MAXDRIVENAME	31	/* largest drive name we allow */
+
+#define kIDXBytesRead		0	/* used as index into the stats array in a drivestats struct */
+#define kIDXBytesWritten	1
+#define kIDXNumReads		2
+#define kIDXNumWrites		3
+#define kIDXBytesReadXhi	4
+#define kIDXBytesReadXlo	5
+#define kIDXBytesWrittenXhi	6
+#define kIDXBytesWrittenXlo	7
+#define kIDXLast		7
+
+struct drivestats {
+    char name[MAXDRIVENAME + 1];
+    long bsd_unit_number;
+    long stats[kIDXLast+1];
+};
+
+static mach_port_t masterPort;		/* to communicate with I/O Kit	*/
+
+static struct drivestats drivestat[MAXDRIVES];
+
+static mach_port_t masterPort;		/* to communicate with I/O Kit	*/
+
+static int num_drives;			/* number of drives detected	*/
+
+void init_diskio_darwin(void)
+{
+    /* Get the I/O Kit communication handle. */
+    IOMasterPort(bootstrap_port, &masterPort);
+}    
+
+static int
+collect_drive_stats(io_registry_entry_t driver, long *stats)
+{
+    CFNumberRef     number;
+    CFDictionaryRef properties;
+    CFDictionaryRef statistics;
+    long            value;
+    SInt64          value64;
+    kern_return_t   status;
+    int             i;
+
+
+    /*
+     * If the drive goes away, we may not get any properties
+     * for it.  So take some defaults. Nb: use memset ??
+     */
+    for (i = 0; i < kIDXLast; i++) {
+	stats[i] = 0;
+    }
+
+    /* retrieve the properties */
+    status = IORegistryEntryCreateCFProperties(driver,
+                                               NETSNMP_REMOVE_CONST(struct __CFDictionary **, &properties),
+					       kCFAllocatorDefault, kNilOptions);
+    if (status != KERN_SUCCESS) {
+	snmp_log(LOG_ERR, "diskio: device has no properties\n");
+/*	fprintf(stderr, "device has no properties\n"); */
+	return (1);
+    }
+
+    /* retrieve statistics from properties */
+    statistics = (CFDictionaryRef)CFDictionaryGetValue(properties,
+						       CFSTR(kIOBlockStorageDriverStatisticsKey));
+    if (statistics) {
+
+	/* Now hand me the crystals. */
+	if ((number = (CFNumberRef)CFDictionaryGetValue(statistics,
+						 CFSTR(kIOBlockStorageDriverStatisticsBytesReadKey)))) {
+	    CFNumberGetValue(number, kCFNumberSInt32Type, &value);
+	    stats[kIDXBytesRead] = value;
+	}
+
+	if ((number = (CFNumberRef)CFDictionaryGetValue(statistics,
+						 CFSTR(kIOBlockStorageDriverStatisticsBytesWrittenKey)))) {
+	    CFNumberGetValue(number, kCFNumberSInt32Type, &value);
+	    stats[kIDXBytesWritten] = value;
+	}
+
+	if ((number = (CFNumberRef)CFDictionaryGetValue(statistics,
+						 CFSTR(kIOBlockStorageDriverStatisticsReadsKey)))) {
+	    CFNumberGetValue(number, kCFNumberSInt32Type, &value);
+	    stats[kIDXNumReads] = value;
+	}
+	if ((number = (CFNumberRef)CFDictionaryGetValue(statistics,
+						 CFSTR(kIOBlockStorageDriverStatisticsWritesKey)))) {
+	    CFNumberGetValue(number, kCFNumberSInt32Type, &value);
+	    stats[kIDXNumWrites] = value;
+	}
+	/* grab the 64 bit versions of the bytes read */
+	if ((number = (CFNumberRef)CFDictionaryGetValue(statistics,
+						 CFSTR(kIOBlockStorageDriverStatisticsBytesReadKey)))) {
+	    CFNumberGetValue(number, kCFNumberSInt64Type, &value64);
+	    stats[kIDXBytesReadXhi] = (long)(value64 >> 32);
+	    stats[kIDXBytesReadXlo] = (long)(value64 & 0xffffffff);	
+	}
+		
+	/* grab the 64 bit versions of the bytes written */
+	if ((number = (CFNumberRef)CFDictionaryGetValue(statistics,
+						 CFSTR(kIOBlockStorageDriverStatisticsBytesWrittenKey)))) {
+	    CFNumberGetValue(number, kCFNumberSInt64Type, &value64);
+	    stats[kIDXBytesWrittenXhi] = (long)(value64 >> 32);
+	    stats[kIDXBytesWrittenXlo] = (long)(value64 & 0xffffffff);	
+	}
+    }
+    /* we're done with the properties, release them */
+    CFRelease(properties);
+    return (0);
+}
+
+/*
+ * Check whether an IORegistryEntry refers to a valid
+ * I/O device, and if so, collect the information.
+ */
+static int
+handle_drive(io_registry_entry_t drive, struct drivestats * dstat)
+{
+    io_registry_entry_t parent;
+    CFMutableDictionaryRef     properties;
+    CFStringRef         name;
+    CFNumberRef         number;
+    kern_return_t       status;
+
+    /* get drive's parent */
+    status = IORegistryEntryGetParentEntry(drive, kIOServicePlane, &parent);
+    if (status != KERN_SUCCESS) {
+	snmp_log(LOG_ERR, "diskio: device has no parent\n");
+/*	fprintf(stderr, "device has no parent\n"); */
+	return(1);
+    }
+
+    if (IOObjectConformsTo(parent, "IOBlockStorageDriver")) {
+
+	/* get drive properties */
+	status = IORegistryEntryCreateCFProperties(drive, &properties,
+					    kCFAllocatorDefault, kNilOptions);
+	if (status != KERN_SUCCESS) {
+	    snmp_log(LOG_ERR, "diskio: device has no properties\n");
+/*	    fprintf(stderr, "device has no properties\n"); */
+	    return(1);
+	}
+
+	/* get BSD name and unitnumber from properties */
+	name = (CFStringRef)CFDictionaryGetValue(properties,
+					  CFSTR(kIOBSDNameKey));
+	number = (CFNumberRef)CFDictionaryGetValue(properties,
+					    CFSTR(kIOBSDUnitKey));
+
+	/* Collect stats and if succesful store them with the name and unitnumber */
+	if (name && number && !collect_drive_stats(parent, dstat->stats)) {
+
+	    CFStringGetCString(name, dstat->name, MAXDRIVENAME, CFStringGetSystemEncoding());
+	    CFNumberGetValue(number, kCFNumberSInt32Type, &dstat->bsd_unit_number);
+	    num_drives++;
+	}
+
+	/* clean up, return success */
+	CFRelease(properties);
+	return(0);
+    }
+
+    /* failed, don't keep parent */
+    IOObjectRelease(parent);
+    return(1);
+}
+
+static int
+diskio_getstats(void)
+{
+    time_t                 now;
+    io_iterator_t          drivelist;
+    io_registry_entry_t    drive;
+    CFMutableDictionaryRef match;
+    kern_return_t          status;
+
+    now = time(NULL);	/* register current time and check wether cache can be used */
+    if (diskio_cache_valid(now)) {
+        return 0;
+    }
+
+    /*  Retrieve a list of drives. */
+    match = IOServiceMatching("IOMedia");
+    CFDictionaryAddValue(match, CFSTR(kIOMediaWholeKey), kCFBooleanTrue);
+    status = IOServiceGetMatchingServices(masterPort, match, &drivelist);
+    if (status != KERN_SUCCESS) {
+	snmp_log(LOG_ERR, "diskio: couldn't match whole IOMedia devices\n");
+/*	fprintf(stderr,"Couldn't match whole IOMedia devices\n"); */
+	return -1;
+    }
+
+    num_drives = 0;  /* NB: Incremented by handle_drive */
+    while ((drive = IOIteratorNext(drivelist)) && (num_drives < MAXDRIVES)) {
+	handle_drive(drive, &drivestat[num_drives]);
+	IOObjectRelease(drive);
+    }
+    IOObjectRelease(drivelist);
+
+    diskio_set_cache_time(now);
+    return 0;
+}
+
+u_char         *
+var_diskio(struct variable * vp,
+           oid * name,
+           size_t * length,
+           int exact, size_t * var_len, WriteMethod ** write_method)
+{
+    static long     long_ret;
+    static struct   counter64 c64_ret;
+    unsigned int    indx;
+
+    if (diskio_getstats() == 1) {
+        return NULL;
+    }
+
+
+    if (header_simple_table
+        (vp, name, length, exact, var_len, write_method, num_drives)) {
+        return NULL;
+    }
+
+    indx = (unsigned int) (name[*length - 1] - 1);
+
+    if (indx >= num_drives)
+        return NULL;
+
+    switch (vp->magic) {
+	case DISKIO_INDEX:
+	    long_ret = (long) drivestat[indx].bsd_unit_number;
+	    return (u_char *) & long_ret;
+	case DISKIO_DEVICE:
+	    *var_len = strlen(drivestat[indx].name);
+	    return (u_char *) drivestat[indx].name;
+	case DISKIO_NREAD:
+	    long_ret = (signed long) drivestat[indx].stats[kIDXBytesRead];
+	    return (u_char *) & long_ret;
+	case DISKIO_NWRITTEN:
+	    long_ret = (signed long) drivestat[indx].stats[kIDXBytesWritten];
+	    return (u_char *) & long_ret;
+	case DISKIO_READS:
+	    long_ret = (signed long) drivestat[indx].stats[kIDXNumReads];
+	    return (u_char *) & long_ret;
+	case DISKIO_WRITES:
+	    long_ret = (signed long) drivestat[indx].stats[kIDXNumWrites];
+	    return (u_char *) & long_ret;
+	case DISKIO_NREADX:
+	    *var_len = 8;
+	    c64_ret.low = (signed long) drivestat[indx].stats[kIDXBytesReadXlo];
+	    c64_ret.high = (signed long) drivestat[indx].stats[kIDXBytesReadXhi];
+	    return (u_char *) & c64_ret;
+	case DISKIO_NWRITTENX:
+	    *var_len = 8;
+	    c64_ret.low = (signed long) drivestat[indx].stats[kIDXBytesWrittenXlo];
+	    c64_ret.high = (signed long) drivestat[indx].stats[kIDXBytesWrittenXhi];
+	    return (u_char *) & c64_ret;
+	default:
+	    ERROR_MSG("diskio.c: don't know how to handle this request.");
+    }
+    return NULL;
+}

agent/mibgroup/ucd-snmp/diskio.h
@@ -3,16 +3,29 @@
 
 config_require(util_funcs/header_simple_table)
 config_add_mib(UCD-DISKIO-MIB)
+#if defined(aix4)
+config_require(ucd-snmp/diskio_aix)
+#elif defined(bsdi3)
+config_require(ucd-snmp/diskio_bsdi)
+#elif defined(darwin)
+config_require(ucd-snmp/diskio_darwin)
+#elif defined(freebsd3)
+config_require(ucd-snmp/diskio_freebsd)
+#elif defined(linux)
+config_require(ucd-snmp/diskio_linux)
+#elif defined(netbsd)
+config_require(ucd-snmp/diskio_netbsd)
+#elif defined(openbsd4)
+config_require(ucd-snmp/diskio_openbsd)
+#elif defined(solaris2)
+config_require(ucd-snmp/diskio_solaris)
+#endif
 
-    /*
-     * Define all our functions using prototyping for ANSI compilers 
-     */
-    /*
-     * These functions are then defined in the example.c file 
-     */
-     void            init_diskio(void);
-     FindVarMethod   var_diskio;
-
+void            init_diskio(void);
+int             diskio_cache_valid(time_t now);
+void            diskio_set_cache_time(time_t now);
+void            devla_getstats(unsigned int regno, void *dummy);
+FindVarMethod   var_diskio;
 
 /*
  * Magic number definitions.  These numbers are the last oid index
@@ -35,5 +48,8 @@ config_add_mib(UCD-DISKIO-MIB)
 #define DISKIO_NREADX           12
 #define DISKIO_NWRITTENX        13
 #define DISKIO_BUSYTIME		14
+
+/* sampling interval, in seconds */
+#define DISKIO_SAMPLE_INTERVAL 5
 
 #endif                          /* _MIBGROUP_DISKIO_H */

agent/mibgroup/ucd-snmp/diskio_aix.c
@@ -1,0 +1,121 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/agent/agent_callbacks.h>
+/*
+ * handle disk statistics via libperfstat
+ */
+#ifdef HAVE_SYS_PROTOSW_H
+#include <sys/protosw.h>
+#endif
+#include <libperfstat.h>
+#include "diskio_aix.h"
+#include "diskio.h"
+#include "util_funcs/header_simple_table.h"
+
+static perfstat_disk_t *ps_disk;	/* storage for all disk values */
+static int ps_numdisks;			/* number of disks in system, may change while running */
+
+void init_diskio_aix(void)
+{
+    /* gather information on first request */
+}
+
+/*
+ * collect statistics for all disks
+ */
+int
+collect_disks(void)
+{
+    time_t          now;
+    int             i;
+    perfstat_id_t   first;
+
+    /* cache valid? if yes, just return */
+    now = time(NULL);
+    if (ps_disk != NULL && diskio_cache_valid(now)) {
+        return 0;
+    }
+
+    /* get number of disks we have */
+    i = perfstat_disk(NULL, NULL, sizeof(perfstat_disk_t), 0);
+    if(i <= 0) return 1;
+
+    /* if number of disks differs or structures are uninitialized, init them */
+    if(i != ps_numdisks || ps_disk == NULL) {
+        if(ps_disk != NULL) free(ps_disk);
+        ps_numdisks = i;
+        ps_disk = malloc(sizeof(perfstat_disk_t) * ps_numdisks);
+        if(ps_disk == NULL) return 1;
+    }
+
+    /* gather statistics about all disks we have */
+    strcpy(first.name, "");
+    i = perfstat_disk(&first, ps_disk, sizeof(perfstat_disk_t), ps_numdisks);
+    if(i != ps_numdisks) return 1;
+
+    diskio_set_cache_time(now);
+    return 0;
+}
+
+
+u_char         *
+var_diskio(struct variable * vp,
+           oid * name,
+           size_t * length,
+           int exact, size_t * var_len, WriteMethod ** write_method)
+{
+    static long     long_ret;
+    static struct counter64 c64_ret;
+    unsigned int    indx;
+
+    /* get disk statistics */
+    if (collect_disks())
+        return NULL;
+
+    if (header_simple_table
+        (vp, name, length, exact, var_len, write_method, ps_numdisks))
+        return NULL;
+
+    indx = (unsigned int) (name[*length - 1] - 1);
+    if (indx >= ps_numdisks)
+        return NULL;
+
+    /* deliver requested data on requested disk */
+    switch (vp->magic) {
+    case DISKIO_INDEX:
+        long_ret = (long) indx;
+        return (u_char *) & long_ret;
+    case DISKIO_DEVICE:
+        *var_len = strlen(ps_disk[indx].name);
+        return (u_char *) ps_disk[indx].name;
+    case DISKIO_NREAD:
+        long_ret = (signed long) ps_disk[indx].rblks * ps_disk[indx].bsize;
+        return (u_char *) & long_ret;
+    case DISKIO_NWRITTEN:
+        long_ret = (signed long) ps_disk[indx].wblks * ps_disk[indx].bsize;
+        return (u_char *) & long_ret;
+    case DISKIO_READS:
+        long_ret = (signed long) ps_disk[indx].xfers;
+        return (u_char *) & long_ret;
+    case DISKIO_WRITES:
+        long_ret = (signed long) 0;	/* AIX has just one value for read/write transfers */
+        return (u_char *) & long_ret;
+    case DISKIO_NREADX:
+        *var_len = sizeof(struct counter64);
+        c64_ret.low = (ps_disk[indx].rblks * ps_disk[indx].bsize) & 0xffffffff;
+        c64_ret.high = (ps_disk[indx].rblks * ps_disk[indx].bsize) >> 32;
+        return (u_char *) & c64_ret;
+    case DISKIO_NWRITTENX:
+        *var_len = sizeof(struct counter64);
+        c64_ret.low = (ps_disk[indx].wblks * ps_disk[indx].bsize) & 0xffffffff;
+        c64_ret.high = (ps_disk[indx].wblks * ps_disk[indx].bsize) >> 32;
+        return (u_char *) & c64_ret;
+
+    default:
+        ERROR_MSG("diskio.c: don't know how to handle this request.");
+    }
+
+    /* return NULL in case of error */
+    return NULL;
+}

agent/mibgroup/ucd-snmp/diskio_aix.h
@@ -1,0 +1,1 @@
+void init_diskio_aix(void);

agent/mibgroup/ucd-snmp/diskio_bsdi.c
@@ -1,0 +1,121 @@
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/agent/agent_callbacks.h>
+#include <string.h>
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <sys/disk.h>
+#include "diskio.h"
+#include "diskio_bsdi.h"
+#include "util_funcs/header_simple_table.h"
+
+static int      ndisk;
+static struct diskstats *dk;
+static char   **dkname;
+
+static int
+diskio_getstats(void)
+{
+    time_t          now;
+    int             mib[2];
+    char           *t, *tp;
+    size_t          size, dkn_size;
+    int             i;
+
+    now = time(NULL);
+    if (diskio_cache_valid(now)) {
+        return 1;
+    }
+    mib[0] = CTL_HW;
+    mib[1] = HW_DISKSTATS;
+    size = 0;
+    if (sysctl(mib, 2, NULL, &size, NULL, 0) < 0) {
+        perror("Can't get size of HW_DISKSTATS mib");
+        return 0;
+    }
+    if (ndisk != size / sizeof(*dk)) {
+        if (dk)
+            free(dk);
+        if (dkname) {
+            for (i = 0; i < ndisk; i++)
+                if (dkname[i])
+                    free(dkname[i]);
+            free(dkname);
+        }
+        ndisk = size / sizeof(*dk);
+        if (ndisk == 0)
+            return 0;
+        dkname = malloc(ndisk * sizeof(char *));
+        mib[0] = CTL_HW;
+        mib[1] = HW_DISKNAMES;
+        if (sysctl(mib, 2, NULL, &dkn_size, NULL, 0) < 0) {
+            perror("Can't get size of HW_DISKNAMES mib");
+            return 0;
+        }
+        tp = t = malloc(dkn_size);
+        if (sysctl(mib, 2, t, &dkn_size, NULL, 0) < 0) {
+            perror("Can't get size of HW_DISKNAMES mib");
+            return 0;
+        }
+        for (i = 0; i < ndisk; i++) {
+            dkname[i] = strdup(tp);
+            tp += strlen(tp) + 1;
+        }
+        free(t);
+        dk = malloc(ndisk * sizeof(*dk));
+    }
+    mib[0] = CTL_HW;
+    mib[1] = HW_DISKSTATS;
+    if (sysctl(mib, 2, dk, &size, NULL, 0) < 0) {
+        perror("Can't get HW_DISKSTATS mib");
+        return 0;
+    }
+    diskio_set_cache_time(now);
+    return 1;
+}
+
+u_char         *
+var_diskio(struct variable * vp,
+           oid * name,
+           size_t * length,
+           int exact, size_t * var_len, WriteMethod ** write_method)
+{
+    static long     long_ret;
+    unsigned int    indx;
+
+    if (diskio_getstats() == 0)
+        return 0;
+
+    if (header_simple_table
+        (vp, name, length, exact, var_len, write_method, ndisk))
+        return NULL;
+
+    indx = (unsigned int) (name[*length - 1] - 1);
+    if (indx >= ndisk)
+        return NULL;
+
+    switch (vp->magic) {
+    case DISKIO_INDEX:
+        long_ret = (long) indx + 1;
+        return (u_char *) & long_ret;
+    case DISKIO_DEVICE:
+        *var_len = strlen(dkname[indx]);
+        return (u_char *) dkname[indx];
+    case DISKIO_NREAD:
+        long_ret =
+            (signed long) (dk[indx].dk_sectors * dk[indx].dk_secsize);
+        return (u_char *) & long_ret;
+    case DISKIO_NWRITTEN:
+        return NULL;            /* Sigh... BSD doesn't keep seperate track */
+    case DISKIO_READS:
+        long_ret = (signed long) dk[indx].dk_xfers;
+        return (u_char *) & long_ret;
+    case DISKIO_WRITES:
+        return NULL;            /* Sigh... BSD doesn't keep seperate track */
+
+    default:
+        ERROR_MSG("diskio.c: don't know how to handle this request.");
+    }
+    return NULL;
+}

agent/mibgroup/ucd-snmp/diskio_bsdi.h
@@ -1,0 +1,1 @@
+void init_diskio_bsdi(void);

agent/mibgroup/ucd-snmp/diskio_darwin.h
@@ -1,0 +1,1 @@
+void init_diskio_darwin(void);

commit 2744c99db5a7569888dcbbce088b24eff0a18387

Darwin: Use IOMainPort instead of deprecated IOMasterPort if available
IOMasterPort() has been deprecated since macOS 12.0 in favor of
IOMainPort(). Add a configure test for IOMainPort() in
configure.d/config_os_functions and use IOMainPort() in
agent/mibgroup/ucd-snmp/diskio_darwin.c if HAVE_IOMAINPORT is defined.

Fixes: 1059ad306040 ("agent/mibgroup/ucd-snmp/diskio.c: Split this file")

agent/mibgroup/ucd-snmp/diskio_darwin.c
@@ -42,7 +42,11 @@ static int num_drives;			/* number of drives detected	*/
 void init_diskio_darwin(void)
 {
     /* Get the I/O Kit communication handle. */
+#ifdef HAVE_IOMAINPORT
+    IOMainPort(bootstrap_port, &masterPort);
+#else
     IOMasterPort(bootstrap_port, &masterPort);
+#endif
 }    
 
 static int

configure
@@ -34611,6 +34611,12 @@ fi
 
 
 # Agent, Darwin (Apple):
+ac_fn_c_check_func "$LINENO" "IOMainPort" "ac_cv_func_IOMainPort"
+if test "x$ac_cv_func_IOMainPort" = xyes
+then :
+  printf "%s\n" "#define HAVE_IOMAINPORT 1" >>confdefs.h
+
+fi
 ac_fn_c_check_func "$LINENO" "vm_region" "ac_cv_func_vm_region"
 if test "x$ac_cv_func_vm_region" = xyes
 then :
@@ -34624,6 +34630,18 @@ then :
 
 fi
 
+if test "x$ac_cv_func_IOMainPort" != "xyes"; then
+    netsnmp_save_LIBS="$LIBS"
+    LIBS="-framework IOKit $LIBS"
+    ac_fn_c_check_func "$LINENO" "IOMainPort" "ac_cv_func_IOMainPort"
+if test "x$ac_cv_func_IOMainPort" = xyes
+then :
+  printf "%s\n" "#define HAVE_IOMAINPORT 1" >>confdefs.h
+
+fi
+
+    LIBS="$netsnmp_save_LIBS"
+fi
 
 { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking for FD_CLOEXEC" >&5
 printf %s "checking for FD_CLOEXEC... " >&6; }

configure.d/config_os_functions
@@ -215,7 +215,13 @@ AC_CHECK_FUNCS([cgetnext        chown          endfsent        ] dnl
                [strncasecmp     system        usleep           ] )
 
 # Agent, Darwin (Apple):
-AC_CHECK_FUNCS([vm_region vm_region_64])
+AC_CHECK_FUNCS([IOMainPort vm_region vm_region_64])
+if test "x$ac_cv_func_IOMainPort" != "xyes"; then
+    netsnmp_save_LIBS="$LIBS"
+    LIBS="-framework IOKit $LIBS"
+    AC_CHECK_FUNCS([IOMainPort])
+    LIBS="$netsnmp_save_LIBS"
+fi
 
 AC_MSG_CHECKING([for FD_CLOEXEC])
 AC_COMPILE_IFELSE([AC_LANG_PROGRAM(

include/net-snmp/net-snmp-config.h.in
@@ -381,6 +381,9 @@
 /* Define to 1 if you have the <ioctls.h> header file. */
 #undef HAVE_IOCTLS_H
 
+/* Define to 1 if you have the 'IOMainPort' function. */
+#undef HAVE_IOMAINPORT
+
 /* Define to 1 if you have the <io.h> header file. */
 #undef HAVE_IO_H
 