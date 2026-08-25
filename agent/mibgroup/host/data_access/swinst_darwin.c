net-snmp/net-snmp

commit 4b47ecf4283e74db07d834e0353c0c42b4300216

Apple patches - add hrSWInstalledTable for darwin
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16693 06827809-a52a-0410-b366-d66718629ded

agent/mibgroup/host/data_access/swinst_darwin.c
@@ -1,0 +1,390 @@
+/*
+ * swinst.c : hrSWInstalledTable data access
+ */
+/*
+ * Copyright (C) 2007 Apple, Inc. All rights reserved.
+ * Use is subject to license terms specified in the COPYING file
+ * distributed with the Net-SNMP package.
+ */
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/library/container.h>
+#include <net-snmp/library/dir_utils.h>
+#include <net-snmp/library/snmp_debug.h>
+#include <net-snmp/data_access/swinst.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#define __APPLE_API_EVOLVING 1
+#include <sys/acl.h> /* or else CoreFoundation.h barfs */
+#undef __APPLE_API_EVOLVING 
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <ApplicationServices/ApplicationServices.h>
+
+/* ---------------------------------------------------------------------
+ */
+static int _add_applications_in_dir(netsnmp_container *, const char* path);
+static int32_t _index;
+static int _check_bundled_app(CFURLRef currentURL, CFStringRef *name,
+                              CFStringRef *info, const char* path);
+static int _check_classic_app(CFURLRef currentURL, CFStringRef *name,
+                              CFStringRef *info, const char* path);
+static netsnmp_container *dirs = NULL;
+
+/* ---------------------------------------------------------------------
+ */
+void
+netsnmp_swinst_arch_init( void )
+{
+    struct stat stat_buf;
+    const char *default_dirs[] = {
+        "/Applications",
+        "/Applications (Mac OS 9)",
+        "/System/Library/CoreServices",
+        "/System/Library/Extensions",
+        "/System/Library/Services"
+#ifdef TEST
+        , "/Developer/Applications"
+        , "/Volumes/audX/Applications (Mac OS 9)"
+#endif
+    };
+    int i, count = sizeof(default_dirs)/sizeof(default_dirs[0]);
+
+    /*
+     * create the container, if needed
+     */
+    if (NULL == dirs) {
+        dirs = netsnmp_container_find("directory_container:cstring");
+        if (NULL == dirs) {
+            snmp_log(LOG_ERR, "couldn't allocate container for dir list\n");
+            return;
+        }
+        dirs->container_name = strdup("directory search list");
+        netsnmp_binary_array_options_set(dirs, 1, CONTAINER_KEY_UNSORTED);
+    }
+
+    /*
+     * add dirs
+     */
+    for(i = 0; i < count; ++i) {
+        char *      tmp;
+        /** xxx: get/save the last mod date? */
+        if(-1 == stat(default_dirs[i], &stat_buf)) {
+            DEBUGMSGTL(("swinst:arch:darwin", "skipping dir %s\n",
+                        default_dirs[i]));
+            continue;
+        }
+        DEBUGMSGTL(("swinst:arch:darwin", "adding dir %s\n",
+                        default_dirs[i]));
+        tmp = strdup(default_dirs[i]);
+        if (NULL == tmp) {
+            snmp_log(LOG_ERR,"strdup failed\n");
+            break;
+        }
+        CONTAINER_INSERT(dirs, tmp);
+    }
+}
+
+void
+netsnmp_swinst_arch_shutdown( void )
+{
+    netsnmp_directory_container_free(dirs);
+}
+
+/* ---------------------------------------------------------------------
+ */
+
+int
+netsnmp_swinst_arch_load( netsnmp_container *container, u_int flags )
+{
+    netsnmp_iterator   *it;
+    const char         *dir;
+    int                 rc;
+
+    DEBUGMSGTL(("swinst:arch:darwin", "load\n"));
+
+    if (NULL == dirs) {
+        DEBUGMSGTL(("swinst:arch:darwin", "no dirs to scan!\n"));
+        return -1;
+    }
+
+    _index = 0;
+    
+    it = CONTAINER_ITERATOR(dirs);
+    for (dir = ITERATOR_FIRST(it); dir; dir = ITERATOR_NEXT(it)) {
+        rc = _add_applications_in_dir(container, dir);
+    }
+    ITERATOR_RELEASE(it);
+    DEBUGMSGTL(("swinst:arch:darwin", "loaded %d apps\n",_index));
+
+    return 0;
+}
+
+void  _dump_flags(u_long flags)
+{
+    static struct {
+        const char*name;
+        u_long bits;
+    } names[] = {
+        { "kLSItemInfoIsPlainFile", 0x00000001 },
+        { "kLSItemInfoIsPackage", 0x00000002 },
+        { "kLSItemInfoIsApplication", 0x00000004 },
+        { "kLSItemInfoIsContainer", 0x00000008 },
+        { "kLSItemInfoIsAliasFile", 0x00000010 },
+        { "kLSItemInfoIsSymlink", 0x00000020 },
+        { "kLSItemInfoIsInvisible", 0x00000040 },
+        { "kLSItemInfoIsNativeApp", 0x00000080 },
+        { "kLSItemInfoIsClassicApp", 0x00000100 },
+        { "kLSItemInfoAppPrefersNative", 0x00000200 },
+        { "kLSItemInfoAppPrefersClassic", 0x00000400 },
+        { "kLSItemInfoAppIsScriptable", 0x00000800 },
+        { "kLSItemInfoIsVolume", 0x00001000 },
+        { "kLSItemInfoExtensionIsHidden", 0x00100000 }
+    };
+    int i, count = sizeof(names)/sizeof(names[0]);
+
+    for(i = 0; i < count; ++i) {
+        if (flags & names[i].bits) {
+            DEBUGMSGTL(("swinst:arch:darwin:flags", "\t%s\n",
+                       names[i].name));
+        }
+    }
+}
+
+static int
+_add_applications_in_dir(netsnmp_container *container, const char* path)
+{
+    netsnmp_container  *files;
+    netsnmp_iterator   *it;
+    const char         *file;
+    netsnmp_swinst_entry *entry = NULL;
+    struct stat	        stat_buf;
+    size_t              date_len;
+    u_char             *date_buf;
+    int                 rc = 0;
+
+    CFStringRef         currentPath = NULL;
+    CFURLRef            currentURL = NULL;
+    LSItemInfoRecord    itemInfoRecord;
+    CFStringRef         prodName = NULL;
+    CFStringRef         version = NULL;
+    
+    DEBUGMSGTL(("swinst:arch:darwin", " adding files from %s\n", path));
+    files = netsnmp_directory_container_read(NULL, path, 0);
+    if (NULL == files) {
+        snmp_log(LOG_ERR, "swinst: could not read directory %s\n", path);
+        return -1;
+    }
+
+    it = CONTAINER_ITERATOR(files);
+    if (NULL == it) {
+        snmp_log(LOG_ERR, "could not get iterator\n");
+        netsnmp_directory_container_free(files);
+        return -1;
+    }
+    for (file = ITERATOR_FIRST(it);
+         file;
+         file = ITERATOR_NEXT(it),
+             CFRelease(currentPath),
+             CFRelease(currentURL)) {
+
+        int                 rc2 = 0;
+        
+        currentPath =
+            CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, file,
+                                            kCFStringEncodingUTF8,
+                                            kCFAllocatorNull);
+        currentURL =
+            CFURLCreateWithFileSystemPath(kCFAllocatorDefault, currentPath,
+                                          kCFURLPOSIXPathStyle, true); 
+        LSCopyItemInfoForURL(currentURL,
+                             kLSRequestBasicFlagsOnly|kLSRequestAppTypeFlags,
+                             &itemInfoRecord); 
+        if((0 == itemInfoRecord.flags) ||
+           (kLSItemInfoIsPlainFile == itemInfoRecord.flags) ||
+           (itemInfoRecord.flags & kLSItemInfoIsInvisible) ||
+           (itemInfoRecord.flags & kLSItemInfoIsAliasFile)) {
+            continue;
+        }
+        /** recurse on non-application containers (i.e. directory) */
+        if ((itemInfoRecord.flags & kLSItemInfoIsContainer) &&
+            (!(itemInfoRecord.flags & kLSItemInfoIsApplication))) {
+            netsnmp_directory_container_read(files, file, 0);
+            continue;
+       }
+
+        /** skip any other non-application files */
+        if (!(itemInfoRecord.flags & kLSItemInfoIsApplication)) {
+            continue;
+       }
+
+        if ((itemInfoRecord.flags & kLSItemInfoIsPackage) ||           
+            (itemInfoRecord.flags & kLSItemInfoIsContainer)) {
+            rc2 = _check_bundled_app(currentURL, &prodName, &version, file);
+        } 
+        else if ((itemInfoRecord.flags & kLSItemInfoIsClassicApp) ||
+                 (itemInfoRecord.flags & kLSItemInfoIsPlainFile)) {
+            rc2 = _check_classic_app(currentURL, &prodName, &version, file);
+        } else {
+            snmp_log(LOG_ERR,"swinst shouldn't get here: %s\n", file);
+            _dump_flags(itemInfoRecord.flags);
+            continue;
+        }
+        if (rc2) { /* not an app. if directory, recurse; else continue */
+            _dump_flags(itemInfoRecord.flags);
+            if (1 == rc2)
+                netsnmp_directory_container_read(files, file, 0);
+            continue;
+        }
+        
+        /*
+         * allocate entry
+         */
+        entry = netsnmp_swinst_entry_create(++_index);
+        if (NULL == entry) {
+            snmp_log(LOG_ERR, "error creating swinst entry\n");
+            rc = -1;
+            SNMP_CFRelease(prodName);
+            SNMP_CFRelease(version);
+            break;
+        }
+
+        entry->swName_len =
+            snprintf(entry->swName, sizeof(entry->swName),
+                     "%s %s", CFStringGetCStringPtr(prodName,0),
+                     CFStringGetCStringPtr(version,0));
+
+        DEBUGMSGTL(("swinst:arch:darwin", "\t%s %s\n", file, entry->swName));
+
+        /** get the last mod date */
+        if(stat(file, &stat_buf) != -1) {
+            date_buf = date_n_time(&stat_buf.st_mtime, &date_len);
+            entry->swDate_len = date_len;
+            memcpy(entry->swDate, date_buf, entry->swDate_len);
+        }
+        
+        CONTAINER_INSERT(container, entry);
+        entry = NULL;
+        SNMP_CFRelease(prodName);
+        SNMP_CFRelease(version);
+    }
+    ITERATOR_RELEASE(it);
+    netsnmp_directory_container_free(files);
+
+    return rc;
+}
+
+int
+_check_bundled_app(CFURLRef currentURL, CFStringRef *prodName,
+                   CFStringRef *version, const char* file)
+{
+    CFBundleRef         theBundle = NULL;
+    CFDictionaryRef     infoDict = NULL;
+            
+    if ((NULL == prodName) || (NULL == version))
+       return -1;
+
+    theBundle = CFBundleCreate (kCFAllocatorDefault, currentURL);
+    if(theBundle == NULL)
+        return -1; /* not a bundle */
+
+    infoDict = CFBundleGetInfoDictionary(theBundle);
+    if(0 == CFDictionaryGetCount(infoDict)) {
+        SNMP_CFRelease(theBundle);
+        return 1; /* directory */
+    }
+
+    *prodName = (CFStringRef)
+        CFDictionaryGetValue (infoDict, CFSTR("CFBundleName"));
+    if (NULL == *prodName) {
+        *prodName = (CFStringRef)
+            CFDictionaryGetValue (infoDict, CFSTR("CFBundleDisplayName"));
+        if (NULL == *prodName) {
+            *prodName = (CFStringRef) CFDictionaryGetValue (infoDict,
+                                      CFSTR("CFBundleExecutable"));
+        }
+    }
+    if(NULL == *prodName) {
+        DEBUGMSGTL(("swinst:arch:darwin", "\tmissing name: %s\n",file));
+        /*CFShow(infoDict);*/
+        SNMP_CFRelease(theBundle);
+        return -1;
+    }
+
+    *version = (CFStringRef)
+        CFDictionaryGetValue (infoDict, CFSTR("CFBundleShortVersionString"));
+    if(NULL == *version) {
+        *version = (CFStringRef)
+            CFDictionaryGetValue (infoDict, CFSTR("CFBundleVersion"));
+        if (*version == NULL) 
+            *version = (CFStringRef) CFDictionaryGetValue (infoDict,
+                                      CFSTR("CFBundleGetInfoString"));
+    }
+    if(NULL == *version) {
+        DEBUGMSGTL(("swinst:arch:darwin", "\tmissing version: %s\n",file));
+        /*CFShow(infoDict);*/
+        SNMP_CFRelease(theBundle);
+        return -1;
+    }
+    
+    if(theBundle != NULL) {
+        CFRetain(*prodName);
+        CFRetain(*version);
+        SNMP_CFRelease(theBundle);
+    }
+    return 0;
+}
+
+static int
+_check_classic_app(CFURLRef currentURL, CFStringRef *prodName,
+                   CFStringRef *version, const char* file)
+{
+    /*
+     * get info for classic or single-file apps
+     */
+    FSRef theFSRef;
+    int theResFile;
+
+    if ((NULL == prodName) || (NULL == version))
+       return -1;
+
+    *prodName = CFURLCopyLastPathComponent(currentURL);
+    if (! CFURLGetFSRef(currentURL, &theFSRef)) {
+        DEBUGMSGTL(("swinst:arch:darwin", "GetFSRef failed: %s\n", file));
+        SNMP_CFRelease(*prodName);
+        return -1;
+    }
+    theResFile = FSOpenResFile(&theFSRef, fsRdPerm);
+    if (ResError() != noErr) {
+        DEBUGMSGTL(("swinst:arch:darwin", "FSOpenResFile failed: %s\n", file));
+        SNMP_CFRelease(*prodName);
+        return -1;
+    }
+    VersRecHndl versHandle = (VersRecHndl)Get1IndResource('vers', 1);
+    if (versHandle != NULL) {
+        *version = CFStringCreateWithPascalString(kCFAllocatorDefault,
+                       (**versHandle).shortVersion, kCFStringEncodingMacRoman);
+        if (*version == NULL) {
+            StringPtr longVersionPtr = (**versHandle).shortVersion;
+            longVersionPtr = (StringPtr)(((Ptr) longVersionPtr) +
+                              1 + ((unsigned char) *longVersionPtr));
+            *version = CFStringCreateWithPascalString(kCFAllocatorDefault,
+                          longVersionPtr,  kCFStringEncodingMacRoman);
+        }
+        ReleaseResource((Handle)versHandle);
+    }
+    CloseResFile(theResFile);
+    if(*version == NULL) {
+        DEBUGMSGTL(("swinst:arch:darwin",
+                    "\tmissing classic/file version: %s\n", file));
+        SNMP_CFRelease(*prodName);
+        return -1;
+    }
+
+    return 0;
+}

commit 3f6b4722853c5db8697c20b750faa204c266de9f

Darwin: Use CFURL resource properties instead of deprecated LaunchServices
LSCopyItemInfoForURL() and associated LSItemInfoRecord types have been
deprecated since OS X 10.11 in favor of CFURLCopyResourcePropertyForKey().
Additionally, Carbon Resource Manager APIs (FSOpenResFile, etc.) used for
classic single-file application resources were deprecated in OS X 10.8.

- Add configure checks for CFURLCopyResourcePropertyForKey,
  CFURLCopyResourcePropertiesForKeys, LSCopyItemInfoForURL, and
  FSOpenResFile.
- In agent/mibgroup/host/data_access/swinst_darwin.c, query file
  properties using CFURLCopyResourcePropertyForKey() when available.
- Guard legacy LaunchServices and Carbon Resource Manager code with
  HAVE_LSCOPYITEMINFOFORURL and HAVE_FSOPENRESFILE respectively.

Fixes: 4b47ecf4283e ("Apple patches")

agent/mibgroup/host/data_access/swinst_darwin.c
@@ -26,7 +26,9 @@
 #undef __APPLE_API_EVOLVING 
 
 #include <CoreFoundation/CoreFoundation.h>
+#if defined(HAVE_FSOPENRESFILE) && !defined(HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY)
 #include <ApplicationServices/ApplicationServices.h>
+#endif
 
 netsnmp_feature_require(container_directory);
 netsnmp_feature_require(date_n_time);
@@ -37,8 +39,10 @@ static int _add_applications_in_dir(netsnmp_container *, const char* path);
 static int32_t _index;
 static int _check_bundled_app(CFURLRef currentURL, CFStringRef *name,
                               CFStringRef *info, const char* path);
+#if defined(HAVE_FSOPENRESFILE) && !defined(HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY)
 static int _check_classic_app(CFURLRef currentURL, CFStringRef *name,
                               CFStringRef *info, const char* path);
+#endif
 static netsnmp_container *dirs = NULL;
 
 /* ---------------------------------------------------------------------
@@ -132,6 +136,22 @@ netsnmp_swinst_arch_load( netsnmp_container *container, u_int flags )
     return 0;
 }
 
+#ifdef HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY
+static Boolean
+_cfurl_get_bool_prop(CFURLRef url, CFStringRef key)
+{
+    CFBooleanRef val = NULL;
+    Boolean res = false;
+
+    if (CFURLCopyResourcePropertyForKey(url, key, &val, NULL) && val) {
+        res = CFBooleanGetValue(val);
+        CFRelease(val);
+    }
+    return res;
+}
+#endif
+
+#if defined(HAVE_LSCOPYITEMINFOFORURL) && !defined(HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY)
 static void
 _dump_flags(u_long flags)
 {
@@ -163,6 +183,7 @@ _dump_flags(u_long flags)
         }
     }
 }
+#endif
 
 static int
 _add_applications_in_dir(netsnmp_container *container, const char* path)
@@ -178,7 +199,9 @@ _add_applications_in_dir(netsnmp_container *container, const char* path)
 
     CFStringRef         currentPath = NULL;
     CFURLRef            currentURL = NULL;
+#if defined(HAVE_LSCOPYITEMINFOFORURL) && !defined(HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY)
     LSItemInfoRecord    itemInfoRecord;
+#endif
     CFStringRef         prodName = NULL;
     CFStringRef         version = NULL;
     CFStringEncoding    sys_encoding = CFStringGetSystemEncoding();
@@ -205,6 +228,9 @@ _add_applications_in_dir(netsnmp_container *container, const char* path)
              CFRelease(currentURL)) {
 
         int                 rc2 = 0;
+#ifdef HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY
+        Boolean             isPlainFile, isDir, isApp, isPkg, isHidden, isAlias;
+#endif
         
         currentPath =
             CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, file,
@@ -213,6 +239,39 @@ _add_applications_in_dir(netsnmp_container *container, const char* path)
         currentURL =
             CFURLCreateWithFileSystemPath(kCFAllocatorDefault, currentPath,
                                           kCFURLPOSIXPathStyle, true); 
+#ifdef HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY
+        isPlainFile = _cfurl_get_bool_prop(currentURL, kCFURLIsRegularFileKey);
+        isDir       = _cfurl_get_bool_prop(currentURL, kCFURLIsDirectoryKey);
+        isApp       = _cfurl_get_bool_prop(currentURL, kCFURLIsApplicationKey);
+        isPkg       = _cfurl_get_bool_prop(currentURL, kCFURLIsPackageKey);
+        isHidden    = _cfurl_get_bool_prop(currentURL, kCFURLIsHiddenKey);
+        isAlias     = _cfurl_get_bool_prop(currentURL, kCFURLIsAliasFileKey);
+
+        if (isHidden || isAlias || (isPlainFile && !isApp)) {
+            continue;
+        }
+        /** recurse on non-application containers (i.e. directory) */
+        if (isDir && !isApp) {
+            netsnmp_directory_container_read(files, file, 0);
+            continue;
+        }
+
+        /** skip any other non-application files */
+        if (!isApp) {
+            continue;
+        }
+
+        if (isPkg || isDir) {
+            rc2 = _check_bundled_app(currentURL, &prodName, &version, file);
+        } else {
+            continue;
+        }
+        if (rc2) { /* not an app. if directory, recurse; else continue */
+            if (1 == rc2)
+                netsnmp_directory_container_read(files, file, 0);
+            continue;
+        }
+#elif defined(HAVE_LSCOPYITEMINFOFORURL)
         LSCopyItemInfoForURL(currentURL,
                              kLSRequestBasicFlagsOnly|kLSRequestAppTypeFlags,
                              &itemInfoRecord); 
@@ -227,21 +286,24 @@ _add_applications_in_dir(netsnmp_container *container, const char* path)
             (!(itemInfoRecord.flags & kLSItemInfoIsApplication))) {
             netsnmp_directory_container_read(files, file, 0);
             continue;
-       }
+        }
 
         /** skip any other non-application files */
         if (!(itemInfoRecord.flags & kLSItemInfoIsApplication)) {
             continue;
-       }
+        }
 
         if ((itemInfoRecord.flags & kLSItemInfoIsPackage) ||           
             (itemInfoRecord.flags & kLSItemInfoIsContainer)) {
             rc2 = _check_bundled_app(currentURL, &prodName, &version, file);
         } 
+#ifdef HAVE_FSOPENRESFILE
         else if ((itemInfoRecord.flags & kLSItemInfoIsClassicApp) ||
                  (itemInfoRecord.flags & kLSItemInfoIsPlainFile)) {
             rc2 = _check_classic_app(currentURL, &prodName, &version, file);
-        } else {
+        }
+#endif
+        else {
             snmp_log(LOG_ERR,"swinst shouldn't get here: %s\n", file);
             _dump_flags(itemInfoRecord.flags);
             continue;
@@ -252,6 +314,9 @@ _add_applications_in_dir(netsnmp_container *container, const char* path)
                 netsnmp_directory_container_read(files, file, 0);
             continue;
         }
+#else
+        continue;
+#endif
         
         /*
          * allocate entry
@@ -364,6 +429,7 @@ _check_bundled_app(CFURLRef currentURL, CFStringRef *prodName,
     return 0;
 }
 
+#if defined(HAVE_FSOPENRESFILE) && !defined(HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY)
 static int
 _check_classic_app(CFURLRef currentURL, CFStringRef *prodName,
                    CFStringRef *version, const char* file)
@@ -414,3 +480,4 @@ _check_classic_app(CFURLRef currentURL, CFStringRef *prodName,
 
     return 0;
 }
+#endif

configure
@@ -34642,6 +34642,68 @@ fi
 
     LIBS="$netsnmp_save_LIBS"
 fi
+ac_fn_c_check_func "$LINENO" "CFURLCopyResourcePropertyForKey" "ac_cv_func_CFURLCopyResourcePropertyForKey"
+if test "x$ac_cv_func_CFURLCopyResourcePropertyForKey" = xyes
+then :
+  printf "%s\n" "#define HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY 1" >>confdefs.h
+
+fi
+ac_fn_c_check_func "$LINENO" "CFURLCopyResourcePropertiesForKeys" "ac_cv_func_CFURLCopyResourcePropertiesForKeys"
+if test "x$ac_cv_func_CFURLCopyResourcePropertiesForKeys" = xyes
+then :
+  printf "%s\n" "#define HAVE_CFURLCOPYRESOURCEPROPERTIESFORKEYS 1" >>confdefs.h
+
+fi
+
+if test "x$ac_cv_func_CFURLCopyResourcePropertyForKey" != "xyes"; then
+    netsnmp_save_LIBS="$LIBS"
+    LIBS="-framework CoreFoundation $LIBS"
+    ac_fn_c_check_func "$LINENO" "CFURLCopyResourcePropertyForKey" "ac_cv_func_CFURLCopyResourcePropertyForKey"
+if test "x$ac_cv_func_CFURLCopyResourcePropertyForKey" = xyes
+then :
+  printf "%s\n" "#define HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY 1" >>confdefs.h
+
+fi
+ac_fn_c_check_func "$LINENO" "CFURLCopyResourcePropertiesForKeys" "ac_cv_func_CFURLCopyResourcePropertiesForKeys"
+if test "x$ac_cv_func_CFURLCopyResourcePropertiesForKeys" = xyes
+then :
+  printf "%s\n" "#define HAVE_CFURLCOPYRESOURCEPROPERTIESFORKEYS 1" >>confdefs.h
+
+fi
+
+    LIBS="$netsnmp_save_LIBS"
+fi
+ac_fn_c_check_func "$LINENO" "LSCopyItemInfoForURL" "ac_cv_func_LSCopyItemInfoForURL"
+if test "x$ac_cv_func_LSCopyItemInfoForURL" = xyes
+then :
+  printf "%s\n" "#define HAVE_LSCOPYITEMINFOFORURL 1" >>confdefs.h
+
+fi
+ac_fn_c_check_func "$LINENO" "FSOpenResFile" "ac_cv_func_FSOpenResFile"
+if test "x$ac_cv_func_FSOpenResFile" = xyes
+then :
+  printf "%s\n" "#define HAVE_FSOPENRESFILE 1" >>confdefs.h
+
+fi
+
+if test "x$ac_cv_func_LSCopyItemInfoForURL" != "xyes"; then
+    netsnmp_save_LIBS="$LIBS"
+    LIBS="-framework CoreServices $LIBS"
+    ac_fn_c_check_func "$LINENO" "LSCopyItemInfoForURL" "ac_cv_func_LSCopyItemInfoForURL"
+if test "x$ac_cv_func_LSCopyItemInfoForURL" = xyes
+then :
+  printf "%s\n" "#define HAVE_LSCOPYITEMINFOFORURL 1" >>confdefs.h
+
+fi
+ac_fn_c_check_func "$LINENO" "FSOpenResFile" "ac_cv_func_FSOpenResFile"
+if test "x$ac_cv_func_FSOpenResFile" = xyes
+then :
+  printf "%s\n" "#define HAVE_FSOPENRESFILE 1" >>confdefs.h
+
+fi
+
+    LIBS="$netsnmp_save_LIBS"
+fi
 
 { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking for FD_CLOEXEC" >&5
 printf %s "checking for FD_CLOEXEC... " >&6; }

configure.d/config_os_functions
@@ -222,6 +222,20 @@ if test "x$ac_cv_func_IOMainPort" != "xyes"; then
     AC_CHECK_FUNCS([IOMainPort])
     LIBS="$netsnmp_save_LIBS"
 fi
+AC_CHECK_FUNCS([CFURLCopyResourcePropertyForKey CFURLCopyResourcePropertiesForKeys])
+if test "x$ac_cv_func_CFURLCopyResourcePropertyForKey" != "xyes"; then
+    netsnmp_save_LIBS="$LIBS"
+    LIBS="-framework CoreFoundation $LIBS"
+    AC_CHECK_FUNCS([CFURLCopyResourcePropertyForKey CFURLCopyResourcePropertiesForKeys])
+    LIBS="$netsnmp_save_LIBS"
+fi
+AC_CHECK_FUNCS([LSCopyItemInfoForURL FSOpenResFile])
+if test "x$ac_cv_func_LSCopyItemInfoForURL" != "xyes"; then
+    netsnmp_save_LIBS="$LIBS"
+    LIBS="-framework CoreServices $LIBS"
+    AC_CHECK_FUNCS([LSCopyItemInfoForURL FSOpenResFile])
+    LIBS="$netsnmp_save_LIBS"
+fi
 
 AC_MSG_CHECKING([for FD_CLOEXEC])
 AC_COMPILE_IFELSE([AC_LANG_PROGRAM(

include/net-snmp/net-snmp-config.h.in
@@ -111,6 +111,13 @@
 /* Define to 1 if you have the 'asprintf' function. */
 #undef HAVE_ASPRINTF
 
+/* Define to 1 if you have the 'CFURLCopyResourcePropertiesForKeys' function.
+   */
+#undef HAVE_CFURLCOPYRESOURCEPROPERTIESFORKEYS
+
+/* Define to 1 if you have the 'CFURLCopyResourcePropertyForKey' function. */
+#undef HAVE_CFURLCOPYRESOURCEPROPERTYFORKEY
+
 /* Define to 1 if you have the 'cgetnext' function. */
 #undef HAVE_CGETNEXT
 
@@ -245,6 +252,9 @@
 
 /* Define to 1 if you have the 'forkall' function. */
 #undef HAVE_FORKALL
+
+/* Define to 1 if you have the 'FSOpenResFile' function. */
+#undef HAVE_FSOPENRESFILE
 
 /* Define to 1 if you have the <fstab.h> header file. */
 #undef HAVE_FSTAB_H
@@ -534,6 +544,9 @@
 
 /* Define to 1 if you have the 'lrand48' function. */
 #undef HAVE_LRAND48
+
+/* Define to 1 if you have the 'LSCopyItemInfoForURL' function. */
+#undef HAVE_LSCOPYITEMINFOFORURL
 
 /* Define to 1 if you have the 'lseek64' function. */
 #undef HAVE_LSEEK64