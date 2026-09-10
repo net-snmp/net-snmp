net-snmp/net-snmp

commit 10b972733d29e7ac8a5e2f975df9997ae1b9084c

Translate GetNext (and GetBulk) requests into simple Get requests, when passing them from the table_data handler to table_dataset.   This is analogous to how the iterate handler works, and allows lower level handlers to override the basic behaviour (without having to duplicate all of the "find next row" code every time).
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@7786 06827809-a52a-0410-b366-d66718629ded

agent/helpers/table_data.c
@@ -296,6 +296,7 @@ netsnmp_table_data_helper_handler(netsnmp_mib_handler *handler,
     netsnmp_table_registration_info *table_reg_info =
         netsnmp_find_table_registration_info(reginfo);
     int             result, regresult;
+    int             oldmode;
 
     for (request = requests; request; request = request->next) {
         if (request->processed)
@@ -396,6 +397,14 @@ netsnmp_table_data_helper_handler(netsnmp_mib_handler *handler,
                                               netsnmp_create_data_list
                                               (TABLE_DATA_NAME, row,
                                                NULL));
+                /*
+                 * Set the name appropriately, so we can pass this
+                 *  request on as a simple GET request
+                 */
+                netsnmp_table_data_build_result(reginfo, reqinfo, request,
+                                                row,
+                                                table_info->colnum,
+                                                ASN_NULL, NULL, 0);
             } else {            /* no decent result found.  Give up. It's beyond us. */
                 request->processed = 1;
             }
@@ -470,9 +479,41 @@ netsnmp_table_data_helper_handler(netsnmp_mib_handler *handler,
         }
     }
 
-    if (valid_request)
-        return netsnmp_call_next_handler(handler, reginfo, reqinfo,
+    if (valid_request) {
+        /*
+         * If this is a GetNext or GetBulk request, then we've identified
+         *  the row that ought to include the appropriate next instance.
+         *  Convert the request into a Get request, so that the lower-level
+         *  handlers don't need to worry about skipping on....
+         */
+        oldmode = reqinfo->mode;
+        if (reqinfo->mode == MODE_GETNEXT || reqinfo->mode == MODE_GETBULK) {
+            reqinfo->mode = MODE_GET;
+        }
+        result = netsnmp_call_next_handler(handler, reginfo, reqinfo,
                                          requests);
+        if (oldmode == MODE_GETNEXT || oldmode == MODE_GETBULK) {       /* XXX */
+            for (request = requests; request; request = request->next) {
+                /*
+                 *  ... but if the lower-level handlers aren't dealing with
+                 *  skipping on to the next instance, then we must handle
+                 *  this situation here.
+                 *    Mark 'holes' in the table as needing to be retried.
+                 *
+                 *    This approach is less efficient than handling such
+                 *  holes directly in the table_dataset handler, but allows
+                 *  user-provided handlers to override the dataset handler
+                 *  if this proves necessary.
+                 */
+                if (requests->requestvb->type == ASN_NULL ||
+                    requests->requestvb->type == SNMP_NOSUCHINSTANCE) {
+                    requests->requestvb->type = ASN_PRIV_RETRY;
+                }
+            }
+            reqinfo->mode = oldmode;
+        }
+        return result;
+    }
     else
         return SNMP_ERR_NOERROR;
 }

agent/helpers/table_dataset.c
@@ -481,54 +481,6 @@ netsnmp_table_data_set_helper_handler(netsnmp_mib_handler *handler,
         case MODE_GET:
         case MODE_GETNEXT:
         case MODE_GETBULK:     /* XXXWWW */
-            if (!data) {
-                netsnmp_table_row *newrow;
-                netsnmp_table_data_set_storage *newdata = NULL;
-                /*
-                 * unsigned int column = table_info->colnum; 
-                 */
-
-                while (table_info->colnum <=
-                       table_info->reg_info->max_column) {
-                    if (table_info->colnum != table_info->colnum) {
-                        /*
-                         * start with a new row 
-                         */
-                        row = datatable->table->first_row;
-                    }
-                    for (newrow = row; newrow; newrow = newrow->next) {
-                        newdata =
-                            (netsnmp_table_data_set_storage *) newrow->
-                            data;
-                        if (newdata)
-                            newdata =
-                                netsnmp_table_data_set_find_column(newdata,
-                                                                   table_info->
-                                                                   colnum);
-                        if (newdata) {
-                            /*
-                             * this is it 
-                             */
-                            data = newdata;
-                            row = newrow;
-                            goto done;
-                        }
-                    }
-                    table_info->colnum++;
-                    row = datatable->table->first_row;
-                    /*
-                     * ???
-                     *
-                     * We probably need to 'rewind' the
-                     * request->parent_data list entry to
-                     * refer back to the first row as well.
-                     * 
-                     * How can this best be done?
-                     * ???
-                     */
-                }
-            }
-          done:
             if (data && data->data.voidp)
                 netsnmp_table_data_build_result(reginfo, reqinfo, request,
                                                 row,
@@ -536,29 +488,6 @@ netsnmp_table_data_set_helper_handler(netsnmp_mib_handler *handler,
                                                 data->type,
                                                 data->data.voidp,
                                                 data->data_len);
-            else {
-                /*
-                 * deal with holes by going to the next data set
-                 * in the row or possibly onward to new columns 
-                 */
-                if (reqinfo->mode == MODE_GETNEXT || reqinfo->mode == MODE_GETBULK) {   /* XXXWWW */
-                    if (row)
-                        row = row->next;
-                    if (row) {
-                        data =
-                            (netsnmp_table_data_set_storage *) row->data;
-                        goto topsearch;
-                    }
-                    if (table_info->colnum <=
-                        table_info->reg_info->max_column) {
-                        table_info->colnum++;
-                        row = datatable->table->first_row;
-                        data =
-                            (netsnmp_table_data_set_storage *) row->data;
-                        goto topsearch;
-                    }
-                }
-            }
             break;
 
         case MODE_SET_RESERVE1:

commit 14cc6bd93cd0ad9b0ba2a77b5d978968489825cc

snmpd: Validate OID length before building GETNEXT result
When handling GETNEXT or GETBULK requests for table_data tables,
netsnmp_table_data_build_result() constructs result OIDs by appending
the row index to the table root OID into a fixed MAX_OID_LEN buffer. If
the combined length exceeds MAX_OID_LEN, memory is written out of
bounds.

Add netsnmp_table_data_build_oid_fits() to verify that rootoid_len <=
MAX_OID_LEN - 2 and index_oid_len <= MAX_OID_LEN - rootoid_len - 2. In
netsnmp_table_data_helper_handler(), skip rows whose constructed OIDs
would not fit. In netsnmp_table_data_build_result(), return
SNMPERR_TOO_LONG if the OID does not fit or SNMPERR_GENERR if the row is
NULL.

Fixes: 10b972733d29 ("Translate GetNext (and GetBulk) requests into simple Get requests, when passing them from the table_data handler to table_dataset. This is analogous to how the iterate handler works, and allows lower level handlers to override the basic behaviour (without having to duplicate all of the \"find next row\" code every time).")

agent/helpers/table_data.c
@@ -489,6 +489,25 @@ netsnmp_unregister_table_data(netsnmp_handler_registration *reginfo)
  * data down to the lower handler(s).  It sets request->processed if
  * the request should not be handled.
  */
+static int
+netsnmp_table_data_build_oid_fits(netsnmp_handler_registration *reginfo,
+                                  netsnmp_table_row *row)
+{
+    return reginfo->rootoid_len <= MAX_OID_LEN - 2 &&
+        row->index_oid_len <= MAX_OID_LEN - reginfo->rootoid_len - 2;
+}
+
+static netsnmp_table_row *
+netsnmp_table_data_get_next_fitting_row(
+    netsnmp_handler_registration *reginfo, netsnmp_table_row *row)
+{
+    for (; row; row = row->next) {
+        if (netsnmp_table_data_build_oid_fits(reginfo, row))
+            return row;
+    }
+    return NULL;
+}
+
 int
 netsnmp_table_data_helper_handler(netsnmp_mib_handler *handler,
                                   netsnmp_handler_registration *reginfo,
@@ -602,11 +621,12 @@ netsnmp_table_data_helper_handler(netsnmp_mib_handler *handler,
                     }
                 }
             }
+            row = netsnmp_table_data_get_next_fitting_row(reginfo, row);
             if (!row) {
                 table_info->colnum++;
-                if (table_info->colnum <= table_reg_info->max_column) {
-                    row = table->first_row;
-                }
+                if (table_info->colnum <= table_reg_info->max_column)
+                    row = netsnmp_table_data_get_next_fitting_row(
+                        reginfo, table->first_row);
             }
             if (row) {
                 valid_request = 1;
@@ -831,8 +851,11 @@ netsnmp_table_data_build_result(netsnmp_handler_registration *reginfo,
 {
     oid             build_space[MAX_OID_LEN];
 
-    if (!reginfo || !reqinfo || !request)
+    if (!reginfo || !reqinfo || !request || !row)
         return SNMPERR_GENERR;
+
+    if (!netsnmp_table_data_build_oid_fits(reginfo, row))
+        return SNMPERR_TOO_LONG;
 
     if (reqinfo->mode == MODE_GETNEXT || reqinfo->mode == MODE_GETBULK) {
         /*