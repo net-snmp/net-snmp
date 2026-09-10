net-snmp/net-snmp

commit 57031cb248e6193c3c3fd6f10f921244a4be7732

Convert v1 traps to v2 format before passing it to the trap handler (plus merging the other 5.1.x patches into the main development tree)
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@9275 06827809-a52a-0410-b366-d66718629ded

apps/snmptrapd_handlers.c
@@ -74,7 +74,7 @@ snmptrapd_parse_traphandle(const char *token, char *line)
 {
     char            buf[STRINGMAX];
     oid             obuf[MAX_OID_LEN];
-    int             olen = MAX_OID_LEN;
+    size_t          olen = MAX_OID_LEN;
     char           *cptr;
     netsnmp_trapd_handler *traph;
 
@@ -110,7 +110,7 @@ parse_forward(const char *token, char *line)
 {
     char            buf[STRINGMAX];
     oid             obuf[MAX_OID_LEN];
-    int             olen = MAX_OID_LEN;
+    size_t          olen = MAX_OID_LEN;
     char           *cptr;
     netsnmp_trapd_handler *traph;
 
@@ -124,7 +124,7 @@ parse_forward(const char *token, char *line)
         if (!read_objid(buf, obuf, &olen)) {
             char            buf1[STRINGMAX];
             snprintf(buf1,  sizeof(buf1),
-                    "Bad trap OID in traphandle directive: %s", buf);
+                    "Bad trap OID in forward directive: %s", buf);
             buf1[ sizeof(buf1)-1 ] = 0;
             config_perror(buf1);
             return;
@@ -592,7 +592,7 @@ int   print_handler(   netsnmp_pdu           *pdu,
 	    }
 	} else {
             if (print_format2) {
-                DEBUGMSGTL(( "snmptrapd", "print_format v1 = '%s'\n", print_format2));
+                DEBUGMSGTL(( "snmptrapd", "print_format v2 = '%s'\n", print_format2));
                 trunc = !realloc_format_trap(&rbuf, &r_len, &o_len, 1,
                                              print_format2, pdu, transport);
 	    } else {
@@ -758,7 +758,6 @@ do_external(char *cmd, struct hostent *host,
     snmp_set_quick_print(oldquick);
 }
 
-
 #define EXECUTE_FORMAT	"%b\n%B\n%V\n%v\n"
 
 int   command_handler( netsnmp_pdu           *pdu,
@@ -772,6 +771,11 @@ int   command_handler( netsnmp_pdu           *pdu,
     DEBUGMSGTL(( "snmptrapd", "command_handler\n"));
     DEBUGMSGTL(( "snmptrapd", "token = '%s'\n", handler->token));
     if (handler && handler->token && *handler->token) {
+	netsnmp_pdu    *v2_pdu = NULL;
+	if (pdu->command == SNMP_MSG_TRAP)
+	    v2_pdu = convert_v1pdu_to_v2(pdu);
+	else
+	    v2_pdu = pdu;
         oldquick = snmp_get_quick_print();
         snmp_set_quick_print(1);
 
@@ -789,14 +793,14 @@ int   command_handler( netsnmp_pdu           *pdu,
          */
         if (handler && handler->format && *handler->format) {
             DEBUGMSGTL(( "snmptrapd", "format = '%s'\n", handler->format));
-            !realloc_format_trap(&rbuf, &r_len, &o_len, 1,
+            realloc_format_trap(&rbuf, &r_len, &o_len, 1,
                                              handler->format,
-                                             pdu, transport);
+                                             v2_pdu, transport);
         } else {
             DEBUGMSGTL(( "snmptrapd", "execute format\n"));
-            !realloc_format_trap(&rbuf, &r_len, &o_len, 1,
+            realloc_format_trap(&rbuf, &r_len, &o_len, 1,
                                              EXECUTE_FORMAT,
-                                             pdu, transport);
+                                             v2_pdu, transport);
 	}
 
         /*
@@ -804,6 +808,8 @@ int   command_handler( netsnmp_pdu           *pdu,
          */
         run_exec_command(handler->token, rbuf, NULL, 0);   /* Not interested in output */
         snmp_set_quick_print(oldquick);
+	if (pdu->command == SNMP_MSG_TRAP)
+	    snmp_free_pdu(v2_pdu);
     }
     return NETSNMPTRAPD_HANDLER_OK;
 }
@@ -842,6 +848,7 @@ int   forward_handler( netsnmp_pdu           *pdu,
 
     snmp_sess_init( &session );
     session.peername = handler->token;
+    session.remote_port = SNMP_TRAP_PORT;
     session.version  = pdu->version;
     ss = snmp_open( &session );
 

commit 58b3f9c983026b962d648d1225f84b788c524ade

snmptrapd: Check convert_v1pdu_to_v2 return value for NULL
When converting SNMPv1 traps to SNMPv2 format, convert_v1pdu_to_v2()
returns NULL if the incoming PDU is malformed (for example, if it has an
invalid trap type or enterprise length). Callers in command_handler(),
log_notification(), and perl_trapd_handler() dereferenced the returned
pointer without checking for NULL, leading to crashes. Additionally,
snmp_input() did not properly validate enterprise OID lengths and
standard trap types.

Fix these issues by:
- Checking the return value of convert_v1pdu_to_v2() for NULL in
  command_handler() and returning NETSNMPTRAPD_HANDLER_FAIL.
- Moving the convert_v1pdu_to_v2() invocation to the start of
  log_notification() and returning early if conversion returns NULL.
- Checking for NULL in perl_trapd_handler() and returning
  NETSNMPTRAPD_HANDLER_FAIL after cleaning up Perl temporaries.
- Validating enterprise OID lengths against MAX_OID_LEN - 2 and standard
  trap types against SNMP_TRAP_COLDSTART through SNMP_TRAP_EGPNEIGHBORLOSS
  in snmp_input().
- Adding regression test T059trapdtraphandle_noauth_simple to verify
  that malformed v1 traps are dropped without invoking the trap handler.

Fixes: 57031cb248e6 ("Convert v1 traps to v2 format before passing it to the trap handler (plus merging the other 5.1.x patches into the main development tree)")

agent/mibgroup/notification-log-mib/notification_log.c
@@ -600,6 +600,12 @@ log_notification(netsnmp_pdu *pdu, netsnmp_transport *transport)
         return;
     }
 
+    if (pdu->command == SNMP_MSG_TRAP) {
+        pdu = convert_v1pdu_to_v2(orig_pdu);
+        if (!pdu)
+            return;
+    }
+
     DEBUGMSGTL(("notification_log", "logging something\n"));
     row = netsnmp_create_table_data_row();
 
@@ -658,8 +664,6 @@ log_notification(netsnmp_pdu *pdu, netsnmp_transport *transport)
     netsnmp_set_row_column(row, COLUMN_NLMLOGCONTEXTNAME, ASN_OCTET_STR,
                            pdu->contextName, pdu->contextNameLen);
 
-    if (pdu->command == SNMP_MSG_TRAP)
-	pdu = convert_v1pdu_to_v2(orig_pdu);
     for (vptr = pdu->variables; vptr; vptr = vptr->next_variable) {
         if (snmp_oid_compare(snmptrapoid, snmptrapoid_len,
                              vptr->name, vptr->name_length) == 0) {

apps/snmptrapd_handlers.c
@@ -861,6 +861,8 @@ int   command_handler( netsnmp_pdu           *pdu,
 	    v2_pdu = convert_v1pdu_to_v2(pdu);
 	else
 	    v2_pdu = pdu;
+        if (!v2_pdu)
+            return NETSNMPTRAPD_HANDLER_FAIL;
         oldquick = netsnmp_ds_get_boolean(NETSNMP_DS_LIBRARY_ID, 
                                           NETSNMP_DS_LIB_QUICK_PRINT);
         netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, 
@@ -1158,10 +1160,10 @@ snmp_input(int op, netsnmp_session *session,
             if (pdu->trap_type == SNMP_TRAP_ENTERPRISESPECIFIC) {
                 trapOidLen = pdu->enterprise_length;
                 /*
-                 * Drop packets that would trigger an out-of-bounds trapOid[]
-                 * access.
+                 * Drop packets that cannot be represented by the v2-style
+                 * trap OID passed to notification handlers.
                  */
-                if (trapOidLen < 1 || trapOidLen > OID_LENGTH(trapOid) - 2)
+                if (trapOidLen < 1 || trapOidLen > MAX_OID_LEN - 2)
                     return 1;
                 memcpy(trapOid, pdu->enterprise, sizeof(oid) * trapOidLen);
                 if (trapOid[trapOidLen - 1] != 0) {
@@ -1172,7 +1174,8 @@ snmp_input(int op, netsnmp_session *session,
                 memcpy(trapOid, stdTrapOidRoot, sizeof(stdTrapOidRoot));
                 trapOidLen = OID_LENGTH(stdTrapOidRoot);  /* 9 */
                 /* Drop packets with an invalid trap type. */
-                if (pdu->trap_type == LONG_MAX)
+                if (pdu->trap_type < SNMP_TRAP_COLDSTART ||
+                    pdu->trap_type > SNMP_TRAP_EGPNEIGHBORLOSS)
                     return 1;
                 trapOid[trapOidLen++] = pdu->trap_type+1;
             }
@@ -1299,4 +1302,3 @@ t        *     d) any other global handlers
     }
     return 0;
 }
-

perl/TrapReceiver/TrapReceiver.xs
@@ -125,6 +125,11 @@ int   perl_trapd_handler( netsnmp_pdu           *pdu,
     /* nuke v1 PDUs */
     if (pdu->command == SNMP_MSG_TRAP) {
         v2pdu = convert_v1pdu_to_v2(pdu);
+        if (!v2pdu) {
+            FREETMPS;
+            LEAVE;
+            return NETSNMPTRAPD_HANDLER_FAIL;
+        }
         pdu = v2pdu;
     }
 

testing/fulltests/default/T059trapdtraphandle_noauth_simple
@@ -1,0 +1,68 @@
+#!/bin/sh
+
+# Testing this script is possible by running e.g. the following commands:
+# srcdir=/z/software/net-snmp/trunk builddir=/z/software/net-snmp/trunk-mingw
+# cd $builddir/testing
+# TRAPD_FLAGS="-Dsnmptrapd -Drun:shell" $srcdir/testing/fulltests/support/simple_run $srcdir/testing/fulltests/default/T059trapdtraphandle_noauth_simple
+
+# "inline" trap handler
+if [ "x$1" = "xtraphandle" ]; then
+  cat - >>"$2"
+  exit 0
+fi
+
+. ../support/simple_eval_tools.sh
+
+TRAPHANDLE_LOGFILE=${SNMP_TMPDIR}/traphandle.log
+
+HEADER snmptrapd traphandle with authorization disabled
+
+SKIPIF NETSNMP_DISABLE_SNMPV1
+SKIPIFNOT USING_UTILITIES_EXECUTE_MODULE
+
+#
+# Begin test
+#
+
+TESTCOMMUNITY=testcommunity
+
+# Make the paths of arguments $0 and $1 absolute.
+NETSNMPDIR="`pwd`"
+NETSNMPDIR="`dirname ${NETSNMPDIR}`"
+NETSNMPDIR="`dirname ${NETSNMPDIR}`"
+NETSNMPDIR="`dirname ${NETSNMPDIR}`"
+if [ "`echo $1|cut -c1`" = "/" ]; then
+  traphandle_arg="$1"
+else
+  traphandle_arg="${NETSNMPDIR}/$1"
+fi
+
+CONFIGTRAPD [snmp] persistentDir $SNMP_TMP_PERSISTENTDIR
+CONFIGTRAPD [snmp] tempFilePattern /tmp/snmpd-tmp-XXXXXX
+CONFIGTRAPD disableAuthorization yes
+CONFIGTRAPD doNotLogTraps true
+if [ "x$OSTYPE" = "xmsys" ]; then
+    CONFIGTRAPD traphandle default $MSYS_SH -c "'" $traphandle_arg traphandle $TRAPHANDLE_LOGFILE "'"
+else
+    CONFIGTRAPD traphandle default $traphandle_arg traphandle $TRAPHANDLE_LOGFILE
+fi
+CONFIGTRAPD agentxsocket /dev/null
+
+STARTTRAPD
+
+## 1) reject malformed v1 traps without invoking the handler
+
+CAPTURE "snmptrap -Ir -d -v 1 -c $TESTCOMMUNITY $SNMP_TRANSPORT_SPEC:$SNMP_TEST_DEST$SNMP_SNMPTRAPD_PORT .1.3.6.1.4.1.8072 127.0.0.1 7 0 0 .1.3.6.1.2.1.1.4.0 s rejected_v1"
+DELAY
+CHECKFILECOUNT $TRAPHANDLE_LOGFILE 0 "rejected_v1"
+
+## 2) a valid v1 trap should still reach the handler
+
+CAPTURE "snmptrap -d -v 1 -c $TESTCOMMUNITY $SNMP_TRANSPORT_SPEC:$SNMP_TEST_DEST$SNMP_SNMPTRAPD_PORT .1.3.6.1.4.1.8072 127.0.0.1 0 0 0 .1.3.6.1.2.1.1.4.0 s handled_v1"
+DELAY
+CHECKORDIE "handled_v1" $TRAPHANDLE_LOGFILE
+
+## stop
+STOPTRAPD
+
+FINISHED