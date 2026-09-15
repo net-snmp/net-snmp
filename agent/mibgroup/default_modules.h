net-snmp/net-snmp

commit a6042e0f08e3b70cc0e3d810ba120d86758f27d8

snmpd: Exclude certain MIBs if the Netlink library is not available
On Linux a significant number of MIB implementations depends on the Netlink
library. Exclude these MIBs if the Netlink library is not available.

agent/mibgroup/default_modules.h
@@ -23,7 +23,9 @@ config_require(mibII/vacm_conf);
 
 #else /* !NETSNMP_MINI_AGENT == the full shabang */
 
+#if !defined(__linux__) || defined(HAVE_NETLINK_ROUTE_ADDR_H)
 config_require(mibII);
+#endif
 config_require(ucd_snmp);
 config_require(notification);
 config_require(notification-log-mib);

agent/mibgroup/host.h
@@ -17,7 +17,9 @@
 	config_require(host/hr_device);
 	config_require(host/hr_other);
 	config_require(host/hr_proc);
+#if !defined(__linux__) || defined(HAVE_NETLINK_ROUTE_ADDR_H)
 	config_require(host/hr_network);
+#endif
 	config_require(host/hr_print);
 	config_require(host/hr_disk);
 	config_require(host/hr_partition);

configure
@@ -27358,6 +27358,101 @@ new_with_out_mib_modules=`echo $with_out_mib_modules | $SED 's/,/ /g;'`
 # set up initial module list
 new_module_list="$new_with_mib_modules"
 
+case "x$PARTIALTARGETOS" in
+    Linux)
+	if "x$ac_cv_header_netlink_route_addr_h" != xyes; then
+new_with_out_mib_modules="$new_with_out_mib_modules
+etherlike-mib
+etherlike-mib/dot3StatsTable
+host/hr_network
+if-mib
+if-mib/data_access/interface
+if-mib/data_access/interface_ioctl
+if-mib/data_access/interface_private
+if-mib/data_access/interface_sysctl
+if-mib/ifTable
+if-mib/ifTable/ifTable
+if-mib/ifTable/ifTable_constants
+if-mib/ifTable/ifTable_data_access
+if-mib/ifTable/ifTable_defs
+if-mib/ifTable/ifTable_interface
+if-mib/ifXTable
+if-mib/ifXTable/ifXTable
+if-mib/ifXTable/ifXTable_constants
+if-mib/ifXTable/ifXTable_data_access
+if-mib/ifXTable/ifXTable_interface
+ip-forward-mib
+ip-mib
+ip-mib/data_access/arp
+ip-mib/data_access/defaultrouter
+ip-mib/data_access/defaultrouter_private
+ip-mib/data_access/ipaddress
+ip-mib/data_access/ipaddress_ioctl
+ip-mib/data_access/ipaddress_linux
+ip-mib/data_access/ipaddress_private
+ip-mib/data_access/ipv6scopezone
+ip-mib/data_access/ipv6scopezone_private
+ip-mib/data_access/scalars_common
+ip-mib/data_access/systemstats
+ip-mib/data_access/systemstats_private
+ip-mib/inetNetToMediaTable
+ip-mib/inetNetToMediaTable/inetNetToMediaTable
+ip-mib/inetNetToMediaTable/inetNetToMediaTable_constants
+ip-mib/inetNetToMediaTable/inetNetToMediaTable_data_access
+ip-mib/inetNetToMediaTable/inetNetToMediaTable_interface
+ip-mib/ipAddressPrefixTable
+ip-mib/ipAddressPrefixTable/ipAddressPrefixTable
+ip-mib/ipAddressPrefixTable/ipAddressPrefixTable_constants
+ip-mib/ipAddressPrefixTable/ipAddressPrefixTable_data_access
+ip-mib/ipAddressPrefixTable/ipAddressPrefixTable_interface
+ip-mib/ipAddressTable
+ip-mib/ipAddressTable/ipAddressTable
+ip-mib/ipAddressTable/ipAddressTable_constants
+ip-mib/ipAddressTable/ipAddressTable_data_access
+ip-mib/ipAddressTable/ipAddressTable_interface
+ip-mib/ipDefaultRouterTable
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_data_access
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_data_get
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_enums
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_interface
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_oids
+ip-mib/ipIfStatsTable
+ip-mib/ipIfStatsTable/ipIfStatsTable
+ip-mib/ipIfStatsTable/ipIfStatsTable_data_access
+ip-mib/ipIfStatsTable/ipIfStatsTable_data_get
+ip-mib/ipIfStatsTable/ipIfStatsTable_enums
+ip-mib/ipIfStatsTable/ipIfStatsTable_interface
+ip-mib/ipIfStatsTable/ipIfStatsTable_oids
+ip-mib/ipSystemStatsTable
+ip-mib/ipSystemStatsTable/ipSystemStatsTable
+ip-mib/ipSystemStatsTable/ipSystemStatsTable_constants
+ip-mib/ipSystemStatsTable/ipSystemStatsTable_data_access
+ip-mib/ipSystemStatsTable/ipSystemStatsTable_interface
+ip-mib/ip_scalars
+ip-mib/ipv4InterfaceTable
+ip-mib/ipv4InterfaceTable/ipv4InterfaceTable
+ip-mib/ipv4InterfaceTable/ipv4InterfaceTable_constants
+ip-mib/ipv4InterfaceTable/ipv4InterfaceTable_data_access
+ip-mib/ipv4InterfaceTable/ipv4InterfaceTable_interface
+ip-mib/ipv6InterfaceTable
+ip-mib/ipv6InterfaceTable/ipv6InterfaceTable
+ip-mib/ipv6InterfaceTable/ipv6InterfaceTable_constants
+ip-mib/ipv6InterfaceTable/ipv6InterfaceTable_data_access
+ip-mib/ipv6InterfaceTable/ipv6InterfaceTable_interface
+ip-mib/ipv6ScopeZoneIndexTable
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable_data_access
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable_enums
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable_interface
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable_oids
+mibII
+tunnel/tunnel
+	"
+	fi
+	;;
+esac
+
 if test "x$enable_agent" != "xno" ; then
   # If the agent should be built then use default_modules
   new_module_list="$new_module_list default_modules"

configure.d/config_modules_agent
@@ -44,6 +44,101 @@ new_with_out_mib_modules=`echo $with_out_mib_modules | $SED 's/,/ /g;'`
 # set up initial module list
 new_module_list="$new_with_mib_modules"
 
+case "x$PARTIALTARGETOS" in
+    Linux)
+	if "x$ac_cv_header_netlink_route_addr_h" != xyes; then
+new_with_out_mib_modules="$new_with_out_mib_modules
+etherlike-mib
+etherlike-mib/dot3StatsTable
+host/hr_network
+if-mib
+if-mib/data_access/interface
+if-mib/data_access/interface_ioctl
+if-mib/data_access/interface_private
+if-mib/data_access/interface_sysctl
+if-mib/ifTable
+if-mib/ifTable/ifTable
+if-mib/ifTable/ifTable_constants
+if-mib/ifTable/ifTable_data_access
+if-mib/ifTable/ifTable_defs
+if-mib/ifTable/ifTable_interface
+if-mib/ifXTable
+if-mib/ifXTable/ifXTable
+if-mib/ifXTable/ifXTable_constants
+if-mib/ifXTable/ifXTable_data_access
+if-mib/ifXTable/ifXTable_interface
+ip-forward-mib
+ip-mib
+ip-mib/data_access/arp
+ip-mib/data_access/defaultrouter
+ip-mib/data_access/defaultrouter_private
+ip-mib/data_access/ipaddress
+ip-mib/data_access/ipaddress_ioctl
+ip-mib/data_access/ipaddress_linux
+ip-mib/data_access/ipaddress_private
+ip-mib/data_access/ipv6scopezone
+ip-mib/data_access/ipv6scopezone_private
+ip-mib/data_access/scalars_common
+ip-mib/data_access/systemstats
+ip-mib/data_access/systemstats_private
+ip-mib/inetNetToMediaTable
+ip-mib/inetNetToMediaTable/inetNetToMediaTable
+ip-mib/inetNetToMediaTable/inetNetToMediaTable_constants
+ip-mib/inetNetToMediaTable/inetNetToMediaTable_data_access
+ip-mib/inetNetToMediaTable/inetNetToMediaTable_interface
+ip-mib/ipAddressPrefixTable
+ip-mib/ipAddressPrefixTable/ipAddressPrefixTable
+ip-mib/ipAddressPrefixTable/ipAddressPrefixTable_constants
+ip-mib/ipAddressPrefixTable/ipAddressPrefixTable_data_access
+ip-mib/ipAddressPrefixTable/ipAddressPrefixTable_interface
+ip-mib/ipAddressTable
+ip-mib/ipAddressTable/ipAddressTable
+ip-mib/ipAddressTable/ipAddressTable_constants
+ip-mib/ipAddressTable/ipAddressTable_data_access
+ip-mib/ipAddressTable/ipAddressTable_interface
+ip-mib/ipDefaultRouterTable
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_data_access
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_data_get
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_enums
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_interface
+ip-mib/ipDefaultRouterTable/ipDefaultRouterTable_oids
+ip-mib/ipIfStatsTable
+ip-mib/ipIfStatsTable/ipIfStatsTable
+ip-mib/ipIfStatsTable/ipIfStatsTable_data_access
+ip-mib/ipIfStatsTable/ipIfStatsTable_data_get
+ip-mib/ipIfStatsTable/ipIfStatsTable_enums
+ip-mib/ipIfStatsTable/ipIfStatsTable_interface
+ip-mib/ipIfStatsTable/ipIfStatsTable_oids
+ip-mib/ipSystemStatsTable
+ip-mib/ipSystemStatsTable/ipSystemStatsTable
+ip-mib/ipSystemStatsTable/ipSystemStatsTable_constants
+ip-mib/ipSystemStatsTable/ipSystemStatsTable_data_access
+ip-mib/ipSystemStatsTable/ipSystemStatsTable_interface
+ip-mib/ip_scalars
+ip-mib/ipv4InterfaceTable
+ip-mib/ipv4InterfaceTable/ipv4InterfaceTable
+ip-mib/ipv4InterfaceTable/ipv4InterfaceTable_constants
+ip-mib/ipv4InterfaceTable/ipv4InterfaceTable_data_access
+ip-mib/ipv4InterfaceTable/ipv4InterfaceTable_interface
+ip-mib/ipv6InterfaceTable
+ip-mib/ipv6InterfaceTable/ipv6InterfaceTable
+ip-mib/ipv6InterfaceTable/ipv6InterfaceTable_constants
+ip-mib/ipv6InterfaceTable/ipv6InterfaceTable_data_access
+ip-mib/ipv6InterfaceTable/ipv6InterfaceTable_interface
+ip-mib/ipv6ScopeZoneIndexTable
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable_data_access
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable_enums
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable_interface
+ip-mib/ipv6ScopeZoneIndexTable/ipv6ScopeZoneIndexTable_oids
+mibII
+tunnel/tunnel
+	"
+	fi
+	;;
+esac
+
 if test "x$enable_agent" != "xno" ; then
   # If the agent should be built then use default_modules
   new_module_list="$new_module_list default_modules"

commit 11607e555a8b7a4fc92a271543b9c1a90d48922d

snmpd: Include mibII in default_modules.h unconditionally
In agent/mibgroup/default_modules.h, config_require(mibII) was wrapped in
#if !defined(__linux__) || defined(HAVE_NETLINK_ROUTE_ADDR_H). However,
HAVE_NETLINK_ROUTE_ADDR_H is defined in net-snmp-config.h, which is not
yet generated or included when default_modules.h is preprocessed during
./configure. As a result, mibII (and its submodules like vacm_conf) was
omitted from default agent builds on Linux.

Remove the preprocessor check in default_modules.h. The configuration
script configure.d/config_modules_agent already checks whether the
Netlink headers are available and excludes mibII when necessary.

Fixes: a6042e0f08e3 ("snmpd: Exclude certain MIBs if the Netlink library is not available")

agent/mibgroup/default_modules.h
@@ -23,9 +23,7 @@ config_require(mibII/vacm_conf);
 
 #else /* !NETSNMP_MINI_AGENT == the full shabang */
 
-#if !defined(__linux__) || defined(HAVE_NETLINK_ROUTE_ADDR_H)
 config_require(mibII);
-#endif
 config_require(ucd_snmp);
 config_require(notification);
 config_require(notification-log-mib);