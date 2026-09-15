net-snmp/net-snmp

commit b28cc6d96fcd876f6ec44b6aecfcb1a39123fc5b

libsnmp: Load OpenSSL 3.0+ providers
Load the 'legacy' and 'default' providers when building with OpenSSL 3.0 or
later. This is necessary to make legacy algorithms like DES available, as
they have been moved to the legacy provider and are no longer enabled by
default.

A shutdown callback is registered to unload the providers on library shutdown.

snmplib/scapi.c
@@ -83,12 +83,16 @@ netsnmp_feature_child_of(usm_scapi, usm_support);
 #endif
 
 #ifdef NETSNMP_USE_OPENSSL
+#include <openssl/opensslv.h>
 #include <openssl/hmac.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <openssl/des.h>
 #ifdef HAVE_AES
 #include <openssl/aes.h>
+#endif
+#ifdef HAVE_OPENSSL_PROVIDER_H
+#include <openssl/provider.h>
 #endif
 
 #ifndef NETSNMP_DISABLE_DES
@@ -535,6 +539,25 @@ sc_get_proper_priv_length_bytype(int privtype)
 }
 
 
+#if defined(NETSNMP_USE_OPENSSL) && defined(HAVE_OPENSSL_PROVIDER_H)
+static OSSL_PROVIDER *legacy_provider;
+static OSSL_PROVIDER *default_provider;
+
+static int
+sc_shutdown(int majorID, int minorID, void *serverarg, void *clientarg)
+{
+    if (legacy_provider) {
+        OSSL_PROVIDER_unload(legacy_provider);
+        legacy_provider = NULL;
+    }
+    if (default_provider) {
+        OSSL_PROVIDER_unload(default_provider);
+        default_provider = NULL;
+    }
+    return SNMPERR_SUCCESS;
+}
+#endif
+
 /*******************************************************************-o-******
  * sc_init
  *
@@ -546,7 +569,17 @@ sc_init(void)
 {
     int             rval = SNMPERR_SUCCESS;
 
-#if !defined(NETSNMP_USE_OPENSSL)
+#if defined(NETSNMP_USE_OPENSSL)
+#if defined(HAVE_OPENSSL_PROVIDER_H)
+    if (legacy_provider == NULL)
+        legacy_provider = OSSL_PROVIDER_load(NULL, "legacy");
+    if (default_provider == NULL)
+        default_provider = OSSL_PROVIDER_load(NULL, "default");
+    snmp_register_callback(SNMP_CALLBACK_LIBRARY,
+                           SNMP_CALLBACK_SHUTDOWN,
+                           sc_shutdown, NULL);
+#endif
+#else /* !NETSNMP_USE_OPENSSL */
 #if defined(NETSNMP_USE_INTERNAL_MD5) || defined(NETSNMP_USE_INTERNAL_CRYPTO)
     struct timeval  tv;
 
@@ -565,7 +598,7 @@ sc_init(void)
      * XXX ogud: The only reason to do anything here with openssl is to 
      * * XXX ogud: seed random number generator 
      */
-#endif                          /* ifndef NETSNMP_USE_OPENSSL */
+#endif                          /* NETSNMP_USE_OPENSSL */
 
     return rval;
 }                               /* end sc_init() */

configure
@@ -19901,6 +19901,12 @@ then :
   printf "%s\n" "#define HAVE_OPENSSL_OSSL_TYP_H 1" >>confdefs.h
 
 fi
+ac_fn_c_check_header_compile "$LINENO" "openssl/provider.h" "ac_cv_header_openssl_provider_h" "$ac_includes_default"
+if test "x$ac_cv_header_openssl_provider_h" = xyes
+then :
+  printf "%s\n" "#define HAVE_OPENSSL_PROVIDER_H 1" >>confdefs.h
+
+fi
 ac_fn_c_check_header_compile "$LINENO" "openssl/rand.h" "ac_cv_header_openssl_rand_h" "$ac_includes_default"
 if test "x$ac_cv_header_openssl_rand_h" = xyes
 then :

configure.d/config_os_headers
@@ -109,6 +109,7 @@ openssl/evp.h					\
 openssl/hmac.h					\
 openssl/objects.h				\
 openssl/ossl_typ.h				\
+openssl/provider.h				\
 openssl/rand.h					\
 openssl/ssl.h					\
 openssl/x509.h					\

include/net-snmp/net-snmp-config.h.in
@@ -766,6 +766,9 @@
 /* Define to 1 if you have the <openssl/ossl_typ.h> header file. */
 #undef HAVE_OPENSSL_OSSL_TYP_H
 
+/* Define to 1 if you have the <openssl/provider.h> header file. */
+#undef HAVE_OPENSSL_PROVIDER_H
+
 /* Define to 1 if you have the <openssl/rand.h> header file. */
 #undef HAVE_OPENSSL_RAND_H
 

commit c663067f696a74deeacf8b919010e23f2e7912f4

libsnmp: Check OpenSSL provider loading and avoid duplicate initialization
In sc_init(), OSSL_PROVIDER_load() return values were ignored, and
snmp_register_callback() was called unconditionally on every sc_init()
invocation without a guard, resulting in duplicate shutdown callbacks.

Track initialization state with a static sc_initialized variable, check
and warn if loading legacy or default providers fails, and register the
shutdown callback only once. Reset sc_initialized in sc_shutdown().

Fixes: b28cc6d96fcd ("libsnmp: Load OpenSSL 3.0+ providers")

snmplib/scapi.c
@@ -542,6 +542,7 @@ sc_get_proper_priv_length_bytype(int privtype)
 #if defined(NETSNMP_USE_OPENSSL) && defined(HAVE_OPENSSL_PROVIDER_H)
 static OSSL_PROVIDER *legacy_provider;
 static OSSL_PROVIDER *default_provider;
+static int sc_initialized;
 
 static int
 sc_shutdown(int majorID, int minorID, void *serverarg, void *clientarg)
@@ -554,6 +555,7 @@ sc_shutdown(int majorID, int minorID, void *serverarg, void *clientarg)
         OSSL_PROVIDER_unload(default_provider);
         default_provider = NULL;
     }
+    sc_initialized = 0;
     return SNMPERR_SUCCESS;
 }
 #endif
@@ -571,13 +573,18 @@ sc_init(void)
 
 #if defined(NETSNMP_USE_OPENSSL)
 #if defined(HAVE_OPENSSL_PROVIDER_H)
-    if (legacy_provider == NULL)
+    if (!sc_initialized) {
+        sc_initialized = 1;
         legacy_provider = OSSL_PROVIDER_load(NULL, "legacy");
-    if (default_provider == NULL)
+        if (legacy_provider == NULL)
+            snmp_log(LOG_WARNING, "Failed to load OpenSSL legacy provider\n");
         default_provider = OSSL_PROVIDER_load(NULL, "default");
-    snmp_register_callback(SNMP_CALLBACK_LIBRARY,
-                           SNMP_CALLBACK_SHUTDOWN,
-                           sc_shutdown, NULL);
+        if (default_provider == NULL)
+            snmp_log(LOG_WARNING, "Failed to load OpenSSL default provider\n");
+        snmp_register_callback(SNMP_CALLBACK_LIBRARY,
+                               SNMP_CALLBACK_SHUTDOWN,
+                               sc_shutdown, NULL);
+    }
 #endif
 #else /* !NETSNMP_USE_OPENSSL */
 #if defined(NETSNMP_USE_INTERNAL_MD5) || defined(NETSNMP_USE_INTERNAL_CRYPTO)