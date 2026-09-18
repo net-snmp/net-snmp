net-snmp/net-snmp

commit cdf51be6caf57943b3f0e5261353ebd89220f14c

NEWS: snmpd: Enable automatic reregistering of sysORTable entries from AgentX subagents. Add the sysORTable interface to the include directory. Rewrite the sysORTable module to use the table_container handler.
git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/trunk@16871 06827809-a52a-0410-b366-d66718629ded

agent/agent_sysORTable.c
@@ -1,0 +1,236 @@
+#include <net-snmp/net-snmp-config.h>
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#if HAVE_STRING_H
+#include <string.h>
+#else
+#include <strings.h>
+#endif
+#include <stddef.h>
+
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include <net-snmp/agent/agent_callbacks.h>
+#include <net-snmp/agent/agent_sysORTable.h>
+#include <net-snmp/agent/sysORTable.h>
+
+typedef struct data_node_s {
+    struct sysORTable data;
+    struct data_node_s* next;
+    struct data_node_s* prev;
+}* data_node;
+
+static data_node table = NULL;
+
+static void
+erase(data_node entry)
+{
+    entry->data.OR_uptime = netsnmp_get_agent_uptime();
+    DEBUGMSGTL(("agent/sysORTable", "UNREG_SYSOR %p\n", &entry->data));
+    snmp_call_callbacks(SNMP_CALLBACK_APPLICATION, SNMPD_CALLBACK_UNREG_SYSOR,
+                        &entry->data);
+    free(entry->data.OR_oid);
+    free((char*)entry->data.OR_descr);
+    if (entry->next == entry)
+        table = NULL;
+    else {
+        entry->next->prev = entry->prev;
+        entry->prev->next = entry->next;
+        if (entry == table)
+            table = entry->next;
+    }
+    free(entry);
+}
+
+void
+netsnmp_sysORTable_foreach(void (*f)(const struct sysORTable*, void*), void* c)
+{
+    DEBUGMSGTL(("agent/sysORTable", "foreach(%p, %p)\n", f, c));
+    if(table) {
+        data_node run = table;
+        do {
+            data_node tmp = run;
+            run = run->next;
+            f(&tmp->data, c);
+        } while(table && run != table);
+    }
+}
+
+int
+register_sysORTable_sess(oid * oidin,
+                         size_t oidlen,
+                         const char *descr, netsnmp_session * ss)
+{
+    data_node entry;
+
+    DEBUGMSGTL(("agent/sysORTable", "registering: "));
+    DEBUGMSGOID(("agent/sysORTable", oidin, oidlen));
+    DEBUGMSG(("agent/sysORTable", ", session %p\n", ss));
+
+    entry = calloc(1, sizeof(struct data_node_s));
+    if (entry == NULL) {
+        DEBUGMSGTL(("agent/sysORTable", "Failed to allocate new entry\n"));
+        return SYS_ORTABLE_REGISTRATION_FAILED;
+    }
+
+    entry->data.OR_descr = strdup(descr);
+    if (entry->data.OR_descr == NULL) {
+        DEBUGMSGTL(("agent/sysORTable", "Failed to allocate new sysORDescr\n"));
+        free(entry);
+        return SYS_ORTABLE_REGISTRATION_FAILED;
+    }
+
+    entry->data.OR_oid = (oid *) malloc(sizeof(oid) * oidlen);
+    if (entry->data.OR_oid == NULL) {
+        DEBUGMSGTL(("agent/sysORTable", "Failed to allocate new sysORID\n"));
+        free((char*)entry->data.OR_descr);
+        free(entry);
+        return SYS_ORTABLE_REGISTRATION_FAILED;
+    }
+
+    memcpy(entry->data.OR_oid, oidin, sizeof(oid) * oidlen);
+    entry->data.OR_oidlen = oidlen;
+    entry->data.OR_sess = ss;
+
+    if(table) {
+        entry->next = table;
+        entry->prev = table->prev;
+        table->prev->next = entry;
+        table->prev = entry;
+    } else
+        table = entry->next = entry->prev = entry;
+
+    entry->data.OR_uptime = netsnmp_get_agent_uptime();
+
+    snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
+                        SNMPD_CALLBACK_REG_SYSOR, &entry->data);
+
+    return SYS_ORTABLE_REGISTERED_OK;
+}
+
+int
+register_sysORTable(oid * oidin, size_t oidlen, const char *descr)
+{
+    return register_sysORTable_sess(oidin, oidlen, descr, NULL);
+}
+
+int
+unregister_sysORTable_sess(oid * oidin,
+                           size_t oidlen, netsnmp_session * ss)
+{
+    int any_unregistered = 0;
+
+    DEBUGMSGTL(("agent/sysORTable", "sysORTable unregistering: "));
+    DEBUGMSGOID(("agent/sysORTable", oidin, oidlen));
+    DEBUGMSG(("agent/sysORTable", ", session %p\n", ss));
+
+    if(table) {
+        data_node run = table;
+        do {
+            data_node tmp = run;
+            run = run->next;
+            if (tmp->data.OR_sess == ss &&
+                snmp_oid_compare(oidin, oidlen,
+                                 tmp->data.OR_oid, tmp->data.OR_oidlen) == 0) {
+                erase(tmp);
+                any_unregistered = 1;
+            }
+        } while(table && run != table);
+    }
+
+    if (any_unregistered) {
+        DEBUGMSGTL(("agent/sysORTable", "unregistering successfull\n"));
+        return SYS_ORTABLE_UNREGISTERED_OK;
+    } else {
+        DEBUGMSGTL(("agent/sysORTable", "unregistering failed\n"));
+        return SYS_ORTABLE_NO_SUCH_REGISTRATION;
+    }
+}
+
+
+int
+unregister_sysORTable(oid * oidin, size_t oidlen)
+{
+    return unregister_sysORTable_sess(oidin, oidlen, NULL);
+}
+
+
+void
+unregister_sysORTable_by_session(netsnmp_session * ss)
+{
+    DEBUGMSGTL(("agent/sysORTable",
+                "sysORTable unregistering session %p\n", ss));
+
+   if(table) {
+        data_node run = table;
+        do {
+            data_node tmp = run;
+            run = run->next;
+            if (((ss->flags & SNMP_FLAGS_SUBSESSION) &&
+                 tmp->data.OR_sess == ss) ||
+                (!(ss->flags & SNMP_FLAGS_SUBSESSION) && tmp->data.OR_sess &&
+                 tmp->data.OR_sess->subsession == ss))
+                erase(tmp);
+        } while(table && run != table);
+    }
+
+    DEBUGMSGTL(("agent/sysORTable",
+                "sysORTable unregistering session %p done\n", ss));
+}
+
+static int
+register_sysOR_callback(int majorID, int minorID, void *serverarg,
+                        void *clientarg)
+{
+    struct sysORTable *parms = (struct sysORTable *) serverarg;
+
+    return register_sysORTable_sess(parms->OR_oid, parms->OR_oidlen,
+                                    parms->OR_descr, parms->OR_sess);
+}
+
+static int
+unregister_sysOR_by_session_callback(int majorID, int minorID,
+                                     void *serverarg, void *clientarg)
+{
+    netsnmp_session *session = (netsnmp_session *) serverarg;
+
+    unregister_sysORTable_by_session(session);
+
+    return 0;
+}
+
+static int
+unregister_sysOR_callback(int majorID, int minorID, void *serverarg,
+                          void *clientarg)
+{
+    struct sysORTable *parms = (struct sysORTable *) serverarg;
+
+    return unregister_sysORTable_sess(parms->OR_oid,
+                                      parms->OR_oidlen,
+                                      parms->OR_sess);
+}
+
+void
+init_agent_sysORTable(void)
+{
+    DEBUGMSGTL(("agent/sysORTable", "init_agent_sysORTable\n"));
+
+    snmp_register_callback(SNMP_CALLBACK_APPLICATION,
+                           SNMPD_CALLBACK_REQ_REG_SYSOR,
+                           register_sysOR_callback, NULL);
+    snmp_register_callback(SNMP_CALLBACK_APPLICATION,
+                           SNMPD_CALLBACK_REQ_UNREG_SYSOR,
+                           unregister_sysOR_callback, NULL);
+    snmp_register_callback(SNMP_CALLBACK_APPLICATION,
+                           SNMPD_CALLBACK_REQ_UNREG_SYSOR_SESS,
+                           unregister_sysOR_by_session_callback, NULL);
+}
+
+void
+shutdown_agent_sysORTable(void)
+{
+    DEBUGMSGTL(("agent/sysORTable", "shutdown_sysORTable\n"));
+    while(table)
+        erase(table);
+}

README.agent-mibs
@@ -40,6 +40,7 @@ Table Style
   G  scalar group helper
   W  watched scalar 
   M  mfd
+  C  container table
 
 Platform Keys
 ------------------
@@ -65,7 +66,7 @@ A number after a platform indicates notes at the bottom of the file.
 SNMPv2-MIB
  system.?.0                   A         5.5     W mibII/system_mib.c
  .sysORLastChange.0           A         5.5     W mibII/sysORTable.c
- sysORTable                   A         3.4     O mibII/sysORTable.c
+ sysORTable                   A         5.5     C mibII/sysORTable.c
  snmp.*.0                     A         5.5     G mibII/snmp_mib.c
  setSerialNo.0                A         5.0     W mibII/setSerialNo.c
 

agent/Makefile.in
@@ -25,6 +25,7 @@ INCLUDESUBDIR=agent
 INCLUDESUBDIRHEADERS=agent_read_config.h \
 	agent_registry.h \
 	agent_index.h \
+	agent_sysORTable.h \
 	agent_trap.h \
 	auto_nlist.h \
 	ds_agent.h \
@@ -34,7 +35,8 @@ INCLUDESUBDIRHEADERS=agent_read_config.h \
 	agent_handler.h \
 	net-snmp-agent-includes.h \
 	mib_modules.h \
-	agent_callbacks.h
+	agent_callbacks.h \
+	sysORTable.h
 
 INSTALLBUILTSUBDIRHEADERS=../include/net-snmp/agent/mib_module_config.h \
 			  ../include/net-snmp/agent/agent_module_config.h \
@@ -60,6 +62,7 @@ INSTALLUCDHEADERS= \
 	agent_index.h	\
 	agent_read_config.h \
 	agent_registry.h \
+	agent_sysORTable.h \
 	agent_trap.h	\
 	auto_nlist.h	\
 	ds_agent.h	\
@@ -104,11 +107,11 @@ MIBOBJS		= $(mibgroup_list_o) mib_modules.o auto_nlist.o
 
 # libnetsnmpagent objects
 LIBAGENTOBJS=snmp_agent.o snmp_vars.o agent_read_config.o \
-	agent_registry.o agent_index.o agent_trap.o kernel.o \
-	 agent_handler.o $(agentgroup_list_o) @OTHERAGENTLIBOBJS@
+	agent_registry.o agent_index.o agent_sysORTable.o agent_trap.o \
+	kernel.o agent_handler.o $(agentgroup_list_o) @OTHERAGENTLIBOBJS@
 LLIBAGENTOBJS=snmp_agent.lo snmp_vars.lo agent_read_config.lo \
-	agent_registry.lo agent_index.lo agent_trap.lo kernel.lo \
-	agent_handler.lo $(agentgroup_list_lo) @OTHERAGENTLIBLOBJS@
+	agent_registry.lo agent_index.lo agent_sysORTable.lo agent_trap.lo \
+	kernel.lo agent_handler.lo $(agentgroup_list_lo) @OTHERAGENTLIBLOBJS@
 
 # The agent objects.
 AGENTOBJS=snmpd.o @other_agentobjs@

agent/mibgroup/agentx/master.h
@@ -7,7 +7,6 @@ config_require(agentx/protocol)
 config_require(agentx/client)
 config_require(agentx/master_admin)
 config_require(agentx/agentx_config)
-config_require(mibII/sysORTable)
 
      void            init_master(void);
      void            real_init_master(void);

agent/mibgroup/agentx/master_admin.c
@@ -45,7 +45,7 @@
 #include <net-snmp/agent/agent_index.h>
 #include <net-snmp/agent/agent_trap.h>
 #include <net-snmp/agent/agent_callbacks.h>
-#include "mibII/sysORTable.h"
+#include <net-snmp/agent/agent_sysORTable.h>
 #include "master.h"
 
 extern struct timeval starttime;
@@ -149,9 +149,7 @@ close_agentx_session(netsnmp_session * session, int sessid)
                 
         unregister_mibs_by_session(session);
         unregister_index_by_session(session);
-        snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
-                            SNMPD_CALLBACK_REQ_UNREG_SYSOR_SESS,
-                            (void*)session);
+        unregister_sysORTable_by_session(session);
 	SNMP_FREE(session->myvoid);
         return AGENTX_ERR_NOERROR;
     }
@@ -163,9 +161,7 @@ close_agentx_session(netsnmp_session * session, int sessid)
         if (sp->sessid == sessid) {
             unregister_mibs_by_session(sp);
             unregister_index_by_session(sp);
-            snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
-                                SNMPD_CALLBACK_REQ_UNREG_SYSOR_SESS,
-                                (void*)sp);
+            unregister_sysORTable_by_session(sp);
 
             *prevNext = sp->next;
 
@@ -380,20 +376,17 @@ int
 add_agent_caps_list(netsnmp_session * session, netsnmp_pdu *pdu)
 {
     netsnmp_session *sp;
-    struct sysORTable parms;
+    char* description;
 
     sp = find_agentx_session(session, pdu->sessid);
     if (sp == NULL)
         return AGENTX_ERR_NOT_OPEN;
 
-    parms.OR_oid = pdu->variables->name;
-    parms.OR_oidlen = pdu->variables->name_length;
-    parms.OR_descr  = netsnmp_strdup_and_null(pdu->variables->val.string,
-                                              pdu->variables->val_len);
-    parms.OR_sess = sp;
-    snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
-                        SNMPD_CALLBACK_REQ_REG_SYSOR, (void*)&parms);
-    free(parms.OR_descr);
+    description = netsnmp_strdup_and_null(pdu->variables->val.string,
+                                          pdu->variables->val_len);
+    register_sysORTable_sess(pdu->variables->name, pdu->variables->name_length,
+                             description, sp);
+    free(description);
     return AGENTX_ERR_NOERROR;
 }
 
@@ -401,22 +394,18 @@ int
 remove_agent_caps_list(netsnmp_session * session, netsnmp_pdu *pdu)
 {
     netsnmp_session *sp;
-    struct sysORTable parms;
+    int rc;
 
     sp = find_agentx_session(session, pdu->sessid);
     if (sp == NULL)
         return AGENTX_ERR_NOT_OPEN;
 
-    parms.OR_oid = pdu->variables->name;
-    parms.OR_oidlen = pdu->variables->name_length;
-    parms.OR_sess = sp;
-    snmp_call_callbacks(SNMP_CALLBACK_APPLICATION,
-                        SNMPD_CALLBACK_REQ_UNREG_SYSOR, (void*)&parms);
-    /*
-     * no easy way to get an error code...
-     * if (rc < 0)
-     *   return AGENTX_ERR_UNKNOWN_AGENTCAPS;
-     */
+    rc = unregister_sysORTable_sess(pdu->variables->name,
+                                    pdu->variables->name_length, sp);
+
+    if (rc < 0)
+      return AGENTX_ERR_UNKNOWN_AGENTCAPS;
+
     return AGENTX_ERR_NOERROR;
 }
 

agent/mibgroup/agentx/subagent.c
@@ -46,9 +46,7 @@
 #include "agentx/agentx_config.h"
 #include <net-snmp/agent/agent_callbacks.h>
 #include <net-snmp/agent/agent_trap.h>
-#ifdef USING_MIBII_SYSORTABLE_MODULE
-#include "mibII/sysORTable.h"
-#endif
+#include <net-snmp/agent/sysORTable.h>
 
 #include "subagent.h"
 
@@ -638,7 +636,6 @@ agentx_registration_callback(int majorID, int minorID, void *serverarg,
 }
 
 
-#ifdef USING_MIBII_SYSORTABLE_MODULE
 int
 agentx_sysOR_callback(int majorID, int minorID, void *serverarg,
                       void *clientarg)
@@ -656,7 +653,6 @@ agentx_sysOR_callback(int majorID, int minorID, void *serverarg,
                                        reg_parms->name,
                                        reg_parms->namelen);
 }
-#endif
 
 
 static int
@@ -695,14 +691,12 @@ agentx_register_callbacks(netsnmp_session * s)
     snmp_register_callback(SNMP_CALLBACK_APPLICATION,
                            SNMPD_CALLBACK_UNREGISTER_OID,
                            agentx_registration_callback, s);
-#ifdef USING_MIBII_SYSORTABLE_MODULE
     snmp_register_callback(SNMP_CALLBACK_APPLICATION,
                            SNMPD_CALLBACK_REG_SYSOR,
                            agentx_sysOR_callback, s);
     snmp_register_callback(SNMP_CALLBACK_APPLICATION,
                            SNMPD_CALLBACK_UNREG_SYSOR,
                            agentx_sysOR_callback, s);
-#endif
 }
 
 /*
@@ -722,15 +716,12 @@ agentx_unregister_callbacks(netsnmp_session * ss)
     snmp_unregister_callback(SNMP_CALLBACK_APPLICATION,
                              SNMPD_CALLBACK_UNREGISTER_OID,
                              agentx_registration_callback, ss, 1);
-#ifdef USING_MIBII_SYSORTABLE_MODULE
     snmp_unregister_callback(SNMP_CALLBACK_APPLICATION,
                              SNMPD_CALLBACK_REG_SYSOR,
                              agentx_sysOR_callback, ss, 1);
     snmp_unregister_callback(SNMP_CALLBACK_APPLICATION,
                              SNMPD_CALLBACK_UNREG_SYSOR,
                              agentx_sysOR_callback, ss, 1);
-#endif
-
 }
 
 /*
@@ -847,6 +838,11 @@ subagent_open_master_session(void)
     return 0;
 }
 
+static void
+agentx_reopen_sysORTable(const struct sysORTable* data, void* v)
+{
+  agentx_sysOR_callback(0, SNMPD_CALLBACK_REG_SYSOR, data, v);
+}
 
 /*
  * Alarm callback function to open a session to the master agent.  If a
@@ -873,6 +869,11 @@ agentx_reopen_session(unsigned int clientreg, void *clientarg)
          * Reregister all our nodes.  
          */
         register_mib_reattach();
+
+        /*
+         * Reregister all our sysOREntries
+         */
+        netsnmp_sysORTable_foreach(&agentx_reopen_sysORTable, main_session);
 
         /*
          * Register a ping alarm (if need be).  

agent/mibgroup/mibII/icmp.c
@@ -18,10 +18,10 @@
 
 #include <net-snmp/agent/cache_handler.h>
 #include <net-snmp/agent/scalar_group.h>
+#include <net-snmp/agent/sysORTable.h>
 
 #include "util_funcs.h"
 #include "icmp.h"
-#include "sysORTable.h"
 
 #ifndef MIB_STATS_CACHE_TIMEOUT
 #define MIB_STATS_CACHE_TIMEOUT	5

agent/mibgroup/mibII/interfaces.c
@@ -197,12 +197,12 @@
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/agent/auto_nlist.h>
+#include <net-snmp/agent/sysORTable.h>
 #include <net-snmp/data_access/interface.h>
 
 #include "interfaces.h"
 #include "struct.h"
 #include "util_funcs.h"
-#include "sysORTable.h"
 
 /* if you want caching enabled for speed retrival purposes, set this to 5?*/
 #define MINLOADFREQ 0                     /* min reload frequency in seconds */

commit a29a63ace5a81fcb34d188f8cfb595b90c22846e

agent: Fix heap-use-after-free and list iteration in sysORTable
In agent_sysORTable.c, `table` was implemented as a circular doubly
linked list. The iteration loops in unregister_sysORTable_by_session()
and unregister_sysORTable_sess() checked `while (table && run != table)`.
When the head node `table` was removed by erase(), erase() updated `table`
to `entry->next`. Because `run` had already advanced to `entry->next`,
`run != table` became false, terminating the loop prematurely after
erasing only the first matching node.

Furthermore, unregister_sysORTable_by_session() only matched session
entries when `tmp->data.OR_sess == ss` for subsessions, and checked
`tmp->data.OR_sess->subsession == ss` for non-subsessions, failing to
match non-subsession direct registrations (`tmp->data.OR_sess == ss`).

When closed sessions were subsequently freed, the remaining unerased
nodes in `table` retained dangling session pointers. Subsequent calls to
unregister_sysORTable_by_session() then dereferenced `OR_sess->subsession`,
causing an AddressSanitizer heap-use-after-free read.

Convert `table` to a standard null-terminated linear doubly-linked list
with head (`table`) and tail (`table_tail`) pointers. Update erase(),
netsnmp_sysORTable_foreach(), unregister_sysORTable_sess(), and
unregister_sysORTable_by_session() to safely iterate through the list
without premature loop termination. Also ensure direct session matches
are checked for all session types and guard against NULL session pointers.

Fixes: https://issues.oss-fuzz.com/issues/563134244
Fixes: cdf51be6caf5 ("NEWS: snmpd: Enable automatic reregistering of sysORTable entries from AgentX subagents. Add the sysORTable interface to the include directory. Rewrite the sysORTable module to use the table_container handler.")

agent/agent_sysORTable.c
@@ -22,6 +22,7 @@ typedef struct data_node_s {
 }* data_node;
 
 static data_node table = NULL;
+static data_node table_tail = NULL;
 
 static void
 erase(data_node entry)
@@ -32,28 +33,27 @@ erase(data_node entry)
                         &entry->data);
     free(entry->data.OR_oid);
     free(entry->data.OR_descr);
-    if (entry->next == entry)
-        table = NULL;
-    else {
-        entry->next->prev = entry->prev;
+    if (entry->prev)
         entry->prev->next = entry->next;
-        if (entry == table)
-            table = entry->next;
-    }
+    else
+        table = entry->next;
+    if (entry->next)
+        entry->next->prev = entry->prev;
+    else
+        table_tail = entry->prev;
     free(entry);
 }
 
 void
 netsnmp_sysORTable_foreach(void (*f)(const struct sysORTable*, void*), void* c)
 {
+    data_node run = table;
+
     DEBUGMSGTL(("agent/sysORTable", "foreach(%p, %p)\n", f, c));
-    if(table) {
-        data_node run = table;
-        do {
-            data_node tmp = run;
-            run = run->next;
-            f(&tmp->data, c);
-        } while(table && run != table);
+    while (run) {
+        data_node next = run->next;
+        f(&run->data, c);
+        run = next;
     }
 }
 
@@ -93,13 +93,13 @@ register_sysORTable_sess(oid * oidin,
     entry->data.OR_oidlen = oidlen;
     entry->data.OR_sess = ss;
 
-    if(table) {
-        entry->next = table;
-        entry->prev = table->prev;
-        table->prev->next = entry;
-        table->prev = entry;
-    } else
-        table = entry->next = entry->prev = entry;
+    if (table == NULL) {
+        table = table_tail = entry;
+    } else {
+        table_tail->next = entry;
+        entry->prev = table_tail;
+        table_tail = entry;
+    }
 
     entry->data.OR_uptime = netsnmp_get_agent_uptime();
 
@@ -119,24 +119,22 @@ int
 unregister_sysORTable_sess(oid * oidin,
                            size_t oidlen, netsnmp_session * ss)
 {
+    data_node run = table;
     int any_unregistered = 0;
 
     DEBUGMSGTL(("agent/sysORTable", "sysORTable unregistering: "));
     DEBUGMSGOID(("agent/sysORTable", oidin, oidlen));
     DEBUGMSG(("agent/sysORTable", ", session %p\n", ss));
 
-    if(table) {
-        data_node run = table;
-        do {
-            data_node tmp = run;
-            run = run->next;
-            if (tmp->data.OR_sess == ss &&
-                snmp_oid_compare(oidin, oidlen,
-                                 tmp->data.OR_oid, tmp->data.OR_oidlen) == 0) {
-                erase(tmp);
-                any_unregistered = 1;
-            }
-        } while(table && run != table);
+    while (run) {
+        data_node next = run->next;
+        if (run->data.OR_sess == ss &&
+            snmp_oid_compare(oidin, oidlen,
+                             run->data.OR_oid, run->data.OR_oidlen) == 0) {
+            erase(run);
+            any_unregistered = 1;
+        }
+        run = next;
     }
 
     if (any_unregistered) {
@@ -159,20 +157,21 @@ unregister_sysORTable(oid * oidin, size_t oidlen)
 void
 unregister_sysORTable_by_session(netsnmp_session * ss)
 {
+    data_node run = table;
+
     DEBUGMSGTL(("agent/sysORTable",
                 "sysORTable unregistering session %p\n", ss));
 
-   if(table) {
-        data_node run = table;
-        do {
-            data_node tmp = run;
-            run = run->next;
-            if (((ss->flags & SNMP_FLAGS_SUBSESSION) &&
-                 tmp->data.OR_sess == ss) ||
-                (!(ss->flags & SNMP_FLAGS_SUBSESSION) && tmp->data.OR_sess &&
-                 tmp->data.OR_sess->subsession == ss))
-                erase(tmp);
-        } while(table && run != table);
+    if (!ss)
+        return;
+
+    while (run) {
+        data_node next = run->next;
+        if (run->data.OR_sess == ss ||
+            (!(ss->flags & SNMP_FLAGS_SUBSESSION) && run->data.OR_sess &&
+             run->data.OR_sess->subsession == ss))
+            erase(run);
+        run = next;
     }
 
     DEBUGMSGTL(("agent/sysORTable",